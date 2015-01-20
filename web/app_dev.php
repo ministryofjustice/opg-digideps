edd147b98a339806bcd6c40b2eb7a9ac8f0ec265
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-01-20 17:08:36 +0000

    phing

--- web/app_dev.php
+++ web/app_dev.php
@@ -1,5 +1,7 @@
 <?php
 
+ini_set("display_errors", "on");
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
@@ -9,13 +11,6 @@ use Symfony\Component\Debug\Debug;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
-) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
-}
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 Debug::enable();
