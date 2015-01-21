b99b92ef7ebf0d6d365d043a1d5fec3893ec06e7
Paul Oforduru <paul.oforduru@digital.justice.gov.uk>
2015-01-21 15:05:55 +0000

    installed guzzle

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -12,6 +12,9 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
+        $client = $this->get('restclient');
+        $response = $client->get('http://symfony.com');
+        print_r($response->getStatusCode()); die;
         return $this->render('default/index.html.twig');
     }
 }
