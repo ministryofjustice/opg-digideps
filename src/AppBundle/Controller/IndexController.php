008911d2e0bd73806041a23429f6b9502043b08f
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-02-05 15:32:54 +0000

    fix

--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\SecurityContextInterface;
+use Symfony\Component\HttpFoundation\Response;
 
 class IndexController extends Controller
 {
