26caeb2440642447bf52cb075f22acc7dd41c304
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-02-16 16:44:31 +0000

    /role endpoint to get all the roles

---
+++ src/AppBundle/Controller/RoleController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use AppBundle\Entity\Role;
+/**
+ * @Route("/role")
+ */
+class RoleController extends RestController
+{
+    /**
+     * @Route("")
+     * @Method({"GET"})
+     */
+    public function getAll()
+    {
+        return $this->getRepository('Role')->findAll();
+    }
+
+}
