29585032dbf178aa17e92c2fd7d0a256ff6edf79
Nick Middleweek <nick.middleweek@digital.justice.gov.uk>
2015-02-17 23:00:28 +0000

    DD-203| * Create twig extension for form_select(). * Style dropdown. * Needs validating with Tom for other browsers.

--- src/AppBundle/Twig/FormFieldsExtension.php
+++ src/AppBundle/Twig/FormFieldsExtension.php
@@ -21,7 +21,8 @@ class FormFieldsExtension extends \Twig_Extension
     {
         return [
                   'form_input' => new \Twig_Function_Method($this, 'renderFormInput'),
-                  'form_submit' => new \Twig_Function_Method($this, 'renderFormSubmit'),
+            'form_submit' => new \Twig_Function_Method($this, 'renderFormSubmit'),
+            'form_dropDown' => new \Twig_Function_Method($this, 'renderFormDropDown'),
                   'step_progress_class' => new \Twig_Function_Method($this, 'stepProgressClass'),
                ];
     }
@@ -49,10 +50,10 @@ class FormFieldsExtension extends \Twig_Extension
         
         return implode(' ', $return);
     }
-    
+
     /**
      * Renders form input field
-     * 
+     *
      * @param type $element
      * @param type $elementName
      * @param type $transIndex
@@ -63,22 +64,54 @@ class FormFieldsExtension extends \Twig_Extension
         //lets get the translation for hintText, labelClass and labelText
         $translationKey = (!is_null($transIndex))? $transIndex.'.'.$elementName : $elementName;
         $domain = $element->parent->vars['translation_domain'];
-        
+
         //sort hint text translation
         $hintTextTrans =  $this->translator->trans($translationKey.'.hint', [],$domain);
         $hintText =  ($hintTextTrans != $translationKey.'.hint')? $hintTextTrans: null;
-       
+
         //sort out labelText translation
         $labelText = isset($vars['labelText'])? $vars['labelText']: $this->translator->trans($translationKey.'.label',[],$domain);
-        
+
         $labelClass = isset($vars['labelClass']) ? $vars['labelClass']: null;
-        
+
         //generate input field html using variables supplied
-        $html = $this->environment->render('AppBundle:Components/Form:_input.html.twig', [ 'labelText' => $labelText, 
-                                                                                           'hintText' => $hintText,
-                                                                                           'element'  => $element,
-                                                                                           'labelClass' => $labelClass
-                                                                                         ]);
+        $html = $this->environment->render('AppBundle:Components/Form:_input.html.twig', [ 'labelText' => $labelText,
+            'hintText' => $hintText,
+            'element'  => $element,
+            'labelClass' => $labelClass
+        ]);
+        echo $html;
+    }
+
+    /**
+     * Renders form select element
+     *
+     * @param type $element
+     * @param type $elementName
+     * @param type $transIndex
+     * @param array $vars
+     */
+    public function renderFormDropDown($element, $elementName,array $vars = [], $transIndex = null )
+    {
+        //lets get the translation for hintText, labelClass and labelText
+        $translationKey = (!is_null($transIndex))? $transIndex.'.'.$elementName : $elementName;
+        $domain = $element->parent->vars['translation_domain'];
+
+        //sort hint text translation
+        $hintTextTrans =  $this->translator->trans($translationKey.'.hint', [],$domain);
+        $hintText =  ($hintTextTrans != $translationKey.'.hint')? $hintTextTrans: null;
+
+        //sort out labelText translation
+        $labelText = isset($vars['labelText'])? $vars['labelText']: $this->translator->trans($translationKey.'.label',[],$domain);
+
+        $labelClass = isset($vars['labelClass']) ? $vars['labelClass']: null;
+
+        //generate select field html using variables supplied
+        $html = $this->environment->render('AppBundle:Components/Form:_select.html.twig', [ 'labelText' => $labelText,
+            'hintText' => $hintText,
+            'element'  => $element,
+            'labelClass' => $labelClass
+        ]);
         echo $html;
     }
     
