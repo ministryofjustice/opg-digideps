0368e78ada3b66a306fa30e867c70cd4fe77c101
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-02-18 10:57:09 +0000

    move twig method repeated login into private method

--- src/AppBundle/Twig/FormFieldsExtension.php
+++ src/AppBundle/Twig/FormFieldsExtension.php
@@ -51,6 +51,7 @@ class FormFieldsExtension extends \Twig_Extension
         return implode(' ', $return);
     }
 
+    
     /**
      * Renders form input field
      *
@@ -61,27 +62,11 @@ class FormFieldsExtension extends \Twig_Extension
      */
     public function renderFormInput($element, $elementName,array $vars = [], $transIndex = null )
     {
-        //lets get the translation for hintText, labelClass and labelText
-        $translationKey = (!is_null($transIndex))? $transIndex.'.'.$elementName : $elementName;
-        $domain = $element->parent->vars['translation_domain'];
-
-        //sort hint text translation
-        $hintTextTrans =  $this->translator->trans($translationKey.'.hint', [],$domain);
-        $hintText =  ($hintTextTrans != $translationKey.'.hint')? $hintTextTrans: null;
-
-        //sort out labelText translation
-        $labelText = isset($vars['labelText'])? $vars['labelText']: $this->translator->trans($translationKey.'.label',[],$domain);
-
-        $labelClass = isset($vars['labelClass']) ? $vars['labelClass']: null;
-
         //generate input field html using variables supplied
-        $html = $this->environment->render('AppBundle:Components/Form:_input.html.twig', [ 
-            'labelText' => $labelText,
-            'hintText' => $hintText,
-            'element'  => $element,
-            'labelClass' => $labelClass
-        ]);
-        echo $html;
+        echo $this->environment->render(
+            'AppBundle:Components/Form:_input.html.twig', 
+            $this->getFormComponentTwigVariables($element, $elementName, $vars, $transIndex)
+        );
     }
 
     /**
@@ -94,28 +79,13 @@ class FormFieldsExtension extends \Twig_Extension
      */
     public function renderFormDropDown($element, $elementName,array $vars = [], $transIndex = null )
     {
-        //lets get the translation for hintText, labelClass and labelText
-        $translationKey = (!is_null($transIndex))? $transIndex.'.'.$elementName : $elementName;
-        $domain = $element->parent->vars['translation_domain'];
-
-        //sort hint text translation
-        $hintTextTrans =  $this->translator->trans($translationKey.'.hint', [],$domain);
-        $hintText =  ($hintTextTrans != $translationKey.'.hint')? $hintTextTrans: null;
-
-        //sort out labelText translation
-        $labelText = isset($vars['labelText'])? $vars['labelText']: $this->translator->trans($translationKey.'.label',[],$domain);
-
-        $labelClass = isset($vars['labelClass']) ? $vars['labelClass']: null;
-
-        //generate select field html using variables supplied
-        $html = $this->environment->render('AppBundle:Components/Form:_select.html.twig', [ 
-            'labelText' => $labelText,
-            'hintText' => $hintText,
-            'element'  => $element,
-            'labelClass' => $labelClass
-        ]);
-        echo $html;
+        //generate input field html using variables supplied
+        echo $this->environment->render(
+            'AppBundle:Components/Form:_select.html.twig', 
+            $this->getFormComponentTwigVariables($element, $elementName, $vars, $transIndex)
+        );
     }
+    
     
     /**
      * @param type $element
@@ -145,5 +115,38 @@ class FormFieldsExtension extends \Twig_Extension
     public function getName()
     {
         return 'form_input_extension';
+    }
+    
+    
+    
+    /**
+     * @param \Symfony\Component\Form\FormView $element
+     * @param string $elementName
+     * @param array $vars
+     * @param string|null $transIndex
+     * 
+     * @return array with vars labelText,hintText,element,labelClass, to pass into twig templates AppBundle:Components/Form:*
+     */
+    private function getFormComponentTwigVariables($element, $elementName, array $vars, $transIndex)
+    {
+        //lets get the translation for hintText, labelClass and labelText
+        $translationKey = (!is_null($transIndex))? $transIndex.'.'.$elementName : $elementName;
+        $domain = $element->parent->vars['translation_domain'];
+
+        //sort hint text translation
+        $hintTextTrans =  $this->translator->trans($translationKey.'.hint', [],$domain);
+        $hintText =  ($hintTextTrans != $translationKey.'.hint')? $hintTextTrans: null;
+
+        //sort out labelText translation
+        $labelText = isset($vars['labelText'])? $vars['labelText']: $this->translator->trans($translationKey.'.label',[],$domain);
+
+        $labelClass = isset($vars['labelClass']) ? $vars['labelClass']: null;
+        
+        return [ 
+            'labelText' => $labelText,
+            'hintText' => $hintText,
+            'element'  => $element,
+            'labelClass' => $labelClass
+        ];
     }
 }
