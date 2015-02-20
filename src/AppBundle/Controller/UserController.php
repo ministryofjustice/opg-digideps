ccb69ae581c96ef2df680c05a9649b6ba044ddb5
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-02-20 11:28:29 +0000

    move 48h setting for token into YML param

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -31,7 +31,9 @@ class UserController extends Controller
         // check $token is correct
         $user = $apiClient->getEntity('User', 'find_user_by_token', [ 'query' => [ 'token' => $token ] ]); /* @var $user User*/
         
-        if (!$user->isTokenSentInTheLastHours(48)) {
+        $hoursExpires = $this->container->hasParameter('token_expires_hours')
+                        ? $this->container->getParameter('token_expires_hours') : 48;
+        if (!$user->isTokenSentInTheLastHours($hoursExpires)) {
             throw new \RuntimeException("token expired, require new link");
         }
         
