9ed222a38f690011c01558d4f7afd537d0d0bc4a
Paul Oforduru <paul.oforduru@digital.justice.gov.uk>
2015-02-20 16:51:09 +0000

    changing migration script 5 to 8

--- app/DoctrineMigrations/Version8.php
+++ app/DoctrineMigrations/Version8.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version5 extends AbstractMigration
+class Version8 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
