7448983553827cfe4eef4d80c0e31bfa47a8585a
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-02-24 15:28:54 +0000

    /client/get-by-user-id/ endpoint + fix client county not saved

--- src/AppBundle/Controller/ClientController.php
+++ src/AppBundle/Controller/ClientController.php
@@ -5,6 +5,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Entity\Client;
+use AppBundle\Exception\NotFound;
 
 /**
  * @Route("/client")
@@ -20,12 +21,18 @@ class ClientController extends RestController
         $clientData = $this->deserializeBodyContent();
         
         $user = $this->getRepository('User')->find($clientData['user']);
-        
         if(empty($user)){
             throw new \Exception("User with id: $user does not exist");
         }
         
-        $client = new Client();
+        if (count($user->getClients()) === 0) {
+            $client = new Client();
+            $client->addUser($user);
+        } else {
+            // update
+            $client = $user->getClients()->first();
+        }
+        
         $client->setFirstname($clientData['firstname']);
         $client->setLastname($clientData['lastname']);
         $client->setCaseNumber($clientData['case_number']);
@@ -35,12 +42,27 @@ class ClientController extends RestController
         $client->setAddress2($clientData['address2']);
         $client->setPostcode($clientData['postcode']);
         $client->setCountry($clientData['country']);
+        $client->setCounty($clientData['county']);
         $client->setPhone($clientData['phone']);
-        $client->addUser($user);
         
         $this->getEntityManager()->persist($client);
         $this->getEntityManager()->flush();
         
         return ['client' => $client ];
+    }
+    
+     /**
+     * @Route("/get-by-user-id/{userId}")
+     * @Method({"GET"})
+     */
+    public function getByUserId($userId)
+    {
+        $user = $this->findEntityBy('User', $userId, "User not found");
+        
+        if (count($user->getClients()) === 0) {
+            throw new NotFound("User has no clients");
+        }
+        
+        return $this->findEntityBy('Client', $user->getClients()->first()->getId(), "Client not found");
     }
 }
