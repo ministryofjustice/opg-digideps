95e9f7db1f8901f7e9d965a2e6740c418dae3fd7
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-03-04 11:13:25 +0000

    move decision into report controller + align naming of form

--- src/AppBundle/Controller/ReportController.php
+++ src/AppBundle/Controller/ReportController.php
@@ -6,6 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use AppBundle\Form\ReportType;
 use AppBundle\Entity\Report;
+use AppBundle\Entity\Decision;
 use AppBundle\Service\ApiClient;
 use AppBundle\Form as FormDir;
 use AppBundle\Entity as EntityDir;
@@ -111,4 +112,42 @@ class ReportController extends Controller
     {
         return [ ];
     }
+    
+    /**
+     * @Route("/decision/{reportId}", name="add_decision")
+     * @Template("AppBundle:Decision:list.html.twig")
+     */
+    public function addDecisionAction($reportId)
+    {
+        $request = $this->getRequest();
+        $apiClient = $this->get('apiclient'); /* @var $apiClient ApiClient */
+        
+        // just needed for title etc,
+        $report = $apiClient->getEntity('Report', 'find_report_by_id', [ 'query' => [ 'id' => $reportId ]]);
+        $decision = new Decision;
+        $decision->setReportId($reportId);
+        
+        $form = $this->createForm(new FormDir\DecisionType([
+            'clientInvolvedBooleanEmptyValue' => $this->get('translator')->trans('clientInvolvedBoolean.defaultOption', [], 'decision')
+        ]), $decision);
+
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+            if ($form->isValid()) {
+                // add decision
+                $response = $apiClient->postC('add_decision', $form->getData());
+                
+                return $this->redirect($this->generateUrl('add_decision', ['reportId'=>$reportId]));
+            }
+        }
+
+        return [
+            'decisions' => $apiClient->getEntities('Decision', 'find_decision_by_report_id', [ 'query' => [ 'reportId' => $reportId ]]),
+            'form' => $form->createView(),
+            'report' => $report,
+            'client' => [], //to pass
+        ];
+    }
+   
+    
 }
