1173b08dc897813d916733e19bb6162eede17ba8
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-03-12 12:23:53 +0000

    fixture system working

--- src/AppBundle/Command/AddUserCommand.php
+++ src/AppBundle/Command/AddUserCommand.php
@@ -12,52 +12,77 @@ use Symfony\Component\Validator\ConstraintViolationList;
 
 class AddUserCommand extends \Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand
 {
+
     protected function configure()
     {
         $this
-            ->setName('digideps:user-add')
-            ->setDescription('User add')
-            ->addArgument('email', null, InputOption::VALUE_REQUIRED, 'email')
-            ->addArgument('firstname', null, InputOption::VALUE_OPTIONAL, 'firstname')
-            ->addArgument('lastname', null, InputOption::VALUE_OPTIONAL, 'lastname')
-            ->addArgument('password', null, InputOption::VALUE_OPTIONAL, 'password. default=test')
-            ->addArgument('roleId', null, InputOption::VALUE_OPTIONAL, '1=admin, 2=deputy (default)')
+                ->setName('digideps:fixtures')
+                ->setDescription('Add data from fixtures')
+                ->addArgument('email', null, InputOption::VALUE_REQUIRED, 'email')
+                ->addArgument('firstname', null, InputOption::VALUE_OPTIONAL, 'firstname')
+                ->addArgument('lastname', null, InputOption::VALUE_OPTIONAL, 'lastname')
+                ->addArgument('password', null, InputOption::VALUE_OPTIONAL, 'password. default=test')
+                ->addArgument('roleId', null, InputOption::VALUE_OPTIONAL, '1=admin, 2=deputy (default)')
         ;
     }
 
+    
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        if (!$input->getArgument('email')) {
-            throw new \InvalidArgumentException("Email needed");
-        }
         $apiClient = $this->getContainer()->get('apiclient'); /* @var $apiClient ApiClient */
-        
-        $user = (new User)
-             ->setFirstname($input->getArgument('firstname') ?: 'Firstname')
-             ->setLastname($input->getArgument('lastname') ?: 'LastName')
-             ->setEmail($input->getArgument('email'))
-             ->setRoleId($input->getArgument('roleId') ?: 2);
-        
-        // check params
-        $violations = $this->getContainer()->get('validator')->validate($user, 'admin_add_user'); /* @var $violations ConstraintViolationList */
-        if ($violations->count()) {
-            echo $violations;die;
+
+        $fixtures = $this->getContainer()->getParameter('fixtures');
+        if (empty($fixtures)) {
+            $output->writeln("No data fixture to add.");
         }
-        
-        // add user
-        $response = $apiClient->postC('add_user', $user, [
-            'deserialise_group' => 'admin_add_user' //only serialise the properties modified by this form)
-        ]);
-        // refresh from aPI
-        $user = $apiClient->getEntity('User', 'user/' . $response['id']);
-        // set password and activate
-        $response = $apiClient->putC('user/' . $user->getId(), json_encode([
-            'password' => $this->encodePassword($user, $input->getArgument('password') ?: 'test'),
-            'active' => true
-        ]));
-        
-        $output->writeln("User  created {$user->getEmail()} password: {$input->getArgument('password')}");
+        foreach ($fixtures as $email => $data) {
+            if ($this->userExists($email)) {
+                $output->writeln("User $email already existing.");
+                continue;
+            }
+
+            $user = (new User)
+                    ->setFirstname($data['firstname'])
+                    ->setLastname($data['lastname'])
+                    ->setEmail($email)
+                    ->setRoleId($data['roleId']);
+
+            // check params
+            $violations = $this->getContainer()->get('validator')->validate($user, 'admin_add_user'); /* @var $violations ConstraintViolationList */
+            if ($violations->count()) {
+                $this->writeln("Cannot add user $email: $violations");
+                continue;
+            }
+
+            // add user
+            $response = $apiClient->postC('add_user', $user, [
+                'deserialise_group' => 'admin_add_user' //only serialise the properties modified by this form)
+            ]);
+            if ($data['activated']) {
+                // refresh from aPI
+                $user = $apiClient->getEntity('User', 'user/' . $response['id']);
+                // set password and activate
+                $response = $apiClient->putC('user/' . $user->getId(), json_encode([
+                    'password' => $this->encodePassword($user, $data['password']),
+                    'active' => true
+                ]));
+            }
+
+            $output->writeln("User $email created.");
+        }
     }
+
+    private function userExists($email)
+    {
+        $apiClient = $this->getContainer()->get('apiclient'); /* @var $apiClient ApiClient */
+        try {
+            $apiClient->getEntity('User', 'find_user_by_email', [ 'query' => [ 'email' => $email]]);
+            return true;
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
     
     /**
      * @param User $user
@@ -70,4 +95,5 @@ class AddUserCommand extends \Symfony\Bundle\FrameworkBundle\Command\ContainerAw
         $encoder = $this->getContainer()->get('security.encoder_factory')->getEncoder($user);
         return $encoder->encodePassword($passwordPlain, $user->getSalt());
     }
+
 }
