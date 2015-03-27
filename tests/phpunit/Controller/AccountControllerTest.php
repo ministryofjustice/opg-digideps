8604b2f76096980aec0b5b18c02ac10bac4afecf
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-03-27 11:54:45 +0000

    function test for PUT /account with lastEdit test

--- tests/phpunit/Controller/AccountControllerTest.php
+++ tests/phpunit/Controller/AccountControllerTest.php
@@ -27,13 +27,20 @@ class AccountControllerTest extends WebTestCase
      */
     public function addAccount()
     {
-        // add report
-        /*$report = new \AppBundle\Entity\Report;
+        $client = new \AppBundle\Entity\Client;
+        $this->em->persist($client);
+        
+        $cot = new \AppBundle\Entity\CourtOrderType;
+        $this->em->persist($cot);
+        
+        $report = new \AppBundle\Entity\Report;
+        $report->setClient($client);
+        $report->setCourtOrderType($cot);
         $this->em->persist($report);
-        $this->em->flush($report);*/
+        
+        $this->em->flush();
 
-        // create user
-        /*$this->client->request(
+        $this->client->request(
             'POST', '/report/add-account',
             array(), array(),
             array('CONTENT_TYPE' => 'application/json'),
@@ -52,13 +59,43 @@ class AccountControllerTest extends WebTestCase
         $this->assertNotEmpty($return, 'Response not json');
         $this->assertTrue($return['success'], $return['message']);
         $this->assertArrayHasKey('message', $return);
-        $this->assertTrue($return['data']['id'] > 0);*/
-
+        $this->assertTrue($return['data']['id'] > 0);
+        
         // assert account created with transactions
-        /*$account = $this->em->getRepository('AppBundle\Entity\Account')->find($return['data']['id']); /* @var $account \AppBundle\Entity\Account */
-        /*$transactionTypesTotal = count($this->em->getRepository('AppBundle\Entity\AccountTransactionType')->findAll());
+        $account = $this->em->getRepository('AppBundle\Entity\Account')->find($return['data']['id']); /* @var $account \AppBundle\Entity\Account */
+        $transactionTypesTotal = count($this->em->getRepository('AppBundle\Entity\AccountTransactionType')->findAll());
         $this->assertCount($transactionTypesTotal, $account->getTransactions(), "transactions not created");
 
-        return $account->getId();*/
+        $this->assertNull($account->getLastEdit(), 'account.lastEdit must be null on creation');
+        
+        return $account->getId();
+    }
+    
+    /**
+     * @test
+     * @depends addAccount
+     */
+    public function editAccount($accountId)
+    {
+        $this->client->request(
+            'PUT', '/account/'.$accountId,
+            array(), array(),
+            array('CONTENT_TYPE' => 'application/json'),
+            json_encode(array(
+                // TODO: transactions
+            ))
+        );
+        $response =  $this->client->getResponse();
+        $this->assertTrue($response->headers->contains('Content-Type','application/json'), 'wrong content type');
+        $return = json_decode($response->getContent(), true);
+        $this->assertNotEmpty($return, 'Response not json');
+        $this->assertTrue($return['success'], $return['message']);
+        $this->assertArrayHasKey('message', $return);
+        $this->assertTrue($return['data']['id'] > 0);
+        
+        //TODO: transaction checks
+         // assert account created with transactions
+        $account = $this->em->getRepository('AppBundle\Entity\Account')->find($return['data']['id']); /* @var $account \AppBundle\Entity\Account */
+        $this->assertTrue(time() - $account->getLastEdit()->getTimestamp() < 1000, 'account.lastIed not udpated with current date');
     }
 }
