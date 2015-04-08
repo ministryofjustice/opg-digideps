5c134c27601044156e796dba82584289d1733040
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-04-08 15:29:11 +0000

    health-check page printing check results

--- src/AppBundle/Controller/HealthCheckController.php
+++ src/AppBundle/Controller/HealthCheckController.php
@@ -3,10 +3,10 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Service\ApiClient;
 use Symfony\Component\HttpFoundation\Response;
-use AppBundle\Entity\User;
 
 /**
  * @Route("/health-check")
@@ -16,12 +16,24 @@ class HealthCheckController extends Controller
     /**
      * @Route("/")
      * @Method({"GET"})
+     * @Template
      */
     public function indexAction()
     {
+        $content = $this->get('apiclient')->get('health-check')->getBody();
+        $contentArray = json_decode($content, 1);
+        if (json_last_error() !== JSON_ERROR_NONE || empty($contentArray['data'])) {
+            throw new \RuntimeException("Cannot decode API response. " . json_last_error_msg());
+        }
+        $data = $contentArray['data'];
         
-        $response = new Response('all good');
-        $response->setStatusCode(200);
+        $response = $this->render('AppBundle:HealthCheck:index.html.twig', [
+            'data' => $data
+        ]);
+        
+        if (!$data['app']['healthy']) {
+            $response->setStatusCode('500');
+        }
         
         return $response;
     }
