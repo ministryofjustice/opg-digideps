3e137e19b624b2e6ce2c4a2641801a7df0931c12
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-04-08 16:16:13 +0000

    refactor and add client health data

--- src/AppBundle/Controller/HealthCheckController.php
+++ src/AppBundle/Controller/HealthCheckController.php
@@ -20,21 +20,38 @@ class HealthCheckController extends Controller
      */
     public function indexAction()
     {
-        $content = $this->get('apiclient')->get('health-check')->getBody();
-        $contentArray = json_decode($content, 1);
-        if (json_last_error() !== JSON_ERROR_NONE || empty($contentArray['data'])) {
-            throw new \RuntimeException("Cannot decode API response. " . json_last_error_msg());
-        }
-        $data = $contentArray['data'];
+        $data = [
+            'api' => $this->getHealthCheckDataFromApi(),
+            'client' => $this->getHealthData(),
+        ];
         
         $response = $this->render('AppBundle:HealthCheck:index.html.twig', [
             'data' => $data
         ]);
         
-        if (!$data['app']['healthy']) {
+        if (!$data['api']['healthy'] || !$data['client']['healthy']) {
             $response->setStatusCode('500');
         }
         
         return $response;
+    }
+    
+    private function getHealthData()
+    {
+        return [
+            'healthy' => true,
+            'php_version' => true
+        ];
+    }
+    
+    
+    private function getHealthCheckDataFromApi()
+    {
+        $content = $this->get('apiclient')->get('health-check')->getBody();
+        $contentArray = json_decode($content, 1);
+        if (json_last_error() !== JSON_ERROR_NONE || empty($contentArray['data'])) {
+            throw new \RuntimeException("Cannot decode API response. " . json_last_error_msg());
+        }
+        return $contentArray['data'];
     }
 }
