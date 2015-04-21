3224148be6b2c7efe19960fc758835028152f95d
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-04-21 09:12:37 +0000

    add unittest for REsponseNoCacheListener

--- src/AppBundle/EventListener/ResponseNoCacheListener.php
+++ src/AppBundle/EventListener/ResponseNoCacheListener.php
@@ -3,10 +3,6 @@ namespace AppBundle\EventListener;
 
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 
-/**
- * Kernel response listener to disable page caching.
- * Disables the browser history navigation after the user logs out
- */
 class ResponseNoCacheListener
 {
     public function onKernelResponse(FilterResponseEvent $event)
