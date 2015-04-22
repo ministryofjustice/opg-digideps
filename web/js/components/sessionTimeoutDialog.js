9d018a33557aebcecf85ee59e7e40a7e41c0dd81
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-04-22 10:36:19 +0000

    refactor JS into a function rather than object

--- web/js/components/sessionTimeoutDialog.js
+++ web/js/components/sessionTimeoutDialog.js
@@ -1,54 +1,45 @@
-// SESSION TIMEOUT POPUP LOGIC    
-var SessionTimeoutDialog = {
-    /**
-     * @param element
-     * @param sessionExpiresMs
-     * @param sessionPopupShowAfterMs
-     * @param refreshUrl
-     */
-    init: function (options) {
-        this.element = options.element;
-        this.sessionExpiresMs = options.sessionExpiresMs;
-        this.sessionPopupShowAfterMs = options.sessionPopupShowAfterMs;
-        this.keepSessionAliveUrl = options.keepSessionAliveUrl;
-        this.redirectAfterMs = 3000;
-        this.startCountdown();
-        var that = this;
-        
-        
-        // attach event
-        this.element.find('[data-role="keep-session-alive"]').click(function(e) {
-          e.preventDefault();
-          that.hidePopupAndRestartCountdown();
-        });
-    },
-    startCountdown: function() {
-      var that = this;
-      this.countDownPopup = window.setInterval(
-        function() {
+// SESSION TIMEOUT POPUP LOGIC  
+/**
+ * @param element
+ * @param sessionExpiresMs
+ * @param sessionPopupShowAfterMs
+ * @param refreshUrl
+ */
+var SessionTimeoutDialog = function (options) {
+  var that = this;
+  this.element = options.element;
+  this.sessionExpiresMs = options.sessionExpiresMs;
+  this.sessionPopupShowAfterMs = options.sessionPopupShowAfterMs;
+  this.keepSessionAliveUrl = options.keepSessionAliveUrl;
+  this.redirectAfterMs = 3000;
+  // attach event
+  this.element.find('[data-role="keep-session-alive"]').click(function (e) {
+    e.preventDefault();
+    that.hidePopupAndRestartCountdown();
+  });
+  this.startCountdown = function () {
+    this.countDownPopup = window.setInterval(
+        function () {
           that.element.css('visibility', 'visible');
         },
         this.sessionPopupShowAfterMs
+        );
+    this.countDownLogout = window.setInterval(
+      function () {
+        window.location.reload();
+      },
+      this.sessionExpiresMs + this.redirectAfterMs
       );
-      this.countDownLogout = window.setInterval(
-        function() {
-          window.location.reload();
-        },
-        this.sessionExpiresMs + this.redirectAfterMs
-      );
-    },
-    hidePopupAndRestartCountdown: function() {
-      this.element.css('visibility', 'hidden');
-      this.keepSessionAlive();
-      // restart countdown
-      clearTimeout(this.countDownPopup);
-      clearTimeout(this.countDownLogout);
-      this.startCountdown();
-    },
-    keepSessionAlive: function() {
-      $.post(this.keepSessionAliveUrl);
-    },
-    getElement: function () {
-      return this.element;
-    }
+  };
+  this.hidePopupAndRestartCountdown = function () {
+    this.element.css('visibility', 'hidden');
+    this.keepSessionAlive();
+    // restart countdown
+    clearTimeout(this.countDownPopup);
+    clearTimeout(this.countDownLogout);
+    this.startCountdown();
+  };
+  this.keepSessionAlive = function () {
+    $.post(this.keepSessionAliveUrl);
+  };
 };
