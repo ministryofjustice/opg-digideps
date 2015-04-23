29e7c74b6dd1c5a8579786174474477f3f2a1709
Nick Middleweek <nick.middleweek@digital.justice.gov.uk>
2015-04-23 17:18:21 +0100

    * Session timeout styles.

--- web/js/components/sessionTimeoutDialog.js
+++ web/js/components/sessionTimeoutDialog.js
@@ -6,40 +6,51 @@
  * @param refreshUrl
  */
 var SessionTimeoutDialog = function (options) {
-  var that = this;
-  this.element = options.element;
-  this.sessionExpiresMs = options.sessionExpiresMs;
-  this.sessionPopupShowAfterMs = options.sessionPopupShowAfterMs;
-  this.keepSessionAliveUrl = options.keepSessionAliveUrl;
-  this.redirectAfterMs = 3000;
-  // attach event
-  this.element.find('[data-role="keep-session-alive"]').click(function (e) {
-    e.preventDefault();
-    that.hidePopupAndRestartCountdown();
-  });
-  this.startCountdown = function () {
-    this.countDownPopup = window.setInterval(
-        function () {
-          that.element.css('visibility', 'visible');
-        },
-        this.sessionPopupShowAfterMs
-        );
-    this.countDownLogout = window.setInterval(
-      function () {
-        window.location.reload();
-      },
-      this.sessionExpiresMs + this.redirectAfterMs
-      );
-  };
-  this.hidePopupAndRestartCountdown = function () {
-    this.element.css('visibility', 'hidden');
-    this.keepSessionAlive();
-    // restart countdown
-    clearTimeout(this.countDownPopup);
-    clearTimeout(this.countDownLogout);
-    this.startCountdown();
-  };
-  this.keepSessionAlive = function () {
-    $.post(this.keepSessionAliveUrl);
-  };
+    var that = this;
+    this.element = options.element;
+    this.sessionExpiresMs = options.sessionExpiresMs;
+    this.sessionPopupShowAfterMs = options.sessionPopupShowAfterMs;
+    this.keepSessionAliveUrl = options.keepSessionAliveUrl;
+    this.redirectAfterMs = 3000;
+
+    //debugger;
+
+    var $okButton = that.element.find('.js-ok-button'),
+        $underlay = $('.session-timeout-underlay');
+
+    // attach click event
+    $okButton.click(function (e) {
+        e.preventDefault();
+        that.hidePopupAndRestartCountdown();
+    });
+
+    this.startCountdown = function () {
+
+        this.countDownPopup = window.setInterval(function () {
+            that.element.css('visibility', 'visible');
+            $underlay.show();
+
+        }, this.sessionPopupShowAfterMs);
+
+        this.countDownLogout = window.setInterval(function () {
+            window.location.reload();
+
+        }, this.sessionExpiresMs + this.redirectAfterMs);
+    };
+
+    this.hidePopupAndRestartCountdown = function () {
+        this.element.hide();
+        $underlay.hide();
+
+        this.keepSessionAlive();
+        // restart countdown
+        window.clearInterval(this.countDownPopup);
+        window.clearInterval(this.countDownLogout);
+        this.startCountdown();
+    };
+
+    this.keepSessionAlive = function () {
+        $.post(this.keepSessionAliveUrl);
+    };
+
 };
