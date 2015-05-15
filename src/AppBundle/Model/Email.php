cce8295ab74ac3bf1f78b8aa443a91ad255702ce
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-05-15 15:01:05 +0000

    dd-375 email support for attachments

--- src/AppBundle/Model/Email.php
+++ src/AppBundle/Model/Email.php
@@ -60,6 +60,11 @@ class Email
     private $bodyHtml;
     
     /**
+     * @JMS\Type("array<AppBundle\Model\EmailAttachment>") 
+     */
+    private $attachments;
+    
+    /**
      * @return string $email
      */
     public function getToEmail()
@@ -192,4 +197,10 @@ class Email
         $this->bodyHtml = $bodyHtml;
         return $this;
     }
+    
+    public function setAttachments(array $attachments)
+    {
+        $this->attachments = $attachments;
+    }
+
 }
