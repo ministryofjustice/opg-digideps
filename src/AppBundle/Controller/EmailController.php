d3e153b4ad8053b9dbb9715d003c7aa4e6e64aa2
Elvis Ciotti <elvisciotti@gmail.com>
2015-05-19 11:50:23 +0000

    dd-375 add smtp transport, refactor emails, move mock logic at the controller level

--- src/AppBundle/Controller/EmailController.php
+++ src/AppBundle/Controller/EmailController.php
@@ -4,6 +4,9 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use AppBundle\Mailer\Utils\MessageUtils;
+use Swift_Mailer;
+use Swift_Message;
 
 //TODO
 //http://symfony.com/doc/current/bundles/SensioFrameworkExtraBundle/annotations/converters.html
@@ -23,31 +26,79 @@ class EmailController extends RestController
     public function sendEmail()
     {
         $data = $this->deserializeBodyContent();
-        
+
         array_map(function($k) use ($data) {
             if (!array_key_exists($k, $data)) {
                 throw new \InvalidArgumentException("Missing parameter $k");
             }
         }, ['toEmail', 'toName', 'fromEmail', 'fromName', 'subject', 'bodyText', 'bodyHtml']);
-        
-        $mailerService = $this->container->get('mailer.service'); /* @var $mailerService \Swift_Mailer */
-        
+
+        $mailTransport = 'mailer.transport.sendgrid';
+        if (isset($data['transport']) && $data['transport'] == 'secureSmtp') {
+            $mailTransport = 'mailer.transport.secureSmtp';
+        }
+
+        $mailerService = new Swift_Mailer($this->container->get($mailTransport));
         $message = $mailerService->createMessage(); /* @var $message \Swift_Message */
         $message->setTo($data['toEmail'], $data['toName']);
         $message->setFrom($data['fromEmail'], $data['fromName']);
-        
+
         $message->setSubject($data['subject']);
         $message->setBody($data['bodyText']);
         $message->addPart($data['bodyHtml'], 'text/html');
-        
+
         if (!empty($data['attachments'])) {
             foreach ($data['attachments'] as $attachment) {
                 $message->attach(new \Swift_Attachment($attachment['content'], $attachment['filename'], $attachment['contentType']));
             }
         }
-        
-        $result = $mailerService->send($message);
-        
-        return array('result'=>$result);
+
+        // behat-@ emails goes to file instead of real sending
+        if ($this->isEmailToMock($message)) {
+            $result = $this->writeMessageIntoFile($message);
+        } else {
+            $result = $mailerService->send($message);
+        }
+
+        return array('result' => $result);
     }
+
+    /**
+     * @param \Swift_Mime_Message $message
+     * @return boolean
+     */
+    private function isEmailToMock(Swift_Message $message)
+    {
+        $addressToMockRegexp = $this->container->getParameter('email_mock_address');
+        $path = $this->container->getParameter('email_mock_path');
+        if (!$addressToMockRegexp || !$path) {
+            return false;
+        }
+
+        // get "to"
+        $to = $message->getTo();
+        reset($to);
+        $emailAddress = key($to);
+
+        return preg_match($addressToMockRegexp, $emailAddress);
+    }
+
+    /**
+     * @param \AppBundle\Controller\Swift_Mime_Message $swiftMessage
+     * @return type
+     * @throws \RuntimeException
+     */
+    private function writeMessageIntoFile(Swift_Message $swiftMessage)
+    {
+        $path = $this->container->getParameter('email_mock_path');
+
+        $data = MessageUtils::messageToArray($swiftMessage);
+        $ret = file_put_contents($path, json_encode($data));
+        if (false === $ret) {
+            throw new \RuntimeException("Cannot write email into $path");
+        }
+
+        return "Email saved into $path. $ret bytes written.";
+    }
+
 }
