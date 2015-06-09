f6d36612332edb8d0f95f15cc74764fb95f3e608
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-06-09 10:30:47 +0000

    rename migration prior to merge

--- app/DoctrineMigrations/Version033.php
+++ app/DoctrineMigrations/Version033.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version032 extends AbstractMigration
+class Version033 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
