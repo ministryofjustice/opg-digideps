9c46f1ddcd78257f3619d2e88453cfbccf81db0d
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-06-16 10:56:49 +0000

    behat email trait improvements

--- tests/behat/bootstrap/EmailTrait.php
+++ tests/behat/bootstrap/EmailTrait.php
@@ -4,57 +4,58 @@ namespace DigidepsBehat;
 
 trait EmailTrait
 {
-    
+
     /**
      * Array (
-            [to] => Array(
-                    [deputyshipservice@publicguardian.gsi.gov.uk] => test Test
-                )
+      [to] => Array(
+      [deputyshipservice@publicguardian.gsi.gov.uk] => test Test
+      )
 
-            [from] => Array(
-                    [admin@digideps.service.dsd.io ] => Digital deputyship service
-                )
-            [bcc] =>
-            [cc] =>
-            [replyTo] =>
-            [returnPath] =>
-            [subject] => Digideps - activation email
-            [body] => Hello test Test, click here http://link.com/activate/testtoken to activate your account
-            [sender] =>
-            [parts] => Array(
-                    [0] => Array(
-                            [body] => Hello test Test<br/><br/>click here <a href="http://link.com/activate/testtoken">http://link.com/activate/testtoken</a> to activate your account
-                            [contentType] => text/html
-                        )
-                )
-        )
+      [from] => Array(
+      [admin@digideps.service.dsd.io ] => Digital deputyship service
+      )
+      [bcc] =>
+      [cc] =>
+      [replyTo] =>
+      [returnPath] =>
+      [subject] => Digideps - activation email
+      [body] => Hello test Test, click here http://link.com/activate/testtoken to activate your account
+      [sender] =>
+      [parts] => Array(
+      [0] => Array(
+      [body] => Hello test Test<br/><br/>click here <a href="http://link.com/activate/testtoken">http://link.com/activate/testtoken</a> to activate your account
+      [contentType] => text/html
+      )
+      )
+      )
      * 
      * @retun array
      */
-    private function getLatestEmailMockFromApi()
+    private function getLatestEmailMockFromApi($throwExceptionIfNotFound = true)
     {
         $this->visitBehatLink('email-get-last');
-        
-        $content =  $this->getSession()->getPage()->getContent();
+
+        $content = $this->getSession()->getPage()->getContent();
         $contentJson = json_decode($content, true);
-        
-        if (empty($contentJson['to'])) {
+
+        if ($throwExceptionIfNotFound && empty($contentJson['to'])) {
             throw new \RuntimeException("Email has not been sent. Api returned: " . $content);
         }
-        
+
         return $contentJson;
     }
-    
-    
+
     /**
-     * @BeforeScenario @cleanMail
+     * @Given I reset the email log
      */
-    public function beforeScenarioCleanMail(BeforeScenarioScope $scope)
+    public function beforeScenarioCleanMail()
     {
         $this->visitBehatLink('email-reset');
         $this->assertResponseStatus(200);
+
+        $this->assertNoEmailShouldHaveBeenSent();
     }
-    
+
     /**
      * @Then an email with subject :subject should have been sent to :to
      */
@@ -62,8 +63,8 @@ trait EmailTrait
     {
         $mail = $this->getLatestEmailMockFromApi();
         $mailTo = key($mail['to']);
-        
-        
+
+
         if ($mail['subject'] != $subject) {
             throw new \RuntimeException("Subject '" . $mail['subject'] . "' does not match the expected '" . $subject . "'");
         }
@@ -71,19 +72,29 @@ trait EmailTrait
             throw new \RuntimeException("Addressee '" . $mailTo . "' does not match the expected '" . $to . "'");
         }
     }
-    
-     /**
+
+    /**
      * @Then an email with subject :subject should have been sent
      */
     public function anEmailWithSubjectShouldHaveBeenSent($subject)
     {
         $mail = $this->getLatestEmailMockFromApi();
-        
+
         if ($mail['subject'] != $subject) {
             throw new \RuntimeException("Subject '" . $mail['subject'] . "' does not match the expected '" . $subject . "'");
         }
     }
-    
+
+    /**
+     * @Then no email should have been sent
+     */
+    public function assertNoEmailShouldHaveBeenSent()
+    {
+        $content = $this->getLatestEmailMockFromApi(false);
+        if ($content) {
+            throw new \RuntimeException("Found unexpected email with subject '" . $content['subject'] . "'");
+        }
+    }
 
     /**
      * @When I open the first link on the email
@@ -91,7 +102,7 @@ trait EmailTrait
     public function iOpenTheFirstLinkOnTheEmail()
     {
         $mailContent = $this->getLatestEmailMockFromApi()['parts'][0]['body'];
-        
+
         preg_match_all('#https?://[^\s"<]+#', $mailContent, $matches);
         if (empty($matches[0])) {
             throw new \Exception("no link found in email. Body:\n $mailContent");
@@ -105,20 +116,18 @@ trait EmailTrait
         // visit the link
         $this->visit($link);
     }
-    
+
     /**
      * @Then the email should contain :text
      */
     public function mailContainsText($text)
     {
-        
+
         $mailContent = $this->getLatestEmailMockFromApi()['parts'][0]['body'];
-        
+
         if (strpos($mailContent, $text) === FALSE) {
             throw new \Exception("Text: $text not found in email. Body: \n $mailContent");
         }
-        
-        
     }
-    
+
 }
