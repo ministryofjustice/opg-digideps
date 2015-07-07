99c7f0ed983f66aec751290e9fda1283063011dc
Zac Tolley <zac.tolley@digital.justice.gov.uk>
2015-07-07 16:19:56 +0100

    whip with parameters for docker.

---
+++ scripts/resetdb.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#let's configure environment
+/etc/my_init.d/*
+
+cd /app
+/sbin/setuser app php app/console doctrine:query:sql "DROP SCHEMA IF EXISTS public cascade; CREATE SCHEMA IF NOT EXISTS public;"
+/sbin/setuser app php app/console doctrine:migrations:migrate --no-interaction
