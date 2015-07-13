c932a7fc05f1b009fd89256893b759864bc79b3d
Zac Tolley <zac.tolley@digital.justice.gov.uk>
2015-07-13 15:18:40 +0100

    Simple bit of javascript that says if there is an error summary, then move to that part of the page.

--- src/AppBundle/Resources/assets/javascripts/application.js
+++ src/AppBundle/Resources/assets/javascripts/application.js
@@ -18,6 +18,13 @@ function mojDateString(localDate) {
 
 }
 
+function scrollIntoView(eleID) {
+   var e = document.getElementById(eleID);
+   if (e) {
+        $('html, body').animate({scrollTop: $(e).offset().top -40 }, 'fast');
+   }
+}
+
 var dateNow = new Date();
 
 //debugger;
@@ -30,3 +37,7 @@ if ($('.registration-client-add')) {
 if ($('.js-report-decisions-input')) {
     new GOVUK.SelectionButtons(":radio");
 }
+
+$(window).load(function(){
+    scrollIntoView('error-summary');   
+});
