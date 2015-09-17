626695c25c71b017c6db13e03d841023837b9017
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-09-17 09:53:52 +0100

    remove duplicate db reset

--- scripts/apiunittest.sh
+++ scripts/apiunittest.sh
@@ -4,6 +4,4 @@ run-parts /etc/my_init.d
 
 cd /app
 # reset database and migrate
-/sbin/setuser app php app/console doctrine:query:sql "DROP SCHEMA IF EXISTS public cascade; CREATE SCHEMA IF NOT EXISTS public;" --env=test
-/sbin/setuser app php app/console doctrine:migrations:migrate --no-interaction --env=test
 /sbin/setuser app php vendor/phpunit/phpunit/phpunit
