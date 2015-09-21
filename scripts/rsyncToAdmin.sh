5044b02e0234edd07bdd150800c09b23f0b0705c
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-09-21 14:17:01 +0100

    restore admin sync script

--- scripts/rsyncToAdmin.sh
+++ scripts/rsyncToAdmin.sh
@@ -7,15 +7,10 @@
 CLIENT_PATH=~/www/opg-digi-deps-client/
 ADMIN_PATH=~/www/opg-digi-deps-admin/
 
+
+# rsync, delete cache, replace YML params
+rsync -va --delete --exclude=.git $CLIENT_PATH $ADMIN_PATH
 rm -rf $ADMIN_PATH"app/cache/*"
-
-# copy config from frontend
-cp $CLIENT_PATH"app/config/parameters.yml" $ADMIN_PATH"app/config/parameters.yml"
-
-#replace redis and env param
 sed -i -e 's/env: prod/env: admin/g' $ADMIN_PATH"app/config/parameters.yml"
 sed -i -e 's/redis:\/\/redisfront/redis:\/\/redisadmin/g' $ADMIN_PATH"app/config/parameters.yml"
-
-echo "Done: parameters.yml for admin contains:"
-cat $ADMIN_PATH"app/config/parameters.yml" | grep env:
-cat $ADMIN_PATH"app/config/parameters.yml" | grep redis_dsn
+echo "Admin area synced"
