3007d3f14d94fb113d1af58b85d96b9177dae611
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-09-21 13:42:58 +0100

    dd-1130 script to symlink and create parameters.yml for admin area

--- scripts/rsyncToAdmin.sh
+++ scripts/rsyncToAdmin.sh
@@ -7,10 +7,15 @@
 CLIENT_PATH=~/www/opg-digi-deps-client/
 ADMIN_PATH=~/www/opg-digi-deps-admin/
 
-
-# rsync, delete cache, replace YML params
-rsync -va --delete $CLIENT_PATH $ADMIN_PATH
 rm -rf $ADMIN_PATH"app/cache/*"
+
+# copy config from frontend
+cp $CLIENT_PATH"app/config/parameters.yml" $ADMIN_PATH"app/config/parameters.yml"
+
+#replace redis and env param
 sed -i -e 's/env: prod/env: admin/g' $ADMIN_PATH"app/config/parameters.yml"
 sed -i -e 's/redis:\/\/redisfront/redis:\/\/redisadmin/g' $ADMIN_PATH"app/config/parameters.yml"
-echo "Admin area synced"
+
+echo "Done: parameters.yml for admin contains:"
+cat $ADMIN_PATH"app/config/parameters.yml" | grep env:
+cat $ADMIN_PATH"app/config/parameters.yml" | grep redis_dsn
