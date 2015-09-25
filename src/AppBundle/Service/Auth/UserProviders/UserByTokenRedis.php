77cc7178c1655f826d6938df5c185ce3a73458f0
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-09-25 16:29:41 +0100

    dd-1130 redis persist creation time and implements timeout

--- src/AppBundle/Service/Auth/UserProviders/UserByTokenRedis.php
+++ src/AppBundle/Service/Auth/UserProviders/UserByTokenRedis.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace AppBundle\Service\Auth\UserProviders;
 
 use AppBundle\Entity\User;
@@ -8,36 +9,37 @@ use Predis\Client as PredisClient;
 use Symfony\Component\Security\Core\Exception\BadCredentialsException;
 use Symfony\Component\Security\Core\Exception\CredentialsExpiredException;
 
-
 /**
  * Get the user from a token (=username) looking at the AuthToken store info
  * throw exception if not found, or the token expired 
  */
 class UserByTokenRedis implements UserByTokenProviderInterface
 {
-     /**
+
+    /**
      * @var PredisClient 
      */
     private $redis;
-    
-     /**
+
+    /**
      * @var EntityManager 
      */
     private $em;
-    
+
     /**
-     * @var EntityRepository
+     * @var integer 
      */
-    private $userRepo;
-    
-    
-    public function __construct(EntityManager $em, PredisClient $redis)
+    private $timeoutSeconds;
+
+
+    public function __construct(EntityManager $em, PredisClient $redis, array $options)
     {
         $this->em = $em;
         $this->redis = $redis;
-        $this->userRepo = $this->em->getRepository('AppBundle\Entity\User');
+        $this->timeoutSeconds = $options['timeout_seconds'];
     }
-    
+
+
     /**
      * @param string $username token (String)
      * @return User
@@ -47,41 +49,45 @@ class UserByTokenRedis implements UserByTokenProviderInterface
      */
     public function loadUserByUsername($username)
     {
-        $userId = $this->redis->get($username);
+        $token = $username;
+
+        list ($userId, $createdAt) = $this->redisGet($token);
         if (!$userId) {
-            throw new BadCredentialsException('Token non existing');
+            throw new BadCredentialsException('Token non existing or not valid');
         }
-//        if ($Use->isExpired($this->timeoutSeconds)) {
-//            throw new CredentialsExpiredException('Token expired');
-//        }
+        if (($createdAt + $this->timeoutSeconds) < time()) {
+            throw new CredentialsExpiredException('Token expired');
+        }
 
-        $User = $this->userRepo->find($userId);
-
-        /** @var $token  AuthToken */
-        if (!$User) {
+        $user = $this->em->getRepository('AppBundle\Entity\User')->find($userId);
+        if (!$user) {
             throw new BadCredentialsException('User associated to token not found');
         }
-        
-        
-        return $User;
+
+        // refresh token creation time
+        $this->redisStore($token, $userId);
+
+        return $user;
     }
+
 
     /**
      * not implemented
      */
     public function refreshUser(\Symfony\Component\Security\Core\User\UserInterface $user)
     {
+        
     }
 
 
     public function supportsClass($class)
     {
-       return 'AppBundle\Entity\User' === $class
-            || is_subclass_of($class, 'AppBundle\Entity\User');
+        return 'AppBundle\Entity\User' === $class || is_subclass_of($class, 'AppBundle\Entity\User');
     }
-    
+
+
     /**
-     * @param string $randomToken
+     * @param string $token
      * @param User $user
      * 
      * @return string
@@ -89,17 +95,42 @@ class UserByTokenRedis implements UserByTokenProviderInterface
     public function generateAndStoreToken(User $user)
     {
         // recreate and persist token
-        $randomToken = $user->getId() . '_' . sha1(microtime() . spl_object_hash($user) . rand(1,999));
-        $this->redis->set($randomToken, $user->getId());
-        
-        return $randomToken;
+        $token = $user->getId() . '_' . sha1(microtime() . spl_object_hash($user) . rand(1, 999));
+
+        $this->redisStore($token, $user->getId());
+
+        return $token;
     }
-    
+
+
     public function removeToken($token)
     {
         return $this->redis->set($token, null);
     }
 
-   
+
+    /**
+     * @param string $token
+     * @param integer $userId
+     */
+    private function redisStore($token, $userId)
+    {
+        return $this->redis->set($token, serialize([$userId, time()]));
+    }
+
+
+    /**
+     * @param type $token
+     * @return array [userId, createdAt]
+     */
+    private function redisGet($token)
+    {
+        $storedData = unserialize($this->redis->get($token));
+        if (!$storedData || !is_array($storedData) || count($storedData) !== 2) {
+            return [null, null];
+        }
+
+        return $storedData;
+    }
 
 }
