3d3b267ef2b492fc07240e2a99baae12e51fde15
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-09-28 13:11:15 +0100

    dd-1130 phpunit clear cache at start

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -7,6 +7,7 @@ require 'app/bootstrap.php.cache';
 require __DIR__ . "/Fixtures.php";
 require __DIR__ . "/Controller/AbstractTestController.php";
 
+exec('php app/console cache:clear --env=test');
 exec('php app/console doctrine:query:sql "DROP SCHEMA IF EXISTS public cascade; CREATE SCHEMA IF NOT EXISTS public;" --env=test');
 exec('php app/console doctrine:migrations:migrate --no-interaction --env=test');
 exec('php app/console digideps:add-user deputy@example.org --firstname=test --lastname=deputy --role=2 --password=Abcd1234 --env=test');
