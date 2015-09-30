e44513b2aeb2995cb139f48c4aaf585585014949
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-09-30 12:39:42 +0100

    dd-1130 typo

--- scripts/fixtures.sh
+++ scripts/fixtures.sh
@@ -1,1 +1,1 @@
-echo fixtures.sh is deprecated. fixtures now added from <api>/resetdb.sh. Remove this file when dd-1130 is fully merged to master
+echo "fixtures.sh is deprecated. fixtures now added from API/resetdb.sh. Remove this file when dd-1130 is fully merged to master"
