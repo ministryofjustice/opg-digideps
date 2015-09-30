16f3632f8fe39c8a47fde7e1bb9d6ea19505ab35
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-09-30 12:16:31 +0100

    improve script message

--- scripts/fixtures.sh
+++ scripts/fixtures.sh
@@ -1,1 +1,1 @@
-echo "deprecated. remove when dd-1130 is fully merged to master"
+echo fixtures.sh is deprecated. fixtures now added from <api>/resetdb.sh. Remove this file when dd-1130 is fully merged to master
