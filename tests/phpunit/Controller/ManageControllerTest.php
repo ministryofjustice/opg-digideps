04174528bc703db9cba45d405e98fd7b6e0cbf45
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-10-07 11:24:11 +0100

    dd-1130 manage controller test

---
+++ tests/phpunit/Controller/ManageControllerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Controller;
+use AppBundle\Entity\User;
+
+
+class ManageControllerTest extends AbstractTestController
+{
+    
+    public function testAvailability()
+    {
+        $ret = $this->assertJsonRequest('GET', '/manage/availability',[
+            'assertResponseCode' => 200
+        ])['data'];
+        
+        $this->assertEquals(1, $ret['healthy']);
+        $this->assertEquals('', $ret['errors']);
+    }
+    
+    public function testElb()
+    {
+        $ret = $this->assertJsonRequest('GET','/manage/elb', [
+            'assertResponseCode' => 200
+        ])['data'];
+
+        $this->assertEquals('ok', $ret);
+    }
+}
