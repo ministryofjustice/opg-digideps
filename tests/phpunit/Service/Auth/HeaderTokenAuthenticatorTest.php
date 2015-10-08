137237641510d160a0bc3fd7ae76049dc8589a11
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-10-08 12:58:07 +0100

    dd-1130 headerTokenAuthenticator unit test

--- tests/phpunit/Service/Auth/HeaderTokenAuthenticatorTest.php
+++ tests/phpunit/Service/Auth/HeaderTokenAuthenticatorTest.php
@@ -2,31 +2,91 @@
 
 namespace AppBundle\Service\Auth;
 
-use \Mockery as m;
+use \MockeryStub as m;
+use Symfony\Component\HttpFoundation\Request;
 
 class HeaderTokenAuthenticatorTest extends \PHPUnit_Framework_TestCase
 {
-    public function testgetTokenFromRequest()
+    /**
+     * @var HeaderTokenAuthenticator 
+     */
+    private $headerTokenAuth;
+    
+    public function setUp()
     {
-         $this->markTestIncomplete(__METHOD__);
+        $this->headerTokenAuth = new HeaderTokenAuthenticator();
     }
-
-
+    
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testcreateTokenNotFound()
+    {
+        $request = new Request();
+        
+        $this->headerTokenAuth->createToken($request, 'providerKey');
+    }
+    
     public function testcreateToken()
     {
-         $this->markTestIncomplete(__METHOD__);
+        $request = new Request();
+        $request->headers->set(HeaderTokenAuthenticator::HEADER_NAME, 'AuthTokenValue');
+        
+        $preAuthToken = $this->headerTokenAuth->createToken($request, 'providerKey');
+        $this->assertInstanceOf('Symfony\Component\Security\Core\Authentication\Token\PreAuthenticatedToken', $preAuthToken);
+        $this->assertEquals('anon.', $preAuthToken->getUser());
+        $this->assertEquals('providerKey', $preAuthToken->getProviderKey());
+        $this->assertEquals('AuthTokenValue', $preAuthToken->getCredentials());
     }
 
-
-    public function testauthenticateToken()
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testauthenticateTokenWrongProvider()
     {
-         $this->markTestIncomplete(__METHOD__);
+        $token = m::mock('Symfony\Component\Security\Core\Authentication\Token\TokenInterface');
+        $user = m::mock('Symfony\Component\Security\Core\User\UserProviderInterface');
+        
+        $this->headerTokenAuth->authenticateToken($token, $user, 'providerKey');
+    }
+    
+    public function testauthenticateTokenSuccess()
+    {
+        $user = m::stub('AppBundle\Entity\User', [
+                'getRoles' => ['role1']
+        ]);
+        
+        $token = m::stub('Symfony\Component\Security\Core\Authentication\Token\TokenInterface', [
+            'getCredentials' => 'AuthTokenValue'
+        ]);
+        $userProvider = m::stub('AppBundle\Service\Auth\UserProvider', [
+            'loadUserByUsername(AuthTokenValue)' => $user
+        ]);
+        
+        $preAuthToken = $this->headerTokenAuth->authenticateToken($token, $userProvider, 'providerKey');
+        $this->assertInstanceOf('Symfony\Component\Security\Core\Authentication\Token\PreAuthenticatedToken', $preAuthToken);
+        $this->assertEquals($user, $preAuthToken->getUser());
+        $this->assertEquals('providerKey', $preAuthToken->getProviderKey());
+        $this->assertEquals('AuthTokenValue', $preAuthToken->getCredentials());
     }
 
 
     public function testsupportsToken()
     {
-         $this->markTestIncomplete(__METHOD__);
+        $token = m::mock('Symfony\Component\Security\Core\Authentication\Token\TokenInterface');
+        $preAuthToken = m::stub('Symfony\Component\Security\Core\Authentication\Token\PreAuthenticatedToken', [
+            'getProviderKey'=>'providerKey'
+        ]);
+        
+        
+        $this->assertFalse($this->headerTokenAuth->supportsToken($token, 'providerKey'));
+        $this->assertFalse($this->headerTokenAuth->supportsToken($preAuthToken, 'providerKey-WRONG'));
+        $this->assertTrue($this->headerTokenAuth->supportsToken($preAuthToken, 'providerKey'));
     }
 
+    public function tearDown()
+    {
+        m::close();
+    }
+    
 }
