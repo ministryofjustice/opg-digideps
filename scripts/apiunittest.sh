26b8973de9237468a239a93298a1f231bb84f0a7
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-10-14 14:26:23 +0100

    restore coverage

--- scripts/apiunittest.sh
+++ scripts/apiunittest.sh
@@ -3,5 +3,4 @@
 run-parts /etc/my_init.d
 
 cd /app
-#/sbin/setuser app php -d zend_extension=xdebug.so vendor/phpunit/phpunit/phpunit -c tests/phpunit/phpunit.xml --coverage-clover=build/coverage.xml --coverage-html=build/coverage-html
-/sbin/setuser app php vendor/phpunit/phpunit/phpunit -c tests/phpunit/phpunit.xml
+/sbin/setuser app php -d zend_extension=xdebug.so vendor/phpunit/phpunit/phpunit -c tests/phpunit/phpunit.xml --coverage-clover=build/coverage.xml --coverage-html=build/coverage-html
