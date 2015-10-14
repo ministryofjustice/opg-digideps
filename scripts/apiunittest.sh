1f2c99f76efabae9782c042f124bbf1de5c5350c
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-10-14 14:19:57 +0100

    restore normal phpunit command

--- scripts/apiunittest.sh
+++ scripts/apiunittest.sh
@@ -3,4 +3,5 @@
 run-parts /etc/my_init.d
 
 cd /app
-/sbin/setuser app php -d zend_extension=xdebug.so vendor/phpunit/phpunit/phpunit -c tests/phpunit/phpunit.xml --coverage-clover=build/coverage.xml --coverage-html=build/coverage-html
+#/sbin/setuser app php -d zend_extension=xdebug.so vendor/phpunit/phpunit/phpunit -c tests/phpunit/phpunit.xml --coverage-clover=build/coverage.xml --coverage-html=build/coverage-html
+/sbin/setuser app php vendor/phpunit/phpunit/phpunit -c tests/phpunit/phpunit.xml
