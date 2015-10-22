09db91e3135820bc9c556779358c047f54a630e5
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-10-22 11:15:46 +0100

    dd-1173 csv to array parser with unit tests

---
+++ src/AppBundle/Service/DataImporter/CsvToArray.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace AppBundle\Service\DataImporter;
+
+class CsvToArray
+{
+    const DELIMITER = ',';
+    const ENCLOSURE = '"';
+    const ESCAPE = "\\";
+
+    private $handle;
+    private $expectedColumns = [];
+
+    /**
+     * @param string $file path to file
+     * @param array $expectedColumns e.g. ['Case','Surname', 'Deputy No', 'Dep Surname', 'Dep Postcode']
+     * @throws \RuntimeException
+     */
+    public function __construct($file, array $expectedColumns)
+    {
+        ini_set('auto_detect_line_endings', true);
+
+        if (!file_exists($file)) {
+            throw new \RuntimeException("file $file not found");
+        }
+        $this->handle = fopen($file, 'r');
+        $this->expectedColumns = $expectedColumns;
+    }
+
+
+    /**
+     * @return array or false when EOF
+     */
+    private function getRow()
+    {
+        return fgetcsv($this->handle, 2000, self::DELIMITER, self::ENCLOSURE, self::ESCAPE);
+    }
+
+
+    /**
+     * Returns 
+     * 
+     * @return array
+     */
+    public function getData()
+    {
+        $ret = [];
+
+        // parse header
+        $header = $this->getRow();
+        if (!$header) {
+            throw new \RuntimeException('Empty or corrupted file, cannot parse CSV header');
+        }
+        $missingColumns = array_diff($this->expectedColumns, $header);
+        if ($missingColumns) {
+            throw new \RuntimeException('Missing Header columns: ' . implode(', ', $missingColumns));
+        }
+
+        // read rows
+        while (($row = $this->getRow()) !== false) {
+            $ret[] = array_combine($header, $row);
+        }
+
+        return $ret;
+    }
+
+
+    public function __destruct()
+    {
+        fclose($this->handle);
+        ini_set('auto_detect_line_endings', false);
+    }
+
+}
---
+++ tests/phpunit/Service/DataImporter/CsvToArrayTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace AppBundle\Service\DataImporter;
+
+class CsvToArrayTest extends \PHPUnit_Framework_TestCase
+{
+    private $columns = ['Case','Surname', 'Deputy No', 'Dep Surname', 'Dep Postcode'];
+    
+    public function testgetData1()
+    {
+        $object = new CsvToArray(__DIR__ . '/csv1.csv', $this->columns);
+        $data = $object->getData();
+
+        $this->assertCount(24, $data);
+
+        $this->assertEquals(['Case' => '20000037',
+            'Surname' => 'SMITH',
+            'Deputy No' => '00063168',
+            'Dep Surname' => 'SMITH',
+            'Dep Postcode' => 'FY8 1FJ'], $data[0]);
+
+
+        $this->assertEquals(['Case' => '20006813',
+            'Surname' => 'HOVIS',
+            'Deputy No' => '00000422',
+            'Dep Surname' => 'HOVIS',
+            'Dep Postcode' => ''], $data[8]);
+    }
+    
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testgetDataMissingFile()
+    {
+        new CsvToArray(__DIR__ . '/THISFILEDOESNOTEXIST.csv', $this->columns);
+    }
+    
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testgetDataInvalidFormat()
+    {
+        $object = new CsvToArray(__DIR__ . '/invalid.csv', $this->columns);
+        $object->getData();
+    }
+    
+    public function testgetDataMissingColumns()
+    {
+        $object = new CsvToArray(__DIR__ . '/missing-columns.csv', $this->columns);
+        
+        try {
+            $object->getData();
+            $this->fail(__METHOD__.': expected exception');
+        } catch (\RuntimeException $e) {
+            $this->assertContains('Missing Header columns: Surname, Dep Surname', $e->getMessage());
+        }
+    }
+
+}
