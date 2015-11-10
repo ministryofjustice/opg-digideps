4eba93e68abdf444c99139af6d25301eedf06c7d
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-11-10 13:21:43 +0000

    1163: change role name

--- app/DoctrineMigrations/Version047.php
+++ app/DoctrineMigrations/Version047.php
@@ -18,7 +18,7 @@ class Version047 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql("INSERT INTO role (name, role) VALUES ('Assisted Digital', 'ROLE_AD')");
+        $this->addSql("INSERT INTO role (name, role) VALUES ('Assisted Digital Support', 'ROLE_AD')");
         
     }
 
