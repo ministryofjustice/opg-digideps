f7e52c5590ff1a54814fa4d8a140ba1122871458
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-11-16 15:26:02 +0000

    ad: behat tests

--- scripts/phpunitdb.sh
+++ scripts/phpunitdb.sh
@@ -3,5 +3,6 @@
 run-parts /etc/my_init.d
 
 cd /app
+/sbin/setuser app php app/console doctrine:query:sql "select pg_terminate_backend(pid) from pg_stat_activity where datname='digideps_unit_test'"
 /sbin/setuser app php app/console doctrine:query:sql "DROP DATABASE IF EXISTS digideps_unit_test;"
 /sbin/setuser app php app/console doctrine:query:sql "CREATE DATABASE digideps_unit_test;"
