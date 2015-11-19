974e6b08cda9872d2783ecf6a83186b4d96d69e3
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-11-19 12:51:21 +0000

    accounts_mk2 attach form to moneyin and out

---
+++ src/AppBundle/Form/TransactionSingleType.php
@@ -0,0 +1,48 @@
+<?php
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use AppBundle\Entity\AccountTransaction;
+
+class TransactionSingleType extends AbstractType
+{
+     public function buildForm(FormBuilderInterface $builder, array $options)
+     {
+         $builder 
+                 ->add('id', 'hidden')
+                 ->add('type', 'hidden')
+                 ->add('amount', 'number', [
+                     'error_bubbling' => false, 
+                     'grouping' => true, 
+                     'precision' => 2,
+                     'invalid_message'=>'account.moneyInOut.amount.notNumeric'
+                 ]);
+         
+//         $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+//            $accountTransaction = $event->getData(); /* @var $accountTransaction AccountTransaction */
+//            $form = $event->getForm();
+//
+//            if ($accountTransaction->hasMoreDetails()) {
+//                $form->add('moreDetails', 'textarea');
+//            }
+//        });
+     }
+     
+     public function setDefaultOptions(OptionsResolverInterface $resolver)
+     {
+         $resolver->setDefaults( [
+             'data_class' => 'AppBundle\Entity\Transaction',
+             'validation_groups' => ['transactions']
+        ]);
+     }
+     
+     public function getName()
+     {
+         return 'transaction_single';
+     }
+     
+}
---
+++ src/AppBundle/Form/TransactionsType.php
@@ -0,0 +1,49 @@
+<?php
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use AppBundle\Form\Type\SortCodeType;
+use AppBundle\Form\Type\AccountNumberType;
+
+class TransactionsType extends AbstractType
+{
+
+     private $property;
+
+    /**
+     * TransactionsType constructor.
+     * @param $property
+     */
+    public function __construct($property)
+    {
+        $this->property = $property;
+    }
+
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+     {
+         $builder 
+                 ->add('id', 'hidden')
+                 ->add($this->property,  'collection', ['type' => new TransactionSingleType()])
+                 ->add('save', 'submit')
+                ;
+     }
+     
+     public function setDefaultOptions(OptionsResolverInterface $resolver)
+     {
+         $resolver->setDefaults( [
+             'data_class' => 'AppBundle\Entity\Report',
+             'validation_groups' => ['transactions'],
+             // enable validation on AccountTransactionSingleType collections
+             'cascade_validation' => true,
+        ]);
+     }
+     
+     public function getName()
+     {
+         return 'transactions';
+     }
+     
+}
