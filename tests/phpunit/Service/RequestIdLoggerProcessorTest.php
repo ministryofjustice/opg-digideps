d7cae1bf4761bb76b9498fb976c03fb504517d93
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-11-23 16:51:24 +0000

    request-id-formatter logger formatter now adds X-Request-Id

---
+++ tests/phpunit/Service/RequestIdLoggerProcessorTest.php
@@ -0,0 +1,73 @@
+<?php
+namespace AppBundle\Service;
+
+use Mockery as m;
+use Symfony\Component\HttpFoundation\ParameterBag;
+use Symfony\Component\HttpFoundation\Request;
+
+class RequestIdLoggerProcessorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var RequestIdLoggerProcessor
+     */
+    private $object;
+
+    private $record = ['key1'=>'abc', 'key2'=>2];
+
+    public function setUp()
+    {
+        $this->container = m::mock('Symfony\Component\DependencyInjection\Container');
+
+        $this->object = new RequestIdLoggerProcessor($this->container);
+    }
+
+
+    public function testProcessRecordScopeInactive()
+    {
+        $this->container->shouldReceive('isScopeActive')->with('request')->andReturn(false);
+
+        $this->assertEquals($this->record, $this->object->processRecord($this->record));
+    }
+
+    public function testProcessRecordHasNoRequest()
+    {
+        $this->container
+            ->shouldReceive('isScopeActive')->with('request')->andReturn(true)
+            ->shouldReceive('has')->with('request')->andReturn(false);
+
+        $this->assertEquals($this->record, $this->object->processRecord($this->record));
+    }
+
+    public function testProcessRecordHasNoRequestId()
+    {
+        $request = new Request();
+        $request->headers = new ParameterBag();
+
+        $this->container
+            ->shouldReceive('isScopeActive')->with('request')->andReturn(true)
+            ->shouldReceive('has')->with('request')->andReturn(true)
+            ->shouldReceive('get')->with('request')->andReturn($request);
+
+        $this->assertEquals($this->record, $this->object->processRecord($this->record));
+    }
+
+    public function testProcessRecordHasRequestId()
+    {
+        $request = new Request();
+        $request->headers = new ParameterBag();
+        $request->headers->set('x-request-id', 'THIS_IS_THE_REQUEST_ID');
+
+        $this->container
+            ->shouldReceive('isScopeActive')->with('request')->andReturn(true)
+            ->shouldReceive('has')->with('request')->andReturn(true)
+            ->shouldReceive('get')->with('request')->andReturn($request);
+
+        $this->assertEquals($this->record + ['extra'=>['request_id'=>'THIS_IS_THE_REQUEST_ID']], $this->object->processRecord($this->record));
+    }
+
+
+    public function tearDown()
+    {
+        m::close();
+    }
+}
