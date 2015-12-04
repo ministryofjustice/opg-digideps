01f5b0e676313db4138e2db2c8fce1d7259b8977
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-12-04 17:34:11 +0000

    accounts_mk2_migration migration with empty name mapping

--- src/AppBundle/Service/DataMigration/AccountMigration.php
+++ src/AppBundle/Service/DataMigration/AccountMigration.php
@@ -12,6 +12,12 @@ class AccountMigration
      */
     private $pdo;
 
+
+    private static $transactionMap = [
+
+    ];
+
+
     public function __construct(Connection $pdo)
     {
         $this->pdo = $pdo;
@@ -19,22 +25,42 @@ class AccountMigration
 
     public function migrateAccounts()
     {
-        $data = $this->getReports();
+        $reports = $this->getReports();
 
-    }
+        $oldTypes = $this->fetchAll("SELECT * from account_transaction_type");
+        $newTypes = $this->fetchAll("SELECT * from transaction_type");
 
-    private function fetchAll($query, $key = 'id')
-    {
-        $results = $this->pdo->query($query)->fetchAll();
+//        $toCsv = function($rows) {
+//            $ret = '';
+//            foreach($rows as $row) {
+//                $ret .= implode(', ', $row) . "\n";
+//            }
+//            return $ret;
+//        };
+//        file_put_contents(__DIR__ . '/types.old.csv', $toCsv($oldTypes, true));
+//        file_put_contents(__DIR__ . '/types.new.csv', $toCsv($newTypes, true));
 
-        $ret = [];
-        foreach ($results as $result) {
-            $keyValue = $result[$key];
-            $ret[$keyValue] = $result;
+        $stmt = $this->pdo->prepare(
+            "INSERT INTO transaction(report_id, transaction_type_id, amount, more_details)"
+            . " VALUES(:id, :transaction_type_id, :amount, :md)");
+
+        foreach ($reports as $reportId => $reportData) {
+            foreach ($reportData['accounts'] as $account) {
+                foreach ($account['transactions_old'] as $typeId => $tRow) {
+                    if (!isset(self::$transactionMap[$typeId])) {
+                        throw new \RuntimeException("cannot map old transacition type [$typeId]");
+                    }
+                    $stmt->execute([
+                        ':id' => $reportId,
+                        ':transaction_type_id' => $typeId,
+                        ':amount' => $tRow['amount'],
+                        ':md' => $tRow['more_details'],
+                    ]);
+                }
+            }
         }
-
-        return $ret;
     }
+
 
     public function getReports()
     {
@@ -45,7 +71,7 @@ class AccountMigration
             $reports[$k]['transactions_new'] =
                 $this->fetchAll('SELECT * from transaction WHERE report_id = ' . $report['id']);
             $reports[$k]['transactions_new_sum'] =
-                $this->calculateTotals($reports[$k]['transactions_new']);
+                $this->calculateAmountsTotal($reports[$k]['transactions_new']);
 
 
             // add accounts
@@ -60,20 +86,46 @@ class AccountMigration
 
                         WHERE account_id = ' . $account['id'], 'account_transaction_type_id');
                 $reports[$k]['accounts'][$ka]['transactions_old_sum'] =
-                    $this->calculateTotals($reports[$k]['accounts'][$ka]['transactions_old']);
+                    $this->calculateAmountsTotal($reports[$k]['accounts'][$ka]['transactions_old']);
             }
         }
 
         return $reports;
     }
 
-    private function calculateTotals(array $transactions, $key = 'amount')
+    /**
+     * @param array $transactions array of [type=>in/out, amount=>integer]
+     * @param string $key
+     * @return array of [in=>integer sum of the amount with type=in, out=>integer sum of the amount with type=out]
+     */
+    private function calculateAmountsTotal(array $transactions)
     {
-        $ret = ['in'=>0.0, 'out'=>0.0];
+        $ret = ['in' => 0.0, 'out' => 0.0];
 
         foreach ($transactions as $t) {
             $ret[$t['type']] += $t['amount'];
         }
+        return $ret;
+    }
+
+
+    /**
+     * Return query ASSOC results, using $key as ID
+     * @param string $query
+     * @param string $key
+     *
+     * @return array
+     */
+    private function fetchAll($query, $key = 'id')
+    {
+        $results = $this->pdo->query($query)->fetchAll();
+
+        $ret = [];
+        foreach ($results as $result) {
+            $keyValue = $result[$key];
+            $ret[$keyValue] = $result;
+        }
+
         return $ret;
     }
 
