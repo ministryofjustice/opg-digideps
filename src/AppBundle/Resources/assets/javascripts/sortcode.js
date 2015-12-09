6de34702a1f9ddbb888d602d904ccd70aeeeb26d
Zac Tolley <zac.tolley@digital.justice.gov.uk>
2015-12-09 13:56:05 +0000

    Start merge for form error display and stop auto tab on sort code.

--- src/AppBundle/Resources/assets/javascripts/sortcode.js
+++ src/AppBundle/Resources/assets/javascripts/sortcode.js
@@ -2,26 +2,16 @@
 var opg = opg || {};
 
 (function ($, opg) {
-    
+
     var sortCodeParts;
     var container;
     var noneNumericError = 'Sort code must be numeric';
     var errors;
 
-    function skipToNext(event) {
-        
-        var target = $(event.target);
-        
-        if(target.val().length == target.attr('maxlength') && target[0] !== sortCodeParts[sortCodeParts.length -1] ) {
-            var nextField = $('input', target.parent().nextAll(".form-group")[0]);
-            nextField.focus();
-        }    
-    }
-
     function showErrorDescription() {
         if ($('.form-sort-code .field-with-errors').length > 0){
-            errors.empty().append('<li class="error-message">' + noneNumericError + '</li>');    
-            container.addClass('field-with-errors');  
+            errors.empty().append('<li class="error-message">' + noneNumericError + '</li>');
+            container.addClass('field-with-errors');
         } else {
             container.removeClass('field-with-errors');
             errors.empty();
@@ -29,7 +19,7 @@ var opg = opg || {};
     }
 
     function validateField(event) {
-        
+
         var field = $(event.target);
         var str = field.val();
         var parent = field.parent();
@@ -42,13 +32,13 @@ var opg = opg || {};
 
         showErrorDescription();
     }
-    
+
     opg.SortCodeValidate = function(target) {
         container = $(target);
         errors = container.find('.errors');
         sortCodeParts = container.find(".sort-code-part");
-        
-        sortCodeParts.on('propertychange input', skipToNext);
+
+        //sortCodeParts.on('propertychange input', skipToNext);
         sortCodeParts.on('blur', validateField);
     };
 
