4087d63b3d9aee901b0e8699315d4b5fe52b82db
Zac Tolley <zac.tolley@digital.justice.gov.uk>
2015-12-09 14:32:24 +0000

    Fix formatting broken by phpstorm

--- src/AppBundle/Form/AssetType.php
+++ src/AppBundle/Form/AssetType.php
@@ -7,35 +7,35 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 /**
  * Asset form
- *
+ * 
  * note: title is hidden (filled from the controller based on AssetTypeTitle form)
  */
 class AssetType extends AbstractType
 {
-
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    
+    public function buildForm(FormBuilderInterface $builder, array $options) 
     {
         $builder->add('title', 'hidden') //use the AssetTypeTile to display the title
-        ->add('value', 'number', [
-            'grouping' => true,
-            'precision' => 2,
-            'invalid_message' => 'asset.value.type'
-        ])
-            ->add('description', 'textarea')
-            ->add('valuationDate', 'date',[ 'widget' => 'text',
-                'input' => 'datetime',
-                'format' => 'dd-MM-yyyy',
-                'invalid_message' => 'Enter a valid date'
-            ])
-            ->add('id', 'hidden')
-            ->add('save', 'submit');
+                ->add('value', 'number', [ 
+                    'grouping' => true, 
+                    'precision' => 2, 
+                    'invalid_message' => 'asset.value.type'
+                ])
+                ->add('description', 'textarea')
+                ->add('valuationDate', 'date',[ 'widget' => 'text',
+                                                 'input' => 'datetime',
+                                                 'format' => 'dd-MM-yyyy',
+                                                 'invalid_message' => 'Enter a valid date'
+                                              ])
+                ->add('id', 'hidden')
+                ->add('save', 'submit');
     }
-
-    public function getName()
+    
+    public function getName() 
     {
         return 'asset';
     }
-
+    
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults( [
