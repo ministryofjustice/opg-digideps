5e643e412146a950d4ebc182edcd64e68675802c
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2015-12-15 12:02:30 +0000

    admin-stats update test

--- tests/phpunit/Controller/StatsControllerTest.php
+++ tests/phpunit/Controller/StatsControllerTest.php
@@ -88,9 +88,11 @@ class StatsControllerTest extends AbstractTestController
         $first = array_shift($data);
         $this->assertArrayHasKey('is_active', $first);
         $this->assertArrayHasKey('email', $first);
+        $this->assertArrayHasKey('total_reports', $first);
         $this->assertArrayHasKey('active_reports', $first);
-        $this->assertArrayHasKey('active_reports_bank_accounts', $first);
-        $this->assertArrayHasKey('active_reports_transactions', $first);
+        $this->assertArrayHasKey('active_reports_due', $first);
+        $this->assertArrayHasKey('active_reports_added_bank_accounts', $first);
+        $this->assertArrayHasKey('active_reports_added_transactions', $first);
     }
 
 }
