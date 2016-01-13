f3a15fbbbea7d51795105c9dfcb2d5ae583816db
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2016-01-13 14:07:47 +0000

    accounts_mk2 delete cache before API unit test

--- scripts/apiunittest.sh
+++ scripts/apiunittest.sh
@@ -3,6 +3,8 @@
 run-parts /etc/my_init.d
 
 cd /app
+# clear cache
+rm -rf app/cache/*
 # run source code tests
 /sbin/setuser app php -d zend_extension=xdebug.so vendor/phpunit/phpunit/phpunit -c tests/phpunit/src/phpunit.xml --debug --coverage-clover=build/coverage.xml --coverage-html=build/coverage-html
 # run data migration tests
