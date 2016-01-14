b5e4989d911844536bfc3d532991d0abef0baf51
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2016-01-14 12:18:12 +0000

    accounts_mk2 remove phpunit debug

--- scripts/apiunittest.sh
+++ scripts/apiunittest.sh
@@ -6,6 +6,6 @@ cd /app
 # clear cache
 rm -rf app/cache/*
 # run source code tests
-/sbin/setuser app php -d zend_extension=xdebug.so vendor/phpunit/phpunit/phpunit -c tests/phpunit/src/phpunit.xml --debug --coverage-clover=build/coverage.xml --coverage-html=build/coverage-html
+/sbin/setuser app php -d zend_extension=xdebug.so vendor/phpunit/phpunit/phpunit -c tests/phpunit/src/phpunit.xml --coverage-clover=build/coverage.xml --coverage-html=build/coverage-html
 # run data migration tests
 /sbin/setuser app php vendor/phpunit/phpunit/phpunit -c tests/phpunit/DataMigration/phpunit.xml
