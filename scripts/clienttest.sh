d01df96abeb99b97ab996ea4237e66e5ec53d60d
Zac Tolley <zac.tolley@digital.justice.gov.uk>
2016-01-19 12:50:23 +0000

    Added support to specify a Selenium host when launching tests so tests can be run using a local browser

--- scripts/clienttest.sh
+++ scripts/clienttest.sh
@@ -24,6 +24,6 @@ else
     behatConfigFile=tests/behat/behat.yml.dist
 fi
 
-export BEHAT_PARAMS="{\"extensions\" : {\"Behat\\\\MinkExtension\\\\ServiceContainer\\\\MinkExtension\" : {\"base_url\" : \"${FRONTEND_NONADMIN_HOST}\",\"browser_stack\" : { \"username\": \"$BROWSERSTACK_USER\", \"access_key\": \"$BROWSERSTACK_KEY\"}}}}" 
+export BEHAT_PARAMS="{\"extensions\" : {\"Behat\\\\MinkExtension\\\\ServiceContainer\\\\MinkExtension\" : {\"base_url\" : \"${FRONTEND_NONADMIN_HOST}\",\"selenium2\" : { \"wd_host\" : \"$WD_HOST\" }, \"browser_stack\" : { \"username\": \"$BROWSERSTACK_USER\", \"access_key\": \"$BROWSERSTACK_KEY\"}}}}" 
 echo BEHAT_PARAMS
 /sbin/setuser app bin/behat --config=$behatConfigFile --suite=${suitename} --profile=${PROFILE} --stop-on-failure
