57cc871bf263562640445a7d2ad8a0bb0a9ce752
Zac Tolley <zac@thetolleys.com>
2016-01-22 11:12:03 +0000

    wip

--- src/AppBundle/Resources/assets/javascripts/autosave.js
+++ src/AppBundle/Resources/assets/javascripts/autosave.js
@@ -18,29 +18,44 @@
         
     };
     
-    AutoSave.prototype.AddEventHandlers = function () {
-        this.changeHandler = this.getChangeHandler();
-
-        this.form.on('submit', this.changeHandler);
-        this.form.find(['input']).on('blur', this.changeHandler);
-        $(window).on("beforeunload", autosave);
+    AutoSave.prototype.addEventHandlers = function () {
+        this.blurHandler = this.getBlurHandler();
+        this.submitHandler = this.getSubmitHandler();
+        this.leaveHandler = this.getLeaveHandler();
         
-        // ?? Does a form field blur when you leave a page????
-        
+        this.form.on('submit', this.submitHandler);
+        this.form.find(['input']).on('blur', this.blurHandler);
+        //$(window).on("beforeunload", this.leaveHandler);
     };
     
-    AutoSave.prototype.getChangeHandler = function () {
+    AutoSave.prototype.getBlurHandler = function () {
         return function (e) {
             e.preventDefault();
-            this.handleChange($(e.target));
+            this.save();
+            return true;
+        }.bind(this);
+    };
+    AutoSave.prototype.getSubmitHandler = function () {
+        return function (e) {
+            e.preventDefault();
+            this.save();
+            // redirect to desired location
+            return false;
+        }.bind(this);
+    };
+    AutoSave.prototype.getLeaveHandler = function () {
+        return function (e) {
+            e.preventDefault();
+            this.save();
+            return true;
         }.bind(this);
     };
     
     AutoSave.prototype.handleChange = function (target) {
-        this.save();
+        // set status flag to say data has changed.
     };
     
-    AutoSave.prototype.save = function () {
+    AutoSave.prototype.save = function (done) {
         
         var data = this.form.serialize();
         
@@ -48,10 +63,16 @@
             type: 'PUT',
             url: this.url,
             data: data,
-            done: this.handleSaveDone,
-            fail: this.handleSaveError
+            done: function(data) {
+                this.handleSaveDone(data);
+                done();
+            },
+            fail: function(data) {
+               this.handleSaveError(data);
+                done();
+            }
         });
-    
+ 
     };
     
     AutoSave.prototype.showFieldErrors = function (errors) {
@@ -66,23 +87,19 @@
     };
     
     AutoSave.prototype.handleSaveDone = function (data) {
-        console.log('done')
+        console.log('done');
         console.log(data);
-    }
+    };
     
     AutoSave.prototype.handleSaveError = function (data) {
         console.log(data);
         if (data.errors.errorCode === 1001 && data.errors.hasOwnProperty('fields')) {
-            showFieldErrors(data.errors.fields);
+            //showFieldErrors(data.errors.fields);
         } else {
-            showGeneralError(data.errors.description);
+            //showGeneralError(data.errors.description);
         }
     };
     
     root.GOVUK.AutoSave = AutoSave;
     
 }).call(this);
-
-
-
-
--- tests/mocha/autosavetests.js
+++ tests/mocha/autosavetests.js
@@ -1,40 +1,103 @@
 describe('Sort Code Tests', function () {
 
     var placeholder = $('#placeholder'),
-        template = $('#template');
+        template = $('#template'),
+        autosave,
+        ajaxSpy;
     
     beforeEach(function() {
-        placeholder.empty('').append(template.text());
+        placeholder.empty('').append(template.html());
 
-        new GOVUK.AutoSave({
+        autosave = new GOVUK.AutoSave({
             form: $('#placeholder form.expanding-transaction-table'),
             statusElement: $('#placeholder #info'),
             url: 'test'
         });
         
-        // mock $.ajax
+        ajaxSpy = sinon.stub(jQuery, 'ajax', function(options) {
+            options.done({status:'success'});
+        });
         
     });
     afterEach(function() {
-        // restore $.ajax
+        ajaxSpy.restore();
     });
     
-    describe('Save Form', function () {
+    describe('Trigger save form', function () {
+        
+        var saveStub;
+        
+        beforeEach(function () {
+            saveStub = sinon.spy(autosave, 'save');
+        });
+        
+        afterEach(function () {
+            saveStub.restore();
+        });
+        
+        it('should call save then whe user moves off a field after changing something manually', function () {
+            $('#placeholder input').eq(0).trigger('keyup').trigger('blur');
+            expect(saveStub.callCount).to.equal(1);
+        });
+        it('should call save then whe user moves off a field after changing something through paste', function () {
+            $('#placeholder input').eq(0).trigger('paste').trigger('blur');
+            expect(saveStub.callCount).to.equal(1);
+        });
+        it('should not call save then whe user moves off a field but not interact with it', function () {
+            $('#placeholder input').eq(0).trigger('blur');
+            expect(saveStub.callCount).to.equal(1);
+        });
+        it('should call save when a user submits a form', function () {
+            $('#placeholder form').eq(0).trigger('submit');
+            expect(saveStub.callCount).to.equal(1); 
+        });
+        it('should call save when a user moves off the page', function () {
+            // When I leave a page through submit, how is this captured?
+            
+            // how about, when I submit a form, set a flag to say don't save (because already saved)
+            // or on paste and key up set status to changed, then after saving set status to saved
+        });
+
+        it('should not call save on form submit if already saved through blur', function () {
+
+        });
+
+        it('should not call save on page leave and form submitted', function () {
+            
+        });
+        it('should not call save on page leave if saved through blur', function () {
+            
+        });
+
+    });
+    
+    describe('Tell the user we are saving', function () {
         
     });
     
-    describe('Tell the user we are saving');
+    describe('Tell the user we saved on success', function () {
+        
+    });
     
-    describe('Tell the user we saved on success');
+    describe('Tell the user about form validation errors', function () {
+        
+    });
     
-    describe('Tell the user about form validation errors');
+    describe('Tell the user about some other error', function () {
+        
+    });
     
-    describe('Tell the user about some other error');
+    describe('Calls save when field blurs', function () {
+        
+    });
     
-    describe('Calls save when field blurs');
+    describe('Calls save when form submits', function () {
+        
+    });
     
-    describe('Calls save when form submits');
+    describe('Calls save when page exits', function () {
+        
+    });
     
-    describe('Calls save when page exits');
-    
+    describe('When I pretend to submit a form, goto the defined location');
 });
