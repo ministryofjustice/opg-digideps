2c6fe6c580011c6bb7822ca14460402594da35f4
Zac Tolley <zac@thetolleys.com>
2016-01-27 15:07:42 +0000

    Fix issues with iPad and Android

--- src/AppBundle/Resources/assets/javascripts/sticky.js
+++ src/AppBundle/Resources/assets/javascripts/sticky.js
@@ -13,38 +13,62 @@
         this.wrapper = $(element);
         this.top = $(element).offset().top - 30;
         this.addEventHandlers();
+        this.fieldFocused = false;
     };
     
     StickyHeader.prototype.addEventHandlers = function () {
+        if (mobileSafari) {
+            this.blurHandler = this.getInputBlurEventHandler();
+            this.focusHandler = this.getInputFocusEventHandler();
+            $('input, textarea').on('blur', this.blurHandler);
+            $('input, textarea').on('focus', this.focusHandler);
+        }
         this.windowScrollHandler = this.getWindowScrollEventHandler();
-        document.addEventListener("scroll", this.windowScrollHandler, false);
+        $(window).on('scroll', this.windowScrollHandler);
     };
     StickyHeader.prototype.getWindowScrollEventHandler = function () {
         return function () {
             this.handleWindowScroll();
         }.bind(this);
     };
-    
+    StickyHeader.prototype.getInputFocusEventHandler = function () {
+        return function () {
+            this.handleFocus();
+        }.bind(this);
+    };
+    StickyHeader.prototype.getInputBlurEventHandler = function () {
+        return function () {
+            this.handleBlur();
+        }.bind(this);
+    };
+    StickyHeader.prototype.handleFocus = function () {
+        this.fieldFocused = true;
+        this.handleWindowScroll();
+    };
+    StickyHeader.prototype.handleBlur = function () {
+        this.fieldFocused = false;
+        this.handleWindowScroll();
+    };
     StickyHeader.prototype.handleWindowScroll = function () {
         if (window.pageYOffset >= this.top) {
             body.addClass('fixed');
             
             // Mobile safari hides fixed elements when the keyboard is shown so use
             // absolute instead.
-            if (mobileSafari) {
+            if (mobileSafari && this.fieldFocused) {
                 this.wrapper.css({
                     position: 'absolute',
                     top: window.pageYOffset + 'px',
                     left: 0
                 });
+            } else {
+                this.wrapper.attr('style','');
             }
             
         } else {
             body.removeClass('fixed');
             if (mobileSafari) {
-                this.wrapper.css({
-                    position: 'static'
-                });
+                this.wrapper.attr('style','');
             }
         }
     };
