5560ac19e790ca7c1116291beded97c2cc58f919
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2016-01-28 12:28:19 +0000

    small improvements on previous script

--- misc/scripts/findThousandsMismatching.php
+++ misc/scripts/findThousandsMismatching.php
@@ -8,27 +8,27 @@ function findTotalsThousandsMismatch($values)
     $ret = [];
     for ($i = 0; $i <= pow(2, $numbersOfValues); $i++) {
 
-        $vals = $values; //copy
+        $vals = $values; 
         $valsWithRealValue = [];
         // modify values
-        $bin = str_pad((string) decbin($i), $numbersOfValues, "0", STR_PAD_LEFT);
+        $bin = str_pad((string) base_convert($i, 10, 2), $numbersOfValues, "0", STR_PAD_LEFT);
         for ($j = 0; $j < strlen($bin); $j++) {
             if ($bin[$j] === '1') {
-                $valsWithRealValue[$j] =$vals[$j] . ' --> ' . $vals[$j] * 1000;
+                $valsWithRealValue[$j] =$vals[$j] . ' => ' . $vals[$j] * 1000;
                 $vals[$j] = $vals[$j] * 1000;
                 
             }
         }
         $diff = abs(array_sum($vals));
-        $ret[(float) $diff] = implode(' | ',$valsWithRealValue);
+        $ret[$diff] = "DIFF: £" . number_format(array_sum($vals), 2) . " Changes: ".implode(' | ',$valsWithRealValue);
     }
     ksort($ret);
 
-    return $ret;
+    return array_values($ret);
 }
 
 $combinations = findTotalsThousandsMismatch([
    // add values here, mone out and closing balance must be negative
 ]);
-print_r($combinations);
+echo implode("\n", $combinations);
 echo "best Match: £" . array_slice(array_keys($combinations), 0, 1)[0]."\n";
