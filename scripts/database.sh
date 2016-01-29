65afe2af0697130907dfbf63641200bfaa537bda
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2016-01-29 16:04:10 +0000

    dd-1336 add status check to database.sh and container

--- scripts/database.sh
+++ scripts/database.sh
@@ -4,5 +4,6 @@ set -e
 run-parts /etc/my_init.d
 
 cd /app
+/sbin/setuser app php app/console doctrine:migrations:status-check
 /sbin/setuser app php app/console doctrine:migrations:migrate --no-interaction -vvv
 /sbin/setuser app php app/console digideps:fixtures
