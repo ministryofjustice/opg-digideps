08334abb94465359fc3c689a1134ac15da1108e0
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2016-02-12 12:12:49 +0000

    dd-1109-next-year-report add detail fields

--- app/DoctrineMigrations/Version056.php
+++ app/DoctrineMigrations/Version056.php
@@ -18,7 +18,7 @@ class Version056 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('CREATE TABLE concern (id SERIAL NOT NULL, report_id INT DEFAULT NULL, do_you_expect_decisions TEXT DEFAULT NULL, do_you_have_concerns TEXT DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE concern (id SERIAL NOT NULL, report_id INT DEFAULT NULL, do_you_expect_decisions VARCHAR(4) DEFAULT NULL, do_you_expect_decisions_details TEXT DEFAULT NULL, do_you_have_concerns VARCHAR(4) DEFAULT NULL, do_you_have_concerns_details TEXT DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_281EFBA84BD2A4C0 ON concern (report_id)');
         $this->addSql('ALTER TABLE concern ADD CONSTRAINT FK_281EFBA84BD2A4C0 FOREIGN KEY (report_id) REFERENCES report (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
