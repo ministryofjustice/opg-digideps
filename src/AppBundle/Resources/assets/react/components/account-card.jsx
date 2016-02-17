49025ca5f749af7a9593609ecd4a3af26a1a0ad5
Zac Tolley <zac@thetolleys.com>
2016-02-17 09:23:35 +0000

    Start moving some react components to be function components. Made the display of the transfer text depend on number of accounts, and only allow use if 2 or more.

--- src/AppBundle/Resources/assets/react/components/account-card.jsx
+++ src/AppBundle/Resources/assets/react/components/account-card.jsx
@@ -1,36 +1,22 @@
 import React, { Component } from 'react';
 
-class AccountCard extends Component {
-
-    constructor (props) {
-        super(props);
-        this.clickCard = this.clickCard.bind(this);
-    }
+const AccountCard =({account, selectAccount}) => {
     
-    clickCard () {
-        if (this.props.selectAccount) {
-            this.props.selectAccount(this.props.account);
-        }
-    }
-    
-    render () {
-        
-        var account = this.props.account;
-        
-        return (
-            <div className="card-item">
-                <div className="card" onClick={this.clickCard}>
-                    <div className="account card-title">{account.name} {account.type}</div>
-                    <dl className="labelvalue">
-                        <dt className="label">Account number:</dt>
-                        <dd className="value">0000{account.accountNumber}</dd>
-                        <dt className="label">Sort code:</dt>
-                        <dd className="value">{account.sortCode}</dd>
-                    </dl>
-                </div>
+    return (
+        <div className="card-item">
+            <div className="card" onClick={() => selectAccount(account)}>
+                <div className="account card-title">{account.name} {account.type}</div>
+                <dl className="labelvalue">
+                    <dt className="label">Account number:</dt>
+                    <dd className="value">0000{account.accountNumber}</dd>
+                    <dt className="label">Sort code:</dt>
+                    <dd className="value">{account.sortCode}</dd>
+                </dl>
             </div>
-        );
-    }
-}
+        </div>
+    );
+
+};
+
 
 export default AccountCard;
