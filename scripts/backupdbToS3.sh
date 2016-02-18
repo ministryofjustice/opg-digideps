67598c0f31ad23569d0306a8fc7178b0baa9c55c
jamromia <rmubayiwa@hotmail.com>
2016-02-18 15:57:26 +0000

    Modified Backup script for API container

--- scripts/backupdbToS3.sh
+++ scripts/backupdbToS3.sh
@@ -16,8 +16,7 @@ fi
 
 db_archive=dd-$API_DATABASE_NAME-$(date +%Y%m%d).pg.gz
 
-if (PGPASSWORD="$API_DATABASE_PASSWORD" pg_dump -C -h $API_DATABASE_HOSTNAME -U $API_DATABASE_USERNAME $API_DATABASE_NAME | gzip | aws s3 cp --region e
-u-west-1 --sse aws:kms - s3://$1/backups/$2/$db_archive)
+if (PGPASSWORD="$API_DATABASE_PASSWORD" pg_dump -C -h $API_DATABASE_HOSTNAME -U $API_DATABASE_USERNAME $API_DATABASE_NAME | gzip | aws s3 cp --region eu-west-1 --sse aws:kms - s3://$1/backups/$2/$db_archive)
 then
         echo "DB snapshot created copied to S3 bucket SUCCESSFULLY"
 else
