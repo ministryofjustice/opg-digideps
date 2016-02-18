49786d84d598e2e16261d43e560a8f4859c3f66e
jamromia <rmubayiwa@hotmail.com>
2016-02-18 16:21:47 +0000

    Review modifications to Backup/Restore scripts for API container

--- scripts/backupdbToS3.sh
+++ scripts/backupdbToS3.sh
@@ -5,7 +5,7 @@
 
 usage ()
 {
-  echo "Usage : $0  <S3 bucket name> <pipeline>"
+  echo "Usage : $0  <S3 bucket name> <snapshot name>"
   exit
 }
 
@@ -14,12 +14,10 @@ then
   usage
 fi
 
-db_archive=dd-$API_DATABASE_NAME-$(date +%Y%m%d).pg.gz
-
-if (PGPASSWORD="$API_DATABASE_PASSWORD" pg_dump -C -h $API_DATABASE_HOSTNAME -U $API_DATABASE_USERNAME $API_DATABASE_NAME | gzip | aws s3 cp --region eu-west-1 --sse aws:kms - s3://$1/backups/$2/$db_archive)
+if (PGPASSWORD="$API_DATABASE_PASSWORD" pg_dump -C -h $API_DATABASE_HOSTNAME -U $API_DATABASE_USERNAME $API_DATABASE_NAME | gzip | aws s3 cp --region eu-west-1 --sse aws:kms - s3://$1/$2)
 then
-        echo "DB snapshot created copied to S3 bucket SUCCESSFULLY"
+        echo "DB snapshot created copied to S3 bucket location s3://$1/$2 SUCCESSFULLY"
 else
-        echo "DB snapshot creation or upload to S3 backup bucket FAILED"
+        echo "DB snapshot s3://$1/$2 creation or upload to S3 backup bucket FAILED"
 	usage
 fi
--- scripts/restoredbFromS3.sh
+++ scripts/restoredbFromS3.sh
@@ -5,21 +5,20 @@
 
 usage ()
 {
-  echo "Usage : $0  <S3 bucket name> <pipeline> <snapshot name>"
+  echo "Usage : $0  <S3 bucket name> <snapshot name>"
   exit
 }
 
-if [ "$#" -ne 3 ]
+if [ "$#" -ne 2 ]
 then
   usage
 fi
 
 # Shutdown existing instance, drop and create DB.
-echo "Restoring saved database snapshot s3://$1/backups/$2/$3..."
+echo "Restoring saved database snapshot s3://$1/$2..."
 echo "Creating a blank Postgres DB...."
 
-PGPASSWORD=$API_DATABASE_PASSWORD psql -P pager=off -U $API_DATABASE_USERNAME -h $API_DATABASE_HOSTNAME -p $API_DATABASE_PORT -c "SELECT pg_terminate_b
-ackend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$API_DATABASE_NAME' AND pid <> pg_backend_pid();"
+PGPASSWORD=$API_DATABASE_PASSWORD psql -P pager=off -U $API_DATABASE_USERNAME -h $API_DATABASE_HOSTNAME -p $API_DATABASE_PORT -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$API_DATABASE_NAME' AND pid <> pg_backend_pid();"
 PGPASSWORD=$API_DATABASE_PASSWORD dropdb -U $API_DATABASE_USERNAME -h $API_DATABASE_HOSTNAME -p $API_DATABASE_PORT $API_DATABASE_NAME
 PGPASSWORD=$API_DATABASE_PASSWORD createdb -U $API_DATABASE_USERNAME -h $API_DATABASE_HOSTNAME -p $API_DATABASE_PORT $API_DATABASE_NAME
 
@@ -28,8 +27,8 @@ echo "Restoring database tables and data...."
 
 if (aws s3 cp --sse aws:kms --region eu-west-1 s3://$1/backups/$2/$3 - | gunzip  | PGPASSWORD=$API_DATABASE_PASSWORD psql -P pager=off -U $API_DATABASE_USERNAME -h $API_DATABASE_HOSTNAME -p $API_DATABASE_PORT $API_DATABASE_NAME)
 then
-        echo "Restore from specified snapshot SUCCESSFUL!"
+        echo "Restore from specified snapshot s3://$1$2 SUCCESSFUL!"
 else
-        echo "DB restore from specified snapshot FAILED!"
+        echo "DB restore from specified snapshot  s3://$1$2 FAILED!"
 	usage
 fi
