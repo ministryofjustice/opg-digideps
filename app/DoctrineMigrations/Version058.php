afaa91af3fe9ee638818734e67888442d416441a
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2016-03-02 17:46:00 +0000

    dd-1397 data migration, to test

--- app/DoctrineMigrations/Version058.php
+++ app/DoctrineMigrations/Version058.php
@@ -5,12 +5,22 @@ namespace Application\Migrations;
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 use AppBundle\Service\DataMigration\AccountMigration;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version058 extends AbstractMigration
+class Version058 extends AbstractMigration implements ContainerAwareInterface
 {
+
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
     /**
      * @param Schema $schema
      */
@@ -22,22 +32,30 @@ class Version058 extends AbstractMigration
 //        $em = $this->container->get('em');
 //        $am = new AccountMigration($em->getConnection());
 //        $am->migrateAll();
-        
+
         $pdo = $this->container->get('em')->getConnection();
-        $reports = $pdo->query('SELECT * from report')->fetchAll();
-        
+        $reports = $pdo->query("SELECT r.id as reportId, t.amount as incomeFromInvAmount from report r LEFT JOIN transaction t ON t.report_id=r.id WHERE t.transaction_type_id='income-from-investments'")->fetchAll();
+
         $this->addSql("UPDATE transaction_type SET display_order = display_order * 10");
-        
+
         $this->addSql("INSERT INTO transaction_type(id, has_more_details, display_order, category, type) VALUES('other-incomes', false, 65, 'income-and-earnings', 'in')");
-        //TODO add to all the report, transaction table
         
-        //TODO: copy values income-from-investments -> other-incomes
+        // ADD new transaction to all the report. copy amount from income-from-investments transaction (will be deleted)
+        $stmt = $pdo->prepare(
+                "INSERT INTO transaction(report_id, transaction_type_id, amount, more_details)"
+                . " VALUES(:report_id, :transaction_type_id, :amount, :md)");
+        foreach ($reports as $report) {
+            $params = [
+                ':report_id' => $report['reportId'],
+                ':transaction_type_id' => 'other-incomes',
+                ':amount' => $report['incomeFromInvAmount'], //could be null
+                ':md' => '',
+            ];
+            $stmt->execute($params);
+        }
+        
         $this->addSql("DELETE FROM transaction WHERE transaction_type_id='income-from-investments'"); //TODO migrate 
         $this->addSql("DELETE FROM transaction_type WHERE id='income-from-investments'"); //TODO migrate 
-        
-        //TODO
-        
-        
     }
 
     /**
@@ -49,7 +67,6 @@ class Version058 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('');
-
-
     }
+
 }
