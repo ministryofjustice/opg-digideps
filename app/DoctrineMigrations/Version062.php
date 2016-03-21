a8e847622bd277aa7799ecea2c5babe5a232949c
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2016-03-21 15:21:18 +0000

    dd-1369 rename migrations

--- app/DoctrineMigrations/Version062.php
+++ app/DoctrineMigrations/Version062.php
@@ -18,7 +18,14 @@ class Version062 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('ALTER TABLE report ADD no_transfers_to_add BOOLEAN DEFAULT \'false\'');
+        $this->addSql('CREATE SEQUENCE money_transfer_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE money_transfer (id INT NOT NULL, from_account_id INT DEFAULT NULL, to_account_id INT DEFAULT NULL, report_id INT DEFAULT NULL, amount NUMERIC(14, 2) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_A15E50EEB0CF99BD ON money_transfer (from_account_id)');
+        $this->addSql('CREATE INDEX IDX_A15E50EEBC58BDC7 ON money_transfer (to_account_id)');
+        $this->addSql('CREATE INDEX IDX_A15E50EE4BD2A4C0 ON money_transfer (report_id)');
+        $this->addSql('ALTER TABLE money_transfer ADD CONSTRAINT FK_A15E50EEB0CF99BD FOREIGN KEY (from_account_id) REFERENCES account (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE money_transfer ADD CONSTRAINT FK_A15E50EEBC58BDC7 FOREIGN KEY (to_account_id) REFERENCES account (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE money_transfer ADD CONSTRAINT FK_A15E50EE4BD2A4C0 FOREIGN KEY (report_id) REFERENCES report (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
     /**
@@ -29,6 +36,7 @@ class Version062 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('ALTER TABLE report DROP no_transfers_to_add');
+        $this->addSql('DROP SEQUENCE money_transfer_id_seq CASCADE');
+        $this->addSql('DROP TABLE money_transfer');
     }
 }
