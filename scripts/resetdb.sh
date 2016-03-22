514065fdf5dbc9bb23b55a2ed6282bf6b63bc189
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2016-03-22 10:10:17 +0000

    dd-1369 try restore old drop script

--- scripts/resetdb.sh
+++ scripts/resetdb.sh
@@ -4,7 +4,7 @@ set -e
 run-parts /etc/my_init.d
 
 cd /app
-/sbin/setuser app php app/console doctrine:schema:drop --force
+/sbin/setuser app php app/console doctrine:query:sql "DROP SCHEMA IF EXISTS public cascade; CREATE SCHEMA IF NOT EXISTS public;"
 /sbin/setuser app php app/console doctrine:migrations:status-check
 /sbin/setuser app php app/console doctrine:migrations:migrate --no-interaction -vvv
 /sbin/setuser app php app/console digideps:fixtures
