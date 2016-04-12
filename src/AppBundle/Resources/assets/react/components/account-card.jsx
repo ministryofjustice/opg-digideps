168edabf500f7642e84e6e201ca7a2149825d546
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2016-04-12 11:41:03 +0100

    master remove sortcode from ransfers card list for postffice/cfo accounts

--- src/AppBundle/Resources/assets/react/components/account-card.jsx
+++ src/AppBundle/Resources/assets/react/components/account-card.jsx
@@ -10,14 +10,18 @@ class AccountCard extends Component {
 
     render() {
         const account = this.props.account;
+        let sortCodeClass = account.sort_code ? '' : 'hidden';
+
         return (
             <div className="card" onClick={this.selectAccount}>
                 <div className="account card-title">{account.bank} {account.account_type_text}</div>
                 <dl className="labelvalue">
                     <dt className="label">Account number:</dt>
                     <dd className="value">****{account.account_number}</dd>
-                    <dt className="label">Sort code:</dt>
-                    <dd className="value">{account.sort_code}</dd>
+                    <div className={sortCodeClass}>
+                        <dt className="label">Sort code:</dt>
+                        <dd className="value">{account.sort_code}</dd>
+                    </div>
                 </dl>
             </div>
         );
