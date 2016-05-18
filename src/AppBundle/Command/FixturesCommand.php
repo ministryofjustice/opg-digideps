19890d3001ea91d97bdc2ecdb91a0f10f66b0719
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2016-05-18 13:16:09 +0200

    syntax-fix rename command

--- src/AppBundle/Command/FixturesCommand.php
+++ src/AppBundle/Command/FixturesCommand.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 /**
  * @codeCoverageIgnore
  */
-class AddUsersFromFixturesCommand extends AddSingleUserCommand
+class FixturesCommand extends AddSingleUserCommand
 {
     protected function configure()
     {
