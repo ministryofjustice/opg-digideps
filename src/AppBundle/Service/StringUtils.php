2314422145cfb265fa25d7900df97cd5465c14e7
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2016-05-19 12:15:03 +0200

    dd-1449-timeout-msg clean code

--- src/AppBundle/Service/StringUtils.php
+++ src/AppBundle/Service/StringUtils.php
@@ -5,38 +5,36 @@ namespace AppBundle\Service;
 class StringUtils
 {
     /**
-     * Transforms 3900 into "1 hour and 5 minutes"
+     * Transforms 3900 into "1 hour and 5 minutes".
      * 
-     * @param integer $seconds
+     * @param int $seconds
      * 
      * @return string
      */
     public static function secondsToHoursMinutes($seconds)
     {
-        $hours = intval(gmdate("H", $seconds));
-        $minutes = intval(gmdate("i", $seconds));
-        
-        $hoursString = $hours . ' ' .  (($hours === 1) ? "hour" : "hours");
-        $minutesString = $minutes . ' ' . (($minutes === 1) ? "minute" : "minutes");
-        
+        $hours = intval(gmdate('H', $seconds));
+        $minutes = intval(gmdate('i', $seconds));
+
+        $hoursString = $hours.' '.(($hours === 1) ? 'hour' : 'hours');
+        $minutesString = $minutes.' '.(($minutes === 1) ? 'minute' : 'minutes');
+
         // less than a minute
         if ($hours === 0 && $minutes === 0) {
             return 'less than a minute';
         }
-        
+
         // X minutes
         if ($hours === 0) {
             return $minutesString;
         }
-        
+
         // X hours 
         if ($minutes === 0) {
             return $hoursString;
         }
-        
+
         // X hours and Y minutes
-        return $hoursString . ' and ' . $minutesString;
-        
+        return $hoursString.' and '.$minutesString;
     }
-    
 }
--- tests/phpunit/Service/StringUtilsTest.php
+++ tests/phpunit/Service/StringUtilsTest.php
@@ -2,7 +2,6 @@
 
 namespace AppBundle\Service;
 
-
 class StringUtilsTest extends \PHPUnit_Framework_TestCase
 {
     public static function secondsToHoursMinutesProvider()
@@ -10,15 +9,15 @@ class StringUtilsTest extends \PHPUnit_Framework_TestCase
         return [
             [3900, '1 hour and 5 minutes'],
             [3600, '1 hour'],
-            [3600*2, '2 hours'],
-            [60*2, '2 minutes'],
+            [3600 * 2, '2 hours'],
+            [60 * 2, '2 minutes'],
             [60, '1 minute'],
-            [60+59, '1 minute'], //ceil
+            [60 + 59, '1 minute'], //ceil
             [0, 'less than a minute'],
             [1, 'less than a minute'],
             [59, 'less than a minute'],
-            [3600 * 2 + 60*6, '2 hours and 6 minutes'],
-            
+            [3600 * 2 + 60 * 6, '2 hours and 6 minutes'],
+
         ];
     }
 
@@ -30,5 +29,4 @@ class StringUtilsTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertEquals($expected, StringUtils::secondsToHoursMinutes($input));
     }
-
 }
