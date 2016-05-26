bdd31f591391028c7f40b457b37a7e950022d417
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2016-05-26 14:34:42 +0200

    dd-377-debts migration to add debts to existing reports, using repos

---
+++ app/DoctrineMigrations/Version077.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * debts
+ */
+class Version077 extends AbstractMigration implements ContainerAwareInterface
+{
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        ini_set('memory_limit','1024M');
+
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $em = $this->container->get('em');
+        $reportRepo = $em->getRepository('AppBundle\Entity\Report');
+        $reports = $reportRepo->findAll();
+
+        foreach($reports as $report) {
+            $reportRepo->addDebtsToReportIfMissing($report);
+            $em->flush();
+        }
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+    }
+}
