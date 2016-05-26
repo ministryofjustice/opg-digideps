f6bb1e63ebd5d3eb6912c6bee064a1cd6e56b799
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2016-05-26 12:41:47 +0200

    dd-377-2-debts controller

--- src/AppBundle/Controller/DebtController.php
+++ src/AppBundle/Controller/DebtController.php
@@ -7,26 +7,40 @@ use AppBundle\Entity\Report;
 use AppBundle\Form as FormDir;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
-/**
- * @Route("/report")
- */
 class DebtController extends AbstractController
 {
     /**
      * List debts
      *
-     * @Route("/{reportId}/debts", name="debts")
+     * @Route("/report/{reportId}/debts", name="debts")
      * @Template("AppBundle:Debt:list.html.twig")
      */
     public function listAction(Request $request, $reportId)
     {
-        $report = $this->getReportIfReportNotSubmitted($reportId, ['transactions', 'basic', 'client', 'asset', 'accounts']);
+        $report = $this->getReport($reportId, ['debts', 'basic', 'client']);
+        if ($report->getSubmitted()) {
+            throw new \RuntimeException('Report already submitted and not editable.');
+        }
+
+        $form = $this->createForm(new FormDir\DebtsType, $report);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+//            $this->get('restClient')->put('report/'.$report->getId(), $form->getData(), [
+//                'deserialise_group' => 'transactionsIn',
+//            ]);
+
+            return $this->redirect($this->generateUrl('debts', ['reportId' => $reportId]));
+        }
 
         return [
             'report' => $report,
+//            'jsonEndpoint' => 'debts',
+            'form' => $form->createView(),
         ];
     }
 }
