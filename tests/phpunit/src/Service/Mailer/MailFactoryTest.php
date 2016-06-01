128f9391c6287bd5d224db87abfe6db873be5f1d
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2016-06-01 15:05:36 +0100

    dd-1506-pdf-name create unit test for mailfactor.createReportEmail

--- tests/phpunit/src/Service/Mailer/MailFactoryTest.php
+++ tests/phpunit/src/Service/Mailer/MailFactoryTest.php
@@ -2,35 +2,93 @@
 
 namespace AppBundle\Service\Mailer;
 
+use MockeryStub as m;
+
 class MailFactoryTest extends \PHPUnit_Framework_TestCase
 {
+
+    /**
+     * @var MailFactory 
+     */
+    private $object;
+
+
+    public function setUp()
+    {
+        $this->translator = m::mock('Symfony\Component\Translation\DataCollectorTranslator');
+        $this->templating = m::mock('Symfony\Bundle\TwigBundle\TwigEngine');
+        $this->translator->shouldReceive('trans')->andReturnUsing(function($input){
+            return $input.' translated';
+        });
+        
+        $this->container = m::mock('Symfony\Component\DependencyInjection\Container');
+        $this->container->shouldReceive('get')->with('translator')->andReturn($this->translator);
+        $this->container->shouldReceive('get')->with('templating')->andReturn($this->templating);
+
+        $roleToArea = ["ROLE_ADMIN" => "admin", "ROLE_AD" => "admin", "ROLE_LAY_DEPUTY" => "frontend"];
+        $this->object = new MailFactory($this->container, $roleToArea);
+    }
+
+
+    public function testcreateReportEmail()
+    {
+        $this->container->shouldReceive('getParameter')->with('email')->andReturn([
+            'base_url'=>['frontend'=>'http://site'],
+            'routes' => ['homepage'=>'/']
+        ]);
+        $this->container->shouldReceive('getParameter')->with('email_report_submit')->andReturn([
+            'from_email' => 'from@email',
+            'to_email' => 'to@email',
+        ]);
+        
+        $this->templating->shouldReceive('render')->with(
+            'AppBundle:Email:report-submission.html.twig', 
+            ['homepageUrl' => 'http://site/']
+        )->andReturn('[TEMPLATE]');
+        
+        $user = m::mock('AppBundle\Entity\User', [
+            'getRole->getRole'=> 'ROLE_LAY_DEPUTY'
+        ]);
+        $client = m::mock('AppBundle\Entity\Client', [
+            'getCaseNumber'=>'1234567t',
+        ]);
+        $email = $this->object->createReportEmail($user, $client, '[REPORT-CONTENT-PDF]');
+        
+        $this->assertEquals('[TEMPLATE]', $email->getBodyHtml());
+        $this->assertEquals('to@email', $email->getToEmail());
+        $this->assertEquals('report-1234567t.pdf', $email->getAttachments()[0]->getFilename());
+        $this->assertEquals('[REPORT-CONTENT-PDF]', $email->getAttachments()[0]->getContent());
+        $this->assertEquals('application/pdf', $email->getAttachments()[0]->getContentType());
+    }
+
+
     public function testcreateActivationEmail()
     {
         $this->markTestIncomplete(__METHOD__);
     }
+
 
     public function testcreateResetPasswordEmail()
     {
         $this->markTestIncomplete(__METHOD__);
     }
 
+
     public function testcreateChangePasswordEmail()
     {
         $this->markTestIncomplete(__METHOD__);
     }
 
-    public function testcreateReportEmail()
-    {
-        $this->markTestIncomplete(__METHOD__);
-    }
 
     public function testcreateFeedbackEmail()
     {
         $this->markTestIncomplete(__METHOD__);
     }
 
+
     public function testcreateReportSubmissionConfirmationEmail()
     {
         $this->markTestIncomplete(__METHOD__);
     }
+
 }
