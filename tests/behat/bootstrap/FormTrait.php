80936624686650d5a1e3e9366be5cee856dd7c5f
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2016-06-01 11:09:27 +0100

    dd-377-debts fix behat form trait

--- tests/behat/bootstrap/FormTrait.php
+++ tests/behat/bootstrap/FormTrait.php
@@ -12,7 +12,8 @@ trait FormTrait
     public function theFormShouldBeInvalid()
     {
         //$this->assertResponseStatus(200);
-        if (!$this->getSession()->getPage()->has('css', '.form-group.error')) {
+        if (!$this->getSession()->getPage()->has('css', '.form-group.error') 
+            && !$this->getSession()->getPage()->has('css', '#error-summary-heading') ) {
             throw new \RuntimeException('No errors found');
         }
     }
