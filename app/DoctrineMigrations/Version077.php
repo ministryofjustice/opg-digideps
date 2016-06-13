2c5726ce6a7e0386f36b69fd29211a87f439bcb1
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2016-06-13 14:25:31 +0100

    dd-377-debts rename migrations

--- app/DoctrineMigrations/Version077.php
+++ app/DoctrineMigrations/Version077.php
@@ -4,42 +4,26 @@ namespace Application\Migrations;
 
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * debts fix existing reports
+ * debts
  */
-class Version077 extends AbstractMigration implements ContainerAwareInterface
+class Version077 extends AbstractMigration
 {
-    private $container;
-
-    public function setContainer(ContainerInterface $container = null)
-    {
-        $this->container = $container;
-    }
-
     /**
      * @param Schema $schema
      */
     public function up(Schema $schema)
     {
-        ini_set('memory_limit','1024M');
-
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $em = $this->container->get('em');
-        $reportRepo = $em->getRepository('AppBundle\Entity\Report');
-        $reports = $reportRepo->findAll();
+        $this->addSql('CREATE TABLE debt (id SERIAL NOT NULL, report_id INT DEFAULT NULL, debt_type_id VARCHAR(255) NOT NULL, amount NUMERIC(14, 2) DEFAULT NULL, has_more_details BOOLEAN NOT NULL, more_details TEXT DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_DBBF0A834BD2A4C0 ON debt (report_id)');
+        $this->addSql('ALTER TABLE debt ADD CONSTRAINT FK_DBBF0A834BD2A4C0 FOREIGN KEY (report_id) REFERENCES report (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
 
-        foreach($reports as $report) {
-            if ($reportRepo->addDebtsToReportIfMissing($report)) {
-                $em->flush();
-                echo "Added debts to report {$report->getId()}\n";
-            }
+        $this->addSql('ALTER TABLE report ADD has_debts VARCHAR(5) DEFAULT NULL');
 
-        }
     }
 
     /**
@@ -49,5 +33,9 @@ class Version077 extends AbstractMigration implements ContainerAwareInterface
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('DROP TABLE debt');
+
+        $this->addSql('ALTER TABLE report DROP has_debts');
     }
 }
