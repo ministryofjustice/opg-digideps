9e6f2c9652c70a0c75935fba8669527603a5b019
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2016-06-22 18:13:37 +0100

    ddpb-40 model

---
+++ src/AppBundle/Entity/Odr/Safeguarding.php
@@ -0,0 +1,295 @@
+<?php
+
+namespace AppBundle\Entity\Odr;
+
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as JMS;
+
+/**
+ * Reports.
+ *
+ * @JMS\XmlRoot("odr_safeguarding")
+ * @JMS\ExclusionPolicy("NONE")
+ * @ORM\Table(name="safeguarding")
+ * @ORM\Entity
+ */
+class Safeguarding
+{
+    /**
+     * @var int
+     *
+     * @JMS\Groups({"transactions","basic"})
+     * @JMS\Type("integer")
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @ORM\SequenceGenerator(sequenceName="safeguarding_id_seq", allocationSize=1, initialValue=1)
+     */
+    private $id;
+
+    /**
+     * @ORM\OneToOne(targetEntity="AppBundle\Entity\Report", inversedBy="safeguarding")
+     * @ORM\JoinColumn(name="report_id", referencedColumnName="id")
+     */
+    private $report;
+
+    /**
+     * @var string
+     *
+     * @JMS\Type("string")
+     * @JMS\Groups({"transactions","basic"})
+     * @ORM\Column(name="do_you_live_with_client", type="string", length=4, nullable=true)
+     */
+    private $doYouLiveWithClient;
+
+    /**
+     * New textarea replacing deprecated values
+     * how_often_do_you_visit
+     * how_often_do_you_phone_or_video_call
+     * how_often_do_you_write_email_or_letter
+     * how_often_does_client_see_other_people
+     * anything_else_to_tell.
+     * 
+     * @var string
+     *
+     * @JMS\Type("string")
+     * @JMS\Groups({"transactions","basic"})
+     * @ORM\Column(name="how_often_contact_client", type="text", nullable=true)
+     */
+    private $howOftenDoYouContactClient;
+
+    /**
+     * @var string
+     *
+     * @JMS\Type("string")
+     * @JMS\Groups({"transactions","basic"})
+     * @ORM\Column( name="does_client_receive_paid_care", type="text", nullable=true)
+     */
+    private $doesClientReceivePaidCare;
+
+    /**
+     * @var string
+     *
+     * @JMS\Type("string")
+     * @JMS\Groups({"transactions","basic"})
+     * @ORM\Column(name="how_is_care_funded", length=255, type="string", nullable=true)
+     */
+    private $howIsCareFunded;
+
+    /**
+     * @var type
+     *
+     * @JMS\Type("string")
+     * @JMS\Groups({"transactions","basic"})
+     * @ORM\Column( name="who_is_doing_the_caring", type="text", nullable=true)
+     */
+    private $whoIsDoingTheCaring;
+
+    /**
+     * @var type
+     *
+     * @JMS\Type("string")
+     * @JMS\Groups({"transactions","basic"})
+     * @ORM\Column( name="does_client_have_a_care_plan", type="string", length=4, nullable=true)
+     */
+    private $doesClientHaveACarePlan;
+
+    /**
+     * @var date
+     *
+     * @JMS\Type("DateTime<'Y-m-d'>")
+     * @JMS\Groups({"transactions","basic"})
+     * @ORM\Column(name="when_was_care_plan_last_reviewed", type="date", nullable=true, options={ "default": null })
+     */
+    private $whenWasCarePlanLastReviewed;
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set report.
+     *
+     * @param Report $report
+     *
+     * @return Contact
+     */
+    public function setReport(Report $report = null)
+    {
+        $this->report = $report;
+
+        return $this;
+    }
+
+    /**
+     * Get report.
+     *
+     * @return Report
+     */
+    public function getReport()
+    {
+        return $this->report;
+    }
+
+    /**
+     * Set doYouLiveWithClient.
+     *
+     * @param string $doYouLiveWithClient
+     *
+     * @return Report
+     */
+    public function setDoYouLiveWithClient($doYouLiveWithClient)
+    {
+        $this->doYouLiveWithClient = $doYouLiveWithClient;
+
+        return $this;
+    }
+
+    /**
+     * Get doYouLiveWithClient.
+     *
+     * @return string
+     */
+    public function getDoYouLiveWithClient()
+    {
+        return $this->doYouLiveWithClient;
+    }
+
+    /**
+     * Set doesClientReceivePaidCare.
+     *
+     * @param string $doesClientReceivePaidCare
+     *
+     * @return Report
+     */
+    public function setDoesClientReceivePaidCare($doesClientReceivePaidCare)
+    {
+        $this->doesClientReceivePaidCare = $doesClientReceivePaidCare;
+
+        return $this;
+    }
+
+    /**
+     * Get doesClientReceivePaidCare.
+     *
+     * @return string
+     */
+    public function getDoesClientReceivePaidCare()
+    {
+        return $this->doesClientReceivePaidCare;
+    }
+
+    /**
+     * Set whoIsDoingTheCaring.
+     *
+     * @param string $whoIsDoingTheCaring
+     *
+     * @return Report
+     */
+    public function setWhoIsDoingTheCaring($whoIsDoingTheCaring)
+    {
+        $this->whoIsDoingTheCaring = $whoIsDoingTheCaring;
+
+        return $this;
+    }
+
+    /**
+     * Get whoIsDoingTheCaring.
+     *
+     * @return string
+     */
+    public function getWhoIsDoingTheCaring()
+    {
+        return $this->whoIsDoingTheCaring;
+    }
+
+    /**
+     * Set doesClientHaveACarePlan.
+     *
+     * @param string $doesClientHaveACarePlan
+     *
+     * @return Report
+     */
+    public function setDoesClientHaveACarePlan($doesClientHaveACarePlan)
+    {
+        $this->doesClientHaveACarePlan = $doesClientHaveACarePlan;
+
+        return $this;
+    }
+
+    /**
+     * Get doesClientHaveACarePlan.
+     *
+     * @return string
+     */
+    public function getDoesClientHaveACarePlan()
+    {
+        return $this->doesClientHaveACarePlan;
+    }
+
+    /**
+     * Set whenWasCarePlanLastReviewed.
+     *
+     * @param \DateTime $whenWasCarePlanLastReviewed
+     *
+     * @return Report
+     */
+    public function setWhenWasCarePlanLastReviewed($whenWasCarePlanLastReviewed)
+    {
+        $this->whenWasCarePlanLastReviewed = $whenWasCarePlanLastReviewed;
+
+        return $this;
+    }
+
+    /**
+     * Get whenWasCarePlanLastReviewed.
+     *
+     * @return \DateTime
+     */
+    public function getWhenWasCarePlanLastReviewed()
+    {
+        return $this->whenWasCarePlanLastReviewed;
+    }
+
+    /**
+     * Set howIsCareFunded.
+     *
+     * @param string $howIsCareFunded
+     *
+     * @return Report
+     */
+    public function setHowIsCareFunded($howIsCareFunded)
+    {
+        $this->howIsCareFunded = $howIsCareFunded;
+
+        return $this;
+    }
+
+    /**
+     * Get howIsCareFunded.
+     *
+     * @return string
+     */
+    public function getHowIsCareFunded()
+    {
+        return $this->howIsCareFunded;
+    }
+
+    public function getHowOftenDoYouContactClient()
+    {
+        return $this->howOftenDoYouContactClient;
+    }
+
+    public function setHowOftenDoYouContactClient($howOftenDoYouContactClient)
+    {
+        $this->howOftenDoYouContactClient = $howOftenDoYouContactClient;
+
+        return $this;
+    }
+}
