79c44526eae1dc7d2960ec049536df3e8bb84905
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2016-07-13 16:05:11 +0100

    odr rename migration

--- app/DoctrineMigrations/Version080.php
+++ app/DoctrineMigrations/Version080.php
@@ -18,8 +18,18 @@ class Version080 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('ALTER TABLE odr_visits_care ADD plan_move_residence VARCHAR(4) DEFAULT NULL');
-        $this->addSql('ALTER TABLE odr_visits_care ADD plan_move_residence_details TEXT DEFAULT NULL');
+        // ODR table
+        $this->addSql('CREATE TABLE odr (id SERIAL NOT NULL, client_id INT DEFAULT NULL, submitted BOOLEAN DEFAULT NULL, submit_date TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_350EBBC19EB6921 ON odr (client_id)');
+
+        //visits care
+        $this->addSql('CREATE TABLE odr_visits_care (id SERIAL NOT NULL, odr_id INT DEFAULT NULL, do_you_live_with_client VARCHAR(4) DEFAULT NULL, how_often_contact_client TEXT DEFAULT NULL, does_client_receive_paid_care TEXT DEFAULT NULL, how_is_care_funded VARCHAR(255) DEFAULT NULL, who_is_doing_the_caring TEXT DEFAULT NULL, does_client_have_a_care_plan VARCHAR(4) DEFAULT NULL, when_was_care_plan_last_reviewed DATE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_9239DE877CE4B994 ON odr_visits_care (odr_id)');
+
+
+        // FKs
+        $this->addSql('ALTER TABLE odr ADD CONSTRAINT FK_350EBBC19EB6921 FOREIGN KEY (client_id) REFERENCES client (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE odr_visits_care ADD CONSTRAINT FK_9239DE877CE4B994 FOREIGN KEY (odr_id) REFERENCES odr (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
     /**
@@ -31,7 +41,8 @@ class Version080 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE odr_visits_care DROP plan_move_residence');
-        $this->addSql('ALTER TABLE odr_visits_care DROP plan_move_residence_details');
+        $this->addSql('ALTER TABLE odr_visits_care DROP CONSTRAINT FK_9239DE877CE4B994');
+        $this->addSql('DROP TABLE odr');
+        $this->addSql('DROP TABLE odr_visits_care');
     }
 }
--- app/DoctrineMigrations/Version082.php
+++ app/DoctrineMigrations/Version082.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version081 extends AbstractMigration
+class Version082 extends AbstractMigration
 {
     /**
      * @param Schema $schema
@@ -18,8 +18,9 @@ class Version081 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('ALTER TABLE odr_visits_care ADD plan_move_residence VARCHAR(4) DEFAULT NULL');
-        $this->addSql('ALTER TABLE odr_visits_care ADD plan_move_residence_details TEXT DEFAULT NULL');
+        $this->addSql('CREATE TABLE odr_account (id SERIAL NOT NULL, odr_id INT DEFAULT NULL, bank_name VARCHAR(500) DEFAULT NULL, account_type VARCHAR(125) DEFAULT NULL, sort_code VARCHAR(6) DEFAULT NULL, account_number VARCHAR(4) DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, balance_on_cod NUMERIC(14, 2) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_C2AEF4FB7CE4B994 ON odr_account (odr_id)');
+        $this->addSql('ALTER TABLE odr_account ADD CONSTRAINT FK_C2AEF4FB7CE4B994 FOREIGN KEY (odr_id) REFERENCES odr (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
     /**
@@ -31,7 +32,6 @@ class Version081 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE odr_visits_care DROP plan_move_residence');
-        $this->addSql('ALTER TABLE odr_visits_care DROP plan_move_residence_details');
+        $this->addSql('DROP TABLE odr_account');
     }
 }
