a85d1c3e3d6e3b608cbd5fef589c9f668ba26ae2
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2016-08-04 12:55:38 +0100

    odr-income form and view first draft

--- src/AppBundle/Controller/Odr/IncomeBenefitsController.php
+++ src/AppBundle/Controller/Odr/IncomeBenefitsController.php
@@ -30,16 +30,45 @@ class IncomeBenefitsController extends AbstractController
      *
      * @return array
      */
-    public function indexAction($odrId)
+    public function indexAction(Request $request, $odrId)
     {
         $odr = $this->getOdr($odrId, self::$odrJmsGroups);
         if ($odr->getSubmitted()) {
             throw new \RuntimeException('Odr already submitted and not editable.');
         }
 
+        // temp to test form
+        $odr->setStateBenefits([
+            new EntityDir\Odr\IncomeBenefit('contributionsBasedAllowance', true),
+            new EntityDir\Odr\IncomeBenefit('housingBenefit', false),
+            new EntityDir\Odr\IncomeBenefit('otherBenefits', true, true, 'santa claus money'),
+        ])
+            ->setReceiveStatePension('no')
+            ->setReceiveOtherIncome('yes')
+            ->setReceiveOtherIncomeDetails('brother sends money')
+            ->setExpectCompensationDamages('yes')
+            ->setOneOff([
+                new EntityDir\Odr\IncomeBenefit('bequestOrInheritance', true),
+                new EntityDir\Odr\IncomeBenefit('cashGiftReceived', false),
+                new EntityDir\Odr\IncomeBenefit('refunds', true),
+            ]);
+
+        $form = $this->createForm(new FormDir\Odr\IncomeBenefitsType(), $odr);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            echo "<pre>";\Doctrine\Common\Util\Debug::dump($odr->getStateBenefits(), 4);die;
+            $this->getRestClient()->put('odr/' . $odrId, $form->getData(), [
+                'deserialise_group' => 'odr-state-',
+            ]);
+
+            return $this->redirect($this->generateUrl('odr-income-benefits', ['odrId' => $odrId]));
+        }
+
         return [
             'odr' => $odr,
             'subsection' => 'incomeBenefits',
+            'form' => $form->createView(),
         ];
     }
 }
