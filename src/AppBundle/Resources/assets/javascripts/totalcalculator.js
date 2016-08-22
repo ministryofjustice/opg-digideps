a82f2194483f285a8a80808e31421209e04776cf
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2016-08-22 17:01:19 +0100

    DDPB-230 refactor calculator

--- src/AppBundle/Resources/assets/javascripts/totalcalculator.js
+++ src/AppBundle/Resources/assets/javascripts/totalcalculator.js
@@ -6,15 +6,14 @@ var opg = opg || {};
 
 (function ($, opg) {
     
-    opg.TotalCalculator = function (wrapper, elementsSelector, totalElementSelector) {
-
+    opg.TotalCalculator = function (options) {
         var _this = this;
 
-        _this.wrapper = $(wrapper);
-        _this.elementsSelector = elementsSelector;
-        _this.totalElementSelector = totalElementSelector;
+        _this.wrapper = $(options.wrapperSelector);
+        _this.amountSelector = options.amountSelector;
+        _this.totalElement = $(options.totalSelector);
 
-        _this.wrapper.on('change keyup', this.elementsSelector, function(){
+        _this.wrapper.on('change keyup', _this.amountSelector, function(){
             _this.updateTotal();
         });
 
@@ -23,13 +22,13 @@ var opg = opg || {};
 
     opg.TotalCalculator.prototype.updateTotal = function() {
         var total = 0;
-        this.wrapper.find(this.elementsSelector).each(function(i, e) {
+        this.wrapper.find(this.amountSelector).each(function(i, e) {
             var eVal = parseFloat($(e).val().replace(/,/g , ""));
             if (!isNaN(eVal) && eVal > 0.01) {
                 total += eVal;
             }
         });
-        this.wrapper.find(this.totalElementSelector).html(total.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,'));
+        this.totalElement.html(total.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,'));
     };
 
 })(jQuery, opg);
