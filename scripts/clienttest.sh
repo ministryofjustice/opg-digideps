52de34a6b879397ca66a78530ea090030e606e18
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2016-09-19 13:32:16 +0100

    odr-360 simplify clienttest.sh script and enable odr suite inside sh script

--- scripts/clienttest.sh
+++ scripts/clienttest.sh
@@ -14,6 +14,7 @@ export PGPASSWORD=api
 export PGDATABASE=api
 export PGUSER=api
 rm -rf app/cache/*
+# deprecated
 suitename=${1:-deputy}
 
 /sbin/setuser app php vendor/phpunit/phpunit/phpunit -c tests/phpunit/
@@ -21,12 +22,15 @@ suitename=${1:-deputy}
 # tests with coverage (install php5-xdebug if needed)
 #/sbin/setuser app php -d zend_extension=xdebug.so bin/phpunit -c tests/phpunit/phpunit.xml --coverage-html=web/coverage-html
 
+# deprecated
 if [ -f tests/behat/behat.yml ]; then
     behatConfigFile=tests/behat/behat.yml
 else
     behatConfigFile=tests/behat/behat.yml.dist
 fi
+# end deprecated
 
 export BEHAT_PARAMS="{\"extensions\" : {\"Behat\\\\MinkExtension\\\\ServiceContainer\\\\MinkExtension\" : {\"base_url\" : \"${FRONTEND_NONADMIN_HOST}\",\"selenium2\" : { \"wd_host\" : \"$WD_HOST\" }, \"browser_stack\" : { \"username\": \"$BROWSERSTACK_USER\", \"access_key\": \"$BROWSERSTACK_KEY\"}}}}" 
 echo BEHAT_PARAMS
-/sbin/setuser app bin/behat --config=$behatConfigFile --suite=${suitename} --profile=${PROFILE} --stop-on-failure
+/sbin/setuser app bin/behat --config=tests/behat/behat.yml.dist --suite=deputy --profile=${PROFILE} --stop-on-failure
+/sbin/setuser app bin/behat --config=tests/behat/behat.yml.dist --suite=deputyodr --profile=${PROFILE} --stop-on-failure
