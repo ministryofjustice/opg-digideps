e9894de107d03bea40b1b564e09505254772eecd
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2016-09-29 11:45:39 +0100

    DDPB-400 move register action into userController

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use AppBundle\Entity as EntityDir;
 use AppBundle\Form as FormDir;
 use AppBundle\Model\Email;
+use AppBundle\Model\SelfRegisterData;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
@@ -325,5 +326,73 @@ class UserController extends AbstractController
     public function passwordSentAction()
     {
         return [];
+    }
+
+    /**
+     * @Route("/register", name="register")
+     * @Template()
+     */
+    public function registerAction(Request $request)
+    {
+        $selfRegisterData = new SelfRegisterData();
+        $form = $this->createForm(new FormDir\SelfRegisterDataType(), $selfRegisterData);
+        $translator = $this->get('translator');
+        $vars = [];
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $data = $form->getData();
+
+            try {
+                $user = $this->getRestClient()->registerUser($data);
+                $activationEmail = $this->getMailFactory()->createActivationEmail($user);
+                $this->getMailSender()->send($activationEmail);
+
+                $bodyText = $translator->trans('thankyou.body', [], 'register');
+                $email = $data->getEmail();
+                $bodyText = str_replace('{{ email }}', $email, $bodyText);
+
+                $signInText = $translator->trans('signin', [], 'register');
+                $signIn = '<a href="'.$this->generateUrl('login').'">'.$signInText.'</a>';
+                $bodyText = str_replace('{{ sign_in }}', $signIn, $bodyText);
+
+                return $this->render('AppBundle:User:registration-thankyou.html.twig', [
+                    'bodyText' => $bodyText,
+                ]);
+            } catch (\Exception $e) {
+                switch ((int) $e->getCode()) {
+                    case 422:
+                        $form->get('email')->get('first')->addError(new FormError($translator->trans('email.first.existingError', [], 'register')));
+                        break;
+
+                    case 421:
+                        $form->addError(new FormError($translator->trans('formErrors.matching', [], 'register')));
+                        break;
+
+                    case 424:
+                        $form->get('postcode')->addError(new FormError($translator->trans('postcode.matchingError', [], 'register')));
+                        break;
+
+                    case 425:
+                        $form->addError(new FormError($translator->trans('formErrors.caseNumberAlreadyUsed', [], 'register')));
+                        break;
+
+                    default:
+                        $form->addError(new FormError($translator->trans('formErrors.generic', [], 'register')));
+                }
+
+                $this->get('logger')->error(__METHOD__.': '.$e->getMessage().', code: '.$e->getCode());
+            }
+        }
+
+        // send different URL to google analytics
+        if (count($form->getErrors())) {
+            $vars['gaCustomUrl'] = '/register/form-errors';
+        }
+
+        return $vars + [
+            'form' => $form->createView(),
+        ];
     }
 }
