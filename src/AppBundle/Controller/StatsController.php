ad4c3a743e6db99112cf2559f52201d02bd06699
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2016-11-07 13:34:10 +0000

    DDPB-563 moved stats into service

--- src/AppBundle/Controller/StatsController.php
+++ src/AppBundle/Controller/StatsController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Service\StatsService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,59 +22,8 @@ class StatsController extends RestController
         $ret = [];
         $this->denyAccessUnlessGranted(EntityDir\Role::ADMIN);
 
-        //$deputy = $this->getRepository('Role')->findBy(['role'=>'ROLE_LAY_DEPUTY']);
-        // pre-join data to reduce number of queries
-        // $users = $this->getRepository('User')->findBy(['role'=>$deputy], ['id' => 'DESC']);
-        $qb = $this->getEntityManager()->createQuery(
-            "SELECT u, c, role FROM AppBundle\Entity\User u
-                LEFT JOIN u.role role
-                LEFT JOIN u.clients c
-                WHERE role.role = 'ROLE_LAY_DEPUTY' ORDER BY u.id DESC" // 87M
-        );
-
-        if ($maxResults = $request->query->get('limit')) {
-            $qb->setMaxResults($maxResults);
-        }
-
-        $users = $qb->getResult();
-
-        // alternative without join and lazy-loading
-        // $deputy = $this->getRepository('Role')->findBy(['role'=>'ROLE_LAY_DEPUTY']);
-        // $users = $this->getRepository('User')->findBy(['role'=>$deputy], ['id' => 'DESC']);
-
-        foreach ($users as $user) {
-            /* @var $user EntityDir\User */
-            $row = [
-                'id' => $user->getId(),
-                'email' => $user->getEmail(),
-                'name' => $user->getFirstname(),
-                'lastname' => $user->getLastname(),
-                'registration_date' => $user->getRegistrationDate() ? $user->getRegistrationDate()->format('Y-m-d') : '-',
-                'last_logged_in' => $user->getLastLoggedIn() ? $user->getLastLoggedIn()->format('Y-m-d H:i:s') : '-',
-                'client_name' => 'n.a.',
-                'client_lastname' => 'n.a.',
-                'client_casenumber' => 'n.a.',
-                'client_court_order_date' => 'n.a.',
-                'total_reports' => 0,
-                'active_reports' => 0,
-            ];
-
-            foreach ($user->getClients() as $client) {
-                $row['client_name'] = $client->getFirstname();
-                $row['client_lastname'] = $client->getLastname();
-                $row['client_casenumber'] = $client->getCaseNumber();
-                $row['client_court_order_date'] = $client->getCourtDate() ? $client->getCourtDate()->format('Y-m-d') : '-';
-                foreach ($client->getReports() as $report) {
-                    ++$row['total_reports'];
-                    if ($report->getSubmitted()) {
-                        continue;
-                    }
-                    ++$row['active_reports'];
-                }
-            }
-
-            $ret[] = $row;
-        }
+        $stats = $this->get('statsService'); /* @var $stats StatsService */
+        $ret = $stats->getRecords($request->query->get('limit'));
 
         $this->get('kernel.listener.responseConverter')->addContextModifier(function ($context) {
             $context->setSerializeNull(true);
