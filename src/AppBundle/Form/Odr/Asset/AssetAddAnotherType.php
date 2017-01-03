1083154bca33b2542ef198b0f0b07d7ac7c7b965
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2017-01-03 19:43:05 +0100

    otpp asset add another use abstraction on both reports

--- src/AppBundle/Form/Odr/Asset/AssetAddAnotherType.php
+++ src/AppBundle/Form/Odr/Asset/AssetAddAnotherType.php
@@ -2,35 +2,14 @@
 
 namespace AppBundle\Form\Odr\Asset;
 
-use Symfony\Component\Form\AbstractType;
+use AppBundle\Form\Odr\AbstractAddAnotherType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
-class AssetAddAnotherType extends AbstractType
+class AssetAddAnotherType extends AbstractAddAnotherType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('addAnother', 'choice', array(
-                'choices' => ['yes' => 'Yes', 'no' => 'No'],
-                'expanded' => true,
-                'mapped' => false,
-                'constraints' => [new NotBlank(['message' => 'odr.asset.addAnother.notBlank'])],
-            ))
-            ->add('save', 'submit', ['label' => 'save.label']);
-    }
-
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
-        $resolver->setDefaults([
-            'translation_domain' => 'odr-assets',
-        ]);
-    }
-
-    public function getName()
-    {
-        return 'odr_assets_add_another';
-    }
+    protected $missingMessage = 'odr.asset.addAnother.notBlank';
+    protected $translationDomain = 'odr-assets';
 }
--- src/AppBundle/Form/Report/Asset/AssetAddAnotherType.php
+++ src/AppBundle/Form/Report/Asset/AssetAddAnotherType.php
@@ -2,35 +2,15 @@
 
 namespace AppBundle\Form\Report\Asset;
 
+use AppBundle\Form\Report\AbstractAddAnotherType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
-class AssetAddAnotherType extends AbstractType
+class AssetAddAnotherType extends AbstractAddAnotherType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('addAnother', 'choice', array(
-                'choices' => ['yes' => 'Yes', 'no' => 'No'],
-                'expanded' => true,
-                'mapped' => false,
-                'constraints' => [new NotBlank(['message' => 'asset.addAnother.notBlank'])],
-            ))
-            ->add('save', 'submit', ['label' => 'save.label']);
-    }
-
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
-        $resolver->setDefaults([
-            'translation_domain' => 'report-assets',
-        ]);
-    }
-
-    public function getName()
-    {
-        return 'assets_add_another';
-    }
+    protected $missingMessage = 'asset.addAnother.notBlank';
+    protected $translationDomain = 'report-assets';
 }
