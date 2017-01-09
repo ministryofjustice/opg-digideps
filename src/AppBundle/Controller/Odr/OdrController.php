7942eb137a60794348ab6acfef87363b025aef27
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2017-01-09 17:55:38 +0100

    DDPB-767 NDR debts deleting when has_debts = no

--- src/AppBundle/Controller/Odr/OdrController.php
+++ src/AppBundle/Controller/Odr/OdrController.php
@@ -2,11 +2,11 @@
 
 namespace AppBundle\Controller\Odr;
 
+use AppBundle\Controller\RestController;
+use AppBundle\Entity as EntityDir;
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Symfony\Component\HttpFoundation\Request;
-use AppBundle\Entity as EntityDir;
-use AppBundle\Controller\RestController;
 
 class OdrController extends RestController
 {
@@ -91,15 +91,17 @@ class OdrController extends RestController
                 $this->getEntityManager()->flush($debt);
             }
             // set debts as per "debts" key
-            foreach ($data['debts'] as $row) {
-                $debt = $odr->getDebtByTypeId($row['debt_type_id']);
-                if (!$debt instanceof EntityDir\Odr\Debt) {
-                    continue; //not clear when that might happen. kept similar to transaction below
+            if ($data['has_debts'] == 'yes') {
+                foreach ($data['debts'] as $row) {
+                    $debt = $odr->getDebtByTypeId($row['debt_type_id']);
+                    if (!$debt instanceof EntityDir\Odr\Debt) {
+                        continue; //not clear when that might happen. kept similar to transaction below
+                    }
+                    $debt->setAmount($row['amount']);
+                    $debt->setMoreDetails($debt->getHasMoreDetails() ? $row['more_details'] : null);
+                    $this->getEntityManager()->flush($debt);
+                    $this->setJmsSerialiserGroups(['debts']); //returns saved data (AJAX operations)
                 }
-                $debt->setAmount($row['amount']);
-                $debt->setMoreDetails($debt->getHasMoreDetails() ? $row['more_details'] : null);
-                $this->getEntityManager()->flush($debt);
-                $this->setJmsSerialiserGroups(['debts']); //returns saved data (AJAX operations)
             }
         }
 
@@ -109,7 +111,7 @@ class OdrController extends RestController
                 if ($e instanceof EntityDir\Odr\IncomeBenefitStateBenefit) {
                     $e
                         ->setPresent($row['present'])
-                        ->setMoreDetails($row['present'] ? $row['more_details']: null);
+                        ->setMoreDetails($row['present'] ? $row['more_details'] : null);
                     $this->getEntityManager()->flush($e);
                 }
             }
@@ -228,12 +230,12 @@ class OdrController extends RestController
             $em->remove($odr->getVisitsCare());
         }
 
-        foreach ($odr->getExpenses() as $e){
+        foreach ($odr->getExpenses() as $e) {
             $em->remove($e);
         }
         $odr->setPaidForAnything(null);
 
-        foreach($odr->getStateBenefits() as $e) {
+        foreach ($odr->getStateBenefits() as $e) {
             $e->setPresent(false);
             $e->setMoreDetails(null);
         }
@@ -242,21 +244,21 @@ class OdrController extends RestController
         $odr->setReceiveOtherIncomeDetails(null);
         $odr->setExpectCompensationDamages(null);
         $odr->setExpectCompensationDamagesDetails(null);
-        foreach($odr->getOneOff() as $e) {
+        foreach ($odr->getOneOff() as $e) {
             $e->setPresent(false);
             $e->setMoreDetails(null);
         }
 
-        foreach ($odr->getBankAccounts() as $e){
+        foreach ($odr->getBankAccounts() as $e) {
             $em->remove($e);
         }
 
-        foreach ($odr->getAssets() as $e){
+        foreach ($odr->getAssets() as $e) {
             $em->remove($e);
         }
         $odr->setNoAssetToAdd(null);
 
-        foreach ($odr->getDebts() as $e){
+        foreach ($odr->getDebts() as $e) {
             $e->setAmount(null);
         }
         $odr->setHasDebts(null);
