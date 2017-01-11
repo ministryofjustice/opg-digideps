f71529c7d3787c0fcc628352c1b56632dd66a45e
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2017-01-11 18:45:52 +0100

    DDPB-769 fix migration into a new migration (fixes staging)

---
+++ app/DoctrineMigrations/Version099.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * convert money transaction: fix
+ */
+class Version099 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        ini_set('memory_limit', '1024M');
+
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $sql = 'SELECT 
+        t.id, 
+        t.report_id as report_id, 
+        t.more_details as description, t.amounts as amounts,
+        tt.category as category,
+        tt.id as transaction_type_id
+        FROM transaction t 
+        LEFT JOIN transaction_type tt ON t.transaction_type_id = tt.id
+        WHERE t.amounts is not null
+        ';
+        $oldTrans = $this->connection->fetchAll($sql);
+        echo count($oldTrans) . " transactions to migrate: ";
+        $this->connection->beginTransaction();
+
+        $i = 0;
+        foreach ($oldTrans as $trans) {
+            $i++;
+            $newRecords = $this->convertOldToNew($trans);
+            //print_r([$trans, $newRecords]);
+            foreach ($newRecords as $nr) {
+                $this->connection->insert('money_transaction', $nr);
+                if (($i % 1000) == 0) {
+                    echo "$i ";
+                    $this->connection->commit();
+                    $this->connection->beginTransaction();
+                }
+            }
+        }
+        $this->connection->commit();
+    }
+
+    private function convertOldToNew($old)
+    {
+        $amounts = explode(',', $old['amounts']);
+        $ret = [];
+        foreach ($amounts as $amount) {
+            $ret[] = [
+                'report_id'   => $old['report_id'],
+                'category'    => $old['transaction_type_id'],
+                'amount'      => $amount,
+                'description' => trim($old['description'], "\n\t. "),
+            ];
+        }
+
+        return $ret;
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('delete from money_transaction');
+    }
+}
