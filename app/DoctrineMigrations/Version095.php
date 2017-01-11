cc7fc22d7296e6ee9177f210fc2bc312d7beb8dc
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2017-01-11 17:08:26 +0100

    add code comment

--- app/DoctrineMigrations/Version095.php
+++ app/DoctrineMigrations/Version095.php
@@ -6,7 +6,7 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
- * Auto-generated Migration: Please modify to your needs!
+ * convert money transaction
  */
 class Version095 extends AbstractMigration
 {
