a5c485935072a447ba002ea233b2fc7fa628e452
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2017-01-24 17:06:07 +0100

    DDPB-746 expense endpoint

---
+++ src/AppBundle/Entity/Report/Expense.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace AppBundle\Entity\Report;
+
+use JMS\Serializer\Annotation as JMS;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="expense")
+ */
+class Expense
+{
+    /**
+     * @var int
+     *
+     * @JMS\Groups({"expenses"})
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @ORM\SequenceGenerator(sequenceName="expense_id_seq", allocationSize=1, initialValue=1)
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @JMS\Type("string")
+     * @JMS\Groups({"expenses"})
+     * @ORM\Column(name="explanation", type="text", nullable=false)
+     */
+    private $explanation;
+
+    /**
+     * @var float
+     *
+     * @JMS\Type("string")
+     * @JMS\Groups({"expenses"})
+     * @ORM\Column(name="amount", type="decimal", precision=14, scale=2, nullable=true)
+     *
+     * @var string
+     */
+    private $amount;
+
+    /**
+     * @var Report
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Report\Report", inversedBy="expenses")
+     * @ORM\JoinColumn(name="report_id", referencedColumnName="id")
+     */
+    private $report;
+
+    /**
+     * Expense constructor.
+     *
+     * @param Report    $report
+     * @param string $explanation
+     * @param $amount
+     */
+    public function __construct(Report $report)
+    {
+        $this->report = $report;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getExplanation()
+    {
+        return $this->explanation;
+    }
+
+    /**
+     * @param mixed $explanation
+     *
+     * @return Expense
+     */
+    public function setExplanation($explanation)
+    {
+        $this->explanation = $explanation;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    /**
+     * @param string $amount
+     *
+     * @return Expense
+     */
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    /**
+     * @return Report
+     */
+    public function getReport()
+    {
+        return $this->report;
+    }
+
+    /**
+     * @param Report $report
+     */
+    public function setReport($report)
+    {
+        $this->report = $report;
+    }
+
+}
---
+++ src/AppBundle/Entity/Traits/ExpensesTrait.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace AppBundle\Entity\Traits;
+
+use AppBundle\Entity\Report\Expense;
+use AppBundle\Entity\Report\Report;
+use Doctrine\Common\Collections\ArrayCollection;
+use JMS\Serializer\Annotation as JMS;
+use Doctrine\ORM\Mapping as ORM;
+
+trait ExpensesTrait
+{
+    /**
+     * @var string
+     *
+     * @JMS\Type("string")
+     * @JMS\Groups({"expenses"})
+     * @ORM\Column(name="paid_for_anything", type="string", length=3, nullable=true)
+     */
+    private $paidForAnything;
+
+    /**
+     * @JMS\Type("array<AppBundle\Entity\Report\Expense>")
+     * @JMS\Groups({"expenses"})
+     * @ORM\OneToMany(targetEntity="AppBundle\Entity\Report\Expense", mappedBy="report", cascade={"persist"})
+     *
+     * @var Expense[]
+     */
+    private $expenses;
+
+    /**
+     * @return string
+     */
+    public function getPaidForAnything()
+    {
+        return $this->paidForAnything;
+    }
+
+    /**
+     * @param string $paidForAnything
+     *
+     * @return Report
+     */
+    public function setPaidForAnything($paidForAnything)
+    {
+        $this->paidForAnything = $paidForAnything;
+
+        return $this;
+    }
+
+    /**
+     * @return Expense[]
+     */
+    public function getExpenses()
+    {
+        return $this->expenses;
+    }
+
+    /**
+     * @param Expense[]|null $expenses
+     *
+     * @return Report
+     */
+    public function setExpenses($expenses)
+    {
+        $this->expenses = $expenses;
+
+        return $this;
+    }
+
+    /**
+     * @param Expense $expense
+     *
+     * @return Report
+     */
+    public function addExpense(Expense $expense)
+    {
+        if (!$this->expenses->contains($expense)) {
+            $this->expenses->add($expense);
+        }
+
+        return $this;
+    }
+}
