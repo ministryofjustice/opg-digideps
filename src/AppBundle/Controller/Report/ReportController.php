e13749811d04f93da265e77258b3276cca8797f4
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2017-01-25 19:36:00 +0100

    DDPB-735 fix

--- src/AppBundle/Controller/Report/ReportController.php
+++ src/AppBundle/Controller/Report/ReportController.php
@@ -18,6 +18,7 @@ class ReportController extends AbstractController
     private static $reportGroupsForValidation = [
         'account',
         'action',
+        'action-more-info',
         'asset',
         'debt',
         'balance',
