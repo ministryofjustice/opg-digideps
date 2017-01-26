022fd02d573e8f3faead212b20502d7d74b63530
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2017-01-26 12:29:56 +0100

    DDPB-728 data model with migration

---
+++ src/AppBundle/Entity/Report/Traits/GiftsTrait.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace AppBundle\Entity\Report\Traits;
+
+use AppBundle\Entity\Report\Gift;
+use AppBundle\Entity\Report\Report;
+use Doctrine\Common\Collections\ArrayCollection;
+use JMS\Serializer\Annotation as JMS;
+use Doctrine\ORM\Mapping as ORM;
+
+trait GiftsTrait
+{
+    /**
+     * @var string
+     *
+     * @JMS\Type("string")
+     * @JMS\Groups({"gifts"})
+     * @ORM\Column(name="gifts_exist", type="string", length=3, nullable=true)
+     */
+    private $giftsExist;
+
+    /**
+     * @JMS\Type("array<AppBundle\Entity\Report\Gift>")
+     * @JMS\Groups({"gifts"})
+     * @ORM\OneToMany(targetEntity="AppBundle\Entity\Report\Gift", mappedBy="report", cascade={"persist"})
+     *
+     * @var Gift[]
+     */
+    private $gifts;
+
+    /**
+     * @return string
+     */
+    public function getGiftsExist()
+    {
+        return $this->giftsExist;
+    }
+
+    /**
+     * @param string $giftsExist
+     */
+    public function setGiftsExist($giftsExist)
+    {
+        $this->giftsExist = $giftsExist;
+    }
+
+
+    /**
+     * @return Gift[]
+     */
+    public function getGifts()
+    {
+        return $this->gifts;
+    }
+
+    /**
+     * @param Gift[]|null $gifts
+     *
+     * @return Report
+     */
+    public function setGifts($gifts)
+    {
+        $this->gifts = $gifts;
+
+        return $this;
+    }
+
+    /**
+     * @param Gift $gift
+     *
+     * @return Report
+     */
+    public function addGift(Gift $gift)
+    {
+        if (!$this->gifts->contains($gift)) {
+            $this->gifts->add($gift);
+        }
+
+        return $this;
+    }
+}
