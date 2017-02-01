fb5499ab5c9055cabd8a863f2678cc8568daf58f
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2017-02-01 09:50:28 +0000

    fix-data command

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -6,4 +6,7 @@ run-parts /etc/my_init.d
 cd /app
 /sbin/setuser app php app/console doctrine:migrations:status-check
 /sbin/setuser app php app/console doctrine:migrations:migrate --no-interaction -vvv
+# add default users
 /sbin/setuser app php app/console digideps:fixtures
+# add missing data potentially notmissing due to failing migrations or previous bugs on data listeners
+/sbin/setuser app php app/console digideps:fix-data
