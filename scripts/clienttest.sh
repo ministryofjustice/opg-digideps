aa55eec3718fb23bb4a578749910af020c86e967
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2017-02-17 17:34:44 +0000

    DDPB-971 behat remove dist and PA to behat.tmpl

--- scripts/clienttest.sh
+++ scripts/clienttest.sh
@@ -17,22 +17,11 @@ export PGDATABASE=${API_DATABASE_NAME:=api}
 export PGUSER=${API_DATABASE_USERNAME:=api}
 rm -rf app/cache/*
 
+# phpunit
 /sbin/setuser app php vendor/phpunit/phpunit/phpunit -c tests/phpunit/
 
-# tests with coverage (install php5-xdebug if needed)
-#/sbin/setuser app php -d zend_extension=xdebug.so bin/phpunit -c tests/phpunit/phpunit.xml --coverage-html=web/coverage-html
-
-# deprecated
-if [ -f tests/behat/behat.yml ]; then
-    behatConfigFile=tests/behat/behat.yml
-else
-    behatConfigFile=tests/behat/behat.yml.dist
-fi
-# end deprecated
-
-echo ${behatConfigFile}
-
+# behat
 export BEHAT_PARAMS="{\"extensions\" : {\"Behat\\\\MinkExtension\\\\ServiceContainer\\\\MinkExtension\" : {\"base_url\" : \"${FRONTEND_NONADMIN_HOST}\",\"selenium2\" : { \"wd_host\" : \"$WD_HOST\" }, \"browser_stack\" : { \"username\": \"$BROWSERSTACK_USER\", \"access_key\": \"$BROWSERSTACK_KEY\"}}}}"
-/sbin/setuser app bin/behat --config=${behatConfigFile} --suite=deputy --profile=${PROFILE:=headless} --stop-on-failure
-/sbin/setuser app bin/behat --config=${behatConfigFile} --suite=deputyodr --profile=${PROFILE:=headless} --stop-on-failure
-/sbin/setuser app bin/behat --config=${behatConfigFile} --suite=pa --profile=${PROFILE:=headless} --stop-on-failure
+/sbin/setuser app bin/behat --config=tests/behat/behat.yml --suite=deputy --profile=${PROFILE:=headless} --stop-on-failure
+/sbin/setuser app bin/behat --config=tests/behat/behat.yml --suite=deputyodr --profile=${PROFILE:=headless} --stop-on-failure
+/sbin/setuser app bin/behat --config=tests/behat/behat.yml --suite=pa --profile=${PROFILE:=headless} --stop-on-failure
