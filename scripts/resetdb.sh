7f8b6dadb50302879308cac00e9a83e148734c8e
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2017-02-17 16:53:30 +0000

    verbose resetdb

--- scripts/resetdb.sh
+++ scripts/resetdb.sh
@@ -8,6 +8,9 @@ export PGHOST=${API_DATABASE_HOSTNAME:=postgres}
 export PGPASSWORD=${API_DATABASE_PASSWORD:=api}
 export PGDATABASE=${API_DATABASE_NAME:=api}
 export PGUSER=${API_DATABASE_USERNAME:=api}
+
+echo "Dropping $PGDATABASE database, user $PGUSER on $PGHOST"
+
 /sbin/setuser app  psql -c "DROP SCHEMA IF EXISTS public CASCADE; CREATE SCHEMA IF NOT EXISTS public;"
 /sbin/setuser app php app/console doctrine:migrations:status-check
 /sbin/setuser app php app/console doctrine:migrations:migrate --no-interaction -vvv
