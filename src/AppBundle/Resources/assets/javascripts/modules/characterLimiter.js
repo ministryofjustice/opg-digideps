3465af9fde0cde1b1807116aa963d771402761e4
Biggs Thorarensen <biggs@devillion.com>
2017-03-08 18:02:01 +0100

    DDPB-243 js tweaks after review

--- src/AppBundle/Resources/assets/javascripts/modules/characterLimiter.js
+++ src/AppBundle/Resources/assets/javascripts/modules/characterLimiter.js
@@ -17,19 +17,20 @@
         var limitElement = $(containerSelector).find("[class*='js-limit-chars-']");
 
         limitElement.on('keyup input paste change', function (event) {
+            var $this = $(event.target);
             // Get the classes
-            var elClass = $(event.target).attr('class');
+            var elClass = $this.attr('class');
             // Get the limiter value (the 'x')
-            var charsLimit = elClass.substr(elClass.indexOf("limit-chars-")+12,1);
+            var charsLimit = parseInt(elClass.substr(elClass.indexOf("limit-chars-")+12,1));
             // The amount of chars in the input
-            var chars = $(event.target).val().length;
+            var chars = $this.val().length;
 
             if(chars <= charsLimit){
                 return true;
             } else {
-                var str = $(event.target).val();
+                var str = $this.val();
                 str = str.substring(0, str.length - 1);
-                $(event.target).val(str);
+                $this.val(str);
             }
 
         }).trigger('keyup');
