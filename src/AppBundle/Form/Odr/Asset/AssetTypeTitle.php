76a9cbd085ca960970abb2686f3516a9ddda1025
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2017-03-17 16:14:05 +0000

    DDPB-980 simplify asset form + rely on YML order

--- src/AppBundle/Form/Odr/Asset/AssetTypeTitle.php
+++ src/AppBundle/Form/Odr/Asset/AssetTypeTitle.php
@@ -7,66 +7,8 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\Translation\TranslatorInterface;
 
-/**
- * Displays "title" and "next" for the asset.
- */
-class AssetTypeTitle extends AbstractType
+class AssetTypeTitle extends \AppBundle\Form\Report\Asset\AssetTypeTitle
 {
-    /**
-     * @var array
-     */
-    private $assetDropdownKeys;
-
-    /**
-     * @var Translator
-     */
-    private $translator;
-
-    /**
-     * @var string
-     */
-    private $translatorDomain;
-
-    public function __construct(array $assetDropdownKeys, TranslatorInterface $translator, $translatorDomain)
-    {
-        $this->assetDropdownKeys = $assetDropdownKeys;
-        $this->translator = $translator;
-        $this->translatorDomain = $translatorDomain;
-    }
-
-    /**
-     * @return array with choices for the "title" dropdown element
-     */
-    public function getTitleChoices()
-    {
-        if (empty($this->assetDropdownKeys)) {
-            return [];
-        }
-
-        $ret = [];
-
-        // translate keys and order by name
-        foreach ($this->assetDropdownKeys as $key) {
-            $translation = $this->translator->trans('form.title.choices.' . $key, [], $this->translatorDomain);
-            $ret[$translation] = $translation;
-        }
-        // order by name (keep position for the last element)
-        $last = array_pop($ret);
-        // order by name
-        asort($ret);
-        $ret[$last] = $last;
-
-        return $ret;
-    }
-
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder->add('title', 'choice', [
-                'choices' => $this->getTitleChoices(),
-                'expanded' => true])
-            ->add('save', 'submit');
-    }
-
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults([
