9aefca485b3c57c8c323b6a8a1506e88569ece71
Shaun Lizzio <shaun@lizzio.co.uk>
2017-03-27 18:02:54 +0100

    Added and fixed ACL using voters

--- src/AppBundle/Security/UserVoter.php
+++ src/AppBundle/Security/UserVoter.php
@@ -5,57 +5,91 @@ namespace AppBundle\Security;
 use AppBundle\Entity\User;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Authorization\AccessDecisionManagerInterface;
 
 class UserVoter extends Voter
 {
-    const ADD = 'add';
-    const EDIT = 'edit';
+    const ADD_USER = 'add-user';
+    const EDIT_USER = 'edit-user';
 
-    protected function supports($attribute, $subject)
+    private $decisionManager;
+
+    /**
+     * UserVoter constructor.
+     *
+     * @param AccessDecisionManagerInterface $decisionManager
+     */
+    public function __construct(AccessDecisionManagerInterface $decisionManager)
     {
-        // if the attribute isn't one we support, return false
-        if (!in_array($attribute, array(self::ADD, self::EDIT))) {
-            return false;
-        }
-
-        // only vote on User objects inside this voter
-        if (!$subject instanceof User) {
-            return false;
-        }
-
-        return true;
+        $this->decisionManager = $decisionManager;
     }
 
+    /**
+     * Does this voter support the attribute?
+     *
+     * @param string $attribute
+     * @param mixed $subject
+     * @return bool
+     */
+    protected function supports($attribute, $subject)
+    {
+        switch($attribute) {
+            case self::ADD_USER:
+                return true;
+            case self::EDIT_USER:
+                // only vote on User objects inside this voter
+                if ($attribute === self::EDIT_USER && $subject instanceof User) {
+                    return true;
+                }
+                break;
+        }
+
+        return false;
+    }
+
+    /**
+     * Vote on whether to grant attribute permission on subject
+     *
+     * @param string $attribute
+     * @param mixed $subject
+     * @param TokenInterface $token
+     * @return bool
+     */
     protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
     {
         $loggedUser= $token->getUser();
-
         if (!$loggedUser instanceof User) {
             // the loggedUSer must be logged in; if not, deny access
-            return false;
+            return Voter::ACCESS_DENIED;
         }
 
-        $loggedUserRoleName = $loggedUser->getRoleName();
-
-        if ($attribute == self::ADD) {
+        if ($attribute === self::ADD_USER) {
             // only Named and Admin can add users
-            return in_array($loggedUserRoleName, [User::ROLE_PA, User::ROLE_PA_ADMIN]);
+            return $this->decisionManager->decide($token, [User::ROLE_PA, USER::ROLE_PA_ADMIN]);
         }
 
-        if ($attribute == self::EDIT) {
-            switch($loggedUserRoleName) {
-                // Named can always edit everyone
+        if ($attribute == self::EDIT_USER) {
+            if ($subject->getId() === $loggedUser->getId()) {
+                // can always edit one's self
+                return Voter::ACCESS_GRANTED;
+            }
+
+            switch($loggedUser->getRoleName()) {
                 case User::ROLE_PA:
-                    return true;
-                // Admin can edit everyone except Named
+                    // Named can always edit everyone
+                    return Voter::ACCESS_GRANTED;
                 case User::ROLE_PA_ADMIN:
-                    return $subject->getRoleName() !== User::ROLE_PA;
-                // Team members can only edit themselves
+                    // Admin can edit everyone except Named
+                    if ($subject->getRoleName() !== User::ROLE_PA) {
+                        return Voter::ACCESS_GRANTED;
+                    }
+                    break;
                 case User::ROLE_PA_TEAM_MEMBER:
-                    return $subject->getId() === $loggedUser->getId();
+                    // Team members can only edit themselves (See above)
+                    return Voter::ACCESS_DENIED;
             }
         }
 
-        throw new \LogicException('This code should not be reached!');
+        return Voter::ACCESS_DENIED;
     }
 }
