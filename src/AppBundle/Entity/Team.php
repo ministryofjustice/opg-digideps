f8b8cae4d282a4fce5b955c5ea90373e607687d7
Shaun Lizzio <shaun@lizzio.co.uk>
2017-03-29 18:59:15 +0100

    DDPB-1062 - Passed Team to TeamMember Form constructor to check whether max admins have been reached.

---
+++ src/AppBundle/Entity/Team.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as JMS;
+
+/**
+ * Team
+ *
+ * @codeCoverageIgnore
+ */
+class Team
+{
+    const MAX_PA_ADMINS = 2;
+
+    /**
+     * @JMS\Type("integer")
+     * @JMS\Groups({"user_details_full", "user_details_basic", "admin_add_user"})
+     *
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @JMS\Groups({"user"})
+     *
+     * @JMS\Type("ArrayCollection<AppBundle\Entity\User>")
+     */
+    private $members;
+
+    /**
+     * @var string
+     *
+     * @JMS\Groups({"team"})
+     * @JMS\Type("string")
+     */
+    private $teamName;
+
+    /**
+     * Team constructor.
+     * @param string $teamName
+     */
+    public function __construct($teamName)
+    {
+        $this->teamName = $teamName;
+        $this->members = new ArrayCollection();
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return $this
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMembers()
+    {
+        return $this->members;
+    }
+
+    /**
+     * @param mixed $members
+     *
+     * @return $this
+     */
+    public function setMembers($members)
+    {
+        $this->members = $members;
+        return $this;
+    }
+
+    /**
+     * Add a member
+     *
+     * @param User $user
+     * @return $this
+     */
+    public function addMember(User $user)
+    {
+        if (!$this->members->contains($user)) {
+            $this->members->add($user);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Add Members
+     *
+     * @param ArrayCollection $members Collection being added
+     *
+     * @return $this
+     */
+    public function addMembers(ArrayCollection $members)
+    {
+        $this->members = new ArrayCollection(
+            array_merge(
+                $this->members->toArray(),
+                $members->toArray()
+            )
+        );
+
+        return $this;
+    }
+
+    /**
+     * Remove a member from the collection
+     *
+     * @param mixed $member collection being removed
+     *
+     * @return $this
+     */
+    public function removeMember($member)
+    {
+        if ($this->members->contains($member)) {
+            $this->members->removeElement($member);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTeamName()
+    {
+        return $this->teamName;
+    }
+
+    /**
+     * @param string $teamName
+     *
+     * @return $this
+     */
+    public function setTeamName($teamName)
+    {
+        $this->teamName = $teamName;
+        return $this;
+    }
+
+    public function canAddAdmin()
+    {
+        $adminCount = 0;
+        /** @var User $member */
+        foreach ($this->members as $member)
+        {
+            if ($member->isPaAdministrator()) {
+                $adminCount++;
+            }
+        }
+
+        return $adminCount < self::MAX_PA_ADMINS;
+    }
+}
