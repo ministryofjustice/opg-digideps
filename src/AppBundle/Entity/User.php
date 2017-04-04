cf0a24873fd64e64c58189edcbab3a285b0c7369
Shaun Lizzio <shaun@lizzio.co.uk>
2017-04-04 13:12:31 +0100

    pa-voters-acl Added requirement for phone and job tiltle when editing users

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -71,9 +71,9 @@ class User implements AdvancedUserInterface
     /**
      * @JMS\Type("string")
      * @JMS\Groups({"admin_add_user", "ad_add_user", "pa_team_add"})
-     * @Assert\NotBlank( message="user.email.notBlank", groups={"admin_add_user", "pa_team_add", "password_reset"} )
-     * @Assert\Email( message="user.email.invalid", groups={"admin_add_user", "password_reset", "pa_team_add"}, checkMX=false, checkHost=false )
-     * @Assert\Length( max=60, maxMessage="user.email.maxLength", groups={"admin_add_user", "password_reset", "pa_team_add"} )
+     * @Assert\NotBlank( message="user.email.notBlank", groups={"admin_add_user", "user_details_pa", "pa_team_add", "password_reset"} )
+     * @Assert\Email( message="user.email.invalid", groups={"admin_add_user", "password_reset", "user_details_pa", "pa_team_add"}, checkMX=false, checkHost=false )
+     * @Assert\Length( max=60, maxMessage="user.email.maxLength", groups={"admin_add_user", "password_reset", "user_details_pa", "pa_team_add"} )
      *
      * @var string
      */
