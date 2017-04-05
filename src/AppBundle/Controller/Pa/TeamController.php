e2544a8725de9864e86b5d783aeb35d7fc01e800
Shaun Lizzio <shaun@lizzio.co.uk>
2017-04-05 11:22:37 +0100

    pa-voters-acl Fixed bug with calling TeamMemberAccount constructor and ensured non team members get PA account edit page

--- src/AppBundle/Controller/Pa/TeamController.php
+++ src/AppBundle/Controller/Pa/TeamController.php
@@ -41,7 +41,7 @@ class TeamController extends AbstractController
 
         $team = $this->getRestClient()->get('user/' .  $this->getUser()->getId() . '/team', 'Team');
 
-        $form = $this->createForm(new FormDir\Pa\TeamMemberAccount($team), $this->getUser());
+        $form = $this->createForm(new FormDir\Pa\TeamMemberAccount($team, $this->getUser()));
 
         $form->handleRequest($request);
 
