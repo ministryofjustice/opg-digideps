bd81426ed8f40edda2cffaa22ff2a91af7558725
Shaun Lizzio <shaun@lizzio.co.uk>
2017-04-13 11:07:58 +0100

    DDPB-1084 Added ConfirmBundle to composer and app kernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -24,6 +24,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Snc\RedisBundle\SncRedisBundle(),
+            new \SymfonyContrib\Bundle\ConfirmBundle\ConfirmBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'])) {
