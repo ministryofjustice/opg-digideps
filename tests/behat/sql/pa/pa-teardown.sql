228e4c6058a8706db7b4b220af665271254867df
David Winchurch <david.winchurch@iterativeit.com>
2017-04-25 09:59:56 +0100

    DDPB-1149: Removing hard coded ids from scripts

--- tests/behat/sql/pa/pa-teardown.sql
+++ tests/behat/sql/pa/pa-teardown.sql
@@ -1,4 +1,15 @@
---Clear all data and users required by pa team tests
-truncate table report,deputy_case,client,user_team,dd_team RESTART IDENTITY cascade;
-DELETE FROM dd_user WHERE id > 4;
-ALTER SEQUENCE dd_user_id_seq RESTART WITH 5;
+--Clear all data and users required by pa team behat tests
+DO $$
+DECLARE
+  maxId integer;
+BEGIN
+  SET client_min_messages TO WARNING;
+  TRUNCATE TABLE report,deputy_case,client,user_team,dd_team RESTART IDENTITY CASCADE;
+  DELETE FROM dd_user WHERE email NOT IN (
+    'ad@publicguardian.gsi.gov.uk',
+    'laydeputy@publicguardian.gsi.gov.uk',
+    'laydeputyodr@publicguardian.gsi.gov.uk',
+    'admin@publicguardian.gsi.gov.uk');
+  SELECT MAX(id) FROM dd_user INTO maxId;
+  PERFORM SETVAL('dd_user_id_seq', maxId);
+END $$;
--- tests/behat/sql/pa/pateam-setup.sql
+++ tests/behat/sql/pa/pateam-setup.sql
@@ -1,60 +1,83 @@
 --Sets up the data required to run the pa team tests.
---Requires a clean database
-INSERT INTO dd_team (team_name) VALUES (NULL);
-INSERT INTO dd_user (firstname, lastname, password, email, active, registration_date, registration_token, token_date, address_postcode, address_country, phone_main, last_logged_in, deputy_no, odr_enabled, ad_managed, role_name, job_title, agree_terms_use, agree_terms_use_date)
-VALUES ('John Named', 'Green', '9k4PZrYAhWIMcVCELlGk/xJmzYtFLGmta924lBP/VvM4T7sfEDomfn373dueeyh+CADl/aPlzOQV0h+3h1N3Wg==', 'behat-pa1@publicguardian.gsi.gov.uk', TRUE, CURRENT_TIMESTAMP, '', CURRENT_TIMESTAMP, 'SW1', 'GB', '10000000001', CURRENT_TIMESTAMP, 9000001, NULL, NULL, 'ROLE_PA', 'Solicitor', true, CURRENT_TIMESTAMP);
-INSERT INTO user_team VALUES (5, 1);
+DO $$
+DECLARE
+  teamId integer;
+  userId integer;
+  clientId integer;
+BEGIN
+  --Insert behat-pa1@publicguardian.gsi.gov.uk user and add to team
+  INSERT INTO dd_team (team_name) VALUES (NULL) RETURNING id INTO teamId;
+  INSERT INTO dd_user (firstname, lastname, password, email, active, registration_date, registration_token, token_date, address_postcode, address_country, phone_main, last_logged_in, deputy_no, odr_enabled, ad_managed, role_name, job_title, agree_terms_use, agree_terms_use_date)
+  VALUES ('John Named', 'Green', '9k4PZrYAhWIMcVCELlGk/xJmzYtFLGmta924lBP/VvM4T7sfEDomfn373dueeyh+CADl/aPlzOQV0h+3h1N3Wg==', 'behat-pa1@publicguardian.gsi.gov.uk', TRUE, CURRENT_TIMESTAMP, '', CURRENT_TIMESTAMP, 'SW1', 'GB', '10000000001', CURRENT_TIMESTAMP, 9000001, NULL, NULL, 'ROLE_PA', 'Solicitor', true, CURRENT_TIMESTAMP)
+  RETURNING id INTO userId;
+  INSERT INTO user_team VALUES (userId, teamId);
 
-INSERT INTO client (case_number, firstname, lastname) VALUES ('1000010', 'CLY1', 'HENT1');
-INSERT INTO client (case_number, firstname, lastname) VALUES ('1138393T', 'CLY2', 'HENT2');
-INSERT INTO client (case_number, firstname, lastname) VALUES ('11498120', 'CLY3', 'HENT3');
-INSERT INTO client (case_number, firstname, lastname) VALUES ('1000011', 'CLY4', 'HENT4');
-INSERT INTO client (case_number, firstname, lastname) VALUES ('1000012', 'CLY5', 'HENT5');
-INSERT INTO client (case_number, firstname, lastname) VALUES ('1000013', 'CLY6', 'HENT6');
-INSERT INTO client (case_number, firstname, lastname) VALUES ('1000014', 'CLY7', 'HENT');
-INSERT INTO client (case_number, firstname, lastname) VALUES ('1000015', 'CLY8', 'HENT8');
-INSERT INTO client (case_number, firstname, lastname) VALUES ('1000016', 'CLY9', 'HENT9');
-INSERT INTO client (case_number, firstname, lastname) VALUES ('1000017', 'CLY10', 'HENT10');
-INSERT INTO client (case_number, firstname, lastname) VALUES ('1000018', 'CL11', 'HENT11');
-INSERT INTO client (case_number, firstname, lastname) VALUES ('1000019', 'CLY12', 'HENT12');
-INSERT INTO client (case_number, firstname, lastname) VALUES ('1000020', 'CLY13', 'HENT13');
-INSERT INTO client (case_number, firstname, lastname) VALUES ('1000021', 'CLY14', 'HENT14');
-INSERT INTO client (case_number, firstname, lastname) VALUES ('1000022', 'CLY15', 'HENT15');
-INSERT INTO client (case_number, firstname, lastname) VALUES ('1000023', 'CLY16', 'HENT16');
-INSERT INTO client (case_number, firstname, lastname) VALUES ('1000024', 'CLY17', 'HENT17');
+  --Client data from behat-pa.csv
+  INSERT INTO client (case_number, firstname, lastname) VALUES ('1000010', 'CLY1', 'HENT1') RETURNING id INTO clientId;
+  INSERT INTO deputy_case VALUES (clientId, userId);
+  INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (clientId, 102, '2017-03-19', NULL, NULL, TRUE);
 
-INSERT INTO deputy_case VALUES (1, 5);
-INSERT INTO deputy_case VALUES (2, 5);
-INSERT INTO deputy_case VALUES (3, 5);
-INSERT INTO deputy_case VALUES (4, 5);
-INSERT INTO deputy_case VALUES (5, 5);
-INSERT INTO deputy_case VALUES (6, 5);
-INSERT INTO deputy_case VALUES (7, 5);
-INSERT INTO deputy_case VALUES (8, 5);
-INSERT INTO deputy_case VALUES (9, 5);
-INSERT INTO deputy_case VALUES (10, 5);
-INSERT INTO deputy_case VALUES (11, 5);
-INSERT INTO deputy_case VALUES (12, 5);
-INSERT INTO deputy_case VALUES (13, 5);
-INSERT INTO deputy_case VALUES (14, 5);
-INSERT INTO deputy_case VALUES (15, 5);
-INSERT INTO deputy_case VALUES (16, 5);
-INSERT INTO deputy_case VALUES (17, 5);
+  INSERT INTO client (case_number, firstname, lastname) VALUES ('1138393T', 'CLY2', 'HENT2') RETURNING id INTO clientId;
+  INSERT INTO deputy_case VALUES (clientId, userId);
+  INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (clientId, 102, '2017-10-01', NULL, NULL, TRUE);
 
-INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (1, 102, '2017-03-19', NULL, NULL, TRUE);
-INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (2, 102, '2017-10-01', NULL, NULL, TRUE);
-INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (3, 102, '2017-05-28', NULL, NULL, TRUE);
-INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (4, 102, '2017-05-28', NULL, NULL, TRUE);
-INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (5, 102, '2017-05-28', NULL, NULL, TRUE);
-INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (6, 102, '2017-05-28', NULL, NULL, TRUE);
-INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (7, 102, '2017-05-28', NULL, NULL, TRUE);
-INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (8, 102, '2017-05-28', NULL, NULL, TRUE);
-INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (9, 102, '2017-05-28', NULL, NULL, TRUE);
-INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (10, 102, '2017-05-28', NULL, NULL, TRUE);
-INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (11, 102, '2017-05-28', NULL, NULL, TRUE);
-INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (12, 102, '2017-05-28', NULL, NULL, TRUE);
-INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (13, 102, '2017-05-28', NULL, NULL, TRUE);
-INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (14, 102, '2017-05-28', NULL, NULL, TRUE);
-INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (15, 102, '2017-05-28', NULL, NULL, TRUE);
-INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (16, 102, '2017-05-28', NULL, NULL, TRUE);
-INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (17, 102, '2017-05-28', NULL, NULL, TRUE);
+  INSERT INTO client (case_number, firstname, lastname) VALUES ('11498120', 'CLY3', 'HENT3') RETURNING id INTO clientId;
+  INSERT INTO deputy_case VALUES (clientId, userId);
+  INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (clientId, 102, '2017-05-28', NULL, NULL, TRUE);
+
+  INSERT INTO client (case_number, firstname, lastname) VALUES ('1000011', 'CLY4', 'HENT4') RETURNING id INTO clientId;
+  INSERT INTO deputy_case VALUES (clientId, userId);
+  INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (clientId, 102, '2017-05-28', NULL, NULL, TRUE);
+
+  INSERT INTO client (case_number, firstname, lastname) VALUES ('1000012', 'CLY5', 'HENT5') RETURNING id INTO clientId;
+  INSERT INTO deputy_case VALUES (clientId, userId);
+  INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (clientId, 102, '2017-05-28', NULL, NULL, TRUE);
+
+  INSERT INTO client (case_number, firstname, lastname) VALUES ('1000013', 'CLY6', 'HENT6') RETURNING id INTO clientId;
+  INSERT INTO deputy_case VALUES (clientId, userId);
+  INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (clientId, 102, '2017-05-28', NULL, NULL, TRUE);
+
+  INSERT INTO client (case_number, firstname, lastname) VALUES ('1000014', 'CLY7', 'HENT') RETURNING id INTO clientId;
+  INSERT INTO deputy_case VALUES (clientId, userId);
+  INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (clientId, 102, '2017-05-28', NULL, NULL, TRUE);
+
+  INSERT INTO client (case_number, firstname, lastname) VALUES ('1000015', 'CLY8', 'HENT8') RETURNING id INTO clientId;
+  INSERT INTO deputy_case VALUES (clientId, userId);
+  INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (clientId, 102, '2017-05-28', NULL, NULL, TRUE);
+
+  INSERT INTO client (case_number, firstname, lastname) VALUES ('1000016', 'CLY9', 'HENT9') RETURNING id INTO clientId;
+  INSERT INTO deputy_case VALUES (clientId, userId);
+  INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (clientId, 102, '2017-05-28', NULL, NULL, TRUE);
+
+  INSERT INTO client (case_number, firstname, lastname) VALUES ('1000017', 'CLY10', 'HENT10') RETURNING id INTO clientId;
+  INSERT INTO deputy_case VALUES (clientId, userId);
+  INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (clientId, 102, '2017-05-28', NULL, NULL, TRUE);
+
+  INSERT INTO client (case_number, firstname, lastname) VALUES ('1000018', 'CL11', 'HENT11') RETURNING id INTO clientId;
+  INSERT INTO deputy_case VALUES (clientId, userId);
+  INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (clientId, 102, '2017-05-28', NULL, NULL, TRUE);
+
+  INSERT INTO client (case_number, firstname, lastname) VALUES ('1000019', 'CLY12', 'HENT12') RETURNING id INTO clientId;
+  INSERT INTO deputy_case VALUES (clientId, userId);
+  INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (clientId, 102, '2017-05-28', NULL, NULL, TRUE);
+
+  INSERT INTO client (case_number, firstname, lastname) VALUES ('1000020', 'CLY13', 'HENT13') RETURNING id INTO clientId;
+  INSERT INTO deputy_case VALUES (clientId, userId);
+  INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (clientId, 102, '2017-05-28', NULL, NULL, TRUE);
+
+  INSERT INTO client (case_number, firstname, lastname) VALUES ('1000021', 'CLY14', 'HENT14') RETURNING id INTO clientId;
+  INSERT INTO deputy_case VALUES (clientId, userId);
+  INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (clientId, 102, '2017-05-28', NULL, NULL, TRUE);
+
+  INSERT INTO client (case_number, firstname, lastname) VALUES ('1000022', 'CLY15', 'HENT15') RETURNING id INTO clientId;
+  INSERT INTO deputy_case VALUES (clientId, userId);
+  INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (clientId, 102, '2017-05-28', NULL, NULL, TRUE);
+
+  INSERT INTO client (case_number, firstname, lastname) VALUES ('1000023', 'CLY16', 'HENT16') RETURNING id INTO clientId;
+  INSERT INTO deputy_case VALUES (clientId, userId);
+  INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (clientId, 102, '2017-05-28', NULL, NULL, TRUE);
+
+  INSERT INTO client (case_number, firstname, lastname) VALUES ('1000024', 'CLY17', 'HENT17') RETURNING id INTO clientId;
+  INSERT INTO deputy_case VALUES (clientId, userId);
+  INSERT INTO report (client_id, type, end_date, no_asset_to_add, no_transfers_to_add, report_seen) VALUES (clientId, 102, '2017-05-28', NULL, NULL, TRUE);
+END $$;
