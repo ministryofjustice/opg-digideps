3c4696816fd35e1429bde73529a4709dc278ce51
David Winchurch <david.winchurch@iterativeit.com>
2017-05-04 16:43:16 +0100

    DDPB-1146: Reverting more behat changes

--- scripts/adtest.sh
+++ scripts/adtest.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-sh scripts/configureEnvironment.sh
+#let's configure environment
+run-parts /etc/my_init.d
+chown app:app /tmp/behat
 
-# environment variables for psql command
+cd /app
+/sbin/setuser app mkdir -p /tmp/behat
+apt-get update > /dev/null 2>&1
 export PGHOST=${API_DATABASE_HOSTNAME:=postgres}
 export PGPASSWORD=${API_DATABASE_PASSWORD:=api}
 export PGDATABASE=${API_DATABASE_NAME:=api}
 export PGUSER=${API_DATABASE_USERNAME:=api}
-
+rm -rf app/cache/*
 /sbin/setuser app bin/behat --config=tests/behat/behat.yml --suite=ad --profile=${PROFILE:=headless} --stop-on-failure
--- scripts/clienttest.sh
+++ scripts/clienttest.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
-sh scripts/configureEnvironment.sh
+set -e
+#let's configure environment
+run-parts /etc/my_init.d
+chown app:app /tmp/behat
+chown app:app /tmp
 
-# environment variables for psql command
+# create log dir locally failing sometimes)
+mkdir -p /var/log/app
+chown app:app /var/log/app
+
+cd /app
+/sbin/setuser app mkdir -p /tmp/behat
 export PGHOST=${API_DATABASE_HOSTNAME:=postgres}
 export PGPASSWORD=${API_DATABASE_PASSWORD:=api}
 export PGDATABASE=${API_DATABASE_NAME:=api}
 export PGUSER=${API_DATABASE_USERNAME:=api}
+rm -rf app/cache/*
 
 # phpunit
-/sbin/setuser app php vendor/phpunit/phpunit/phpunit -c tests/phpunit/ #Unit tests. Initial run can take a significant amount of time but subsequent runs are <10 seconds
+/sbin/setuser app php vendor/phpunit/phpunit/phpunit -c tests/phpunit/
 
 # behat
 export BEHAT_PARAMS="{\"extensions\" : {\"Behat\\\\MinkExtension\\\\ServiceContainer\\\\MinkExtension\" : {\"base_url\" : \"${FRONTEND_NONADMIN_HOST}\",\"selenium2\" : { \"wd_host\" : \"$WD_HOST\" }, \"browser_stack\" : { \"username\": \"$BROWSERSTACK_USER\", \"access_key\": \"$BROWSERSTACK_KEY\"}}}}"
