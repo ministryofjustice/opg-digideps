330b987964aa5ecc4aba79b401e9a205887aff15
Elvis Ciotti <elvis@MacBook-Pro-2.local>
2017-05-10 11:37:50 +0200

    DDPB-1201 migration to fix data

---
+++ app/DoctrineMigrations/Version129.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Application\Migrations;
+
+use AppBundle\Service\FixDataService;
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class Version129 extends AbstractMigration  implements ContainerAwareInterface
+{
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $em = $this->container->get('em');
+
+        $fixDataService = new FixDataService($em);
+        $messages = $fixDataService->fixReports()->getMessages();
+        echo "Fix report results: " . print_r($messages, true);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
