4729f29d2dc8a6a0a6b38ffa5a6bf13dc4d77546
Elvis Ciotti <elvisciotti@gmail.com>
2017-05-31 18:14:09 +0200

    pa-upload-big-file error contains deputyNo and case number

--- src/AppBundle/Service/PaService.php
+++ src/AppBundle/Service/PaService.php
@@ -88,10 +88,8 @@ class PaService
                 $user = $this->createUser($row);
                 $client = $this->createClient($row, $user);
                 $this->createReport($row, $client, $user);
-            } catch (\RuntimeException $e) {
-                $errors[] = $e->getMessage() . ' in line ' . $line;
             } catch (\Exception $e) {
-                $message = 'Unable to add Deputy No: ' . $row['Deputy No'] . ' at line ' . $line;
+                $message = 'Error for Deputy No: ' . $row['Deputy No'] . ', case '.$row['Case'].': ' . $e->getMessage();
                 $errors[] = $message;
             }
         }
