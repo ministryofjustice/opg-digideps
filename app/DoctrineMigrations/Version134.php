8952c7d807ff0b6ba7aaa68b447e862e8baf38dd
Elvis Ciotti <elvisciotti@gmail.com>
2017-06-05 15:02:33 +0200

    DDPB-807-view-client-details down migration remove drop schema

--- app/DoctrineMigrations/Version134.php
+++ app/DoctrineMigrations/Version134.php
@@ -29,7 +29,6 @@ class Version134 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE client DROP date_of_birth');
     }
 }
