4e47451529eb9b739fb5e8190b7cf3697aa1fe4a
Shaun Lizzio <shaun@lizzio.co.uk>
2017-06-13 11:28:34 +0100

    DDPB-797 Added Notes endpoint to add note

---
+++ src/AppBundle/Controller/Report/NoteController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AppBundle\Controller\Report;
+
+use AppBundle\Controller\RestController;
+use AppBundle\Entity as EntityDir;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class NoteController extends RestController
+{
+    /**
+     * @Route("/report/{reportId}/note", requirements={"reportId":"\d+"})
+     * @Method({"POST"})
+     */
+    public function add(Request $request, $reportId)
+    {
+        $this->denyAccessUnlessGranted(
+            [
+                EntityDir\User::ROLE_PA,
+                EntityDir\User::ROLE_PA_ADMIN,
+                EntityDir\User::ROLE_PA_TEAM_MEMBER
+            ]
+        );
+
+        $data = $this->deserializeBodyContent($request);
+
+        $report = $this->findEntityBy(EntityDir\Report\Report::class, $reportId); /* @var $report EntityDir\Report\Report */
+        $this->denyAccessIfReportDoesNotBelongToUser($report);
+        $this->validateArray($data, [
+            'title' => 'mustExist',
+        ]);
+        $note = new EntityDir\Note($report, $data['category'], $data['title'], $data['content']);
+
+        $this->persistAndFlush($note);
+
+        return ['id' => $note->getId()];
+    }
+}
