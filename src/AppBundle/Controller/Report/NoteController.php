08560e22a40fb156f3639084ee0b7bd0c16f5259
Shaun Lizzio <shaun@lizzio.co.uk>
2017-06-14 15:19:28 +0100

    DDPB-797 moved assertion as part of method call

--- src/AppBundle/Controller/Report/NoteController.php
+++ src/AppBundle/Controller/Report/NoteController.php
@@ -24,13 +24,11 @@ class NoteController extends RestController
             ]
         );
 
-        $data = $this->deserializeBodyContent($request);
+        $data = $this->deserializeBodyContent($request, ['title' => 'mustExist']);
 
         $report = $this->findEntityBy(EntityDir\Report\Report::class, $reportId); /* @var $report EntityDir\Report\Report */
         $this->denyAccessIfReportDoesNotBelongToUser($report);
-        $this->validateArray($data, [
-            'title' => 'mustExist',
-        ]);
+
         $note = new EntityDir\Note($report, $data['category'], $data['title'], $data['content']);
 
         $note->setCreatedBy($this->getUser());
