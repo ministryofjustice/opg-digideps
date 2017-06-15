4d0bf675eca01c546f454e90f20d57ccc570a599
Elvis Ciotti <elvisciotti@gmail.com>
2017-06-15 16:28:58 +0200

    DDPB-800-edit-note /note/ID GET and PUT

--- src/AppBundle/Controller/Report/NoteController.php
+++ src/AppBundle/Controller/Report/NoteController.php
@@ -37,4 +37,57 @@ class NoteController extends RestController
 
         return ['id' => $note->getId()];
     }
+
+    /**
+     * @Route("/note/{id}")
+     * @Method({"GET"})
+     */
+    public function getOneById(Request $request, $id)
+    {
+        $this->denyAccessUnlessGranted(
+            [
+                EntityDir\User::ROLE_PA,
+                EntityDir\User::ROLE_PA_ADMIN,
+                EntityDir\User::ROLE_PA_TEAM_MEMBER
+            ]
+        );
+
+        $serialisedGroups = $request->query->has('groups')
+            ? (array) $request->query->get('groups') : ['notes'];
+        $this->setJmsSerialiserGroups($serialisedGroups);
+
+        $note = $this->findEntityBy(EntityDir\Note::class, $id); /* @var $note EntityDir\Note */
+        $this->denyAccessIfClientDoesNotBelongToUser($note->getClient());
+
+        return $note;
+    }
+
+    /**
+     * @Route("/note/{id}")
+     * @Method({"PUT"})
+     */
+    public function updateNote(Request $request, $id)
+    {
+        $this->denyAccessUnlessGranted(
+            [
+                EntityDir\User::ROLE_PA,
+                EntityDir\User::ROLE_PA_ADMIN,
+                EntityDir\User::ROLE_PA_TEAM_MEMBER
+            ]
+        );
+
+        $note = $this->findEntityBy(EntityDir\Note::class, $id); /* @var $note EntityDir\Note */
+        $this->denyAccessIfClientDoesNotBelongToUser($note->getClient());
+
+        $data = $this->deserializeBodyContent($request);
+        $this->hydrateEntityWithArrayData($note, $data, [
+            'category' => 'setCategory',
+            'title' => 'setTitle',
+            'content' => 'setContent',
+        ]);
+
+        $this->getEntityManager()->flush($note);
+
+        return $note;
+    }
 }
