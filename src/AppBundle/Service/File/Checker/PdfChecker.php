d11eab08958e29067657a636374afe9e1f4cb91a
Elvis Ciotti <elvisciotti@gmail.com>
2017-07-04 16:11:47 +0100

    DDPB-814-upload add sample PDF checker throwing exception

---
+++ src/AppBundle/Service/File/Checker/PdfChecker.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AppBundle\Service\File\Checker;
+
+
+use AppBundle\Service\File\Checker\Exception\RiskyFileException;
+
+class PdfChecker implements FileCheckerInterface
+{
+    public function checkFile($body)
+    {
+        throw new RiskyFileException("Found embedded JS in PDF");
+    }
+
+}
