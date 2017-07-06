99c5b851e186339b2f1ca1f7a8466ae51f16b425
Elvis Ciotti <elvisciotti@gmail.com>
2017-07-06 11:19:21 +0100

    DDPB-814-upload fix name

--- app/DoctrineMigrations/Version137.php
+++ app/DoctrineMigrations/Version137.php
@@ -18,7 +18,7 @@ class Version137 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('CREATE TABLE document (id SERIAL NOT NULL, report_id INT DEFAULT NULL, created_by INT DEFAULT NULL, local_filename VARCHAR(150) NOT NULL, uploaded_filename VARCHAR(150) DEFAULT NULL, storageReference VARCHAR(150) DEFAULT NULL, created_on TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE document (id SERIAL NOT NULL, report_id INT DEFAULT NULL, created_by INT DEFAULT NULL, local_filename VARCHAR(150) NOT NULL, uploaded_filename VARCHAR(150) DEFAULT NULL, storage_reference VARCHAR(150) DEFAULT NULL, created_on TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX ix_document_report_id ON document (report_id)');
         $this->addSql('CREATE INDEX ix_document_created_by ON document (created_by)');
         $this->addSql('ALTER TABLE document ADD CONSTRAINT FK_D8698A764BD2A4C0 FOREIGN KEY (report_id) REFERENCES report (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
