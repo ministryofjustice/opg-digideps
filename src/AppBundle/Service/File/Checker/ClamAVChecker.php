d2f6573201d5a818be5d418fef76c48fb92ddcc2
Shaun Lizzio <shaun@lizzio.co.uk>
2017-07-11 10:49:33 +0100

    DDPB-1409 tmp commit

--- src/AppBundle/Service/File/Checker/ClamAVChecker.php
+++ src/AppBundle/Service/File/Checker/ClamAVChecker.php
@@ -11,23 +11,48 @@ class ClamAVChecker implements FileCheckerInterface
     /**
      * ClamAv constructor.
      */
-    public function __construct(Client $client , LoggerInterface $logger, array $options = [])
+    public function __construct(Client $client, LoggerInterface $logger, array $options = [])
     {
         $this->client = $client;
         $this->logger = $logger;
         $this->options = [];
     }
 
+    /**
+     *
+     * @param $body
+     * @return bool
+     */
     public function checkFile($body)
     {
+        return true;
         // POST body to clamAV
         try {
-            $response = $this->client->post('upload', ['body' => $body]);
-            var_dump($response);
+            $response = $this->getScanResults($body);
+
+            if (strtoupper(trim($response['av_scan_result'])) === 'FAIL') {
+                throw new VirusFoundException('Found virus in file');
+            }
         } catch (\Exception $e) {
-var_dump($e->getMessage());
+
         }
-exit;
-        throw new VirusFoundException('Found virus in file');
+    }
+
+    /**
+     * POSTS the file body to file scanner, and continually polls until result is returned.
+     *
+     * @param $body
+     * @return array
+     */
+    private function getScanResults($body)
+    {
+        //$this->client->post('upload', ['body' => $body]);
+
+        return [
+            'celery_task_state' => 'SUCCESS',
+            'pdf_scan_result' => 'PASS',
+            'av_scan_result' => 'FAIL',
+            'file_scanner_result' => 'PASS'
+        ];
     }
 }
