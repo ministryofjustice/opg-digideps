4a838f2b3649e4bd1a9eef5f92a8f58c54adf21d
Elvis Ciotti <elvisciotti@gmail.com>
2017-07-13 17:32:16 +0200

    DDPB-1420 default params needed for install

--- app/config/env_parameters.php
+++ app/config/env_parameters.php
@@ -1,4 +1,5 @@
 <?php
-$container->setParameter('env', getenv('FRONTEND_ROLE'));
-$container->setParameter('api_client_secret', getenv('FRONTEND_API_CLIENT_SECRET'));
-$container->setParameter('redis_dsn', getenv('FRONTEND_SESSION_REDIS_DSN'));
+// default params needed for composer post-install scripts
+$container->setParameter('env', getenv('FRONTEND_ROLE') ?: null);
+$container->setParameter('api_client_secret', getenv('FRONTEND_API_CLIENT_SECRET') ?: null);
+$container->setParameter('redis_dsn', getenv('FRONTEND_SESSION_REDIS_DSN') ?: 'redis://localhost');
