a20e7367726d3177e931caa944d7ab6fb16baea1
Elvis Ciotti <elvisciotti@gmail.com>
2017-07-21 14:02:09 +0200

    DDPB-1395 change group to stop recursion

--- src/AppBundle/Entity/Client.php
+++ src/AppBundle/Entity/Client.php
@@ -41,7 +41,7 @@ class Client
     private $users;
 
     /**
-     * @JMS\Groups({"report"})
+     * @JMS\Groups({"client-report"})
      * @JMS\Type("array")
      * @ORM\OneToMany(targetEntity="AppBundle\Entity\Report\Report", mappedBy="client", cascade={"persist"})
      */
