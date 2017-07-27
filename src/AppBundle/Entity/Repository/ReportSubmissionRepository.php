7a787bb17069132e0f365cbecf8338e6af9ae234
Elvis Ciotti <elvisciotti@gmail.com>
2017-07-27 15:44:54 +0200

    report-submission disable soft delete filter to show user but remove deleted documents

--- src/AppBundle/Entity/Repository/ReportSubmissionRepository.php
+++ src/AppBundle/Entity/Repository/ReportSubmissionRepository.php
@@ -7,32 +7,49 @@ use AppBundle\Entity\Report\Fee as ReportFee;
 use AppBundle\Entity\Report\MoneyShortCategory as ReportMoneyShortCategory;
 use AppBundle\Entity\Report\Report;
 use AppBundle\Entity\Report\ReportSubmission;
+use AppBundle\Entity\User;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\EntityRepository;
 
 class ReportSubmissionRepository extends EntityRepository
 {
     /**
+     * deleted user are still returned
      * @param boolean $archived
      * @return ArrayCollection|ReportSubmission[]
      */
     public function getReportSubmissions($archived)
     {
+        $this->_em->getFilters()->disable('softdeleteable');
+
         $qb = $this->_em->getRepository(ReportSubmission::class)
             ->createQueryBuilder('rs')
             ->leftJoin('rs.report', 'r')
             ->leftJoin('rs.archivedBy', 'ab')
+            ->leftJoin('rs.createdBy', 'cb')
             ->leftJoin('r.client', 'c')
-            ->leftJoin('c.users', 'u')
             ->leftJoin('rs.documents', 'd')
             ->orderBy('rs.id', 'DESC');
 
         if ($archived) {
-            $qb->where('rs.archivedBy is not null' );
+            $qb->andWhere('rs.archivedBy is not null' );
         } else {
-            $qb->where('rs.archivedBy is null' );
+            $qb->andWhere('rs.archivedBy is null' );
         }
 
-        return $qb->getQuery()->getResult();
+        $results = $qb->getQuery()->getResult(); /* @var $results ReportSubmission[] */
+
+        // manually remove soft-deleted documents
+        foreach($results as $result) {
+            foreach($result->getDocuments() as $document) {
+                if ($document->isDeleted()) {
+                    $result->getDocuments()->removeElement($document);
+                }
+            }
+        }
+
+        $this->_em->getFilters()->enable('softdeleteable');
+
+        return $results;
     }
 }
