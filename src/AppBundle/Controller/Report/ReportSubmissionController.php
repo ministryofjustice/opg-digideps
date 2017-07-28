69a6aa0760aa32c7d064d937bb74c9f485c93500
Elvis Ciotti <elvisciotti@gmail.com>
2017-07-28 18:06:57 +0200

    DDPB-1398-search move logic to repository

--- src/AppBundle/Controller/Report/ReportSubmissionController.php
+++ src/AppBundle/Controller/Report/ReportSubmissionController.php
@@ -32,71 +32,18 @@ class ReportSubmissionController extends RestController
     {
         $this->denyAccessUnlessGranted([EntityDir\User::ROLE_DOCUMENT_MANAGE]);
 
-        $offset = $request->get('offset', 0);
-        $limit = $request->get('limit', 15);
-        $status = $request->get('status');
-
-        $qb = $this->getRepository(EntityDir\Report\ReportSubmission::class)->createQueryBuilder('rs')
-            ->leftJoin('rs.report', 'r')
-            ->leftJoin('rs.archivedBy', 'ab')
-            ->leftJoin('rs.createdBy', 'cb')
-            ->leftJoin('r.client', 'c')
-            ->leftJoin('rs.documents', 'd')
-            ->orderBy('rs.id', 'DESC');
-
-        // search filter
-        // similar to reportController::getAll() filter used by PA dashboard
-        $q = $request->get('q', false);
-        if ($q) {
-            $qb->andWhere(implode(' OR ', [
-                // user
-                'lower(cb.firstname) LIKE :qLike',
-                'lower(cb.lastname) LIKE :qLike',
-                // client names and case number (exact match)
-                'lower(c.firstname) LIKE :qLike',
-                'lower(c.lastname) LIKE :qLike',
-                // case number
-                'c.caseNumber = :q'
-            ]));
-            $qb->setParameter('qLike', '%' . strtolower($q) . '%');
-            $qb->setParameter('q', $q);
-        }
-
-        $createdByRole = $request->get('created_by_role');
-        if ($createdByRole) {
-            $qb->andWhere('cb.roleName = :roleName');
-            $qb->setParameter('roleName', $createdByRole);
-        }
-
-        $this->get('em')->getFilters()->disable('softdeleteable');
-        $records = $qb->getQuery()->getResult(); /* @var $records EntityDir\Report\ReportSubmission[] */
-        $this->get('em')->getFilters()->enable('softdeleteable');
-
-        // calculate total counts, filter based on status, then and apply last limit/offset
-        $counts = [
-            'new' => 0,
-            'archived' => 0,
-        ];
-        foreach ($records as $record) {
-            if ($record->getArchivedBy()) {
-                $counts['archived']++;
-            } else {
-                $counts['new']++;
-            }
-        }
-        $records = array_filter($records, function ($report) use ($status) {
-            return ($status === 'new')
-                ? ($report->getArchivedBy() === null)
-                : ($report->getArchivedBy() !== null);
-        });
-        $records = array_slice($records, $offset, $limit);
+        $ret = $this->getRepository(EntityDir\Report\ReportSubmission::class)
+            ->findByFiltersWithCounts(
+                $request->get('status'),
+                $request->get('q'),
+                $request->get('created_by_role'),
+                $request->get('offset', 0),
+                $request->get('limit', 15)
+            );
 
         $this->setJmsSerialiserGroups(self::$jmsGroups);
 
-        return [
-            'counts'=>$counts,
-            'records'=>$records
-        ];
+        return $ret;
     }
 
     /**
