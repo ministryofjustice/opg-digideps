c7360f360744e72323704fc01490e9dfff3b29bd
Elvis Ciotti <elvisciotti@gmail.com>
2017-08-01 13:57:02 +0200

    DDPB-1395-bugs better download logic

--- src/AppBundle/Controller/Admin/ReportSubmissionController.php
+++ src/AppBundle/Controller/Admin/ReportSubmissionController.php
@@ -50,52 +50,59 @@ class ReportSubmissionController extends AbstractController
      */
     public function downloadAction(Request $request, $reportSubmissionId)
     {
-        $reportSubmission = $this->getRestClient()->get("/report-submission/{$reportSubmissionId}", 'Report\\ReportSubmission');
+        try {
+            $reportSubmission = $this->getRestClient()->get("/report-submission/{$reportSubmissionId}", 'Report\\ReportSubmission');
 
-        // store files locally, for subsequent memory-less ZIP creation
-        $s3Storage = $this->get('s3_storage');
-        $filesToAdd = [];
-        if (empty($reportSubmission->getDocuments())) {
-            throw new \RuntimeException('No documents found for downloading');
+            // store files locally, for subsequent memory-less ZIP creation
+            $s3Storage = $this->get('s3_storage');
+            $filesToAdd = [];
+            if (empty($reportSubmission->getDocuments())) {
+                throw new \RuntimeException('No documents found for downloading');
+            }
+            foreach($reportSubmission->getDocuments() as $document) {
+                $content = $s3Storage->retrieve($document->getStorageReference()); //might throw exception
+                $dfile = '/tmp/DDDocument'.$document->getId().microtime(1);
+                file_put_contents($dfile, $content);
+                unset($content);
+                $filesToAdd[$document->getFileName()] = $dfile;
+            }
+
+            // create ZIP files and add previously-stored uploaded documents
+            $filename = '/tmp/'.$reportSubmission->getZipName();
+            $zip = new \ZipArchive();
+            $zip->open($filename, \ZipArchive::CREATE | \ZipArchive::OVERWRITE | \ZipArchive::CHECKCONS);
+            foreach($filesToAdd as $localname => $filePath) {
+                $zip->addFile($filePath, $localname); // addFromString crashes
+            }
+            $zip->close();
+            unset($zip);
+
+            // clean up
+            foreach($filesToAdd as $f) {
+                unlink($f);
+            }
+
+            // send ZIP to user
+            $response = new Response();
+            $response->headers->set('Pragma', 'public');
+            $response->headers->set('Cache-Control', 'must-revalidate, post-check=0, pre-check=0');
+            $response->headers->set('Expires' ,'0');
+            $response->headers->set('Content-type', 'application/octet-stream');
+            $response->headers->set('Content-Description', 'File Transfer');
+            $response->headers->set('Content-Disposition', 'attachment; filename="'.basename($filename).'";');
+            $response->headers->set('Content-Length', filesize($filename));
+            $response->sendHeaders();
+            $response->setContent(readfile($filename));
+
+            unlink($filename);
+
+            return $response;
+
+        } catch (\Exception $e) {
+            $request->getSession()->getFlashBag()->add('error', 'Cannot download documents. Details: ' . $e->getMessage());
+
+            return $this->redirectToRoute('admin_documents');
         }
-        foreach($reportSubmission->getDocuments() as $document) {
-            $content = $s3Storage->retrieve($document->getStorageReference()); //might throw exception
-            $dfile = '/tmp/DDDocument'.$document->getId().microtime(1);
-            file_put_contents($dfile, $content);
-            unset($content);
-            $filesToAdd[$document->getFileName()] = $dfile;
-        }
-
-        // create ZIP files and add previously-stored uploaded documents
-        $filename = '/tmp/'.$reportSubmission->getZipName();
-        $zip = new \ZipArchive();
-        $zip->open($filename, \ZipArchive::CREATE | \ZipArchive::OVERWRITE | \ZipArchive::CHECKCONS);
-        foreach($filesToAdd as $localname => $filePath) {
-            $zip->addFile($filePath, $localname); // addFromString crashes
-        }
-        $zip->close();
-        unset($zip);
-
-        // clean up
-        foreach($filesToAdd as $f) {
-            unlink($f);
-        }
-
-        // send ZIP to user
-        $response = new Response();
-        $response->headers->set('Pragma', 'public');
-        $response->headers->set('Cache-Control', 'must-revalidate, post-check=0, pre-check=0');
-        $response->headers->set('Expires' ,'0');
-        $response->headers->set('Content-type', 'application/octet-stream');
-        $response->headers->set('Content-Description', 'File Transfer');
-        $response->headers->set('Content-Disposition', 'attachment; filename="'.basename($filename).'";');
-        $response->headers->set('Content-Length', filesize($filename));
-        $response->sendHeaders();
-        $response->setContent(readfile($filename));
-
-        unlink($filename);
-
-        return $response;
     }
 
     /**
