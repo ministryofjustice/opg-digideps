72a4652a199ac0d6f9b63c56f5f84fd72fc41ea2
Elvis Ciotti <elvisciotti@gmail.com>
2017-08-17 17:28:37 +0200

    DDPB-1351 entity.createOn set null on delete

---
+++ app/DoctrineMigrations/Version144.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version144 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE report_submission DROP CONSTRAINT FK_C84776C8DE12AB56');
+        $this->addSql('ALTER TABLE report_submission ADD CONSTRAINT FK_C84776C8DE12AB56 FOREIGN KEY (created_by) REFERENCES dd_user (id) ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE document DROP CONSTRAINT FK_D8698A76DE12AB56');
+        $this->addSql('ALTER TABLE document ADD CONSTRAINT FK_D8698A76DE12AB56 FOREIGN KEY (created_by) REFERENCES dd_user (id) ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE note DROP CONSTRAINT FK_CFBDFA14DE12AB56');
+        $this->addSql('ALTER TABLE note ADD CONSTRAINT FK_CFBDFA14DE12AB56 FOREIGN KEY (created_by) REFERENCES dd_user (id) ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA digideps_unit_test');
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE report ALTER wish_to_provide_documentation TYPE VARCHAR(3)');
+        $this->addSql('ALTER TABLE note DROP CONSTRAINT fk_cfbdfa14de12ab56');
+        $this->addSql('ALTER TABLE note ALTER category DROP NOT NULL');
+        $this->addSql('ALTER TABLE note ALTER content DROP NOT NULL');
+        $this->addSql('ALTER TABLE note ADD CONSTRAINT fk_cfbdfa14de12ab56 FOREIGN KEY (created_by) REFERENCES dd_user (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE report_submission DROP CONSTRAINT fk_c84776c8de12ab56');
+        $this->addSql('ALTER TABLE report_submission ADD CONSTRAINT fk_c84776c8de12ab56 FOREIGN KEY (created_by) REFERENCES dd_user (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE document DROP CONSTRAINT fk_d8698a76de12ab56');
+        $this->addSql('ALTER TABLE document ADD CONSTRAINT fk_d8698a76de12ab56 FOREIGN KEY (created_by) REFERENCES dd_user (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+}
