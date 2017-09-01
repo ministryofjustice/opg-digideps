29d496b96f80601e6d4a0c5594f0701c8d542884
Shaun Lizzio <shaun@lizzio.co.uk>
2017-09-01 08:24:40 +0100

    DDPB-940 Revised migration script. Added controller for endpoints to update lifestyle entity

--- app/DoctrineMigrations/Version145.php
+++ app/DoctrineMigrations/Version145.php
@@ -18,7 +18,7 @@ class Version145 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('CREATE TABLE lifestyle (id SERIAL NOT NULL, report_id INT DEFAULT NULL, care_appointments VARCHAR(255) DEFAULT NULL, does_client_undertake_social_activities VARCHAR(4) NOT NULL, activity_details VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE lifestyle (id SERIAL NOT NULL, report_id INT DEFAULT NULL, care_appointments TEXT DEFAULT NULL, does_client_undertake_social_activities VARCHAR(4) DEFAULT NULL, activity_details TEXT DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX ix_lifestyle_report_id ON document (report_id)');
         $this->addSql('CREATE INDEX ix_lifestyle_created_by ON document (created_by)');
         $this->addSql('ALTER TABLE lifestyle ADD CONSTRAINT FK_D8ABFC764BD2A4C00 FOREIGN KEY (report_id) REFERENCES report (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
