6fbad7d68cb3d919466d5fe0690230d4b15d4150
Jack Goodby <jack.goodby@digital.justice.gov.uk>
2017-09-14 13:31:46 +0100

    DDPB-1510 send users details over with a case, not just user ids

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -54,7 +54,7 @@ class User implements UserInterface
 
     /**
      * @JMS\Type("ArrayCollection<AppBundle\Entity\Team>")
-     * @JMS\Groups({ "team"})
+     * @JMS\Groups({"team"})
      * @ORM\ManyToMany(targetEntity="AppBundle\Entity\Team", inversedBy="members", cascade={"persist"})
      *
      * @var ArrayCollection
