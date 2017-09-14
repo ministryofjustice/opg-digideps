828b9f44cb1efb53b8383b55e890fa9946dd0b4c
Shaun Lizzio <shaun@lizzio.co.uk>
2017-09-14 14:29:30 +0100

    DDPB-1507 Added Endpoint to add co deputy

---
+++ src/AppBundle/Controller/CoDeputyController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity as EntityDir;
+use JMS\Serializer\Exception\RuntimeException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+
+/**
+ * @Route("/codeputy/")
+ */
+class CoDeputyController extends RestController
+{
+
+    /**
+     * @Route("add")
+     * @Method({"POST"})
+     */
+    public function add(Request $request)
+    {
+        $this->denyAccessUnlessGranted([EntityDir\User::ROLE_DEPUTY]);
+
+        $data = $this->deserializeBodyContent($request, [
+            'email' => 'notEmpty',
+        ]);
+
+        $loggedInUser = $this->getUser();
+        $user = new EntityDir\User();
+        $newUser = clone $loggedInUser;
+
+        $newUser->setFirstname('');
+        $newUser->setLastname('');
+        $newUser->setEmail($data['email']);
+
+        $userService = $this->get('opg_digideps.user_service');
+
+        $userService->addUser($loggedInUser, $newUser, $data);
+
+        $groups = $request->query->has('groups') ?
+            $request->query->get('groups') : ['user'];
+        $this->setJmsSerialiserGroups($groups);
+
+        return $user;
+    }
+}
