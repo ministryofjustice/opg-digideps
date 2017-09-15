298be1e960c69c5b48f24e5feec48c5e303fb0c5
Elvis Ciotti <elvisciotti@gmail.com>
2017-09-15 12:05:20 +0200

    DDPB-1053-joint-report-support clean up

--- src/AppBundle/Controller/BehatController.php
+++ src/AppBundle/Controller/BehatController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\AuditLogEntry;
+use AppBundle\Entity\Client;
 use AppBundle\Entity\Report\Report;
 use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -10,6 +11,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
+ * Controller only used from BEHAT
  * @codeCoverageIgnore
  * @Route("/behat")
  */
@@ -20,6 +22,26 @@ class BehatController extends RestController
         if (!$this->container->getParameter('behat_controller_enabled')) {
             return $this->createNotFoundException('Behat endpoint disabled, check the behat_controller_enabled parameter');
         }
+    }
+
+    /**
+     * @Route("/client/{caseNumber}")
+     * @Method({"PUT"})
+     */
+    public function clientEditAction(Request $request, $caseNumber)
+    {
+        $this->securityChecks();
+
+        /* @var $client Client */
+        $client = $this->findEntityBy(Client::class, ['caseNumber' => $caseNumber]);
+
+        $data = $this->deserializeBodyContent($request);
+        if (array_key_exists('current_report_type', $data)) {
+            $report = $client->getCurrentReport();
+            $report->setType($data['current_report_type']);
+            $this->get('em')->flush($report);
+        }
+
     }
 
     /**
@@ -56,22 +78,22 @@ class BehatController extends RestController
      * @Route("/check-app-params")
      * @Method({"GET"})
      */
-    public function checkParamsAction()
-    {
-        $this->securityChecks();
-
-        $param = $this->container->getParameter('email_report_submit')['to_email'];
-        if (!preg_match('/^behat\-/', $param)) {
-            throw new DisplayableException("email_report_submit.to_email must be a behat- email in order to test emails, $param given.");
-        }
-
-        $param = $this->container->getParameter('email_feedback_send')['to_email'];
-        if (!preg_match('/^behat\-/', $param)) {
-            throw new DisplayableException("email_feedback_send.to_email must be a behat- email in order to test emails, $param given.");
-        }
-
-        return 'valid';
-    }
+//    public function checkParamsAction()
+//    {
+//        $this->securityChecks();
+//
+//        $param = $this->container->getParameter('email_report_submit')['to_email'];
+//        if (!preg_match('/^behat\-/', $param)) {
+//            throw new DisplayableException("email_report_submit.to_email must be a behat- email in order to test emails, $param given.");
+//        }
+//
+//        $param = $this->container->getParameter('email_feedback_send')['to_email'];
+//        if (!preg_match('/^behat\-/', $param)) {
+//            throw new DisplayableException("email_feedback_send.to_email must be a behat- email in order to test emails, $param given.");
+//        }
+//
+//        return 'valid';
+//    }
 
     /**
      * @Route("/audit-log")
