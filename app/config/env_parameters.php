2398dedef85b66ee632b4d44a1fd56321d610307
Elvis Ciotti <elvisciotti@gmail.com>
2017-09-21 16:01:12 +0100

    fail if ENV vars missing

--- app/config/env_parameters.php
+++ app/config/env_parameters.php
@@ -1,5 +1,13 @@
 <?php
 // default params needed for composer post-install scripts
-$container->setParameter('env', getenv('FRONTEND_ROLE') ?: null);
-$container->setParameter('api_client_secret', getenv('FRONTEND_API_CLIENT_SECRET') ?: null);
-$container->setParameter('redis_dsn', getenv('FRONTEND_SESSION_REDIS_DSN') ?: 'redis://localhost');
+$container->setParameter('env', getenv('FRONTEND_ROLE') ?: 'FRONTEND_ROLE var missing');
+
+if (empty(getenv('FRONTEND_API_CLIENT_SECRET'))) {
+    throw new RuntimeException('missing FRONTEND_API_CLIENT_SECRET variable');
+}
+$container->setParameter('api_client_secret', getenv('FRONTEND_API_CLIENT_SECRET'));
+
+if (empty(getenv('FRONTEND_SESSION_REDIS_DSN'))) {
+    throw new RuntimeException('missing FRONTEND_SESSION_REDIS_DSN variable');
+}
+$container->setParameter('redis_dsn', getenv('FRONTEND_SESSION_REDIS_DSN'));
