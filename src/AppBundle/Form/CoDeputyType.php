b04c54cec2f1840e9b55b7a92c50dbc02de6a49e
Jack Goodby <jack.goodby@digital.justice.gov.uk>
2017-09-21 15:48:11 +0100

    DDPB-1526 - Codep invite form doesn’t need client dependency

--- src/AppBundle/Form/CoDeputyType.php
+++ src/AppBundle/Form/CoDeputyType.php
@@ -2,26 +2,14 @@
 
 namespace AppBundle\Form;
 
-use AppBundle\Entity\Client;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class CoDeputyType extends AbstractType
 {
-    /**
-     * @var Client
-     */
-    private $client;
-
-    public function __construct(Client $client)
-    {
-        $this->client = $client;
-    }
-
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-
         $builder->add('email', 'text')
                 ->add('submit', 'submit');
     }
