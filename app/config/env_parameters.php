6613c3d8a7696aa390a5cdf330e85cfadc3cdad0
Elvis Ciotti <elvisciotti@gmail.com>
2017-09-22 11:22:19 +0200

    restore default env params

--- app/config/env_parameters.php
+++ app/config/env_parameters.php
@@ -1,13 +1,7 @@
 <?php
 // default params needed for composer post-install scripts
-$container->setParameter('env', getenv('FRONTEND_ROLE') ?: 'FRONTEND_ROLE var missing');
+$container->setParameter('env', getenv('FRONTEND_ROLE') ?: 'FRONTEND_ROLE.default');
 
-if (empty(getenv('FRONTEND_API_CLIENT_SECRET'))) {
-    throw new RuntimeException('missing FRONTEND_API_CLIENT_SECRET variable');
-}
-$container->setParameter('api_client_secret', getenv('FRONTEND_API_CLIENT_SECRET'));
+$container->setParameter('api_client_secret', getenv('FRONTEND_API_CLIENT_SECRET') ?: 'FRONTEND_API_CLIENT_SECRET.default');
 
-if (empty(getenv('FRONTEND_SESSION_REDIS_DSN'))) {
-    throw new RuntimeException('missing FRONTEND_SESSION_REDIS_DSN variable');
-}
-$container->setParameter('redis_dsn', getenv('FRONTEND_SESSION_REDIS_DSN'));
+$container->setParameter('redis_dsn', getenv('FRONTEND_SESSION_REDIS_DSN') ?: 'redis://FRONTEND_SESSION_REDIS_DSN.default');
