8eabd4c71b07fcc896e9714e77bcaca91980cbd0
Elvis Ciotti <elvisciotti@gmail.com>
2017-10-05 11:30:16 +0100

    DDPB-1645 clean data command

---
+++ src/AppBundle/Command/CleanDataCommand.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace AppBundle\Command;
+
+use AppBundle\Entity\Client;
+use AppBundle\Entity\Report\Report;
+use AppBundle\Service\FixDataService;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+/**
+ * Add data that wasn't added with listeners
+ * Firstly wrote when data wasn't added with temporary 103 user on staging
+ *
+ * @codeCoverageIgnore
+ */
+class CleanDataCommand extends ContainerAwareCommand
+{
+    use ContainerAwareTrait;
+
+    protected function configure()
+    {
+        $this
+            ->setName('digideps:clean-data')
+            ->setDescription('delete unassigned and duplicate reports');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $rs = $this->getContainer()->get('opg_digideps.report_service');
+        $em = $this->getContainer()->get('em'); /* @var $em \Doctrine\ORM\EntityManager */
+
+        // clean up duplicates
+        $deleted = 0;
+        $total = 0;
+        $offset = 0;
+        $limit = 250;
+        do {
+            $clients = $em->getRepository(Client::class)->findBy([], ['id'=>'ASC'], $limit, $offset);
+            foreach($clients as $c) {
+                // cleanup reports
+                $unsubmittedReports = $c->getUnsubmittedReports();
+                $total += (count($unsubmittedReports) - 1);
+                if (count($unsubmittedReports) > 1) {
+                    if ($deleteableReports = $rs->findDeleteableReports($unsubmittedReports)) {
+                        foreach($deleteableReports as $deleteableReport) {
+                            $rs->deleteRecursive($deleteableReport);
+                            $output->writeln("deleted ".$deleteableReport->getId()." (duplicate)");
+                            $deleted++;
+                        }
+                    }
+                }
+            }
+            $offset += $limit;
+        } while(!empty($clients));
+
+
+        $output->writeln("deleted $deleted / $total ");
+    }
+
+}
