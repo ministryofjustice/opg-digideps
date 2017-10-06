738fced776c2ac2ace23ed6b37b3fa20dcd76555
Elvis Ciotti <elvisciotti@gmail.com>
2017-10-06 17:12:20 +0200

    DDPB-1645 fix script

--- src/AppBundle/Command/CleanDataCommand.php
+++ src/AppBundle/Command/CleanDataCommand.php
@@ -30,10 +30,13 @@ class CleanDataCommand extends ContainerAwareCommand
         $rs = $this->getContainer()->get('opg_digideps.report_service');
         $em = $this->getContainer()->get('em'); /* @var $em \Doctrine\ORM\EntityManager */
 
-        // clean up duplicates reports. Find them in chuncks
+        // find data to remove
         $offset = 0;
-        $limit = 250;
+        $limit = 50;
+        $clientsToRemove = [];
+        $reportsToRemove = [];
         do {
+            $output->write('.');
             $clients = $em->getRepository(Client::class)->findBy([], ['id'=>'ASC'], $limit, $offset);
             foreach($clients as $c) {
 
@@ -41,27 +44,38 @@ class CleanDataCommand extends ContainerAwareCommand
                  * delete client without users. Recursively (so reports will be deleted too)
                  */
                 if (count($c->getUsers()) === 0) {
-                    $output->writeln("deleting client ".$c->getId()." and related reports (reason: no users)");
-                    $em->remove($c);
-                    $em->flush($c);
-                }
-
-                /**
-                 * cleanup reports
-                 */
-                $unsubmittedReports = $c->getUnsubmittedReports();
-                if (count($unsubmittedReports) > 1) {
-                    if ($deleteableReports = $rs->findDeleteableReports($unsubmittedReports)) {
-                        foreach($deleteableReports as $deleteableReport) {
-                            $output->writeln("deleting report ".$deleteableReport->getId()." (reason: duplicate)");
-                            $em->remove($deleteableReport);
-                            $em->flush($deleteableReport);
+                    $output->writeln("client ".$c->getId()." and related reports flagged for deletion (reason: no users)");
+                    $clientsToRemove[] = $c;
+                } else {
+                    /**
+                     * cleanup reports
+                     */
+                    $unsubmittedReports = $c->getUnsubmittedReports();
+                    if (count($unsubmittedReports) > 1) {
+                        if ($deleteableReports = $rs->findDeleteableReports($unsubmittedReports)) {
+                            foreach($deleteableReports as $deleteableReport) {
+                                $output->writeln("report ".$deleteableReport->getId()." flagged for deletion (reason: duplicate)");
+                                $reportsToRemove[] = $deleteableReport;
+                            }
                         }
                     }
                 }
+
+
             }
             $offset += $limit;
         } while(!empty($clients));
+
+        $output->write("Performing deletion...");
+        // remove !
+        foreach($clientsToRemove as $c) {
+            $em->remove($c);
+            $em->flush($c);
+        }
+        foreach($reportsToRemove as $r) {
+            $em->remove($r);
+            $em->flush($r);
+        }
 
         $output->writeln("Done");
     }
