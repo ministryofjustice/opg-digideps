0649078d3345429668acd2e0c529e5d91f9d1d9c
Elvis Ciotti <elvisciotti@gmail.com>
2017-10-09 13:37:18 +0200

    DDPB-1645 compatible migration with unit test from 144

--- app/DoctrineMigrations/Version153.php
+++ app/DoctrineMigrations/Version153.php
@@ -18,11 +18,11 @@ class Version153 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('ALTER TABLE account DROP opening_date');
-        $this->addSql('ALTER TABLE account DROP closing_date');
-        $this->addSql('ALTER TABLE account DROP opening_date_explanation');
-        $this->addSql('ALTER TABLE account DROP closing_balance_explanation');
-        $this->addSql('ALTER TABLE account DROP closing_date_explanation');
+        $this->addSql('ALTER TABLE account DROP COLUMN IF EXISTS opening_date');
+        $this->addSql('ALTER TABLE account DROP COLUMN IF EXISTS closing_date');
+        $this->addSql('ALTER TABLE account DROP COLUMN IF EXISTS opening_date_explanation');
+        $this->addSql('ALTER TABLE account DROP COLUMN IF EXISTS closing_balance_explanation');
+        $this->addSql('ALTER TABLE account DROP COLUMN IF EXISTS closing_date_explanation');
     }
 
     /**
--- app/DoctrineMigrations/Version154.php
+++ app/DoctrineMigrations/Version154.php
@@ -18,8 +18,10 @@ class Version154 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('ALTER TABLE note DROP CONSTRAINT fk_cfbdfa144bd2a4c0');
-        $this->addSql('ALTER TABLE note DROP CONSTRAINT FK_CFBDFA1465CF370E');
+        $this->addSql('ALTER TABLE note DROP CONSTRAINT IF EXISTS fk_cfbdfa144bd2a4c0');
+        $this->addSql('ALTER TABLE note DROP CONSTRAINT IF EXISTS FK_CFBDFA1465CF370E');
+        $this->addSql('ALTER TABLE note DROP CONSTRAINT IF EXISTS FK_CFBDFA1419EB6921');
+        $this->addSql('ALTER TABLE note DROP CONSTRAINT IF EXISTS FK_CFBDFA1465CF370E');
         $this->addSql('ALTER TABLE note ADD CONSTRAINT FK_CFBDFA1419EB6921 FOREIGN KEY (client_id) REFERENCES client (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE note ADD CONSTRAINT FK_CFBDFA1465CF370E FOREIGN KEY (last_modified_by) REFERENCES dd_user (id) ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE client_contact DROP CONSTRAINT FK_1E5FA245DE12AB56');
