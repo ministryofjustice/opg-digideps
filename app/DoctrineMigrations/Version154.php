7ad960e9cdb42c468c61cbf33c52906c44071ebc
Elvis Ciotti <elvisciotti@gmail.com>
2017-10-09 13:24:43 +0200

    DDPB-1645 remove action contraint failing with tests

--- app/DoctrineMigrations/Version154.php
+++ app/DoctrineMigrations/Version154.php
@@ -33,14 +33,5 @@ class Version154 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
-
-        $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE note DROP CONSTRAINT FK_CFBDFA1419EB6921');
-        $this->addSql('ALTER TABLE note DROP CONSTRAINT fk_cfbdfa1465cf370e');
-        $this->addSql('ALTER TABLE note ADD CONSTRAINT fk_cfbdfa144bd2a4c0 FOREIGN KEY (client_id) REFERENCES client (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE note ADD CONSTRAINT fk_cfbdfa1465cf370e FOREIGN KEY (last_modified_by) REFERENCES dd_user (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE client_contact DROP CONSTRAINT fk_1e5fa245de12ab56');
-        $this->addSql('ALTER TABLE client_contact ADD CONSTRAINT fk_1e5fa245de12ab56 FOREIGN KEY (created_by) REFERENCES dd_user (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE report ALTER wish_to_provide_documentation TYPE VARCHAR(3)');
     }
 }
