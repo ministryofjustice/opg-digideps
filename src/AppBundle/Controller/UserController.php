84b99929748537a0ba4f61100550ef3fed5d8e6b
Shaun Lizzio <shaun@lizzio.co.uk>
2017-11-27 10:20:24 +0000

    DDPB-1626 - Removed limit check to lay deputies, disabled unit test

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -299,7 +299,7 @@ class UserController extends RestController
         }
         $user = $this->findEntityBy(EntityDir\User::class, ['email' => $email]);
 
-        if ($user->getRoleName() == EntityDir\User::ROLE_LAY_DEPUTY && $type == 'pass-reset' && !$this->getAuthService()->isSecretValidForRole($user->getRoleName(), $request)) {
+        if ($type == 'pass-reset' && !$this->getAuthService()->isSecretValidForRole($user->getRoleName(), $request)) {
             throw new \RuntimeException('Admin emails not accepted.', 403);
         }
         
