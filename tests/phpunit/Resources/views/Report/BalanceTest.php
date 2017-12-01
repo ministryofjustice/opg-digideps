1e291e6770312c6ae31f71be8abc83e6822048f9
Elvis Ciotti <elvisciotti@gmail.com>
2017-12-01 17:44:25 +0100

    DDPB-1758-balance-section unit test

--- tests/phpunit/Resources/views/Report/BalanceTest.php
+++ tests/phpunit/Resources/views/Report/BalanceTest.php
@@ -4,22 +4,16 @@ namespace AppBundle\Resources\views\Report;
 
 use AppBundle\Entity\Client;
 use AppBundle\Entity\Report\Account;
-use AppBundle\Entity\Report\Action;
-use AppBundle\Entity\Report\AssetOther;
-use AppBundle\Entity\Report\AssetProperty;
-use AppBundle\Entity\Report\BankAccount;
-use AppBundle\Entity\Report\Debt;
-use AppBundle\Entity\Report\Decision;
-use AppBundle\Entity\Report\MoneyTransaction;
-use AppBundle\Entity\Report\MoneyTransfer;
 use AppBundle\Entity\Report\Report as Report;
 use AppBundle\Entity\Report\Status;
 use AppBundle\Entity\User;
-use Mockery as m;
+use AppBundle\Form\Report\ReasonForBalanceType;
+use MockeryStub as ms;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\Form\Form;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
 use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Symfony\Component\HttpFoundation\Session\Session;
 
@@ -48,149 +42,169 @@ class BalanceTest extends WebTestCase
         $this->container->set('request', $request, 'request');
         $this->twig = $this->frameworkBundleClient->getContainer()->get('templating');
         $this->container->get('request_stack')->push(Request::createFromGlobals());
+    }
 
-
-        $this->client = m::mock(Client::class)
+    /**
+     * @param array $methods
+     * @return Report
+     */
+    private function getMockedReport(array $methods)
+    {
+        $client = ms::stub(Client::class)
             ->shouldReceive('getFirstname')->andReturn('Peter')
             ->getMock();
 
-        $this->report = m::mock(Report::class)
-            ->shouldReceive('hasSection')->andReturn(true)
-            ->shouldReceive('getId')->andReturn(1)
-            ->shouldReceive('getType')->andReturn(Report::TYPE_102)
-            ->shouldReceive('getAvailableSections')->andReturn([//102
-                'decisions', 'contacts','visitsCare',
-                'lifestyle','balance','bankAccounts',
-                'moneyTransfers',
-                'moneyIn', 'moneyOut',
-                'moneyInShort', 'moneyOutShort',
-                'assets', 'debts', 'gifts',
-                'actions', 'otherInfo', 'deputyExpenses',
-                'paDeputyExpenses', 'documents'
-            ])
-            ->shouldReceive('totalsOffset')->andReturn(0)
-            ->shouldReceive('getClient')->andReturn($this->client)
-            ->getMock()
-        ;
-
-        $this->reportStatus = m::mock(Status::class);
-
+        return ms::mock(Report::class, $methods + [
+            'getId' => 1,
+            'getClient' => $client,
+            'getTotalsOffset' => null,
+            'getMoneyInTotal' => 1,
+            'getMoneyOutTotal' => 2,
+            'getGiftsTotalValue' => 3,
+            'getCalculatedBalance' => 999,
+            'getAccountsClosingBalanceTotal' => 100,
+//            'hasSection(deputyExpenses)' => true,
+//            'hasSection(paDeputyExpenses)' => true,
+//            'hasSection(decisions)' => true,
+//            'hasSection(contacts)' => true,
+//            'hasSection(visitsCare)' => true,
+            'hasSection' => true, // tested with all the section, and potentally any report type
+            'hasSection' => true,
+            'getFeesTotal' => null,
+            'getExpensesTotal' => null,
+            'getType' => null, // irrelevant
+            'getEndDate' => new \DateTime(),
+            'getBalanceMismatchExplanation'=>'explanation-content'
+        ]);
     }
 
-    public function testNotReadyToBalance()
+    /**
+     * @param array $methods
+     * @return Status
+     */
+    private function getMockedStatus(array $methods)
     {
-        $this->report->shouldReceive('getTotalsOffset')->andReturn(0);
-        $this->report->shouldReceive('isDue')->andReturn(false);
+        return ms::stub(Status::class, $methods + [
+            'getBankAccountsState' => ['state'=>'not-started'],
+            'getMoneyInState' => ['state'=>'not-started'],
+            'getMoneyOutState' => ['state'=>'not-started'],
+            'getExpensesState' => ['state'=>'not-started'],
+            'getPaFeesExpensesState' => ['state'=>'not-started'],
+            'getGiftsState' => ['state'=>'not-started'],
 
-        $this->reportStatus->shouldReceive(
-            'getGiftsState',
-            'getExpensesState',
-            'getBankAccountsState',
-            'getMoneyInState',
-            'getMoneyOutState'
-        )->andReturn(['state'=>'not-started']);
+        ]);
+    }
 
-        $crawler = $this->renderTemplateAndGetCrawler();
+
+    public function testNotReadyNotDue()
+    {
+        $crawler = $this->renderTemplateAndGetCrawler(
+            $this->getMockedReport(['isDue'=>false]),
+            $this->getMockedStatus([
+                'getBalanceState' => ['state'=>'not-started']
+            ])
+        );
 
         $this->assertCount(1, $crawler->filter('#alert-not-started'));
         $this->assertCount(0, $crawler->filter('#alert-balanced'));
         $this->assertCount(0, $crawler->filter('#alert-not-balanced'));
+        $this->assertCount(0, $crawler->filter('#calculated-balance-foot'));
+        $this->assertCount(0, $crawler->filter('#calculated-balance-table'));
     }
 
-    public function testReadyToBalanceTotalMatch()
+    public function testReadyNotBalanced()
     {
-        $this->report
-            ->shouldReceive('isDue')->andReturn(false)
-            ->shouldReceive('getTotalsOffset')->andReturn(0)
-            ->shouldReceive('getStartDate')->andReturn(new \DateTime('2017-12-31'))
-            ->shouldReceive('getEndDate')->andReturn(new \DateTime('2018-12-31'))
-            ->shouldReceive('getMoneyInTotal')->andReturn(100)
-            ->shouldReceive('getMoneyOutTotal')->andReturn(100)
-            ->shouldReceive('getExpensesTotal')->andReturn(100)
-            ->shouldReceive('getGiftsTotalValue')->andReturn(100)
-            ->shouldReceive('getAccountsClosingBalanceTotal')->andReturn(100)
-            ->shouldReceive('getAccountsOpeningBalanceTotal')->andReturn(100)
-            ->shouldReceive('isTotalsMatch')->andReturn(true);
+        $crawler = $this->renderTemplateAndGetCrawler(
+            $this->getMockedReport([
+                'isDue'=>false,
+                'isTotalsMatch'=>false
+            ]),
+            $this->getMockedStatus([
+                'getBalanceState' => ['state'=>'not-matching']
+            ])
+        );
 
-        $this->reportStatus->shouldReceive(
-            'getGiftsState',
-            'getExpensesState',
-            'getBankAccountsState',
-            'getMoneyInState',
-            'getMoneyOutState'
-        )->andReturn(['state'=>'done']);
-
-        $crawler = $this->renderTemplateAndGetCrawler();
-
-        $this->assertCount(0, $crawler->filter('#alert-not-started'));
-        $this->assertCount(1, $crawler->filter('#alert-balanced'));
-        $this->assertCount(0, $crawler->filter('#alert-not-balanced'));
-
-        //TODO assert more here
-    }
-
-    public function testReadyToBalanceTotalNotMatch()
-    {
-        $this->report
-            ->shouldReceive('isDue')->andReturn(false)
-            ->shouldReceive('getTotalsOffset')->andReturn(0)
-            ->shouldReceive('getStartDate')->andReturn(new \DateTime('2017-12-31'))
-            ->shouldReceive('getEndDate')->andReturn(new \DateTime('2018-12-31'))
-            ->shouldReceive('getMoneyInTotal')->andReturn(100)
-            ->shouldReceive('getMoneyOutTotal')->andReturn(100)
-            ->shouldReceive('getExpensesTotal')->andReturn(100)
-            ->shouldReceive('getGiftsTotalValue')->andReturn(100)
-            ->shouldReceive('getAccountsClosingBalanceTotal')->andReturn(100)
-            ->shouldReceive('getAccountsOpeningBalanceTotal')->andReturn(100)
-            ->shouldReceive('isTotalsMatch')->andReturn(false);
-
-        $this->reportStatus->shouldReceive(
-            'getGiftsState',
-            'getExpensesState',
-            'getBankAccountsState',
-            'getMoneyInState',
-            'getMoneyOutState'
-        )->andReturn(['state'=>'done']);
-
-        $crawler = $this->renderTemplateAndGetCrawler();
+        $html = $crawler->html();
 
         $this->assertCount(0, $crawler->filter('#alert-not-started'));
         $this->assertCount(0, $crawler->filter('#alert-balanced'));
         $this->assertCount(1, $crawler->filter('#alert-not-balanced'));
 
-
-        //TODO assert more here
+        $this->assertCount(1, $crawler->filter('#calculated_balance_foot'));
+        $this->assertCount(1, $crawler->filter('#calculated_balance_table'));
     }
 
+    public function testReadyBalanced()
+    {
+        $crawler = $this->renderTemplateAndGetCrawler(
+            $this->getMockedReport([
+                'isDue'=>false,
+                'isTotalsMatch'=>true
+            ]),
+            $this->getMockedStatus([
+                'getBalanceState' => ['state'=>'not-matching']
+            ])
+        );
+
+        $html = $crawler->html();
+
+        $this->assertCount(0, $crawler->filter('#alert-not-started'));
+        $this->assertCount(1, $crawler->filter('#alert-balanced'));
+        $this->assertCount(0, $crawler->filter('#alert-not-balanced'));
+
+        $this->assertCount(1, $crawler->filter('#calculated_balance_foot'));
+        $this->assertCount(1, $crawler->filter('#calculated_balance_table'));
+    }
+
+    public static function explanationFormProvider()
+    {
+        return [
+            [true, true, false, 1],
+            [false, true, false, 0],
+            [true, false, false, 0],
+            [true, true, true, 0],
+        ];
+    }
 
     /**
-     * Get 'app' var for template rendering
-     * //TODO move to outer class if needed elsewhere
+     * @dataProvider  explanationFormProvider
      */
-    private static function getTemplateAppMocked()
+    public function testExplanationForm($isDue, $readyBalance, $isTotalMatch, $expectedForm)
     {
-        $emptyFlashBag = m::mock(FlashBagInterface::class)->shouldIgnoreMissing();
-        $emptySession = m::mock(Session::class)
-            ->shouldReceive('get')->andReturn(false)
-            ->shouldReceive('getFlashBag')->andReturn($emptyFlashBag)
-            ->getMock();
-
-        return [
-            'session' => $emptySession,
-            'user' => m::mock(User::class)->shouldIgnoreMissing()
-        ];
+        $crawler = $this->renderTemplateAndGetCrawler(
+            $this->getMockedReport([
+                'isDue'=>$isDue,
+                'isTotalsMatch'=>$isTotalMatch
+            ]),
+            $this->getMockedStatus([
+                'getBalanceState' => ['state'=>$readyBalance ? 'not-matching' : 'not-started']
+            ])
+        );
+        $this->assertCount($expectedForm, $crawler->filter('#cantFindTheProblem'));
+        $this->assertCount($expectedForm, $crawler->filter('#balance_balanceMismatchExplanation'));
     }
 
     /**
      * @return Crawler
      */
-    private function renderTemplateAndGetCrawler()
+    private function renderTemplateAndGetCrawler(Report $report, Status $status)
     {
+        $form = $this->container->get('form.factory')->create(ReasonForBalanceType::class, $report);
+
         $html = $this->twig->render('AppBundle:Report/Balance:balance.html.twig', [
-            'report' => $this->report,
-            'reportStatus' => $this->reportStatus,
+            'report' => $report,
+            'reportStatus' => $status,
+            'form' => $form->createView(),
             'backLink' => '[backLinkUrl]',
-            'app'=> self::getTemplateAppMocked()
+            'app'=> [
+                'session' => ms::mock(Session::class)
+                    ->shouldReceive('get')->andReturn(false)
+                    ->shouldReceive('getFlashBag')->andReturn(
+                        ms::mock(FlashBagInterface::class)->shouldIgnoreMissing()
+                    )
+                    ->getMock(),
+                'user' => ms::mock(User::class)->shouldIgnoreMissing()
+            ]
         ]);
 
         return new Crawler($html);
@@ -200,7 +214,7 @@ class BalanceTest extends WebTestCase
 
     public function tearDown()
     {
-        m::close();
+        ms::close();
         $this->container->leaveScope('request');
         unset($this->frameworkBundleClient);
     }
