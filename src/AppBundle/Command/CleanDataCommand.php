d7faeff485be5d645d44a5a984c1b3a84be8a2c3
Elvis Ciotti <elvisciotti@gmail.com>
2017-12-21 14:49:51 +0100

    DDPB-1798 digideps:clean-data command to add missing deputyNo

--- src/AppBundle/Command/CleanDataCommand.php
+++ src/AppBundle/Command/CleanDataCommand.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Command;
 
 use AppBundle\Entity\Client;
+use AppBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -27,54 +28,50 @@ class CleanDataCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $rs = $this->getContainer()->get('opg_digideps.report_service');
         $em = $this->getContainer()->get('em'); /* @var $em \Doctrine\ORM\EntityManager */
+        $vs = $this->getContainer()->get('opg_digideps.casrec_verification_service');
 
-        // find data to remove
-        $offset = 0;
-        $limit = 50;
-        $clientsToRemove = [];
-        $reportsToRemove = [];
-        do {
-            $output->write('.');
-            $clients = $em->getRepository(Client::class)->findBy([], ['id'=>'ASC'], $limit, $offset);
-            foreach ($clients as $c) {
+        /**
+         *
+         * add DeputyNo to LAY users that skipped the self-registration process.
+         * DeputyNo is needed for stats
+         *
+         */
+        /** @var $user User */
+        $fixed = 0;
+        $mismatch = 0;
+        $clientNotCreated = 0;
 
-                /**
-                 * delete client without users. Recursively (so reports will be deleted too)
-                 */
-                if (count($c->getUsers()) === 0) {
-                    $output->writeln('client ' . $c->getId() . ' and related reports flagged for deletion (reason: no users)');
-                    $clientsToRemove[] = $c;
-                } else {
-                    /**
-                     * cleanup reports
-                     */
-                    $unsubmittedReports = $c->getUnsubmittedReports();
-                    if (count($unsubmittedReports) > 1) {
-                        if ($deleteableReports = $rs->findDeleteableReports($unsubmittedReports)) {
-                            foreach ($deleteableReports as $deleteableReport) {
-                                $output->writeln('report ' . $deleteableReport->getId() . ' flagged for deletion (reason: duplicate)');
-                                $reportsToRemove[] = $deleteableReport;
-                            }
-                        }
-                    }
+        foreach ($em->getRepository(User::class)->findBy([
+            'deputyNo'=>null,
+            'roleName' => User::ROLE_LAY_DEPUTY,
+        ]) as $user) {
+            $output->write("User {$user->getId()}: ");
+            try {
+                $client = $user->getFirstClient();
+                if (!$client) {
+                    $clientNotCreated++;
+                    throw new \Exception('Client not yet created. skipped');
                 }
+
+                $caseNumber = $client->getCaseNumber();
+                $clientSurname = $client->getLastname();
+                $deputySurname = $user->getLastname();
+                $deputyPostcode = $user->getAddressPostcode();
+
+                $vs->validate($caseNumber, $clientSurname, $deputySurname, $deputyPostcode);
+                $deputyNo = implode(',', $vs->getLastMatchedDeputyNumbers());
+                $user->setDeputyNo($deputyNo);
+                $em->flush($user);
+                $output->writeln(" deputyNo set to $deputyNo ");
+                $fixed++;
+            } catch (\Exception $e) {
+                $error = $e->getCode() === 400 ? 'CASREC match not found' : $e->getMessage();
+                $output->writeln(" ERROR: ".$error);
+                $mismatch++;
             }
-            $offset += $limit;
-        } while (!empty($clients));
-
-        $output->write('Performing deletion...');
-        // remove !
-        foreach ($clientsToRemove as $c) {
-            $em->remove($c);
-            $em->flush($c);
-        }
-        foreach ($reportsToRemove as $r) {
-            $em->remove($r);
-            $em->flush($r);
         }
 
-        $output->writeln('Done');
+        $output->writeln("Done. Fixed $fixed , mismatch  $mismatch , client not created $clientNotCreated");
     }
 }
