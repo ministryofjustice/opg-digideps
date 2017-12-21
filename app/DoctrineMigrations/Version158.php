7104f8ecdd920034ad2e974dd7ae286f3d88ef40
Elvis Ciotti <elvisciotti@gmail.com>
2017-12-21 14:49:26 +0100

    DDPB-1798 move query

--- app/DoctrineMigrations/Version158.php
+++ app/DoctrineMigrations/Version158.php
@@ -18,6 +18,8 @@ class Version158 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
+        $this->addSql("DELETE FROM migrations WHERE version in ('144', '145', '146', '147', '148', '149', '150', '151', '152', '153', '154', '155', '156')");
+
         $this->addSql('ALTER TABLE casrec ADD ndr_submitted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL');
     }
 
