86af381c14289d7048ee14795950f29df5767dbe
Shaun Lizzio <shaun@lizzio.co.uk>
2017-12-22 12:00:14 +0000

    DDPB-1839 restored Migration 158

---
+++ app/DoctrineMigrations/Version158.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version158 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql("DELETE FROM migrations WHERE version in ('144', '145', '146', '147', '148', '149', '150', '151', '152', '153', '154', '155', '156')");
+
+        $this->addSql('ALTER TABLE casrec ADD ndr_submitted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+    }
+}
