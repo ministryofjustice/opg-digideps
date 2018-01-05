c9addfdc1bdfa2f11a41090391bea981c0c1f7a3
Elvis Ciotti <elvisciotti@gmail.com>
2018-01-05 16:03:40 +0100

    DDPB-1847 add comments

--- src/AppBundle/Service/File/Checker/ClamAVMocks.php
+++ src/AppBundle/Service/File/Checker/ClamAVMocks.php
@@ -4,21 +4,22 @@ namespace AppBundle\Service\File\Checker;
 
 use AppBundle\Service\File\Types\UploadableFileInterface;
 
+/**
+ * ClamAV responses for known files, based on file hash
+ * To use for behat-tests and avoid non-responsiveness of the ClamAV service on test env
+ */
 class ClamAVMocks
 {
-    public static function getCachedResponse(UploadableFileInterface $file)
-    {
-        $uploadedFileHash = hash_file('md5', $file->getUploadedFile()->getPathName());
-        if (isset(self::$fileHashToResponse[$uploadedFileHash])) {
-            return self::$fileHashToResponse[$uploadedFileHash];
-        }
-    }
-
+    /**
+     * `tests/behat/fixtures` has files along with response
+     *
+     * @var array
+     */
     private static $fileHashToResponse = [
         'fa7d7e650b2cec68f302b31ba28235d8' => [  // good.pdf
             'celery_task_state'    => 'SUCCESS',
-            'file_scanner_code'    => NULL,
-            'file_scanner_message' => NULL,
+            'file_scanner_code'    => null,
+            'file_scanner_message' => null,
             'file_scanner_result'  => 'PASS',
         ],
         'a1ddc9ebe19a3d43ec25889085ad3ed8' => [ // pdf-doc-vba-eicar-dropper.pdf
@@ -29,13 +30,13 @@ class ClamAVMocks
         ],
         'd459dc4890f2ba3c285e014190ca0560' => [ //good.jpg
             'celery_task_state'    => 'SUCCESS',
-            'file_scanner_code'    => NULL,
+            'file_scanner_code'    => null,
             'file_scanner_message' => 'Image details 500x500 mode RGB',
             'file_scanner_result'  => 'PASS',
         ],
         '86c9b243a641dfd2d6b013da32503141' => [//good.png
             'celery_task_state'    => 'SUCCESS',
-            'file_scanner_code'    => NULL,
+            'file_scanner_code'    => null,
             'file_scanner_message' => 'Image details 500x500 mode RGB',
             'file_scanner_result'  => 'PASS',
         ],
@@ -53,17 +54,30 @@ class ClamAVMocks
         ],
         'a2ba3341104c77c2697666690559a907' => [ // file1.pdf
             'celery_task_state' => 'SUCCESS',
-            'file_scanner_code' => NULL,
-            'file_scanner_message' => NULL,
+            'file_scanner_code' => null,
+            'file_scanner_message' => null,
             'file_scanner_result' => 'PASS'
         ],
         '96deed834c6424a81b4b755d5d9ec504' => [ //file2.pdf
             'celery_task_state' => 'SUCCESS',
-            'file_scanner_code' => NULL,
-            'file_scanner_message' => NULL,
+            'file_scanner_code' => null,
+            'file_scanner_message' => null,
             'file_scanner_result' => 'PASS'
         ]
 
     ];
 
+    /**
+     * Returns cache response if there is one. Null otherwise
+     *
+     * @param  UploadableFileInterface $file
+     * @return mixed
+     */
+    public static function getCachedResponse(UploadableFileInterface $file)
+    {
+        $uploadedFileHash = hash_file('md5', $file->getUploadedFile()->getPathName());
+        if (isset(self::$fileHashToResponse[$uploadedFileHash])) {
+            return self::$fileHashToResponse[$uploadedFileHash];
+        }
+    }
 }
