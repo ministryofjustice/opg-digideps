392b177b6d5b06c015b8bc9b77db2aa33a61c021
Biggs Thorarensen <biggs@devillion.com>
2018-01-08 10:35:07 +0000

    DDPB-1440 doc upload js refactored into uploadFile module

--- src/AppBundle/Resources/assets/javascripts/modules/upload.js
+++ src/AppBundle/Resources/assets/javascripts/modules/upload.js
@@ -9,30 +9,6 @@
         root.GOVUK = {};
     }
 
-    var uploadFile = function (containerSelector) {
-        $(containerSelector).on('click', function(){
-            var fileName = $('#report_document_upload_file').val();
-            if (fileName) {
-                $('#upload-progress').removeClass('hidden');
-            }
-        });
-    };
-
-    root.GOVUK.uploadFile = uploadFile;
-
-}).call(this);
-
-/*jshint browser: true */
-(function () {
-    "use strict";
-
-    var root = this,
-        $ = root.jQuery;
-
-    if (typeof GOVUK === 'undefined') {
-        root.GOVUK = {};
-    }
-
     var uploadProgress = function (element) {
         // check if exists
         if ($(element).length == 1) {
@@ -122,6 +98,50 @@
     };
 
     root.GOVUK.uploadProgressPA = uploadProgressPA;
+
+}).call(this);
+
+/*jshint browser: true */
+(function () {
+    "use strict";
+
+    var root = this,
+        $ = root.jQuery;
+
+    if (typeof GOVUK === 'undefined') {
+        root.GOVUK = {};
+    }
+
+    var uploadFile = function (containerSelector) {
+
+        // Show in progress message
+        $(containerSelector).on('click', function(){
+            var fileName = $('#report_document_upload_file').val();
+            if (fileName) {
+                $('#upload-progress').removeClass('hidden');
+            }
+        });
+
+        // Show an error if file is over 15mb
+        $('#upload_form').on('submit', function (e) {
+            e.preventDefault();
+            var fileElement = $('#report_document_upload_file');
+            var actionUrl = $(this).attr('action');
+
+            // check whether browser fully supports all File API
+            if (window.File && window.FileReader && window.FileList && window.Blob && fileElement[0].files.length > 0) {
+                var fsize = fileElement[0].files[0].size;
+                if (fsize > 15 * 1024 * 1024 ) {
+                    window.location = actionUrl + '?error=tooBig';
+                    return;
+                }
+            }
+
+            this.submit();
+        });
+    };
+
+    root.GOVUK.uploadFile = uploadFile;
 
 }).call(this);
 
