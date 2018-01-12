8b2b2fc7ffe63bcf484c9fa55d1a09c9bbe85014
Ian Lindsay <ian@hemera-business-services.co.uk>
2018-01-12 15:53:13 +0000

    ddpb-1669 remove money transfers when editing to 'no' transfers

--- src/AppBundle/Controller/Report/ReportController.php
+++ src/AppBundle/Controller/Report/ReportController.php
@@ -222,6 +222,13 @@ class ReportController extends RestController
         }
 
         if (array_key_exists('no_transfers_to_add', $data)) {
+            if ($data['no_transfers_to_add'] === true) {
+                //true here means "no", so remove existing transfers
+                foreach ($report->getMoneyTransfers() as $e) {
+                    $this->getEntityManager()->remove($e);
+                }
+            }
+
             $report->setNoTransfersToAdd($data['no_transfers_to_add']);
         }
 
