8e48916a6bd21d0aae6bfde4be4f1ef0f3832829
Shaun Lizzio <shaun@lizzio.co.uk>
2018-02-05 11:37:18 +0000

    DDPB-1768 Added NDR group

--- src/AppBundle/Controller/ClientController.php
+++ src/AppBundle/Controller/ClientController.php
@@ -107,7 +107,7 @@ class ClientController extends RestController
      */
     public function detailsAction(Request $request, $id)
     {
-        $this->setJmsSerialiserGroups(['client', 'client-users', 'user', 'report', 'client-reports', 'status']);
+        $this->setJmsSerialiserGroups(['client', 'client-users', 'user', 'report', 'client-reports', 'status', 'ndr']);
 
         $result = $this->findEntityBy(EntityDir\Client::class, $id);
 
