6e97c0c5e5af9556ed0a672167c4de3e618fb6f1
Elvis Ciotti <elvisciotti@gmail.com>
2018-02-14 17:27:44 +0100

    DDPB-857-unsubmit-reports PHPdoc comment + back link

--- src/AppBundle/Entity/Report/UnsubmittedSection.php
+++ src/AppBundle/Entity/Report/UnsubmittedSection.php
@@ -2,32 +2,24 @@
 
 namespace AppBundle\Entity\Report;
 
-use AppBundle\Entity\ReportInterface;
-use AppBundle\Entity\Client;
-use AppBundle\Entity\Report\Traits as ReportTraits;
-use JMS\Serializer\Annotation as JMS;
-use Symfony\Component\Validator\Constraints as Assert;
-use Symfony\Component\Validator\ExecutionContextInterface;
-
-
 class UnsubmittedSection
 {
     /**
-     * @JMS\Type("integer")
-     * @JMS\Groups({"visits-care"})
+     * Store section identifier
      *
-     * @var int
+     * @var string
      */
     private $id;
 
     /**
+     * Store checkbox value
      * @var boolean
      */
     private $present;
 
     /**
      * UnsubmittedSection constructor.
-     * @param int $id
+     * @param string $id
      * @param bool $present
      */
     public function __construct($id, $present)
