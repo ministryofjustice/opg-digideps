1c0c046df15b725e7bea0d0e62fc171979eb50fa
Elvis Ciotti <elvisciotti@gmail.com>
2018-03-08 12:45:04 +0100

    DDPB-1984-prof-current-fees refactor into traits

--- src/AppBundle/Entity/Report/Traits/ReportProfServiceFeesTrait.php
+++ src/AppBundle/Entity/Report/Traits/ReportProfServiceFeesTrait.php
@@ -6,10 +6,36 @@ use AppBundle\Entity\Report\Fee;
 use AppBundle\Entity\Report\ProfServiceFee;
 use JMS\Serializer\Annotation as JMS;
 use Symfony\Component\Validator\Constraints as Assert;
-use Symfony\Component\Validator\ExecutionContextInterface;
 
 trait ReportProfServiceFeesTrait
 {
+
+    /**
+     * @var string yes/no
+     *
+     * @JMS\Type("string")
+     * @JMS\Groups({"report", "current-prof-payments-received"})
+     * @Assert\NotBlank(message="common.yesnochoice.notBlank", groups={"current-prof-payments-received"})
+     */
+    private $currentProfPaymentsReceived;
+
+    /**
+     * @var string yes/no
+     *
+     * @JMS\Type("string")
+     * @JMS\Groups({"report", "report-prof-estimate-fees"})
+     * @Assert\NotBlank(message="profServiceFee.estimates.previousProfFeesEstimateGiven.notBlank", groups={"previous-prof-fees-estimate-choice"})
+     */
+    private $previousProfFeesEstimateGiven;
+
+    /**
+     * @var string
+     *
+     * @JMS\Type("string")
+     * @JMS\Groups({"report", "report-prof-estimate-fees"})
+     */
+    private $profFeesEstimateSccoReason;
+
     /**
      * @JMS\Type("array<AppBundle\Entity\Report\ProfServiceFee>")
      * @JMS\Groups({"report-prof-service-fees"})
@@ -17,6 +43,148 @@ trait ReportProfServiceFeesTrait
      * @var ProfServiceFee[]
      */
     private $profServiceFees = [];
+
+
+    /**
+     * @return string
+     */
+    public function getCurrentProfPaymentsReceived()
+    {
+        return $this->currentProfPaymentsReceived;
+    }
+
+    /**
+     * @param $currentProfPaymentsReceived
+     *
+     * @return $this
+     */
+    public function setCurrentProfPaymentsReceived($currentProfPaymentsReceived)
+    {
+        $this->currentProfPaymentsReceived = $currentProfPaymentsReceived;
+
+        return $this;
+    }
+
+
+
+    /**
+     * Return filtered array of ProfServiceFee's
+     *
+     * @param string $feeTypeId current|estimated|previous
+     * @param string $fixedOrAssessed
+     * @return array
+     * @throws \Exception
+     */
+    public function getFilteredFees($feeTypeId, $fixedOrAssessed)
+    {
+        switch ($feeTypeId) {
+            case ProfServiceFee::TYPE_CURRENT_FEE:
+                $fees = $this->getProfServiceFeesByType(ProfServiceFee::TYPE_CURRENT_FEE);
+                break;
+            case ProfServiceFee::TYPE_ESTIMATED_FEE:
+                $fees = $this->getProfServiceFeesByType(ProfServiceFee::TYPE_ESTIMATED_FEE);
+                break;
+            case ProfServiceFee::TYPE_PREVIOUS_FEE:
+                $fees = $this->getProfServiceFeesByType(ProfServiceFee::TYPE_PREVIOUS_FEE);
+                break;
+            default:
+                throw new \Exception('Invalid Fee type Id:' . $feeTypeId);
+        }
+
+        return array_filter($fees, function ($profServiceFee) use ($fixedOrAssessed) {
+            /** @var $profServiceFee ProfServiceFee  */
+            return $profServiceFee->getAssessedOrFixed() === $fixedOrAssessed;
+        });
+    }
+
+    /**
+     * @param string $feeTypeId "current"|"estimated"|"previous"
+     *
+     * @return array
+     *
+     * @throws \Exception
+     */
+    public function getProfServiceFeesByType($feeTypeId)
+    {
+        if (!in_array(
+            $feeTypeId,
+            [
+                ProfServiceFee::TYPE_CURRENT_FEE,
+                ProfServiceFee::TYPE_PREVIOUS_FEE,
+                ProfServiceFee::TYPE_ESTIMATED_FEE
+            ]
+        )) {
+            throw new \Exception('Invalid feeTypeId: ' . $feeTypeId);
+        }
+
+        return array_filter($this->getProfServiceFees(), function ($profServiceFee) use ($feeTypeId) {
+            /** @var $profServiceFee \AppBundle\Entity\Report\ProfServiceFee */
+            return $profServiceFee->getFeeTypeId() === $feeTypeId;
+        });
+    }
+
+    /**
+     * Returns current Fixed service fees
+     *
+     * @return array
+     */
+    public function getCurrentFixedServiceFees()
+    {
+        return $this->getFilteredFees(
+            ProfServiceFee::TYPE_CURRENT_FEE,
+            ProfServiceFee::TYPE_FIXED_FEE
+        );
+    }
+
+    /**
+     * Returns current Assessed service fees
+     *
+     * @return array
+     */
+    public function getCurrentAssessedServiceFees()
+    {
+        return $this->getFilteredFees(
+            ProfServiceFee::TYPE_CURRENT_FEE,
+            ProfServiceFee::TYPE_ASSESSED_FEE
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public function getPreviousProfFeesEstimateGiven()
+    {
+        return $this->previousProfFeesEstimateGiven;
+    }
+
+    /**
+     * @param string $previousProfFeesEstimateGiven
+     * @return $this
+     */
+    public function setPreviousProfFeesEstimateGiven($previousProfFeesEstimateGiven)
+    {
+        $this->previousProfFeesEstimateGiven = $previousProfFeesEstimateGiven;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getProfFeesEstimateSccoReason()
+    {
+        return $this->profFeesEstimateSccoReason;
+    }
+
+    /**
+     * @param string $profFeesEstimateSccoReason
+     * @return $this
+     */
+    public function setProfFeesEstimateSccoReason($profFeesEstimateSccoReason)
+    {
+        $this->profFeesEstimateSccoReason = $profFeesEstimateSccoReason;
+        return $this;
+    }
+
 
     /**
      * @return ProfServiceFee[]
