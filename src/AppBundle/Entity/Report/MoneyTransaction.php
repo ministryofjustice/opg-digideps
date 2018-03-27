02b9b719b04488143b3010a82e7e205650b32acc
Shaun Lizzio <shaun@lizzio.co.uk>
2018-03-27 13:16:28 +0100

    DDPB-2040 Synced config array and removed 'order'

--- src/AppBundle/Entity/Report/MoneyTransaction.php
+++ src/AppBundle/Entity/Report/MoneyTransaction.php
@@ -24,76 +24,95 @@ class MoneyTransaction implements MoneyTransactionInterface
      */
     public static $categories = [
         // category | hasMoreDetails | order | group | type (in/out)
-        ['account-interest', false, '20', 'income-and-earnings', 'in'],
-        ['dividends', false, '30', 'income-and-earnings', 'in'],
-        ['income-from-property-rental', false, '50', 'income-and-earnings', 'in'],
-        ['salary-or-wages', false, '60', 'income-and-earnings', 'in'],
-        ['other-incomes', true, '65', 'income-and-earnings', 'in'],
-        ['attendance-allowance', false, '70', 'state-benefits', 'in'],
-        ['disability-living-allowance', false, '80', 'state-benefits', 'in'],
-        ['employment-support-allowance', false, '90', 'state-benefits', 'in'],
-        ['housing-benefit', false, '100', 'state-benefits', 'in'],
-        ['incapacity-benefit', false, '110', 'state-benefits', 'in'],
-        ['income-support', false, '120', 'state-benefits', 'in'],
-        ['pension-credit', false, '130', 'state-benefits', 'in'],
-        ['personal-independence-payment', false, '140', 'state-benefits', 'in'],
-        ['severe-disablement-allowance', false, '150', 'state-benefits', 'in'],
-        ['universal-credit', false, '160', 'state-benefits', 'in'],
-        ['winter-fuel-cold-weather-payment', false, '170', 'state-benefits', 'in'],
-        ['other-benefits', true, '180', 'state-benefits', 'in'],
-        ['personal-pension', false, '190', 'pensions', 'in'],
-        ['state-pension', false, '200', 'pensions', 'in'],
-        ['compensation-or-damages-award', true, '210', 'damages', 'in'],
-        ['bequest-or-inheritance', false, '220', 'one-off', 'in'],
-        ['cash-gift-received', false, '230', 'one-off', 'in'],
-        ['refunds', false, '240', 'one-off', 'in'],
-        ['sale-of-asset', true, '250', 'one-off', 'in'],
-        ['sale-of-investment', true, '260', 'one-off', 'in'],
-        ['sale-of-property', true, '270', 'one-off', 'in'],
-        ['anything-else', true, '290', 'moneyin-other', 'in'],
-        ['broadband', false, '300', 'household-bills', 'out'],
-        ['council-tax', false, '310', 'household-bills', 'out'],
-        ['electricity', false, '320', 'household-bills', 'out'],
-        ['food', false, '330', 'household-bills', 'out'],
-        ['gas', false, '340', 'household-bills', 'out'],
-        ['insurance-eg-life-home-contents', false, '350', 'household-bills', 'out'],
-        ['other-insurance', false, '360', 'household-bills', 'out'],
-        ['property-maintenance-improvement', true, '370', 'household-bills', 'out'],
-        ['telephone', false, '380', 'household-bills', 'out'],
-        ['tv-services', false, '390', 'household-bills', 'out'],
-        ['water', false, '400', 'household-bills', 'out'],
-        ['households-bills-other', true, '410', 'household-bills', 'out'],
-        ['accommodation-service-charge', false, '420', 'accommodation', 'out'],
-        ['mortgage', false, '430', 'accommodation', 'out'],
-        ['rent', false, '440', 'accommodation', 'out'],
-        ['accommodation-other', true, '450', 'accommodation', 'out'],
-        ['care-fees', false, '460', 'care-and-medical', 'out'],
-        ['local-authority-charges-for-care', false, '470', 'care-and-medical', 'out'],
-        ['medical-expenses', false, '480', 'care-and-medical', 'out'],
-        ['medical-insurance', false, '490', 'care-and-medical', 'out'],
-        ['client-transport-bus-train-taxi-fares', false, '500', 'client-expenses', 'out'],
-        ['clothes', false, '510', 'client-expenses', 'out'],
-        ['day-trips', false, '520', 'client-expenses', 'out'],
-        ['holidays', false, '530', 'client-expenses', 'out'],
-        ['personal-allowance-pocket-money', false, '540', 'client-expenses', 'out'],
-        ['toiletries', false, '550', 'client-expenses', 'out'],
-        ['deputy-security-bond', false, '560', 'fees', 'out'],
-        ['opg-fees', false, '570', 'fees', 'out'],
-        ['other-fees', true, '580', 'fees', 'out'],
-        ['professional-fees-eg-solicitor-accountant', true, '590', 'fees', 'out'],
-        ['investment-bonds-purchased', true, '610', 'major-purchases', 'out'],
-        ['investment-account-purchased', true, '620', 'major-purchases', 'out'],
-        ['purchase-over-1000', true, '630', 'major-purchases', 'out'],
-        ['stocks-and-shares-purchased', true, '640', 'major-purchases', 'out'],
-        ['bank-charges', false, '660', 'debt-and-charges', 'out'],
-        ['credit-cards-charges', false, '670', 'debt-and-charges', 'out'],
-        ['unpaid-care-fees', false, '680', 'debt-and-charges', 'out'],
-        ['loans', false, '690', 'debt-and-charges', 'out'],
-        ['tax-payments-to-hmrc', false, '700', 'debt-and-charges', 'out'],
-        ['debt-and-charges-other', true, '710', 'debt-and-charges', 'out'],
-        ['cash-withdrawn', true, '720', 'moving-money', 'out'],
-        ['transfers-out-to-other-accounts', true, '730', 'moving-money', 'out'],
-        ['anything-else-paid-out', true, '740', 'moneyout-other', 'out'],
+
+        // Money In
+        ['salary-or-wages', false, 'salary-or-wages', 'in'],
+
+        ['account-interest', false, 'income-and-earnings', 'in'],
+        ['dividends', false, 'income-and-earnings', 'in'],
+        ['income-from-property-rental', false, 'income-and-earnings', 'in'],
+
+        ['personal-pension', false, 'pensions', 'in'],
+        ['state-pension', false, 'pensions', 'in'],
+
+        ['attendance-allowance', false, 'state-benefits', 'in'],
+        ['disability-living-allowance', false, 'state-benefits', 'in'],
+        ['employment-support-allowance', false, 'state-benefits', 'in'],
+        ['housing-benefit', false, 'state-benefits', 'in'],
+        ['incapacity-benefit', false, 'state-benefits', 'in'],
+        ['income-support', false, 'state-benefits', 'in'],
+        ['pension-credit', false, 'state-benefits', 'in'],
+        ['personal-independence-payment', false, 'state-benefits', 'in'],
+        ['severe-disablement-allowance', false, 'state-benefits', 'in'],
+        ['universal-credit', false, 'state-benefits', 'in'],
+        ['winter-fuel-cold-weather-payment', false, 'state-benefits', 'in'],
+        ['other-benefits', true, 'state-benefits', 'in'],
+
+        ['compensation-or-damages-award', true, 'compensation-or-damages-award', 'in'],
+
+        ['bequest-or-inheritance', false, 'one-off', 'in'],
+        ['cash-gift-received', false, 'one-off', 'in'],
+        ['refunds', false, 'one-off', 'in'],
+        ['sale-of-asset', true, 'one-off', 'in'],
+        ['sale-of-investment', true, 'one-off', 'in'],
+        ['sale-of-property', true, 'one-off', 'in'],
+
+        ['anything-else', true, 'moneyin-other', 'in'], // no group
+
+        // Money Out
+        ['broadband', false, 'household-bills', 'out'],
+        ['council-tax', false, 'household-bills', 'out'],
+        ['electricity', false, 'household-bills', 'out'],
+        ['food', false, 'household-bills', 'out'],
+        ['gas', false, 'household-bills', 'out'],
+        ['insurance-eg-life-home-contents', false, 'household-bills', 'out'],
+        ['other-insurance', false, 'household-bills', 'out'],
+        ['property-maintenance-improvement', true, 'household-bills', 'out'],
+        ['telephone', false, 'household-bills', 'out'],
+        ['tv-services', false, 'household-bills', 'out'],
+        ['water', false, 'household-bills', 'out'],
+        ['households-bills-other', true, 'household-bills', 'out'],
+
+        ['accommodation-service-charge', false, 'accommodation', 'out'],
+        ['mortgage', false, 'accommodation', 'out'],
+        ['rent', false, 'accommodation', 'out'],
+        ['accommodation-other', true, 'accommodation', 'out'],
+
+        ['care-fees', false, 'care-and-medical', 'out'],
+        ['local-authority-charges-for-care', false, 'care-and-medical', 'out'],
+        ['medical-expenses', false, 'care-and-medical', 'out'],
+        ['medical-insurance', false, 'care-and-medical', 'out'],
+
+        ['client-transport-bus-train-taxi-fares', false, 'client-expenses', 'out'],
+        ['clothes', false, 'client-expenses', 'out'],
+        ['day-trips', false, 'client-expenses', 'out'],
+        ['holidays', false, 'client-expenses', 'out'],
+        ['personal-allowance-pocket-money', false, 'client-expenses', 'out'],
+        ['toiletries', false, 'client-expenses', 'out'],
+
+        ['deputy-security-bond', false, 'fees', 'out'],
+        ['opg-fees', false, 'fees', 'out'],
+        ['professional-fees-eg-solicitor-accountant', true, 'fees', 'out'],
+        ['other-fees', true, 'fees', 'out'],
+
+        ['investment-bonds-purchased', true, 'major-purchases', 'out'],
+        ['investment-account-purchased', true, 'major-purchases', 'out'],
+        ['stocks-and-shares-purchased', true, 'major-purchases', 'out'],
+        ['purchase-over-1000', true, 'major-purchases', 'out'],
+
+        ['bank-charges', false, 'debt-and-charges', 'out'],
+        ['credit-cards-charges', false, 'debt-and-charges', 'out'],
+        ['loans', false, 'debt-and-charges', 'out'],
+        ['tax-payments-to-hmrc', false, 'debt-and-charges', 'out'],
+        ['unpaid-care-fees', false, 'debt-and-charges', 'out'],
+        ['debt-and-charges-other', true, 'debt-and-charges', 'out'],
+
+        ['cash-withdrawn', true, 'cash-withdrawn', 'out'],
+
+        ['transfers-out-to-other-accounts', true, 'transfers-out-to-other-accounts', 'out'],
+
+        ['moneyout-other', true, 'moneyout-other', 'out'],
+
     ];
 
     /**
@@ -256,7 +275,7 @@ class MoneyTransaction implements MoneyTransactionInterface
     public function getGroup()
     {
         foreach (self::$categories as $cat) {
-            list($categoryId, $hasDetails, $order, $groupId, $type) = $cat;
+            list($categoryId, $hasDetails, $groupId, $type) = $cat;
             if ($this->getCategory() == $categoryId) {
                 return $groupId;
             }
@@ -277,7 +296,7 @@ class MoneyTransaction implements MoneyTransactionInterface
     public function getType()
     {
         foreach (self::$categories as $cat) {
-            list($categoryId, $hasDetails, $order, $groupId, $type) = $cat;
+            list($categoryId, $hasDetails, $groupId, $type) = $cat;
             if ($this->getCategory() == $categoryId) {
                 return $type;
             }
