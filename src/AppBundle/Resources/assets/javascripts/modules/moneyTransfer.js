0350f077d61a7abd08ae35d40f3d9bcd9420f563
Biggs Thorarensen <biggs@devillion.com>
2018-04-09 16:19:12 +0100

    DDPB-1913 moneyTransfer.js module: removing selected 'from' bank account from the 'to' accounts dropdown

---
+++ src/AppBundle/Resources/assets/javascripts/modules/moneyTransfer.js
@@ -0,0 +1,41 @@
+/*jshint browser: true */
+(function () {
+    "use strict";
+
+    var root = this,
+        $ = root.jQuery;
+
+    if (typeof GOVUK === 'undefined') {
+        root.GOVUK = {};
+    }
+
+    var moneyTransfer = function (containerSelector) {
+        // if 'from' account is already selected when loading page, remove that account from the 'to'
+        // (for example when only 'from' account is selected when posting form - which returns an error)
+        var selectedFromAccount = $(containerSelector).val();
+        if (selectedFromAccount != '') {
+            $('.js-transfer-to option[value=' + selectedFromAccount + ']').hide();
+        }
+
+        // when 'from' account dropdown changes
+        $(containerSelector).on('change', function(e){
+            // show all 'to' accounts
+            $('.js-transfer-to option').show();
+            // selected 'from' account
+            var selectedId = $(this).val();
+            // only update 'to' accounts if a 'from' account is selected
+            if (selectedId != '') {
+                // if 'to' account matches selected 'from' account, reset dropdown
+                var $selectTo = $('.js-transfer-to');
+                if ($selectTo.val() == selectedId) {
+                    $selectTo.val('');
+                }
+                // hide the selected 'from' account from the 'to' accounts list
+                $('.js-transfer-to option[value=' + selectedId + ']').hide();
+            }
+        });
+    };
+
+    root.GOVUK.moneyTransfer = moneyTransfer;
+
+}).call(this);
