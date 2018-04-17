27a7f489447b10e9f7330b427c21ffcc6b66d888
Shaun Lizzio <shaun@lizzio.co.uk>
2018-04-17 08:05:43 +0100

    DDPB-2045 Revised migration to set bank account to null on deletion of bank account

--- app/DoctrineMigrations/Version173.php
+++ app/DoctrineMigrations/Version173.php
@@ -19,15 +19,13 @@ class Version173 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('ALTER TABLE expense ADD bank_account_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE expense ADD CONSTRAINT FK_2D3A8DA612CB990C FOREIGN KEY (bank_account_id) REFERENCES account (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE expense ADD CONSTRAINT FK_2D3A8DA612CB990C FOREIGN KEY (bank_account_id) REFERENCES account (id) ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('CREATE INDEX IDX_2D3A8DA612CB990C ON expense (bank_account_id)');
-
         $this->addSql('ALTER TABLE gift ADD bank_account_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE gift ADD CONSTRAINT FK_A47C990D12CB990C FOREIGN KEY (bank_account_id) REFERENCES account (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE gift ADD CONSTRAINT FK_A47C990D12CB990C FOREIGN KEY (bank_account_id) REFERENCES account (id) ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('CREATE INDEX IDX_A47C990D12CB990C ON gift (bank_account_id)');
-
         $this->addSql('ALTER TABLE money_transaction ADD bank_account_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE money_transaction ADD CONSTRAINT FK_D21254E212CB990C FOREIGN KEY (bank_account_id) REFERENCES account (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE money_transaction ADD CONSTRAINT FK_D21254E212CB990C FOREIGN KEY (bank_account_id) REFERENCES account (id) ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('CREATE INDEX IDX_D21254E212CB990C ON money_transaction (bank_account_id)');
     }
 
@@ -43,13 +41,11 @@ class Version173 extends AbstractMigration
         $this->addSql('ALTER TABLE money_transaction DROP CONSTRAINT FK_D21254E212CB990C');
         $this->addSql('DROP INDEX IDX_D21254E212CB990C');
         $this->addSql('ALTER TABLE money_transaction DROP bank_account_id');
-
-        $this->addSql('ALTER TABLE gift DROP CONSTRAINT FK_A47C990D12CB990C');
-        $this->addSql('DROP INDEX IDX_A47C990D12CB990C');
-        $this->addSql('ALTER TABLE gift DROP bank_account_id');
-
         $this->addSql('ALTER TABLE expense DROP CONSTRAINT FK_2D3A8DA612CB990C');
         $this->addSql('DROP INDEX IDX_2D3A8DA612CB990C');
         $this->addSql('ALTER TABLE expense DROP bank_account_id');
+        $this->addSql('ALTER TABLE gift DROP CONSTRAINT FK_A47C990D12CB990C');
+        $this->addSql('DROP INDEX IDX_A47C990D12CB990C');
+        $this->addSql('ALTER TABLE gift DROP bank_account_id');
     }
 }
