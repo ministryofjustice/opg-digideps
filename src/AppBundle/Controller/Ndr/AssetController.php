8678daff46f227d9849b30ae6810ef0dfe805b5f
Elvis Ciotti <elvisciotti@gmail.com>
2018-05-25 12:13:26 +0200

    DDPB-2142-mask-url-data custom URLs for bank accounts, properties, asset other + disable global query string

--- src/AppBundle/Controller/Ndr/AssetController.php
+++ src/AppBundle/Controller/Ndr/AssetController.php
@@ -128,6 +128,8 @@ class AssetController extends AbstractController
             'backLink' => $this->generateUrl('ndr_assets_type', ['ndrId' => $ndrId]),
             'form' => $form->createView(),
             'ndr' => $ndr,
+            // avoid sending query string to GA containing user's data
+            'gaCustomUrl' => $this->generateUrl('ndr_asset_other_add', ['ndrId'=>$ndrId, 'title'=>'type'])
         ];
     }
 
@@ -308,6 +310,7 @@ class AssetController extends AbstractController
             'form' => $form->createView(),
             'backLink' => $stepRedirector->getBackLink(),
             'skipLink' => null,
+            'gaCustomUrl' => $request->getPathInfo() // avoid sending query string to GA containing user's data
         ];
     }
 
