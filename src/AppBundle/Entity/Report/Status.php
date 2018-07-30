4349fd4ff5c08be1aed2ff1467c36e5e389ffcd0
Elvis Ciotti <elvisciotti@gmail.com>
2018-07-30 13:43:42 +0200

    DDPB-2253-org-report-unsubmit fix labels

--- src/AppBundle/Entity/Report/Status.php
+++ src/AppBundle/Entity/Report/Status.php
@@ -540,9 +540,12 @@ class Status
     public function getState()
     {
         switch ($this->status) {
-            case 'notStarted': return Status::STATE_NOT_STARTED;
-            case 'notFinished': return Status::STATE_INCOMPLETE;
-            case 'readyToSubmit': return Status::STATE_DONE;
+            case 'notStarted':
+                return Status::STATE_NOT_STARTED;
+            case 'notFinished':
+                return Status::STATE_INCOMPLETE;
+            case 'readyToSubmit':
+                return Status::STATE_DONE;
         }
     }
 }
