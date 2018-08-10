e58b6dd1b16020ef0e0f8399d243155a4bdd4a2f
Shaun Lizzio <shaun@lizzio.co.uk>
2018-08-10 15:22:09 +0100

    DDPB-2252 Tidyup, removed constants no longer used

--- src/AppBundle/Entity/Report/Report.php
+++ src/AppBundle/Entity/Report/Report.php
@@ -32,25 +32,6 @@ class Report implements ReportInterface
     use ReportTraits\ReportProfServiceFeesTrait;
     use ReportTraits\ReportUnsubmittedSections;
 
-
-    const TYPE_103 = '103';
-    const TYPE_102 = '102';
-    const TYPE_104 = '104';
-    const TYPE_103_4 = '103-4';
-    const TYPE_102_4 = '102-4';
-
-    const TYPE_103_6 = '103-6';
-    const TYPE_102_6 = '102-6';
-    const TYPE_104_6 = '104-6';
-    const TYPE_103_4_6 = '103-4-6';
-    const TYPE_102_4_6 = '102-4-6';
-
-    const TYPE_103_5 = '103-5';
-    const TYPE_102_5 = '102-5';
-    const TYPE_104_5 = '104-5';
-    const TYPE_103_4_5 = '103-4-5';
-    const TYPE_102_4_5 = '102-4-5';
-
     /**
      * @JMS\Type("integer")
      * @JMS\Groups({"visits-care"})
