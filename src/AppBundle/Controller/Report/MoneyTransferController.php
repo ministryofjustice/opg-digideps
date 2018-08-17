a70d2c5065abe2f9d48719f6d079a71f764c77b5
Shaun Lizzio <shaun@lizzio.co.uk>
2018-08-17 17:02:00 +0100

    DDPB-2291 Reinstated view,  revised switch values to ensure correct API call is made

--- src/AppBundle/Controller/Report/MoneyTransferController.php
+++ src/AppBundle/Controller/Report/MoneyTransferController.php
@@ -59,12 +59,12 @@ class MoneyTransferController extends AbstractController
         ]);
 
         $form->handleRequest($request);
-
         if ($form->isValid()) {
+
             switch ($report->getNoTransfersToAdd()) {
-                case false:
+                case 0:
                     return $this->redirectToRoute('money_transfers_step', ['reportId' => $reportId, 'step' => 1]);
-                case true:
+                case 1:
                     $this->getRestClient()->put('report/' . $reportId, $report, ['money-transfers-no-transfers']);
                     return $this->redirectToRoute('money_transfers_summary', ['reportId' => $reportId]);
             }
@@ -98,7 +98,6 @@ class MoneyTransferController extends AbstractController
         $stepUrlData = $dataFromUrl;
         $report = $this->getReportIfNotSubmitted($reportId, self::$jmsGroups);
         $fromPage = $request->get('from');
-
 
         $stepRedirector = $this->stepRedirector()
             ->setRoutes('money_transfers', 'money_transfers_step', 'money_transfers_summary')
