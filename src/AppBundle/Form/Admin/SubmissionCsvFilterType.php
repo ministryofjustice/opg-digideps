17e906563d852d5ee8df42403f13c79278e95c7b
Alex Eves <alex.eves@aiesoftware.com>
2018-12-01 21:26:13 +0000

    DDPB-2359: Adds end date input field to Report Submission download feature

--- src/AppBundle/Form/Admin/SubmissionCsvFilterType.php
+++ src/AppBundle/Form/Admin/SubmissionCsvFilterType.php
@@ -6,17 +6,47 @@ use AppBundle\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type as FormTypes;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class SubmissionCsvFilterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('fromDate', FormTypes\DateType::class, ['widget' => 'text',
-                'input'           => 'datetime',
-                'format'          => 'dd-MM-yyyy',
-                'invalid_message' => 'Enter a valid date'])
-            ->add('submitAndDownload', FormTypes\SubmitType::class);
+        $builder
+            ->add(
+                'fromDate',
+                FormTypes\DateType::class, [
+                    'widget' => 'text',
+                    'input' => 'datetime',
+                    'format' => 'dd-MM-yyyy',
+                    'invalid_message' => 'Enter a valid date'
+                ]
+            )
+            ->add(
+                'toDate',
+                FormTypes\DateType::class, [
+                    'widget' => 'text',
+                    'input' => 'datetime',
+                    'format' => 'dd-MM-yyyy',
+                    'invalid_message' => 'Enter a valid date'
+                ]
+            )
+            ->add('submitAndDownload', FormTypes\SubmitType::class)
+            ->addEventListener(FormEvents::POST_SUBMIT, [$this, 'onPostSubmit']);
+    }
+
+    /**
+     * @param FormEvent $event
+     */
+    public function onPostSubmit(FormEvent $event)
+    {
+        $formData = $event->getData();
+
+        if ($formData['toDate'] instanceof \DateTime) {
+            $formData['toDate']->setTime(23, 59, 59);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver)
