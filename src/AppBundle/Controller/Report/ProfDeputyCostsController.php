a5eba3b16bad99098eddb5b045f1e9c6cfd16e4e
Elvis Ciotti <elvis.ciotti@digital.justice.gov.uk>
2018-12-03 16:39:57 +0100

    DDPB-2343-cost-links start page

--- src/AppBundle/Controller/Report/ProfDeputyCostsController.php
+++ src/AppBundle/Controller/Report/ProfDeputyCostsController.php
@@ -41,6 +41,63 @@ class ProfDeputyCostsController extends AbstractController
         ];
     }
 
+    /**
+     * @Route("/how-charged", name="prof_deputy_costs_how_charged")
+     * @Template()
+     */
+    public function howChargedAction(Request $request, $reportId)
+    {
+        $report = $this->getReportIfNotSubmitted($reportId, self::$jmsGroups);
+        $fromSummaryPage = $request->get('from') == 'summary';
+
+        $form = $this->createForm(FormDir\Report\MoneyShortType::class, $report, ['field' => 'moneyShortCategoriesIn']);
+        $form->handleRequest($request);
+
+        if ($form->get('save')->isClicked() && $form->isValid()) {
+            $data = $form->getData();
+
+//            $this->getRestClient()->put('report/' . $reportId, $data, ['moneyShortCategoriesIn']);
+
+            if ($fromSummaryPage) {
+//                $request->getSession()->getFlashBag()->add(
+//                    'notice',
+//                    'Answer edited'
+//                );
+
+                return $this->redirectToRoute('prof_deputy_costs_summary', ['reportId'=>$reportId]);
+            }
+
+            return $this->redirectToRoute('prof_deputy_costs', ['reportId'=>$reportId]);
+        }
+
+
+        return [
+            'report' => $report,
+            'form' => $form->createView(),
+            'backLink' => $this->generateUrl($fromSummaryPage ? 'money_in_short_summary' : 'money_in_short', ['reportId'=>$reportId])
+        ];
+    }
+
+    /**
+     * @Route("/summary", name="prof_deputy_costs_summary")
+     * @Template()
+     *
+     * @param int $reportId
+     *
+     * @return array
+     */
+    public function summaryAction($reportId)
+    {
+        $report = $this->getReportIfNotSubmitted($reportId, self::$jmsGroups);
+        if ($report->getStatus()->getProfDeputyCostsState()['state'] == EntityDir\Report\Status::STATE_NOT_STARTED) {
+            return $this->redirect($this->generateUrl('prof_deputy_costs', ['reportId' => $reportId]));
+        }
+
+        return [
+            'report' => $report,
+        ];
+    }
+
 
     /**
      * @return string
