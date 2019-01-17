95b0a5e91f6781e7c99a97d24ab2551b32ced7bf
Alex Eves <alex.eves@aiesoftware.com>
2019-01-17 11:37:20 +0000

    DDPB-2355: Adds unit test for DeputyCostsEstimateReportUpdateHandler

--- src/AppBundle/Service/RestHandler/Report/DeputyCostsEstimateReportUpdateHandler.php
+++ src/AppBundle/Service/RestHandler/Report/DeputyCostsEstimateReportUpdateHandler.php
@@ -11,11 +11,21 @@ class DeputyCostsEstimateReportUpdateHandler implements ReportUpdateHandlerInter
      */
     public function handle(Report $report, array $data)
     {
+        $this->updateHowCharged($report, $data);
+    }
+
+    /**
+     * @param Report $report
+     * @param array $data
+     * @return $this
+     */
+    private function updateHowCharged(Report $report, array $data)
+    {
         if (array_key_exists('prof_deputy_costs_estimate_how_charged', $data)) {
             $report->setProfDeputyCostsEstimateHowCharged($data['prof_deputy_costs_estimate_how_charged']);
             $report->updateSectionsStatusCache([Report::SECTION_PROF_DEPUTY_COSTS_ESTIMATE]);
         }
 
-        // todo add more updates relevant to the Deputy Costs Estimate section.
+        return $this;
     }
 }
