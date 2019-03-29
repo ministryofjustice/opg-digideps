f0611720eb1f5d2d3a8e489f175cae07ee76cc19
Shaun Lizzio <shaun@lizzio.co.uk>
2019-03-29 13:21:18 +0000

    DDPB-2518 Fixed more unit tests

--- tests/phpunit/Resources/views/Report/FormattedTest.php
+++ tests/phpunit/Resources/views/Report/FormattedTest.php
@@ -1,4 +1,4 @@
-<?php
+    <?php
 
 namespace AppBundle\Resources\views\Report;
 
@@ -430,16 +430,14 @@ class FormattedTest extends WebTestCase
     {
         $this
             ->report
-            ->setProfDeputyCostsHowChargedFixed(true)
-            ->setProfDeputyCostsHowChargedAssessed(false);
+            ->setProfDeputyCostsHowCharged('fixed');
     }
 
     private function ensureDeputyCostsWithNonFixedAndInterimEqualTo($interim)
     {
         $this
             ->report
-            ->setProfDeputyCostsHowChargedFixed(false)
-            ->setProfDeputyCostsHowChargedAssessed(true)
+            ->setProfDeputyCostsHowCharged('assessed')
             ->setProfDeputyCostsHasInterim($interim);
     }
 
@@ -450,8 +448,7 @@ class FormattedTest extends WebTestCase
     {
         $this
             ->report
-            ->setProfDeputyCostsHowChargedFixed(true)
-            ->setProfDeputyCostsHowChargedAssessed(true)
+            ->setProfDeputyCostsHowCharged('both')
             ->setProfDeputyCostsHasInterim(null);
     }
 
