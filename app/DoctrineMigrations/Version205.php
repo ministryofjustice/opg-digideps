b890c6b24a5c713c58f18617df550e69effe29a8
Shaun Lizzio <shaun@lizzio.co.uk>
2019-04-17 08:56:34 +0100

    Moved merged 204 migration into 205 to ensure it is run

--- app/DoctrineMigrations/Version205.php
+++ app/DoctrineMigrations/Version205.php
@@ -17,7 +17,7 @@ class Version205 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
-
+        $this->addSql('ALTER TABLE report ADD prof_dc_estimate_hc VARCHAR(10) DEFAULT NULL');
         $this->addSql('CREATE TABLE prof_deputy_estimate_cost (id SERIAL NOT NULL, report_id INT DEFAULT NULL, prof_deputy_estimate_cost_type_id VARCHAR(255) NOT NULL, amount NUMERIC(14, 2) DEFAULT NULL, has_more_details BOOLEAN NOT NULL, more_details TEXT DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_40B6C2924BD2A4C0 ON prof_deputy_estimate_cost (report_id)');
         $this->addSql('ALTER TABLE prof_deputy_estimate_cost ADD CONSTRAINT FK_40B6C2924BD2A4C0 FOREIGN KEY (report_id) REFERENCES report (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
@@ -30,8 +30,8 @@ class Version205 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
-
         $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE report DROP prof_dc_estimate_hc');
         $this->addSql('DROP TABLE prof_deputy_estimate_cost');
     }
 }
