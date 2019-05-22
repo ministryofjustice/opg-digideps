8d91a73eedaf9e30d83505235a68aaa100d2f658
Luke Bradbury <luke@nqyr.io>
2019-05-22 15:38:42 +0100

    Update circle paths

--- environment/cloud9.tf
+++ environment/cloud9.tf
@@ -1,16 +1,16 @@
 data "aws_security_groups" "cloud9" {
   filter {
-    name = "tag-key"
+    name   = "tag-key"
     values = ["aws:cloud9:owner"]
   }
 }
 
 resource "aws_security_group_rule" "api_rds_cloud9_in" {
-  count = "${length(data.aws_security_groups.cloud9.ids)}"
+  count                    = "${length(data.aws_security_groups.cloud9.ids)}"
   type                     = "ingress"
   protocol                 = "tcp"
   from_port                = 5432
   to_port                  = 5432
-  security_group_id = "${aws_security_group.api_rds.id}"
+  security_group_id        = "${aws_security_group.api_rds.id}"
   source_security_group_id = "${element(data.aws_security_groups.cloud9.ids, count.index)}"
 }
