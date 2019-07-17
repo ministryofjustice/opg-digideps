b650c3d8c0ebb7405c9b78e886e68c7b64150e45
Shaun Lizzio <shaun@lizzio.co.uk>
2019-07-17 15:42:43 +0100

    DDPB-2751 Ensured all emails are lowercase

--- src/AppBundle/Entity/ClientContact.php
+++ src/AppBundle/Entity/ClientContact.php
@@ -143,7 +143,7 @@ class ClientContact
      */
     public function setEmail($email)
     {
-        $this->email = $email;
+        $this->email = strtolower($email);
         return $this;
     }
 
