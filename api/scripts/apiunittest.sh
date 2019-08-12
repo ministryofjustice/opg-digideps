687af9e91bce0a54b015d3a8e7f235533c9da220
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2019-08-12 17:23:31 +0100

    Add explanation to why  `apiunittest.sh` exists

--- api/scripts/apiunittest.sh
+++ api/scripts/apiunittest.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Run each folder of unit tests individually. If we were to run them all
+# individually it would cause a memory leak.
 php bin/phpunit -c tests/phpunit.xml tests/AppBundle/Controller/
 php bin/phpunit -c tests/phpunit.xml tests/AppBundle/Controller-Report/
 php bin/phpunit -c tests/phpunit.xml tests/AppBundle/Controller-Ndr/
