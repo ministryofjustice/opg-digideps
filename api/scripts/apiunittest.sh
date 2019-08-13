a41b2cb682d1c3dca56193bdf46668649bf9e880
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2019-08-13 08:08:11 +0100

    Use a config folder for PHPUnit rather than a file
    This is consistent with how frontend unit tests are run, and allows for easier config change in the future

--- api/scripts/apiunittest.sh
+++ api/scripts/apiunittest.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Run each folder of unit tests individually. If we were to run them all
 # individually it would cause a memory leak.
-php bin/phpunit -c tests/phpunit.xml tests/AppBundle/Controller/
-php bin/phpunit -c tests/phpunit.xml tests/AppBundle/Controller-Report/
-php bin/phpunit -c tests/phpunit.xml tests/AppBundle/Controller-Ndr/
-php bin/phpunit -c tests/phpunit.xml tests/AppBundle/Service/
-php bin/phpunit -c tests/phpunit.xml tests/AppBundle/Entity/
-php bin/phpunit -c tests/phpunit.xml tests/AppBundle/Transformer/
+php bin/phpunit -c tests tests/AppBundle/Controller/
+php bin/phpunit -c tests tests/AppBundle/Controller-Report/
+php bin/phpunit -c tests tests/AppBundle/Controller-Ndr/
+php bin/phpunit -c tests tests/AppBundle/Service/
+php bin/phpunit -c tests tests/AppBundle/Entity/
+php bin/phpunit -c tests tests/AppBundle/Transformer/
