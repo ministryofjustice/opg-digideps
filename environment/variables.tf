fe75ae6f8110f66175209eb5ce92045095f1313e
Luke Bradbury <luke@nqyr.io>
2019-08-14 12:37:38 +0100

    Add script to format workspace name

--- environment/variables.tf
+++ environment/variables.tf
@@ -64,8 +64,8 @@ locals {
     "81.134.202.29/32",
     "94.30.9.148/32",
   ]
-  environment     = substr(lower(terraform.workspace), 0, 14)
-  account         = contains(keys(var.accounts), local.environment) ? var.accounts[local.environment] : var.accounts["default"]
+  environment     = lower(terraform.workspace)
+  account         = lookup(var.accounts, local.environment, "default")
   host_suffix     = local.account["host_suffix_enabled"] ? local.environment : ""
   front_whitelist = length(local.account["front_whitelist"]) > 0 ? local.account["front_whitelist"] : local.default_whitelist
   admin_whitelist = length(local.account["admin_whitelist"]) > 0 ? local.account["admin_whitelist"] : local.default_whitelist
