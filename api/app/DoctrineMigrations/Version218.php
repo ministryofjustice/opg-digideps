bb19e02aa712557563e0cc1b6b65d237edf4740a
Alex Eves <alex.eves@aiesoftware.com>
2019-08-20 11:44:42 +0100

    Renamed migration

--- api/app/DoctrineMigrations/Version218.php
+++ api/app/DoctrineMigrations/Version218.php
@@ -8,7 +8,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version217 extends AbstractMigration
+final class Version218 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
