93898a4526ba9ded21c042092fe8e06f4cd512bc
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2019-08-20 18:08:51 +0100

    Set postgres env variables for Behat
    The Behat tests use command line psql to run commands. This requires the PG* env variables

--- client/scripts/clienttest.sh
+++ client/scripts/clienttest.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# generate behat.yml
-confd -onetime -backend env
+# Export DB config so psql CLI can be used
+export PGHOST=${API_DATABASE_HOSTNAME:=postgres}
+export PGPASSWORD=${API_DATABASE_PASSWORD:=api}
+export PGDATABASE=${API_DATABASE_NAME:=api}
+export PGUSER=${API_DATABASE_USERNAME:=api}
 
 # wait for frontend to come up
 waitforit -address=$FRONTEND_API_URL/manage/availability -timeout=$TIMEOUT -insecure
