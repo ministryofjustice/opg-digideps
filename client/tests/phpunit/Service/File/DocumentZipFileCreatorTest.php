7ab9f78f6da5df64e84b8aa709ecaa057162c15b
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2019-09-06 18:36:23 +0100

    Allows download zip to be created when S3 files are missing
    Currently if we try to retrieve a file from S3 using a reference that does not exist we are throwing an error and not allowing any files to be downloaded. This change catches FileNotFoundExceptions and silently fails allowing the download to continue

--- client/tests/phpunit/Service/File/DocumentZipFileCreatorTest.php
+++ client/tests/phpunit/Service/File/DocumentZipFileCreatorTest.php
@@ -3,10 +3,18 @@
 namespace AppBundle\Service\File;
 
 use AppBundle\Entity\Report\Document;
+use AppBundle\Entity\Report\Report;
 use AppBundle\Entity\Report\ReportSubmission;
+use AppBundle\Service\File\Storage\FileNotFoundException;
+use AppBundle\Service\File\Storage\S3Storage;
 use AppBundle\Service\File\Storage\StorageInterface;
+use Aws\Command;
+use Aws\S3\Exception\S3Exception;
+use Doctrine\Common\Collections\ArrayCollection;
 use Mockery as m;
 use PHPUnit\Framework\TestCase;
+use Prophecy\Prophecy\ObjectProphecy;
+use ZipArchive;
 
 class DocumentZipFileCreatorTest extends TestCase
 {
@@ -73,6 +81,51 @@ class DocumentZipFileCreatorTest extends TestCase
         $this->object->cleanUp();
 
         $this->assertFalse(file_exists($fileName));
+    }
+
+    /**
+     * @group acs
+     */
+    public function testGracefullyHandleMissingFiles()
+    {
+        /** @var S3Storage|ObjectProphecy $storage */
+        $storage = self::prophesize(S3Storage::class);
+        $storage->retrieve('ref-1')->shouldBeCalled()->willReturn(['some-valid' => 'S3Response-1']);
+        $storage->retrieve('ref-2')->shouldBeCalled()->willThrow(new FileNotFoundException("Cannot find file with reference ref-2"));
+        $storage->retrieve('ref-3')->shouldBeCalled()->willReturn(['some-valid' => 'S3Response-3']);
+
+        /** @var ObjectProphecy|ReportSubmission $reportSubmission */
+        $reportSubmission = self::prophesize(ReportSubmission::class);
+
+        $doc1 = self::prophesize(Document::class);
+        $doc1->getStorageReference()->willReturn('ref-1');
+        $doc1->getId()->willReturn(1);
+        $doc1->getFileName()->willReturn('file-name1.pdf');
+
+        $doc2 = self::prophesize(Document::class);
+        $doc2->getStorageReference()->willReturn('ref-2');
+        $doc2->getId()->willReturn(2);
+        $doc2->getFileName()->willReturn('file-name2.pdf');
+
+        $doc3 = self::prophesize(Document::class);
+        $doc3->getStorageReference()->willReturn('ref-3');
+        $doc3->getId()->willReturn(3);
+        $doc3->getFileName()->willReturn('file-name3.pdf');
+
+        $reportSubmission->isDownloadable()->willReturn(true);
+        $reportSubmission->getDocuments()->willReturn(new ArrayCollection([$doc1->reveal(), $doc2->reveal(), $doc3->reveal()]));
+        $reportSubmission->getZipName()->willReturn('Report_12345678_2017_2018.zip');
+
+        $sut = new DocumentsZipFileCreator($reportSubmission->reveal(), $storage->reveal());
+
+        $fileName = $sut->createZipFile();
+
+        self::assertTrue(file_exists($fileName));
+
+        $za = new ZipArchive();
+        $za->open($fileName);
+
+        self::assertEquals(2, $za->numFiles);
     }
 
     public function tearDown(): void
