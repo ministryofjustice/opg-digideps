8a1795cb78d84b3cb261bc4469f702cf7e90fc7f
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2019-09-09 17:30:48 +0100

    Removes redundant tests from DocumentZipFileCreatorTest
    After extracting responsibilities not linked to zipping files from DocumentZipFileCreator most of the exiting tests were not required.

--- client/tests/phpunit/Service/File/DocumentZipFileCreatorTest.php
+++ client/tests/phpunit/Service/File/DocumentZipFileCreatorTest.php
@@ -23,103 +23,41 @@ class DocumentZipFileCreatorTest extends \PHPUnit_Framework_TestCase
      */
     private $object;
 
-//    public function setUp()
-//    {
-//        $this->storage = m::mock(StorageInterface::class);
-//        $this->reportSubmission = m::mock(ReportSubmission::class, [
-//            'getDocuments' => [],
-//        ]);
-//
-//        $this->object = new DocumentsZipFileCreator($this->reportSubmission, $this->storage);
-//    }
-//
-//    public function testcreateZipFileNoDocuments()
-//    {
-//        $this->reportSubmission->shouldReceive('isDownloadable')->once()->withNoArgs()->andReturn(true);
-//        $this->reportSubmission->shouldReceive('getDocuments')->andReturn([]);
-//
-//        $this->setExpectedException('RuntimeException');
-//        $this->object->createZipFile();
-//    }
-//
-//    public function testcreateZipFileNotDownloadable()
-//    {
-//        $this->reportSubmission->shouldReceive('isDownloadable')->once()->withNoArgs()->andReturn(false);
-//
-//        $this->setExpectedException('RuntimeException', DocumentsZipFileCreator::MSG_NOT_DOWNLOADABLE);
-//        $this->object->createZipFile();
-//    }
-//
-//    public function testcreateZipFile()
-//    {
-//        $zipFileName = 'Report_12345678_2017_2018.zip';
-//        $doc1 = m::mock(Document::class, [
-//            'getId' => 1,
-//            'getFileName' => 'file1.pdf',
-//            'getStorageReference' => 'r1'
-//        ]);
-//        $doc2 = m::mock(Document::class, [
-//            'getId' => 2,
-//            'getFileName' => 'file2.pdf',
-//            'getStorageReference' => 'r2'
-//        ]);
-//        $this->storage->shouldReceive('retrieve')->with('r1')->andReturn('doc1-content');
-//        $this->storage->shouldReceive('retrieve')->with('r2')->andReturn('doc2-content');
-//
-//        $this->reportSubmission
-//            ->shouldReceive('getZipName')->andReturn($zipFileName)
-//            ->shouldReceive('getDocuments')->andReturn([$doc1, $doc2])
-//            ->shouldReceive('isDownloadable')->once()->withNoArgs()->andReturn(true)
-//        ;
-//
-//        $fileName = $this->object->createZipFile();
-//
-//        $this->assertContains($zipFileName, $fileName);
-//        $this->assertEquals('doc1-content', exec("unzip -c $fileName file1.pdf"));
-//        $this->assertEquals('doc2-content', exec("unzip -c $fileName file2.pdf"));
-//
-//        $this->object->cleanUp();
-//
-//        $this->assertFalse(file_exists($fileName));
-//    }
+    public function setUp()
+    {
+        $this->storage = m::mock(StorageInterface::class);
+        $this->reportSubmission = m::mock(ReportSubmission::class, [
+            'getDocuments' => [],
+        ]);
+
+        $this->object = new DocumentsZipFileCreator($this->reportSubmission, $this->storage);
+    }
 
     /**
      * @group acs
      */
-    public function testGracefullyHandleMissingFiles()
+    public function testcreateZipFile()
     {
-        $doc1 = self::prophesize(Document::class);
-        $doc1->getStorageReference()->willReturn('ref-1');
-        $doc1->getId()->willReturn(1);
-        $doc1->getFileName()->willReturn('file-name1.pdf');
-
-        $doc2 = self::prophesize(Document::class);
-        $doc2->getStorageReference()->willReturn('ref-2');
-        $doc2->getId()->willReturn(2);
-        $doc2->getFileName()->willReturn('file-name2.pdf');
-
-        $doc3 = self::prophesize(Document::class);
-        $doc3->getStorageReference()->willReturn('ref-3');
-        $doc3->getId()->willReturn(3);
-        $doc3->getFileName()->willReturn('file-name3.pdf');
-
         /** @var ObjectProphecy|ReportSubmission $reportSubmission */
         $reportSubmission = self::prophesize(ReportSubmission::class);
-        $reportSubmission->isDownloadable()->willReturn(true);
-        $reportSubmission->getDocuments()->willReturn(new ArrayCollection([$doc1->reveal(), $doc2->reveal(), $doc3->reveal()]));
         $reportSubmission->getZipName()->willReturn('Report_12345678_2017_2018.zip');
 
-        $documentsContents = ['file-name1.pdf' => 'doc1 content', 'file-name3.pdf' => 'doc3 content'];
+        $documentsContents = [
+            'file-name1.pdf' => 'doc1 content',
+            'file-name2.pdf' => 'doc2 content',
+            'file-name3.pdf' => 'doc3 content'
+        ];
+
         $sut = new DocumentsZipFileCreator();
 
         $fileName = $sut->createZipFileFromDocumentContents($documentsContents, $reportSubmission->reveal());
-        
+
         self::assertTrue(file_exists($fileName));
 
         $zip = new ZipArchive();
         $zip->open($fileName);
 
-        self::assertEquals(2, $zip->numFiles);
+        self::assertEquals(3, $zip->numFiles);
     }
 
     /**
