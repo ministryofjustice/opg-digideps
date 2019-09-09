282e7644cd3f77862add27d1b2349009038dd2b5
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2019-09-09 17:11:21 +0100

    Refactor zip process
    There was duplication of code between MultiDocumentZipFileCreator and DocumentZipFileCreator which has now been reduced by extracting and simplifying the functionality
    
    of MultiDocumentZipFileCreator into DocumentZipFileCreator. The controller now has slightly more actions present but this has facilitated removing MultiDocumentZipFileCreator completely.

--- client/src/AppBundle/Service/File/DocumentsZipFileCreator.php
+++ client/src/AppBundle/Service/File/DocumentsZipFileCreator.php
@@ -4,8 +4,6 @@ namespace AppBundle\Service\File;
 
 use AppBundle\Entity\Report\Document;
 use AppBundle\Entity\Report\ReportSubmission;
-use AppBundle\Service\File\Storage\FileNotFoundException;
-use AppBundle\Service\File\Storage\StorageInterface;
 use ZipArchive;
 
 class DocumentsZipFileCreator
@@ -14,62 +12,44 @@ class DocumentsZipFileCreator
     const MSG_NOT_DOWNLOADABLE = 'This report is not downloadable';
 
     /**
-     * @var ReportSubmission
+     * @var array
      */
-    private $reportSubmission;
+    private $zipFiles;
 
     /**
-     * @var StorageInterface
+     * @param array $documentsContents
+     * @param ReportSubmission $reportSubmission
+     * @return string
      */
-    private $s3Storage;
-
-    /**
-     * @var string
-     */
-    private $zipFile;
-
-    /**
-     * DocumentsZipFileCreator constructor.
-     * @param $reportSubmission
-     * @param $s3Storage
-     */
-    public function __construct(ReportSubmission $reportSubmission, StorageInterface $s3Storage)
-    {
-        $this->reportSubmission = $reportSubmission;
-        $this->s3Storage = $s3Storage;
-    }
-
-    public function createZipFile()
+    public function createZipFileFromDocumentContents(array $documentsContents, ReportSubmission $reportSubmission)
     {
         // store files locally, for subsequent memory-less ZIP creation
         $filesToAdd = [];
 
-        if ($this->reportSubmission->isDownloadable() !== true) {
+        if ($reportSubmission->isDownloadable() !== true) {
             throw new \RuntimeException(self::MSG_NOT_DOWNLOADABLE);
         }
 
-        if (empty($this->reportSubmission->getDocuments())) {
+        if (empty($reportSubmission->getDocuments())) {
             throw new \RuntimeException('No documents found for downloading');
         }
-        foreach ($this->reportSubmission->getDocuments() as $document) {
-            try {
-                $content = $this->s3Storage->retrieve($document->getStorageReference()); //might throw exception
-                $dfile = self::createDocumentTmpFilePath($document);
-                file_put_contents($dfile, $content);
-                unset($content);
-                $filesToAdd[$document->getFileName()] = $dfile;
-            } catch(FileNotFoundException $e) {
-                echo "Error caught";
-            }
+
+        foreach ($documentsContents as $fileName => $content) {
+            $document = self::createDocumentTmpFilePath($fileName);
+            file_put_contents($document, $content);
+            unset($content);
+            $filesToAdd[$fileName] = $document;
         }
 
         // create ZIP files and add previously-stored uploaded documents
-        $filename = self::createZipFilePath($this->reportSubmission);
+        $zipFileName = self::createZipFilePath($reportSubmission);
         $zip = new ZipArchive();
-        $zip->open($filename, ZipArchive::CREATE | ZipArchive::OVERWRITE | ZipArchive::CHECKCONS);
-        foreach ($filesToAdd as $localname => $filePath) {
-            $zip->addFile($filePath, $localname);
+        $zip->open($zipFileName, ZipArchive::CREATE | ZipArchive::OVERWRITE | ZipArchive::CHECKCONS);
+
+        foreach ($filesToAdd as $localName => $filePath) {
+            $zip->addFile($filePath, $localName);
         }
+
         $zip->close();
         unset($zip);
 
@@ -78,18 +58,47 @@ class DocumentsZipFileCreator
             unlink($f);
         }
 
-        $this->zipFile = $filename;
+        $this->zipFiles[] = $zipFileName;
 
-        return $filename;
+        return $zipFileName;
     }
 
     /**
-     * remove temporarly files
+     * @param array $zipFiles
+     * @return string
+     */
+    public function createMultiZipFile(array $zipFiles)
+    {
+        $parentFilename = self::createMultiZipFilePath();
+
+        $zip = new ZipArchive();
+        $zip->open(
+            $parentFilename,
+            ZipArchive::CREATE | ZipArchive::OVERWRITE | ZipArchive::CHECKCONS
+        );
+
+        //add each individual zipped report into the main zip file
+        foreach ($zipFiles as $zipFile) {
+            $zip->addFile($zipFile, basename($zipFile));
+        }
+
+        $zip->close();
+        $this->zipFiles[] = $parentFilename;
+
+        return $parentFilename;
+    }
+
+    /**
+     * remove temporary files
      */
     public function cleanUp()
     {
-        if ($this->zipFile && file_exists($this->zipFile)) {
-            unlink($this->zipFile);
+        if (!empty($this->zipFiles)) {
+            foreach($this->zipFiles as $zipfile) {
+                if (file_exists($zipfile)) {
+                    unlink($zipfile);
+                }
+            }
         }
     }
 
@@ -97,9 +106,9 @@ class DocumentsZipFileCreator
      * @param  Document $document
      * @return string
      */
-    private static function createDocumentTmpFilePath(Document $document)
+    private static function createDocumentTmpFilePath(string $fileName)
     {
-        return self::TMP_ROOT_PATH . 'dd_temp_zip_' . $document->getId() . microtime(1);
+        return self::TMP_ROOT_PATH . 'dd_temp_zip_' . $fileName . microtime(1);
     }
 
     /**
@@ -109,6 +118,14 @@ class DocumentsZipFileCreator
     private static function createZipFilePath(ReportSubmission $reportSubmission)
     {
         return self::TMP_ROOT_PATH . $reportSubmission->getZipName();
+    }
+
+    /**
+     * @return string
+     */
+    private static function createMultiZipFilePath()
+    {
+        return self::TMP_ROOT_PATH . '/multidownload-' . microtime(1) . '.zip';
     }
 
     public function __destruct()
