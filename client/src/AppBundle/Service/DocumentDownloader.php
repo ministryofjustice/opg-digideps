4cbde51fa14b47bf6cb653b2b5f70fec8936a87c
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2019-09-18 14:42:46 +0100

    Move to Twig for rendering missing docs flash message
    Following PR comments this change moves to using Twig for rendering the flash message content for both clarity and security to help protect against XSS attacks

--- client/src/AppBundle/Service/DocumentDownloader.php
+++ client/src/AppBundle/Service/DocumentDownloader.php
@@ -87,8 +87,6 @@ class DocumentDownloader
         $response->headers->set('Content-type', 'application/octet-stream');
         $response->headers->set('Content-Description', 'File Transfer');
         $response->headers->set('Content-Disposition', 'attachment; filename="' . basename($fileName) . '";');
-        // currently disabled as behat goutte driver gets a corrupted file with this setting
-        //$response->headers->set('Content-Length', filesize($filename));
         $response->sendHeaders();
         $response->setContent(readfile($fileName));
         return $response;
