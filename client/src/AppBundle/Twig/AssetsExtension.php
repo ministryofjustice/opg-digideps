4f63f87f31a16f4bb912bfa3cadc428b93382199
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2019-09-20 14:52:11 +0100

    Always get latest CSS file, not earliest

--- client/src/AppBundle/Twig/AssetsExtension.php
+++ client/src/AppBundle/Twig/AssetsExtension.php
@@ -49,7 +49,7 @@ class AssetsExtension extends \Twig_Extension
         if (!$this->tag) {
             // List the files in the web/assets folder
             $assetRoot = $this->rootDir . '/../web/assets';
-            $assetContents = array_diff(scandir($assetRoot), ['..', '.']);
+            $assetContents = array_diff(scandir($assetRoot, SCANDIR_SORT_DESCENDING), ['..', '.']);
 
             // set the value to the folder we find.
             $this->tag = array_values($assetContents)[0];
