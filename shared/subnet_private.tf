d56979b5184283f9372106952e2d31ba1c1f6e88
Luke Bradbury <luke@nqyr.io>
2019-09-23 11:01:11 +0100

    Fixup tags

---
+++ shared/subnet_private.tf
@@ -0,0 +1,25 @@
+resource "aws_subnet" "private" {
+  count             = 3
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 7, count.index + 95)
+  availability_zone = data.aws_availability_zones.all.names[count.index]
+  vpc_id            = aws_vpc.main.id
+  tags = merge(
+    local.default_tags,
+    { Name = "private" },
+  )
+}
+
+resource "aws_route_table_association" "private" {
+  count          = 3
+  route_table_id = aws_route_table.private[count.index].id
+  subnet_id      = aws_subnet.private[count.index].id
+}
+
+resource "aws_route_table" "private" {
+  count  = 3
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat[count.index].id
+  }
+}
