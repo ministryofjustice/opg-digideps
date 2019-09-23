b1437664bd9a8259614b42364df925bbb348cf53
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2019-09-23 10:52:20 +0100

    Switch to JavaScript Standard Style

--- client/src/AppBundle/Resources/assets/javascripts/modules/cookieBanner.js
+++ client/src/AppBundle/Resources/assets/javascripts/modules/cookieBanner.js
@@ -1,32 +1,30 @@
-/*jshint browser: true */
 (function () {
-    "use strict";
+  'use strict'
 
-    var root = this,
-        $ = root.jQuery;
+  var root = this
+  var $ = root.jQuery
 
-    if (typeof GOVUK === 'undefined') {
-        root.GOVUK = {};
-    }
+  if (typeof GOVUK === 'undefined') {
+    root.GOVUK = {}
+  }
 
-    function cookieBanner() {
-        var $banner = $('[data-module="opg-cookie-banner"]');
-        var $acceptAll = $banner.find('[data-js="accept-all"]');
+  function cookieBanner () {
+    var $banner = $('[data-module="opg-cookie-banner"]')
+    var $acceptAll = $banner.find('[data-js="accept-all"]')
 
-        $acceptAll.on('click', function(event) {
-            var days = 365;
-            var expires = new Date(Date.now() + days * 864e5).toUTCString();
-            var policy = {
-                essential: true,
-                usage: true,
-            };
+    $acceptAll.on('click', function (event) {
+      var days = 365
+      var expires = new Date(Date.now() + days * 864e5).toUTCString()
+      var policy = {
+        essential: true,
+        usage: true
+      }
 
-            document.cookie = 'cookie_policy=' + JSON.stringify(policy) + '; expires=' + expires + '; secure';
-            $banner.addClass('hidden');
-            event.preventDefault();
-        });
-    }
+      document.cookie = 'cookie_policy=' + JSON.stringify(policy) + '; expires=' + expires + '; secure'
+      $banner.addClass('hidden')
+      event.preventDefault()
+    })
+  }
 
-    root.GOVUK.cookieBanner = cookieBanner;
-
-}).call(this);
+  root.GOVUK.cookieBanner = cookieBanner
+}).call(this)
