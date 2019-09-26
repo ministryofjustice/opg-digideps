6d5b9a9bfee680381df72fc96a3a4349e91b4f6d
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2019-09-26 13:07:28 +0100

    Set start/end date based on period selection

--- client/src/AppBundle/Form/Admin/StatPeriodType.php
+++ client/src/AppBundle/Form/Admin/StatPeriodType.php
@@ -5,6 +5,8 @@ namespace AppBundle\Form\Admin;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type as FormTypes;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class StatPeriodType extends AbstractType
@@ -41,6 +43,24 @@ class StatPeriodType extends AbstractType
                 'data' => new \DateTime()
             ])
             ->add('update', FormTypes\SubmitType::class);
+
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) {
+            $data = $event->getData();
+
+            if ($data['period'] === 'last-30') {
+                $startDate = new \DateTime('-30 days');
+                $data['startDate'] = ['day' => date('d', $startDate), 'month' => date('m', $startDate), 'year' => date('Y', $startDate)];
+                $data['endDate'] = ['day' => date('d'), 'month' => date('m'), 'year' => date('Y')];
+            } elseif ($data['period'] === 'this-year') {
+                $data['startDate'] = ['day' => 1, 'month' => 1, 'year' => date('Y')];
+                $data['endDate'] = ['day' => date('d'), 'month' => date('m'), 'year' => date('Y')];
+            } elseif ($data['period'] === 'all-time') {
+                $data['startDate'] = ['day' => 1, 'month' => 1, 'year' => 2000];
+                $data['endDate'] = ['day' => date('d'), 'month' => date('m'), 'year' => date('Y')];
+            }
+
+            $event->setData($data);
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver)
