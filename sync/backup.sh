be9f3e5dbe1765cb3212599512641ac25d9cc4f1
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2019-10-10 11:50:55 +0100

    Break sync service out into backup and restore

---
+++ sync/backup.sh
@@ -0,0 +1,63 @@
+#! /bin/sh
+
+set -e
+set -o pipefail
+
+if [ "${S3_BUCKET}" = "**None**" ]; then
+  echo "You need to set the S3_BUCKET environment variable."
+  exit 1
+fi
+
+if [ "${S3_ENDPOINT}" = "**None**" ]; then
+  echo "You need to set the S3_ENDPOINT environment variable."
+  exit 1
+fi
+
+if [ "${S3_S3V4}" = "**None**" ]; then
+  echo "You need to set the S3_S3V4 environment variable."
+  exit 1
+fi
+
+if [ "${POSTGRES_DATABASE}" = "**None**" ]; then
+  echo "You need to set the POSTGRES_DATABASE environment variable."
+  exit 1
+fi
+
+if [ "${POSTGRES_HOST}" = "**None**" ]; then
+  if [ -n "${POSTGRES_PORT_5432_TCP_ADDR}" ]; then
+    POSTGRES_HOST=$POSTGRES_PORT_5432_TCP_ADDR
+    POSTGRES_PORT=$POSTGRES_PORT_5432_TCP_PORT
+  else
+    echo "You need to set the POSTGRES_HOST environment variable."
+    exit 1
+  fi
+fi
+
+if [ "${POSTGRES_USER}" = "**None**" ]; then
+  echo "You need to set the POSTGRES_USER environment variable."
+  exit 1
+fi
+
+if [ "${POSTGRES_PASSWORD}" = "**None**" ]; then
+  echo "You need to set the POSTGRES_PASSWORD environment variable or link to a container named POSTGRES."
+  exit 1
+fi
+
+export PGPASSWORD=$POSTGRES_PASSWORD
+POSTGRES_HOST_OPTS="-h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER $POSTGRES_EXTRA_OPTS"
+
+echo "Creating dump of ${POSTGRES_DATABASE} database from ${POSTGRES_HOST}..."
+
+pg_dump $POSTGRES_HOST_OPTS $POSTGRES_DATABASE | gzip > dump.sql.gz
+
+echo "Uploading dump to $S3_BUCKET"
+
+if [ "${S3_S3V4}" = "yes" ]; then
+    aws configure set default.s3.signature_version s3v4
+fi
+
+FILE_NAME=${POSTGRES_DATABASE}_$(date +"%Y-%m-%dT%H:%M:%SZ").sql.gz
+
+cat dump.sql.gz | aws --endpoint-url $S3_ENDPOINT s3 cp - s3://$S3_BUCKET/$S3_PREFIX/${FILE_NAME} || exit 2
+
+echo "SQL backup uploaded successfully"
--- sync/restore.sh
+++ sync/restore.sh
@@ -3,23 +3,23 @@
 set -e
 set -o pipefail
 
-if [ "${POSTGRES_DATABASE_DESTINATION}" = "**None**" ]; then
-  echo "You need to set the POSTGRES_DATABASE_DESTINATION environment variable."
+if [ "${POSTGRES_DATABASE}" = "**None**" ]; then
+  echo "You need to set the POSTGRES_DATABASE environment variable."
   exit 1
 fi
 
-if [ "${POSTGRES_HOST_DESTINATION}" = "**None**" ]; then
+if [ "${POSTGRES_HOST}" = "**None**" ]; then
   if [ -n "${POSTGRES_PORT_5432_TCP_ADDR}" ]; then
     POSTGRES_HOST=$POSTGRES_PORT_5432_TCP_ADDR
     POSTGRES_PORT=$POSTGRES_PORT_5432_TCP_PORT
   else
-    echo "You need to set the POSTGRES_HOST_DESTINATION environment variable."
+    echo "You need to set the POSTGRES_HOST environment variable."
     exit 1
   fi
 fi
 
-if [ "${POSTGRES_USER_DESTINATION}" = "**None**" ]; then
-  echo "You need to set the POSTGRES_USER_DESTINATION environment variable."
+if [ "${POSTGRES_USER}" = "**None**" ]; then
+  echo "You need to set the POSTGRES_USER environment variable."
   exit 1
 fi
 
@@ -29,8 +29,8 @@ if [ "${POSTGRES_PASSWORD_DESTIONATION}" = "**None**" ]; then
 fi
 
 
-export PGPASSWORD=$POSTGRES_PASSWORD_DESTINATION
-POSTGRES_HOST_OPTS_DESTINATION="-h $POSTGRES_HOST_DESTINATION -p $POSTGRES_PORT_DESTINATION -U $POSTGRES_USER_DESTINATION"
+export PGPASSWORD=$POSTGRES_PASSWORD
+POSTGRES_HOST_OPTS="-h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER"
 
 echo "Finding latest backup"
 
@@ -43,11 +43,11 @@ gzip -d dump.sql.gz
 
 if [ "${DROP_PUBLIC}" == "yes" ]; then
 	echo "Recreating the public schema"
-	psql $POSTGRES_HOST_OPTS_DESTINATION -d $POSTGRES_DATABASE_DESTINATION -c "drop schema public cascade; create schema public;"
+	psql $POSTGRES_HOST_OPTS -d $POSTGRES_DATABASE -c "drop schema public cascade; create schema public;"
 fi
 
 echo "Restoring ${LATEST_BACKUP}"
 
-psql $POSTGRES_HOST_OPTS_DESTINATION -d $POSTGRES_DATABASE_DESTINATION < dump.sql
+psql $POSTGRES_HOST_OPTS -d $POSTGRES_DATABASE < dump.sql
 
 echo "Restore complete"
