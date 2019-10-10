302375b33476ebacf6fb767d03deb1a7173ad4bb
Greg Tyler <gregtyler@users.noreply.github.com>
2019-10-10 09:10:28 +0100

    Rename Version223 to Version221
    I was trying to account for DDPB-2842, but that's just going to confuse matters more

--- api/app/DoctrineMigrations/Version221.php
+++ api/app/DoctrineMigrations/Version221.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version223 extends AbstractMigration
+final class Version221 extends AbstractMigration
 {
     public function getDescription() : string
     {
