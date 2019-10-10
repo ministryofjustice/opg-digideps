e13c90e69798f833d6f45db637e8019fbe835e1b
Shaun Lizzio <shaun@lizzio.co.uk>
2019-10-10 13:18:07 +0100

    DDPB-2923 default public domain to false

--- api/app/DoctrineMigrations/Version221.php
+++ api/app/DoctrineMigrations/Version221.php
@@ -22,7 +22,7 @@ final class Version221 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('ALTER TABLE organisation ADD is_public_domain BOOLEAN DEFAULT \'true\' NOT NULL');
+        $this->addSql('ALTER TABLE organisation ADD is_public_domain BOOLEAN DEFAULT \'false\' NOT NULL');
     }
 
     public function down(Schema $schema) : void
