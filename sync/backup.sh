88e488f97244a665f2381f0a7f6d8957089ce74b
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2019-10-11 11:04:51 +0100

    Refactor sync scripts

--- sync/backup.sh
+++ sync/backup.sh
@@ -3,64 +3,13 @@
 set -e
 set -o pipefail
 
-if [ "${S3_BUCKET}" = "**None**" ]; then
-  echo "You need to set the S3_BUCKET environment variable."
-  exit 1
-fi
-
-if [ "${S3_ENDPOINT}" = "**None**" ]; then
-  echo "You need to set the S3_ENDPOINT environment variable."
-  exit 1
-fi
-
-if [ "${S3_S3V4}" = "**None**" ]; then
-  echo "You need to set the S3_S3V4 environment variable."
-  exit 1
-fi
-
-if [ "${POSTGRES_DATABASE}" = "**None**" ]; then
-  echo "You need to set the POSTGRES_DATABASE environment variable."
-  exit 1
-fi
-
-if [ "${POSTGRES_HOST}" = "**None**" ]; then
-  if [ -n "${POSTGRES_PORT_5432_TCP_ADDR}" ]; then
-    POSTGRES_HOST=$POSTGRES_PORT_5432_TCP_ADDR
-    POSTGRES_PORT=$POSTGRES_PORT_5432_TCP_PORT
-  else
-    echo "You need to set the POSTGRES_HOST environment variable."
-    exit 1
-  fi
-fi
-
-if [ "${POSTGRES_USER}" = "**None**" ]; then
-  echo "You need to set the POSTGRES_USER environment variable."
-  exit 1
-fi
-
-if [ "${POSTGRES_PASSWORD}" = "**None**" ]; then
-  echo "You need to set the POSTGRES_PASSWORD environment variable or link to a container named POSTGRES."
-  exit 1
-fi
-
-export PGPASSWORD=$POSTGRES_PASSWORD
-POSTGRES_HOST_OPTS="-h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER $POSTGRES_EXTRA_OPTS"
+source common.sh
 
 echo "Creating dump of ${POSTGRES_DATABASE} database from ${POSTGRES_HOST}..."
 
 pg_dump $POSTGRES_HOST_OPTS $POSTGRES_DATABASE | gzip > dump.sql.gz
 
 echo "Uploading dump to $S3_BUCKET"
-
-if [ "${S3_S3V4}" = "yes" ]; then
-    aws configure set default.s3.signature_version s3v4
-fi
-
-if [ "${S3_ENDPOINT}" == "**None**" ]; then
-  AWS_ARGS=""
-else
-  AWS_ARGS="--endpoint-url ${S3_ENDPOINT}"
-fi
 
 FILE_NAME=${POSTGRES_DATABASE}_$(date +"%Y-%m-%dT%H:%M:%SZ").sql.gz
 
--- sync/restore.sh
+++ sync/restore.sh
@@ -3,40 +3,7 @@
 set -e
 set -o pipefail
 
-if [ "${POSTGRES_DATABASE}" = "**None**" ]; then
-  echo "You need to set the POSTGRES_DATABASE environment variable."
-  exit 1
-fi
-
-if [ "${POSTGRES_HOST}" = "**None**" ]; then
-  if [ -n "${POSTGRES_PORT_5432_TCP_ADDR}" ]; then
-    POSTGRES_HOST=$POSTGRES_PORT_5432_TCP_ADDR
-    POSTGRES_PORT=$POSTGRES_PORT_5432_TCP_PORT
-  else
-    echo "You need to set the POSTGRES_HOST environment variable."
-    exit 1
-  fi
-fi
-
-if [ "${POSTGRES_USER}" = "**None**" ]; then
-  echo "You need to set the POSTGRES_USER environment variable."
-  exit 1
-fi
-
-if [ "${POSTGRES_PASSWORD_DESTIONATION}" = "**None**" ]; then
-  echo "You need to set the POSTGRES_PASSWORD_DESTIONATION environment variable or link to a container named POSTGRES."
-  exit 1
-fi
-
-
-export PGPASSWORD=$POSTGRES_PASSWORD
-POSTGRES_HOST_OPTS="-h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER"
-
-if [ "${S3_ENDPOINT}" == "**None**" ]; then
-  AWS_ARGS=""
-else
-  AWS_ARGS="--endpoint-url ${S3_ENDPOINT}"
-fi
+source common.sh
 
 echo "Finding latest backup"
 
