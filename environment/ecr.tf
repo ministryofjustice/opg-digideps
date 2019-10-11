542a367a4a412d4896d34573ded2fe5f9d261aba
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2019-10-11 13:12:40 +0100

    Fix typo in file-scanner repository name

--- environment/ecr.tf
+++ environment/ecr.tf
@@ -2,7 +2,7 @@ locals {
   images = {
     api          = "${data.aws_ecr_repository.images["api"].repository_url}:${var.OPG_DOCKER_TAG}"
     client       = "${data.aws_ecr_repository.images["client"].repository_url}:${var.OPG_DOCKER_TAG}"
-    file_scanner = "${data.aws_ecr_repository.images["file_scanner"].repository_url}:latest"
+    file_scanner = "${data.aws_ecr_repository.images["file-scanner"].repository_url}:latest"
     sync         = "${data.aws_ecr_repository.images["sync"].repository_url}:${var.OPG_DOCKER_TAG}"
     wkhtmltopdf  = "${data.aws_ecr_repository.images["wkhtmltopdf"].repository_url}:latest"
   }
@@ -10,7 +10,7 @@ locals {
   repositories = [
     "api",
     "client",
-    "file_scanner",
+    "file-scanner",
     "sync",
     "wkhtmltopdf",
   ]
