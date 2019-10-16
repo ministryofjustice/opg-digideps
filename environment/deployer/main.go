b096862680080651a60514a9ab6906e4dba098fe
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2019-10-16 17:45:30 +0100

    Further refinements

--- environment/deployer/main.go
+++ environment/deployer/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
@@ -14,7 +16,6 @@ import (
 	"time"
 )
 
-
 func (t *Task) IsStopped() bool {
 	return *t.task.LastStatus != "STOPPED"
 }
@@ -24,64 +25,109 @@ func (t *Task) GetTaskID() string {
 }
 
 type Task struct {
-	svc *ecs.ECS
-	task *ecs.Task
+	svc   *ecs.ECS
+	task  *ecs.Task
+	input *ecs.RunTaskInput
 }
 
+type Log struct {
+	svc   *cloudwatchlogs.CloudWatchLogs
+	input *cloudwatchlogs.GetLogEventsInput
+}
+
+type Poll struct {
+	count    int
+	interval int
+	timeOut  int
+}
+
+type Config struct {
+	Sensitive bool
+	Type []interface{}
+	Value struct{
+		AccountID      *string
+		Cluster        *string
+		SecurityGroups []*string
+		Subnets        []*string
+		TaskDefinition *string
+	}
+}
+
+// TF_WORKSPACE must exist - CI
+// Role - default to CI, allow overriding
+// Task Definition Name - Command line or env var
+
+// Terraform output
+
+// Cluster - Terrform output, push to file
+// Security Groups - Terrform output, push to file
+// Subnets - Terrform output, push to file
+// Account ID - terraform.tfvars.json
+
+
 func main() {
-	var task Task
-	cluster := "ddpb2944"
-	securityGroups := []string{"sg-0ee40a8bbc67747e3"}
-	subnets := []string{"subnet-d0b880a6", "subnet-a31455fb", "subnet-9ad4d1fe"}
-	taskDefinition := "sync-ddpb2944"
-	command := []string{"./backup.sh"}
-	containerName := "sync"
-	logGroupName := "ddpb2944"
-	streamPrefix := "sync"
-	delay := 5
-	timeOut := getEnvInt("DEPLOYER_TIMEOUT")
+	config := NewConfig("output.json", "sync")
 
 	sess, _ := session.NewSession()
-	creds := stscreds.NewCredentials(sess, "arn:aws:iam::248804316466:role/operator")
+	creds := stscreds.NewCredentials(sess, fmt.Sprintf("arn:aws:iam::%s:role/operator", *config.Value.AccountID))
+
 	awsConfig := aws.Config{Credentials: creds, Region: aws.String("eu-west-1")}
-	task.svc = ecs.New(sess, &awsConfig)
 
-	cloudwatchLogsSvc := cloudwatchlogs.New(sess, &awsConfig)
-
-	//run task
-	task.Run(cluster, securityGroups, subnets, taskDefinition, command, containerName)
-
-	cloudwatchLogsInput := &cloudwatchlogs.GetLogEventsInput{
-		LogGroupName:  aws.String(logGroupName),
-		LogStreamName: aws.String(fmt.Sprintf("%s/%s/%s", streamPrefix, containerName, task.GetTaskID())),
-		StartFromHead: aws.Bool(true),
+	task := Task{
+		svc: ecs.New(sess, &awsConfig),
+		input: &ecs.RunTaskInput{
+			Cluster:    config.Value.Cluster,
+			LaunchType: aws.String("FARGATE"),
+			NetworkConfiguration: &ecs.NetworkConfiguration{
+				AwsvpcConfiguration: &ecs.AwsVpcConfiguration{
+					SecurityGroups: config.Value.SecurityGroups,
+					Subnets:        config.Value.Subnets,
+				},
+			},
+			TaskDefinition: config.Value.TaskDefinition,
+			Overrides: &ecs.TaskOverride{
+				ContainerOverrides: []*ecs.ContainerOverride{
+					{
+						Command: aws.StringSlice([]string{"./backup.sh"}),
+						Name:    aws.String("sync"),
+					},
+				},
+			},
+		},
 	}
 
-	count := 0
+	//run task
+	task.Run()
+
+	logConfigurationOptions := task.GetLogConfigurationOptions()
+
+	cwLog := Log{
+		svc: cloudwatchlogs.New(sess, &awsConfig),
+		input: &cloudwatchlogs.GetLogEventsInput{
+			LogGroupName:  logConfigurationOptions["awslogs-group"],
+			LogStreamName: aws.String(fmt.Sprintf("%s/%s/%s", *logConfigurationOptions["awslogs-stream-prefix"], task.GetContainerName(), task.GetTaskID())),
+			StartFromHead: aws.Bool(true),
+		},
+	}
+
+	poll := Poll{
+		count:    0,
+		interval: 5,
+		timeOut:  getEnvInt("DEPLOYER_TIMEOUT", "60"),
+	}
 
 	task.Update()
 
 	for task.IsStopped() {
-
 		task.Update()
-		cloudwatchLogsOutput, err := cloudwatchLogsSvc.GetLogEvents(cloudwatchLogsInput)
 
-		if err != nil {
-			log.Println(err)
+		cwLog.PrintLogEvents()
+
+		if poll.IsTimedOut() {
+			log.Fatalf("Timed out after %v\n", poll.timeOut)
 		}
 
-		cloudwatchLogsInput.NextToken = cloudwatchLogsOutput.NextForwardToken
-
-		for _, event := range cloudwatchLogsOutput.Events {
-			log.Println(*event.Message)
-		}
-
-		if count * delay >= timeOut {
-			log.Fatalf("Timed out after %v\n", timeOut)
-		} 
-
-		time.Sleep(time.Duration(delay) * time.Second)
-		count++
+		poll.Sleep()
 	}
 
 	log.Printf("Container exited with code %d", *task.task.Containers[0].ExitCode)
@@ -89,28 +135,8 @@ func main() {
 	os.Exit(int(*task.task.Containers[0].ExitCode))
 }
 
-func (t *Task) Run(cluster string, securityGroups []string, subnets []string, taskDefinition string, command []string, containerName string) {
-	taskInput := &ecs.RunTaskInput{
-		Cluster:    aws.String(cluster),
-		LaunchType: aws.String("FARGATE"),
-		NetworkConfiguration: &ecs.NetworkConfiguration{
-			AwsvpcConfiguration: &ecs.AwsVpcConfiguration{
-				SecurityGroups: aws.StringSlice(securityGroups),
-				Subnets:        aws.StringSlice(subnets),
-			},
-		},
-		TaskDefinition: aws.String(taskDefinition),
-		Overrides: &ecs.TaskOverride{
-			ContainerOverrides: []*ecs.ContainerOverride{
-				{
-					Command: aws.StringSlice(command),
-					Name:    aws.String(containerName),
-				},
-			},
-		},
-	}
-
-	tasksOutput, err := t.svc.RunTask(taskInput)
+func (t *Task) Run() {
+	tasksOutput, err := t.svc.RunTask(t.input)
 
 	if err != nil {
 		log.Fatalln(err)
@@ -134,12 +160,70 @@ func (t *Task) Update() {
 	t.task = describeTasksOutput.Tasks[0]
 }
 
-func getEnvInt(name string) int {
-	env, err := strconv.Atoi(os.Getenv(name))
+func (t *Task) GetContainerName() string {
+	return *t.task.Containers[0].Name
+}
+
+func (t *Task) GetLogConfigurationOptions() map[string]*string {
+	output, err := t.svc.DescribeTaskDefinition(&ecs.DescribeTaskDefinitionInput{
+		TaskDefinition: t.input.TaskDefinition,
+	})
 
 	if err != nil {
-		log.Fatalf("Error getting %s: %v", name, err)
+		log.Fatalln(err)
 	}
 
-	return env
+	return output.TaskDefinition.ContainerDefinitions[0].LogConfiguration.Options
+}
+
+func getEnvInt(name string, defaultVar string) int {
+	env, isSet := os.LookupEnv(name)
+
+	if !isSet {
+		env = defaultVar
+	}
+
+	intEnv, err := strconv.Atoi(env)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return intEnv
+}
+
+func (l *Log) PrintLogEvents() {
+	cloudwatchLogsOutput, err := l.svc.GetLogEvents(l.input)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	l.input.NextToken = cloudwatchLogsOutput.NextForwardToken
+
+	for _, event := range cloudwatchLogsOutput.Events {
+		log.Println(*event.Message)
+	}
+}
+
+func (p *Poll) IsTimedOut() bool {
+	return p.count*p.interval >= p.timeOut
+}
+
+func (p *Poll) Sleep() {
+	time.Sleep(time.Duration(p.interval) * time.Second)
+	p.count++
+}
+
+func NewConfig(fileName, taskName string) Config {
+	byteValue, _ := ioutil.ReadFile(fileName)
+
+	var configs map[string]Config
+	err := json.Unmarshal(byteValue, &configs)
+	
+	if err != nil{
+		log.Fatalln(err)
+	}
+
+	return configs[taskName]
 }
