08a73ddd5e8bedf0dd8057b8fde546cdffb75015
Alex Eves <alex.eves@aiesoftware.com>
2019-10-17 15:24:56 +0100

    DDPB-2848: Replaces file scanner will third party dockerised container

---
+++ client/src/AppBundle/Service/File/Scanner/ClamFileScanner.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace AppBundle\Service\File\Scanner;
+
+use AppBundle\Service\File\Scanner\Exception\VirusFoundException;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\Exception\ServerException;
+use GuzzleHttp\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class ClamFileScanner
+{
+    /** @var ClientInterface */
+    private $client;
+
+    /** @var LoggerInterface */
+    private $logger;
+
+    /** @var int */
+    const MAX_SCAN_ATTEMPTS = 90;
+
+    /**
+     * @param ClientInterface $client
+     * @param LoggerInterface $logger
+     */
+    public function __construct(ClientInterface $client, LoggerInterface $logger)
+    {
+        $this->client = $client;
+        $this->logger = $logger;
+    }
+
+    /**
+     * @param UploadedFile $file
+     * @return bool
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function scanFile(UploadedFile $file): bool
+    {
+        $attempts = 0;
+        while ($attempts < self::MAX_SCAN_ATTEMPTS) {
+            try {
+                $attempts += 1;
+                $response = $this->attemptScan($file);
+                break;
+            }
+            catch (ServerException $e) {}
+            catch (ConnectException $e) {}
+        }
+
+        if (!$response instanceof Response) {
+            $this->logger->error(sprintf('Scanner service down: %s', $e->getMessage()));
+            throw new \RuntimeException('Scanner service not available');
+        }
+
+        if (!$this->scanResultIsPass($response)) {
+            $this->logger->info(sprintf('Scan result: virus found in file: %s', $file->getClientOriginalName()));
+            throw new VirusFoundException();
+        }
+
+        return true;
+    }
+
+    /**
+     * @param UploadedFile $file
+     * @return mixed|\Psr\Http\Message\ResponseInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    private function attemptScan(UploadedFile $file): ResponseInterface
+    {
+        return $this
+            ->client
+            ->request('POST', "/scan?name={$file->getClientOriginalName()}", [
+            'multipart' => [
+                [
+                    'name' => 'file',
+                    'contents' => fopen($file->getPathName(), 'r'),
+                ]
+            ]
+        ]);
+    }
+
+    /**
+     * @param Response $response
+     * @return bool
+     */
+    private function scanResultIsPass(Response $response): bool
+    {
+        $result = explode(':', trim($response->getBody()->getContents()));
+
+        return trim(end($result)) === 'true' ?: false;
+    }
+}
---
+++ client/tests/phpunit/Service/File/Scanner/ClamFileScannerTest.php
@@ -0,0 +1,201 @@
+<?php
+
+namespace AppBundle\Service\File\Scanner;
+
+use AppBundle\Service\File\Scanner\Exception\VirusFoundException;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\Exception\ServerException;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class ClamFileScannerTest extends TestCase
+{
+    /** @var ClamFileScanner */
+    private $scanner;
+
+    /** @var LoggerInterface | MockObject */
+    private $logger;
+
+    /** @var Client */
+    private $client;
+
+    /** mixed */
+    private $result;
+
+    protected function setUp(): void
+    {
+        $this->logger = $this->createMock(LoggerInterface::class);
+    }
+
+    /**
+     * @test
+     */
+    public function scanFile_returnsGracefullyOnCleanFile()
+    {
+        $this
+            ->ensureFileWillBeClean()
+            ->invokeTest()
+            ->assertGracefulReturn();
+    }
+
+    /**
+     * @test
+     */
+    public function scanFile_throws_VirusFoundException_onVirusFound()
+    {
+        $this->expectException(VirusFoundException::class);
+
+        $this
+            ->ensureVirusWillBeFound()
+            ->ensureVirusWillBeLogged()
+            ->invokeTest();
+    }
+
+    /**
+     * @test
+     */
+    public function scanFile_makesMultipleReattemptsIfScanServiceIsUnavailable()
+    {
+        $this
+            ->ensureServiceIsTemporarilyUnavailable()
+            ->invokeTest()
+            ->assertGracefulReturn();
+    }
+
+    /**
+     * @test
+     */
+    public function scanFile_throws_RuntimeException_ifServiceIsForeverUnavailable()
+    {
+        $this->expectException(\RuntimeException::class);
+
+        $this
+            ->ensureServiceIsForeverUnavailable()
+            ->ensureErrorWillBeLogged()
+            ->invokeTest();
+    }
+
+    /**
+     * @return ClamFileScannerTest
+     */
+    private function ensureFileWillBeClean(): ClamFileScannerTest
+    {
+        $response = new Response(200, [], 'Everything ok : true');
+        $this->presetClientResponses([$response]);
+
+        return $this;
+    }
+
+    /**
+     * @return ClamFileScannerTest
+     */
+    private function ensureVirusWillBeFound(): ClamFileScannerTest
+    {
+        $response = new Response(200, [], 'Everything ok : false');
+        $this->presetClientResponses([$response]);
+
+        return $this;
+    }
+
+    /**
+     * @return ClamFileScannerTest
+     */
+    private function ensureServiceIsTemporarilyUnavailable(): ClamFileScannerTest
+    {
+        $mockResponses = [];
+        // Ensures all but the last attempt is unsuccessful
+        for ($i = 0; $i < ClamFileScanner::MAX_SCAN_ATTEMPTS - 1; $i++) {
+            $mockResponses[] = new ServerException('unavailable', new Request('get', 'test'));
+        }
+
+        // Final attempt is good.
+        $mockResponses[] = new Response(200, [], 'Everything ok : true');
+
+        $this->presetClientResponses($mockResponses);
+
+        return $this;
+    }
+
+    /**
+     * @return ClamFileScannerTest
+     */
+    private function ensureServiceIsForeverUnavailable(): ClamFileScannerTest
+    {
+        $mockResponses = [];
+        // Mix of both types of response exceptions
+        for ($i = 0; $i < ClamFileScanner::MAX_SCAN_ATTEMPTS / 2; $i++) {
+            $mockResponses[] = new ServerException('unavailable', new Request('get', 'test'));
+            $mockResponses[] = new ConnectException('unavailable', new Request('get', 'test'));
+        }
+
+
+        // Ensure the MAX_SCAN_ATTEMPTS + 1 attempt would be good, to prove that we quit trying before this request is made.
+        $mockResponses[] = new Response(200, [], 'Everything ok : true');
+
+        $this->presetClientResponses($mockResponses);
+
+        return $this;
+    }
+
+    /**
+     * @param array $mockResponses
+     */
+    private function presetClientResponses(array $mockResponses): void
+    {
+        $handler = HandlerStack::create(new MockHandler($mockResponses));
+        $this->client = new Client(['handler' => $handler]);
+
+    }
+
+    /**
+     * @return ClamFileScannerTest
+     */
+    private function ensureVirusWillBeLogged(): ClamFileScannerTest
+    {
+        $this
+            ->logger
+            ->expects($this->once())
+            ->method('info')
+            ->with('Scan result: virus found in file: file.txt');
+
+        return $this;
+    }
+
+    /**
+     * @return ClamFileScannerTest
+     */
+    private function ensureErrorWillBeLogged(): ClamFileScannerTest
+    {
+        $this
+            ->logger
+            ->expects($this->once())
+            ->method('error')
+            ->with('Scanner service down: unavailable');
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    private function invokeTest(): ClamFileScannerTest
+    {
+        $this->scanner = new ClamFileScanner($this->client, $this->logger);
+        $this->result = $this->scanner->scanFile(new UploadedFile(__DIR__.'/file.txt', 'file.txt'));
+
+        return $this;
+    }
+
+    private function assertGracefulReturn()
+    {
+        $this->assertTrue($this->result);
+    }
+}
