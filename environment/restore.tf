e6d438d8e3240f1af0fdc8694b9d970e6d79e5b6
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2019-10-22 15:07:11 +0100

    Add missing inputs to Task

--- environment/restore.tf
+++ environment/restore.tf
@@ -1,13 +1,15 @@
 module "restore" {
   source = "./task"
+  name   = "restore"
 
-  name = "restore"
-  default_tags = local.default_tags
-  environment = local.environment
-  execution_role_arn = aws_iam_role.execution_role.arn
-  task_role_arn = data.aws_iam_role.sync.arn
-  vpc_id = data.aws_vpc.vpc.id
+  cluster_name          = aws_ecs_cluster.main.name
   container_definitions = "[${local.restore_container}]"
+  default_tags          = local.default_tags
+  environment           = local.environment
+  execution_role_arn    = aws_iam_role.execution_role.arn
+  subnet_ids            = data.aws_subnet.private[*].id
+  task_role_arn         = data.aws_iam_role.sync.arn
+  vpc_id                = data.aws_vpc.vpc.id
 }
 
 locals {
