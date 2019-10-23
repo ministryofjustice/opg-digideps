201a5010654118a382f6c14c51bfae9b515239ad
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2019-10-23 09:31:56 +0100

    Resolve migration version ordering

--- api/app/DoctrineMigrations/Version221.php
+++ api/app/DoctrineMigrations/Version221.php
@@ -22,10 +22,9 @@ final class Version221 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('DROP TABLE organisation_client');
-        $this->addSql('ALTER TABLE client ADD organisation_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE client ADD CONSTRAINT FK_C74404559E6B1585 FOREIGN KEY (organisation_id) REFERENCES organisation (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('CREATE INDEX IDX_C74404559E6B1585 ON client (organisation_id)');
+        $this->addSql('ALTER TABLE satisfaction DROP comments');
+        $this->addSql('ALTER TABLE satisfaction ALTER deputy_role DROP NOT NULL');
+        $this->addSql('ALTER TABLE satisfaction ALTER report_type DROP NOT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -33,13 +32,8 @@ final class Version221 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('CREATE TABLE organisation_client (organisation_id INT NOT NULL, client_id INT NOT NULL, PRIMARY KEY(organisation_id, client_id))');
-        $this->addSql('CREATE INDEX idx_455fde4819eb6921 ON organisation_client (client_id)');
-        $this->addSql('CREATE INDEX idx_455fde489e6b1585 ON organisation_client (organisation_id)');
-        $this->addSql('ALTER TABLE organisation_client ADD CONSTRAINT fk_455fde489e6b1585 FOREIGN KEY (organisation_id) REFERENCES organisation (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE organisation_client ADD CONSTRAINT fk_455fde4819eb6921 FOREIGN KEY (client_id) REFERENCES client (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE client DROP CONSTRAINT FK_C74404559E6B1585');
-        $this->addSql('DROP INDEX IDX_C74404559E6B1585');
-        $this->addSql('ALTER TABLE client DROP organisation_id');
+        $this->addSql('ALTER TABLE satisfaction ADD comments TEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE satisfaction ALTER deputy_role SET NOT NULL');
+        $this->addSql('ALTER TABLE satisfaction ALTER report_type SET NOT NULL');
     }
 }
---
+++ api/app/DoctrineMigrations/Version223.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version223 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'Replaced client.named deputy with new foreign key to dedicated Named Deputy table to store named deputy details';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        // reset all references to named deputy to be null to be reassociated with new ids
+        $this->addSql('UPDATE client SET named_deputy = NULL');
+
+        $this->addSql('CREATE TABLE named_deputy (id SERIAL NOT NULL, deputy_no VARCHAR(15) NOT NULL, deputy_type VARCHAR(5) DEFAULT NULL, firstname VARCHAR(100) NOT NULL, lastname VARCHAR(100) NOT NULL, email1 VARCHAR(60) NOT NULL, email2 VARCHAR(60) DEFAULT NULL, email3 VARCHAR(60) DEFAULT NULL, dep_addr_no INT DEFAULT NULL, address1 VARCHAR(200) DEFAULT NULL, address2 VARCHAR(200) DEFAULT NULL, address3 VARCHAR(200) DEFAULT NULL, address4 VARCHAR(200) DEFAULT NULL, address5 VARCHAR(200) DEFAULT NULL, address_postcode VARCHAR(10) DEFAULT NULL, address_country VARCHAR(10) DEFAULT NULL, phone_main VARCHAR(20) DEFAULT NULL, phone_alternative VARCHAR(20) DEFAULT NULL, fee_payer BOOLEAN DEFAULT NULL, corres BOOLEAN DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX named_deputy_no_idx ON named_deputy (deputy_no)');
+        $this->addSql('ALTER TABLE client DROP CONSTRAINT fk_c74404551058689');
+        $this->addSql('DROP INDEX idx_c74404551058689');
+        $this->addSql('ALTER TABLE client RENAME COLUMN named_deputy TO named_deputy_id');
+        $this->addSql('ALTER TABLE client ADD CONSTRAINT FK_C744045595162E7C FOREIGN KEY (named_deputy_id) REFERENCES named_deputy (id) ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('CREATE INDEX IDX_C744045595162E7C ON client (named_deputy_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE client DROP CONSTRAINT FK_C744045595162E7C');
+        $this->addSql('DROP TABLE named_deputy');
+        $this->addSql('DROP INDEX IDX_C744045595162E7C');
+        $this->addSql('ALTER TABLE client RENAME COLUMN named_deputy_id TO named_deputy');
+        $this->addSql('ALTER TABLE client ADD CONSTRAINT fk_c74404551058689 FOREIGN KEY (named_deputy) REFERENCES dd_user (id) ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('CREATE INDEX idx_c74404551058689 ON client (named_deputy)');
+    }
+}
