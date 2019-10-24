3f319c1fcb8e5b43a62de871cbb548fd30899879
Luke Bradbury <luke@nqyr.io>
2019-10-24 17:23:49 +0100

    Handle log stream missing error

--- ecs_helper/internal/log.go
+++ ecs_helper/internal/log.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
 	"log"
 )
@@ -15,7 +16,17 @@ func (l *Log) PrintLogEvents() {
 	cloudwatchLogsOutput, err := l.Svc.GetLogEvents(l.Input)
 
 	if err != nil {
-		log.Println(err)
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case cloudwatchlogs.ErrCodeResourceNotFoundException:
+				log.Println("Waiting for log stream to start...")
+				err = nil
+			default:
+				log.Fatalln(aerr)
+			}
+		} else {
+			log.Fatalln(err)
+		}
 	}
 
 	l.Input.NextToken = cloudwatchLogsOutput.NextForwardToken
