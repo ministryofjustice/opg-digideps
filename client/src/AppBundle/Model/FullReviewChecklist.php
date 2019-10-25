a3db295459d71a912a38408c69f3f301ca9f0503
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2019-10-25 13:51:56 +0100

    Serialise FullReviewChecklist

--- client/src/AppBundle/Model/FullReviewChecklist.php
+++ client/src/AppBundle/Model/FullReviewChecklist.php
@@ -1,29 +1,32 @@
 <?php declare(strict_types=1);
 
-
 namespace AppBundle\Model;
+
+use JMS\Serializer\Annotation as JMS;
 
 class FullReviewChecklist
 {
     /**
      * @var string
+     * @JMS\Groups({"full-review-checklist"})
+     * @JMS\Type("string")
      */
-    private $decision;
+    private $decisionExplanation;
 
     /**
      * @return string
      */
-    public function getDecision(): ?string
+    public function getDecisionExplanation(): ?string
     {
-        return $this->decision;
+        return $this->decisionExplanation;
     }
 
     /**
      * @param string $decision
      */
-    public function setDecision(string $decision)
+    public function setDecisionExplanation($decisionExplanation)
     {
-        $this->decision = $decision;
+        $this->decisionExplanation = $decisionExplanation;
         return $this;
     }
 }
