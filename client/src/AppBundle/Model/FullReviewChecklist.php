7202ec1b6cdac38b3ee16bb95dc827d1b80e8893
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2019-11-01 11:41:16 +0000

    Add new questions

--- client/src/AppBundle/Model/FullReviewChecklist.php
+++ client/src/AppBundle/Model/FullReviewChecklist.php
@@ -11,6 +11,78 @@ class FullReviewChecklist
      * @var string
      * @JMS\Groups({"full-review-checklist"})
      * @JMS\Type("string")
+     * @Assert\Choice({"yes", "no", "na"})
+     * @Assert\NotBlank
+     */
+    public $fullBankStatementsExist;
+
+    /**
+     * @var string
+     * @JMS\Groups({"full-review-checklist"})
+     * @JMS\Type("string")
+     * @Assert\Choice({"yes", "no", "na"})
+     * @Assert\NotBlank
+     */
+    public $anyLodgingConcerns;
+
+    /**
+     * @var string
+     * @JMS\Groups({"full-review-checklist"})
+     * @JMS\Type("string")
+     * @Assert\Choice({"yes", "no", "na"})
+     * @Assert\NotBlank
+     */
+    public $spendingAcceptable;
+
+    /**
+     * @var string
+     * @JMS\Groups({"full-review-checklist"})
+     * @JMS\Type("string")
+     * @Assert\Choice({"yes", "no", "na"})
+     * @Assert\NotBlank
+     */
+    public $expensesReasonable;
+
+    /**
+     * @var string
+     * @JMS\Groups({"full-review-checklist"})
+     * @JMS\Type("string")
+     * @Assert\Choice({"yes", "no", "na"})
+     * @Assert\NotBlank
+     */
+    public $giftingReasonable;
+
+    /**
+     * @var string
+     * @JMS\Groups({"full-review-checklist"})
+     * @JMS\Type("string")
+     * @Assert\Choice({"yes", "no", "na"})
+     * @Assert\NotBlank
+     */
+    public $debtManageable;
+
+    /**
+     * @var string
+     * @JMS\Groups({"full-review-checklist"})
+     * @JMS\Type("string")
+     * @Assert\Choice({"yes", "no", "na"})
+     * @Assert\NotBlank
+     */
+    public $anySpendingConcerns;
+
+    /**
+     * @var string
+     * @JMS\Groups({"full-review-checklist"})
+     * @JMS\Type("string")
+     * @Assert\Choice({"yes", "no"})
+     * @Assert\NotBlank
+     */
+    public $needReferral;
+
+    /**
+     * @var string
+     * @JMS\Groups({"full-review-checklist"})
+     * @JMS\Type("string")
      * @Assert\Type("string")
      */
     public $decisionExplanation;
