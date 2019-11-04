b8c936723f0182826c93620b6bca39c085910f42
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2019-11-04 15:18:29 +0000

    Add outbound security groups for tasks

--- environment/task/main.tf
+++ environment/task/main.tf
@@ -8,14 +8,50 @@ resource "aws_security_group" "task" {
   }
 }
 
-resource "aws_security_group_rule" "task_out" {
-  type      = "egress"
-  protocol  = "-1"
-  from_port = 0
-  to_port   = 0
+data "aws_vpc_endpoint" "ecr_endpoint" {
+  service_name = "com.amazonaws.eu-west-1.ecr.dkr"
+  vpc_id       = var.vpc_id
+}
 
+data "aws_vpc_endpoint" "logs_endpoint" {
+  service_name = "com.amazonaws.eu-west-1.logs"
+  vpc_id       = var.vpc_id
+}
+
+data "aws_vpc_endpoint" "s3_endpoint" {
+  service_name = "com.amazonaws.eu-west-1.s3"
+  vpc_id       = var.vpc_id
+}
+
+resource "aws_security_group_rule" "task_https_out_ecr" {
+  for_each = toset(data.aws_vpc_endpoint.ecr_endpoint.security_group_ids)
+
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = 443
+  to_port                  = 443
+  security_group_id        = aws_security_group.task.id
+  source_security_group_id = each.value
+}
+
+resource "aws_security_group_rule" "task_https_out_logs" {
+  for_each = toset(data.aws_vpc_endpoint.logs_endpoint.security_group_ids)
+
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = 443
+  to_port                  = 443
+  security_group_id        = aws_security_group.task.id
+  source_security_group_id = each.value
+}
+
+resource "aws_security_group_rule" "task_https_out_s3" {
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = 443
+  to_port           = 443
   security_group_id = aws_security_group.task.id
-  cidr_blocks       = ["0.0.0.0/0"]
+  prefix_list_ids   = [data.aws_vpc_endpoint.s3_endpoint.prefix_list_id]
 }
 
 resource "aws_ecs_task_definition" "task" {
