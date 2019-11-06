fe6bd3f5f608c44bd81c026922030102c444ad86
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2019-11-06 17:15:44 +0000

    Be more specific with front security groups

--- environment/ecr.tf
+++ environment/ecr.tf
@@ -2,7 +2,7 @@ locals {
   images = {
     api          = "${data.aws_ecr_repository.images["api"].repository_url}:${var.OPG_DOCKER_TAG}"
     client       = "${data.aws_ecr_repository.images["client"].repository_url}:${var.OPG_DOCKER_TAG}"
-    file_scanner = "${data.aws_ecr_repository.images["file-scanner"].repository_url}:latest"
+    file_scanner = "${data.aws_ecr_repository.images["file-scanner"].repository_url}:${var.OPG_DOCKER_TAG}"
     sync         = "${data.aws_ecr_repository.images["sync"].repository_url}:${var.OPG_DOCKER_TAG}"
     test         = "${data.aws_ecr_repository.images["test"].repository_url}:${var.OPG_DOCKER_TAG}"
     wkhtmltopdf  = "${data.aws_ecr_repository.images["wkhtmltopdf"].repository_url}:latest"
