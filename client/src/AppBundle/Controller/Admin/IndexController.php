216c41b60f2c57cc7038835b95261c18fdf4716f
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2019-11-13 11:28:30 +0000

    Initialise session
    Stops an error when using `addFlash` after outputting content

--- client/src/AppBundle/Controller/Admin/IndexController.php
+++ client/src/AppBundle/Controller/Admin/IndexController.php
@@ -476,7 +476,7 @@ class IndexController extends AbstractController
             /** @var EntityDir\User $currentUser */
             $currentUser = $this->getUser();
 
-            $request = $client->post('org/bulk-add', [
+            $apiRequest = $client->post('org/bulk-add', [
                 'headers' => [
                     'AuthToken' => $tokenStorage->get($currentUser->getId())
                 ],
@@ -487,10 +487,10 @@ class IndexController extends AbstractController
 
             $outputStreamResponse = isset($_GET['ajax']);
 
-            $stream = $request->getBody();
+            $stream = $apiRequest->getBody();
             $response = new StreamedResponse();
 
-            $response->setCallback(function() use ($stream, $outputStreamResponse) {
+            $response->setCallback(function() use ($stream, $outputStreamResponse, $request) {
                 $errors = [];
                 $warnings = [];
                 $added = [
@@ -501,6 +501,10 @@ class IndexController extends AbstractController
                 ];
 
                 $carryover = '';
+
+                /** @var \Symfony\Component\HttpFoundation\Session\Session */
+                $session = $request->getSession();
+                $session->getFlashBag();
 
                 while (!$stream->eof()) {
                     $partial = $carryover . $stream->read(1024);
@@ -553,15 +557,16 @@ class IndexController extends AbstractController
                         $added['prof_users'],
                         $added['pa_users'],
                         $added['clients'],
-                        $added['reports'],
+                        $added['reports']
                     )
                 );
 
                 $redirectUrl = $this->generateUrl('admin_org_upload');
 
                 if ($outputStreamResponse) {
-                    echo 'REDIR ' . $redirectUrl;
-                    echo 'END';
+                    echo "REDIR $redirectUrl\n";
+                    echo "END\n";
+                    flush();
                 } else {
                     header('Location: '. $redirectUrl);
                 }
