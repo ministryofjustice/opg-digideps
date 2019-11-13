30918f60fc900b9c2a42f3010454ad401055a094
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2019-11-13 15:33:53 +0000

    Divide OrgService into atomic methods

--- client/src/AppBundle/Service/OrgService.php
+++ client/src/AppBundle/Service/OrgService.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Service;
 
 use AppBundle\Service\Client\RestClient;
+use GuzzleHttp\Psr7\Stream;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 use Twig\Environment;
@@ -27,7 +28,21 @@ class OrgService
     /**
      * @var bool
      */
-    private $outputToStream = false;
+    private $outputLogging = false;
+
+    /**
+     * @var array
+     */
+    private $output = [
+        'errors' => [],
+        'warnings' => [],
+        'added' => [
+            'prof_users' => 0,
+            'pa_users' => 0,
+            'clients' => 0,
+            'reports' => 0,
+        ],
+    ];
 
     /**
      * @param RestClient $restClient
@@ -40,100 +55,94 @@ class OrgService
     }
 
     /**
-     * @param bool $outputToStream
+     * @param bool $outputLogging
+     * @return $this
      */
-    public function setOutputStream(bool $outputToStream)
+    public function setLogging(bool $outputLogging)
     {
-        $this->outputToStream = $outputToStream;
+        $this->outputLogging = $outputLogging;
+        return $this;
     }
 
-    protected function generateResponse($stream, $redirectUrl)
+    /**
+     * Push some output to the buffer, if enabled
+     *
+     * @param string $output
+     */
+    protected function log(string $output)
+    {
+        if (!$this->outputLogging) return;
+
+        echo $output . "\n";
+        flush();
+    }
+
+    /**
+     * Parse a line of the API output
+     *
+     * @param string $line
+     */
+    protected function parseLine(string $line)
+    {
+        if (substr($line, 0, 5) === 'PROG ') {
+            $this->log($line);
+        } else if (substr($line, 0, 4) === 'ERR ') {
+            $this->output['errors'][] = substr($line, 4);
+        } else if (substr($line, 0, 5) === 'WARN ') {
+            $this->output['warnings'][] = substr($line, 5);
+        } else if (substr($line, 0, 4) === 'ADD ') {
+            list(, $amount, $key) = explode(' ', $line);
+            $this->output['added'][strtolower($key)] += $amount;
+        }
+    }
+
+    /**
+     * Set flash messages about results of upload
+     */
+    protected function addFlashMessages()
+    {
+        $flashBag = $this->session->getFlashBag();
+
+        if (count($this->output['errors'])) {
+            $flash = $this->twig->render('AppBundle:Admin/Index:_uploadErrorAlert.html.twig', [
+                'type' => 'errors',
+                'errors' => $this->output['errors'],
+            ]);
+
+            $flashBag->add('error', $flash);
+        }
+
+        if (count($this->output['warnings'])) {
+            $flash = $this->twig->render('AppBundle:Admin/Index:_uploadErrorAlert.html.twig', [
+                'type' => 'warnings',
+                'errors' => $this->output['warnings'],
+            ]);
+
+            $flashBag->add('warning', $flash);
+        }
+
+        $flashBag->add(
+            'notice',
+            sprintf('Added %d Prof users, %d PA users, %d clients and %d reports. Go to users tab to enable them',
+                $this->output['added']['prof_users'],
+                $this->output['added']['pa_users'],
+                $this->output['added']['clients'],
+                $this->output['added']['reports']
+            )
+        );
+    }
+
+    /**
+     * Generate a streamed response
+     *
+     * @return StreamedResponse
+     */
+    protected function generateStreamedResponse()
     {
         $response = new StreamedResponse();
-
-        $response->setCallback(function() use ($stream, $redirectUrl) {
-            $flashBag = $this->session->getFlashBag();
-
-            $errors = [];
-            $warnings = [];
-            $added = [
-                'prof_users' => 0,
-                'pa_users' => 0,
-                'clients' => 0,
-                'reports' => 0,
-            ];
-
-            $carryover = '';
-
-            while (!$stream->eof()) {
-                $partial = $carryover . $stream->read(1024);
-                $carryover = '';
-
-                $lines = explode("\n", $partial);
-
-                // If partial didn't finish with a newline, carry over the last line
-                if ($lines[count($lines) - 1] !== '') {
-                    $carryover = array_pop($lines);
-                }
-
-                foreach ($lines as $line) {
-                    if (substr($line, 0, 5) === 'PROG ' && $this->outputToStream) {
-                        echo $line . "\n";
-                        flush();
-                    } else if (substr($line, 0, 4) === 'ERR ') {
-                        $errors[] = substr($line, 4);
-                    } else if (substr($line, 0, 5) === 'WARN ') {
-                        $warnings[] = substr($line, 5);
-                    } else if (substr($line, 0, 4) === 'ADD ') {
-                        list(, $amount, $key) = explode(' ', $line);
-                        $added[strtolower($key)] += $amount;
-                    }
-                }
-
-                gc_collect_cycles();
-            }
-
-            if (count($errors)) {
-                $flash = $this->twig->render('AppBundle:Admin/Index:_uploadErrorAlert.html.twig', [
-                    'type' => 'errors',
-                    'errors' => $errors,
-                ]);
-
-                $flashBag->add('error', $flash);
-            }
-
-            if (count($warnings)) {
-                $flash = $this->twig->render('AppBundle:Admin/Index:_uploadErrorAlert.html.twig', [
-                    'type' => 'warnings',
-                    'errors' => $warnings,
-                ]);
-
-                $flashBag->add('warning', $flash);
-            }
-
-            $flashBag->add(
-                'notice',
-                sprintf('Added %d Prof users, %d PA users, %d clients and %d reports. Go to users tab to enable them',
-                    $added['prof_users'],
-                    $added['pa_users'],
-                    $added['clients'],
-                    $added['reports']
-                )
-            );
-
-            if ($this->outputToStream) {
-                echo "REDIR $redirectUrl\n";
-                echo "END\n";
-                flush();
-            } else {
-                header('Location: '. $redirectUrl);
-            }
-
-            die();
-        });
-
         $response->setStatusCode(200);
-        if ($this->outputToStream) {
+
+        if ($this->outputLogging) {
             $response->headers->set('X-Accel-Buffering', 'no');
             $response->headers->set('Content-Type', 'text/plain; charset=utf-8');
         }
@@ -142,14 +151,69 @@ class OrgService
     }
 
     /**
-     * @param mixed $compressedData
+     * When the API stream has finished, force a redirect
+     *
+     * @param string $redirectUrl
      */
-    public function upload($compressedData, $redirectUrl)
+    protected function finishStream($redirectUrl)
     {
+        $this->addFlashMessages();
+        $this->log("REDIR $redirectUrl");
+        $this->log('END');
+
+        if (!$this->outputLogging) {
+            header('Location: '. $redirectUrl);
+        }
+    }
+
+    /**
+     * Convert the API stream into suitable output
+     *
+     * @param Stream $stream
+     * @param string $redirectUrl
+     */
+    protected function convertStreamToOutput(Stream $stream, string $redirectUrl)
+    {
+        if ($this->outputLogging) {
+            $this->session->start();
+        }
+
+        $carryover = '';
+
+        while (!$stream->eof()) {
+            $partial = $carryover . $stream->read(1024);
+            $carryover = '';
+
+            $lines = explode("\n", $partial);
+
+            // If partial didn't finish with a newline, carry over the last line
+            if ($lines[count($lines) - 1] !== '') {
+                $carryover = array_pop($lines);
+            }
+
+            foreach ($lines as $line) {
+                $this->parseLine($line);
+            }
+
+            gc_collect_cycles();
+        }
+
+        $this->finishStream($redirectUrl);
+
+        die();
+    }
+
+    /**
+     * @param mixed $data
+     * @return Stream
+     */
+    public function upload($data)
+    {
+        /** @var Stream $stream */
         $stream = $this->restClient->apiCall(
             'post',
             'org/bulk-add',
-            json_encode($compressedData),
+            json_encode($data),
             'raw',
             [
                 'timeout' => 600,
@@ -157,6 +221,24 @@ class OrgService
             ]
         );
 
-        return $this->generateResponse($stream, $redirectUrl);
+        return $stream;
+    }
+
+    /**
+     * @param mixed $data
+     * @param string $redirectUrl
+     * @return StreamedResponse
+     */
+    public function process($data, $redirectUrl)
+    {
+        $stream = $this->upload($data);
+
+        $response = $this->generateStreamedResponse();
+
+        $response->setCallback(function() use ($stream, $redirectUrl) {
+            $this->convertStreamToOutput($stream, $redirectUrl);
+        });
+
+        return $response;
     }
 }
