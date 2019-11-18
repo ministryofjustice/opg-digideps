a6a0986cd23c8ba15d7cde90280ccd18a78196f0
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2019-11-18 13:22:09 +0000

    Add reset_database ingress rule to RDS

--- environment/api_sg.tf
+++ environment/api_sg.tf
@@ -107,6 +107,13 @@ locals {
       target_type = "security_group_id"
       target      = module.integration_test.security_group_id
     }
+    reset_database = {
+      port        = 5432
+      type        = "ingress"
+      protocol    = "tcp"
+      target_type = "security_group_id"
+      target      = module.reset_database.security_group_id
+    }
   }
 }
 
