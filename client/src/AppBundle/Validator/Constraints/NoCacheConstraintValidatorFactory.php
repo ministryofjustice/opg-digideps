b85d592055c6d5c2f533d517dd55831eedcb6e90
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2019-12-02 15:38:09 +0000

    Fix namespacing errors

--- client/src/AppBundle/Validator/Constraints/NoCacheConstraintValidatorFactory.php
+++ client/src/AppBundle/Validator/Constraints/NoCacheConstraintValidatorFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace AppBundle\Validator;
+namespace AppBundle\Validator\Constraints;
 
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidatorFactoryInterface;
