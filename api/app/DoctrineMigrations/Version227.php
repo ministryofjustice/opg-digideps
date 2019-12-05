4bcaba8a25c79f4ffb78e37e0a7ccfe80f9f18a8
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2019-12-05 14:55:53 +0000

    Correct migration class name

--- api/app/DoctrineMigrations/Version227.php
+++ api/app/DoctrineMigrations/Version227.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version226 extends AbstractMigration
+final class Version227 extends AbstractMigration
 {
     public function getDescription() : string
     {
