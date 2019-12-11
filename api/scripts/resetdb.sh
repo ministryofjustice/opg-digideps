a18bac3ea3a0367daf153a8b71ac1326a9b80481
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2019-12-11 09:36:43 +0000

    Don't reset database structure when applying migrations and fixtures

--- api/scripts/resetdb.sh
+++ api/scripts/resetdb.sh
@@ -3,8 +3,6 @@ set -e
 # We need below to create the params file on container start
 confd -onetime -backend env
 
-. /var/www/scripts/initialize_schema.sh
-
 su-exec www-data php app/console doctrine:migrations:status-check
 su-exec www-data php app/console doctrine:migrations:migrate --no-interaction -vvv
 su-exec www-data php app/console doctrine:fixtures:load --no-interaction
