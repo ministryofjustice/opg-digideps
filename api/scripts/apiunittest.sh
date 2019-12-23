02f71e6187f0d6c1b1fa3d9071608d5795b3be17
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2019-12-23 14:21:32 +0000

    Add codecov to pipeline and generate covs from api unit tests

--- api/scripts/apiunittest.sh
+++ api/scripts/apiunittest.sh
@@ -13,13 +13,13 @@ export PGUSER=${DATABASE_USERNAME:=api}
 
 # Run each folder of unit tests individually. If we were to run them all
 # individually it would cause a memory leak.
-php bin/phpunit -c tests tests/AppBundle/Controller/
-php bin/phpunit -c tests tests/AppBundle/ControllerReport/
-php bin/phpunit -c tests tests/AppBundle/Controller-Ndr/
-php bin/phpunit -c tests tests/AppBundle/Entity/
-php bin/phpunit -c tests tests/AppBundle/Factory/
-php bin/phpunit -c tests tests/AppBundle/Security/
-php bin/phpunit -c tests tests/AppBundle/Service/
-php bin/phpunit -c tests tests/AppBundle/Stats/
-php bin/phpunit -c tests tests/AppBundle/Transformer/
-php bin/phpunit -c tests tests/AppBundle/v2/
+php bin/phpunit -c tests tests/AppBundle/Controller/ --coverage-php tests/coverage/Controller.cov
+php bin/phpunit -c tests tests/AppBundle/ControllerReport/ --coverage-php tests/coverage/ControllerReport.cov
+#php bin/phpunit -c tests tests/AppBundle/Controller-Ndr/ --coverage-php var/coverage/unit.cov
+#php bin/phpunit -c tests tests/AppBundle/Entity/ --coverage-php var/coverage/unit.cov
+#php bin/phpunit -c tests tests/AppBundle/Factory/ --coverage-php var/coverage/unit.cov
+#php bin/phpunit -c tests tests/AppBundle/Security/ --coverage-php var/coverage/unit.cov
+#php bin/phpunit -c tests tests/AppBundle/Service/ --coverage-php var/coverage/unit.cov
+#php bin/phpunit -c tests tests/AppBundle/Stats/ --coverage-php var/coverage/unit.cov
+#php bin/phpunit -c tests tests/AppBundle/Transformer/ --coverage-php var/coverage/unit.cov
+#php bin/phpunit -c tests tests/AppBundle/v2/ --coverage-php var/coverage/unit.cov
