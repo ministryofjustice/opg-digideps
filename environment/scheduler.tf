00a30499276d6b8bb61706aab08aff62a977d294
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2020-01-15 08:53:30 +0000

    Dirtying commit

--- environment/scheduler.tf
+++ environment/scheduler.tf
@@ -9,6 +9,7 @@ resource "aws_cloudwatch_event_rule" "redeploy_file_scanner" {
   tags                = local.default_tags
 }
 
+
 resource "aws_cloudwatch_event_target" "redeploy_file_scanner" {
   rule  = aws_cloudwatch_event_rule.redeploy_file_scanner.name
   arn   = data.aws_lambda_function.redeployer_lambda.arn
