ac3aeb32e75015bad6ac6638f6da46f627d74e71
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2020-01-15 10:03:04 +0000

    Add Lambda permission to Terraform
    This is effectively the inverse of aws_cloudwatch_event_target, allowing CloudWatch to trigger the Lambda
    
    I missed it before because it was created when I initially experimented manually.

--- environment/scheduler.tf
+++ environment/scheduler.tf
@@ -9,7 +9,6 @@ resource "aws_cloudwatch_event_rule" "redeploy_file_scanner" {
   tags                = local.default_tags
 }
 
-
 resource "aws_cloudwatch_event_target" "redeploy_file_scanner" {
   rule  = aws_cloudwatch_event_rule.redeploy_file_scanner.name
   arn   = data.aws_lambda_function.redeployer_lambda.arn
@@ -19,4 +18,12 @@ resource "aws_cloudwatch_event_target" "redeploy_file_scanner" {
       "service": "${aws_ecs_service.scan.name}"
     }
   EOF
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_call_lambda" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = data.aws_lambda_function.redeployer_lambda.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.redeploy_file_scanner.arn
 }
