2e9dbc3878e0abfca06cd99e8566213c597c5dac
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2020-01-30 16:47:35 +0000

    Remove unused healthcheck config
    There's no associated healthcheck on the Route53 records to be configured

--- environment/api_service.tf
+++ environment/api_service.tf
@@ -15,10 +15,6 @@ resource "aws_service_discovery_service" "api" {
 
     routing_policy = "MULTIVALUE"
   }
-
-  health_check_custom_config {
-    failure_threshold = 1
-  }
 }
 
 resource "aws_ecs_task_definition" "api" {
