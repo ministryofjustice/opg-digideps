ed37ab4b4cfc98756cf3532dcf299fe6cf78dbd0
Greg Tyler <gregtyler@users.noreply.github.com>
2020-02-05 10:33:43 +0000

    DDPB-3198: Move Slack SNS subscription to per-account (#222)
    * Move Slack notification subscription to account
    
    We want one Slack notification per account, not one per environment.
    
    * Remove unused variables/resources

--- environment/sns.tf
+++ environment/sns.tf
@@ -1,20 +1,3 @@
 data "aws_sns_topic" "alerts" {
   name = "alerts"
 }
-
-module "notify_slack" {
-  source  = "terraform-aws-modules/notify-slack/aws"
-  version = "~> 2.0"
-
-  sns_topic_name   = data.aws_sns_topic.alerts.name
-  create_sns_topic = false
-
-  lambda_function_name = "notify-slack-${local.environment}"
-
-  slack_webhook_url = data.aws_secretsmanager_secret_version.slack_webhook_url.secret_string
-  slack_channel     = local.account.is_production == 1 ? "#opg-digideps-team" : "#opg-digideps-devs"
-  slack_username    = "aws"
-  slack_emoji       = ":warning:"
-
-  tags = local.default_tags
-}
--- shared/sns.tf
+++ shared/sns.tf
@@ -2,8 +2,47 @@ resource "aws_sns_topic" "alerts" {
   name = "alerts"
 }
 
+module "notify_slack" {
+  source  = "terraform-aws-modules/notify-slack/aws"
+  version = "~> 2.0"
+
+  sns_topic_name   = aws_sns_topic.alerts.name
+  create_sns_topic = false
+
+  lambda_function_name = "notify-slack"
+
+  slack_webhook_url = data.aws_secretsmanager_secret_version.slack_webhook_url.secret_string
+  slack_channel     = local.account.name == "production" ? "#opg-digideps-team" : "#opg-digideps-devs"
+  slack_username    = "aws"
+  slack_emoji       = ":warning:"
+
+  tags = local.default_tags
+}
+
 resource "aws_sns_topic" "availability-alert" {
   provider     = aws.us-east-1
   name         = "availability-alert"
   display_name = "${local.default_tags["application"]} ${local.default_tags["environment-name"]} Availability Alert"
+}
+
+module "notify_slack_us-east-1" {
+  source  = "terraform-aws-modules/notify-slack/aws"
+  version = "~> 2.0"
+
+  providers = {
+    aws = aws.us-east-1
+  }
+
+  sns_topic_name   = aws_sns_topic.availability-alert.name
+  create_sns_topic = false
+  create           = local.account.name != "development"
+
+  lambda_function_name = "notify-slack"
+
+  slack_webhook_url = data.aws_secretsmanager_secret_version.slack_webhook_url.secret_string
+  slack_channel     = local.account.name == "production" ? "#opg-digideps-team" : "#opg-digideps-devs"
+  slack_username    = "aws"
+  slack_emoji       = ":warning:"
+
+  tags = local.default_tags
 }
