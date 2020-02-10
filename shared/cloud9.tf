55b4db368b4d8e1378b2281147347817ad770272
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2020-02-10 15:33:53 +0000

    Use public subnet and apply owner to shared cloud9 env

--- shared/cloud9.tf
+++ shared/cloud9.tf
@@ -3,13 +3,6 @@ resource "aws_cloud9_environment_ec2" "shared" {
   name                        = "shared-env"
   automatic_stop_time_minutes = 20
   description                 = "Shared Cloud9 instance to be used by all devs"
-  subnet_id                   = aws_subnet.private[0].id
+  subnet_id                   = aws_subnet.public[0].id
+  owner_arn                   = "arn:aws:iam::${var.accounts[terraform.workspace].account_id}:assumed-role/operator/alex.saunders"
 }
-
-
-//name - (Required) The name of the environment.
-//instance_type - (Required) The type of instance to connect to the environment, e.g. t2.micro.
-//automatic_stop_time_minutes - (Optional) The number of minutes until the running instance is shut down after the environment has last been used.
-//description - (Optional) The description of the environment.
-//owner_arn - (Optional) The ARN of the environment owner. This can be ARN of any AWS IAM principal. Defaults to the environment's creator.
-//subnet_id - (Optional) The ID of the subnet in Amazon VPC that AWS Cloud9 will use to communicate with the Amazon EC2 instance.
