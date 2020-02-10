75d81556daae3bb63063e116ea0c9a65f976a6c9
Alex Eves <alex.eves@aiesoftware.com>
2020-02-10 18:42:06 +0000

    DDPB-3181: Behat tests for registration from different sources

--- behat/tests/bootstrap/FeatureContext.php
+++ behat/tests/bootstrap/FeatureContext.php
@@ -5,6 +5,7 @@ namespace DigidepsBehat;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\MinkExtension\Context\MinkContext;
 use DigidepsBehat\Common\AuthenticationTrait;
+use DigidepsBehat\Common\EmailTrait;
 use DigidepsBehat\Common\FormTrait;
 use DigidepsBehat\Common\LinksTrait;
 use DigidepsBehat\Common\RegionTrait;
