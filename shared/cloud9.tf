9ab000ee387139c691c6da5f51080c5b1f81eec6
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2020-02-11 16:13:31 +0000

    Change cloud9 env

--- shared/cloud9.tf
+++ shared/cloud9.tf
@@ -1,6 +1,6 @@
 resource "aws_cloud9_environment_ec2" "shared" {
   instance_type               = "t2.micro"
-  name                        = "shared-env"
+  name                        = "team-cloud9-env"
   automatic_stop_time_minutes = 20
   description                 = "Shared Cloud9 instance to be used by all devs"
   subnet_id                   = aws_subnet.public[0].id
