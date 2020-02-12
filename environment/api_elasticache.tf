44659fff1a9b87b43300fe939c959cc33ead3cab
jamesrwarren <58939809+jamesrwarren@users.noreply.github.com>
2020-02-12 14:18:26 +0000

    ddpb-3224 add replication group without dns switch
    * ddpb-3224 add replication group without dns switch as part of ddpb-3192

--- environment/api_elasticache.tf
+++ environment/api_elasticache.tf
@@ -1,4 +1,28 @@
+resource "aws_elasticache_replication_group" "api" {
+  // Include this once first two subtasks of DDBP-3192 are done
+  //  count                         = local.account.is_production == 1 ? 1 : 0
+  automatic_failover_enabled    = true
+  engine                        = "redis"
+  engine_version                = "5.0.0"
+  availability_zones            = ["eu-west-1a", "eu-west-1b"]
+  replication_group_id          = "api-rep-group-${local.environment}"
+  replication_group_description = "Replication Group for API"
+  node_type                     = "cache.t2.small"
+  number_cache_clusters         = 2
+  parameter_group_name          = "default.redis5.0"
+  port                          = 6379
+  subnet_group_name             = local.account.ec_subnet_group
+  security_group_ids            = [module.api_cache_security_group.id]
+  apply_immediately             = true
+  tags = {
+    InstanceName = "api-${local.environment}"
+    Stack        = local.environment
+  }
+}
+
 resource "aws_elasticache_cluster" "api" {
+  // Include this once first two subtasks of DDBP-3192 are done
+  //  count                = local.account.is_production == 1 ? 0 : 1
   cluster_id           = "api-${local.environment}"
   engine               = "redis"
   node_type            = "cache.t2.small"
@@ -14,12 +38,4 @@ resource "aws_elasticache_cluster" "api" {
     InstanceName = "api-${local.environment}"
     Stack        = local.environment
   }
-}
-
-resource "aws_route53_record" "api_redis" {
-  name    = "api-redis"
-  type    = "CNAME"
-  zone_id = aws_route53_zone.internal.id
-  records = [aws_elasticache_cluster.api.cache_nodes[0].address]
-  ttl     = 300
 }
--- environment/front_elasticache.tf
+++ environment/front_elasticache.tf
@@ -1,4 +1,25 @@
+resource "aws_elasticache_replication_group" "front" {
+  // Include this once first two subtasks of DDBP-3192 are done
+  //  count                         = local.account.is_production == 1 ? 1 : 0
+  automatic_failover_enabled    = true
+  engine                        = "redis"
+  engine_version                = "5.0.0"
+  availability_zones            = ["eu-west-1a", "eu-west-1b"]
+  replication_group_id          = "front-rep-group-${local.environment}"
+  replication_group_description = "Replication Group for Front"
+  node_type                     = "cache.t2.small"
+  number_cache_clusters         = 2
+  parameter_group_name          = "default.redis5.0"
+  port                          = 6379
+  subnet_group_name             = local.account.ec_subnet_group
+  security_group_ids            = [module.front_cache_security_group.id]
+  tags                          = local.default_tags
+  apply_immediately             = true
+}
+
 resource "aws_elasticache_cluster" "front" {
+  // Include this once first two subtasks of DDBP-3192 are done
+  //  count                = local.account.is_production == 1 ? 0 : 1
   cluster_id           = "front-${local.environment}"
   engine               = "redis"
   node_type            = "cache.t2.small"
