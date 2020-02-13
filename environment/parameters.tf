88a6a354fdbce4b241cf91f1860b37df3b1f710e
Alex Eves <alex.eves@aiesoftware.com>
2020-02-13 10:36:56 +0000

    Attempted fix

--- environment/parameters.tf
+++ environment/parameters.tf
@@ -1,3 +1,3 @@
 data "aws_ssm_parameter" "sirius_api_base_uri" {
-  name = "/default/sirius-api-base-uri"
+  name = format("/%s", join("/", compact([local.account.secrets_prefix, "sirius-api-base-uri"])))
 }
