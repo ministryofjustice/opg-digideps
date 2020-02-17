2432095a23588b41a65485d2057cf52819c37a24
Alex Eves <alex.eves@aiesoftware.com>
2020-02-17 08:34:06 +0000

    DDPB-3160: Authenticating with API

--- client/src/AppBundle/Service/Availability/SiriusApiAvailability.php
+++ client/src/AppBundle/Service/Availability/SiriusApiAvailability.php
@@ -2,6 +2,10 @@
 
 namespace AppBundle\Service\Availability;
 
+use Aws\Credentials\CredentialProvider;
+use Aws\Signature\SignatureV4;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Uri;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class SiriusApiAvailability extends ServiceAvailabilityAbstract
@@ -11,7 +15,19 @@ class SiriusApiAvailability extends ServiceAvailabilityAbstract
         $this->isHealthy = true;
 
         try {
-            $response = $container->get('guzzle_api_gateway_client')->get('/v1/healthcheck');
+            $url = new Uri(getenv('SIRIUS_API_BASE_URI').'/v1/healthcheck');
+            $request = new Request('GET', $url, $headers = [
+                'Accept'        => 'application/json',
+                'Content-type'  => 'application/json'
+            ]);
+
+            $provider = CredentialProvider::defaultProvider();
+            $signer = new SignatureV4('execute-api', 'eu-west-1');
+
+            // Sign the request with an AWS Authorization header.
+            $signedRequest = $signer->signRequest($request, $provider()->wait());
+            $response = $container->get('guzzle_api_gateway_client')->sendRequest($signedRequest);
+            
             if (200 !== $response->getStatusCode()) {
                 throw new \RuntimeException('returned HTTP code ' . $response->getStatusCode());
             }
