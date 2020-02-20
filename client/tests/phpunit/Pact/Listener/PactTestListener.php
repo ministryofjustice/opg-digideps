f25c0d3290f1ad3ba9b213e1c7f4a9a7330f4995
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2020-02-20 15:34:28 +0000

    Remove unused imports

--- client/tests/phpunit/Pact/Listener/PactTestListener.php
+++ client/tests/phpunit/Pact/Listener/PactTestListener.php
@@ -3,11 +3,8 @@
 namespace AppBundle\Pact\Listener;
 
 use GuzzleHttp\Client;
-use GuzzleHttp\Psr7\Uri;
-use PhpPact\Broker\Service\BrokerHttpClient;
 use PhpPact\Http\GuzzleClient;
 use PhpPact\Standalone\Exception\MissingEnvVariableException;
-use PhpPact\Standalone\MockService\MockServer;
 use PhpPact\Standalone\MockService\MockServerConfigInterface;
 use PhpPact\Standalone\MockService\MockServerEnvConfig;
 use PhpPact\Standalone\MockService\Service\MockServerHttpService;
