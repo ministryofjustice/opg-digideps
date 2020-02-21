c6feb536f3c9aa0e183c8273f57cbdf246c47041
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2020-02-21 16:44:49 +0000

    Move enhanced monitoring role into shared
    We only need one for each account

--- environment/api_rds.tf
+++ environment/api_rds.tf
@@ -92,26 +92,8 @@ resource "aws_rds_cluster" "api" {
 #   }
 # }
 
-resource "aws_iam_role" "enhanced_monitoring" {
-  name               = "rds-enhanced-monitoring"
-  assume_role_policy = data.aws_iam_policy_document.enhanced_monitoring.json
-}
-
-resource "aws_iam_role_policy_attachment" "enhanced_monitoring" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
-  role       = aws_iam_role.enhanced_monitoring.name
-}
-
-data "aws_iam_policy_document" "enhanced_monitoring" {
-  statement {
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      identifiers = ["monitoring.rds.amazonaws.com"]
-      type        = "Service"
-    }
-  }
+data "aws_iam_role" "enhanced_monitoring" {
+  name = "rds-enhanced-monitoring"
 }
 
 locals {
