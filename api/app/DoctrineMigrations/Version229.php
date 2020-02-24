8a354ced70424a2704eda9f46c9bbb98ed81e840
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2020-02-24 16:43:57 +0000

    Fix class name typo

--- api/app/DoctrineMigrations/Version229.php
+++ api/app/DoctrineMigrations/Version229.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200224145703 extends AbstractMigration
+final class Version229 extends AbstractMigration
 {
     public function getDescription() : string
     {
