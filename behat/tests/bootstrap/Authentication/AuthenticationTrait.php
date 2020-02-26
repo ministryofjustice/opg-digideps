b5adcf37a9f1311c893a9112e77900aeb794aa8a
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2020-02-26 13:51:04 +0000

    Fix admin password reset tests

--- behat/tests/bootstrap/Authentication/AuthenticationTrait.php
+++ behat/tests/bootstrap/Authentication/AuthenticationTrait.php
@@ -5,17 +5,22 @@ namespace DigidepsBehat\Authentication;
 trait AuthenticationTrait
 {
     /**
-     * @When /^I open the (activation|password reset) page for "(.+)"$/
+     * @When /^I open the (admin |)(activation|password reset) page for "(.+)"$/
      */
-    public function openActivationOrPasswordResetPage($pageType, $email)
+    public function openActivationOrPasswordResetPage($admin, $pageType, $email)
     {
         $url = sprintf('/admin/fixtures/user-registration-token?email=%s', $email);
         $this->iAmLoggedInToAdminAsWithPassword('admin@publicguardian.gov.uk', 'Abcd1234');
         $this->visitAdminPath($url);
         $token = $this->getSession()->getPage()->getContent();
+        $this->visitAdminPath('/logout');
 
         $page = $pageType === 'activation' ? 'activate' : 'password-reset';
 
-        $this->visitPath("/user/$page/$token");
+        if ($admin === '') {
+            $this->visitPath("/user/$page/$token");
+        } else {
+            $this->visitAdminPath("/user/$page/$token");
+        }
     }
 }
