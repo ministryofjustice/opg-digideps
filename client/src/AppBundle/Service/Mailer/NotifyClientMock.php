6dabd179ba170931a81f199a046871444f138bec
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2020-02-26 17:05:16 +0000

    Mock notify in dev/test environments

---
+++ client/src/AppBundle/Service/Mailer/NotifyClientMock.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Service\Mailer;
+
+use Alphagov\Notifications\Client;
+use Alphagov\Notifications\Exception\NotifyException;
+
+class NotifyClientMock extends Client
+{
+    public function __construct()
+    {
+    }
+
+    public function sendEmail($emailAddress, $templateId, array $personalisation = array(), $reference = '', $emailReplyToId = NULL)
+    {
+        if ($emailAddress === 'break@publicguardian.gov.uk') {
+            throw new NotifyException('Intentional mock exception');
+        } else {
+            return true;
+        }
+    }
+}
