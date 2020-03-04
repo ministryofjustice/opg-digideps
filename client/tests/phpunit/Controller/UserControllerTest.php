21249e69e58311156a44d58e9d6589be2205d4f8
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2020-03-04 13:05:21 +0000

    Add more tests for activation emails

--- client/tests/phpunit/Controller/UserControllerTest.php
+++ client/tests/phpunit/Controller/UserControllerTest.php
@@ -4,6 +4,8 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\User;
 use AppBundle\Model\Email;
+use AppBundle\Model\SelfRegisterData;
+use AppBundle\Service\Mailer\MailFactory;
 use AppBundle\Service\Mailer\MailSender;
 use Prophecy\Argument;
 
@@ -12,6 +14,80 @@ class UserControllerTest extends AbstractControllerTestCase
     public function setUp(): void
     {
         parent::setUp();
+    }
+
+    public function testActivateLinkSendAction(): void
+    {
+        $emailAddress = 'test@mailbox.example';
+        $token = 'token';
+        $user = new User();
+        $user->setEmail($emailAddress);
+        $user->setRegistrationToken('token');
+
+        $this->restClient->loadUserByToken($token)->shouldBeCalled()->willReturn($user);
+        $this->restClient->userRecreateToken($emailAddress, 'activate')->shouldBeCalled()->willReturn($user);
+
+        $this->injectProphecyService(MailSender::class, function($mailSender) use ($emailAddress) {
+            $mailSender->send(Argument::that(function ($email) use ($emailAddress) {
+                return $email instanceof Email
+                    && $email->getToEmail() === $emailAddress
+                    && $email->getTemplate() === MailFactory::ACTIVATION_TEMPLATE_ID
+                    && strpos($email->getParameters()['activationLink'], "user/activate/$token") !== false;
+            }), Argument::cetera())->shouldBeCalled()->willReturn();
+        });
+
+        $this->client->request('GET', "/user/activate/password/send/$token");
+        $this->client->followRedirect();
+        $response = $this->client->getResponse();
+
+        self::assertEquals(200, $response->getStatusCode());
+        self::assertStringContainsString('Check your email inbox, we&#039;ve sent you an email with a new link.', $response->getContent());
+    }
+
+    public function testRegisterAction(): void
+    {
+        $crawler = $this->client->request('GET', '/register');
+        $button = $crawler->selectButton('Sign up');
+
+        $data = new SelfRegisterData();
+        $data->setFirstname('Denis');
+        $data->setLastname('Brauchla');
+        $data->setPostcode('DB1 9FI');
+        $data->setEmail('d.brauchla@mailbox.example');
+        $data->setClientFirstname('Abraham');
+        $data->setClientLastname('Ruhter');
+        $data->setCaseNumber('13859388');
+
+        $user = (new User())
+            ->setEmail('d.brauchla@mailbox.example')
+            ->setRegistrationToken('selfregister-token');
+
+        $this->restClient->registerUser($data)->willReturn($user);
+
+        $this->injectProphecyService(MailSender::class, function($mailSender) {
+            $mailSender->send(Argument::that(function ($email) {
+                return $email instanceof Email
+                    && $email->getToEmail() === 'd.brauchla@mailbox.example'
+                    && $email->getTemplate() === MailFactory::ACTIVATION_TEMPLATE_ID
+                    && strpos($email->getParameters()['activationLink'], 'user/activate/selfregister-token') !== false;
+            }), Argument::cetera())->shouldBeCalled()->willReturn();
+        });
+
+        $this->client->submit($button->form(), [
+            'self_registration[firstname]' => $data->getFirstname(),
+            'self_registration[lastname]' => $data->getLastname(),
+            'self_registration[postcode]' => $data->getPostcode(),
+            'self_registration[email][first]' => $data->getEmail(),
+            'self_registration[email][second]' => $data->getEmail(),
+            'self_registration[clientFirstname]' => $data->getClientFirstname(),
+            'self_registration[clientLastname]' => $data->getClientLastname(),
+            'self_registration[caseNumber]' => $data->getCaseNumber(),
+        ]);
+
+        $response = $this->client->getResponse();
+
+        self::assertEquals(200, $response->getStatusCode());
+        self::assertStringContainsString('We\'ve sent you a link to <strong class="bold-small">d.brauchla@mailbox.example</strong>', $response->getContent());
     }
 
     public function testPasswordForgottenAction(): void
@@ -27,6 +103,7 @@ class UserControllerTest extends AbstractControllerTestCase
             $mailSender->send(Argument::that(function ($email) use ($emailAddress) {
                 return $email instanceof Email
                     && $email->getToEmail() === $emailAddress
+                    && $email->getTemplate() === MailFactory::RESET_PASSWORD_TEMPLATE_ID
                     && strpos($email->getParameters()['resetLink'], 'user/password-reset/test') !== false;
             }), Argument::cetera())->shouldBeCalled()->willReturn();
         });
@@ -41,5 +118,11 @@ class UserControllerTest extends AbstractControllerTestCase
         $this->client->submit($button->form(), [
             'password_forgotten[email]' => $emailAddress,
         ]);
+
+        $this->client->followRedirect();
+        $response = $this->client->getResponse();
+
+        self::assertEquals(200, $response->getStatusCode());
+        self::assertStringContainsString('We have sent a new registration link to your email', $response->getContent());
     }
 }
