acab1a20969818cfea60ec23b8824889e9d0c938
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2020-03-09 17:49:57 +0000

    Create endpoint for SSM
    So it can be accessed by services inside our VPC

---
+++ shared/endpoint_ssm.tf
@@ -0,0 +1,28 @@
+resource "aws_vpc_endpoint" "ssm" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.eu-west-1.ssm"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  security_group_ids  = aws_security_group.ssm_endpoint[*].id
+  subnet_ids          = aws_subnet.private[*].id
+  tags                = merge(local.default_tags, { Name = "ssm" })
+}
+
+resource "aws_security_group" "ssm_endpoint" {
+  name_prefix = "ssm_endpoint"
+  vpc_id      = aws_vpc.main.id
+  tags        = merge(local.default_tags, { Name = "ssm_endpoint" })
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "ssm_endpoint_https_in" {
+  from_port         = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.ssm_endpoint.id
+  to_port           = 443
+  type              = "ingress"
+  cidr_blocks       = [aws_vpc.main.cidr_block]
+}
