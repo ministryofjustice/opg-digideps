9b75bc4da182633682e1b594127cadb9ed8e1f7a
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2020-03-09 16:49:57 +0000

    More parameter to depend on environment
    This means it can be managed cleanly by Terraform, and we can change environment-specific settings individually (rather than e.g. turning off for all environments at once)

--- environment/parameters.tf
+++ environment/parameters.tf
@@ -3,7 +3,11 @@ data "aws_ssm_parameter" "sirius_api_base_uri" {
 }
 
 resource "aws_ssm_parameter" "flag_document_sync" {
-  name  = format("/%s", join("/", compact([local.account.secrets_prefix, "flag/document-sync"])))
+  name  = format("/%s", join("/", compact([local.environment, "flag/document-sync"])))
   type  = "String"
   value = "1"
+
+  lifecycle {
+    ignore_changes = [value]
+  }
 }
