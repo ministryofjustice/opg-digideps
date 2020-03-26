4b1a4b7a4bbc7bb76121f96086cacf220b978353
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2020-03-26 13:22:57 +0000

    Move migrations to `/DoctrineMigrations`

--- api/DoctrineMigrations/HOW-TO-MERGE-MIGRATIONS.md
+++ api/DoctrineMigrations/HOW-TO-MERGE-MIGRATIONS.md
@@ -6,7 +6,7 @@ Replace `Version181` in those instruction with the last migration on production.
 
 Run `select max(version) from migrations;` on prod to make sure.
 
-Note: this operations won't affect any environment already at that version 181 or above 
+Note: this operations won't affect any environment already at that version 181 or above
 
 
 * branch into `merge-migrations`
@@ -16,6 +16,6 @@ Note: this operations won't affect any environment already at that version 181 o
 
         sh scripts/initialize_schema.sh
         app/console doctrine:migrations:diff
-        
+
 * Rename the newly created migration into `Version181`
 * Run unit tests locally, and also run on jenkins. merge if green
--- api/DoctrineMigrations/Version224.php
+++ api/DoctrineMigrations/Version224.php
@@ -20,14 +20,14 @@ final class Version224 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql("UPDATE report SET due_date = end_date + INTERVAL '21 days' WHERE end_date > '2019-11-12' AND 
+        $this->addSql("UPDATE report SET due_date = end_date + INTERVAL '21 days' WHERE end_date > '2019-11-12' AND
                        type IN ('102', '103', '104', '102-4', '103-4')");
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql("UPDATE report SET due_date = end_date + INTERVAL '56 days' WHERE end_date > '2019-11-12' AND 
+        $this->addSql("UPDATE report SET due_date = end_date + INTERVAL '56 days' WHERE end_date > '2019-11-12' AND
                        type IN ('102', '103', '104', '102-4', '103-4')");
     }
 }
