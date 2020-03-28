50f0371d3671d047f6fe2ed6c5560d2dc0e00615
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2020-03-28 13:53:11 +0000

    Include report submission when getting queued Documents

--- client/src/AppBundle/Command/DocumentSyncCommand.php
+++ client/src/AppBundle/Command/DocumentSyncCommand.php
@@ -69,7 +69,16 @@ class DocumentSyncCommand extends DaemonableCommand
     {
         $options = [
             'query' => [
-                'groups' => ['documents', 'document-synchronisation', 'document-storage-reference', 'document-report', 'report', 'report-client', 'client-case-number']
+                'groups' => [
+                    'documents',
+                    'document-synchronisation',
+                    'document-storage-reference',
+                    'document-report',
+                    'report',
+                    'report-client',
+                    'client-case-number',
+                    'document-report-submission'
+                ]
             ]
         ];
 
