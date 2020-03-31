a0950f9be001ab565dc91dc9ff4856853455cb76
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2020-03-31 15:45:04 +0100

    Wrap button in link

--- client/src/AppBundle/Resources/assets/javascripts/modules/buttonToggler.js
+++ client/src/AppBundle/Resources/assets/javascripts/modules/buttonToggler.js
@@ -1,12 +1,14 @@
 class ButtonToggler {
-    toggleButton(buttonId) {
-        let button = document.getElementById(buttonId);
-        button.disabled = (buttonId === 'false') ? 'true' : 'false';
-    }
+  static toggleButton (buttonId) {
+    let button = document.getElementById(buttonId)
+    button.disabled = (buttonId === 'false') ? 'true' : 'false'
+  }
 
-    static addToggleEventListener(elementId, buttonId) {
-        elementId.addEventListener('onclick', this.toggleButton(buttonId))
-    }
+  static addToggleEventListener (elementId, buttonId) {
+      let element = document.getElementById(elementId)
+      element.addEventListener('onclick', this.toggleButton(buttonId))
+      // Update classes and aria-disabled too
+  }
 }
 
 export default ButtonToggler
