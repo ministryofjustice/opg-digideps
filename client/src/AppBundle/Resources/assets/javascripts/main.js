9a3d1af47b9feea681cf4f13a9289fb39894d142
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2020-04-09 11:52:33 +0100

    Use GDS polyfill for forEach

--- client/src/AppBundle/Resources/assets/javascripts/main.js
+++ client/src/AppBundle/Resources/assets/javascripts/main.js
@@ -20,6 +20,19 @@ var uploadFile = require('./modules/uploadFile.js')
 var uploadProgressPA = require('./modules/uploadProgressPA.js')
 var uploadProgress = require('./modules/uploadProgress.js')
 
+/**
+ * Taken from govuk-frontend. Supports back to IE8
+ * See: https://github.com/alphagov/govuk-frontend/blob/063cd8e2470b62b824c6e50ca66342ac7a95d2d8/src/govuk/common.js#L6
+ */
+export function nodeListForEach (nodes, callback) {
+  if (window.NodeList.prototype.forEach) {
+    return nodes.forEach(callback)
+  }
+  for (let i = 0; i < nodes.length; i++) {
+    callback.call(window, nodes[i], i, nodes)
+  }
+}
+
 window.opg = {
   Ga: Ga,
   SessionTimeoutDialog: SessionTimeoutDialog
@@ -68,11 +81,11 @@ $(document).ready(function () {
   var showHideContent = new ShowHideContent()
   showHideContent.init()
 
-  const togglers = document.querySelectorAll('[data-module="opg-button-toggler"]')
+  const $togglers = document.querySelectorAll('[data-module="opg-button-toggler"]')
 
-  if (togglers !== null) {
-    togglers.forEach(toggler => {
-      new ButtonToggler().init(toggler)
+  if ($togglers !== null) {
+    nodeListForEach($togglers, function ($el) {
+      new ButtonToggler().init($el)
     })
   }
 })
--- client/src/AppBundle/Resources/assets/javascripts/modules/buttonToggler.js
+++ client/src/AppBundle/Resources/assets/javascripts/modules/buttonToggler.js
@@ -1,18 +1,27 @@
+import { nodeListForEach } from '../main.js'
+
 class ButtonToggler {
   init (toggle) {
     toggle.addEventListener('click', () => this.toggleButtonDisabled())
   }
 
   toggleButtonDisabled () {
-    const buttons = document.querySelectorAll('[data-module="opg-toggleable-button"]')
+    const $buttons = document.querySelectorAll('[data-module="opg-toggleable-button"]')
 
-    buttons.forEach(button => {
-      const disabled = !button.disabled
+    nodeListForEach($buttons, function ($el) {
+      const disabled = !$el.disabled
 
-      button.disabled = disabled
-      button.setAttribute('aria-disabled', disabled.toString())
-      button.classList.toggle('govuk-button--disabled')
+      $el.disabled = disabled
+      $el.setAttribute('aria-disabled', disabled.toString())
+      $el.classList.toggle('govuk-button--disabled')
     })
+    // buttons.forEach(button => {
+    //   const disabled = !button.disabled
+    //
+    //   button.disabled = disabled
+    //   button.setAttribute('aria-disabled', disabled.toString())
+    //   button.classList.toggle('govuk-button--disabled')
+    // })
   }
 }
 
