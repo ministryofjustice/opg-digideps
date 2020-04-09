e656992330009a0a62199d0b0b62e57e14dcab61
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2020-04-09 11:52:33 +0100

    Use GDS polyfill for forEach

--- client/src/AppBundle/Resources/assets/javascripts/main.js
+++ client/src/AppBundle/Resources/assets/javascripts/main.js
@@ -25,7 +25,7 @@ var uploadProgress = require('./modules/uploadProgress.js')
  * Taken from govuk-frontend. Supports back to IE8
  * See: https://github.com/alphagov/govuk-frontend/blob/063cd8e2470b62b824c6e50ca66342ac7a95d2d8/src/govuk/common.js#L6
  */
-function nodeListForEach (nodes, callback) {
+export function nodeListForEach (nodes, callback) {
   if (window.NodeList.prototype.forEach) {
     return nodes.forEach(callback)
   }
@@ -88,11 +88,11 @@ $(document).ready(function () {
   var showHideContent = new ShowHideContent()
   showHideContent.init()
 
-  const togglers = document.querySelectorAll('[data-module="opg-button-toggler"]')
+  const $togglers = document.querySelectorAll('[data-module="opg-button-toggler"]')
 
-  if (togglers !== null) {
-    togglers.forEach(toggler => {
-      new ButtonToggler().init(toggler)
+  if ($togglers !== null) {
+    nodeListForEach($togglers, function ($el) {
+      new ButtonToggler().init($el)
     })
   }
 })
