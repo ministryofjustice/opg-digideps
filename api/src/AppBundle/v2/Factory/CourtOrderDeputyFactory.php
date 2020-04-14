dd07a25d55f163b7ff90ea6856ff80eb810158c8
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2020-04-14 15:19:16 +0100

    Create CourtOrderDeputy factory

---
+++ api/src/AppBundle/v2/Factory/CourtOrderDeputyFactory.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace AppBundle\v2\Factory;
+
+use AppBundle\Entity\Client;
+use AppBundle\Entity\CourtOrder;
+use AppBundle\Entity\CourtOrderAddress;
+use AppBundle\Entity\CourtOrderDeputy;
+use AppBundle\Entity\Report\Report;
+use AppBundle\v2\DTO\CourtOrderAddressDto;
+use AppBundle\v2\DTO\CourtOrderDeputyDto;
+use AppBundle\v2\DTO\CourtOrderDto;
+
+class CourtOrderDeputyFactory
+{
+    /**
+     * @param CourtOrderDto $dto
+     * @param Client $client
+     * @param Report $report
+     * @return CourtOrder
+     */
+    public function create(CourtOrderDeputyDto $deputyDto, CourtOrderAddressDto $addressDto, CourtOrder $courtOrder)
+    {
+        $address = new CourtOrderAddress();
+        $address
+            ->setAddressLine1($addressDto->getAddressLine1())
+            ->setAddressLine2($addressDto->getAddressLine2())
+            ->setAddressLine3($addressDto->getAddressLine3())
+            ->setTown($addressDto->getTown())
+            ->setCounty($addressDto->getCounty())
+            ->setPostcode($addressDto->getPostcode())
+            ->setCountry($addressDto->getCountry());
+
+        $deputy = new CourtOrderDeputy();
+        $deputy
+            ->setDeputyNumber($deputyDto->getDeputyNumber())
+            ->setFirstname($deputyDto->getFirstname())
+            ->setSurname($deputyDto->getSurname())
+            ->setEmail($deputyDto->getEmail());
+
+        $deputy->addAddress($address);
+        $courtOrder->addDeputy($deputy);
+
+        return $deputy;
+    }
+}
