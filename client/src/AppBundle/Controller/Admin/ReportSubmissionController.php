db6c670a07b17b9758bd8914d9e5c128e964ba1a
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2020-04-20 14:32:04 +0100

    Control button placement with feature flag

--- client/src/AppBundle/Controller/Admin/ReportSubmissionController.php
+++ client/src/AppBundle/Controller/Admin/ReportSubmissionController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller\Admin;
 use AppBundle\Controller\AbstractController;
 use AppBundle\Entity as EntityDir;
 use AppBundle\Service\DocumentDownloader;
+use AppBundle\Service\FeatureFlagService;
 use AppBundle\Service\File\Storage\S3Storage;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -54,7 +55,7 @@ class ReportSubmissionController extends AbstractController
      *
      * @return array<mixed>|Response
      */
-    public function indexAction(Request $request)
+    public function indexAction(Request $request, FeatureFlagService $featureFlagService)
     {
         if ($request->isMethod('POST')) {
             $ret = $this->processPost($request);
@@ -76,7 +77,11 @@ class ReportSubmissionController extends AbstractController
         if ($currentFilters['status'] === 'archived') {
             $postActions = [self::ACTION_DOWNLOAD];
         } else {
-            $postActions = [self::ACTION_DOWNLOAD, self::ACTION_ARCHIVE, self::ACTION_SYNCHRONISE];
+            $postActions = [self::ACTION_DOWNLOAD, self::ACTION_ARCHIVE];
+        }
+
+        if ($featureFlagService->get(FeatureFlagService::FLAG_DOCUMENT_SYNC) === '1') {
+            $postActions[] = self::ACTION_SYNCHRONISE;
         }
 
         return [
