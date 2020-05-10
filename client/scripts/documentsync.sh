1024243632186760f64238224faea60488959a78
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2020-05-10 10:28:09 +0100

    Create cron schedule

--- client/scripts/documentsync.sh
+++ client/scripts/documentsync.sh
@@ -4,4 +4,18 @@ set -e
 # We need below to create the params file on container start
 confd -onetime -backend env
 
-su-exec www-data php app/console digideps:document-sync $@
+EXITSIGNAL=1
+
+while [ ${EXITSIGNAL} -eq 1 ]; do
+  php app/console digideps:document-sync $@
+  printf 'Return value is %s' $?
+  EXITSIGNAL=$?
+done
+
+
+
+echo 'hi'
+echo "$0"
+echo 'hi'
+echo "$@"
+#su-exec www-data php app/console digideps:document-sync $0 $@
