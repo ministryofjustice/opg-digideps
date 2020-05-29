95e8f90bafe0b13cf02f831c51c3f83efe38bb91
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2020-05-29 18:06:58 +0100

    Create YesNoNa validator and applies validator to profDeputyCosts questions

---
+++ client/src/AppBundle/Validator/Constraints/YesNoNaValidator.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace AppBundle\Validator\Constraints;
+
+use AppBundle\Entity\ReportInterface;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+
+class YesNoNaValidator extends ConstraintValidator
+{
+    /**
+     * @param mixed $data
+     * @param Constraint $constraint
+     */
+    public function validate($value, Constraint $constraint)
+    {
+        // custom constraints should ignore null and empty values to allow
+        // other constraints (NotBlank, NotNull, etc.) take care of that
+        if (null === $value || '' === $value) {
+            return;
+        }
+
+        if (!is_string($value)) {
+            throw new UnexpectedTypeException($value, 'string');
+        }
+
+        if (!in_array($value, ['yes', 'no'])) {
+            $this->context->buildViolation($constraint->message)
+                ->setParameter('{{ string }}', $value)
+                ->addViolation();
+        }
+    }
+}
---
+++ client/tests/phpunit/Entity/Report/ChecklistTest.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types=1);
+
+
+namespace Tests\AppBundle\Entity\Report;
+
+use AppBundle\Entity\Report\Checklist;
+use AppBundle\Entity\Report\Report;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class ChecklistTest extends KernelTestCase
+{
+    /** @var ValidatorInterface */
+    private $validator;
+
+    public function setUp(): void
+    {
+        self::bootKernel();
+        $this->validator = self::$kernel->getContainer()->get('validator.builder')->enableAnnotationMapping()->getValidator();
+    }
+
+    /**
+     * @test
+     * @dataProvider submit_profDeputyCosts_checklist_valuesProvider
+     */
+    public function validations_submit_profDeputyCosts_checklist(
+        $profCostsReasonableAndProportionate,
+        $paymentsMatchCostCertificate,
+        $hasDeputyOverchargedFromPreviousEstimates,
+        $expectedCountErrors
+    )
+    {
+        $report = new Report();
+
+        $checklist = new Checklist($report);
+        $checklist->setProfCostsReasonableAndProportionate($profCostsReasonableAndProportionate);
+        $checklist->setPaymentsMatchCostCertificate($paymentsMatchCostCertificate);
+        $checklist->setHasDeputyOverchargedFromPreviousEstimates($hasDeputyOverchargedFromPreviousEstimates);
+
+        $errors = $this->validator->validate($checklist, null, ['submit-profDeputyCosts-checklist']);
+        self::assertEquals($expectedCountErrors, count($errors));
+    }
+
+    public function submit_profDeputyCosts_checklist_valuesProvider()
+    {
+        return [
+            'valid values' => [null, 'yes', 'no', 0],
+            '1 invalid value' => ['maybe', null, 'yes', 1],
+            '2 valid values' => ['yes', 'possibly', 'potentially', 2],
+            '3 valid values' => ['definitely', 'negative', 'nope', 3]
+        ];
+    }
+}
