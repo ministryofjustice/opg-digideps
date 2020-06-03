4218c6e7e5cb8005c7e15e99d148c9b3e24d5673
Alex Eves <alex.eves@aiesoftware.com>
2020-06-03 11:12:22 +0100

    Fixed Version class name

--- api/app/DoctrineMigrations/Version237.php
+++ api/app/DoctrineMigrations/Version237.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version236 extends AbstractMigration
+final class Version237 extends AbstractMigration
 {
     public function getDescription() : string
     {
