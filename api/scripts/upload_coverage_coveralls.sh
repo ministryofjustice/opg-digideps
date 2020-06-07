1ab47db4638c20af80b3a5cfe850bb3c0c2eba25
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2020-06-07 17:00:47 +0100

    Perms

--- api/scripts/upload_coverage_coveralls.sh
+++ api/scripts/upload_coverage_coveralls.sh
@@ -5,6 +5,4 @@ set -e
 wget https://github.com/php-coveralls/php-coveralls/releases/download/v2.2.0/php-coveralls.phar
 chmod +x php-coveralls.phar
 
-chmod -R 777 merged.xml
-
 ./php-coveralls.phar -v --json_path ./coveralls-upload.json --coverage_clover merged.xml --root_dir api
