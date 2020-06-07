49fad3f768e551d953f2440e0e56ecd7e725c795
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2020-06-07 13:10:53 +0100

    Try removing project name

---
+++ api/scripts/upload_coverage_coveralls.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# exit on error
+set -e
+
+wget https://github.com/php-coveralls/php-coveralls/releases/download/v2.2.0/php-coveralls.phar
+chmod +x php-coveralls.phar
+
+./php-coveralls.phar -v --json_path ./coveralls-upload.json --coverage_clover merged.xml --root_dir api
