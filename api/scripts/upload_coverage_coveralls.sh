3963b373d4d6d43a89826f7f9419751549cd2533
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2020-06-07 17:53:09 +0100

    Switch back to docker

--- api/scripts/upload_coverage_coveralls.sh
+++ api/scripts/upload_coverage_coveralls.sh
@@ -5,4 +5,4 @@ set -e
 wget https://github.com/php-coveralls/php-coveralls/releases/download/v2.2.0/php-coveralls.phar
 chmod +x php-coveralls.phar
 
-./php-coveralls.phar -v --json_path ./coveralls-upload.json --coverage_clover merged.xml --root_dir api
+./php-coveralls.phar -v --json_path ./coveralls-upload.json --coverage_clover tests/coverage/merged.xml --root_dir .
