bf4e401fa09278711577a175f507d17423c2dc2d
james.warren <james.warren@digital.justice.gov.uk>
2020-07-21 18:41:41 +0100

    ddpb-3507 test from scratch

--- shared/endpoint_ecr.tf
+++ shared/endpoint_ecr.tf
@@ -26,3 +26,34 @@ resource "aws_security_group_rule" "ecr_endpoint_https_in" {
   type              = "ingress"
   cidr_blocks       = [aws_vpc.main.cidr_block]
 }
+
+
+
+resource "aws_vpc_endpoint" "ecr_api" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.eu-west-1.ecr.api"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  security_group_ids  = aws_security_group.ecr_api_endpoint[*].id
+  subnet_ids          = aws_subnet.private[*].id
+  tags                = merge(local.default_tags, { Name = "ecr_api" })
+}
+
+resource "aws_security_group" "ecr_api_endpoint" {
+  name_prefix = "ecr_api_endpoint"
+  vpc_id      = aws_vpc.main.id
+  tags        = merge(local.default_tags, { Name = "ecr_api_endpoint" })
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "ecr_api_endpoint_https_in" {
+  from_port         = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.ecr_api_endpoint.id
+  to_port           = 443
+  type              = "ingress"
+  cidr_blocks       = [aws_vpc.main.cidr_block]
+}
