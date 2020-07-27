602d932a0d3c509c7f84c7786ca6ab81d08f8b4b
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2020-07-27 11:51:59 +0100

    Export db creds when loading fixtures

--- api/scripts/reset_db_fixtures.sh
+++ api/scripts/reset_db_fixtures.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+export PGHOST=${DATABASE_HOSTNAME:=postgres}
+export PGPASSWORD=${DATABASE_PASSWORD:=api}
+export PGDATABASE=${DATABASE_NAME:=api}
+export PGUSER=${DATABASE_USERNAME:=api}
+
 # We need below to create the params file on container start
 confd -onetime -backend env
 
