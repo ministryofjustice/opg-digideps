007f41a915ed952fc34a09acd53c7ab875106401
Alex Eves <alex.eves@aiesoftware.com>
2020-07-31 15:13:52 +0100

    Update to point to v2 pact

--- client/tests/phpunit/Pact/SiriusApiGatewayClientTest.php
+++ client/tests/phpunit/Pact/SiriusApiGatewayClientTest.php
@@ -257,7 +257,7 @@ class SiriusDocumentsContractTest extends KernelTestCase
         // Create your expected request from the consumer.
         $request = (new ConsumerRequest())
             ->setMethod('POST')
-            ->setPath(sprintf('/v1/clients/%s/reports/%s/checklists', $caseRef, $reportPdfDocumentUuid))
+            ->setPath(sprintf('/v2/clients/%s/reports/%s/checklists', $caseRef, $reportPdfDocumentUuid))
             ->addHeader('Content-Type', 'application/json')
             ->setBody([
                 'checklist' => [
