1f0c05931aefe82357dbf834c34dabefad3929b0
james.warren <james.warren@digital.justice.gov.uk>
2020-08-05 16:35:41 +0100

    DDPB-3532 create development environment

--- environment/front_service.tf
+++ environment/front_service.tf
@@ -90,7 +90,8 @@ locals {
       { "name": "SESSION_REDIS_DSN", "value": "redis://${aws_route53_record.front_redis.fqdn}" },
       { "name": "SYMFONY_ENV", "value": "${local.account.symfony_env}" },
       { "name": "OPG_DOCKER_TAG", "value": "${var.OPG_DOCKER_TAG}" },
-      { "name": "WKHTMLTOPDF_ADDRESS", "value": "http://${local.wkhtmltopdf_service_fqdn}" }
+      { "name": "WKHTMLTOPDF_ADDRESS", "value": "http://${local.wkhtmltopdf_service_fqdn}" },
+      { "name": "ENVIRONMENT", "value": "${local.environment}" }
     ]
   }
 
