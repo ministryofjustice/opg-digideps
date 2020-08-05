1d15bdb711d914c23f2118ab3348064cb5cb90da
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2020-08-05 12:03:20 +0100

    ADd further logging for sync process

--- client/src/AppBundle/Service/Client/Sirius/SiriusApiGatewayClient.php
+++ client/src/AppBundle/Service/Client/Sirius/SiriusApiGatewayClient.php
@@ -77,6 +77,9 @@ class SiriusApiGatewayClient
     {
         $reportJson = $this->serializer->serialize(['report' => ['data' => $upload]], 'json');
 
+        $this->logger->warning("Syncing reportPDF document");
+        $this->logger->warning($reportJson);
+
         $signedRequest = $this->buildSignedRequest(
             sprintf(self::SIRIUS_REPORT_ENDPOINT, $caseRef),
             'POST',
@@ -100,7 +103,8 @@ class SiriusApiGatewayClient
     {
         $reportJson = $this->serializer->serialize(['supporting_document' => ['data' => $upload]], 'json');
 
-        $this->logger->warning("Syncing supporting document ID with UUID: $submissionUuid");
+        $this->logger->warning("Syncing supporting document with UUID: $submissionUuid");
+        $this->logger->warning($reportJson);
 
         $signedRequest = $this->buildSignedRequest(
             sprintf(self::SIRIUS_SUPPORTING_DOCUMENTS_ENDPOINT, $caseRef, $submissionUuid),
