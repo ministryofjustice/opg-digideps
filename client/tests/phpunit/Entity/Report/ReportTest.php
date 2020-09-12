5c24e382ad9949abb5ff6d5104816de8b4785b5d
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2020-09-12 20:39:54 +0100

    Test fix

--- client/tests/phpunit/Entity/Report/ReportTest.php
+++ client/tests/phpunit/Entity/Report/ReportTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\AppBundle\Entity\Report;
 
-
 use AppBundle\Entity\Report\Report;
 use PHPUnit\Framework\TestCase;
 
@@ -23,8 +22,8 @@ class ReportTest extends TestCase
             'HW' => [(new Report())->setType(Report::TYPE_HEALTH_WELFARE), 'HW'],
             'PF - low' => [(new Report())->setType(Report::TYPE_PROPERTY_AND_AFFAIRS_LOW_ASSETS), 'PF'],
             'PF - high' => [(new Report())->setType(Report::TYPE_PROPERTY_AND_AFFAIRS_HIGH_ASSETS), 'PF'],
-            'Combined - low' => [(new Report())->setType(Report::TYPE_COMBINED_LOW_ASSETS), 'Combined'],
-            'Combined - high' => [(new Report())->setType(Report::TYPE_COMBINED_HIGH_ASSETS), 'Combined'],
+            'Combined - low' => [(new Report())->setType(Report::TYPE_COMBINED_LOW_ASSETS), 'COMBINED'],
+            'Combined - high' => [(new Report())->setType(Report::TYPE_COMBINED_HIGH_ASSETS), 'COMBINED'],
         ];
     }
 }
