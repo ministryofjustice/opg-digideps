a162d93bedebce5e0c30c58ad8d97566d6d7182c
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2020-09-14 21:58:11 +0100

    Drop test specific symfony commands from db scripts

--- api/scripts/reset_db_structure.sh
+++ api/scripts/reset_db_structure.sh
@@ -11,13 +11,7 @@ confd -onetime -backend env
 
 echo "Dropping $PGDATABASE database, user $PGUSER on $PGHOST"
 
-# Apply migrations to rebuild database
-su-exec www-data php app/console doctrine:database:drop --force --env=test --if-exists
-su-exec www-data php app/console doctrine:database:create --env=test
-su-exec www-data php app/console doctrine:migrations:status-check --env=test
-su-exec www-data php app/console doctrine:migrations:migrate --no-interaction -vvv --env=test
-
-# Apply migrations to rebuild database
+#Apply migrations to rebuild database
 su-exec www-data php app/console doctrine:database:drop --force --if-exists
 su-exec www-data php app/console doctrine:database:create
 su-exec www-data php app/console doctrine:migrations:status-check
