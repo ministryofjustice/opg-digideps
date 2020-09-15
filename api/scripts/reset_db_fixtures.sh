550562f1d5ee549db6c41a75747ec6574405e3b2
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2020-09-15 17:00:28 +0100

    Try setting env to test again for CI

--- api/scripts/reset_db_fixtures.sh
+++ api/scripts/reset_db_fixtures.sh
@@ -4,4 +4,8 @@ set -e
 # We need below to create the params file on container start
 confd -onetime -backend env
 
+su-exec www-data php app/console cache:clear --no-interaction --no-warmup
+su-exec www-data php app/console cache:clear --no-interaction --no-warmup --env=test
+
 su-exec www-data php app/console doctrine:fixtures:load --no-interaction
+su-exec www-data php app/console doctrine:fixtures:load --no-interaction --env=test
--- api/scripts/reset_db_structure.sh
+++ api/scripts/reset_db_structure.sh
@@ -9,10 +9,13 @@ export PGUSER=${DATABASE_USERNAME:=api}
 # We need below to create the params file on container start
 confd -onetime -backend env
 
-echo "Dropping $PGDATABASE database, user $PGUSER on $PGHOST"
-
 #Apply migrations to rebuild database
 su-exec www-data php app/console doctrine:database:drop --force --if-exists
 su-exec www-data php app/console doctrine:database:create
 su-exec www-data php app/console doctrine:migrations:status-check
 su-exec www-data php app/console doctrine:migrations:migrate --no-interaction -vvv
+
+su-exec www-data php app/console doctrine:database:drop --force --if-exists --env=test
+su-exec www-data php app/console doctrine:database:create --env=test
+su-exec www-data php app/console doctrine:migrations:status-check --env=test
+su-exec www-data php app/console doctrine:migrations:migrate --no-interaction -vvv --env=test
