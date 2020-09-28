b5d32b860c5d2feec6eeffb03a37ff90b3e17606
Tom Withers <thomas.withers@digital.justice.gov.uk>
2020-09-28 09:15:09 +0100

    DDPB-3228 - Add allow major version upgrade flag :face_with_rolling_eyes:

--- environment/api_rds.tf
+++ environment/api_rds.tf
@@ -3,34 +3,35 @@ data "aws_kms_key" "rds" {
 }
 
 resource "aws_db_instance" "api" {
-  count                      = local.account.always_on ? 1 : 0
-  name                       = "api"
-  identifier                 = "api-${local.environment}"
-  instance_class             = "db.m3.medium"
-  allocated_storage          = "10"
-  availability_zone          = "eu-west-1a"
-  backup_retention_period    = local.account.backup_retention_period
-  backup_window              = "00:00-00:30"
-  db_subnet_group_name       = local.account.db_subnet_group
-  engine                     = "postgres"
-  engine_version             = local.account.psql_engine_version
-  kms_key_id                 = data.aws_kms_key.rds.arn
-  license_model              = "postgresql-license"
-  maintenance_window         = "sun:01:00-sun:02:30"
-  monitoring_interval        = "0"
-  option_group_name          = "default:postgres-9-6"
-  parameter_group_name       = "default.postgres9.6"
-  port                       = "5432"
-  skip_final_snapshot        = false
-  storage_encrypted          = true
-  storage_type               = "gp2"
-  username                   = "digidepsmaster"
-  password                   = data.aws_secretsmanager_secret_version.database_password.secret_string
-  deletion_protection        = true
-  delete_automated_backups   = false
-  auto_minor_version_upgrade = false
-  final_snapshot_identifier  = "api-${local.environment}-final"
-  vpc_security_group_ids     = [module.api_rds_security_group.id]
+  count                       = local.account.always_on ? 1 : 0
+  name                        = "api"
+  identifier                  = "api-${local.environment}"
+  instance_class              = "db.m3.medium"
+  allocated_storage           = "10"
+  availability_zone           = "eu-west-1a"
+  backup_retention_period     = local.account.backup_retention_period
+  backup_window               = "00:00-00:30"
+  db_subnet_group_name        = local.account.db_subnet_group
+  engine                      = "postgres"
+  engine_version              = local.account.psql_engine_version
+  kms_key_id                  = data.aws_kms_key.rds.arn
+  license_model               = "postgresql-license"
+  maintenance_window          = "sun:01:00-sun:02:30"
+  monitoring_interval         = "0"
+  option_group_name           = "default:postgres-9-6"
+  parameter_group_name        = "default.postgres9.6"
+  port                        = "5432"
+  skip_final_snapshot         = false
+  storage_encrypted           = true
+  storage_type                = "gp2"
+  username                    = "digidepsmaster"
+  password                    = data.aws_secretsmanager_secret_version.database_password.secret_string
+  deletion_protection         = true
+  delete_automated_backups    = false
+  auto_minor_version_upgrade  = false
+  final_snapshot_identifier   = "api-${local.environment}-final"
+  vpc_security_group_ids      = [module.api_rds_security_group.id]
+  allow_major_version_upgrade = terraform.workspace == "production02" ? false : true
 
   tags = merge(
     local.default_tags,
