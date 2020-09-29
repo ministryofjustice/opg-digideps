c07689fbb71a086f8a42dcac03c1d4bb50607f6b
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2020-09-29 09:57:10 +0100

    Dont update nameddeputy if they exist

--- api/src/AppBundle/Entity/Repository/NamedDeputyRepository.php
+++ api/src/AppBundle/Entity/Repository/NamedDeputyRepository.php
@@ -16,14 +16,7 @@ class NamedDeputyRepository extends EntityRepository
             return $found;
         }
 
-        $namedDeputy = (new NamedDeputy())
-            ->setEmail1($dto->getEmail())
-            ->setDeputyNo($dto->getDeputyNumber())
-            ->setFirstname($dto->getFirstname())
-            ->setLastname($dto->getLastname());
 
-        $this->_em->persist($namedDeputy);
-        $this->_em->flush();
 
         return $namedDeputy;
     }
