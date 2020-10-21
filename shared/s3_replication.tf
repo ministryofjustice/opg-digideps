f3859d40edd810f55dc2a8d72719e1e8534c4e03
james.warren <james.warren@digital.justice.gov.uk>
2020-10-21 10:58:58 +0100

    try with aes256

--- shared/s3_replication.tf
+++ shared/s3_replication.tf
@@ -23,7 +23,7 @@ resource "aws_s3_bucket" "pa_uploads_branch_replication" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorithm = "aws:kms"
+        sse_algorithm = "AES256"
       }
     }
   }
