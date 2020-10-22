f039e6351129fcee00d911fac0f010b98b987eb0
jamesrwarren <58939809+jamesrwarren@users.noreply.github.com>
2020-10-22 19:05:32 +0100

    DDPB-3184 add default encryption to s3

--- environment/s3.tf
+++ environment/s3.tf
@@ -31,6 +31,14 @@ resource "aws_s3_bucket" "pa_uploads" {
     }
   }
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
   replication_configuration {
     role = aws_iam_role.replication.arn
 
