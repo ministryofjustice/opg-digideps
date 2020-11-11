63cd36ac834b4852c109000cd898c0901430390c
Gugandeep Chani <Gugandeep.Chani@digital.justice.gov.uk>
2020-11-11 14:49:19 +0000

    Removing bash from dockerfile and entrypoint. Reverting volumes for test service.

--- behat/docker-entrypoint.sh
+++ behat/docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 export BEHAT_PARAMS="{\"extensions\": {\"Behat\\\\MinkExtension\\\\ServiceContainer\\\\MinkExtension\": {\"base_url\": \"$NONADMIN_HOST\", \"browser_stack\": { \"username\": \"$BROWSERSTACK_USERNAME\", \"access_key\": \"$BROWSERSTACK_KEY\"}}}}"
