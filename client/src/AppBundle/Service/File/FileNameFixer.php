ca6eb7e460b5e45ab5437ea62b3a4a86ff8f9546
Alex Saunders <acsauk@gmail.com>
2020-12-09 14:49:12 +0000

    Add failing test for adding missing extensions

--- client/src/AppBundle/Service/File/FileNameFixer.php
+++ client/src/AppBundle/Service/File/FileNameFixer.php
@@ -2,13 +2,34 @@
 
 namespace AppBundle\Service\File;
 
+use League\MimeTypeDetection\FinfoMimeTypeDetector;
+
 class FileNameFixer
 {
-    public static function removeWhiteSpaceBeforeFileExtension(string $fileName)
+    private FinfoMimeTypeDetector $mimeTypeDetector;
+    private string $projectDir;
+
+    public function __construct(FinfoMimeTypeDetector $mimeTypeDetector, string $projectDir)
+    {
+        $this->mimeTypeDetector = $mimeTypeDetector;
+        $this->projectDir = $projectDir;
+    }
+
+    public function removeWhiteSpaceBeforeFileExtension(string $fileName)
     {
         $pattern = "/\s+(\.[^.]+)$/";
         $replacement = '$1';
 
         return preg_replace($pattern, $replacement, $fileName);
+    }
+
+    /**
+     * @param string $relativeFilePath
+     * @return string
+     */
+    public function addMissingFileExtension(string $relativeFilePath): string
+    {
+        $mimeType = $this->mimeTypeDetector->detectMimeType($relativeFilePath);
+        return 'good-jpg.jpg';
     }
 }
--- client/tests/phpunit/Service/File/FileNameFixerTest.php
+++ client/tests/phpunit/Service/File/FileNameFixerTest.php
@@ -2,17 +2,26 @@
 
 namespace AppBundle\Service\File;
 
-use PHPUnit\Framework\TestCase;
+use League\MimeTypeDetection\FinfoMimeTypeDetector;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
-class FileNameFixerTest extends TestCase
+class FileNameFixerTest extends KernelTestCase
 {
+    private string $projectDir;
+
+    public function setUp(): void
+    {
+        $this->projectDir = (self::bootKernel())->getProjectDir();
+    }
+
     /**
      * @test
      * @dataProvider fileNameProvider
      */
     public function removeWhiteSpaceBeforeFileExtension(string $originalFileName, string $expectedFileName)
     {
-        $fixedFilename = FileNameFixer::removeWhiteSpaceBeforeFileExtension($originalFileName);
+        $sut = new FileNameFixer(new FinfoMimeTypeDetector());
+        $fixedFilename = $sut->removeWhiteSpaceBeforeFileExtension($originalFileName);
         self::assertEquals($expectedFileName, $fixedFilename);
     }
 
@@ -23,6 +32,27 @@ class FileNameFixerTest extends TestCase
             'single whitespace character is removed' => ['dd_fileuploadertest .pdf', 'dd_fileuploadertest.pdf'],
             'multiple whitespace characters are removed' => ['dd_fileuploadertest     .pdf', 'dd_fileuploadertest.pdf'],
             'file with full stops' => ['dd_fileuploadertest 1.2.3     .pdf', 'dd_fileuploadertest 1.2.3.pdf'],
+        ];
+    }
+
+    /**
+     * @dataProvider missingExtensionFilesProvider
+     * @test
+     */
+    public function addMissingFileExtension(string $relativeFilePath, string $expectedFilename)
+    {
+        $sut = new FileNameFixer(new FinfoMimeTypeDetector(), $this->projectDir);
+        $alteredFileName = $sut->addMissingFileExtension($relativeFilePath);
+
+        self::assertEquals($expectedFilename, $alteredFileName);
+    }
+
+    public function missingExtensionFilesProvider()
+    {
+        return [
+            'jpg' => ['/tests/TestData/good-jpg', 'good-jpg.jpg'],
+            'pdf' => ['/tests/TestData/good-pdf', 'good-pdf.pdf'],
+            'png' => ['/tests/TestData/good-png', 'good-png.png'],
         ];
     }
 }
