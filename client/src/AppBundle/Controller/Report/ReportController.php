5a2b72beef8f5644532830a1eced980ea47e7010
Alex Saunders <acsauk@gmail.com>
2020-12-18 10:27:58 +0000

    Check for laydeputy rather than deputy

--- client/src/AppBundle/Controller/Report/ReportController.php
+++ client/src/AppBundle/Controller/Report/ReportController.php
@@ -444,7 +444,7 @@ class ReportController extends AbstractController
         return [
             'report' => $report,
             'form' => $form->createView(),
-            'homePathName' => $this->getUser()->isDeputy() ? 'lay_home' : "org_dashboard"
+            'homePathName' => $this->getUser()->isLayDeputy() ? 'lay_home' : "org_dashboard"
         ];
     }
 
