461af47a87cfd182d76a1a5a337e958be3384c88
Alex Saunders <acsauk@gmail.com>
2021-01-06 11:40:26 +0000

    Drop Dotenv env var loading in index.php and make app env logic clearer

--- client/public/index.php
+++ client/public/index.php
@@ -1,16 +1,10 @@
 <?php declare(strict_types=1);
 
 use Symfony\Component\Debug\Debug;
-use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 require dirname(__DIR__) . '/app/AppKernel.php';
-
-// We don't want to load the .env files if we are running on a server - ENVIRONMENT is only injected via terraform
-if (empty($_SERVER['ENVIRONMENT'])) {
-    (new Dotenv())->load(dirname(__DIR__).'/frontend.env', dirname(__DIR__).'/admin.env');
-}
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
@@ -34,7 +28,13 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false
 }
 
 $_SERVER += $_ENV;
-$_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null) ?: 'dev';
+
+if (!empty($_ENV['APP_ENV'])) {
+    $_SERVER['APP_ENV'] = $_ENV['APP_ENV'];
+} else {
+    $_SERVER['APP_ENV'] = $_SERVER['APP_ENV'] ?? 'dev';
+}
+
 $_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? false;
 
 $kernel = new AppKernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
