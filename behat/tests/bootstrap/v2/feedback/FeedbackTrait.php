b4487eb02a8aed6a9122b41d9ea6d23df4e036a5
Alex Saunders <acsauk@gmail.com>
2021-02-23 10:09:15 +0000

    Add feedback specs

---
+++ behat/tests/bootstrap/v2/feedback/FeedbackTrait.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+
+namespace DigidepsBehat\v2\Feedback;
+
+trait FeedbackTrait
+{
+    /**
+     * @Given a Lay Deputy completes and submits a report
+     */
+    public function aLayDeputyCompletesAndSubmitsAReport()
+    {
+    }
+}
