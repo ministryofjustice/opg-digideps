869b567ad465914b2c1a0d8749d94da210d62910
james.warren <james.warren@digital.justice.gov.uk>
2021-03-12 09:56:48 +0000

    DDPB-3920 fix missed vpc endpoint rules

--- environment/restore_from_production.tf
+++ environment/restore_from_production.tf
@@ -15,9 +15,12 @@ module "restore_from_production" {
 
 locals {
   restore_from_production_sg_rules = {
-    ecr  = local.common_sg_rules.ecr
-    logs = local.common_sg_rules.logs
-    s3   = local.common_sg_rules.s3
+    ecr     = local.common_sg_rules.ecr
+    logs    = local.common_sg_rules.logs
+    s3      = local.common_sg_rules.s3
+    ssm     = local.common_sg_rules.ssm
+    ecr_api = local.common_sg_rules.ecr_api
+    secrets = local.common_sg_rules.secrets
     rds = {
       port        = 5432
       protocol    = "tcp"
