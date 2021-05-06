47615fdca37e739e1cf95e2d681bbaae03547ff0
Stephen Hinett <stephen.hinett@digital.justice.gov.uk>
2021-05-06 14:08:15 +0100

    [DDPB-3817] Fixed migration version number

--- api/src/Migrations/Version253.php
+++ api/src/Migrations/Version253.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version252 extends AbstractMigration
+final class Version253 extends AbstractMigration
 {
     public function getDescription() : string
     {
