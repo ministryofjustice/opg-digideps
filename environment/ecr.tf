76e09d32769ef6fb3a5f337a508a2d464623bcc1
james.warren <james.warren@digital.justice.gov.uk>
2021-05-11 16:24:41 +0100

    DDPB-3982 fix bug with terraform apply

--- environment/ecr.tf
+++ environment/ecr.tf
@@ -3,19 +3,17 @@ locals {
   client_tag       = local.environment == "development" ? "development-${local.docker_tag_elems[1]}" : var.OPG_DOCKER_TAG
 
   images = {
-    api          = "${data.aws_ecr_repository.images["api"].repository_url}:${var.OPG_DOCKER_TAG}"
-    client       = "${data.aws_ecr_repository.images["client"].repository_url}:${local.client_tag}"
-    file_scanner = "${data.aws_ecr_repository.images["file-scanner"].repository_url}:20201113"
-    sync         = "${data.aws_ecr_repository.images["sync"].repository_url}:${var.OPG_DOCKER_TAG}"
-    wkhtmltopdf  = "${data.aws_ecr_repository.images["wkhtmltopdf"].repository_url}:${var.OPG_DOCKER_TAG}"
-    drbackup     = "${data.aws_ecr_repository.images["dr-backup"].repository_url}:${var.OPG_DOCKER_TAG}"
+    api         = "${data.aws_ecr_repository.images["api"].repository_url}:${var.OPG_DOCKER_TAG}"
+    client      = "${data.aws_ecr_repository.images["client"].repository_url}:${local.client_tag}"
+    sync        = "${data.aws_ecr_repository.images["sync"].repository_url}:${var.OPG_DOCKER_TAG}"
+    wkhtmltopdf = "${data.aws_ecr_repository.images["wkhtmltopdf"].repository_url}:${var.OPG_DOCKER_TAG}"
+    drbackup    = "${data.aws_ecr_repository.images["dr-backup"].repository_url}:${var.OPG_DOCKER_TAG}"
   }
 
   repositories = [
     "api",
     "client",
     "dr-backup",
-    "file-scanner",
     "sync",
     "wkhtmltopdf"
   ]
