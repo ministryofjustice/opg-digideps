f01bec3812c160b870eb3620f5342dc35148d048
Alex Saunders <acsauk@gmail.com>
2021-05-24 17:42:45 +0100

    Comments styling

--- api/tests/Behat/bootstrap/v2/Common/FormFillingTrait.php
+++ api/tests/Behat/bootstrap/v2/Common/FormFillingTrait.php
@@ -9,18 +9,14 @@ trait FormFillingTrait
     public array $submittedAnswersByFormSections = [];
 
     /**
-     * @param string $field
-     *                      field id|name|label|value
-     * @param $value
-     * field value to enter
-     * @param string|null $formSectionName
-     *                                     define with any name you like - only include if you want to assert on the value entered
-     *                                     on a summary page at the end of the form flow
-     * @param int         $answerGroup
-     *                                     set to a value greater than 0 if form answers are grouped together to enable removing all
-     *                                     associated answers when required
+     * @param string      $field           field id|name|label|value
+     * @param string      $value           field value to enter
+     * @param string|null $formSectionName define with any name you like - only include if you want to assert on the
+     *                                     value entered on a summary page at the end of the form flow
+     * @param int         $answerGroup     set to a value greater than 0 if form answers are grouped together to enable
+     *                                     removing all associated answers when required
      */
-    public function fillInField(string $field, $value, ?string $formSectionName = null, int $answerGroup = 0)
+    public function fillInField(string $field, string $value, ?string $formSectionName = null, int $answerGroup = 0)
     {
         if ($formSectionName) {
             $this->submittedAnswersByFormSections[$formSectionName][$answerGroup][$field] = $value;
@@ -30,16 +26,12 @@ trait FormFillingTrait
     }
 
     /**
-     * @param string      $select
-     *                                     select id|name|label|value
-     * @param string      $option
-     *                                     option value to choose
-     * @param string|null $formSectionName
-     *                                     define with any name you like - only include if you want to assert on the value entered
-     *                                     on a summary page at the end of the form flow
-     * @param int         $answerGroup
-     *                                     set to a value greater than 0 if form answers are grouped together to enable removing all
-     *                                     associated answers when required
+     * @param string      $select          select id|name|label|value
+     * @param string      $option          option value to choose
+     * @param string|null $formSectionName define with any name you like - only include if you want to assert on
+     *                                     the value entered on a summary page at the end of the form flow
+     * @param int         $answerGroup     set to a value greater than 0 if form answers are grouped together to enable
+     *                                     removing all associated answers when required
      */
     public function chooseOption(string $select, string $option, ?string $formSectionName = null, int $answerGroup = 0)
     {
@@ -59,13 +51,13 @@ trait FormFillingTrait
     }
 
     /**
-     * @param int    $answerGroupNumber
-     *                                           The number used to associate field/option answers when filling the form in
-     *                                           (see fillInField() and chooseOption())
-     * @param string $fieldInAnswerGroupToRemove
-     *                                           The field/option id|name|label|value as set in fillInField() or chooseOption()
-     * @param string $formSectionName
-     *                                           The name given to the section of the form being completed as set in fillInField() or chooseOption()
+     * @param int    $answerGroupNumber          The number used to associate field/option answers when filling the
+     *                                           form in (see fillInField() and chooseOption())
+     * @param string $fieldInAnswerGroupToRemove The field/option id|name|label|value as set in fillInField() or
+     *                                           chooseOption()
+     * @param string $formSectionName            The name given to the section of the form being completed as set in
+     *                                           fillInField() or chooseOption()
+     * @param string $removeButtonText           Text value of the remove button on confirmation page
      *
      * @throws \Behat\Mink\Exception\ElementNotFoundException
      */
