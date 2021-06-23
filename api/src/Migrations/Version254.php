e1b9141a7d4d6f9992862c72918bddd6bc4df75d
Alex Saunders <acsauk@gmail.com>
2021-06-23 20:15:30 +0100

    Fix migration name

--- api/src/Migrations/Version254.php
+++ api/src/Migrations/Version254.php
@@ -7,7 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20210623191158 extends AbstractMigration
+final class Version254 extends AbstractMigration
 {
     public function getDescription(): string
     {
