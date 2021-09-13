2f08dfe86db6d7239f7943319b99898f178242a4
Stephen Hinett <stephen.hinett@digital.justice.gov.uk>
2021-09-13 15:54:41 +0100

    [DDPB-4060] fixed formatting bug on the NDR report

--- client/src/Twig/FormFieldsExtension.php
+++ client/src/Twig/FormFieldsExtension.php
@@ -447,6 +447,7 @@ class FormFieldsExtension extends AbstractExtension
             // label is translated directly here
             if ('' != $translationKey) {
                 $labelText = $this->translator->trans($translationKey.'.label', $labelParams, $domain);
+                $hintText = $this->translator->trans($translationKey.'.hint', $labelParams, $domain);
             } else {
                 $labelText = '';
             }
