0bd06dc2bfc698d7a98b8c12526ad523f7e12e7f
Alex Saunders <acsauk@gmail.com>
2021-09-21 17:04:10 +0100

    Fix migration class names

--- api/src/Migrations/Version255.php
+++ api/src/Migrations/Version255.php
@@ -7,7 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version254 extends AbstractMigration
+final class Version255 extends AbstractMigration
 {
     public function getDescription(): string
     {
--- api/src/Migrations/Version256.php
+++ api/src/Migrations/Version256.php
@@ -7,7 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version255 extends AbstractMigration
+final class Version256 extends AbstractMigration
 {
     public function getDescription(): string
     {
