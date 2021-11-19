89018aab734e8ad9c8929d8c52c59bdfa799c853
Alex Saunders <acsauk@gmail.com>
2021-11-19 12:18:48 +0000

    DDPB-4171: Fix redis and add service health integration tests (#726)
    * Add openapispec for Notify public endpoints we use
    
    * Add mock notify docker container and point to it in local env only
    
    * Inject Redis in availability check
    
    * Make fake notify key contain a valid UUID v4

---
+++ api/tests/Behat/features-v2/service-health/external-dependecy-checks.feature
@@ -0,0 +1,12 @@
+@v2 @service-health @v2_admin
+Feature: Overview of external dependencies the application requires to function
+
+    @infra @smoke
+    Scenario: check maintenance page checks
+        When I go to "/manage/availability"
+        Then I should see "Sirius: OK"
+        And I should see "Api: OK"
+        And I should see "Redis: OK"
+        And I should see "Notify: OK"
+        And I should see "ClamAV: OK"
+        And I should see "htmlToPdf: OK"
--- client/src/Service/Availability/RedisAvailability.php
+++ client/src/Service/Availability/RedisAvailability.php
@@ -2,6 +2,7 @@
 
 namespace App\Service\Availability;
 
+use Predis\ClientInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class RedisAvailability extends ServiceAvailabilityAbstract
@@ -9,21 +10,21 @@ class RedisAvailability extends ServiceAvailabilityAbstract
     const TEST_KEY = 'RedisAvailabilityTestKey';
 
     private ContainerInterface $container;
+    private ClientInterface $redis;
 
-    public function __construct(ContainerInterface $container)
+    public function __construct(ContainerInterface $container, ClientInterface $redis)
     {
         $this->isHealthy = false;
         $this->container = $container;
+        $this->redis = $redis;
     }
 
     public function ping()
     {
         try {
-            // get the redis service, save and read a key
-            $redis = $this->container->get('snc_redis.default');
-            $redis->set(self::TEST_KEY, 'valueSaved');
+            $this->redis->set(self::TEST_KEY, 'valueSaved');
 
-            if ('valueSaved' == $redis->get(self::TEST_KEY)) {
+            if ('valueSaved' == $this->redis->get(self::TEST_KEY)) {
                 $this->isHealthy = true;
             }
         } catch (\Throwable $e) {
