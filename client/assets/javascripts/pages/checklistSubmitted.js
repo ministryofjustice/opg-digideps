f9c1a2a612db0c1c0c61bd9d04516023d233e28c
Alex Saunders <acsauk@gmail.com>
2021-12-08 15:01:32 +0000

    Move checklistSubmitted inline js to page specific js

--- client/assets/javascripts/pages/checklistSubmitted.js
+++ client/assets/javascripts/pages/checklistSubmitted.js
@@ -2,5 +2,5 @@ import { Redirector } from '../modules/redirector'
 
 window.addEventListener('DOMContentLoaded', (event) => {
   const url = document.querySelector('[data-redirect-url]').dataset.redirectUrl
-  Redirector(url, document)
+  Redirector(url)
 })
