2e777364d0af06e0b0ba9c0648a245c9e50eae22
Tom Gulliver <tgulliver97@gmail.com>
2021-12-15 13:48:34 +0000

    DPPB-4123 - add alb associations

---
+++ environment/waf.tf
@@ -0,0 +1,16 @@
+data "aws_wafv2_web_acl" "main" {
+  name  = "${local.environment.account_name}-web-acl"
+  scope = "REGIONAL"
+}
+
+resource "aws_wafv2_web_acl_association" "admin" {
+  count        = local.environment.associate_alb_with_waf_web_acl_enabled ? 1 : 0
+  resource_arn = aws_lb.front.arn
+  web_acl_arn  = data.aws_wafv2_web_acl.main.arn
+}
+
+resource "aws_wafv2_web_acl_association" "front" {
+  count        = local.environment.associate_alb_with_waf_web_acl_enabled ? 1 : 0
+  resource_arn = aws_lb.front.arn
+  web_acl_arn  = data.aws_wafv2_web_acl.main.arn
+}
