6aeaf8b126800de3fe8821ca3a3431d7ef5cf0a8
Tom Gulliver <tgulliver97@gmail.com>
2021-12-15 14:15:10 +0000

    DDPB-4123 - fix variable references

--- environment/waf.tf
+++ environment/waf.tf
@@ -1,16 +1,16 @@
 data "aws_wafv2_web_acl" "main" {
-  name  = "${local.environment.account_name}-web-acl"
+  name  = "${local.environment}-web-acl"
   scope = "REGIONAL"
 }
 
 resource "aws_wafv2_web_acl_association" "admin" {
-  count        = local.environment.associate_alb_with_waf_web_acl_enabled ? 1 : 0
+  count        = local.account.associate_alb_with_waf_web_acl_enabled ? 1 : 0
   resource_arn = aws_lb.front.arn
   web_acl_arn  = data.aws_wafv2_web_acl.main.arn
 }
 
 resource "aws_wafv2_web_acl_association" "front" {
-  count        = local.environment.associate_alb_with_waf_web_acl_enabled ? 1 : 0
+  count        = local.account.associate_alb_with_waf_web_acl_enabled ? 1 : 0
   resource_arn = aws_lb.front.arn
   web_acl_arn  = data.aws_wafv2_web_acl.main.arn
 }
