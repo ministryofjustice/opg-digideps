67e28c31ed26fce15cdd981b3822997a8db43143
Alex Saunders <acsauk@gmail.com>
2021-12-22 13:43:48 +0000

    Add page specific js for client benefits check form

--- client/assets/javascripts/pages/clientBenefitsCheckForm.js
+++ client/assets/javascripts/pages/clientBenefitsCheckForm.js
@@ -2,7 +2,9 @@ import ConditionalFieldRevealer from '../modules/ConditionalFieldRevealer'
 
 window.addEventListener('DOMContentLoaded', (event) => {
   // We need to wait for gov.uk DS to apply its JS/CSS after DOM has loaded
-  setTimeout(() => {
-    ConditionalFieldRevealer.init()
-  }, 200)
+  window.setTimeout(timeOutCallback, 200)
 })
+
+const timeOutCallback = () => {
+  ConditionalFieldRevealer.init()
+}
