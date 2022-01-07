8821ccd00cf0ff32759164345bd4432443fb818b
Alex Saunders <acsauk@gmail.com>
2022-01-07 21:48:14 +0000

    Start to refactor use of deprecated at() in ChecklistSyncCommand tests

--- client/tests/phpunit/Command/ChecklistSyncCommandTest.php
+++ client/tests/phpunit/Command/ChecklistSyncCommandTest.php
@@ -1,27 +1,31 @@
 <?php
+
 namespace App\Tests\Command;
 
 use App\Command\ChecklistSyncCommand;
 use App\Entity\Client;
 use App\Entity\Report\Checklist;
 use App\Entity\Report\Report;
+use App\Entity\User;
 use App\Exception\PdfGenerationFailedException;
 use App\Exception\SiriusDocumentSyncFailedException;
-use App\Entity\User;
 use App\Model\Sirius\QueuedChecklistData;
 use App\Service\ChecklistPdfGenerator;
 use App\Service\ChecklistSyncService;
 use App\Service\Client\RestClient;
 use App\Service\ParameterStoreService;
+use DateTime;
 use PHPUnit\Framework\MockObject\MockObject;
+use Prophecy\PhpUnit\ProphecyTrait;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Tester\CommandTester;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Throwable;
 
 class ChecklistSyncCommandTest extends KernelTestCase
 {
+    use ProphecyTrait;
+
     /** @var MockObject */
     private $syncService;
     private $parameterStore;
@@ -30,7 +34,6 @@ class ChecklistSyncCommandTest extends KernelTestCase
 
     /** @var CommandTester */
     private $commandTester;
-
 
     public function setUp(): void
     {
@@ -59,17 +62,121 @@ class ChecklistSyncCommandTest extends KernelTestCase
             ->invokeTest();
     }
 
+    private function invokeTest(): void
+    {
+        $this->commandTester->execute([]);
+    }
+
+    private function assertSyncServiceIsNotInvoked(): ChecklistSyncCommandTest
+    {
+        $this->syncService
+            ->expects($this->never())
+            ->method('sync');
+
+        return $this;
+    }
+
+    private function ensureFeatureIsDisabled(): ChecklistSyncCommandTest
+    {
+        $this->parameterStore
+            ->method('getFeatureFlag')
+            ->willReturn('0');
+
+        return $this;
+    }
+
     /**
      * @test
      */
     public function updatesSyncStatusOnFailedPdfGenerations()
     {
+        $apiCallArguments = [
+            [
+                'get',
+                'report/all-with-queued-checklists',
+                ['row_limit' => '30'],
+                'Report\Report[]',
+                [],
+                false,
+            ],
+            [
+                'put',
+                'checklist/3923',
+                json_encode([
+                    'syncStatus' => Checklist::SYNC_STATUS_PERMANENT_ERROR,
+                    'syncError' => 'Failed to generate PDF',
+                ]),
+                'raw',
+                [],
+                false,
+            ],
+        ];
+
+        $returnValues = [
+            [],
+            [
+                $this->buildReport(),
+                $this->buildReport(),
+            ],
+        ];
+
         $this
             ->ensureFeatureIsEnabled()
-            ->ensureRestClientReturnsRows()
+            ->assertApiCallsAreMade($apiCallArguments, $returnValues)
             ->ensurePdfGenerationWillFailWith(new PdfGenerationFailedException('Failed to generate PDF'))
-            ->assertChecklistStatusWillBeUpdatedWithError('Failed to generate PDF')
             ->invokeTest();
+    }
+
+    private function buildReport()
+    {
+        $user = (new User())->setEmail('test@test.com');
+
+        $report = (new Report())
+            ->setStartDate(new DateTime())
+            ->setEndDate(new DateTime())
+            ->setReportSubmissions([])
+            ->setType(Report::TYPE_PROPERTY_AND_AFFAIRS_HIGH_ASSETS);
+
+        $checklist = (new Checklist($report))->setSubmittedBy($user);
+        $checklist->setId(3923);
+
+        $report->setChecklist($checklist);
+
+        $client = new Client();
+        $client->setCaseNumber('case-number');
+
+        $report->setClient($client);
+
+        return $report;
+    }
+
+    private function ensurePdfGenerationWillFailWith(Throwable $e): ChecklistSyncCommandTest
+    {
+        $this->pdfGenerator
+            ->method('generate')
+            ->willThrowException($e);
+
+        return $this;
+    }
+
+    private function assertApiCallsAreMade(array $arguments, array $returnValues): ChecklistSyncCommandTest
+    {
+        $this->restClient
+            ->expects($this->once())
+            ->method('apiCall')
+            ->withConsecutive(...$arguments)
+            ->willReturnOnConsecutiveCalls(...$returnValues);
+
+        return $this;
+    }
+
+    private function ensureFeatureIsEnabled(): ChecklistSyncCommandTest
+    {
+        $this->parameterStore
+            ->method('getFeatureFlag')
+            ->willReturn('1');
+
+        return $this;
     }
 
     /**
@@ -85,6 +192,21 @@ class ChecklistSyncCommandTest extends KernelTestCase
             ->invokeTest();
     }
 
+    private function ensureRestClientReturnsRows(): ChecklistSyncCommandTest
+    {
+        $this->restClient
+            ->expects($this->once())
+            ->method('apiCall')
+            ->withConsecutive([])
+            ->with('get', 'report/all-with-queued-checklists', ['row_limit' => '30'], 'Report\Report[]', [], false)
+            ->willReturnOnConsecutiveCalls([
+                $this->buildReport(),
+                $this->buildReport(),
+            ]);
+
+        return $this;
+    }
+
     /**
      * @test
      */
@@ -96,6 +218,29 @@ class ChecklistSyncCommandTest extends KernelTestCase
             ->ensurePdfGenerationWillSucceed()
             ->assertEachRowWillBeTransformedAndSentToSyncService()
             ->invokeTest();
+    }
+
+    private function assertEachRowWillBeTransformedAndSentToSyncService(): ChecklistSyncCommandTest
+    {
+        $this->syncService
+            ->expects($this->exactly(2))
+            ->method('sync')
+            ->withConsecutive(
+                [$this->isInstanceOf(QueuedChecklistData::class)],
+                [$this->isInstanceOf(QueuedChecklistData::class)]
+            )
+            ->willReturnOnConsecutiveCalls('uuid-1', 'uuid-2');
+
+        return $this;
+    }
+
+    private function ensurePdfGenerationWillSucceed(): ChecklistSyncCommandTest
+    {
+        $this->pdfGenerator
+            ->method('generate')
+            ->willReturn('file-contents');
+
+        return $this;
     }
 
     /**
@@ -111,6 +256,30 @@ class ChecklistSyncCommandTest extends KernelTestCase
             ->invokeTest();
     }
 
+    private function assertChecklistsAreFetchedWithLimitOf(string $limit)
+    {
+        $this->restClient
+            ->expects($this->at(0))
+            ->method('apiCall')
+            ->with('get', 'report/all-with-queued-checklists', ['row_limit' => $limit], 'Report\Report[]', [], false)
+            ->willReturn([
+                $this->buildReport(),
+                $this->buildReport(),
+            ]);
+
+        return $this;
+    }
+
+    private function ensureConfigurableRowLimitIsSetTo(string $limit): ChecklistSyncCommandTest
+    {
+        $this->parameterStore
+            ->method('getParameter')
+            ->with(ParameterStoreService::PARAMETER_CHECKLIST_SYNC_ROW_LIMIT)
+            ->willReturn($limit);
+
+        return $this;
+    }
+
     /**
      * @test
      */
@@ -122,6 +291,30 @@ class ChecklistSyncCommandTest extends KernelTestCase
             ->ensurePdfGenerationWillSucceed()
             ->assertChecklistsAreFetchedWithDefaultLimit()
             ->invokeTest();
+    }
+
+    private function assertChecklistsAreFetchedWithDefaultLimit()
+    {
+        $this->restClient
+            ->expects($this->at(0))
+            ->method('apiCall')
+            ->with('get', 'report/all-with-queued-checklists', ['row_limit' => ChecklistSyncCommand::FALLBACK_ROW_LIMITS], 'Report\Report[]', [], false)
+            ->willReturn([
+                $this->buildReport(),
+                $this->buildReport(),
+            ]);
+
+        return $this;
+    }
+
+    private function ensureConfigurableRowLimitIsNotSet(): ChecklistSyncCommandTest
+    {
+        $this->parameterStore
+            ->method('getParameter')
+            ->with(ParameterStoreService::PARAMETER_CHECKLIST_SYNC_ROW_LIMIT)
+            ->willReturn(null);
+
+        return $this;
     }
 
     /**
@@ -136,6 +329,25 @@ class ChecklistSyncCommandTest extends KernelTestCase
             ->assertChecklistsAreFetchedWithDefaultLimit()
             ->assertChecklistStatusWillBeUpdatedWithSuccess()
             ->invokeTest();
+    }
+
+    private function assertChecklistStatusWillBeUpdatedWithSuccess(): ChecklistSyncCommandTest
+    {
+        $this->restClient
+            ->expects($this->at(1))
+            ->method('apiCall')
+            ->with(
+                'put',
+                'checklist/3923',
+                json_encode([
+                    'syncStatus' => Checklist::SYNC_STATUS_SUCCESS,
+                ]),
+                'raw',
+                [],
+                false
+            );
+
+        return $this;
     }
 
     /**
@@ -153,119 +365,7 @@ class ChecklistSyncCommandTest extends KernelTestCase
             ->invokeTest();
     }
 
-    private function ensureFeatureIsEnabled(): ChecklistSyncCommandTest
-    {
-        $this->parameterStore
-            ->method('getFeatureFlag')
-            ->willReturn('1');
-
-        return $this;
-    }
-
-    private function ensureFeatureIsDisabled(): ChecklistSyncCommandTest
-    {
-        $this->parameterStore
-            ->method('getFeatureFlag')
-            ->willReturn('0');
-
-        return $this;
-    }
-
-    private function ensurePdfGenerationWillFailWith(\Throwable $e): ChecklistSyncCommandTest
-    {
-        $this->pdfGenerator
-            ->method('generate')
-            ->willThrowException($e);
-
-        return $this;
-    }
-
-    private function ensurePdfGenerationWillSucceed(): ChecklistSyncCommandTest
-    {
-        $this->pdfGenerator
-            ->method('generate')
-            ->willReturn('file-contents');
-
-        return $this;
-    }
-
-    private function ensureConfigurableRowLimitIsSetTo(string $limit): ChecklistSyncCommandTest
-    {
-        $this->parameterStore
-            ->method('getParameter')
-            ->with(ParameterStoreService::PARAMETER_CHECKLIST_SYNC_ROW_LIMIT)
-            ->willReturn($limit);
-
-        return $this;
-    }
-
-    private function ensureConfigurableRowLimitIsNotSet(): ChecklistSyncCommandTest
-    {
-        $this->parameterStore
-            ->method('getParameter')
-            ->with(ParameterStoreService::PARAMETER_CHECKLIST_SYNC_ROW_LIMIT)
-            ->willReturn(null);
-
-        return $this;
-    }
-
-    private function ensureRestClientReturnsRows(): ChecklistSyncCommandTest
-    {
-        $this->restClient
-            ->expects($this->at(0))
-            ->method('apiCall')
-            ->with('get', 'report/all-with-queued-checklists', ['row_limit' => '30'], 'Report\Report[]', [], false)
-            ->willReturn([
-                $this->buildReport(),
-                $this->buildReport()
-            ]);
-
-        return $this;
-    }
-
-    private function assertChecklistsAreFetchedWithLimitOf(string $limit)
-    {
-        $this->restClient
-            ->expects($this->at(0))
-            ->method('apiCall')
-            ->with('get', 'report/all-with-queued-checklists', ['row_limit' => $limit], 'Report\Report[]', [], false)
-            ->willReturn([
-                $this->buildReport(),
-                $this->buildReport()
-            ]);
-
-        return $this;
-    }
-
-    private function assertChecklistsAreFetchedWithDefaultLimit()
-    {
-        $this->restClient
-            ->expects($this->at(0))
-            ->method('apiCall')
-            ->with('get', 'report/all-with-queued-checklists', ['row_limit' => ChecklistSyncCommand::FALLBACK_ROW_LIMITS], 'Report\Report[]', [], false)
-            ->willReturn([
-                $this->buildReport(),
-                $this->buildReport()
-            ]);
-
-        return $this;
-    }
-
-    private function assertEachRowWillBeTransformedAndSentToSyncService(): ChecklistSyncCommandTest
-    {
-        $this->syncService
-            ->expects($this->exactly(2))
-            ->method('sync')
-            ->withConsecutive(
-                [$this->isInstanceOf(QueuedChecklistData::class)],
-                [$this->isInstanceOf(QueuedChecklistData::class)]
-            )
-            ->willReturnOnConsecutiveCalls('uuid-1', 'uuid-2');
-
-        return $this;
-    }
-
-    private function ensureSyncWillFailWith(\Throwable $e): ChecklistSyncCommandTest
+    private function ensureSyncWillFailWith(Throwable $e): ChecklistSyncCommandTest
     {
         $this->syncService
             ->expects($this->exactly(2))
@@ -277,80 +377,5 @@ class ChecklistSyncCommandTest extends KernelTestCase
             ->willThrowException($e);
 
         return $this;
-    }
-
-    private function assertChecklistStatusWillBeUpdatedWithError($error): ChecklistSyncCommandTest
-    {
-        $this->restClient
-            ->expects($this->at(1))
-            ->method('apiCall')
-            ->with(
-                'put',
-                'checklist/3923',
-                json_encode([
-                    'syncStatus' => Checklist::SYNC_STATUS_PERMANENT_ERROR,
-                    'syncError' => $error
-                ]),
-                'raw',
-                [],
-                false
-            );
-
-        return $this;
-    }
-
-    private function assertChecklistStatusWillBeUpdatedWithSuccess(): ChecklistSyncCommandTest
-    {
-        $this->restClient
-            ->expects($this->at(1))
-            ->method('apiCall')
-            ->with(
-                'put',
-                'checklist/3923',
-                json_encode([
-                    'syncStatus' => Checklist::SYNC_STATUS_SUCCESS
-                ]),
-                'raw',
-                [],
-                false
-            );
-
-        return $this;
-    }
-
-    private function assertSyncServiceIsNotInvoked(): ChecklistSyncCommandTest
-    {
-        $this->syncService
-            ->expects($this->never())
-            ->method('sync');
-
-        return $this;
-    }
-
-    private function buildReport()
-    {
-        $user = (new User())->setEmail('test@test.com');
-
-        $report = (new Report())
-            ->setStartDate(new \DateTime())
-            ->setEndDate(new \DateTime())
-            ->setReportSubmissions([])
-            ->setType(Report::TYPE_PROPERTY_AND_AFFAIRS_HIGH_ASSETS);
-
-        $checklist = (new Checklist($report))->setSubmittedBy($user);
-        $checklist->setId(3923);
-
-        $report->setChecklist($checklist);
-
-        $client = new Client();
-        $client->setCaseNumber('case-number');
-
-        $report->setClient($client);
-        return $report;
-    }
-
-    private function invokeTest(): void
-    {
-        $this->commandTester->execute([]);
     }
 }
