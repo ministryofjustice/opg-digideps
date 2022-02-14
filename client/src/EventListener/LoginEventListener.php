8091d235c79f80007cfbac359f6613e4e1a78c45
Alex Saunders <acsauk@gmail.com>
2022-02-14 15:44:15 +0000

    Revert LoginEventListener

--- client/src/EventListener/LoginEventListener.php
+++ client/src/EventListener/LoginEventListener.php
@@ -3,14 +3,11 @@
 namespace App\EventListener;
 
 //use Symfony\Component\EventDispatcher\EventDispatcher;
-use App\Service\Client\TokenStorage\RedisStorage;
 use App\Service\Redirector;
-use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 /**
  * Login listener.
@@ -25,25 +22,14 @@ class LoginEventListener
      * @var Redirector
      */
     protected $redirector;
-    private HttpClientInterface $phpApiClient;
-    private RedisStorage $redisStorage;
-    private LoggerInterface $logger;
 
     /**
      * @param EventDispatcher $dispatcher
      */
-    public function __construct(
-        EventDispatcherInterface $dispatcher,
-        Redirector $Redirector,
-        HttpClientInterface $phpApiClient,
-        RedisStorage $redisStorage,
-        LoggerInterface $logger
-    ) {
+    public function __construct(EventDispatcherInterface $dispatcher, Redirector $Redirector)
+    {
         $this->dispatcher = $dispatcher;
         $this->redirector = $Redirector;
-        $this->phpApiClient = $phpApiClient;
-        $this->redisStorage = $redisStorage;
-        $this->logger = $logger;
     }
 
     public function onSecurityInteractiveLogin(InteractiveLoginEvent $event)
