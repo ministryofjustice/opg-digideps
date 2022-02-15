1a5f9ea53222aa3608571326c52152251c014d98
Gugandeep Chani <Gugandeep.Chani@digital.justice.gov.uk>
2022-02-15 13:01:43 +0000

    Using a valid http client

--- client/src/Service/Client/GovUK/BankHolidaysAPIClient.php
+++ client/src/Service/Client/GovUK/BankHolidaysAPIClient.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Service\Client\GovUK;
 
-use App\Entity\ClientInterface;
+use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Psr7\Request;
 use Psr\Log\LoggerInterface;
 
