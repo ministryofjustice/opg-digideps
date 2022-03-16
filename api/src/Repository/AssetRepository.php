5cc135f9974cd9dfe76029355f6f980d8a8c9c82
Alex Saunders <acsauk@gmail.com>
2022-03-16 09:38:02 +0000

    Apply correct calc to ReportRepository (#797)

--- api/src/Repository/AssetRepository.php
+++ api/src/Repository/AssetRepository.php
@@ -26,7 +26,7 @@ class AssetRepository extends ServiceEntityRepository
             throw new InvalidArgumentException('Only "AssetProperty" or "AssetOther" assets are supported');
         }
 
-        $selectQuery = AssetOther::class === $assetType ? 'SUM(a.value)' : 'SUM(a.value * a.ownedPercentage)';
+        $selectQuery = AssetOther::class === $assetType ? 'SUM(a.value)' : 'SUM(a.value * (a.ownedPercentage / 100))';
 
         $query = $this
             ->getEntityManager()
