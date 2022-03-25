28a3d0dd38f4a1eea423900fa39f3071080ce4b4
Alex Saunders <acsauk@gmail.com>
2022-03-25 14:24:46 +0000

    Fix JS formatting

--- client/assets/javascripts/main.js
+++ client/assets/javascripts/main.js
@@ -27,7 +27,7 @@ const uploadProgress = require('./modules/uploadProgress.js')
  * Taken from govuk-frontend. Supports back to IE8
  * See: https://github.com/alphagov/govuk-frontend/blob/063cd8e2470b62b824c6e50ca66342ac7a95d2d8/src/govuk/common.js#L6
  */
-export function nodeListForEach (nodes, callback) {
+export function nodeListForEach(nodes, callback) {
   if (window.NodeList.prototype.forEach) {
     return nodes.forEach(callback)
   }
@@ -78,7 +78,9 @@ $(document).ready(function () {
   tableMultiSelect()
 
   // Detached details/summary
-  const $detachedDetails = document.querySelectorAll('[data-module="opg-detached-details"]')
+  const $detachedDetails = document.querySelectorAll(
+    '[data-module="opg-detached-details"]'
+  )
   nodeListForEach($detachedDetails, function ($el) {
     new DetachedDetails($el).init()
   })
@@ -87,7 +89,9 @@ $(document).ready(function () {
   const showHideContent = new ShowHideContent()
   showHideContent.init()
 
-  const $togglers = document.querySelectorAll('[data-module="opg-button-toggler"]')
+  const $togglers = document.querySelectorAll(
+    '[data-module="opg-button-toggler"]'
+  )
 
   if ($togglers !== null) {
     nodeListForEach($togglers, function ($el) {
@@ -101,15 +105,23 @@ $(document).ready(function () {
     new MOJButtonMenu().init($menuButtons)
   }
 
-  const $submitButtons = document.querySelectorAll('[data-module="opg-toggleable-submit"]')
+  const $submitButtons = document.querySelectorAll(
+    '[data-module="opg-toggleable-submit"]'
+  )
 
   if ($submitButtons !== null) {
     $submitButtons.forEach(function ($el) {
       $el.addEventListener('click', function ($e) {
-        $e.target.classList.add('opg-submit-link--disabled', 'govuk-button--disabled')
+        $e.target.classList.add(
+          'opg-submit-link--disabled',
+          'govuk-button--disabled'
+        )
 
         setTimeout(function () {
-          $e.target.classList.remove('opg-submit-link--disabled', 'govuk-button--disabled')
+          $e.target.classList.remove(
+            'opg-submit-link--disabled',
+            'govuk-button--disabled'
+          )
         }, 3000)
       })
     })
@@ -143,5 +155,5 @@ $(document).ready(function () {
 GOVUKFrontend.initAll()
 
 // Polyfill elements with position:sticky
-var elements = document.querySelectorAll('.opg-sticky-menu')
+const elements = document.querySelectorAll('.opg-sticky-menu')
 Stickyfill.add(elements)
--- client/assets/javascripts/modules/uploadProgress.js
+++ client/assets/javascripts/modules/uploadProgress.js
@@ -2,38 +2,38 @@
 const uploadProgress = function (element) {
   // check if exists
   if ($(element).length === 1) {
-    const nOfChunks = $(element).attr("max") - 1;
-    const casrecDeleteUrl = $(element).data("path-casrec-ajax");
+    const nOfChunks = $(element).attr('max') - 1
+    const casrecDeleteUrl = $(element).data('path-casrec-ajax')
 
     $.ajax({
       url: casrecDeleteUrl,
-      dataType: "json",
+      dataType: 'json'
     }).done(function (data) {
-      $(element).val(1);
-      uploadChunk(0, nOfChunks, element);
-    });
+      $(element).val(1)
+      uploadChunk(0, nOfChunks, element)
+    })
   }
-};
+}
 
 var uploadChunk = function (currentChunk, nOfChunks, element) {
-  const casrecAddAjaxUrl = $(element).data("path-casrec-add-ajax");
-  const casrecUploadUrl = $(element).data("path-casrec-upload");
+  const casrecAddAjaxUrl = $(element).data('path-casrec-add-ajax')
+  const casrecUploadUrl = $(element).data('path-casrec-upload')
 
   if (currentChunk < nOfChunks) {
     $.ajax({
-      url: casrecAddAjaxUrl + "?chunk=" + currentChunk,
-      dataType: "json",
+      url: casrecAddAjaxUrl + '?chunk=' + currentChunk,
+      dataType: 'json'
     })
       .done(function (data) {
-        $(element).val(currentChunk + 1);
-        uploadChunk(currentChunk + 1, nOfChunks, element);
+        $(element).val(currentChunk + 1)
+        uploadChunk(currentChunk + 1, nOfChunks, element)
       })
       .error(function () {
-        window.alert("Upload error. please try uploading again");
-      });
+        window.alert('Upload error. please try uploading again')
+      })
   } else {
-    window.location.href = casrecUploadUrl;
+    window.location.href = casrecUploadUrl
   }
-};
+}
 
-module.exports = uploadProgress;
+module.exports = uploadProgress
