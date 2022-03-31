fa4079ec5b023c2f6bacea8c022cd977cae923ea
Alex Saunders <acsauk@gmail.com>
2022-03-31 13:15:51 +0100

    Fix fixtures column names, prefer rename column rather than drop

--- api/src/Migrations/Version262.php
+++ api/src/Migrations/Version262.php
@@ -11,7 +11,11 @@ final class Version262 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Rename casrec table to pre_registration and add new deputy address fields. Amend references of casrec to sirius in other table columns.';
+        return <<<DESC
+- Rename casrec table to pre_registration
+- Add new deputy address and deputy_uid fields
+- Amend references of casrec to sirius in checklist columns
+DESC;
     }
 
     public function up(Schema $schema): void
@@ -20,10 +24,8 @@ final class Version262 extends AbstractMigration
         $this->addSql('CREATE TABLE pre_registration (id SERIAL NOT NULL, client_case_number VARCHAR(20) NOT NULL, client_lastname VARCHAR(50) NOT NULL, deputy_uid VARCHAR(100) NOT NULL, deputy_lastname VARCHAR(100) DEFAULT NULL, deputy_address_1 VARCHAR(255) DEFAULT NULL, deputy_address_2 VARCHAR(255) DEFAULT NULL, deputy_address_3 VARCHAR(255) DEFAULT NULL, deputy_address_4 VARCHAR(255) DEFAULT NULL, deputy_address_5 VARCHAR(255) DEFAULT NULL, deputy_postcode VARCHAR(10) DEFAULT NULL, type_of_report VARCHAR(10) DEFAULT NULL, ndr BOOLEAN DEFAULT NULL, uploaded_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, order_date TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, order_type VARCHAR(255) DEFAULT NULL, is_co_deputy BOOLEAN DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX updated_at_index ON pre_registration (updated_at)');
         $this->addSql('DROP TABLE casrec');
-        $this->addSql('ALTER TABLE checklist ADD deputy_full_name_accurate_in_sirius VARCHAR(3) DEFAULT NULL');
-        $this->addSql('ALTER TABLE checklist ADD bond_order_match_sirius VARCHAR(3) DEFAULT NULL');
-        $this->addSql('ALTER TABLE checklist DROP deputy_full_name_accurate_in_casrec');
-        $this->addSql('ALTER TABLE checklist DROP bond_order_match_casrec');
+        $this->addSql('ALTER TABLE checklist RENAME deputy_full_name_accurate_in_casrec TO deputy_full_name_accurate_in_sirius');
+        $this->addSql('ALTER TABLE checklist RENAME bond_order_match_casrec TO bond_order_match_sirius');
     }
 
     public function down(Schema $schema): void
@@ -32,9 +34,7 @@ final class Version262 extends AbstractMigration
         $this->addSql('CREATE TABLE casrec (id SERIAL NOT NULL, client_case_number VARCHAR(20) NOT NULL, client_lastname VARCHAR(50) NOT NULL, deputy_uid VARCHAR(100) NOT NULL, deputy_lastname VARCHAR(100) DEFAULT NULL, deputy_postcode VARCHAR(10) DEFAULT NULL, type_of_report VARCHAR(10) DEFAULT NULL, uploaded_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, order_date TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, deputy_address_1 VARCHAR(255) DEFAULT NULL, deputy_address_2 VARCHAR(255) DEFAULT NULL, deputy_address_3 VARCHAR(255) DEFAULT NULL, deputy_address_4 VARCHAR(255) DEFAULT NULL, deputy_address_5 VARCHAR(255) DEFAULT NULL, ndr BOOLEAN DEFAULT NULL, order_type VARCHAR(255) DEFAULT NULL, is_co_deputy BOOLEAN DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX updated_at_index ON casrec (updated_at)');
         $this->addSql('DROP TABLE pre_registration');
-        $this->addSql('ALTER TABLE checklist ADD deputy_full_name_accurate_in_casrec VARCHAR(3) DEFAULT NULL');
-        $this->addSql('ALTER TABLE checklist ADD bond_order_match_casrec VARCHAR(3) DEFAULT NULL');
-        $this->addSql('ALTER TABLE checklist DROP deputy_full_name_accurate_in_sirius');
-        $this->addSql('ALTER TABLE checklist DROP bond_order_match_sirius');
+        $this->addSql('ALTER TABLE checklist RENAME deputy_full_name_accurate_in_sirius TO deputy_full_name_accurate_in_casrec');
+        $this->addSql('ALTER TABLE checklist RENAME bond_order_match_sirius TO bond_order_match_casrec');
     }
 }
