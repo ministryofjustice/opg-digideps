004343b206b1fb4eb182be4701df0d6d3e15d0e5
Gugandeep Chani <Gugandeep.Chani@digital.justice.gov.uk>
2022-04-08 09:49:35 +0100

    Reverting change to file to see if pipeline improves

--- api/tests/Behat/run-tests.sh
+++ api/tests/Behat/run-tests.sh
@@ -5,9 +5,5 @@ export BEHAT_PARAMS="{\"extensions\": {\"Behat\\\\MinkExtension\": {\"base_url\"
 export APP_ENV=dev
 
 confd -onetime -backend env
-
-su-exec www-data php app/console doctrine:database:drop --force
-su-exec www-data php app/console doctrine:database:create
-su-exec www-data php app/console doctrine:migrations:migrate --no-interaction
-
+su-exec www-data php app/console doctrine:fixtures:load --no-interaction
 ./vendor/bin/behat --config=./tests/Behat/behat.yml  --stop-on-failure $@
