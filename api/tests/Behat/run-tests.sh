765055d71435bd91a1af3db74411a9d6f7ab0d3e
Alex Saunders <acsauk@gmail.com>
2022-04-11 15:43:05 +0100

    Drop commented code

--- api/tests/Behat/run-tests.sh
+++ api/tests/Behat/run-tests.sh
@@ -5,5 +5,4 @@ export BEHAT_PARAMS="{\"extensions\": {\"Behat\\\\MinkExtension\": {\"base_url\"
 export APP_ENV=dev
 
 confd -onetime -backend env
-#su-exec www-data php app/console doctrine:fixtures:load --no-interaction
 ./vendor/bin/behat --config=./tests/Behat/behat.yml  --stop-on-failure --profile v2-tests-goutte $@
