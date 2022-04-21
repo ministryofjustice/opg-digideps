3fafc225ba3f1a617b02e26c4ea9478e6ab3b86a
Tom Gulliver <tgulliver97@gmail.com>
2022-04-21 13:58:49 +0100

    DDPB-4219 - ssm perm debug

--- environment/sync.tf
+++ environment/sync.tf
@@ -6,3 +6,16 @@ data "aws_s3_bucket" "backup" {
   bucket   = "backup.complete-deputy-report.service.gov.uk"
   provider = aws.management
 }
+
+data "aws_iam_policy_document" "sync_put_parameter_ssm" {
+  statement {
+    sid    = "AllowPutSSMParameters"
+    effect = "Allow"
+    actions = [
+      "ssm:PutParameter"
+    ]
+    resources = [
+      aws_ssm_parameter.flag_document_sync.arn,
+    ]
+  }
+}
