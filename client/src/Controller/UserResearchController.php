86df12c41725128c4725c2680fa0f488eee5a8bd
Alex Saunders <acsauk@gmail.com>
2022-04-24 13:22:27 +0100

    Update deprecated interfaces

--- client/src/Controller/UserResearchController.php
+++ client/src/Controller/UserResearchController.php
@@ -13,7 +13,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Translation\TranslatorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class UserResearchController extends AbstractController
 {
--- client/src/Form/UserResearchResponseType.php
+++ client/src/Form/UserResearchResponseType.php
@@ -10,7 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Translation\TranslatorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class UserResearchResponseType extends AbstractType
 {
