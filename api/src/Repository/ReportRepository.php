01461e0fb25370a15e504e0eef78f7d0f7aa2928
Stephen Hinett <stephen.hinett@digital.justice.gov.uk>
2022-04-25 13:58:30 +0100

    [DDPB-4206] removed debugging function

--- api/src/Repository/ReportRepository.php
+++ api/src/Repository/ReportRepository.php
@@ -296,41 +296,4 @@ END deputy_type";
 
         return $query->getQuery()->getArrayResult();
     }
-
-    /**
-     * Replaces any parameter placeholders in a query with the value of that
-     * parameter. Useful for debugging. Assumes anonymous parameters from
-     * $params are are in the same order as specified in $query
-     *
-     * @param string $query The sql query with parameter placeholders
-     * @param array $params The array of substitution parameters
-     * @return string The interpolated query
-     */
-    public function interpolateQuery($query, $params)
-    {
-        $keys = array();
-        $values = $params;
-
-        # build a regular expression for each parameter
-        foreach ($params as $key => $value) {
-            if (is_string($key)) {
-                $keys[] = '/:' . $key . '/';
-            } else {
-                $keys[] = '/[?]/';
-            }
-
-            if (is_string($value))
-                $values[$key] = "" . $value . "";
-
-            if (is_array($value))
-                $values[$key] = "'" . implode("','", $value) . "'";
-
-            if (is_null($value))
-                $values[$key] = 'NULL';
-        }
-
-        $query = preg_replace($keys, $values, $query);
-
-        return $query;
-    }
 }
