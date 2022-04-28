77397571f600dd1dc655d2191fbf09828290ffd1
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2022-04-28 14:22:55 +0100

    add type hinting to parameters

--- api/src/Service/ParameterStoreService.php
+++ api/src/Service/ParameterStoreService.php
@@ -42,13 +42,13 @@ class ParameterStoreService
         return $flag['Parameter']['Value'];
     }
 
-    public function putParameter($parameterName, $parameterValue)
+    public function putParameter(string $parameterName, string $parameterValue)
     {
         $parameterName = $this->parameterPrefix.$parameterName;
         $this->ssmClient->putParameter(['Name' => $parameterName, 'Value' => $parameterValue, 'Overwrite' => true]);
     }
 
-    public function putFeatureFlag($flagName, $flagValue)
+    public function putFeatureFlag(string $flagName, string $flagValue)
     {
         $flagName = $this->flagPrefix.$flagName;
         $this->ssmClient->putParameter(['Name' => $flagName, 'Value' => $flagValue, 'Overwrite' => true]);
