86c81fe2b6ad8c8a6befb747f673d7ca00cc828e
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2022-05-03 12:00:10 +0100

    Revert "add TokenStorage to IndexController and refactored constructor"
    This reverts commit 508091a5ef7832beee6aaac18a51f9b5e9343800.
    
    Token Storage was added to the wrong file

--- client/src/Controller/Admin/IndexController.php
+++ client/src/Controller/Admin/IndexController.php
@@ -14,7 +14,6 @@ use App\Security\UserVoter;
 use App\Service\Audit\AuditEvents;
 use App\Service\Client\Internal\UserApi;
 use App\Service\Client\RestClient;
-use App\Service\Client\TokenStorage\TokenStorageInterface;
 use App\Service\CsvUploader;
 use App\Service\DataImporter\CsvToArray;
 use App\Service\Logger;
@@ -40,15 +39,27 @@ use Symfony\Contracts\Translation\TranslatorInterface;
  */
 class IndexController extends AbstractController
 {
+    private OrgService $orgService;
+    private UserVoter $userVoter;
+    private Logger $logger;
+    private RestClient $restClient;
+    private UserApi $userApi;
+    private ObservableEventDispatcher $eventDispatcher;
+
     public function __construct(
-        private OrgService $orgService,
-        private UserVoter $userVoter,
-        private Logger $logger,
-        private RestClient $restClient,
-        private UserApi $userApi,
-        private ObservableEventDispatcher $eventDispatcher,
-        private TokenStorageInterface $tokenStorage
+        OrgService $orgService,
+        UserVoter $userVoter,
+        Logger $logger,
+        RestClient $restClient,
+        UserApi $userApi,
+        ObservableEventDispatcher $eventDispatcher
     ) {
+        $this->orgService = $orgService;
+        $this->userVoter = $userVoter;
+        $this->logger = $logger;
+        $this->restClient = $restClient;
+        $this->userApi = $userApi;
+        $this->eventDispatcher = $eventDispatcher;
     }
 
     /**
@@ -572,13 +583,10 @@ class IndexController extends AbstractController
 
     private function dispatchCSVUploadEvent(string $source)
     {
-        $currentUser = $this->tokenStorage->getToken()->getUser();
-
         $csvUploadedEvent = new CSVUploadedEvent(
             $source,
             User::TYPE_LAY,
-            AuditEvents::EVENT_CSV_UPLOADED,
-            $currentUser
+            AuditEvents::EVENT_CSV_UPLOADED
         );
 
         $this->eventDispatcher->dispatch($csvUploadedEvent, CSVUploadedEvent::NAME);
