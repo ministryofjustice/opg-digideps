81061607dc7fa91feeb7daa4c7b0568bb173eeab
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2022-05-09 10:38:52 +0100

    OrgCreatedSubscriber test added

--- client/tests/phpunit/EventSubscriber/OrgCreatedSubscriberTest.php
+++ client/tests/phpunit/EventSubscriber/OrgCreatedSubscriberTest.php
@@ -1,1 +1,64 @@
 <?php
+
+declare(strict_types=1);
+
+namespace Tests\App\EventListener;
+
+use App\Event\OrgCreatedEvent;
+use App\EventSubscriber\OrgCreatedSubscriber;
+use App\Service\Audit\AuditEvents;
+use App\Service\Time\DateTimeProvider;
+use App\TestHelpers\OrganisationHelpers;
+use App\TestHelpers\UserHelpers;
+use DateTime;
+use PHPUnit\Framework\TestCase;
+use Prophecy\PhpUnit\ProphecyTrait;
+use Psr\Log\LoggerInterface;
+
+class OrgCreatedSubscriberTest extends TestCase
+{
+    use ProphecyTrait;
+
+    /** @test */
+    public function getSubscribedEvents()
+    {
+        self::assertEquals([
+            OrgCreatedEvent::NAME => 'auditLog',
+        ], OrgCreatedSubscriber::getSubscribedEvents());
+    }
+
+    /**
+     * @test
+     */
+    public function auditLog()
+    {
+        $logger = self::prophesize(LoggerInterface::class);
+        $dateTimeProvider = self::prophesize(DateTimeProvider::class);
+
+        $now = new DateTime();
+        $dateTimeProvider->getDateTime()->willReturn($now);
+        $trigger = 'ADMIN_MANUAL_ORG_CREATION';
+
+        $sut = new OrgCreatedSubscriber($logger->reveal(), $dateTimeProvider->reveal());
+
+        $currentUser = UserHelpers::createSuperAdminUser();
+        $organisation = OrganisationHelpers::createActivatedOrganisation();
+
+        $orgCreatedEvent = new OrgCreatedEvent($trigger, $currentUser, $organisation);
+
+        $expectedEvent = [
+            'trigger' => $trigger,
+            'created_by' => $currentUser->getEmail(),
+            'organisation_id' => $organisation->getId(),
+            'organisation_name' => $organisation->getName(),
+            'organisation_identifier' => $organisation->getEmailIdentifier(),
+            'organisation_status' => $organisation->isActivated(),
+            'created_on' => $now->format(DateTime::ATOM),
+            'event' => AuditEvents::EVENT_ORG_CREATED,
+            'type' => 'audit',
+        ];
+
+        $logger->notice('', $expectedEvent)->shouldBeCalled();
+        $sut->auditLog($orgCreatedEvent);
+    }
+}
