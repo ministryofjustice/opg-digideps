386be43dae696e7f2636f4afc737892e368c2074
jamesrwarren <james.warren@digital.justice.gov.uk>
2022-05-31 11:25:58 +0100

    format

--- environment/api_elasticache.tf
+++ environment/api_elasticache.tf
@@ -1,16 +1,16 @@
 resource "aws_elasticache_replication_group" "api" {
-  automatic_failover_enabled    = local.account.elasticache_count == 1 ? false : true
-  engine                        = "redis"
-  engine_version                = "5.0.0"
-  replication_group_id          = "api-rep-group-${local.environment}"
-  replication_group_description = "Replication Group for API"
-  node_type                     = "cache.t2.micro"
-  number_cache_clusters         = local.account.elasticache_count
-  parameter_group_name          = "default.redis5.0"
-  port                          = 6379
-  subnet_group_name             = local.account.ec_subnet_group
-  security_group_ids            = [module.api_cache_security_group.id]
-  apply_immediately             = true
+  automatic_failover_enabled = local.account.elasticache_count == 1 ? false : true
+  engine                     = "redis"
+  engine_version             = "5.0.0"
+  replication_group_id       = "api-rep-group-${local.environment}"
+  description                = "Replication Group for API"
+  node_type                  = "cache.t2.micro"
+  num_cache_clusters         = local.account.elasticache_count
+  parameter_group_name       = "default.redis5.0"
+  port                       = 6379
+  subnet_group_name          = local.account.ec_subnet_group
+  security_group_ids         = [module.api_cache_security_group.id]
+  apply_immediately          = true
   tags = merge({
     InstanceName = "api-${local.environment}"
     Stack        = local.environment
