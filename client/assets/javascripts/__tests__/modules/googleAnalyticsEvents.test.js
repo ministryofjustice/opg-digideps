1937236ce7e5e9f58be624414d63c6af05acfc59
Gugandeep Chani <gugandeep.chani@digital.justice.gov.uk>
2022-06-28 15:42:50 +0100

    Added initial test for sending form errors to ga

--- client/assets/javascripts/__tests__/modules/googleAnalyticsEvents.test.js
+++ client/assets/javascripts/__tests__/modules/googleAnalyticsEvents.test.js
@@ -33,6 +33,39 @@ const setDocumentBody = () => {
               data-ga-label="site-interaction"
             >2</button>
         </div>
+        <form name="create_account" method="post" novalidate="">
+        <fieldset class="govuk-fieldset">
+            <legend class="govuk-fieldset__legend govuk-fieldset__legend--xl">
+                <h1 class="govuk-fieldset__heading">Create an account</h1>
+            </legend>
+            <div class="govuk-form-group govuk-form-group--error">
+                <label class="govuk-label" for="email">
+                    Enter your email address
+                </label>
+                <span class="govuk-error-message">
+                    <span class="govuk-visually-hidden">Error:</span> Enter an email address in the correct format, like name@example.com
+                </span>
+                <input class="govuk-input" id="email" name="email" type="email" value="" inputmode="email" spellcheck="false" autocomplete="email">
+            </div>
+            <div class="govuk-form-group govuk-form-group--error">
+                <label class="govuk-label" for="show_hide_password">
+                    Create a password
+                </label>
+                <span class="govuk-error-message">
+                <span class="govuk-visually-hidden">Error:</span> Password must be 8 characters or more
+                </span>
+                <span class="govuk-error-message">
+                <span class="govuk-visually-hidden">Error:</span> Password must include a number
+                </span>
+                <span class="govuk-error-message">
+                <span class="govuk-visually-hidden">Error:</span> Password must include a capital letter
+                </span>
+                <input class="govuk-input govuk-input moj-password-reveal__input govuk-input--width-20" id="show_hide_password" name="show_hide_password" type="password" value="">
+                <button class="govuk-button govuk-button--secondary moj-password-reveal__button" data-module="govuk-button" type="button" data-showpassword="Show" data-hidepassword="Hide">Show</button>
+            </div>
+            <button data-prevent-double-click="true" type="submit" class="govuk-button">Create account</button>
+        </fieldset>
+    </form>
     `
 }
 
@@ -41,7 +74,50 @@ beforeAll(() => {
   GoogleAnalyticsEvents.init()
 })
 
+
 describe('googleAnalyticsEvents', () => {
+  describe('initFormValidationErrors', () => {
+    it('send form validation errors to google analytics', () => {
+      const expectedEmailErrorMessage = "Enter an email address in the correct format, like name@example.com"
+      const expectedPasswordErrorMessageOne = "Password must be 8 characters or more"
+      const expectedPasswordErrorMessageTwo = "Password must include a number"
+      const expectedPasswordErrorMessageThree = "Password must include a capital letter"
+
+      GoogleAnalyticsEvents.initFormValidationErrors();
+
+      expect(window.gtag).toHaveBeenNthCalledWith(
+        1,
+        'event',
+        'Enter your email address',
+        { event_category: 'Form errors', event_label: `#email - ${expectedEmailErrorMessage}`}
+      )
+
+      expect(window.gtag).toHaveBeenNthCalledWith(
+        2,
+        'event',
+        'Create a password',
+        { event_category: 'Form errors', event_label: `#show_hide_password - ${expectedPasswordErrorMessageOne}`}
+      )
+
+      expect(window.gtag).toHaveBeenNthCalledWith(
+        3,
+        'event',
+        'Create a password',
+        { event_category: 'Form errors', event_label: `#show_hide_password - ${expectedPasswordErrorMessageTwo}`}
+      )
+
+      expect(window.gtag).toHaveBeenNthCalledWith(
+        4,
+        'event',
+        'Create a password',
+        { event_category: 'Form errors', event_label: `#show_hide_password - ${expectedPasswordErrorMessageThree}`}
+      )
+
+    })
+
+
+  })
+
   describe('extractEventInfo', () => {
     it('extracts event action, event_category and event_label from ga-event element', () => {
       const button1 = document.getElementById('button1')
