8b163d07f40c81554c7d8f2a2c817e4d37de776e
jamesrwarren <james.warren@digital.justice.gov.uk>
2022-07-15 15:59:49 +0100

    wip

--- environment/events_iam.tf
+++ environment/events_iam.tf
@@ -22,44 +22,39 @@ resource "aws_iam_role_policy" "events_task_runner" {
   role   = aws_iam_role.events_task_runner.id
 }
 
-#locals {
-#  events_task_dr_role_list = local.account.dr_backup ? [module.disaster_recovery_backup.task_role_arn] : []
-#  events_task_role_list = [
-#      aws_iam_role.front.arn,
-#      aws_iam_role.api.arn,
-#      data.aws_iam_role.sync.arn,
-#      aws_iam_role.execution_role.arn,
-#  ]
-#  combined_events_task_role_list = tolist(concat(local.events_task_role_list, local.events_task_dr_role_list))
-#}
+locals {
+  events_task_dr_role_list = local.account.dr_backup ? [module.disaster_recovery_backup.task_role_arn] : []
+  events_task_role_list = [
+    aws_iam_role.front.arn,
+    aws_iam_role.api.arn,
+    data.aws_iam_role.sync.arn,
+    aws_iam_role.execution_role.arn,
+  ]
+  combined_events_task_role_list = tolist(concat(local.events_task_role_list, local.events_task_dr_role_list))
 
-#local.combined_events_task_role_list
+  events_dr_task_list = local.account.dr_backup ? [module.disaster_recovery_backup.task_definition_arn] : []
+  events_task_list = [
+    aws_ecs_task_definition.check_csv_uploaded.arn,
+    aws_ecs_task_definition.check_csv_uploaded.arn,
+    aws_ecs_task_definition.checklist_sync.arn,
+    aws_ecs_task_definition.api.arn,
+    aws_ecs_task_definition.document_sync.arn,
+  ]
+  combined_events_task_list = tolist(concat(local.events_task_list, local.events_dr_task_list))
+}
 
 data "aws_iam_policy_document" "events_task_runner_policy" {
   statement {
-    effect = "Allow"
-    resources = [
-      aws_iam_role.front.arn,
-      aws_iam_role.api.arn,
-      data.aws_iam_role.sync.arn,
-      aws_iam_role.execution_role.arn,
-      one(module.disaster_recovery_backup[*].task_role_arn)
-    ]
+    effect    = "Allow"
+    resources = local.combined_events_task_role_list
     actions = [
       "iam:GetRole",
       "iam:PassRole"
     ]
   }
   statement {
-    effect = "Allow"
-    resources = [
-      aws_ecs_task_definition.check_csv_uploaded.arn,
-      aws_ecs_task_definition.check_csv_uploaded.arn,
-      aws_ecs_task_definition.checklist_sync.arn,
-      aws_ecs_task_definition.api.arn,
-      aws_ecs_task_definition.document_sync.arn,
-      one(module.disaster_recovery_backup[*].task_definition_arn),
-    ]
-    actions = ["ecs:RunTask"]
+    effect    = "Allow"
+    resources = local.combined_events_task_list
+    actions   = ["ecs:RunTask"]
   }
 }
