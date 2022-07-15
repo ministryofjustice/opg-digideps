c3cde37903f024f7789a50841a167b8c39c5e5e4
jamesrwarren <james.warren@digital.justice.gov.uk>
2022-07-15 14:14:33 +0100

    fix the lists

--- environment/events_iam.tf
+++ environment/events_iam.tf
@@ -22,6 +22,19 @@ resource "aws_iam_role_policy" "events_task_runner" {
   role   = aws_iam_role.events_task_runner.id
 }
 
+#locals {
+#  events_task_dr_role_list = local.account.dr_backup ? [module.disaster_recovery_backup.task_role_arn] : []
+#  events_task_role_list = [
+#      aws_iam_role.front.arn,
+#      aws_iam_role.api.arn,
+#      data.aws_iam_role.sync.arn,
+#      aws_iam_role.execution_role.arn,
+#  ]
+#  combined_events_task_role_list = tolist(concat(local.events_task_role_list, local.events_task_dr_role_list))
+#}
+
+#local.combined_events_task_role_list
+
 data "aws_iam_policy_document" "events_task_runner_policy" {
   statement {
     effect = "Allow"
@@ -29,8 +42,8 @@ data "aws_iam_policy_document" "events_task_runner_policy" {
       aws_iam_role.front.arn,
       aws_iam_role.api.arn,
       data.aws_iam_role.sync.arn,
-      aws_iam_role.execution_role,
-      one(module.disaster_recovery_backup[*].task_role_arn),
+      aws_iam_role.execution_role.arn,
+      one(module.disaster_recovery_backup[*].task_role_arn)
     ]
     actions = [
       "iam:GetRole",
