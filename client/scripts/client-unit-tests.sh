531a2fdb706a74f4d9c66d9ff59c43efce88aef0
jamesrwarren <james.warren@digital.justice.gov.uk>
2022-09-07 09:36:13 +0100

    pass through fake creds for the tests as seems to be using the ci ones

--- client/scripts/client-unit-tests.sh
+++ client/scripts/client-unit-tests.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # exit on error
 set -e
-
-php vendor/bin/phpunit -c tests/phpunit --coverage-php tests/phpunit/coverage/client-unit-tests.cov
+php vendor/bin/phpunit -c tests/phpunit --coverage-php tests/phpunit/coverage/client-unit-tests-service.cov
+#php vendor/bin/phpunit -c tests/phpunit --testsuite "Client unit tests service files" --coverage-php tests/phpunit/coverage/client-unit-tests-service.cov
+#php vendor/bin/phpunit -c tests/phpunit --testsuite "Client unit tests service directories 1" --coverage-php tests/phpunit/coverage/client-unit-tests-service.cov
+#php vendor/bin/phpunit -c tests/phpunit --testsuite "Client unit tests service directories 2" --coverage-php tests/phpunit/coverage/client-unit-tests-service.cov
+#php vendor/bin/phpunit -c tests/phpunit --testsuite "Client unit tests" --coverage-php tests/phpunit/coverage/client-unit-tests.cov
+#php vendor/bin/phpunit -c tests/phpunit --testsuite "Pact contract tests" --coverage-php tests/phpunit/coverage/pact-contract-tests.cov
 
 php vendor/phpunit/phpcov/phpcov merge --clover "./tests/phpunit/coverage/client-unit-tests.xml" "./tests/phpunit/coverage"
