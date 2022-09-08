16339b3b2f18dc2167ec859c5a8c630c808fff89
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2022-09-08 14:20:04 +0100

    translation refactored to return code and detail of error message

--- client/src/Service/SiriusApiErrorTranslator.php
+++ client/src/Service/SiriusApiErrorTranslator.php
@@ -27,34 +27,8 @@ class SiriusApiErrorTranslator
 
         $apiError = $this->deserializeError($errorString);
 
-        $translations = [
-            'OPGDATA-API-FORBIDDEN' => 'Credentials used for integration lack correct permissions',
-            'OPGDATA-API-API_CONFIGURATION_ERROR' => 'Integration API internal error',
-            'OPGDATA-API-AUTHORIZER_CONFIGURATION_ERROR' => 'Integration API internal error',
-            'OPGDATA-API-AUTHORIZER_FAILURE' => 'Integration API internal error',
-            'OPGDATA-API-INVALIDREQUEST' => 'The body of the request is not valid',
-            'OPGDATA-API-BAD_REQUEST_PARAMETERS' => 'The parameters of the request are not valid',
-            'OPGDATA-API-SERVERERROR' => 'Integration API server error',
-            'OPGDATA-API-EXPIRED_TOKEN' => 'Auth token has expired',
-            'OPGDATA-API-INTEGRATION_FAILURE' => 'There was a problem syncing from the integration to Sirius',
-            'OPGDATA-API-INTEGRATION_TIMEOUT' => 'The sync process timed out while communicating with Sirius',
-            'OPGDATA-API-INVALID_API_KEY' => 'The API key used in the request is not valid',
-            'OPGDATA-API-INVALID_SIGNATURE' => 'The signature of the request is not valid',
-            'OPGDATA-API-MISSING_AUTHENTICATION_TOKEN' => 'Authentication token is missing from the request',
-            'OPGDATA-API-QUOTA_EXCEEDED' => 'API quota has been exceeded',
-            'OPGDATA-API-FILESIZELIMIT' => 'The size of the file exceeded the file size limit (6MB)',
-            'OPGDATA-API-NOTFOUND' => 'Invalid URL used during integration or the resource no longer exists',
-            'OPGDATA-API-THROTTLED' => 'Too many requests made - throttling in action',
-            'OPGDATA-API-UNAUTHORISED' => 'No user/auth provided during requests',
-            'OPGDATA-API-MEDIA' => 'Media type of the file is not supported',
-            'OPGDATA-API-WAF_FILTERED' => 'AWS WAF filtered this request and it was not sent to Sirius',
-        ];
+        return sprintf('%s: %s', $apiError->getCode(), $apiError->getDetail());
 
-        if (is_null($apiError->getCode()) || is_null($translations[$apiError->getCode()] ?? null)) {
-            return $errorString;
-        } else {
-            return sprintf('%s: %s', $apiError->getCode(), $translations[$apiError->getCode()]);
-        }
     }
 
     /**
@@ -62,7 +36,7 @@ class SiriusApiErrorTranslator
      */
     private function deserializeError(string $errorString)
     {
-        $decodedJson = json_decode($errorString, true)['body']['error'];
+        $decodedJson = json_decode($errorString, true)['error'];
 
         return $this->serializer->deserialize(json_encode($decodedJson), 'App\Model\Sirius\SiriusApiError', 'json');
     }
@@ -72,7 +46,9 @@ class SiriusApiErrorTranslator
         $decodedJson = json_decode($errorString, true);
 
         return is_null($decodedJson) ||
-        !array_key_exists('body', $decodedJson) ||
-        (array_key_exists('body', $decodedJson) && (!array_key_exists('error', $decodedJson['body'])));
+        !array_key_exists('error', $decodedJson) ||
+        (array_key_exists('error', $decodedJson) && (!array_key_exists('code', $decodedJson['error']))) ||
+        (array_key_exists('error', $decodedJson) && (!array_key_exists('detail', $decodedJson['error']))) ||
+        (array_key_exists('error', $decodedJson) && (!array_key_exists('title', $decodedJson['error'])));
     }
 }
