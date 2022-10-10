a552309c5edab3545b6b2f904a33dac019cc83cd
Matt Machell <matt.machell@digital.justice.gov.uk>
2022-10-10 15:30:09 +0100

    Try new image with some config changes

--- client/scripts/client-unit-tests.sh
+++ client/scripts/client-unit-tests.sh
@@ -2,6 +2,6 @@
 # exit on error
 set -e
 
-php vendor/bin/phpunit -c tests/phpunit --coverage-php tests/phpunit/coverage/client-unit-tests.cov
+php vendor/bin/phpunit -c tests/phpunit --coverage-php tests/phpunit/coverage/client-unit-tests.cov --verbose
 
 php vendor/phpunit/phpcov/phpcov merge --clover "./tests/phpunit/coverage/client-unit-tests.xml" "./tests/phpunit/coverage"
