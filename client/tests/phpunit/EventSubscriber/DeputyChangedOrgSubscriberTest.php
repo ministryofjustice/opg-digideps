7dd5228ab771698c297857676e247ebae0958688
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2022-10-25 16:03:07 +0100

    Subscriber passing test

--- client/tests/phpunit/EventSubscriber/DeputyChangedOrgSubscriberTest.php
+++ client/tests/phpunit/EventSubscriber/DeputyChangedOrgSubscriberTest.php
@@ -8,7 +8,10 @@ use App\Event\DeputyChangedOrgEvent;
 use App\EventSubscriber\DeputyChangedOrgSubscriber;
 use App\Service\Audit\AuditEvents;
 use App\Service\Time\DateTimeProvider;
+use App\TestHelpers\ClientHelpers;
 use App\TestHelpers\NamedDeputyHelper;
+use App\TestHelpers\OrganisationHelpers;
+use App\TestHelpers\UserHelpers;
 use PHPUnit\Framework\TestCase;
 use Psr\Log\LoggerInterface;
 use DateTime;
@@ -36,55 +39,44 @@ class DeputyChangedOrgSubscriberTest extends TestCase
 
         $now = new DateTime();
         $dateTimeProvider->getDateTime()->willReturn($now);
+
         $trigger = 'DEPUTY_CHANGED_ORG';
+
+//      Client record currently in database
+        $client = ClientHelpers::createClient();
+        $clientOrg = OrganisationHelpers::createActivatedOrganisation();
+        $namedDeputy = NamedDeputyHelper::createNamedDeputy();
+        $client->setNamedDeputy($namedDeputy);
+        $client->setOrganisation($clientOrg);
+
+        $deputyId = $client->getDeputy()->getId();
+        $previousOrgId = $client->getOrganisation()->getId();
+
+
+//      New organisation linked to client
+        $newOrg = OrganisationHelpers::createActivatedOrganisation();
+        $client->setOrganisation($newOrg);
+        $newOrgId = $client->getOrganisation()->getId();
+
+        $clientIds = [$client->getId()];
 
         $sut = new DeputyChangedOrgSubscriber($logger->reveal(), $dateTimeProvider->reveal());
 
-        $namedDeputy = NamedDeputyHelper::createNamedDeputy();
-
-        $clientPreCSVUpload =
-            [
-                'id' => 30,
-                'case_number' => '10000000',
-                'court_date' => '2022-01-01',
-                'email' => 'client@hotmail.co.uk',
-                'first_name' => 'Julie',
-                'last_name' => 'Brown',
-                'organisation_id' => 7,
-                'named_deputy_id' => $namedDeputy->getId(),
-             ];
-
-        $previousOrg = $clientPreCSVUpload['organisation_id'];
-
-        $clientPostCSVUpload =
-            [
-                'id' => 30,
-                'case_number' => '10000000',
-                'court_date' => '2022-01-01',
-                'email' => 'client@hotmail.co.uk',
-                'first_name' => 'Julie',
-                'last_name' => 'Brown',
-                'organisation_id' => 8,
-                'named_deputy_id' => $namedDeputy->getId()
-            ];
-
-        $newOrg = $clientPostCSVUpload['organisation_id'];
-
-        $deputyChangedEvent = new DeputyChangedOrgEvent($trigger, $previousOrg, $newOrg, $clientPostCSVUpload);
+        $deputyChangedOrgEvent = new DeputyChangedOrgEvent($trigger, $deputyId, $previousOrgId, $newOrgId, $clientIds);
 
         $expectedEvent = [
             'trigger' => $trigger,
             'date_deputy_changed' => $now->format(DateTime::ATOM),
-            'deputy_id' => $clientPreCSVUpload['named_deputy_id'],
-            'organisation_moved_from' => $previousOrg,
-            'organisation_moved_to' => $newOrg,
-            'clients_moved_over' => $clientPreCSVUpload['id'],
+            'deputy_id' => $deputyId,
+            'organisation_moved_from' => $previousOrgId,
+            'organisation_moved_to' => $newOrgId,
+            'clients_moved_over' => $clientIds,
             'event' => AuditEvents::EVENT_DEPUTY_CHANGED_ORG,
             'type' => 'audit',
         ];
 
         $logger->notice('', $expectedEvent)->shouldBeCalled();
-        $sut->auditLog($deputyChangedEvent);
+        $sut->auditLog($deputyChangedOrgEvent);
     }
 }
 
