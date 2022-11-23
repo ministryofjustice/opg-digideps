165ceb5b3d54173fa786464ab589aa0982aa7ff8
Gugandeep Chani <62657652+Gugandeep@users.noreply.github.com>
2022-11-23 17:05:32 +0000

    DDPB-4558 - User sessions that timeout result in a general application error (#1028)

---
+++ client/src/EventListener/UnauthorizedExceptionListener.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class UnauthorizedExceptionListener
+{
+    public function onKernelException(ExceptionEvent $exceptionEvent)
+    {
+        $exception = $exceptionEvent->getThrowable();
+
+        if ($exception instanceof HttpException) {
+            if (Response::HTTP_UNAUTHORIZED == $exception->getStatusCode()) {
+                $response = new RedirectResponse('/login?from=api');
+                $exceptionEvent->setResponse($response);
+                $exceptionEvent->stopPropagation();
+            }
+        }
+    }
+}
