09fba3f6de676dda00cc0137c4abad8ba27e0899
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2022-12-15 13:49:07 +0000

    increased varchar in migration file

--- api/src/Migrations/Version267.php
+++ api/src/Migrations/Version267.php
@@ -19,7 +19,7 @@ final class Version267 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE pre_registration ADD deputy_firstname VARCHAR(6) DEFAULT NULL');
+        $this->addSql('ALTER TABLE pre_registration ADD deputy_firstname VARCHAR(100) DEFAULT NULL');
 
     }
 
