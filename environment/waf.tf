c052a41876bd35b4f9d8af5c01f4c6dd1110f0b4
Jim <58939809+jamesrwarren@users.noreply.github.com>
2022-12-20 10:37:53 +0000

    DDPB-4552 add resource protection (#1040)
    * DDPB-4552 add resource protection
    
    * add waf associations

--- environment/waf.tf
+++ environment/waf.tf
@@ -14,3 +14,27 @@ resource "aws_wafv2_web_acl_association" "front" {
   resource_arn = aws_lb.front.arn
   web_acl_arn  = data.aws_wafv2_web_acl.main.arn
 }
+
+resource "aws_shield_protection" "admin_alb_protection" {
+  name         = "admin-alb-protection-${local.environment}"
+  resource_arn = aws_lb.admin.arn
+
+  tags = local.default_tags
+}
+
+resource "aws_shield_protection_health_check_association" "admin" {
+  health_check_arn     = aws_route53_health_check.availability-admin.arn
+  shield_protection_id = aws_shield_protection.admin_alb_protection.id
+}
+
+resource "aws_shield_protection" "front_alb_protection" {
+  name         = "front-alb-protection-${local.environment}"
+  resource_arn = aws_lb.front.arn
+
+  tags = local.default_tags
+}
+
+resource "aws_shield_protection_health_check_association" "front" {
+  health_check_arn     = aws_route53_health_check.availability-front.arn
+  shield_protection_id = aws_shield_protection.front_alb_protection.id
+}
