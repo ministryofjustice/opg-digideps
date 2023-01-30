98c237552c682899d7a73002fada6cc35daa9db6
James Warren <james.warren@digital.justice.gov.uk>
2023-01-30 09:05:55 +0000

    add unit tests

--- api/tests/Unit/DBAL/ConnectionWrapperTest.php
+++ api/tests/Unit/DBAL/ConnectionWrapperTest.php
@@ -1,130 +1,118 @@
 <?php
 
-//
-// declare(strict_types=1);
-//
-// namespace App\Tests\Unit\DBAL;
-//
-// use PHPUnit\Framework\TestCase;
-// use App\DBAL\ConnectionWrapper;
-// use Doctrine\DBAL\Driver;
-// use Doctrine\DBAL\Configuration;
-// use Aws\SecretsManager\SecretsManagerClient;
-// use Doctrine\Common\EventManager;
-// use Predis\Client as PredisClient;
-//
-// class ConnectionWrapperTest extends TestCase
-// {
-//    private $connectionWrapper;
-//    private $driverMock;
-//    private $configMock;
-//    private $eventManagerMock;
-//    private $secretsManagerClientMock;
-//    private $predisClientMock;
-//
-//    protected function setUp(): void
-//    {
-//        $this->driverMock = $this->createMock(Driver::class);
-//        $this->configMock = $this->createMock(Configuration::class);
-//        $this->eventManagerMock = $this->createMock(EventManager::class);
-//        $this->secretsManagerClientMock = $this->createMock(SecretsManagerClient::class);
-//        $this->predisClientMock = $this->createMock(PredisClient::class);
-//
-//        $params = [
-//            'user' => 'myuser',
-//            'password' => 'mypassword',
-//            'host' => 'myhost',
-//            'dbname' => 'mydb',
-//        ];
-//
-//        $this->connectionWrapper = new ConnectionWrapper(
-//            $params,
-//            $this->driverMock,
-//            $this->configMock,
-//            $this->eventManagerMock
-//        );
-//        $this->connectionWrapper->setSecretsManagerClient($this->secretsManagerClientMock);
-//        $this->connectionWrapper->setPredisClient($this->predisClientMock);
-//    }
-//
-//    public function testConnect()
-//    {
-//        $this->predisClientMock->expects($this->once())
-//            ->method('get')
-//            ->with(ConnectionWrapper::DB_PASSWORD)
-//            ->willReturn('mypassword');
-//        $this->driverMock->expects($this->once())
-//            ->method('connect')
-//            ->with([
-//                'user' => 'myuser',
-//                'password' => 'mypassword',
-//                'host' => 'myhost',
-//                'dbname' => 'mydb',
-//            ])
-//            ->willReturn('connection');
-//
-//        $this->configMock->expects($this->once())
-//            ->method('getAutoCommit')
-//            ->willReturn(true);
-//
-//        $this->assertFalse($this->connectionWrapper->isConnected());
-//        $this->connectionWrapper->connect();
-//        $this->assertTrue($this->connectionWrapper->isConnected());
-//    }
-//
-//    public function testConnectFails()
-//    {
-//        $this->predisClientMock->expects($this->once())
-//            ->method('get')
-//            ->with(ConnectionWrapper::DB_PASSWORD)
-//            ->willReturn(null);
-//        $this->driverMock->expects($this->once())
-//            ->method('connect')
-//            ->with([
-//                'user' => 'myuser',
-//                'password' => 'mypassword',
-//                'host' => 'myhost',
-//                'dbname' => 'mydb',
-//            ])
-//            ->willThrowException(new Driver\Exception());
-//        $this->secretsManagerClientMock->expects($this->once())
-//            ->method('getSecretValue')
-//            ->willReturn(['SecretString' => 'newpassword']);
-//        $this->predisClientMock->expects($this->once())
-//            ->method('set')
-//            ->with(ConnectionWrapper::DB_PASSWORD, 'newpassword');
-//        $this->driverMock->expects($this->once())
-//            ->method('connect')
-//            ->with([
-//                'user' => 'myuser',
-//                'password' => 'newpassword',
-//                'host' => 'myhost',
-//                'dbname' => 'mydb',
-//            ])
-//            ->willReturn('connection');
-//
-//        $this->configMock->expects($this->once())
-//            ->method('getAutoCommit')
-//            ->willReturn(true);
-//
-//        $this->assertFalse($this->connectionWrapper->isConnected());
-//        $this->connectionWrapper->connect();
-//        $this->assertTrue($this->connectionWrapper->isConnected());
-//    }
-//
-//    public function testRefreshToken()
-//    {
-//        $this->secretsManagerClientMock->expects($this->once())
-//            ->method('getSecretValue')
-//            ->with([
-//                'SecretId' => 'SECRETS_PREFIX_DBdatabase-password',
-//            ])
-//            ->willReturn(['SecretString' => 'newpassword']);
-//        $this->predisClientMock->expects($this->once())
-//            ->method('set')
-//            ->with(ConnectionWrapper::DB_PASSWORD, 'newpassword');
-//
-//        $this->connectionWrapper->refreshToken();
-//        $this->assertEquals('newpassword', $this->connectionWrapper->getPassword());
-//    }
-// }
+namespace App\Tests\Unit\DBAL;
+
+use App\DBAL\ConnectionWrapper;
+use Aws\SecretsManager\Exception\SecretsManagerException;
+use Aws\SecretsManager\SecretsManagerClient;
+use Doctrine\Common\EventManager;
+use Doctrine\DBAL\Configuration;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Driver;
+use PHPUnit\Framework\TestCase;
+use Predis\Client as PredisClient;
+
+class ConnectionWrapperTest extends TestCase
+{
+    public function testConnect()
+    {
+        // Create mocks for the dependencies
+        $driver = $this->createMock(Driver::class);
+        $config = $this->createMock(Configuration::class);
+        $eventManager = $this->createMock(EventManager::class);
+        $redis = $this->getMockClass('Predis\Client', [['exists', 'get', 'set']]);
+        $secretClient = $this->createMock(SecretsManagerClient::class);
+
+        // Set up the mock for the connect method of the driver
+        $conn = $this->createMock(Connection::class);
+        $driver->expects($this->once())->method('connect')->will($this->returnValue($conn));
+
+        // Set up the mock for the autoCommit method of the configuration
+        $config->expects($this->once())->method('getAutoCommit')->will($this->returnValue(false));
+
+        // Set up the mocks for the get and set methods of the RedisClient
+        $redis->expects($this->once())->method('get')->will($this->returnValue(false));
+
+        $redis->expects($this->once())->method('set')->with(
+            ConnectionWrapper::DB_PASSWORD,
+            'initial_pw'
+        );
+
+        // Set up the mock for the refreshPassword method
+        $secretClient->expects($this->once())->method('getSecretValue')->willThrowException(
+            new SecretsManagerException('Test exception')
+        );
+
+        $params = [
+            'dbname' => 'testdb',
+            'user' => 'testuser',
+            'password' => 'testpassword',
+            'host' => 'testhost',
+            'driver' => 'pdo_postgresql',
+        ];
+
+        // Create an instance of the ConnectionWrapper and call the connect method
+        $wrapper = new ConnectionWrapper(
+            $params,
+            $driver,
+            $config,
+            $eventManager
+        );
+//        $wrapper->setRedis($redis);
+//        $wrapper->setSecretsManagerClient($secretClient);
+        $result = $wrapper->connect();
+
+        // Assert the result of the connect method
+        $this->assertTrue($result);
+    }
+
+    public function testRefreshPassword()
+    {
+        $params = [
+            'dbname' => 'testdb',
+            'user' => 'testuser',
+            'password' => 'testpassword',
+            'host' => 'testhost',
+            'driver' => 'pdo_postgresql',
+        ];
+
+        $mockDriver = $this->createMock(Driver::class);
+        $mockConfig = $this->createMock(Configuration::class);
+        $mockEventManager = $this->createMock(EventManager::class);
+
+        $mockConfig->method('getAutoCommit')
+            ->willReturn(true);
+
+        $mockRedis = $this->createMock(PredisClient::class);
+        $mockRedis->method('get')
+            ->willReturn(null);
+
+        $mockRedis->method('set')
+            ->willReturn(true);
+
+        $mockSecretClient = $this->createMock(SecretsManagerClient::class);
+        $mockSecretClient->method('getSecretValue')
+            ->willReturn(['SecretString' => 'newsecret']);
+
+        $connectionWrapper = $this->getMockBuilder(ConnectionWrapper::class)
+            ->setConstructorArgs([$params, $mockDriver, $mockConfig, $mockEventManager])
+            ->setMethods(['setRedis', 'setSecretsManagerClient'])
+            ->getMock();
+
+        $connectionWrapper->expects($this->once())
+            ->method('setRedis')
+            ->with($this->equalTo($mockRedis));
+
+        $connectionWrapper->expects($this->once())
+            ->method('setSecretsManagerClient')
+            ->with($this->equalTo($mockSecretClient));
+
+        $connectionWrapper->connect();
+
+        $reflectedPassword = new ReflectionProperty(ConnectionWrapper::class, 'params');
+        $reflectedPassword->setAccessible(true);
+        $params = $reflectedPassword->getValue($connectionWrapper);
+
+        $this->assertEquals('newsecret', $params['password']);
+    }
+}
