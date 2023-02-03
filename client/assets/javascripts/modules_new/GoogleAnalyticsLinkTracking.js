e0862ad8cd331813a57b53d32c4ac9c7da6e2ff6
Matt Machell <matt.machell@digital.justice.gov.uk>
2023-02-03 13:27:37 +0000

    move e to event

--- client/assets/javascripts/modules_new/GoogleAnalyticsLinkTracking.js
+++ client/assets/javascripts/modules_new/GoogleAnalyticsLinkTracking.js
@@ -1,22 +1,22 @@
 /* globals ga */
 module.exports = {
-    //TODO this can probably be moved into the main GA Events class
-    init: function(document, timeout){
-        document.addEventListener('click', (event)=>{
-            if(event.target.matches("a.js-trackDownloadLink")){
-                e.preventDefault()
-                const link = event.target.getAttribute('href')
-            
-                // track page view with the "href" link
-                ga('send', 'pageview', link)
-            
-                // continue to load page
-                setTimeout(function () {
-                  window.location.href = link
-                }, timeout)
-            
-                return false
-            }
-        })
-    }
+  // TODO this can probably be moved into the main GA Events class
+  init: function (document, timeout) {
+    document.addEventListener('click', (event) => {
+      if (event.target.matches('a.js-trackDownloadLink')) {
+        event.preventDefault()
+        const link = event.target.getAttribute('href')
+
+        // track page view with the "href" link
+        ga('send', 'pageview', link)
+
+        // continue to load page
+        setTimeout(function () {
+          window.location.href = link
+        }, timeout)
+
+        return false
+      }
+    })
+  }
 }
