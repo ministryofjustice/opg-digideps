a2500d8581849d7a0acea6f49b06d97e2887baa7
Gugandeep Chani <gugandeep.chani@digital.justice.gov.uk>
2023-02-15 15:51:25 +0000

    Adding in client archived audit logging, event and event dispatching

---
+++ api/src/EventSubscriber/ClientArchivedSubscriber.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EventSubscriber;
+
+use App\Event\ClientArchivedEvent;
+use App\Service\Audit\AuditEvents;
+use App\Service\Time\DateTimeProvider;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class ClientArchivedSubscriber implements EventSubscriberInterface
+{
+    /** @var LoggerInterface */
+    private $logger;
+
+    /** @var DateTimeProvider */
+    private $dateTimeProvider;
+
+    public function __construct(LoggerInterface $logger, DateTimeProvider $dateTimeProvider)
+    {
+        $this->logger = $logger;
+        $this->dateTimeProvider = $dateTimeProvider;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            ClientArchivedEvent::NAME => 'logEvent',
+        ];
+    }
+
+    public function logEvent(ClientArchivedEvent $event)
+    {
+        $this->logger->notice('', (new AuditEvents($this->dateTimeProvider))->clientArchived(
+            $event->getTrigger(),
+            $event->getClient()->getCaseNumber(),
+            $event->getCurrentUser()->getEmail(),
+        ));
+    }
+}
---
+++ api/src/Service/Audit/AuditEvents.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\Audit;
+
+use App\Service\Time\DateTimeProvider;
+
+final class AuditEvents
+{
+    public const EVENT_CLIENT_ARCHIVED = 'CLIENT_ARCHIVED';
+
+    public const TRIGGER_USER_ARCHIVED_CLIENT = 'USER_ARCHIVED_CLIENT';
+
+    /**
+     * @var DateTimeProvider
+     */
+    private $dateTimeProvider;
+
+    public function __construct(DateTimeProvider $dateTimeProvider)
+    {
+        $this->dateTimeProvider = $dateTimeProvider;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function clientArchived(
+        string $trigger,
+        string $caseNumber,
+        string $archivedBy,
+    ): array {
+        $event = [
+            'trigger' => $trigger,
+            'case_number' => $caseNumber,
+            'archived_by' => $archivedBy,
+            'discharged_on' => $this->dateTimeProvider->getDateTime()->format(\DateTime::ATOM),
+        ];
+
+        return $event + $this->baseEvent(AuditEvents::EVENT_CLIENT_ARCHIVED);
+    }
+
+    private function baseEvent(string $eventName): array
+    {
+        return [
+            'event' => $eventName,
+            'type' => 'audit',
+        ];
+    }
+}
