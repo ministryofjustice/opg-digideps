646a3666e043f0c643ca35490829b03db94a3868
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2023-04-17 16:01:39 +0100

    A user completes the decisions section test passes

--- api/tests/Behat/bootstrap/v2/Reporting/Sections/DecisionSectionTrait.php
+++ api/tests/Behat/bootstrap/v2/Reporting/Sections/DecisionSectionTrait.php
@@ -36,4 +36,61 @@ trait DecisionSectionTrait
             throw new BehatException(sprintf('Not on decisions start page. Current URL is: %s', $currentUrl));
         }
     }
+
+    /**
+     * @Given /^I confirm that the clients mental capacity is the same$/
+     */
+    public function iConfirmThatTheClientsMentalCapacityIsTheSame()
+    {
+        $this->chooseOption('mental_capacity[hasCapacityChanged]', 'stayedSame', 'hasCapacityChanged');
+        $this->pressButton('Save and continue');
+        $this->iAmOnDecisionsPage2();
+    }
+
+    /**
+     * @Given /^I confirm the clients last assessment date$/
+     */
+    public function iConfirmTheClientsLastAssessmentDate()
+    {
+        $this->fillInField('mental_assessment[mentalAssessmentDate][month]', '01', 'mentalAssessmentDate');
+
+        $this->fillInField('mental_assessment[mentalAssessmentDate][year]', '01', 'mentalAssessmentDate');
+
+        $this->pressButton('Save and continue');
+        $this->iAmOnDecisionsPage3();
+    }
+
+    /**
+     * @Given /^I confirm that no significant decisions have been made for the client$/
+     */
+    public function iConfirmThatNoSignificantDecisionsHaveBeenMadeForTheClient()
+    {
+        $this->chooseOption('decision_exist[significantDecisionsMade]', 'No', 'significantDecisionsMade');
+        $this->fillInField('decision_exist[reasonForNoDecisions]', 'test', 'reasonForNoDecisions');
+        $this->pressButton('Save and continue');
+    }
+
+    /**
+     * @Then /^the decisions summary page should contain the details I entered$/
+     */
+    public function theDecisionsSummaryPageShouldContainTheDetailsIEntered()
+    {
+        $this->iAmOnDecisionsSummaryPage();
+
+        if ($this->getSectionAnswers('mental_capacity[hasCapacityChanged]')) {
+            $this->expectedResultsDisplayedSimplified('hasCapacityChanged', true);
+        }
+
+        if ($this->getSectionAnswers('mental_capacity[mentalAssessmentDate]')) {
+            $this->expectedResultsDisplayedSimplified('mentalAssessmentDate', true);
+        }
+
+        if ($this->getSectionAnswers('decision_exist[significantDecisionsMade]')) {
+            $this->expectedResultsDisplayedSimplified('significantDecisionsMade', true);
+        }
+
+        if ($this->getSectionAnswers('decision_exist[reasonForNoDecisions]')) {
+            $this->expectedResultsDisplayedSimplified('reasonForNoDecision', true);
+        }
+    }
 }
