97325d78f01cfe296fecf0893c0a62821031c92c
Gugandeep Chani <gugandeep.chani@digital.justice.gov.uk>
2023-04-19 14:51:11 +0100

    Implemented swapping of reports using client ids, added in error handling and providing user feedback

---
+++ api/src/v2/Tools/Controller/ToolsController.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\v2\Tools\Controller;
+
+use App\Entity\Client;
+use App\Repository\ClientRepository;
+use App\Repository\ReportRepository;
+use App\Repository\UserRepository;
+use App\v2\Controller\ControllerTrait;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/tools")
+ */
+class ToolsController extends AbstractController
+{
+    use ControllerTrait;
+
+    public function __construct(
+        private EntityManagerInterface $em,
+        private ReportRepository $reportRepository,
+        private UserRepository $userRepository,
+        private ClientRepository $clientRepository,
+    ) {
+    }
+
+    /**
+     * @Route("/reassign-reports", methods={"POST"})
+     * @Security("is_granted('ROLE_SUPER_ADMIN')")
+     *
+     * @return JsonResponse
+     *
+     * @throws \Exception
+     */
+    public function reassignReports(Request $request)
+    {
+        $fromRequest = json_decode($request->getContent(), true);
+
+        $firstClientId = intval($fromRequest['firstClientId']);
+        $secondClientId = intval($fromRequest['secondClientId']);
+
+        if (0 == $firstClientId || 0 == $secondClientId) {
+            return $this->buildErrorResponse('The client ids provided are not valid numbers');
+        }
+
+        if ($firstClientId == $secondClientId) {
+            return $this->buildErrorResponse('Need two different Client ids to reassign reports!');
+        }
+
+        /** @var Client $firstClient */
+        if (null === $firstClient = $this->clientRepository->findByIdIncludingDischarged($firstClientId)) {
+            return $this->buildErrorResponse(sprintf('First Client with id %s not found', $firstClientId));
+        }
+
+        /** @var Client $secondClient */
+        if (null === $secondClient = $this->clientRepository->findByIdIncludingDischarged($secondClientId)) {
+            return $this->buildErrorResponse(sprintf('Second Client with id %s not found', $secondClientId));
+        }
+
+        $firstReports = $firstClient->getReports();
+        $secondReports = $secondClient->getReports();
+
+        foreach ($firstReports as $report) {
+            $report->setClient($secondClient);
+        }
+
+        foreach ($secondReports as $report) {
+            $report->setClient($firstClient);
+        }
+
+        $this->em->flush();
+
+        return $this->buildSuccessResponse([], 'Reports have been reassigned successfully');
+    }
+}
---
+++ client/src/Controller/Admin/ToolsController.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use App\Controller\AbstractController;
+use App\Exception\RestClientException;
+use App\Form\Admin\Tool\ReportReassignmentType;
+use App\Service\Client\RestClient;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin/tools")
+ */
+class ToolsController extends AbstractController
+{
+    public function __construct(
+        private RestClient $restClient
+    ) {
+    }
+
+    /**
+     * @Route("/", name="admin_tools")
+     * @Security("is_granted('ROLE_SUPER_ADMIN')")
+     * @Template("@App/Admin/Tools/index.html.twig")
+     */
+    public function tools()
+    {
+        return [];
+    }
+
+    /**
+     * @Route("/report-reassignment", name="admin_tools_report_reassignment")
+     * @Security("is_granted('ROLE_SUPER_ADMIN')")
+     * @Template("@App/Admin/Tools/report-reasssignment.html.twig")
+     */
+    public function reportAssignmentAction(Request $request)
+    {
+        $form = $this->createForm(ReportReassignmentType::class, null, ['method' => 'POST']);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $submitted = $form->getData();
+
+            try {
+                $this->restClient->post('v2/tools/reassign-reports', json_encode([
+                    'firstClientId' => $submitted['firstClientId'],
+                    'secondClientId' => $submitted['secondClientId'],
+                ]), [], 'response');
+                $this->addFlash('fixture', 'Reports reassigned successfully!');
+            } catch (RestClientException $e) {
+                $this->addFlash('error', $e->getData()['message']);
+            }
+        }
+
+        return ['form' => $form->createView()];
+    }
+}
