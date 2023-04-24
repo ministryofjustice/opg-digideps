ce3be74427f021a24823fcd49fe37a46e5f742ed
James Warren <james.warren@digital.justice.gov.uk>
2023-04-24 12:05:00 +0100

    DDPB-4673 move to BucketOwnerEnforced from ACLs

--- environment/s3_bucket/main.tf
+++ environment/s3_bucket/main.tf
@@ -8,6 +8,13 @@ resource "aws_s3_bucket_acl" "bucket_acl" {
   acl    = var.acl
 }
 
+resource "aws_s3_bucket_ownership_controls" "bucket_object_ownership" {
+  bucket = aws_s3_bucket.bucket.id
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
 resource "aws_s3_bucket_versioning" "bucket_versioning" {
   bucket = aws_s3_bucket.bucket.id
 
