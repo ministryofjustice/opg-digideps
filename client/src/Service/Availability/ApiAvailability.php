4c99009e1edb9f153c1bb137a702a1670dc4c531
James Warren <james.warren@digital.justice.gov.uk>
2023-05-30 16:51:37 +0100

    DDPB-4674 create seperated health check endpoints

--- client/src/Service/Availability/ApiAvailability.php
+++ client/src/Service/Availability/ApiAvailability.php
@@ -16,7 +16,7 @@ class ApiAvailability extends ServiceAvailabilityAbstract
     public function ping()
     {
         try {
-            $data = $this->restClient->get('manage/availability', 'array');
+            $data = $this->restClient->get('health-check/service', 'array');
             // API not healtyh
             if (JSON_ERROR_NONE !== json_last_error() || !isset($data['healthy'])) {
                 $this->isHealthy = false;
