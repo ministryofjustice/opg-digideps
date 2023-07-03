267ffabac9813a0f74d66542d89c6e1266264b9f
Chris Rafferty <chris.rafferty@digital.justice.gov.uk>
2023-07-03 15:40:48 +0100

    DDPB-4687 - Adds fron end form & backend request for imbalance metrics

---
+++ client/src/Service/Csv/ReportImbalanceCsvGenerator.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\Csv;
+
+class ReportImbalanceCsvGenerator
+{
+    const CSV_HEADERS = [
+        'Deputy Type',
+        'No Imbalance Reported',
+        'Imbalance Reported',
+        'Imbalance %',
+        'Total Submitted'
+    ];
+
+    public function __construct(private CsvBuilder $csvBuilder)
+    {}
+
+    public function generateReportImbalanceCsv(array $data): string
+    {
+        $csvRows = [];
+
+        foreach  ($data as $row) {
+            $csvRows = [
+                $row['deputy_type'],
+                $row['no_imbalance'],
+                $row['reported_imbalance'],
+                $row['imbalance_percent'],
+            ];
+        }
+
+        return $this->csvBuilder->buildCsv(self::CSV_HEADERS, $csvRows);
+    }
+}
