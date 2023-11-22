d74f72b5ad215526bf162c57e7e4484e0f8aa73d
jamesrwarren <james.warren@digital.justice.gov.uk>
2023-11-22 14:26:22 +0000

    handle not found exceptions better

---
+++ client/app/src/EventListener/NotFoundExceptionListener.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\EventListener;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Twig\Environment;
+
+class NotFoundExceptionListener
+{
+    private $logger;
+
+    private $twig;
+
+    public function __construct(LoggerInterface $logger, Environment $environment)
+    {
+        $this->logger = $logger;
+        $this->twig = $environment;
+    }
+
+    public function onKernelException(ExceptionEvent $event)
+    {
+        $exception = $event->getThrowable();
+
+        $html = $this->twig->render('bundles/TwigBundle/Exception/error404.html.twig', []);
+
+        if ($exception instanceof NotFoundHttpException) {
+            // Log as a warning instead of an error
+            $this->logger->warning('Not found exception: '.$exception->getMessage());
+
+            $response = new Response($html, 404);
+            $event->setResponse($response);
+            $event->stopPropagation();
+        }
+    }
+}
