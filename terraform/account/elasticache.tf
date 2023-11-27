270ac0b2c586fae93ba544bb83631505a7a9212f
Jim <58939809+jamesrwarren@users.noreply.github.com>
2023-11-27 14:26:47 +0000

    DDLS-69 fix snapshot window conflict (#1469)

--- terraform/account/elasticache.tf
+++ terraform/account/elasticache.tf
@@ -13,6 +13,7 @@ resource "aws_elasticache_replication_group" "cache_api" {
   security_group_ids         = [aws_security_group.cache_api_sg.id]
   snapshot_retention_limit   = 1
   apply_immediately          = local.account.apply_immediately
+  snapshot_window            = "02:00-03:50"
   maintenance_window         = local.account.name == "production" ? "wed:04:00-wed:06:00" : "tue:04:00-tue:06:00"
   at_rest_encryption_enabled = true
   #tfsec:ignore:aws-elasticache-enable-in-transit-encryption - too much of a performance hit. To be re-evaluated.
@@ -48,6 +49,7 @@ resource "aws_elasticache_replication_group" "front_api" {
   security_group_ids         = [aws_security_group.cache_front_sg.id]
   snapshot_retention_limit   = 1
   apply_immediately          = local.account.apply_immediately
+  snapshot_window            = "02:00-03:50"
   maintenance_window         = local.account.name == "production" ? "wed:04:00-wed:06:00" : "tue:04:00-tue:06:00"
   at_rest_encryption_enabled = true
   #tfsec:ignore:aws-elasticache-enable-in-transit-encryption - too much of a performance hit. To be re-evaluated.
