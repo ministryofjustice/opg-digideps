5b4a77e77b266ea153e8214556507afb82495612
Chris Rafferty <chris.rafferty@digital.justice.gov.uk>
2023-12-19 16:07:24 +0000

    DDPB-4769 - Updates existing unit tests to better match refactored class

--- api/app/tests/Unit/Service/RequestIdLoggerProcessorTest.php
+++ api/app/tests/Unit/Service/RequestIdLoggerProcessorTest.php
@@ -1,12 +1,13 @@
 <?php
 
-namespace App\Tests\Unit\Service;
+namespace App\Service;
 
-use App\Service\RequestIdLoggerProcessor;
 use Mockery as m;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\HttpFoundation\ParameterBag;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class RequestIdLoggerProcessorTest extends TestCase
 {
@@ -19,15 +20,24 @@ class RequestIdLoggerProcessorTest extends TestCase
 
     public function setUp(): void
     {
-        $this->container = m::mock('Symfony\Component\DependencyInjection\Container');
-        $this->reqStack = m::mock('Symfony\Component\HttpFoundation\RequestStack');
+        $this->container = m::mock(Container::class);
+        $this->reqStack = m::mock(RequestStack::class);
+        $this->request = m::mock(Request::class);
 
         $this->object = new RequestIdLoggerProcessor($this->container);
     }
 
-    public function testProcessRecordHasNoReqStack()
+    public function testProcessRecordNoReqStack()
     {
-        $this->container->shouldReceive('has')->with('request_stack')->andReturn(false);
+        $this->container->shouldReceive('get')->with('request_stack')->andReturn(false);
+
+        $this->assertEquals($this->record, $this->object->processRecord($this->record));
+    }
+
+    public function testProcessRecordHasNoRequest()
+    {
+        $this->reqStack->shouldReceive('getCurrentRequest')->andReturn(false);
+        $this->container->shouldReceive('get')->with('request_stack')->andReturn($this->reqStack);
 
         $this->assertEquals($this->record, $this->object->processRecord($this->record));
     }
@@ -38,10 +48,8 @@ class RequestIdLoggerProcessorTest extends TestCase
         $request->headers = new ParameterBag();
 
         $this->reqStack->shouldReceive('getCurrentRequest')->andReturn($request);
-
-        $this->container
-            ->shouldReceive('has')->with('request_stack')->andReturn(true)
-            ->shouldReceive('get')->with('request_stack')->andReturn($this->reqStack);
+        $this->reqStack->shouldReceive('has')->with('x-request-id')->andReturn(false);
+        $this->container->shouldReceive('get')->with('request_stack')->andReturn($this->reqStack);
 
         $this->assertEquals($this->record, $this->object->processRecord($this->record));
     }
@@ -53,10 +61,9 @@ class RequestIdLoggerProcessorTest extends TestCase
         $request->headers->set('x-request-id', 'THIS_IS_THE_REQUEST_ID');
 
         $this->reqStack->shouldReceive('getCurrentRequest')->andReturn($request);
-
-        $this->container
-            ->shouldReceive('has')->with('request_stack')->andReturn(true)
-            ->shouldReceive('get')->with('request_stack')->andReturn($this->reqStack);
+        $this->reqStack->shouldReceive('has')->with('x-request-id')->andReturn(true);
+        $this->reqStack->shouldReceive('get')->with('x-request-id')->andReturn('THIS_IS_THE_REQUEST_ID');
+        $this->container->shouldReceive('get')->with('request_stack')->andReturn($this->reqStack);
 
         $this->assertEquals($this->record + ['extra' => ['request_id' => 'THIS_IS_THE_REQUEST_ID']], $this->object->processRecord($this->record));
     }
