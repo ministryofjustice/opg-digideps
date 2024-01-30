77964f7e715a9ad3cd24ccbc118005976f04563f
Jim <58939809+jamesrwarren@users.noreply.github.com>
2024-01-30 11:34:56 +0000

    DDLS-118 create DNS in new management hosted zone (#1517)

--- terraform/environment/dns_public.tf
+++ terraform/environment/dns_public.tf
@@ -1,11 +1,11 @@
 locals {
-  domain         = "complete-deputy-report.service.gov.uk"
-  service_domain = "digideps.opg.service.justice.gov.uk"
+  complete_deputy_report = "complete-deputy-report.service.gov.uk"
+  service_justice_domain = "digideps.opg.service.justice.gov.uk"
 }
 
-# Main complete-deputy-report DNS
+# Main complete-deputy-report DNS in Digideps Production Account
 data "aws_route53_zone" "public" {
-  name     = local.domain
+  name     = local.complete_deputy_report
   provider = aws.dns
 }
 
@@ -48,10 +48,55 @@ resource "aws_route53_record" "www" {
   provider = aws.dns
 }
 
+# New complete-deputy-report DNS in Management Account
+data "aws_route53_zone" "complete_deputy_report" {
+  name     = local.complete_deputy_report
+  provider = aws.management
+}
+
+resource "aws_route53_record" "complete_deputy_report_front" {
+  name    = local.subdomain
+  type    = "A"
+  zone_id = data.aws_route53_zone.complete_deputy_report.id
+
+  alias {
+    evaluate_target_health = false
+    name                   = module.eu_west_1[0].aws_lb_front.dns_name
+    zone_id                = module.eu_west_1[0].aws_lb_front.zone_id
+  }
+  provider = aws.management
+}
+
+resource "aws_route53_record" "complete_deputy_report_admin" {
+  name    = join(".", compact(["admin", local.subdomain]))
+  type    = "A"
+  zone_id = data.aws_route53_zone.complete_deputy_report.id
+
+  alias {
+    evaluate_target_health = false
+    name                   = module.eu_west_1[0].aws_lb_admin.dns_name
+    zone_id                = module.eu_west_1[0].aws_lb_admin.zone_id
+  }
+  provider = aws.management
+}
+
+resource "aws_route53_record" "complete_deputy_report_www" {
+  name    = join(".", compact(["www", local.subdomain]))
+  type    = "A"
+  zone_id = data.aws_route53_zone.complete_deputy_report.id
+
+  alias {
+    evaluate_target_health = false
+    name                   = module.eu_west_1[0].aws_lb_front.dns_name
+    zone_id                = module.eu_west_1[0].aws_lb_front.zone_id
+  }
+  provider = aws.management
+}
+
 # Alternative Service Justice Records
 
 data "aws_route53_zone" "service" {
-  name     = local.service_domain
+  name     = local.service_justice_domain
   provider = aws.management
 }
 
