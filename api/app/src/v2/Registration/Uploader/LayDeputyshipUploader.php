08d83aa3e502fa0caa57a06ebeb4a79958a37e5b
Raffers <Chris.rafferty@digital.justice.gov.uk>
2024-02-20 16:20:16 +0000

    DDLS-45 - Automate CSV Process Investigation (#1536)
    * DDLS-45 - Increases memory limit for process.
    * DDLS-45 - Fix various memory hogging issues
    * DDLS-45 - Removes debugging statements & resets array upon each call
    
    ---------
    
    Co-authored-by: jamesrwarren <james.warren@digital.justice.gov.uk>
    Co-authored-by: Jim <58939809+jamesrwarren@users.noreply.github.com>

--- api/app/src/v2/Registration/Uploader/LayDeputyshipUploader.php
+++ api/app/src/v2/Registration/Uploader/LayDeputyshipUploader.php
@@ -41,7 +41,7 @@ class LayDeputyshipUploader
     public function upload(LayDeputyshipDtoCollection $collection): array
     {
         $this->throwExceptionIfDataTooLarge($collection);
-
+        $this->preRegistrationEntriesByCaseNumber = [];
         $added = 0;
         $errors = [];
 
@@ -52,7 +52,7 @@ class LayDeputyshipUploader
                 try {
                     $caseNumber = strtolower((string) $layDeputyshipDto->getCaseNumber());
                     $this->preRegistrationEntriesByCaseNumber[$caseNumber] = $this->createAndPersistNewPreRegistrationEntity($layDeputyshipDto);
-                    $added++;
+                    ++$added;
                 } catch (PreRegistrationCreationException $e) {
                     $message = sprintf('ERROR IN LINE %d: %s', $index + 2, $e->getMessage());
                     $this->logger->error($message);
@@ -64,11 +64,13 @@ class LayDeputyshipUploader
             $this
                 ->updateReportTypes()
                 ->commitTransactionToDatabase();
+            $this->em->clear();
         } catch (\Throwable $e) {
             $this->logger->error($e->getMessage());
 
             return ['added' => $added, 'errors' => [$e->getMessage()]];
         }
+        $this->logger->warning('reports update in func - '.count($this->reportsUpdated));
 
         return [
             'added' => $added,
@@ -106,6 +108,7 @@ class LayDeputyshipUploader
         $reportCaseNumber = '';
         $currentActiveReportId = null;
         $caseNumbers = array_keys($this->preRegistrationEntriesByCaseNumber);
+        $this->logger->warning('Case number - '.count($caseNumbers));
         $reports = $this->reportRepository->findAllActiveReportsByCaseNumbersAndRole($caseNumbers, User::ROLE_LAY_DEPUTY);
 
         try {
