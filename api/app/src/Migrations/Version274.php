03c6d939795b08e603d557348e7fe21dae8a3df0
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2024-04-15 11:05:12 +0100

    rename migration file to resolve conflict

--- api/app/src/Migrations/Version274.php
+++ api/app/src/Migrations/Version274.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version273 extends AbstractMigration
+final class Version274 extends AbstractMigration
 {
     public function getDescription(): string
     {
