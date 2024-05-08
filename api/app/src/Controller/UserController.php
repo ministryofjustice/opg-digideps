3fd4028cf43dfe998b090a74c6ed2aa6be0f382a
Niladri Das <niladri.das@digital.justice.gov.uk>
2024-05-08 16:39:01 +0100

    DDLS-144 : check for empty deputy UID while populating user data

--- api/app/src/Controller/UserController.php
+++ api/app/src/Controller/UserController.php
@@ -101,8 +101,11 @@ class UserController extends RestController
             'job_title' => 'setJobTitle',
             'co_deputy_client_confirmed' => 'setCoDeputyClientConfirmed',
             'deputy_no' => 'setDeputyNo',
-            'deputy_uid' => 'setDeputyUid',
         ]);
+
+        if (array_key_exists('deputy_uid', $data) && !empty($data['deputy_uid'])) {
+            $user->setDeputyUid($data['deputy_uid']);
+        }
 
         if (array_key_exists('last_logged_in', $data)) {
             $user->setLastLoggedIn(new \DateTime($data['last_logged_in']));
