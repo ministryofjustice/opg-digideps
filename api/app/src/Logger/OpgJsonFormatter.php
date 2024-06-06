a8d9db004089d45c182d4ebbcf56280df2ee519d
jamesrwarren <james.warren@digital.justice.gov.uk>
2024-06-06 16:03:54 +0100

    DDLS-198 use a standardised OPG log formatter

---
+++ api/app/src/Logger/OpgJsonFormatter.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Logger;
+
+use Monolog\Formatter\JsonFormatter as MonologJsonFormatter;
+
+class OpgJsonFormatter extends MonologJsonFormatter
+{
+    public function format(array $record): string
+    {
+        $caller = $this->getCallerInfo();
+        $file = $caller['file'] ?? 'unknown file';
+        $line = $caller['line'] ?? 'unknown line';
+
+        $formattedRecord = [
+            'time' => $record['datetime']->format('Y-m-d\TH:i:s\Z'),
+            'level' => strtoupper($record['level_name']),
+            'msg' => $record['message'],
+            'service_name' => 'api',
+            'request' => [
+                'method' => $_SERVER['REQUEST_METHOD'] ?? 'UNKNOWN',
+                'path' => $_SERVER['REQUEST_URI'] ?? 'UNKNOWN',
+            ],
+            'location' => [
+                'file' => $file,
+                'line' => $line,
+            ],
+        ];
+
+        return $this->toJson($formattedRecord, true).($this->appendNewline ? "\n" : '');
+    }
+
+    private function getCallerInfo(): array
+    {
+        $backtrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+
+        foreach ($backtrace as $trace) {
+            // Files go up stack trace as monolog and finally the name of this file before we get to relevant calling file
+            if (isset($trace['file']) && !str_contains($trace['file'], 'monolog') && !str_contains($trace['file'], 'OpgJsonFormatter.php')) {
+                return [
+                    'file' => $trace['file'],
+                    'line' => $trace['line'],
+                ];
+            }
+        }
+
+        return [];
+    }
+}
