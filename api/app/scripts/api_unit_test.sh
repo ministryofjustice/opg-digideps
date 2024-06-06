02f39b9bf457a61982e168904fc3c03867a93091
jamesrwarren <james.warren@digital.justice.gov.uk>
2024-06-06 16:03:54 +0100

    DDLS-198 use a standardised OPG log formatter

--- api/app/scripts/api_unit_test.sh
+++ api/app/scripts/api_unit_test.sh
@@ -63,6 +63,8 @@ case "$1" in
     php vendor/bin/phpunit -c tests/Unit tests/Unit/Transformer/ --coverage-php tests/coverage/Transformer.cov
     printf '\n Running v2 Suite \n\n'
     php vendor/bin/phpunit -c tests/Unit tests/Unit/v2/ --coverage-php tests/coverage/v2.cov
+    printf '\n Running Logger Suite \n\n'
+    php vendor/bin/phpunit -c tests/Unit tests/Unit/Logger/ --coverage-php tests/coverage/logger.cov
     ;;
   selection-all)
     # Run all three blocks
@@ -90,6 +92,8 @@ case "$1" in
     php vendor/bin/phpunit -c tests/Unit tests/Unit/Transformer/ --coverage-php tests/coverage/Transformer.cov
     printf '\n Running v2 Suite \n\n'
     php vendor/bin/phpunit -c tests/Unit tests/Unit/v2/ --coverage-php tests/coverage/v2.cov
+    printf '\n Running Logger Suite \n\n'
+    php vendor/bin/phpunit -c tests/Unit tests/Unit/Logger/ --coverage-php tests/coverage/logger.cov
     ;;
   *)
     echo "Invalid argument. Please provide one of the following arguments: selection-1, selection-2, selection-3, selection-all"
