8290f10deef6b68e06c15b014b5df32beee987a2
jamesrwarren <james.warren@digital.justice.gov.uk>
2024-06-12 12:00:56 +0100

    try squid proxy on client container

---
+++ squid/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+echo "Initializing cache..."
+$(which squid) -N -f /etc/squid/squid.conf -z
+
+echo "Starting squid..."
+exec $(which squid) -f /etc/squid/squid.conf -NYCd 1 ${EXTRA_ARGS}
