37738a8007a06795caed7ddfab6b894652b85401
Gugandeep Chani <gugandeep.chani@digital.justice.gov.uk>
2024-07-23 10:03:23 +0100

    Updated version to resolve merge conflict

--- api/app/src/Migrations/Version282.php
+++ api/app/src/Migrations/Version282.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version281 extends AbstractMigration
+final class Version282 extends AbstractMigration
 {
     public function getDescription(): string
     {
