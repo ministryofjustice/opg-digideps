7813d47afecbefbdd0b42bf47f456d1597c1758b
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2024-07-24 14:22:24 +0100

    output json file and store in api container to rectify get request permissions to endpoint from the api

--- api/app/src/Service/BankHolidaysAPIService.php
+++ api/app/src/Service/BankHolidaysAPIService.php
@@ -2,31 +2,14 @@
 
 namespace App\Service;
 
-use GuzzleHttp\ClientInterface;
-use GuzzleHttp\Exception\GuzzleException;
-use GuzzleHttp\Psr7\Request;
-
 class BankHolidaysAPIService
 {
-    public const BANK_HOLIDAYS_ENDPOINT = 'https://www.gov.uk/bank-holidays.json';
-    private ClientInterface $httpClient;
+    public const BANK_HOLIDAYS_FILE_LOCATION = '/tmp/bank-holidays.json';
 
-    public function __construct(
-        ClientInterface $httpClient,
-    ) {
-        $this->httpClient = $httpClient;
-    }
-
-    /**
-     * @throws GuzzleException
-     */
     public function getBankHolidays(): array
     {
-        $request = new Request('GET', self::BANK_HOLIDAYS_ENDPOINT);
-
-        $response = $this->httpClient->send($request, ['connect_timeout' => 5, 'timeout' => 5]);
-
-        $jsonDecoded = json_decode((string) $response->getBody(), true);
+        $bankHolidayJson = file_get_contents(self::BANK_HOLIDAYS_FILE_LOCATION);
+        $jsonDecoded = json_decode($bankHolidayJson, true);
 
         $englandAndWalesDates = [];
 
