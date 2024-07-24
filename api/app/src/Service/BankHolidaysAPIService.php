04040d3497f99655a044d0af45942a59e0dd7e75
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2024-07-24 15:52:26 +0100

    throw excepiton if file does not exist

--- api/app/src/Service/BankHolidaysAPIService.php
+++ api/app/src/Service/BankHolidaysAPIService.php
@@ -6,8 +6,15 @@ class BankHolidaysAPIService
 {
     public const BANK_HOLIDAYS_FILE_LOCATION = '/tmp/bank-holidays.json';
 
+    /**
+     * @throws \Exception
+     */
     public function getBankHolidays(): array
     {
+        if (!file_exists(self::BANK_HOLIDAYS_FILE_LOCATION)) {
+            throw new \Exception('Bank holidays file does not exist');
+        }
+
         $bankHolidayJson = file_get_contents(self::BANK_HOLIDAYS_FILE_LOCATION);
         $jsonDecoded = json_decode($bankHolidayJson, true);
 
