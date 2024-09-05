46ca29b48ef5e6e3a1ebcee71501f810847ee25f
jamesrwarren <james.warren@digital.justice.gov.uk>
2024-09-05 10:54:39 +0100

    DDLS-322 add rate limiting

--- orchestration/tests/run-smoke-tests.sh
+++ orchestration/tests/run-smoke-tests.sh
@@ -3,6 +3,8 @@
 set -e
 set -o pipefail
 
+echo "Waiting for 1 minute to allow environment to spin up properly..."
+sleep 60
 echo "===== Running smoke test against Admin ====="
 node tests/smoke-tests/AdminSmokeTest.js
 echo ""
