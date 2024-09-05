220ba6c47775d4aa47cae157e9b1c3f6ee05b780
jamesrwarren <james.warren@digital.justice.gov.uk>
2024-09-05 10:54:39 +0100

    DDLS-322 add rate limiting

--- orchestration/tests/smoke-tests/FrontSmokeTest.js
+++ orchestration/tests/smoke-tests/FrontSmokeTest.js
@@ -17,9 +17,10 @@ const runSmoke = async () => {
   const browser = await puppeteer.launch(
     {
       executablePath: '/usr/bin/chromium-browser',
-      args: ['--no-sandbox', '--headless']
+      args: ['--no-sandbox', '--headless'],
+      protocolTimeout: 30000
     });
-  const page = await browser.newPage();
+    const page = await openPageWithRetries(browser);
 
   try {
     const { admin_user, admin_password, client, deputy_user, deputy_password } = await getSecret(environment, endpoint);
