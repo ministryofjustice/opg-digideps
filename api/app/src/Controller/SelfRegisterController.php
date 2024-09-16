f926065829a6f6344ea3ed896a8cf134ca4eec68
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2024-09-16 09:18:41 +0100

    drop unused attribute

--- api/app/src/Controller/SelfRegisterController.php
+++ api/app/src/Controller/SelfRegisterController.php
@@ -24,7 +24,6 @@ class SelfRegisterController extends RestController
     private RestFormatter $formatter;
 
     private EntityManagerInterface $em;
-    private $serializer;
 
     public function __construct(
         LoggerInterface $logger,
