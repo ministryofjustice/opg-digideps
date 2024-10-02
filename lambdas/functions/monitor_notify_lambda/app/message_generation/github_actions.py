5034541953a608b2599d0e0c3a293caa02445135
Jim <58939809+jamesrwarren@users.noreply.github.com>
2024-10-02 10:18:52 +0100

    DDLS-306 refactor slack lambda (#1678)

---
+++ lambdas/functions/monitor_notify_lambda/app/message_generation/github_actions.py
@@ -0,0 +1,57 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel("INFO")
+
+
+def github_actions_message(message):
+    branch = message["Branch"]
+    logger.info(f"Attempting to process github actions event for branch {branch}")
+
+    workflow_name = message["WorkflowName"]
+    github_actor = message["GhActor"]
+    success = message["Success"]
+    job_url = message["JobUrl"]
+    frontend_url = message["FrontendUrl"]
+    admin_url = message["AdminUrl"]
+    commit_message = message["CommitMessage"]
+    scheduled_task = message["ScheduledTask"]
+
+    path_to_live = True if "Path to live" in workflow_name else False
+
+    status_emoji = ":white_check_mark:" if success == "yes" else ":x:"
+    success_string = "Success" if success == "yes" else "Failure"
+    workflow_type = "Digideps Live Release" if path_to_live else "Digideps Workflow"
+    extra_emoji = ":rocket:" if path_to_live and success == "yes" else ""
+
+    if scheduled_task != "":
+        with open("templates/github_actions_scheduled_task.txt", "r") as file:
+            template_text = file.read()
+
+        formatted_text = template_text.format(
+            scheduled_task=scheduled_task,
+            status_emoji=status_emoji,
+            success_string=success_string,
+            job_url=job_url,
+        )
+    else:
+        with open("templates/github_actions.txt", "r") as file:
+            template_text = file.read()
+
+        formatted_text = template_text.format(
+            workflow_name=workflow_name,
+            workflow_type=workflow_type,
+            extra_emoji=extra_emoji,
+            github_actor=github_actor,
+            status_emoji=status_emoji,
+            success_string=success_string,
+            job_url=job_url,
+            branch=branch,
+            frontend_url=frontend_url,
+            admin_url=admin_url,
+            commit_message=commit_message,
+        )
+
+    payload = {"text": formatted_text, "channel": "default"}
+
+    return payload
---
+++ lambdas/functions/monitor_notify_lambda/app/message_sending/send.py
@@ -0,0 +1,51 @@
+import boto3
+import requests
+import json
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel("INFO")
+
+
+def get_slack_webhook_secret(secret_name, channel_identifier):
+    try:
+        # Initialize the Secrets Manager client
+        client = boto3.client("secretsmanager")
+        response = client.get_secret_value(SecretId=secret_name)
+        secret = response["SecretString"]
+        secret_dict = json.loads(secret)
+        webhook_url = secret_dict.get(channel_identifier)
+        return webhook_url
+    except Exception as e:
+        # Handle any exceptions here
+        logger.error(f"Error: {str(e)}")
+        return None
+
+
+def send_message(payload):
+    # Data for the message
+    data = {"text": payload["text"]}
+    # Send the POST request to the Slack webhook URL
+    webhook_url = get_slack_webhook_secret("slack-webhook-url", payload["channel"])
+    response = requests.post(webhook_url, data=json.dumps(data))
+    # Check the response
+    response_object = {
+        "statusCode": None,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+        },
+        "body": "",
+    }
+    if response.status_code == 200:
+        logger.info(f"Slack message sent")
+        response_object["statusCode"] = 200
+        response_object["body"] = "Event processed successfully"
+    else:
+        logger.warning(
+            f"Failed to send slack message: {response.status_code} - {response.text}"
+        )
+        response_object["statusCode"] = 500
+        response_object["body"] = "Event failed to process"
+
+    return response_object
