7a3c8f166c0de3680cf58c6d4616fba5401c8e25
Iqpal Mannan <iqpal.mannan@digital.justice.gov.uk>
2024-10-17 09:34:12 +0100

    Added behat tests for breadcrumbs

--- api/app/tests/Behat/bootstrap/v2/Common/INavigateToFrontendTrait.php
+++ api/app/tests/Behat/bootstrap/v2/Common/INavigateToFrontendTrait.php
@@ -32,4 +32,48 @@ trait INavigateToFrontendTrait
         $this->clickLink('Complete the deputy report');
         $this->iAmOnChooseAClientMainPage();
     }
+
+    /**
+     * @When /^the Lay deputy navigates back to the Choose a Client homepage using the breadcrumb$/
+     */
+    public function theLayDeputyNavigatesBackToTheChooseAClientHomepageUsingBreadcrumb()
+    {
+        $this->clickLink('Choose a client');
+        $this->iAmOnChooseAClientMainPage();
+    }
+
+    /**
+     * @When /^the Lay deputy navigates to the report overview page$/
+     */
+    public function theLayDeputyNavigatesToTheReportOverviewPage()
+    {
+        $this->clickLink('Continue');
+        $this->iAmOnReportsOverviewPage();
+    }
+
+    /**
+     * @When the Lay Deputy navigates back to the Client dashboard using the breadcrumb
+     */
+    public function theLayDeputynavigatesBackToTheClientDashboardUsingTheBreadcrumb()
+    {
+        $this->clickBasedOnText($this->layPfaHighNotStartedMultiClientDeputyPrimaryUser->getClientFirstName());
+    }
+
+    /**
+     * @When /^the Lay deputy navigates to your details page$/
+     */
+    public function theLayDeputyNavigatesToTheYourDetailsPage()
+    {
+        $this->clickLink('Your details');
+        $this->iAmOnYourDetailsPage();
+    }
+
+    /**
+     * @When /^the Lay deputy navigates to client details page$/
+     */
+    public function theLayDeputyNavigatesToTheClientDetailsPage()
+    {
+        $this->clickLink('Client details');
+        $this->iAmOnClientDetailsPage();
+    }
 }
