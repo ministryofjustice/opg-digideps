55c56fc3e99a35da18f5ec3b476de4ced874685a
Jim <58939809+jamesrwarren@users.noreply.github.com>
2024-11-04 20:58:18 +0000

    DDLS-388 move app logs kms to account (#1716)

--- terraform/account/region/kms_service_logs.tf
+++ terraform/account/region/kms_service_logs.tf
@@ -1,4 +1,6 @@
 ##### Shared KMS key for logs #####
+
+# Account logs encryption
 module "logs_kms" {
   source                  = "./modules/kms_key"
   encrypted_resource      = "Logs"
@@ -13,6 +15,22 @@ module "logs_kms" {
   }
 }
 
+# Application logs encryption
+module "logs_application_kms" {
+  source                  = "./modules/kms_key"
+  encrypted_resource      = "Logs"
+  kms_key_alias_name      = "digideps_application_logs_encryption_key"
+  enable_key_rotation     = true
+  enable_multi_region     = false
+  deletion_window_in_days = 10
+  kms_key_policy          = var.account.name == "development" ? data.aws_iam_policy_document.kms_logs_merged_for_development.json : data.aws_iam_policy_document.kms_logs_merged.json
+  providers = {
+    aws.eu_west_1 = aws.eu_west_1
+    aws.eu_west_2 = aws.eu_west_2
+  }
+}
+
+# Policies
 data "aws_iam_policy_document" "kms_logs_merged_for_development" {
   provider = aws.global
   source_policy_documents = [
