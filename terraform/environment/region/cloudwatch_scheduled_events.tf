d93b1b7c0c34a55904ec938c3bf567821a62d1d7
Jim <58939809+jamesrwarren@users.noreply.github.com>
2024-11-05 12:29:43 +0000

    DDLS-383 add overnight checklist resubmission (#1708)

--- terraform/environment/region/cloudwatch_scheduled_events.tf
+++ terraform/environment/region/cloudwatch_scheduled_events.tf
@@ -189,6 +189,44 @@ resource "aws_cloudwatch_event_target" "resubmit_error_documents" {
   )
 }
 
+# Resubmit re-submittable error checklists
+
+resource "aws_cloudwatch_event_rule" "resubmit_error_checklists" {
+  name                = "resync-resubmittable-error-checklists-${local.environment}"
+  description         = "Resync resubmittable error checklists in ${terraform.workspace}"
+  schedule_expression = "cron(05 08 * * ? *)"
+  tags                = var.default_tags
+}
+
+resource "aws_cloudwatch_event_target" "resubmit_error_checklists" {
+  rule     = aws_cloudwatch_event_rule.resubmit_error_checklists.name
+  arn      = aws_ecs_cluster.main.arn
+  role_arn = aws_iam_role.events_task_runner.arn
+
+  ecs_target {
+    task_count          = 1
+    task_definition_arn = aws_ecs_task_definition.api.arn
+    launch_type         = "FARGATE"
+    platform_version    = "1.4.0"
+
+    network_configuration {
+      security_groups  = [module.api_service_security_group.id]
+      subnets          = data.aws_subnet.private[*].id
+      assign_public_ip = false
+    }
+  }
+  input = jsonencode(
+    {
+      "containerOverrides" : [
+        {
+          "name" : "api_app",
+          "command" : ["sh", "scripts/task_run_console_command.sh", "digideps:resync-resubmittable-error-checklists"]
+        }
+      ]
+    }
+  )
+}
+
 # Redeploy file scanner
 
 data "aws_lambda_function" "redeployer_lambda" {
