733981ea6878c70ba24037a41e5e7a325e5c5ee2
Jim <58939809+jamesrwarren@users.noreply.github.com>
2024-11-14 13:31:27 +0000

    DDLS-398 add s3 kms encryption (#1732)

--- terraform/account/region/secrets.tf
+++ terraform/account/region/secrets.tf
@@ -36,6 +36,7 @@ module "development_environment_secrets" {
 }
 
 # Account wide secrets
+#trivy:ignore:avd-aws-0098 - Complications with updating this secret and not a particularly sensitive secret
 resource "aws_secretsmanager_secret" "cloud9_users" {
   name        = "cloud9-users"
   description = "Digideps team Cloud9 users"
