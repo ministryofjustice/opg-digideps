457d8a027bfe87d17155969555f9560bbeb9ab6c
jamesrwarren <james.warren@digital.justice.gov.uk>
2024-11-18 14:00:46 +0000

    DDLS-398b use account CMK for s3

--- terraform/environment/region/s3_bucket.tf
+++ terraform/environment/region/s3_bucket.tf
@@ -15,7 +15,7 @@ module "pa_uploads" {
   account_name                         = var.account.name
   bucket_name                          = "pa-uploads-${local.environment}"
   force_destroy                        = var.account.force_destroy_bucket
-  kms_key_id                           = aws_kms_key.s3.key_id
+  kms_key_id                           = data.aws_kms_key.s3.key_id
   environment_name                     = local.environment
   enable_lifecycle                     = true
   expiration_days                      = local.expiration_days
--- terraform/environment/region/s3_kms.tf
+++ terraform/environment/region/s3_kms.tf
@@ -9,3 +9,7 @@ resource "aws_kms_alias" "s3" {
   name          = "alias/s3-digideps-${terraform.workspace}"
   target_key_id = aws_kms_key.s3.key_id
 }
+
+data "aws_kms_key" "s3" {
+  key_id = "alias/digideps_s3_encryption_key"
+}
