1f50f3c9c6573cffcdf361168b830959131b36e5
Jim <58939809+jamesrwarren@users.noreply.github.com>
2024-11-25 21:04:20 +0000

    Revert "DDLS-408 add dynamodb kms key part 1 of 2 (#1737)" (#1743)
    This reverts commit 1aabf70869d92f79a7c9d9c8e2d19f1799a6bae2.

--- terraform/account/region/elasticache.tf
+++ terraform/account/region/elasticache.tf
@@ -27,10 +27,9 @@ resource "aws_elasticache_replication_group" "cache_api" {
 }
 
 resource "aws_security_group" "cache_api_sg" {
-  name        = "${var.account.name}-account-cache-api"
-  vpc_id      = aws_vpc.main.id
-  tags        = merge(var.default_tags, { Name = "${var.account.name}-account-cache--api" })
-  description = "cache api - ${var.account.name}"
+  name   = "${var.account.name}-account-cache-api"
+  vpc_id = aws_vpc.main.id
+  tags   = merge(var.default_tags, { Name = "${var.account.name}-account-cache--api" })
 
   lifecycle {
     create_before_destroy = true
@@ -64,10 +63,9 @@ resource "aws_elasticache_replication_group" "front_api" {
 }
 
 resource "aws_security_group" "cache_front_sg" {
-  name        = "${var.account.name}-account-cache-frontend"
-  vpc_id      = aws_vpc.main.id
-  description = "cache front - ${var.account.name}"
-  tags        = merge(var.default_tags, { Name = "${var.account.name}-account-cache-frontend" })
+  name   = "${var.account.name}-account-cache-frontend"
+  vpc_id = aws_vpc.main.id
+  tags   = merge(var.default_tags, { Name = "${var.account.name}-account-cache-frontend" })
 
   lifecycle {
     create_before_destroy = true
