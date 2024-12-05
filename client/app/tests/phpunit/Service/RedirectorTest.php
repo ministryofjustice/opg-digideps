4ed298446dd46b866e11ab42dd01f074369b3076
Iqpal Mannan <iqpal.mannan@digital.justice.gov.uk>
2024-12-05 15:58:08 +0000

    Added in registration route set to co deputy invite for failing redirector test

--- client/app/tests/phpunit/Service/RedirectorTest.php
+++ client/app/tests/phpunit/Service/RedirectorTest.php
@@ -158,7 +158,8 @@ class RedirectorTest extends TestCase
         $isDeputyOrg,
         $clientKnown,
         $hasAddress,
-        $expectedRoute
+        $expectedRoute,
+        $registrationRoute = 'CO_DEPUTY_INVITE'
     ) {
         $this->user->setRoleName($userRole);
 
@@ -168,6 +169,7 @@ class RedirectorTest extends TestCase
         $this->user->shouldReceive('isDeputyOrg')->andReturn($isDeputyOrg);
         $this->user->shouldReceive('getIdOfClientWithDetails')->andReturn($clientKnown);
         $this->user->shouldReceive('hasAddressDetails')->andReturn($hasAddress);
+        $this->user->shouldReceive('getRegistrationRoute')->andReturn($registrationRoute);
         $this->parameterStoreService->shouldReceive('getFeatureFlag')->andReturn('0');
 
         $correctRoute = $this->object->getCorrectRouteIfDifferent($this->user, $currentRoute);
