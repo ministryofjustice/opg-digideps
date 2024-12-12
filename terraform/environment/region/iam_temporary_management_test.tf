da7de2d0657ad8f077293904e36ff38c11ab3d20
jamesrwarren <james.warren@digital.justice.gov.uk>
2024-12-12 15:18:59 +0000

    testing out restricted management role

---
+++ terraform/environment/region/iam_temporary_management_test.tf
@@ -0,0 +1,101 @@
+resource "aws_iam_role" "ci_test" {
+  assume_role_policy = data.aws_iam_policy_document.ci_test_assume_policy.json
+  name               = "digideps-test-ci"
+  tags               = var.default_tags
+  provider           = aws.management
+}
+
+data "aws_iam_policy_document" "ci_test_assume_policy" {
+  statement {
+    sid    = "Trust relations"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::631181914621:role/oidc-digideps-development"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role_policy" "ci_test" {
+  name     = "digideps-test-ci"
+  policy   = data.aws_iam_policy_document.ci_test.json
+  role     = aws_iam_role.ci_test.id
+  provider = aws.management
+}
+
+data "aws_iam_policy_document" "ci_test" {
+  statement {
+    sid    = "Write DNS access"
+    effect = "Allow"
+    actions = [
+      "route53:Change*",
+      "route53:Create*",
+      "route53:Delete*",
+      "route53:Associate*",
+      "route53:Activate*",
+      "route53:Deactivate*",
+    ]
+    resources = [
+      "arn:aws:route53:::hostedzone/Z07336273PD3FH7YGYOLV",
+      "arn:aws:route53:::hostedzone/Z0818402Q9ADP2GK9BOL"
+    ]
+  }
+
+  statement {
+    sid    = "Read DNS"
+    effect = "Allow"
+    actions = [
+      "route53:Get*",
+      "route53:List*"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    sid    = "ECR Allow List"
+    effect = "Allow"
+    actions = [
+      "ecr:List*",
+      "ecr:Describe*"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "ECR Allow Read"
+    effect = "Allow"
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetRepositoryPolicy",
+      "ecr:DescribeRepositories",
+      "ecr:ListImages",
+      "ecr:DescribeImages",
+      "ecr:BatchGetImage",
+      "ecr:GetLifecyclePolicy",
+      "ecr:GetLifecyclePolicyPreview",
+      "ecr:ListTagsForResource",
+      "ecr:DescribeImageScanFindings"
+    ]
+    resources = [
+      "311462405659.dkr.ecr.eu-west-1.amazonaws.com/digideps/*"
+    ]
+  }
+
+  statement {
+    sid    = "GetSecrets"
+    effect = "Allow"
+    actions = [
+      "secretsmanager:GetSecretValue",
+    ]
+    resources = [
+      "arn:aws:secretsmanager:eu-west-1:311462405659:secret:digideps/*"
+    ]
+  }
+}
