310ba0a40f7f4cbafa396bed3ab42e4f79e00144
Chris Rafferty <chris.rafferty@digital.justice.gov.uk>
2024-12-17 16:38:53 +0000

    DDLS-402 - Adds migration script to insert missing deputies from user table

---
+++ api/app/src/Migrations/Version287.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version287 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Adds mirgration script';
+    }
+
+    public function up(Schema $schema): void
+    {
+        if (getenv('RUN_ONE_OFF_MIGRATIONS')) {
+            $sql = <<<SQL
+            INSERT INTO deputy 
+            (user_id, deputy_uid, firstname, lastname, email1, address1, address2, 
+            address3, address4, address5, address_postcode, address_country, phone_main, phone_alternative)
+            SELECT id, deputy_uid, firstname, lastname, email, address1, address2, address3, 
+            address4, address5, address_postcode, address_country, phone_main, phone_alternative
+            FROM dd_user
+            WHERE id IN (
+                SELECT u.id
+                FROM client c
+                LEFT JOIN deputy_case dc ON c.id = dc.client_id
+                LEFT JOIN dd_user u ON dc.user_id = u.id
+                WHERE dc.user_id IN (
+                    SELECT id
+                    FROM dd_user 
+                    WHERE is_primary = TRUE
+                    AND deputy_uid NOT IN (
+                        SELECT deputy_uid 
+                        FROM deputy
+                        )
+                )
+            )
+            SQL;
+
+            $this->addSql($sql);
+        }
+    }
+}
