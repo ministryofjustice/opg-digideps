789e6435fa5ec611003e7cc06f954350ddea3152
jamesrwarren <james.warren@digital.justice.gov.uk>
2024-12-30 16:50:12 +0000

    DDLS-434 attempt different state file format

--- terraform/account/provider.tf
+++ terraform/account/provider.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "opg.terraform.state"
-    key     = "digideps-infrastructure-shared/terraform.tfstate"
+    key     = "opg-digideps-environment/terraform.tfstate"
     encrypt = true
     region  = "eu-west-1"
     assume_role = {
