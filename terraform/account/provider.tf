91b2322d28d9742d0a19d31200dc83ca6c2e6594
jamesrwarren <james.warren@digital.justice.gov.uk>
2025-01-08 09:11:17 +0000

    DDLS-434f swap back to digideps-ci with retricted access

--- terraform/account/provider.tf
+++ terraform/account/provider.tf
@@ -30,7 +30,7 @@ provider "aws" {
     tags = local.default_tags
   }
   assume_role {
-    role_arn     = "arn:aws:iam::311462405659:role/${var.DEFAULT_ROLE_MGMT}"
+    role_arn     = "arn:aws:iam::311462405659:role/${var.DEFAULT_ROLE}"
     session_name = "terraform-session"
   }
 }
@@ -54,7 +54,7 @@ provider "aws" {
     tags = local.default_tags
   }
   assume_role {
-    role_arn     = "arn:aws:iam::311462405659:role/${var.DEFAULT_ROLE_MGMT}"
+    role_arn     = "arn:aws:iam::311462405659:role/${var.DEFAULT_ROLE}"
     session_name = "terraform-session"
   }
 }
