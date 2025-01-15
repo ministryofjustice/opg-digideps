f63a1ec5727abe9e62a08c53e00582717d99e29f
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-01-15 14:29:51 +0000

    Generate HTML version of client unit test coverage

--- client/app/scripts/client-unit-tests.sh
+++ client/app/scripts/client-unit-tests.sh
@@ -5,3 +5,7 @@ set -e
 php vendor/bin/phpunit -c tests/phpunit --coverage-php tests/phpunit/coverage/client-unit-tests.cov
 
 php vendor/phpunit/phpcov/phpcov merge --clover "./tests/phpunit/coverage/client-unit-tests.xml" "./tests/phpunit/coverage"
+
+php vendor/phpunit/phpcov/phpcov merge --html "./tests/phpunit/coverage/client-unit-tests" "./tests/phpunit/coverage"
+
+zip -r client-unit-tests.html.zip ./tests/phpunit/coverage/client-unit-tests
