71921b584b5a1ab535f2ca34a84d4007df4a9f0e
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-01-17 17:21:29 +0000

    Generating HTML coverage report needs more memory

--- api/app/scripts/api_unit_test.sh
+++ api/app/scripts/api_unit_test.sh
@@ -100,7 +100,9 @@ case "$1" in
     printf '\n Running Logger Suite \n\n'
     php vendor/bin/phpunit -c tests/Unit tests/Unit/Logger/ --coverage-php tests/coverage/logger.cov
 
-    php vendor/phpunit/phpcov/phpcov merge --html "./build/coverage-api" "./tests/coverage"
+    # generate HTML coverage report
+    cp -a ./tests/coverage ./build/api-coverage-raw
+    php -d memory_limit=256M vendor/phpunit/phpcov/phpcov merge --html "./build/coverage-api" "./tests/coverage"
     ;;
   *)
     echo "Invalid argument. Please provide one of the following arguments: selection-1, selection-2, selection-3, selection-all"
