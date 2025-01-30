4fad0ebd12f2a2597582f3077113924d4afb0f24
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-01-30 12:21:09 +0000

    Rename migration to prevent merge conflict

--- api/app/src/Migrations/Version290.php
+++ api/app/src/Migrations/Version290.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version289 extends AbstractMigration
+final class Version290 extends AbstractMigration
 {
     public function getDescription(): string
     {
