8d9ee9d3c258a2b51ad2ba7fc2531f9e58d3ea9e
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-02-04 17:03:21 +0000

    Extend test to co-deputy sign up

--- api/app/tests/Behat/bootstrap/v2/Registration/RegistrationFeatureContext.php
+++ api/app/tests/Behat/bootstrap/v2/Registration/RegistrationFeatureContext.php
@@ -6,12 +6,14 @@ use App\Tests\Behat\Common\AuthenticationTrait;
 use App\Tests\Behat\Common\FormTrait;
 use App\Tests\Behat\v2\AdminManagement\AdminManagementTrait;
 use App\Tests\Behat\v2\Common\BaseFeatureContext;
+use App\Tests\Behat\v2\Common\FixturesTrait;
 
 class RegistrationFeatureContext extends BaseFeatureContext
 {
     use ActivateTrait;
     use AdminManagementTrait;
     use AuthenticationTrait;
+    use FixturesTrait;
     use FormTrait;
     use IngestTrait;
     use SelfRegistrationTrait;
