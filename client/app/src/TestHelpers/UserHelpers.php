9a945ed93eaa7432ee7c8c0121f36f8f400800b0
iqpalm <62601059+iqpalm@users.noreply.github.com>
2025-02-04 10:48:03 +0000

    DDLS-465 Case missing deputy_uid (#1805)
    * Ensure deputy uid is present when returning deputy for client
    
    * Missing deputy uid now required for client deleted event test
    
    * Fix failing pre registration codeputy fixture
    
    ---------
    
    Co-authored-by: Iqpal Mannan <iqpal.mannan@digital.justice.gov.uk>

--- client/app/src/TestHelpers/UserHelpers.php
+++ client/app/src/TestHelpers/UserHelpers.php
@@ -15,7 +15,7 @@ class UserHelpers extends KernelTestCase
      */
     public static function createUser(?array $data = null)
     {
-        $container = (self::bootKernel())->getContainer();
+        $container = self::bootKernel()->getContainer();
         $serializer = $container->get('serializer');
         $faker = Faker\Factory::create();
 
@@ -25,6 +25,7 @@ class UserHelpers extends KernelTestCase
 
         return (new User())
             ->setId($faker->numberBetween(1, 999999999))
+            ->setDeputyUid($faker->randomNumber(8))
             ->setFirstname($faker->firstName())
             ->setLastname($faker->lastName())
             ->setRoleName($faker->jobTitle())
@@ -36,7 +37,7 @@ class UserHelpers extends KernelTestCase
      */
     public static function createLayUser()
     {
-        return (self::createUser())->setRoleName(User::ROLE_LAY_DEPUTY);
+        return self::createUser()->setRoleName(User::ROLE_LAY_DEPUTY);
     }
 
     public static function createInvitedCoDeputy(): User
@@ -54,56 +55,56 @@ class UserHelpers extends KernelTestCase
 
     public static function createAdminUser(): User
     {
-        return (self::createUser())->setRoleName(User::ROLE_ADMIN)->setIsCaseManager(true);
+        return self::createUser()->setRoleName(User::ROLE_ADMIN)->setIsCaseManager(true);
     }
 
     public static function createSuperAdminUser(): User
     {
-        return (self::createUser())->setRoleName(User::ROLE_SUPER_ADMIN);
+        return self::createUser()->setRoleName(User::ROLE_SUPER_ADMIN);
     }
 
     public static function createAdminManager(): User
     {
-        return (self::createUser())->setRoleName(User::ROLE_ADMIN_MANAGER)->setIsCaseManager(true);
+        return self::createUser()->setRoleName(User::ROLE_ADMIN_MANAGER)->setIsCaseManager(true);
     }
 
     public static function createProfDeputyUser(): User
     {
-        return (self::createUser())->setRoleName(User::ROLE_PROF);
+        return self::createUser()->setRoleName(User::ROLE_PROF);
     }
 
     public static function createProfNamedDeputyUser(): User
     {
-        return (self::createUser())->setRoleName(User::ROLE_PROF_NAMED);
+        return self::createUser()->setRoleName(User::ROLE_PROF_NAMED);
     }
 
     public static function createProfAdminUser(): User
     {
-        return (self::createUser())->setRoleName(User::ROLE_PROF_ADMIN);
+        return self::createUser()->setRoleName(User::ROLE_PROF_ADMIN);
     }
 
     public static function createProfTeamMemberUser(): User
     {
-        return (self::createUser())->setRoleName(User::ROLE_PROF_TEAM_MEMBER);
+        return self::createUser()->setRoleName(User::ROLE_PROF_TEAM_MEMBER);
     }
 
     public static function createPaDeputyUser(): User
     {
-        return (self::createUser())->setRoleName(User::ROLE_PA);
+        return self::createUser()->setRoleName(User::ROLE_PA);
     }
 
     public static function createPaNamedDeputyUser(): User
     {
-        return (self::createUser())->setRoleName(User::ROLE_PA_NAMED);
+        return self::createUser()->setRoleName(User::ROLE_PA_NAMED);
     }
 
     public static function createPaAdminUser(): User
     {
-        return (self::createUser())->setRoleName(User::ROLE_PA_ADMIN);
+        return self::createUser()->setRoleName(User::ROLE_PA_ADMIN);
     }
 
     public static function createPaTeamMemberUser(): User
     {
-        return (self::createUser())->setRoleName(User::ROLE_PA_TEAM_MEMBER);
+        return self::createUser()->setRoleName(User::ROLE_PA_TEAM_MEMBER);
     }
 }
