b99e76b1d7ce2b6432e1bcf725ad19c865a02e11
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-02-05 17:37:47 +0000

    Add test for discharging a client from a multi-client deputy

--- api/app/tests/Behat/bootstrap/v2/Registration/RegistrationFeatureContext.php
+++ api/app/tests/Behat/bootstrap/v2/Registration/RegistrationFeatureContext.php
@@ -4,6 +4,8 @@ namespace App\Tests\Behat\v2\Registration;
 
 use App\Tests\Behat\Common\AuthenticationTrait;
 use App\Tests\Behat\Common\FormTrait;
+use App\Tests\Behat\Common\SiteNavigationTrait;
+use App\Tests\Behat\CourtOrderManagement\CourtOrderManagementTrait;
 use App\Tests\Behat\v2\AdminManagement\AdminManagementTrait;
 use App\Tests\Behat\v2\Common\BaseFeatureContext;
 use App\Tests\Behat\v2\Common\FixturesTrait;
@@ -13,8 +15,10 @@ class RegistrationFeatureContext extends BaseFeatureContext
     use ActivateTrait;
     use AdminManagementTrait;
     use AuthenticationTrait;
+    use CourtOrderManagementTrait;
     use FixturesTrait;
     use FormTrait;
     use IngestTrait;
     use SelfRegistrationTrait;
+    use SiteNavigationTrait;
 }
