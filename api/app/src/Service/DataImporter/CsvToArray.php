beae15e8e9223b2c4ee3bf63bcc6190285718f28
Raffers <Chris.rafferty@digital.justice.gov.uk>
2025-02-11 14:51:19 +0000

    DDLS-456 - Set up infrastructure for importing court order CSV provided by Sirius (#1809)
    * DDLS-456 - Creates command to pull CourtOrders CSV from S3
    
    * DDLS-456 - Refactors CsvToArray to be more robust
    
    * DDLS-456 - Adds invalid csv handling to CsvToArray
    
    * DDLS-456 - Refactors Lay/Org commands to refactored CsvToArray
    
    * DDLS-456 - Creates skeleton command for CourtOrders, & adjusts test csv to be more complete
    
    * DDLS-456 - Adds tests for new CourtOrderCSVCommand
    
    * DDLS-456 - Reintroduces OptionalCoulmns to CsvToArray, so it is more robust.
    
    * DDLS-456 - Moves CSV file to correct test location
    
    * DDLS-456 - Fixes tests & adjusts output from command
    
    * DDLS-456 - Fixes tests & adjusts output from command
    
    * DDLS-456 - Fixes var name
    
    * Use League\CSV to process CSV files
    
    * Correctly convert iterator to array
    
    * DDLS-456 - Adds messaging to assertions on failing test
    
    * DDLS-456 - Changes incorrect var names
    
    ---------
    
    Co-authored-by: Elliot Smith <elliot.smith2@justice.gov.uk>

--- api/app/src/Service/DataImporter/CsvToArray.php
+++ api/app/src/Service/DataImporter/CsvToArray.php
@@ -2,152 +2,57 @@
 
 namespace App\Service\DataImporter;
 
-use RuntimeException;
+use League\Csv\Exception;
+use League\Csv\Reader;
+use League\Csv\UnavailableStream;
 
 class CsvToArray
 {
-    const DELIMITER = ',';
-    const ENCLOSURE = '"';
-    const ESCAPE = '\\';
-    const CHAR_LIMIT_PER_ROW = 2000; // current average is around the 300-400 chars
-
-    /**
-     * @var resource|false
-     */
-    private $handle;
-
-    /**
-     * @var array
-     */
-    private array $expectedColumns = [];
-
-    /**
-     * Columns that we definitely dont expect.
-     * (those that are present that would indicate the wrong CSV is being used).
-     */
-    private array $unexpectedColumns = [];
-
-    private array $optionalColumns = [];
-
-    private array $firstRow = [];
-
-    public function __construct(string $file, bool $normaliseNewLines)
-    {
-        $this->normaliseNewLines = $normaliseNewLines;
-
-        if (!file_exists($file)) {
-            throw new RuntimeException("file $file not found");
-        }
-
-        $this->handle = fopen($file, 'r');
+    public function __construct(
+        private readonly array $expectedColumns = [],
+        private readonly array $optionalColumns = [],
+    ) {
     }
 
-    public function setExpectedColumns(array $expectedColumns)
+    private function keepExpectedAndOptionalColumns(array $record)
     {
-        $this->expectedColumns = $expectedColumns;
+        $trimmedRecord = [];
 
-        return $this;
-    }
-
-    public function setUnexpectedColumns(array $unexpectedColumns)
-    {
-        $this->unexpectedColumns = $unexpectedColumns;
-
-        return $this;
-    }
-
-    public function setOptionalColumns(array $optionalColumns)
-    {
-        $this->optionalColumns = $optionalColumns;
-
-        return $this;
-    }
-
-    /**
-     * @return array|false|null returns false when EOF
-     */
-    private function getRow(): array|false|null
-    {
-        if (!empty($this->handle)) {
-            return fgetcsv($this->handle, self::CHAR_LIMIT_PER_ROW, self::DELIMITER, self::ENCLOSURE, self::ESCAPE);
-        }
-
-        throw new RuntimeException('Resource handle empty');
-    }
-
-    /**
-     * @return array
-     */
-    public function getFirstRow(): array
-    {
-        if (empty($this->firstRow)) {
-            $this->firstRow = $this->getRow();
-        }
-
-        return $this->firstRow;
-    }
-
-    /**
-     * Returns.
-     *
-     * @return array
-     */
-    public function getData(): array
-    {
-        $ret = [];
-
-        // parse header
-        $header = $this->getFirstRow();
-        if (!$header) {
-            throw new RuntimeException('Empty or corrupted file, cannot parse CSV header');
-        }
-       $missingColumns = array_diff($this->expectedColumns, $header);
-       if ($missingColumns) {
-           throw new RuntimeException('Invalid file. Cannot find expected header columns: '.implode(', ', $missingColumns));
-       }
-
-        $rogueColumns = array_intersect($header, $this->unexpectedColumns);
-        if (!empty($rogueColumns)) {
-            throw new RuntimeException('Invalid file. File contains unexpected header columns: '.implode(', ', $rogueColumns));
-        }
-
-        // read rows
-        $rowNumber = 1;
-        while (($row = $this->getRow()) !== false) {
-            ++$rowNumber;
-            $rowArray = [];
-            foreach ($this->expectedColumns as $expectedColumn) {
-                if (empty($header)) {
-                    throw new RuntimeException('Empty header in CSV file');
-                }
-                $index = array_search($expectedColumn, $header);
-                if (false !== $index && !empty($row)) {
-                    if (!array_key_exists($index, $row)) {
-                        throw new RuntimeException("Can't find $expectedColumn column in line $rowNumber");
-                    }
-                    $rowArray[$expectedColumn] = $row[$index];
-                }
+        foreach ($record as $column => $value) {
+            if (in_array($column, $this->expectedColumns) || in_array($column, $this->optionalColumns)) {
+                $trimmedRecord[$column] = $value;
             }
-            foreach ($this->optionalColumns as $optionalColumn) {
-                $index = array_search($optionalColumn, $header);
-                if (false !== $index) {
-                    // fix for CSV with last two columns being empty and not having commas
-                    if (!isset($row[$index])) {
-                        $row[$index] = '';
-                    }
-                    $rowArray[$optionalColumn] = $row[$index];
-                }
-            }
-            $ret[] = $rowArray;
         }
 
-        return $ret;
+        return $trimmedRecord;
     }
 
-    public function __destruct()
+    /**
+     * Convert CSV into an array of associative arrays in format [fieldname => fieldvalue, ...].
+     * If the CSV header is missing any expected column, an exception is thrown.
+     * The returned arrays only contain expected and optional columns.
+     */
+    public function create(string $filename): array
     {
-        if (false !== $this->handle) {
-            fclose($this->handle);
+        try {
+            $reader = Reader::createFromPath($filename);
+            $reader->setHeaderOffset(0);
+        } catch (UnavailableStream) {
+            throw new \RuntimeException("file {$filename} not found");
         }
+
+        // check for all expected columns
+        if (!empty(array_diff($this->expectedColumns, $reader->getHeader()))) {
+            throw new \RuntimeException("CSV file $filename does not contain all expected columns in header");
+        }
+
+        try {
+            $records = $reader->getRecords();
+        } catch (Exception) {
+            throw new \RuntimeException('Malformed row within file or invalid CSV');
+        }
+
+        // only keep expected and optional columns
+        return array_map([$this, 'keepExpectedAndOptionalColumns'], iterator_to_array($records));
     }
 }
