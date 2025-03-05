3f2bc8dd1bf588f863438fd65c7ce8f226c4814c
Iqpal Mannan <iqpal.mannan@digital.justice.gov.uk>
2025-03-05 10:36:51 +0000

    Amend bootstrap files

--- api/app/tests/Integration/bootstrap.php
+++ api/app/tests/Integration/bootstrap.php
@@ -1,10 +1,5 @@
 <?php
 
-require __DIR__.'/../../vendor/autoload.php';
-
-require __DIR__.'/MockeryStub.php';
-require __DIR__.'/Controller/AbstractTestController.php';
-
 // keep aligned with API_SECRETS_*_KEY env var (digi-deps-local-dev repo)
 define('API_TOKEN_DEPUTY', getenv('SECRETS_FRONT_KEY'));
 define('API_TOKEN_ADMIN', getenv('SECRETS_ADMIN_KEY'));
