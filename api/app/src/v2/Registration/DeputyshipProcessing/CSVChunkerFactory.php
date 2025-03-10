47352be8291c74067f6c18d6a447b1028ce97043
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-03-10 16:26:06 +0000

    Add CSV processor

---
+++ api/app/src/v2/Registration/DeputyshipProcessing/CSVChunkerFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\v2\Registration\DeputyshipProcessing;
+
+use League\Csv\Exception as CSVException;
+use League\Csv\Reader;
+use League\Csv\UnavailableStream;
+
+class CSVChunkerFactory
+{
+    /**
+     * @throws UnavailableStream
+     * @throws CSVException
+     */
+    public function create(string $fileLocation, string $entityClass, int $chunkSize = 10000): CSVChunker
+    {
+        $reader = Reader::createFromPath($fileLocation);
+        $csvFile = $reader->getRecordsAsObject($entityClass);
+
+        return new CSVChunker($csvFile, $chunkSize);
+    }
+}
--- api/app/src/v2/Registration/DeputyshipProcessing/DeputyshipsCSVLoader.php
+++ api/app/src/v2/Registration/DeputyshipProcessing/DeputyshipsCSVLoader.php
@@ -4,10 +4,56 @@ declare(strict_types=1);
 
 namespace App\v2\Registration\DeputyshipProcessing;
 
+use App\Entity\StagingDeputyship;
+use Doctrine\DBAL\Exception;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Exception\ORMException;
+use Doctrine\Persistence\Mapping\MappingException;
+use League\Csv\Exception as CSVException;
+use League\Csv\UnavailableStream;
+use Psr\Log\LoggerInterface;
+
 class DeputyshipsCSVLoader
 {
+    public function __construct(
+        private readonly EntityManager $em,
+        private readonly CSVChunkerFactory $chunkerFactory,
+        private readonly LoggerInterface $logger,
+    ) {
+    }
+
     public function load(string $fileLocation): bool
     {
-        return true;
+        $conn = $this->em->getConnection();
+        $stagingDeputyshipTableName = $this->em->getClassMetadata(StagingDeputyship::class)->getTableName();
+
+        try {
+            // delete records from staging table
+            $this->em->beginTransaction();
+            $conn->executeStatement("DELETE FROM {$stagingDeputyshipTableName}");
+            $this->em->commit();
+
+            // dump CSV into staging table
+            $chunker = $this->chunkerFactory->create($fileLocation, StagingDeputyship::class);
+
+            $this->em->beginTransaction();
+            while (!is_null($chunk = $chunker->getChunk())) {
+                foreach ($chunk as $record) {
+                    $this->em->persist($record);
+                }
+                $this->em->flush();
+            }
+            $this->em->commit();
+            $this->em->clear();
+
+            return true;
+        } catch (ORMException|MappingException|Exception|UnavailableStream|CSVException $e) {
+            $this->logger->error(
+                'Error loading CSV into staging table: exception type = '.get_class($e).'; message = '.$e->getMessage()
+            );
+            $this->em->rollback();
+
+            return false;
+        }
     }
 }
