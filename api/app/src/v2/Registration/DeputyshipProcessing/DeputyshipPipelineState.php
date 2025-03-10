d81a7b9125e038425f922fe4005c9e0a1128f206
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-03-10 16:26:06 +0000

    Add CSV processor

--- api/app/src/v2/Registration/DeputyshipProcessing/DeputyshipPipelineState.php
+++ api/app/src/v2/Registration/DeputyshipProcessing/DeputyshipPipelineState.php
@@ -4,16 +4,16 @@ declare(strict_types=1);
 
 namespace App\v2\Registration\DeputyshipProcessing;
 
-use App\v2\Registration\DTO\DeputyshipRowDto;
+use App\Entity\StagingDeputyship;
 use App\v2\Registration\Enum\DeputyshipProcessingStatus;
 
 class DeputyshipPipelineState
 {
-    public DeputyshipRowDto $deputyShipRowDto;
+    public StagingDeputyship $deputyShipRowDto;
     public DeputyshipProcessingStatus $status;
 
     public function __construct(
-        DeputyshipRowDto $deputyShipRowDto,
+        StagingDeputyship $deputyShipRowDto,
         DeputyshipProcessingStatus $status = DeputyshipProcessingStatus::NOT_STARTED,
     ) {
         $this->deputyShipRowDto = $deputyShipRowDto;
