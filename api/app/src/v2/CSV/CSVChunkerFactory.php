927dcf7ce4530115ceba1ed0537b0dc617e6d4e4
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-03-11 14:51:07 +0000

    Reset index of the iterator returned by CSV reader

--- api/app/src/v2/CSV/CSVChunkerFactory.php
+++ api/app/src/v2/CSV/CSVChunkerFactory.php
@@ -20,6 +20,10 @@ class CSVChunkerFactory
         $reader->setHeaderOffset(0);
         $csvFile = $reader->getRecordsAsObject($entityClass);
 
+        // because the header is in row 0, the first element in $csvFile has an index of 1, so
+        // we have to move the pointer to that element in the iterator (feels like a bug in League\Csv to me...)
+        $csvFile->rewind();
+
         return new CSVChunker($csvFile, $chunkSize);
     }
 }
