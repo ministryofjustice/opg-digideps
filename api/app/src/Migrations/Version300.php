4254d3414f09a7c685cf1c894f8fd170f8afb4e7
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-03-18 09:06:45 +0000

    Update migration to new fieldnames

--- api/app/src/Migrations/Version300.php
+++ api/app/src/Migrations/Version300.php
@@ -20,7 +20,7 @@ final class Version300 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('CREATE SCHEMA staging');
-        $this->addSql('CREATE TABLE staging.deputyship (OrderUid VARCHAR(30) NOT NULL, DeputyUid VARCHAR(30) NOT NULL, OrderType VARCHAR(30) DEFAULT NULL, OrderSubType VARCHAR(30) DEFAULT NULL, OrderMadeDate VARCHAR(30) DEFAULT NULL, OrderStatus VARCHAR(30) DEFAULT NULL, OrderUpdatedDate VARCHAR(30) DEFAULT NULL, CaseNumber VARCHAR(30) DEFAULT NULL, ClientUid VARCHAR(30) DEFAULT NULL, ClientStatus VARCHAR(30) DEFAULT NULL, ClientStatusDate VARCHAR(30) DEFAULT NULL, DeputyType VARCHAR(30) DEFAULT NULL, DeputyStatusOnOrder VARCHAR(30) DEFAULT NULL, DeputyStatusChangeDate VARCHAR(30) DEFAULT NULL, ReportType VARCHAR(30) DEFAULT NULL, IsHybrid VARCHAR(30) DEFAULT NULL, PRIMARY KEY(OrderUid, DeputyUid))');
+        $this->addSql('CREATE TABLE staging.deputyship (order_uid VARCHAR(30) NOT NULL, deputy_uid VARCHAR(30) NOT NULL, order_type VARCHAR(30) DEFAULT NULL, order_sub_type VARCHAR(30) DEFAULT NULL, order_made_date VARCHAR(30) DEFAULT NULL, order_status VARCHAR(30) DEFAULT NULL, order_updated_date VARCHAR(30) DEFAULT NULL, case_number VARCHAR(30) DEFAULT NULL, client_uid VARCHAR(30) DEFAULT NULL, client_status VARCHAR(30) DEFAULT NULL, client_status_date VARCHAR(30) DEFAULT NULL, deputy_type VARCHAR(30) DEFAULT NULL, deputy_status_on_order VARCHAR(30) DEFAULT NULL, deputy_status_change_date VARCHAR(30) DEFAULT NULL, report_type VARCHAR(30) DEFAULT NULL, is_hybrid VARCHAR(30) DEFAULT NULL, PRIMARY KEY(order_uid, deputy_uid))');
     }
 
     public function down(Schema $schema): void
