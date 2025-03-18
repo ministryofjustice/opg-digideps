87bd8f87cfcb8e2cf69f4b14cbaf9e00ddb4bd57
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-03-18 14:10:09 +0000

    Match client statuses to Sirius

--- api/app/src/v2/Registration/Enum/ClientStatus.php
+++ api/app/src/v2/Registration/Enum/ClientStatus.php
@@ -6,6 +6,13 @@ namespace App\v2\Registration\Enum;
 
 enum ClientStatus: string
 {
-    case DEATH_CONFIRMED = 'DEATH_CONFIRMED';
-    case ACTIVE = 'ACTIVE';
+    case Open = 'OPEN';
+    case Active = 'ACTIVE';
+    case Inactive = 'INACTIVE';
+    case Deceased = 'DECEASED';
+    case Closed = 'CLOSED';
+    case Duplicate = 'DUPLICATE';
+    case RegainedCapacity = 'REGAINED_CAPACITY';
+    case DeathNotified = 'DEATH_NOTIFIED';
+    case DeathConfirmed = 'DEATH_CONFIRMED';
 }
