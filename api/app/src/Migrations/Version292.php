4b02929aea6ba432b7c6366c79985e0d5bb2ddae
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-03-18 12:41:21 +0000

    Move migration to later in the chain

--- api/app/src/Migrations/Version292.php
+++ api/app/src/Migrations/Version292.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version291 extends AbstractMigration
+final class Version292 extends AbstractMigration
 {
     public function getDescription(): string
     {
