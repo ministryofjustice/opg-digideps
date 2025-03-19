848c6c0a01d6da298da1423bf672437b36b63598
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-03-19 16:19:29 +0000

    Only create staging schema if it doesn't exist

--- api/app/src/Migrations/Version292.php
+++ api/app/src/Migrations/Version292.php
@@ -19,7 +19,7 @@ final class Version292 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('CREATE SCHEMA staging');
+        $this->addSql('CREATE SCHEMA IF NOT EXISTS staging');
         $this->addSql('CREATE TABLE staging.deputyship (order_uid VARCHAR(30) NOT NULL, deputy_uid VARCHAR(30) NOT NULL, order_type VARCHAR(30) DEFAULT NULL, order_sub_type VARCHAR(30) DEFAULT NULL, order_made_date VARCHAR(30) DEFAULT NULL, order_status VARCHAR(30) DEFAULT NULL, order_updated_date VARCHAR(30) DEFAULT NULL, case_number VARCHAR(30) DEFAULT NULL, client_uid VARCHAR(30) DEFAULT NULL, client_status VARCHAR(30) DEFAULT NULL, client_status_date VARCHAR(30) DEFAULT NULL, deputy_type VARCHAR(30) DEFAULT NULL, deputy_status_on_order VARCHAR(30) DEFAULT NULL, deputy_status_change_date VARCHAR(30) DEFAULT NULL, report_type VARCHAR(30) DEFAULT NULL, is_hybrid VARCHAR(30) DEFAULT NULL, PRIMARY KEY(order_uid, deputy_uid))');
     }
 
