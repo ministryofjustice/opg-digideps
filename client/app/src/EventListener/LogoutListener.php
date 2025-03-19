6d59bac46583f42f190738582543ddaea46b8146
jamesrwarren <james.warren@digital.justice.gov.uk>
2025-03-19 11:44:52 +0000

    DDLS-524 make logout eventlistener

--- client/app/src/EventListener/LogoutListener.php
+++ client/app/src/EventListener/LogoutListener.php
@@ -4,28 +4,16 @@ namespace App\EventListener;
 
 use App\Service\Client\RestClientInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
-use Symfony\Component\Security\Http\Logout\LogoutSuccessHandlerInterface;
+use Symfony\Component\Security\Http\Event\LogoutEvent;
 
-class LogoutListener implements LogoutSuccessHandlerInterface
+class LogoutListener
 {
-    /**
-     * @var TokenStorageInterface
-     */
-    private $tokenStorage;
-
-    /**
-     * @var RouterInterface
-     */
-    private $router;
-
-    /**
-     * @var RestClientInterface
-     */
-    private $restClient;
+    private TokenStorageInterface $tokenStorage;
+    private RestClientInterface $restClient;
+    private RouterInterface $router;
 
     public function __construct(TokenStorageInterface $tokenStorage, RestClientInterface $restClient, RouterInterface $router)
     {
@@ -34,22 +22,24 @@ class LogoutListener implements LogoutSuccessHandlerInterface
         $this->router = $router;
     }
 
-    public function onLogoutSuccess(Request $request)
+    public function onLogout(LogoutEvent $event): void
     {
+        $request = $event->getRequest();
+
+        // Handle token-based logout
         if ($this->tokenStorage->getToken() instanceof UsernamePasswordToken) {
             $this->restClient->logout();
         }
 
+        // Handle session value setting (merged from AuthenticationHandler)
         $notPrimaryAccount = $request->query->get('notPrimaryAccount');
 
         if (!$notPrimaryAccount) {
             $request->getSession()->set('loggedOutFrom', 'logoutPage');
         }
 
-        return new RedirectResponse(
-            $this->router->generate(
-                'login'
-            )
-        );
+        // Set the redirect response
+        $response = new RedirectResponse($this->router->generate('login'));
+        $event->setResponse($response);
     }
 }
