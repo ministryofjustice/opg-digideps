24c6a3420ec979711011814714afa722eb9c68aa
ramchale <legionsofbob@gmail.com>
2025-03-21 15:54:30 +0000

    Convert API symfony annotations to attributes Controller

--- api/app/src/Controller/SelfRegisterController.php
+++ api/app/src/Controller/SelfRegisterController.php
@@ -13,9 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-/**
- * @Route("/selfregister")
- */
+#[Route(path: '/selfregister')]
 class SelfRegisterController extends RestController
 {
     public function __construct(private readonly LoggerInterface $logger, private readonly ValidatorInterface $validator, private readonly AuthService $authService, private readonly RestFormatter $formatter, private readonly EntityManagerInterface $em)
@@ -23,9 +21,7 @@ class SelfRegisterController extends RestController
         parent::__construct($em);
     }
 
-    /**
-     * @Route("", methods={"POST"})
-     */
+    #[Route(path: '', methods: ['POST'])]
     public function register(Request $request, UserRegistrationService $userRegistrationService)
     {
         if (!$this->authService->isSecretValid($request)) {
@@ -72,9 +68,7 @@ class SelfRegisterController extends RestController
         return $user;
     }
 
-    /**
-     * @Route("/verifycodeputy", methods={"POST"})
-     */
+    #[Route(path: '/verifycodeputy', methods: ['POST'])]
     public function verifyCoDeputy(Request $request, UserRegistrationService $userRegistrationService)
     {
         if (!$this->authService->isSecretValid($request)) {
@@ -122,9 +116,7 @@ class SelfRegisterController extends RestController
         return ['verified' => $coDeputyVerified, 'coDeputyUid' => $coDeputyUid, 'existingDeputyAccounts' => $existingDeputyAccounts];
     }
 
-    /**
-     * @Route("/updatecodeputy/{userId}", requirements={"userId":"\d+"}, methods={"PUT"})
-     */
+    #[Route(path: '/updatecodeputy/{userId}', requirements: ['userId' => '\d+'], methods: ['PUT'])]
     public function updateCoDeputyWithVerificationData(Request $request, $userId): User
     {
         $user = $this->em->getRepository('App\Entity\User')->findOneBy(['id' => $userId]);
