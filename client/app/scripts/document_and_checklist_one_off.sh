6873439fa75365c7b6c92158f313433c0d26b55f
jamesrwarren <james.warren@digital.justice.gov.uk>
2025-03-26 13:53:03 +0000

    DDLS-553 remove confd from additional client scripts

--- client/app/scripts/document_and_checklist_one_off.sh
+++ client/app/scripts/document_and_checklist_one_off.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# We need below to create the params file on container start
-confd -onetime -backend env
-
 echo "performing document sync at: $(date)"
 php app/console digideps:document-sync
 echo "performing checklist sync at: $(date)"
