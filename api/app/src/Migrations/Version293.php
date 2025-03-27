6363359a633a4ac05a46979f9ea7ab18fd8b8387
Chris Rafferty <chris.rafferty@digital.justice.gov.uk>
2025-03-27 13:39:01 +0000

    Changes filename to 293
    Signed-off-by: Chris Rafferty <chris.rafferty@digital.justice.gov.uk>

--- api/app/src/Migrations/Version293.php
+++ api/app/src/Migrations/Version293.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version292 extends AbstractMigration
+final class Version293 extends AbstractMigration
 {
     public function getDescription(): string
     {
