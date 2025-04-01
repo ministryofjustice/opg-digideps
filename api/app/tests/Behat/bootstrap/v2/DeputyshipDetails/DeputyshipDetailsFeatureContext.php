f162bf119aa7a7d5c72c50c02953c40f10c5ff0a
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-04-01 15:43:46 +0100

    Add behat test for clients list

---
+++ api/app/tests/Behat/bootstrap/v2/DeputyshipDetails/DeputyshipDetailsFeatureContext.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Behat\v2\DeputyshipDetails;
+
+use App\Tests\Behat\v2\Common\BaseFeatureContext;
+use App\Tests\Behat\v2\Common\UserExistsTrait;
+
+class DeputyshipDetailsFeatureContext extends BaseFeatureContext
+{
+    use UserExistsTrait;
+}
---
+++ api/app/tests/Behat/features-v2/deputyship-details/client-list.feature
@@ -0,0 +1,6 @@
+@v2 @deputyship-details
+Feature: List clients for a deputy
+
+    @deputyship-details-client-list
+    Scenario: A deputy can see a list of clients whose reports they can contribute to
+        Given a Lay Deputy exists
