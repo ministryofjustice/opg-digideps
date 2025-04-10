1d2a279899d5cc8ea1b0edf6cde8ff30d0e39c6f
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-04-10 12:48:14 +0100

    Restrict access to court order to its deputies

--- api/app/src/v2/Service/CourtOrderService.php
+++ api/app/src/v2/Service/CourtOrderService.php
@@ -5,22 +5,49 @@ declare(strict_types=1);
 namespace App\v2\Service;
 
 use App\Entity\CourtOrder;
+use App\Entity\User;
 use App\Repository\CourtOrderRepository;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 class CourtOrderService
 {
     public function __construct(
-        private readonly CourtOrderRepository $repository,
+        private readonly CourtOrderRepository $courtOrderRepository,
     ) {
     }
 
     /**
      * Get a court order by UID $uid, but only if $user is a deputy on it.
      */
-    public function getByUidAsUser(string $uid, ?UserInterface $user): ?CourtOrder
+    public function getByUidAsUser(string $uid, ?User $user): ?CourtOrder
     {
-        return $this->repository->findOneBy(['courtOrderUid' => $uid]);
-        // return null;
+        if (is_null($user)) {
+            return null;
+        }
+
+        $deputyUid = $user->getDeputyUid();
+
+        if (is_null($deputyUid)) {
+            return null;
+        }
+
+        /** @var CourtOrder $courtOrder */
+        $courtOrder = $this->courtOrderRepository->findOneBy(['courtOrderUid' => $uid]);
+
+        // only return court order if the logged-in user is a deputy on it
+        $deputyUidStr = "$deputyUid";
+
+        $isDeputyOnCourtOrder = false;
+        foreach ($courtOrder->getActiveDeputies() as $deputy) {
+            if ($deputy->getDeputyUid() === $deputyUidStr) {
+                $isDeputyOnCourtOrder = true;
+                break;
+            }
+        }
+
+        if (!$isDeputyOnCourtOrder) {
+            return null;
+        }
+
+        return $courtOrder;
     }
 }
