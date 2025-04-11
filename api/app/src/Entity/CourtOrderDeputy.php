1901c5c591645bcd6d3bec8ff2461fa3002dd384
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-04-11 11:13:11 +0100

    Simplify CourtOrderDeputy relationships
    The additional annotations are not necessary and don't break
    anything.

--- api/app/src/Entity/CourtOrderDeputy.php
+++ api/app/src/Entity/CourtOrderDeputy.php
@@ -19,7 +19,7 @@ class CourtOrderDeputy
     /**
      * @ORM\Id
      *
-     * @ORM\ManyToOne(targetEntity="App\Entity\CourtOrder", inversedBy="courtOrderDeputyRelationships", cascade={"persist"})
+     * @ORM\ManyToOne(targetEntity="App\Entity\CourtOrder", cascade={"persist"})
      *
      * @ORM\JoinColumn(name="court_order_id", referencedColumnName="id", nullable=false)
      */
@@ -30,7 +30,7 @@ class CourtOrderDeputy
      *
      * @ORM\Id
      *
-     * @ORM\ManyToOne(targetEntity="App\Entity\Deputy", inversedBy="courtOrderDeputyRelationships", cascade={"persist"})
+     * @ORM\ManyToOne(targetEntity="App\Entity\Deputy", cascade={"persist"})
      *
      * @ORM\JoinColumn(name="deputy_id", referencedColumnName="id", nullable=false)
      */
