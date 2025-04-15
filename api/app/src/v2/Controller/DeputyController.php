a24704a8ff863942659c4e75131b37dc6de294a2
Chris Rafferty <chris.rafferty@digital.justice.gov.uk>
2025-04-15 17:36:43 +0100

    DDLS-529 - Moves new endpoint into v2 controller, changes from docblock to annotation
    Signed-off-by: Chris Rafferty <chris.rafferty@digital.justice.gov.uk>

--- api/app/src/v2/Controller/DeputyController.php
+++ api/app/src/v2/Controller/DeputyController.php
@@ -2,41 +2,69 @@
 
 namespace App\v2\Controller;
 
+use App\Repository\DeputyRepository;
 use App\Repository\UserRepository;
 use App\v2\Assembler\UserAssembler;
 use App\v2\Transformer\UserTransformer;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/deputy")
- */
-class DeputyController
+
+#[Route(path: '/deputy', name: 'v2_deputy_')]
+class DeputyController extends AbstractController
 {
     use ControllerTrait;
 
     public function __construct(
+        private readonly DeputyRepository $deputyRepository,
+        private readonly LoggerInterface $logger,
         private readonly UserRepository $repository,
         private readonly UserAssembler $assembler,
-        private readonly UserTransformer $transformer
+        private readonly UserTransformer $transformer,
     ) {
     }
 
-    /**
-     * @Route("/{id}", requirements={"id":"\d+"}, methods={"GET"})
-     *
-     * @return JsonResponse
-     */
-    public function getByIdAction($id)
+    #[Route(path: '/{id}', requirements:['id' => '\d+'], methods: ['GET'])]
+    public function getByIdAction($id): JsonResponse
     {
         if (null === ($data = $this->repository->findUserArrayById($id))) {
-            throw new NotFoundHttpException(sprintf('Deputy id %s not found', $id));
+            $this->buildNotFoundResponse(sprintf('Deputy id %s not found', $id));
         }
 
         $dto = $this->assembler->assembleFromArray($data);
         $transformedDto = $this->transformer->transform($dto);
 
         return $this->buildSuccessResponse($transformedDto);
+    }
+
+    #[Route(path:'/{uid}/reports', name:'deputy_find_reports_by_uid', requirements:['uid' => '\d+'], methods:['GET'])]
+    public function getAllDeputyReports(Request $request, int $uid): JsonResponse
+    {
+        $user = $this->getUser();
+        $data = [];
+
+        if ($uid !== $user->getDeputyUid()) {
+            return $this->buildNotFoundResponse();
+        }
+
+        $inactive = $request->query->has('inactive') ? $request->query->get('inactive') : null;
+
+        try {
+            $results = $this->deputyRepository->findReportsInfoByUid($uid, (bool) $inactive);
+        } catch (\Exception $e) {
+            $this->logger->error(sprintf('Error occurred during retrieval:%s', $e->getMessage()));
+
+            return $this->buildErrorResponse();
+        }
+        
+        if (is_null($results)) {
+            return $this->buildNotFoundResponse();
+        }
+
+        return $this->buildSuccessResponse($data);
     }
 }
