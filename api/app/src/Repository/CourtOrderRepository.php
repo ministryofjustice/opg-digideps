05ee1a6150e27b48c51d0e9328755bf958c66bf0
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-04-17 16:03:59 +0100

    Fix phpstan issues

--- api/app/src/Repository/CourtOrderRepository.php
+++ api/app/src/Repository/CourtOrderRepository.php
@@ -8,6 +8,9 @@ use App\Entity\CourtOrder;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
+/**
+ * @extends ServiceEntityRepository<CourtOrder>
+ */
 class CourtOrderRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
