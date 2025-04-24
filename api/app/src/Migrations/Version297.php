a914da537ec4cd2273825576cd8a733a75cc79be
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-04-24 10:20:00 +0100

    Rename migrations for merge

--- api/app/src/Migrations/Version297.php
+++ api/app/src/Migrations/Version297.php
@@ -14,23 +14,20 @@ final class Version297 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Create selected candidates staging table';
+        return '';
     }
 
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA IF NOT EXISTS staging');
-        $this->addSql(<<<'SQL'
-            CREATE TABLE staging.selectedCandidates (id SERIAL NOT NULL, order_uid VARCHAR(30) NOT NULL, deputy_uid VARCHAR(30) NOT NULL, action VARCHAR(30) NOT NULL, order_status VARCHAR(30) DEFAULT NULL, order_type VARCHAR(5) DEFAULT NULL, report_type VARCHAR(30) DEFAULT NULL, order_made_date VARCHAR(30) DEFAULT NULL, deputy_type VARCHAR(30) DEFAULT NULL, deputy_status_on_order BOOLEAN DEFAULT NULL, is_hybrid VARCHAR(30) DEFAULT NULL, order_id INT DEFAULT NULL, client_id INT DEFAULT NULL, report_id INT DEFAULT NULL, deputy_id INT DEFAULT NULL, PRIMARY KEY(id))
-        SQL);
+        $this->addSql('ALTER TABLE court_order ADD order_made_date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL');
+        $this->addSql('ALTER TABLE court_order RENAME COLUMN type TO order_type');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql(<<<'SQL'
-            DROP TABLE staging.selectedCandidates
-        SQL);
+        $this->addSql('ALTER TABLE court_order DROP order_made_date');
+        $this->addSql('ALTER TABLE court_order RENAME COLUMN order_type TO type');
     }
 }
