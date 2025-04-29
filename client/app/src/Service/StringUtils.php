fc4b0069bfe32b3a6008f3b430b825a8d6100369
jamesrwarren <james.warren@digital.justice.gov.uk>
2025-04-29 16:55:56 +0100

    DDLS-623 do some cleanup on additional carriage returns on form entry

--- client/app/src/Service/StringUtils.php
+++ client/app/src/Service/StringUtils.php
@@ -16,39 +16,52 @@ class StringUtils
         $hours = intval(gmdate('H', $seconds));
         $minutes = intval(gmdate('i', $seconds));
 
-        $hoursString = $hours . ' ' . (($hours === 1) ? 'hour' : 'hours');
-        $minutesString = $minutes . ' ' . (($minutes === 1) ? 'minute' : 'minutes');
+        $hoursString = $hours.' '.((1 === $hours) ? 'hour' : 'hours');
+        $minutesString = $minutes.' '.((1 === $minutes) ? 'minute' : 'minutes');
 
         // less than a minute
-        if ($hours === 0 && $minutes === 0) {
+        if (0 === $hours && 0 === $minutes) {
             return 'less than a minute';
         }
 
         // X minutes
-        if ($hours === 0) {
+        if (0 === $hours) {
             return $minutesString;
         }
 
         // X hours
-        if ($minutes === 0) {
+        if (0 === $minutes) {
             return $hoursString;
         }
 
         // X hours and Y minutes
-        return $hoursString . ' and ' . $minutesString;
+        return $hoursString.' and '.$minutesString;
     }
 
     /**
-     * Implodes strings, but with a different joiner between the last two
+     * Implodes strings, but with a different joiner between the last two.
      */
     public static function implodeWithDifferentLast(array $strings, string $joiner, string $lastJoiner)
     {
         $last = array_pop($strings);
 
         if ($strings) {
-            return implode($joiner, $strings) . $lastJoiner . $last;
+            return implode($joiner, $strings).$lastJoiner.$last;
         }
 
         return $last;
+    }
+
+    /**
+     * Replaces all returns with standard \n, removes tabs and extra spaces and any excess new lines.
+     */
+    public static function cleanText(string $input): string
+    {
+        $input = trim($input);
+        $input = str_replace(["\r\n", "\r"], "\n", $input);
+        $input = preg_replace("/[ \t]+/", ' ', $input) ?? $input;
+        $input = preg_replace("/\n{2,}/", "\n", $input) ?? $input;
+
+        return $input;
     }
 }
