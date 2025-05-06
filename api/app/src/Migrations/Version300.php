848435b2f81f0d1703da0a8b69833b1c9838e1ce
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-05-06 14:25:21 +0100

    Make way for other migrations

--- api/app/src/Migrations/Version300.php
+++ api/app/src/Migrations/Version300.php
@@ -11,26 +11,20 @@ final class Version300 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Add ndr_id field to stating selected candidates table';
     }
 
     public function up(Schema $schema): void
     {
         $this->addSql(<<<'SQL'
-            ALTER TABLE staging.selectedcandidates DROP is_hybrid
-        SQL);
-        $this->addSql(<<<'SQL'
-            ALTER TABLE staging.selectedcandidates ALTER deputy_uid DROP NOT NULL
+            ALTER TABLE staging.selectedcandidates ADD ndr_id INT DEFAULT NULL
         SQL);
     }
 
     public function down(Schema $schema): void
     {
         $this->addSql(<<<'SQL'
-            ALTER TABLE staging.selectedCandidates ADD is_hybrid VARCHAR(30) DEFAULT NULL
-        SQL);
-        $this->addSql(<<<'SQL'
-            ALTER TABLE staging.selectedCandidates ALTER deputy_uid SET NOT NULL
+            ALTER TABLE staging.selectedCandidates DROP ndr_id
         SQL);
     }
 }
