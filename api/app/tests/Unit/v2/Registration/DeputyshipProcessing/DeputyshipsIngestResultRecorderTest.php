f8a31da4cef927ed8d31f7898ac196723a6449d2
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-05-21 15:24:03 +0100

    Unit tests for result recorder

--- api/app/tests/Unit/v2/Registration/DeputyshipProcessing/DeputyshipsIngestResultRecorderTest.php
+++ api/app/tests/Unit/v2/Registration/DeputyshipProcessing/DeputyshipsIngestResultRecorderTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Tests\Unit\v2\Registration\DeputyshipProcessing;
 
+use App\v2\Registration\DeputyshipProcessing\DeputyshipBuilderResult;
 use App\v2\Registration\DeputyshipProcessing\DeputyshipCandidatesSelectorResult;
 use App\v2\Registration\DeputyshipProcessing\DeputyshipsCSVLoaderResult;
 use App\v2\Registration\DeputyshipProcessing\DeputyshipsIngestResultRecorder;
@@ -58,5 +59,41 @@ class DeputyshipsIngestResultRecorderTest extends TestCase
 
         self::assertTrue($result->success);
         self::assertStringContainsString($expectedMessage, $result->message);
+    }
+
+    public function testRecordBuilderResult(): void
+    {
+        $builderResult1 = $this->createMock(DeputyshipBuilderResult::class);
+        $builderResult1->expects($this->once())->method('getNumCandidatesApplied')->willReturn(7);
+        $builderResult1->expects($this->once())->method('getNumCandidatesFailed')->willReturn(1);
+        $this->sut->recordBuilderResult($builderResult1);
+
+        $result = $this->sut->result();
+        self::assertStringContainsString('number of candidates applied = 7', $result->message);
+        self::assertStringContainsString('number of candidates failed = 1', $result->message);
+
+        $builderResult2 = $this->createMock(DeputyshipBuilderResult::class);
+        $builderResult2->expects($this->once())->method('getNumCandidatesApplied')->willReturn(3);
+        $builderResult2->expects($this->once())->method('getNumCandidatesFailed')->willReturn(4);
+        $this->sut->recordBuilderResult($builderResult2);
+
+        $result = $this->sut->result();
+        self::assertStringContainsString('number of candidates applied = 10', $result->message);
+        self::assertStringContainsString('number of candidates failed = 5', $result->message);
+    }
+
+    public function testStartAndEndDateAndTimings(): void
+    {
+        $start = new \DateTimeImmutable('2025-05-21 23:59:00');
+        $end = new \DateTimeImmutable('2025-05-22 01:05:10');
+
+        $this->sut->recordStart($start);
+        $this->sut->recordEnd($end);
+
+        $expected = '1h 6m 10s';
+
+        $result = $this->sut->result();
+
+        self::assertStringContainsString($expected, $result->message);
     }
 }
