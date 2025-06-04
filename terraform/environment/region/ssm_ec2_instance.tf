53ad928e6f9a6a499d8c25c0214413815a95f4c9
Matty Cooke <matthew.cooke@justice.gov.uk>
2025-06-04 10:51:58 +0100

    Format was off

--- terraform/environment/region/ssm_ec2_instance.tf
+++ terraform/environment/region/ssm_ec2_instance.tf
@@ -1,11 +1,11 @@
 module "ssm_ec2_instance" {
   source = "./modules/ssm_ec2_instance"
 
-  instance_type      = "t3.micro"
-  subnet_id          = data.aws_subnet.private[0].id
-  name               = "ssm-instance"
-  tags               = var.default_tags
+  instance_type = "t3.micro"
+  subnet_id     = data.aws_subnet.private[0].id
+  name          = "ssm-instance"
+  tags          = var.default_tags
 
-  vpc_id             = data.aws_vpc.vpc.id
-  environment        = local.environment
+  vpc_id      = data.aws_vpc.vpc.id
+  environment = local.environment
 }
