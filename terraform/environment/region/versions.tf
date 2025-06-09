076f7ac05e72d58d7f0be06456fa52fd90fcb36f
Matty Cooke <matthew.cooke@justice.gov.uk>
2025-06-09 10:13:01 +0100

    Changed back

--- terraform/environment/region/versions.tf
+++ terraform/environment/region/versions.tf
@@ -1,9 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0.0"
-      configuration_aliases = [aws.management]
+      source                = "hashicorp/aws"
+      version               = ">= 5.0.0"
     }
     archive = {
       source  = "hashicorp/archive"
