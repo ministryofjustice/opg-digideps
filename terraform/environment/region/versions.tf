56b61ac77197f914815e678fbfd22f7bee3dd72d
Matty Cooke <matthew.cooke@justice.gov.uk>
2025-06-09 10:25:19 +0100

    Fixed versions file

--- terraform/environment/region/versions.tf
+++ terraform/environment/region/versions.tf
@@ -1,8 +1,9 @@
 terraform {
   required_providers {
     aws = {
-      source                = "hashicorp/aws"
-      version               = ">= 5.0.0"
+      source  = "hashicorp/aws"
+      version = ">= 5.0.0"
+
     }
     archive = {
       source  = "hashicorp/archive"
