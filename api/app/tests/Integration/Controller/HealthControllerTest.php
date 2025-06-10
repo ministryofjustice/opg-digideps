60ca27319f6ff21b52526f616afbef7642ffe113
ramchale <legionsofbob@gmail.com>
2025-06-10 09:56:41 +0100

    Initial PoC

--- api/app/tests/Integration/Controller/HealthControllerTest.php
+++ api/app/tests/Integration/Controller/HealthControllerTest.php
@@ -2,6 +2,10 @@
 
 namespace App\Tests\Integration\Controller;
 
+use Doctrine\DBAL\Connection;
+use Osteel\OpenApi\Testing\ValidatorBuilder;
+use ReflectionObject;
+
 class HealthControllerTest extends AbstractTestController
 {
     public function testServiceHealth()
@@ -21,5 +25,48 @@ class HealthControllerTest extends AbstractTestController
         ])['data'];
 
         $this->assertEquals('ok', $ret);
+    }
+
+    public function testHealthCheckOk()
+    {
+        self::$frameworkBundleClient->request('GET', '/health-check');
+
+        $validator = ValidatorBuilder::fromYamlFile(__DIR__ . '/temp/api-docs.yaml')->getValidator();
+
+        $this->assertTrue($validator->validate(self::$frameworkBundleClient->getResponse(), '/health-check', 'get'));
+    }
+
+    public function testServiceHealthCheckOk()
+    {
+        self::$frameworkBundleClient->request('GET', '/health-check/service');
+
+        $validator = ValidatorBuilder::fromYamlFile(__DIR__ . '/temp/api-docs.yaml')->getValidator();
+
+        $this->assertTrue($validator->validate(self::$frameworkBundleClient->getResponse(), '/health-check/service', 'get'));
+    }
+
+    public function testServiceHealthCheckError()
+    {
+        $this->breakTheDBConnection();
+
+        self::$frameworkBundleClient->request('GET', '/health-check/service');
+
+        $validator = ValidatorBuilder::fromYamlFile(__DIR__ . '/temp/api-docs.yaml')->getValidator();
+
+        $response = self::$frameworkBundleClient->getResponse();
+
+        $this->assertTrue($validator->validate($response, '/health-check/service', 'get'));
+    }
+
+    public function breakTheDBConnection(): void
+    {
+        $connection = self::getContainer()->get('doctrine.dbal.default_connection');
+        $mockConnection = $this->createMock(Connection::class);
+        $mockConnection->method('connect')
+            ->willThrowException(new \Exception('Database connection failed'));
+
+        $ref = new ReflectionObject($connection);
+        $prop = $ref->getProperty('_conn');
+        $prop->setValue($connection, $mockConnection);
     }
 }
