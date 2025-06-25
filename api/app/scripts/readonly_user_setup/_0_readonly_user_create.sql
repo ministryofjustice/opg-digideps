3dff5c44e3dbc85b4b9f73e1f9c6fa463044a440
Matty Cooke <matthew.cooke@justice.gov.uk>
2025-06-25 11:17:12 +0100

    Enabled IAM Connect

--- api/app/scripts/readonly_user_setup/_0_readonly_user_create.sql
+++ api/app/scripts/readonly_user_setup/_0_readonly_user_create.sql
@@ -1,9 +1,13 @@
 DO
 $$
 BEGIN
+
   IF NOT EXISTS (SELECT * FROM pg_user WHERE usename = 'readonly_sql_user') THEN
      CREATE USER readonly_sql_user WITH PASSWORD 'string_to_replace_with_real_password';
   END IF;
+
+  GRANT rds_iam TO readonly_sql_user;
+
 END
 $$
 ;
--- terraform/environment/region/rds.tf
+++ terraform/environment/region/rds.tf
@@ -1,24 +1,25 @@
 module "api_aurora" {
-  source                 = "./modules/aurora"
-  count                  = 1
-  aurora_serverless      = var.account.aurora_serverless
-  account_id             = data.aws_caller_identity.current.account_id
-  apply_immediately      = var.account.deletion_protection ? false : true
-  cluster_identifier     = "api"
-  ca_cert_identifier     = "rds-ca-rsa2048-g1"
-  db_subnet_group_name   = var.account.db_subnet_group
-  database_name          = "api"
-  engine_version         = var.account.psql_engine_version
-  master_username        = "digidepsmaster"
-  master_password        = data.aws_secretsmanager_secret_version.database_password.secret_string
-  instance_count         = var.account.aurora_instance_count
-  instance_class         = "db.t3.medium"
-  kms_key_id             = data.aws_kms_key.rds.arn
-  skip_final_snapshot    = var.account.deletion_protection ? false : true
-  vpc_security_group_ids = [module.api_rds_security_group.id]
-  deletion_protection    = var.account.deletion_protection ? true : false
-  tags                   = var.default_tags
-  log_group              = aws_cloudwatch_log_group.api_cluster.name
+  source                              = "./modules/aurora"
+  count                               = 1
+  aurora_serverless                   = var.account.aurora_serverless
+  account_id                          = data.aws_caller_identity.current.account_id
+  apply_immediately                   = var.account.deletion_protection ? false : true
+  cluster_identifier                  = "api"
+  ca_cert_identifier                  = "rds-ca-rsa2048-g1"
+  db_subnet_group_name                = var.account.db_subnet_group
+  database_name                       = "api"
+  engine_version                      = var.account.psql_engine_version
+  master_username                     = "digidepsmaster"
+  master_password                     = data.aws_secretsmanager_secret_version.database_password.secret_string
+  instance_count                      = var.account.aurora_instance_count
+  instance_class                      = "db.t3.medium"
+  kms_key_id                          = data.aws_kms_key.rds.arn
+  skip_final_snapshot                 = var.account.deletion_protection ? false : true
+  vpc_security_group_ids              = [module.api_rds_security_group.id]
+  deletion_protection                 = var.account.deletion_protection ? true : false
+  tags                                = var.default_tags
+  log_group                           = aws_cloudwatch_log_group.api_cluster.name
+  iam_database_authentication_enabled = true
 }
 
 locals {
@@ -52,7 +53,7 @@ resource "aws_route53_record" "api_postgres" {
 data "aws_caller_identity" "current" {}
 
 
-#Allow the Operator Role to Connect
+# Allow the Operator Role to Connect
 
 data "aws_iam_role" "operator" {
   name = "operator"
