5b088813cd35f2f8550252fd530d8880c233058b
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-06-25 10:03:36 +0100

    Add single submit form attribute
    Add a function which prevents a form being submitted multiple
    times. This prevents multiple submissions of the same report
    from a single form.

--- client/resources/assets/javascripts/common.js
+++ client/resources/assets/javascripts/common.js
@@ -1,44 +1,50 @@
-import SessionTimeout from './globals/SessionTimeout'
-import CookieBanner from './globals/CookieBanner'
-import ConditionalFieldRevealer from './modules_new/ConditionalFieldRevealer'
-import TextAreaAutoSize from './modules_new/TextAreaAutoSize'
-import PreventDoubleClickButton from './modules_new/PreventDoubleClickButton'
-import PreventDoubleClickLink from './modules_new/PreventDoubleClickLink'
-import DetailsExpander from './modules_new/DetailsExpander'
-import uploadFile from './modules_new/UploadFile'
-import FormatCurrency from './modules_new/FormatCurrency'
-import MoneyTransfer from './modules_new/MoneyTransfer'
-import TableMultiSelect from './modules_new/TableMultiSelect'
-import ReturnHTML from './modules_new/ReturnHTML'
-import ShowHideContent from './modules_new/ShowHideContent'
-import { initAll as MOJFrontendAll } from '@ministryofjustice/frontend'
-import { Accordion, Button, CharacterCount, Checkboxes, ErrorSummary, ExitThisPage, Header, NotificationBanner, PasswordInput, Radios, SkipLink, createAll } from 'govuk-frontend'
-import GoogleAnalyticsLinkTracking from './modules_new/GoogleAnalyticsLinkTracking'
-import { GoogleAnalyticsEvents } from './modules_new/googleAnalyticsEvents'
-import GoogleAnalyticsObject from './modules_new/GoogleAnalyticsObject'
-import GoogleAnalyticsGtag from './modules_new/GoogleAnalyticsGtag'
-import EnableJavascript from './modules_new/EnableJavascript'
+import SessionTimeout from "./globals/SessionTimeout";
+import CookieBanner from "./globals/CookieBanner";
+import ConditionalFieldRevealer from "./modules_new/ConditionalFieldRevealer";
+import TextAreaAutoSize from "./modules_new/TextAreaAutoSize";
+import PreventDoubleClickButton from "./modules_new/PreventDoubleClickButton";
+import PreventDoubleClickLink from "./modules_new/PreventDoubleClickLink";
+import DetailsExpander from "./modules_new/DetailsExpander";
+import uploadFile from "./modules_new/UploadFile";
+import FormatCurrency from "./modules_new/FormatCurrency";
+import MoneyTransfer from "./modules_new/MoneyTransfer";
+import TableMultiSelect from "./modules_new/TableMultiSelect";
+import ReturnHTML from "./modules_new/ReturnHTML";
+import ShowHideContent from "./modules_new/ShowHideContent";
+import { initAll as MOJFrontendAll } from "@ministryofjustice/frontend";
+import {
+  Accordion,
+  Button,
+  CharacterCount,
+  Checkboxes,
+  createAll,
+  ErrorSummary,
+  ExitThisPage,
+  Header,
+  NotificationBanner,
+  PasswordInput,
+  Radios,
+  SkipLink
+} from "govuk-frontend";
+import GoogleAnalyticsLinkTracking from "./modules_new/GoogleAnalyticsLinkTracking";
+import { GoogleAnalyticsEvents } from "./modules_new/googleAnalyticsEvents";
+import GoogleAnalyticsObject from "./modules_new/GoogleAnalyticsObject";
+import GoogleAnalyticsGtag from "./modules_new/GoogleAnalyticsGtag";
+import EnableJavascript from "./modules_new/EnableJavascript";
+import FormSingleSubmit from "./modules_new/FormSingleSubmit";
 
 window.addEventListener('DOMContentLoaded', () => {
-  // Session Timeout
   SessionTimeout()
 
-  // Cookie Banner
   CookieBanner()
 
-  // Format currency module
   FormatCurrency.init(document)
 
-  // Double Click Buttons
   PreventDoubleClickButton.init(document)
 
-  // Double Click Links
   PreventDoubleClickLink.init(document)
 
-  // Conditional Field Revealer
   ConditionalFieldRevealer.init()
-
-  // Text Area Auto Size
 
   TextAreaAutoSize.init(document)
 
@@ -63,6 +69,8 @@ window.addEventListener('DOMContentLoaded', () => {
   GoogleAnalyticsObject.init(document)
 
   GoogleAnalyticsGtag.init(document)
+
+  FormSingleSubmit.init(document)
 
   // Error summaries
   const errorSummaries = document.querySelector('#error-summary')
