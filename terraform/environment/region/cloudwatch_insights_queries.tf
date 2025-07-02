425339f1c572c11fb34953dce6fd2fe3862e2de4
Matty Cooke <matthew.cooke@justice.gov.uk>
2025-07-02 15:11:31 +0100

    Corrected with the name of the log groups and added queries

--- terraform/environment/region/cloudwatch_insights_queries.tf
+++ terraform/environment/region/cloudwatch_insights_queries.tf
@@ -15,12 +15,24 @@ EOF
 
 resource "aws_cloudwatch_query_definition" "readonly_db_iam_assumptions" {
   name            = "IAM Audit/${local.environment} readonly-db-iam role assumptions"
-  log_group_names = [aws_cloudwatch_log_group.cloudtrail.name]
+  log_group_names = ["cloudtrail"]
 
   query_string = <<EOF
 fields @timestamp, eventName, userIdentity.arn, requestParameters.roleArn, sourceIPAddress
 | filter eventName = "AssumeRole"
 | filter requestParameters.roleArn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/readonly-db-iam-${local.environment}"
+| sort @timestamp desc
+| limit 50
+EOF
+}
+
+resource "aws_cloudwatch_query_definition" "readonly_db_iam_queries" {
+  name            = "IAM Audit/${local.environment} readonly-db-iam DB queries"
+  log_group_names = ["/aws/rds/cluster/api-${local.environment}/postgresql"]
+
+  query_string = <<EOF
+fields @timestamp, @message
+| filter @message like 'readonly-db-iam-${local.environment}'
 | sort @timestamp desc
 | limit 50
 EOF
