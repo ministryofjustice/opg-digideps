906037dd7c867f23289fd2b05c5acfa34496212d
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-07-11 10:11:06 +0100

    Add integration test that deleting a deputy deletes records in court_order_deputy

--- api/app/tests/Integration/Entity/DeputyTest.php
+++ api/app/tests/Integration/Entity/DeputyTest.php
@@ -4,28 +4,38 @@ namespace App\Tests\Integration\Entity;
 
 use App\Entity\CourtOrder;
 use App\Entity\Deputy;
+use App\Repository\DeputyRepository;
 use App\TestHelpers\DeputyTestHelper;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 use Doctrine\ORM\EntityManagerInterface;
 use Faker\Factory;
+use Faker\Generator;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class DeputyTest extends KernelTestCase
 {
-    /**
-     * @var EntityManagerInterface
-     */
-    private $em;
+    private EntityManagerInterface $em;
+    private Generator $faker;
+    private DeputyRepository $deputyRepository;
 
     protected function setUp(): void
     {
         $kernel = self::bootKernel();
         $this->em = $kernel->getContainer()->get('doctrine')->getManager();
+        $this->faker = Factory::create();
+
+        /* @var DeputyRepository $deputyRepository */
+        $this->deputyRepository = $this->em->getRepository(Deputy::class);
     }
 
-    public function testGetCourtOrdersWithStatus()
+    protected function tearDown(): void
     {
-        $faker = Factory::create();
-        $fakeUid = $faker->unique()->randomNumber(8);
+        (new ORMPurger($this->em))->purge();
+    }
+
+    public function testGetCourtOrdersWithStatus(): void
+    {
+        $fakeUid = $this->faker->unique()->randomNumber(8);
 
         $deputyHelper = new DeputyTestHelper();
         $deputy = $deputyHelper->generateDeputy();
@@ -45,19 +55,55 @@ class DeputyTest extends KernelTestCase
         $this->em->flush();
 
         // retrieve the deputy from the db and check the association is populated correctly
-        $repo = $this->em->getRepository(Deputy::class);
-        $retrievedDeputy = $repo->findOneBy(['deputyUid' => $deputy->getDeputyUid()]);
+        $retrievedDeputy = $this->deputyRepository->findOneBy(['deputyUid' => $deputy->getDeputyUid()]);
 
         $actual = $retrievedDeputy->getCourtOrdersWithStatus();
-        $this->assertArrayHasKey('courtOrder', $actual[0]);
-        $this->assertArrayHasKey('isActive', $actual[0]);
+        self::assertArrayHasKey('courtOrder', $actual[0]);
+        self::assertArrayHasKey('isActive', $actual[0]);
 
         $isActive = $actual[0]['isActive'];
         $actualCourtOrder = $actual[0]['courtOrder'];
 
-        $this->assertEquals(1, count($actual));
-        $this->assertEquals(true, $isActive);
-        $this->assertEquals($fakeUid, $actualCourtOrder->getCourtOrderUid());
-        $this->assertEquals('hybrid', $actualCourtOrder->getOrderType());
+        self::assertEquals(1, count($actual));
+        self::assertEquals(true, $isActive);
+        self::assertEquals($fakeUid, $actualCourtOrder->getCourtOrderUid());
+        self::assertEquals('hybrid', $actualCourtOrder->getOrderType());
+    }
+
+    /**
+     * Deleting a deputy should delete court_order_deputy records.
+     */
+    public function testCascadeDeleteCourtOrderDeputy(): void
+    {
+        $fakeCourtOrderUid = $this->faker->unique()->randomNumber(8);
+        $fakeDeputyUid = $this->faker->unique()->randomNumber(8);
+
+        $deputyHelper = new DeputyTestHelper();
+        $deputy = $deputyHelper->generateDeputy(deputyUid: $fakeDeputyUid);
+        $deputy->setLastname('VOLO');
+
+        $courtOrder = new CourtOrder();
+        $courtOrder
+            ->setCourtOrderUid($fakeCourtOrderUid)
+            ->setOrderType('hybrid')
+            ->setStatus('ACTIVE')
+            ->setOrderMadeDate(new \DateTime('2020-06-14'));
+
+        $deputy->associateWithCourtOrder($courtOrder);
+
+        $this->em->persist($courtOrder);
+        $this->em->persist($deputy);
+        $this->em->flush();
+
+        // check deputy persisted and court_order <-> deputy association exists
+        $retrievedDeputy = $this->deputyRepository->findOneBy(['deputyUid' => $fakeDeputyUid]);
+        self::assertNotNull($retrievedDeputy);
+
+        $courtOrders = $retrievedDeputy->getCourtOrdersWithStatus();
+        self::assertCount(1, $courtOrders);
+
+        // delete the deputy and confirm that it and the court_order_deputy entry are gone
+        $this->em->remove($deputy);
+        $this->em->flush();
     }
 }
