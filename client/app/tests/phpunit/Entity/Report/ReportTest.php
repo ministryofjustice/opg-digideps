67ae47643b1739a9c10ddbad1c0bd9fed7896845
MGordon91 <81708178+MiaGordon91@users.noreply.github.com>
2025-07-11 08:55:04 +0100

    DDLS-767: Update Org Dashboard to show report type (#2041)
    * add a getter to define report type definition
    
    * add test for new entity method
    
    * update query to handle updated ordering of dual reports & content update
    
    * add repository integration test and move abstract existing unit tests to a seperate class

--- client/app/tests/phpunit/Entity/Report/ReportTest.php
+++ client/app/tests/phpunit/Entity/Report/ReportTest.php
@@ -11,11 +11,22 @@ class ReportTest extends TestCase
 {
     /**
      * @dataProvider typeProvider
+     *
      * @test
      */
     public function determineReportType(Report $report, string $expectedType)
     {
         self::assertEquals($expectedType, $report->determineReportType());
+    }
+
+    /**
+     * @dataProvider typeDefinition
+     *
+     * @test
+     */
+    public function getReportTypeDefinition(Report $report, string $expectedType)
+    {
+        self::assertEquals($expectedType, $report->getReportTypeDefinition());
     }
 
     public function typeProvider()
@@ -26,6 +37,18 @@ class ReportTest extends TestCase
             'PF - high' => [(new Report())->setType(Report::TYPE_PROPERTY_AND_AFFAIRS_HIGH_ASSETS), 'PF'],
             'Combined - low' => [(new Report())->setType(Report::TYPE_COMBINED_LOW_ASSETS), 'COMBINED'],
             'Combined - high' => [(new Report())->setType(Report::TYPE_COMBINED_HIGH_ASSETS), 'COMBINED'],
+        ];
+    }
+
+    public function typeDefinition()
+    {
+        return [
+            'HW - Lay' => [(new Report())->setType(Report::TYPE_HEALTH_WELFARE), 'Health and Welfare Report'],
+            'HW - Professional' => [(new Report())->setType('104-5'), 'Health and Welfare Report'],
+            'PF - Lay' => [(new Report())->setType(Report::TYPE_PROPERTY_AND_AFFAIRS_LOW_ASSETS), 'Property & Affairs Report'],
+            'PF - Pubic Authority' => [(new Report())->setType('102-6'), 'Property & Affairs Report'],
+            'Combined - low' => [(new Report())->setType(Report::TYPE_COMBINED_LOW_ASSETS), 'Property & Affairs with Health & Welfare Report'],
+            'Combined - low - Professional' => [(new Report())->setType('103-4-5'), 'Property & Affairs with Health & Welfare Report'],
         ];
     }
 }
