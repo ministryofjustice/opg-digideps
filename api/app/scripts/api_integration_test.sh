04a5368f7cc014e7fbb1e9a0851f9d1a4cbbcf3c
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-07-16 15:37:27 +0100

    Return API integration test runner to former configuration

--- api/app/scripts/api_integration_test.sh
+++ api/app/scripts/api_integration_test.sh
@@ -61,29 +61,29 @@ case "$INTEGRATION_SELECTION" in
     php vendor/bin/phpunit -c tests/Integration tests/Integration/v2/ --coverage-php tests/coverage/v2.cov
     ;;
   selection-all)
-#    # selection-1
-#    printf '\n Running Controller Suite \n\n'
-#    php vendor/bin/phpunit -c tests/Integration tests/Integration/Controller/ --coverage-php tests/coverage/Controller.cov
-#
-#    # selection-2
-#    printf '\n Running ControllerReport Suite \n\n'
-#    php vendor/bin/phpunit -c tests/Integration tests/Integration/ControllerReport/ --coverage-php tests/coverage/ControllerReport.cov
-#
-#    # selection-3
-#    printf '\n Running DBAL Suite \n\n'
-#    php vendor/bin/phpunit --debug -c tests/Integration tests/Integration/DBAL/ --coverage-php tests/coverage/DBAL.cov
-#    printf '\n Running Controller-Ndr Suite \n\n'
-#    php vendor/bin/phpunit -c tests/Integration tests/Integration/Controller-Ndr/ --coverage-php tests/coverage/Controller-Ndr.cov
-#    printf '\n Running Entity Suite \n\n'
-#    php vendor/bin/phpunit -c tests/Integration tests/Integration/Entity/ --coverage-php tests/coverage/Entity.cov
-#    printf '\n Running Command Suite \n\n'
-#    php vendor/bin/phpunit -c tests/Integration tests/Integration/Command/ --coverage-php tests/coverage/Command.cov
-#    printf '\n Running Security Suite \n\n'
-#    php vendor/bin/phpunit -c tests/Integration tests/Integration/Security/ --coverage-php tests/coverage/Security.cov
-#    printf '\n Running Stats Suite \n\n'
-#    php vendor/bin/phpunit -c tests/Integration tests/Integration/Stats/ --coverage-php tests/coverage/Stats.cov
-#    printf '\n Running v2 Suite \n\n'
-    php vendor/bin/phpunit -c tests/Integration tests/Integration/ --coverage-php tests/coverage/v2.cov
+    # selection-1
+    printf '\n Running Controller Suite \n\n'
+    php vendor/bin/phpunit -c tests/Integration tests/Integration/Controller/ --coverage-php tests/coverage/Controller.cov
+
+    # selection-2
+    printf '\n Running ControllerReport Suite \n\n'
+    php vendor/bin/phpunit -c tests/Integration tests/Integration/ControllerReport/ --coverage-php tests/coverage/ControllerReport.cov
+
+    # selection-3
+    printf '\n Running DBAL Suite \n\n'
+    php vendor/bin/phpunit --debug -c tests/Integration tests/Integration/DBAL/ --coverage-php tests/coverage/DBAL.cov
+    printf '\n Running Controller-Ndr Suite \n\n'
+    php vendor/bin/phpunit -c tests/Integration tests/Integration/Controller-Ndr/ --coverage-php tests/coverage/Controller-Ndr.cov
+    printf '\n Running Entity Suite \n\n'
+    php vendor/bin/phpunit -c tests/Integration tests/Integration/Entity/ --coverage-php tests/coverage/Entity.cov
+    printf '\n Running Command Suite \n\n'
+    php vendor/bin/phpunit -c tests/Integration tests/Integration/Command/ --coverage-php tests/coverage/Command.cov
+    printf '\n Running Security Suite \n\n'
+    php vendor/bin/phpunit -c tests/Integration tests/Integration/Security/ --coverage-php tests/coverage/Security.cov
+    printf '\n Running Stats Suite \n\n'
+    php vendor/bin/phpunit -c tests/Integration tests/Integration/Stats/ --coverage-php tests/coverage/Stats.cov
+    printf '\n Running v2 Suite \n\n'
+    php vendor/bin/phpunit -c tests/Integration tests/Integration/v2 --coverage-php tests/coverage/v2.cov
 
     # generate HTML coverage report
     php -d memory_limit=256M vendor/phpunit/phpcov/phpcov merge --html "./build/coverage-api" "./tests/coverage"
