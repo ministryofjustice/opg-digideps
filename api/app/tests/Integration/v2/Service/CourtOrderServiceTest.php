7067838088f5a78bf7c8bd4a78697e290bf87841
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-07-17 15:31:04 +0100

    Add integration test for <api>/CourtOrderService
    I wanted to work out the best way to cope with duplicate
    associations, and wasn't sure if exceptions would be thrown
    and what they look like.

---
+++ api/app/tests/Integration/v2/Service/CourtOrderServiceTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Integration\v2\Service;
+
+use App\Tests\Integration\ApiBaseTestCase;
+use App\Tests\Integration\Fixtures;
+use App\v2\Service\CourtOrderService;
+
+class CourtOrderServiceTest extends ApiBaseTestCase
+{
+    private Fixtures $fixtures;
+    private CourtOrderService $sut;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->fixtures = new Fixtures($this->entityManager);
+        $this->sut = $this->getContainer()->get(CourtOrderService::class);
+    }
+
+    public function testAssociateDeputyWithCourtOrderNoDeputy(): void
+    {
+        $success = $this->sut->associateDeputyWithCourtOrder('123456', '123567');
+        $this->assertFalse($success);
+    }
+
+    public function testAssociateDeputyWithCourtOrderDeputyButNoNoCourtOrder(): void
+    {
+        $this->fixtures->createDeputy(['setDeputyUid' => '123456']);
+
+        $success = $this->sut->associateDeputyWithCourtOrder('123456', '123567');
+        $this->assertFalse($success);
+    }
+
+    public function testAssociateDeputyWithCourtOrderDuplicate(): void
+    {
+        $deputy = $this->fixtures->createDeputy(['setDeputyUid' => '123456']);
+        $courtOrder = $this->fixtures->createCourtOrder('123567', 'pfa', 'ACTIVE');
+        $deputy->associateWithCourtOrder($courtOrder);
+        $this->entityManager->persist($deputy);
+        $this->entityManager->flush();
+
+        $success = $this->sut->associateDeputyWithCourtOrder('123456', '123567');
+        $this->assertFalse($success);
+    }
+
+    public function testAssociateDeputyWithCourtOrderSuccess(): void
+    {
+        $this->fixtures->createDeputy(['setDeputyUid' => 'x123456']);
+        $courtOrder = $this->fixtures->createCourtOrder('x123567', 'pfa', 'ACTIVE');
+        $this->entityManager->persist($courtOrder);
+        $this->entityManager->flush();
+
+        $success = $this->sut->associateDeputyWithCourtOrder('x123456', 'x123567');
+        $this->assertTrue($success);
+    }
+}
