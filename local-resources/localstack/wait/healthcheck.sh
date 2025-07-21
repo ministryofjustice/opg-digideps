ec52d0ca2f5acc14f414a5cfd1e6e492b5f7d83d
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-07-21 09:51:07 +0100

    DDLS-887 Upgrade localstack to latest version (#2070)
    * Upgrade to localstack 4.6.0
    * Fix integration test healthcheck on localstack to new style endpoint
    * Localstack now requires secrets to be created for a region
    * bash-completion was causing problems and not essential
    
    We don't need this package (it's a nice to have), so
    remove it from the image to reduce the number of
    packages which could cause potential problems.
    
    (This package caused DDLS-887.)

--- local-resources/localstack/wait/healthcheck.sh
+++ local-resources/localstack/wait/healthcheck.sh
@@ -3,6 +3,14 @@
 # S3
 buckets=$(awslocal s3 ls)
 
-echo $buckets | grep "pa-uploads-local" || exit 1
-echo $buckets | grep "sirius-bucket-local" || exit 1
-echo $buckets | grep "opg-performance-data" || exit 1
+expected_buckets=("pa-uploads-local" "sirius-bucket-local" "opg-performance-data")
+for bucket in ${expected_buckets[@]} ; do
+	echo "Checking for $bucket"
+	if [[ $buckets != *"$bucket"* ]]; then
+		echo "$bucket was not found"
+        exit 1
+    fi
+done
+
+echo "Health checks completed successfully"
+exit 0
