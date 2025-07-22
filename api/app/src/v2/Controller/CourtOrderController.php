a61ae5d64fd5d73382e24659613793f9f877ecaa
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-07-22 16:26:41 +0100

    Add inviteDeputyAction to court order

--- api/app/src/v2/Controller/CourtOrderController.php
+++ api/app/src/v2/Controller/CourtOrderController.php
@@ -4,12 +4,17 @@ declare(strict_types=1);
 
 namespace App\v2\Controller;
 
+use App\Entity\User;
+use App\v2\DTO\InviteeDTO;
+use App\v2\Service\CourtOrderInviteService;
 use App\v2\Service\CourtOrderService;
 use JMS\Serializer\SerializationContext;
 use JMS\Serializer\SerializerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -22,6 +27,7 @@ class CourtOrderController extends AbstractController
 
     public function __construct(
         private readonly CourtOrderService $courtOrderService,
+        private readonly CourtOrderInviteService $courtOrderInviteService,
         private readonly SerializerInterface $serializer,
     ) {
     }
@@ -32,7 +38,7 @@ class CourtOrderController extends AbstractController
      * Note that reports are not returned in a guaranteed order, so the "submitted" property, or possibly null
      * submit_date and un_submit_date, should be used to determine whether the report has been submitted.
      *
-     * path on API = /v2/courtorder/<UID>.
+     * path on API = /v2/courtorder/<UID>
      */
     #[Route('/{uid}', requirements: ['uid' => '\w+'], methods: ['GET'])]
     #[Security('is_granted("ROLE_DEPUTY")')]
@@ -51,7 +57,7 @@ class CourtOrderController extends AbstractController
 
         $ctx = SerializationContext::create()
             ->setGroups([
-                'court-order-full', 'client', 'deputy', 'report', 'ndr', 'report-submission'
+                'court-order-full', 'client', 'deputy', 'report', 'ndr', 'report-submission',
             ])
             ->setSerializeNull(true);
 
@@ -62,5 +68,44 @@ class CourtOrderController extends AbstractController
         ], 'json', $ctx);
 
         return new JsonResponse(data: $data, json: true);
+    }
+
+    /**
+     * Invite a co-deputy to a court order.
+     *
+     * path on API = /v2/courtorder/<UID>/invite
+     */
+    #[Route('/{uid}/invite', requirements: ['uid' => '\w+'], methods: ['POST'])]
+    #[Security('is_granted("ROLE_DEPUTY")')]
+    public function inviteDeputyAction(Request $request, string $uid): JsonResponse
+    {
+        /** @var ?array $data */
+        $data = json_decode($request->getContent(), true);
+
+        if (is_null($data)) {
+            return $this->buildErrorResponse('Invalid request data', status: Response::HTTP_NOT_ACCEPTABLE);
+        }
+
+        $inviteeDTO = new InviteeDTO(
+            $data['email'] ?? '',
+            $data['firstname'] ?? '',
+            $data['lastname'] ?? '',
+            $data['role_name'] ?? User::ROLE_LAY_DEPUTY,
+        );
+
+        if (!$inviteeDTO->isValid()) {
+            return $this->buildErrorResponse('invalid invitee details', status: Response::HTTP_NOT_ACCEPTABLE);
+        }
+
+        /** @var ?User $user */
+        $user = $this->getUser();
+
+        if (is_null($user)) {
+            return $this->buildErrorResponse('invalid inviting user', Response::HTTP_FORBIDDEN);
+        }
+
+        $success = $this->courtOrderInviteService->invite($uid, $user, $inviteeDTO);
+
+        return new JsonResponse(data: ['invitedSuccessfully' => $success], json: true);
     }
 }
