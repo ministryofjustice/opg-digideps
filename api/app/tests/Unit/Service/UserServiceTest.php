606e25ae56cc6cefa6af40ba2d43b91b77923bfd
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-07-29 14:19:55 +0100

    Add test for user with duplicate deputy UID

--- api/app/tests/Unit/Service/UserServiceTest.php
+++ api/app/tests/Unit/Service/UserServiceTest.php
@@ -7,11 +7,15 @@ use App\Entity\User;
 use App\Repository\ClientRepository;
 use App\Repository\UserRepository;
 use App\Service\UserService;
+use App\v2\DTO\InviteeDto;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityRepository;
 use Mockery\MockInterface;
 use MockeryStub as m;
 use PHPUnit\Framework\TestCase;
+
+use function PHPUnit\Framework\assertInstanceOf;
+use function PHPUnit\Framework\isInstanceOf;
 
 class UserServiceTest extends TestCase
 {
@@ -24,10 +28,13 @@ class UserServiceTest extends TestCase
     public function setUp(): void
     {
         $this->user = new User();
+        $this->user->setRoleName(User::ROLE_LAY_DEPUTY);
+
         $client = new Client();
         $client->addUser($this->user);
         $client->setCaseNumber('12345678');
         $client->setCourtDate(new \DateTime('2014-06-06'));
+
         $email = 'test@tester.co.uk';
 
         $this->em = m::mock(EntityManager::class);
@@ -81,5 +88,64 @@ class UserServiceTest extends TestCase
         $this->sut->addUser($loggedInUser, $userToAdd, $clientId);
 
         $this->assertEquals($expectedRoute, $userToAdd->getRegistrationRoute());
+    }
+
+    // get or add a user when an existing user with the same email address already exists
+    public function testGetOrAddUserExistingUser(): void
+    {
+        $clientId = 99919992;
+
+        $invitee = new InviteeDto('foo@bar.com', 'Karban', 'Steelcore');
+
+        $existingUser = m::mock(User::class);
+        $existingUser->shouldReceive('getRegistrationToken')->andReturn(null);
+        $existingUser->shouldReceive('recreateRegistrationToken');
+        $existingUser->shouldReceive('setActive')->with(true);
+
+        $this->userRepository->shouldReceive('findOneBy')
+            ->with(['email' => 'foo@bar.com'])
+            ->andReturn($existingUser);
+
+        $this->clientRepository->shouldReceive('saveUserToClient')
+            ->with($existingUser, $clientId);
+
+        $this->em->shouldReceive('persist')->with($existingUser);
+        $this->em->shouldReceive('flush');
+
+        $user = $this->sut->getOrAddUser($invitee, $this->user, 12345667, $clientId);
+
+        self::assertEquals($existingUser, $user);
+    }
+
+    // get or add a user when an existing user with the same deputy UID (but different email) exists
+    public function testGetOrAddUserPrimaryExists(): void
+    {
+        $deputyUid = 9988553311;
+        $clientId = 1837478367;
+        $invitee = new InviteeDto('foo@bar.com', 'Karban', 'Steelcore');
+
+        $existingUser = m::mock(User::class);
+
+        $this->userRepository->shouldReceive('findOneBy')
+            ->with(['email' => 'foo@bar.com'])
+            ->andReturn(null);
+
+        $this->userRepository->shouldReceive('findOneBy')
+            ->with([
+                'deputyUid' => $deputyUid,
+                'active' => true,
+                'isPrimary' => true,
+            ])
+            ->andReturn($existingUser);
+
+        $this->em->shouldReceive('persist')->with(isInstanceOf(User::class));
+        $this->em->shouldReceive('flush');
+
+        $this->clientRepository->shouldReceive('saveUserToClient')
+            ->with(isInstanceOf(User::class), $clientId);
+
+        $user = $this->sut->getOrAddUser($invitee, $this->user, $deputyUid, $clientId);
+
+        assertInstanceOf(User::class, $user);
     }
 }
