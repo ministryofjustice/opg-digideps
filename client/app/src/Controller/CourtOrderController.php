89ef17e8d3c276c6b903df2d2f616f454db8aebb
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-07-30 15:41:56 +0100

    Always show deputy list and invite link for court orders

--- client/app/src/Controller/CourtOrderController.php
+++ client/app/src/Controller/CourtOrderController.php
@@ -42,7 +42,6 @@ class CourtOrderController extends AbstractController
         $client = $this->clientApi->getById($courtOrder->getClient()->getId());
 
         $templateValues = [
-            'courtOrderHasMultipleDeputies' => count($courtOrder->getCoDeputies()) > 1,
             'coDeputies' => $courtOrder->getCoDeputies(),
             'courtOrder' => $courtOrder,
             'reportType' => $courtOrder->getActiveReportType(),
