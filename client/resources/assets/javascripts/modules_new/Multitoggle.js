8d3e728ac957e4a326f6f27036ae6121287815ed
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-07-31 16:09:50 +0100

    Fix js lint errors

--- client/resources/assets/javascripts/modules_new/Multitoggle.js
+++ client/resources/assets/javascripts/modules_new/Multitoggle.js
@@ -1,14 +1,14 @@
 // only works for yes/no radio buttons;
 // see client/app/templates/Report/Lifestyle/step.html.twig for an example
 module.exports = {
-  init: function(document) {
-    function showElement(elt) {
+  init: function (document) {
+    function showElement (elt) {
       elt.setAttribute('aria-expanded', 'true')
       elt.setAttribute('aria-hidden', 'false')
       elt.classList.remove('js-hidden')
     }
 
-    function hideElement(elt) {
+    function hideElement (elt) {
       elt.setAttribute('aria-expanded', 'false')
       elt.setAttribute('aria-hidden', 'true')
       elt.classList.add('js-hidden')
@@ -17,14 +17,14 @@ module.exports = {
     // the 'change' event only fires when a radio button is checked, not
     // when it is unchecked
     document.addEventListener('change', function (event) {
-      let elem = event.target
+      const elem = event.target
 
       if (elem || elem.matches('input[type="radio"][data-multitoggle-on]')) {
         const toggledElementsContainer = elem.closest('[data-role="multitoggle-group"]')
 
         if (elem.checked) {
-          let onElt = toggledElementsContainer.querySelector('#' + elem.getAttribute('data-multitoggle-on'))
-          let offElt = toggledElementsContainer.querySelector('#' + elem.getAttribute('data-multitoggle-off'))
+          const onElt = toggledElementsContainer.querySelector('#' + elem.getAttribute('data-multitoggle-on'))
+          const offElt = toggledElementsContainer.querySelector('#' + elem.getAttribute('data-multitoggle-off'))
 
           showElement(onElt)
           hideElement(offElt)
