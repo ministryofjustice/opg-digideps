eaf1a0817b8d8fcea7db51fc69d9e18edf468038
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-07-31 14:10:40 +0100

    Add show/hide to multitoggle

--- client/resources/assets/javascripts/modules_new/Multitoggle.js
+++ client/resources/assets/javascripts/modules_new/Multitoggle.js
@@ -2,12 +2,33 @@
 // see client/app/templates/Report/Lifestyle/step.html.twig for an example
 module.exports = {
   init: function(document) {
-    document.addEventListener('click', function (event) {
+    function showElement(elt) {
+      elt.setAttribute('aria-expanded', 'true')
+      elt.setAttribute('aria-hidden', 'false')
+      elt.classList.remove('js-hidden')
+    }
+
+    function hideElement(elt) {
+      elt.setAttribute('aria-expanded', 'false')
+      elt.setAttribute('aria-hidden', 'true')
+      elt.classList.add('js-hidden')
+    }
+
+    // the 'change' event only fires when a radio button is checked, not
+    // when it is unchecked
+    document.addEventListener('change', function (event) {
       let elem = event.target
 
-      if (elem && elem.matches('input[type="radio"][data-multitoggle-on],input[type="radio"][data-multitoggle-off]')) {
-        console.log('MULTITOGGLE')
-        console.log(elem)
+      if (elem || elem.matches('input[type="radio"][data-multitoggle-on]')) {
+        const toggledElementsContainer = elem.closest('[data-role="multitoggle-group"]')
+
+        if (elem.checked) {
+          let onElt = toggledElementsContainer.querySelector('#' + elem.getAttribute('data-multitoggle-on'))
+          let offElt = toggledElementsContainer.querySelector('#' + elem.getAttribute('data-multitoggle-off'))
+
+          showElement(onElt)
+          hideElement(offElt)
+        }
       }
     })
   }
