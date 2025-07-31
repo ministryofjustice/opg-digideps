354f9b5f1c645acef2d4d85040edc3f334627e35
ramchale <legionsofbob@gmail.com>
2025-07-31 15:35:16 +0100

    DDLS-862 Remove sensio/framework-extra-bundle from the API
    Signed-off-by: ramchale <legionsofbob@gmail.com>

--- api/app/src/v2/Registration/Controller/OrgDeputyshipController.php
+++ api/app/src/v2/Registration/Controller/OrgDeputyshipController.php
@@ -9,24 +9,24 @@ use App\Service\Formatter\RestFormatter;
 use App\v2\Controller\ControllerTrait;
 use App\v2\Registration\DeputyshipProcessing\CSVDeputyshipProcessing;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class OrgDeputyshipController extends AbstractController
 {
     use ControllerTrait;
 
     public function __construct(
-        private DataCompression $dataCompression,
-        private CSVDeputyshipProcessing $csvProcessing,
-        private RestFormatter $restFormatter
+        private readonly DataCompression $dataCompression,
+        private readonly CSVDeputyshipProcessing $csvProcessing,
+        private readonly RestFormatter $restFormatter
     ) {
     }
 
     #[Route(path: '/org-deputyships', methods: ['POST'])]
-    #[Security("is_granted('ROLE_ADMIN')")]
-    public function create(Request $request)
+    #[IsGranted(attribute: 'ROLE_ADMIN')]
+    public function create(Request $request): array
     {
         $decompressedData = $this->dataCompression->decompress($request->getContent());
 
