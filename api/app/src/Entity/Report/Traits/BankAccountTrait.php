bea647f78cc586100f2b50988ec335878087ef0e
Richard McHale <legionsofbob@gmail.com>
2025-08-27 12:05:38 +0100

    Declare types in API ReportInterface (#2116)

--- api/app/src/Entity/Report/Traits/BankAccountTrait.php
+++ api/app/src/Entity/Report/Traits/BankAccountTrait.php
@@ -2,14 +2,18 @@
 
 namespace App\Entity\Report\Traits;
 
+use App\Entity\BankAccountInterface;
 use App\Entity\Report\BankAccount;
 use App\Entity\Report\Report;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use JMS\Serializer\Annotation as JMS;
 
 trait BankAccountTrait
 {
     /**
+     * @var Collection<int, BankAccountInterface>|BankAccountInterface[]
+     *
      * @JMS\Groups({"account"})
      *
      * @JMS\Type("ArrayCollection<App\Entity\Report\BankAccount>")
@@ -18,7 +22,7 @@ trait BankAccountTrait
      *
      * @ORM\OrderBy({"id" = "ASC"})
      */
-    private $bankAccounts;
+    private Collection|array $bankAccounts;
 
     /**
      * Add accounts.
@@ -41,19 +45,17 @@ trait BankAccountTrait
     }
 
     /**
-     * Get accounts.
-     *
-     * @return BankAccount[]
+     * @return Collection<int, BankAccountInterface>|BankAccountInterface[]
      */
-    public function getBankAccounts()
+    public function getBankAccounts(): Collection|array
     {
         return $this->bankAccounts;
     }
 
     /**
-     * @return BankAccount[]
+     * @return Collection<int, BankAccountInterface>|BankAccountInterface[]
      */
-    public function getBankAccountsIncomplete()
+    public function getBankAccountsIncomplete(): Collection|array
     {
         return $this->getBankAccounts()->filter(function ($b) {
             return null == $b->getClosingBalance();
