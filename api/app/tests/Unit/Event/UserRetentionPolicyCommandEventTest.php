facdf8761d5899461c5f61717491adc21ddff276
ramchale <legionsofbob@gmail.com>
2025-08-28 13:27:21 +0100

    Tidy API Event unit tests
    Signed-off-by: ramchale <legionsofbob@gmail.com>

--- api/app/tests/Unit/Event/UserRetentionPolicyCommandEventTest.php
+++ api/app/tests/Unit/Event/UserRetentionPolicyCommandEventTest.php
@@ -1,22 +1,24 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Tests\Unit\Event;
 
+use PHPUnit\Framework\Attributes\Test;
+use DateTime;
 use App\Entity\User;
 use App\Event\UserRetentionPolicyCommandEvent;
 use App\TestHelpers\UserTestHelper;
 use PHPUnit\Framework\TestCase;
 
-class UserRetentionPolicyCommandEventTest extends TestCase
+final class UserRetentionPolicyCommandEventTest extends TestCase
 {
-    /**
-     * @test
-     */
-    public function eventIsInitialisedCorrectly()
+    #[Test]
+    public function eventIsInitialisedCorrectly(): void
     {
         $deletedAdminUser = UserTestHelper::create();
         $user = $deletedAdminUser->createUser(null, User::ROLE_ADMIN_MANAGER)
-            ->setLastLoggedIn(new \DateTime('-36 months'));
+            ->setLastLoggedIn(new DateTime('-36 months'));
         $trigger = 'A_TRIGGER';
 
         $event = new UserRetentionPolicyCommandEvent($user, $trigger);
