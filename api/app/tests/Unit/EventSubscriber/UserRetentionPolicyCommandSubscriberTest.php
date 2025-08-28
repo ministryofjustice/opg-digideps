f9daef4f7998f06fd4ff288e398e7da25d61b4bc
ramchale <legionsofbob@gmail.com>
2025-08-28 13:28:17 +0100

    Tidy API EventSubscriber unit tests
    Signed-off-by: ramchale <legionsofbob@gmail.com>

--- api/app/tests/Unit/EventSubscriber/UserRetentionPolicyCommandSubscriberTest.php
+++ api/app/tests/Unit/EventSubscriber/UserRetentionPolicyCommandSubscriberTest.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Tests\Unit\EventSubscriber;
 
+use App\Service\Time\DateTimeProvider;
+use PHPUnit\Framework\Attributes\Test;
+use DateTime;
 use App\Entity\User;
 use App\Event\UserRetentionPolicyCommandEvent;
 use App\EventSubscriber\UserRetentionPolicyCommandSubscriber;
@@ -14,35 +17,29 @@ use Prophecy\Prophecy\ObjectProphecy;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
-class UserRetentionPolicyCommandSubscriberTest extends KernelTestCase
+final class UserRetentionPolicyCommandSubscriberTest extends KernelTestCase
 {
     use ProphecyTrait;
 
     private ObjectProphecy $logger;
-
-    private \App\Service\Time\DateTimeProvider $dateTimeProvider;
-
+    private DateTimeProvider $dateTimeProvider;
     private UserRetentionPolicyCommandSubscriber $sut;
 
     public function setUp(): void
     {
-        $this->dateTimeProvider = new \App\Service\Time\DateTimeProvider();
+        $this->dateTimeProvider = new DateTimeProvider();
     }
 
-    /**
-     * @test
-     */
-    public function getSubscribedEvents()
+    #[Test]
+    public function getSubscribedEvents(): void
     {
         self::assertEquals([
             UserRetentionPolicyCommandEvent::NAME => 'logEvent',
         ], UserRetentionPolicyCommandSubscriber::getSubscribedEvents());
     }
 
-    /**
-     * @test
-     */
-    public function logEvent()
+    #[Test]
+    public function logEvent(): void
     {
         $this->logger = self::prophesize(LoggerInterface::class);
 
@@ -55,16 +52,16 @@ class UserRetentionPolicyCommandSubscriberTest extends KernelTestCase
         $user = UserTestHelper::create();
         $deletedAdminUser = $user->createUser(null, User::ROLE_ADMIN_MANAGER);
         $deletedAdminUser->setId(1);
-        $deletedAdminUser->setLastLoggedIn(new \DateTime('-25 months'));
+        $deletedAdminUser->setLastLoggedIn(new DateTime('-25 months'));
 
-        $now = new \DateTime();
+        $now = new DateTime();
 
         $expectedEvent = [
             'trigger' => $trigger,
             'id' => $deletedAdminUser->getId(),
             'email_address' => $deletedAdminUser->getEmail(),
             'message' => 'Deletion due to two year retention policy',
-            'deleted_on' => $now->format(\DateTime::ATOM),
+            'deleted_on' => $now->format(DateTime::ATOM),
             'event' => AuditEvents::USER_DELETED_AUTOMATION,
             'type' => 'audit',
         ];
