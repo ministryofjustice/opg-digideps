1bf77cb51b7631a0f7abce38fb1afe081214d171
Richard McHale <legionsofbob@gmail.com>
2025-08-29 14:19:01 +0100

    Revert "Remove Gedmo\Timestampable from ClientBenefitsCheck (#2120)"
    This reverts commit 775fc4a569f3829c4f5a0992103da693616fd357.

--- api/app/src/Entity/Report/ClientBenefitsCheck.php
+++ api/app/src/Entity/Report/ClientBenefitsCheck.php
@@ -10,6 +10,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\Mapping\OrderBy;
+use Gedmo\Mapping\Annotation as Gedmo;
 use JMS\Serializer\Annotation as JMS;
 use Ramsey\Uuid\Doctrine\UuidGenerator;
 use Ramsey\Uuid\Uuid;
@@ -46,6 +47,8 @@ class ClientBenefitsCheck implements ClientBenefitsCheckInterface
 
     /**
      * @ORM\Column(name="created_at", type="datetime",nullable=true)
+     *
+     * @Gedmo\Timestampable(on="create")
      *
      * @JMS\Groups({"client-benefits-check"})
      *
