30a7923c7bd12a13e3a25bb0a2faa221572b2742
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-09-02 17:34:27 +0100

    Start implementing method to add missing deputies to users

--- api/app/src/Service/UserDeputyService.php
+++ api/app/src/Service/UserDeputyService.php
@@ -4,10 +4,32 @@ declare(strict_types=1);
 
 namespace App\Service;
 
+use App\Repository\PreRegistrationRepository;
+
 class UserDeputyService
 {
-    public function addMissingUserDeputyAssociations(): int
+    public function __construct(
+        private readonly PreRegistrationRepository $preRegistrationRepository,
+        private readonly DeputyService $deputyService,
+    ) {
+    }
+
+    /**
+     * Create deputy records for deputy UIDs in pre_registration where they don't exist.
+     * Associate users with deputy records where they aren't already associated.
+     */
+    public function addMissingUserDeputies(): int
     {
+        // find pre-reg rows whose deputy UIDs aren't in the deputy table
+        $preRegs = $this->preRegistrationRepository->findWithoutDeputies();
+
+        // add deputy records for those UIDs, using pre-reg data
+        foreach ($preRegs as $preReg) {
+            $this->deputyService->createDeputyFromPreRegistration($preReg);
+        }
+
+        // find users who have no deputy associated with them
+
         return 0;
     }
 }
