ff2dc70e566d4a0237886d5ea806500b705e5ddc
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-09-02 17:13:34 +0100

    Refactor deputy populating method

--- api/app/src/Service/DeputyService.php
+++ api/app/src/Service/DeputyService.php
@@ -4,6 +4,7 @@ namespace App\Service;
 
 use App\Entity\Deputy;
 use App\Entity\User;
+use App\Model\Hydrator;
 use App\Repository\DeputyRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
@@ -31,5 +32,32 @@ class DeputyService
         $this->em->flush();
 
         return $deputyToAdd;
+    }
+
+    public function populateDeputy($deputy, $data): Deputy
+    {
+        Hydrator::hydrateEntityWithArrayData($deputy, $data, [
+            'firstname' => 'setFirstname',
+            'lastname' => 'setLastname',
+            'address1' => 'setAddress1',
+            'address2' => 'setAddress2',
+            'address3' => 'setAddress3',
+            'address4' => 'setAddress4',
+            'address5' => 'setAddress5',
+            'address_postcode' => 'setAddressPostcode',
+            'address_country' => 'setAddressCountry',
+            'phone_alternative' => 'setPhoneAlternative',
+            'phone_main' => 'setPhoneMain',
+        ]);
+
+        if (array_key_exists('email', $data) && !empty($data['email'])) {
+            $deputy->setEmail1($data['email']);
+        }
+
+        if (array_key_exists('deputy_uid', $data) && !empty($data['deputy_uid'])) {
+            $deputy->setDeputyUid($data['deputy_uid']);
+        }
+
+        return $deputy;
     }
 }
