cb90d53b50773c1ad57322145d4308cba82f7c92
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-09-09 14:30:10 +0100

    If file is too big, return false so form does not submit

--- client/resources/assets/javascripts/modules_new/UploadFile.js
+++ client/resources/assets/javascripts/modules_new/UploadFile.js
@@ -25,18 +25,19 @@ const uploadFile = {
         return true
       }
 
+      // don't allow file selector to be clicked again
+      elt.disabled = "disabled"
+
       // if any file is too large, redirect to error page
       const files = elt.files
       for (let i = 0; i < files.length; i++) {
         if (files[i].size > UPLOAD_LIMIT) {
           window.location = form.action + "?error=tooBig"
+          return false
         }
       }
 
-      // disable the file chooser
-      elt.disabled = "disabled"
-
-      // show progress bar
+      // show progress
       form.querySelector("[data-role=file-chooser-form-progress]")?.classList.remove("hidden")
 
       form.submit()
