5377899a89a4dd0e330e88fd0eec578598e79b54
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-09-10 09:09:10 +0100

    Refactor UploadFile so it can be tested more easily

--- client/resources/assets/javascripts/modules_new/UploadFile.js
+++ client/resources/assets/javascripts/modules_new/UploadFile.js
@@ -1,51 +1,52 @@
 const UPLOAD_LIMIT = 15 * 1024 * 1024
 const uploadFile = {
-  init: function (document) {
-    document.addEventListener('change', event => {
-      const elt = event.target
+  handleChangeEvent: function (event) {
+    const elt = event.target
 
-      // if not a file element or no files selected, do nothing
-      if (
-        elt.nodeName !== 'INPUT' ||
-        elt.type !== 'file' ||
-        elt.getAttribute('id') !== 'report_document_upload_files' ||
-        elt.files.length < 1
-      ) {
-        return true
-      }
-
-      const form = elt.form
-
-      // if not a file chooser form, do nothing
-      if (
-        form === 'undefined' ||
-        form.nodeName !== 'FORM' ||
-        form.getAttribute('data-role') !== 'file-chooser-form'
-      ) {
-        return true
-      }
-
-      // if any file is too large, redirect to error page
-      const files = elt.files
-      for (let i = 0; i < files.length; i++) {
-        if (files[i].size > UPLOAD_LIMIT) {
-          window.location = form.action + '?error=tooBig'
-          return false
-        }
-      }
-
-      // show progress
-      form.querySelector('[data-role=file-chooser-form-progress]')?.classList.remove('hidden')
-
-      form.submit()
-
-      // don't allow file selector to be clicked again;
-      // NB this has to happen *after* the submit otherwise no files are sent
-      elt.disabled = 'disabled'
-
+    // if not a file element or no files selected, do nothing
+    if (
+      elt.nodeName !== 'INPUT' ||
+      elt.type !== 'file' ||
+      elt.getAttribute('id') !== 'report_document_upload_files' ||
+      elt.files.length < 1
+    ) {
       return true
-    })
-  }
+    }
 
+    const form = elt.form
+
+    // if not a file chooser form, do nothing
+    if (
+      form === 'undefined' ||
+      form.nodeName !== 'FORM' ||
+      form.getAttribute('data-role') !== 'file-chooser-form'
+    ) {
+      return true
+    }
+
+    // if any file is too large, redirect to error page
+    const files = elt.files
+    for (let i = 0; i < files.length; i++) {
+      if (files[i].size > UPLOAD_LIMIT) {
+        window.location = form.action + '?error=tooBig'
+        return false
+      }
+    }
+
+    // show progress
+    form.querySelector('[data-role=file-chooser-form-progress]')?.classList.remove('hidden')
+
+    form.submit()
+
+    // don't allow file selector to be clicked again;
+    // NB this has to happen *after* the submit otherwise no files are sent
+    elt.disabled = 'disabled'
+
+    return true
+  },
+
+  init: function (document) {
+    document.addEventListener('change', this.handleChangeEvent)
+  }
 }
 export default uploadFile
