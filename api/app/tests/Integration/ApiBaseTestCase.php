cc755858539e0e954d2bd0520f56a5672f4a44da
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-09-11 11:35:17 +0100

    Move db purge and cleanup after test class has run

--- api/app/tests/Integration/ApiBaseTestCase.php
+++ api/app/tests/Integration/ApiBaseTestCase.php
@@ -11,37 +11,55 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class ApiBaseTestCase extends KernelTestCase
 {
-    protected EntityManagerInterface $entityManager;
+    protected static ?EntityManagerInterface $staticEntityManager;
+    protected static ?ContainerInterface $staticContainer;
     protected ContainerInterface $container;
+    protected EntityManagerInterface $entityManager;
 
     protected function setUp(): void
     {
         parent::setUp();
+        $this->container = self::$staticContainer;
+        $this->entityManager = self::$staticEntityManager;
+    }
+
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
 
         self::bootKernel(['environment' => 'test']);
 
         // static::getContainer() is subtly different from $kernel->getContainer();
         // the latter doesn't always work, depending on the test, but the former does
-        $this->container = static::getContainer();
+        self::$staticContainer = self::$kernel->getContainer();
 
-        $this->entityManager = $this->container->get('doctrine')->getManager();
+        self::$staticEntityManager = self::$staticContainer->get('doctrine')->getManager();
     }
 
     protected function tearDown(): void
     {
         parent::tearDown();
-
-        $this->purgeDatabase();
-        $this->entityManager->close();
         unset($this->entityManager);
+        unset($this->container);
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        parent::tearDownAfterClass();
+
+        self::purgeDatabase();
+
+        self::$staticEntityManager->close();
+        self::$staticEntityManager = null;
+        self::$staticContainer = null;
     }
 
     /**
      * @param string[] $excludeTables Tables to exclude from purge
      */
-    protected function purgeDatabase(array $excludeTables = ['dd_user']): void
+    protected static function purgeDatabase(array $excludeTables = ['dd_user']): void
     {
-        $purger = new ORMPurger($this->entityManager, $excludeTables);
+        $purger = new ORMPurger(self::$staticEntityManager, $excludeTables);
         $purger->setPurgeMode(ORMPurger::PURGE_MODE_DELETE);
         $purger->purge();
     }
