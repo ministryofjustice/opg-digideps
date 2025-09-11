5454afbc3bf623a7bea8fb461d15ac6775d02682
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-09-11 16:43:44 +0100

    Make test more discriminating

--- api/app/tests/Integration/Entity/Repository/UserRepositoryTest.php
+++ api/app/tests/Integration/Entity/Repository/UserRepositoryTest.php
@@ -13,67 +13,56 @@ use App\Tests\Integration\Fixtures;
 
 class UserRepositoryTest extends ApiBaseTestCase
 {
-    private UserRepository $sut;
-    private Fixtures $fixtures;
+    private static Fixtures $fixtures;
+    private static UserRepository $sut;
 
-    protected function setUp(): void
+    public static function setUpBeforeClass(): void
     {
-        parent::setUp();
+        parent::setUpBeforeClass();
 
-        $this->fixtures = new Fixtures($this->entityManager);
+        self::$fixtures = new Fixtures(self::$staticEntityManager);
 
         /** @var UserRepository $sut */
-        $sut = $this->entityManager->getRepository(User::class);
-        $this->sut = $sut;
+        $sut = self::$staticEntityManager->getRepository(User::class);
 
-        // important: purge the dd_user table
-        $this->purgeDatabase([]);
-    }
-
-    protected function tearDown(): void
-    {
-        // important: purge the dd_user table
-        $this->purgeDatabase([]);
-
-        $this->entityManager->close();
-        unset($this->entityManager);
+        self::$sut = $sut;
     }
 
     public function testCountsInactiveUsers()
     {
-        $oldUserWithNoClient = $this->fixtures->createUser();
+        $oldUserWithNoClient = self::$fixtures->createUser();
         $oldUserWithNoClient->setRegistrationDate(DateTime::createFromFormat('Y-m-d', '2019-03-03'));
         $oldUserWithNoClient->setRoleName(User::ROLE_LAY_DEPUTY);
 
-        $oldUserWithNoReports = $this->fixtures->createUser();
+        $oldUserWithNoReports = self::$fixtures->createUser();
         $oldUserWithNoReports->setRegistrationDate(DateTime::createFromFormat('Y-m-d', '2019-03-03'));
         $oldUserWithNoReports->setRoleName(User::ROLE_LAY_DEPUTY);
-        $this->fixtures->createClient($oldUserWithNoReports);
+        self::$fixtures->createClient($oldUserWithNoReports);
 
-        $oldUserWithReport = $this->fixtures->createUser();
+        $oldUserWithReport = self::$fixtures->createUser();
         $oldUserWithReport->setRegistrationDate(DateTime::createFromFormat('Y-m-d', '2019-03-03'));
         $oldUserWithReport->setRoleName(User::ROLE_LAY_DEPUTY);
-        $oldClientWithReport = $this->fixtures->createClient($oldUserWithReport);
-        $this->fixtures->createReport($oldClientWithReport);
+        $oldClientWithReport = self::$fixtures->createClient($oldUserWithReport);
+        self::$fixtures->createReport($oldClientWithReport);
 
-        $oldUserWithNdr = $this->fixtures->createUser();
+        $oldUserWithNdr = self::$fixtures->createUser();
         $oldUserWithNdr->setRegistrationDate(DateTime::createFromFormat('Y-m-d', '2019-03-03'));
         $oldUserWithNdr->setRoleName(User::ROLE_LAY_DEPUTY);
-        $oldClientWithNdr = $this->fixtures->createClient($oldUserWithNdr);
-        $this->fixtures->createNdr($oldClientWithNdr);
+        $oldClientWithNdr = self::$fixtures->createClient($oldUserWithNdr);
+        self::$fixtures->createNdr($oldClientWithNdr);
 
-        $oldProfUserWithNoClient = $this->fixtures->createUser();
+        $oldProfUserWithNoClient = self::$fixtures->createUser();
         $oldProfUserWithNoClient->setRegistrationDate(DateTime::createFromFormat('Y-m-d', '2019-03-03'));
         $oldProfUserWithNoClient->setRoleName(User::ROLE_PROF_ADMIN);
 
-        $recentUserWithNoClient = $this->fixtures->createUser();
+        $recentUserWithNoClient = self::$fixtures->createUser();
         $recentUserWithNoClient->setRegistrationDate(new DateTime());
         $recentUserWithNoClient->setRoleName(User::ROLE_LAY_DEPUTY);
-        $this->fixtures->createClient($recentUserWithNoClient);
+        self::$fixtures->createClient($recentUserWithNoClient);
 
-        $this->entityManager->flush();
+        self::$staticEntityManager->flush();
 
-        $inactiveUsers = $this->sut->findInactive();
+        $inactiveUsers = self::$sut->findInactive();
 
         self::assertCount(2, $inactiveUsers);
     }
@@ -84,37 +73,37 @@ class UserRepositoryTest extends ApiBaseTestCase
         $reportHelper = ReportTestHelper::create();
         $clientHelper = ClientTestHelper::create();
 
-        $clientOne = $clientHelper->generateClient($this->entityManager);
-        $activeUserOne = $userHelper->createAndPersistUser($this->entityManager, $clientOne);
-        $reportOne = $reportHelper->generateReport($this->entityManager, $clientOne)->setSubmitDate(new DateTime());
+        $clientOne = $clientHelper->generateClient(self::$staticEntityManager);
+        $activeUserOne = $userHelper->createAndPersistUser(self::$staticEntityManager, $clientOne);
+        $reportOne = $reportHelper->generateReport(self::$staticEntityManager, $clientOne)->setSubmitDate(new DateTime());
 
-        $clientTwo = $clientHelper->generateClient($this->entityManager);
-        $activeUserTwo = $userHelper->createAndPersistUser($this->entityManager, $clientTwo);
-        $reportTwo = $reportHelper->generateReport($this->entityManager, $clientTwo)->setSubmitDate(new DateTime());
+        $clientTwo = $clientHelper->generateClient(self::$staticEntityManager);
+        $activeUserTwo = $userHelper->createAndPersistUser(self::$staticEntityManager, $clientTwo);
+        $reportTwo = $reportHelper->generateReport(self::$staticEntityManager, $clientTwo)->setSubmitDate(new DateTime());
 
-        $clientThree = $clientHelper->generateClient($this->entityManager);
-        $reportThree = $reportHelper->generateReport($this->entityManager, $clientThree)->setSubmitDate(new DateTime());
-        $inactiveUserOne = $userHelper->createAndPersistUser($this->entityManager, $clientThree);
+        $clientThree = $clientHelper->generateClient(self::$staticEntityManager);
+        $reportThree = $reportHelper->generateReport(self::$staticEntityManager, $clientThree)->setSubmitDate(new DateTime());
+        $inactiveUserOne = $userHelper->createAndPersistUser(self::$staticEntityManager, $clientThree);
         $inactiveUserOne->setLastLoggedIn(new DateTime('-380 days'));
 
-        $clientFour = $clientHelper->generateClient($this->entityManager);
-        $reportFour = $reportHelper->generateReport($this->entityManager, $clientFour);
-        $inactiveUserTwo = $userHelper->createAndPersistUser($this->entityManager, $clientFour);
+        $clientFour = $clientHelper->generateClient(self::$staticEntityManager);
+        $reportFour = $reportHelper->generateReport(self::$staticEntityManager, $clientFour);
+        $inactiveUserTwo = $userHelper->createAndPersistUser(self::$staticEntityManager, $clientFour);
         $inactiveUserTwo->setLastLoggedIn(new DateTime());
 
-        $this->entityManager->persist($inactiveUserOne);
-        $this->entityManager->persist($inactiveUserTwo);
-        $this->entityManager->persist($reportOne);
-        $this->entityManager->persist($reportTwo);
-        $this->entityManager->persist($reportThree);
-        $this->entityManager->persist($reportFour);
-        $this->entityManager->persist($clientOne);
-        $this->entityManager->persist($clientTwo);
-        $this->entityManager->persist($clientThree);
-        $this->entityManager->persist($clientFour);
-        $this->entityManager->flush();
+        self::$staticEntityManager->persist($inactiveUserOne);
+        self::$staticEntityManager->persist($inactiveUserTwo);
+        self::$staticEntityManager->persist($reportOne);
+        self::$staticEntityManager->persist($reportTwo);
+        self::$staticEntityManager->persist($reportThree);
+        self::$staticEntityManager->persist($reportFour);
+        self::$staticEntityManager->persist($clientOne);
+        self::$staticEntityManager->persist($clientTwo);
+        self::$staticEntityManager->persist($clientThree);
+        self::$staticEntityManager->persist($clientFour);
+        self::$staticEntityManager->flush();
 
-        $results = $this->sut->findActiveLaysInLastYear();
+        $results = self::$sut->findActiveLaysInLastYear();
         $resultsUserIds = [];
 
         foreach ($results as $userData) {
@@ -134,21 +123,24 @@ class UserRepositoryTest extends ApiBaseTestCase
         $usersToAdd[] = $adminUser = $userHelper->createUser(null, User::ROLE_ADMIN);
         $usersToAdd[] = $adminManagerUser = $userHelper->createUser(null, User::ROLE_ADMIN_MANAGER);
         $usersToAdd[] = $superAdminUser = $userHelper->createUser(null, User::ROLE_ADMIN_MANAGER);
-        $usersToAdd[] = $layDeputyUser = $userHelper->createUser(null, User::ROLE_LAY_DEPUTY);
+        $usersToAdd[] = $layDeputyUser = $userHelper->createUser();
         $usersToAdd[] = $profDeputyUser = $userHelper->createUser(null, User::ROLE_PROF_ADMIN);
         $usersToAdd[] = $paDeputyUser = $userHelper->createUser(null, User::ROLE_PROF_ADMIN);
 
         foreach ($usersToAdd as $user) {
-            $this->entityManager->persist($user);
+            self::$staticEntityManager->persist($user);
         }
 
-        $this->entityManager->flush();
+        self::$staticEntityManager->flush();
 
         $expectedAdminUsersReturned = [$adminUser, $adminManagerUser, $superAdminUser];
         $expectedDeputyUsersNotReturned = [$layDeputyUser, $profDeputyUser, $paDeputyUser];
-        $actualAdminUsers = $this->sut->getAllAdminAccounts();
 
-        self::assertEquals($expectedAdminUsersReturned, $actualAdminUsers);
+        $actualAdminUsers = self::$sut->getAllAdminAccounts();
+
+        foreach ($expectedAdminUsersReturned as $adminUser) {
+            self::assertContains($adminUser, $actualAdminUsers);
+        }
 
         foreach ($expectedDeputyUsersNotReturned as $deputyUser) {
             self::assertNotContains($deputyUser, $actualAdminUsers);
@@ -177,15 +169,15 @@ class UserRepositoryTest extends ApiBaseTestCase
         $nonAdminUserLessThan60Days->setLastLoggedIn(null);
 
         foreach ($usersToAdd as $user) {
-            $this->entityManager->persist($user);
+            self::$staticEntityManager->persist($user);
         }
 
-        $this->entityManager->flush();
+        self::$staticEntityManager->flush();
 
         $expectedAdminUsersReturned = [$adminUserMoreThan60Days, $superAdminUserMoreThan60Days, $adminManagerUserMoreThan60Days];
         $expectedAdminUsersNotReturned = [$adminUserLessThan60Days, $nonAdminUserLessThan60Days];
 
-        $actualAdminUsers = $this->sut->getAllAdminAccountsCreatedButNotActivatedWithin('-60 days');
+        $actualAdminUsers = self::$sut->getAllAdminAccountsCreatedButNotActivatedWithin('-60 days');
 
         self::assertEquals($expectedAdminUsersReturned, $actualAdminUsers);
 
@@ -211,17 +203,19 @@ class UserRepositoryTest extends ApiBaseTestCase
         $activeDeputyUser->setLastLoggedIn(new DateTime());
 
         foreach ($usersToAdd as $user) {
-            $this->entityManager->persist($user);
+            self::$staticEntityManager->persist($user);
         }
 
-        $this->entityManager->flush();
+        self::$staticEntityManager->flush();
 
         $expectedActiveAdminUsersReturned = [$activeAdminUser, $activeSuperAdminUser, $activeAdminManagerUser];
         $expectedAdminUsersNotReturned = [$inactiveAdminManagerUser, $activeDeputyUser];
 
-        $actualAdminUsers = $this->sut->getAllActivatedAdminAccounts();
+        $actualAdminUsers = self::$sut->getAllActivatedAdminAccounts();
 
-        self::assertEquals($expectedActiveAdminUsersReturned, $actualAdminUsers);
+        foreach ($expectedActiveAdminUsersReturned as $adminUser) {
+            self::assertContains($adminUser, $actualAdminUsers);
+        }
 
         foreach ($expectedAdminUsersNotReturned as $user) {
             self::assertNotContains($user, $actualAdminUsers);
@@ -245,15 +239,15 @@ class UserRepositoryTest extends ApiBaseTestCase
         $recentlyLoggedInDeputyUser->setLastLoggedIn(new DateTime('-1 day'));
 
         foreach ($usersToAdd as $user) {
-            $this->entityManager->persist($user);
+            self::$staticEntityManager->persist($user);
         }
 
-        $this->entityManager->flush();
+        self::$staticEntityManager->flush();
 
         $expectedLoggedInAdminUsers = [$loggedInAdminUser, $loggedInSuperAdminUser, $loggedInAdminManagerUser];
         $expectedRecentlyLoggedInUsersNotReturned = [$recentlyLoggedInAdminManagerUser, $recentlyLoggedInDeputyUser];
 
-        $actualLoggedOutUsers = $this->sut->getAllAdminAccountsNotUsedWithin('-90 days');
+        $actualLoggedOutUsers = self::$sut->getAllAdminAccountsNotUsedWithin('-90 days');
 
         self::assertEquals($expectedLoggedInAdminUsers, $actualLoggedOutUsers);
 
@@ -270,7 +264,7 @@ class UserRepositoryTest extends ApiBaseTestCase
         $usersToAdd[] = $loggedInSuperAdminUser = $userHelper->createUser(null, User::ROLE_SUPER_ADMIN);
         $usersToAdd[] = $loggedInAdminManagerUser = $userHelper->createUser(null, User::ROLE_ADMIN_MANAGER);
         $usersToAdd[] = $notRecentlyLoggedInAdminManagerUser = $userHelper->createUser(null, User::ROLE_ADMIN_MANAGER);
-        $usersToAdd[] = $notRecentlyLoggedInDeputyUser = $userHelper->createUser(null, User::ROLE_LAY_DEPUTY);
+        $usersToAdd[] = $notRecentlyLoggedInDeputyUser = $userHelper->createUser();
 
         $loggedInAdminUser->setLastLoggedIn(new DateTime('-50 days'));
         $loggedInSuperAdminUser->setLastLoggedIn(new DateTime('-50 days'));
@@ -279,17 +273,19 @@ class UserRepositoryTest extends ApiBaseTestCase
         $notRecentlyLoggedInDeputyUser->setLastLoggedIn(new DateTime('-100 days'));
 
         foreach ($usersToAdd as $user) {
-            $this->entityManager->persist($user);
+            self::$staticEntityManager->persist($user);
         }
 
-        $this->entityManager->flush();
+        self::$staticEntityManager->flush();
 
         $expectedLoggedInAdminUsers = [$loggedInAdminUser, $loggedInSuperAdminUser, $loggedInAdminManagerUser];
         $expectedLoggedOutUsersNotReturned = [$notRecentlyLoggedInAdminManagerUser, $notRecentlyLoggedInDeputyUser];
 
-        $actualLoggedInUsers = $this->sut->getAllAdminAccountsUsedWithin('-90 days');
+        $actualLoggedInUsers = self::$sut->getAllAdminAccountsUsedWithin('-90 days');
 
-        self::assertEquals($expectedLoggedInAdminUsers, $actualLoggedInUsers);
+        foreach ($expectedLoggedInAdminUsers as $adminUser) {
+            self::assertContains($adminUser, $actualLoggedInUsers);
+        }
 
         foreach ($expectedLoggedOutUsersNotReturned as $user) {
             self::assertNotContains($user, $actualLoggedInUsers);
@@ -316,15 +312,15 @@ class UserRepositoryTest extends ApiBaseTestCase
         $recentlyLoggedInDeputyUser->setLastLoggedIn(new DateTime('-10 days'));
 
         foreach ($usersToAdd as $user) {
-            $this->entityManager->persist($user);
+            self::$staticEntityManager->persist($user);
         }
 
-        $this->entityManager->flush();
+        self::$staticEntityManager->flush();
 
         $expectedLoggedInAdminUsers = [$notRecentlyLoggedInAdminUser, $notRecentlyLoggedInSuperAdminManagerUser];
         $expectedRecentlyLoggedInUsersNotReturned = [$recentlyLoggedInAdminUser, $recentlyLoggedInAdminManagerUser, $recentlyLoggedInDeputyUser];
 
-        $actualLoggedInAdminUsers = $this->sut->getAllAdminAccountsNotUsedWithin('-12 months');
+        $actualLoggedInAdminUsers = self::$sut->getAllAdminAccountsNotUsedWithin('-12 months');
 
         self::assertEquals($expectedLoggedInAdminUsers, $actualLoggedInAdminUsers);
 
@@ -348,25 +344,25 @@ class UserRepositoryTest extends ApiBaseTestCase
             ->setLastLoggedIn(new DateTime('-26 months'));
 
         foreach ($usersToAdd as $user) {
-            $this->entityManager->persist($user);
+            self::$staticEntityManager->persist($user);
         }
 
-        $this->entityManager->flush();
+        self::$staticEntityManager->flush();
 
         $adminUserIds = [];
         foreach ($usersToAdd as $user) {
             $adminUserIds[] = $user->getId();
         }
 
-        $this->sut->deleteInactiveAdminUsers($adminUserIds);
+        self::$sut->deleteInactiveAdminUsers($adminUserIds);
 
         $adminUsersDeleted = [$inactiveAdminUser->getId(), $inactiveAdminManagerUser->getId()];
         $usersNotDeleted = [$activeAdminUser->getId(), $activeLayDeputyUser->getId()];
 
-        $deletedAdminUsers = $this->sut->findBy(['id' => $adminUsersDeleted]);
+        $deletedAdminUsers = self::$sut->findBy(['id' => $adminUsersDeleted]);
         $this->assertCount(0, $deletedAdminUsers);
 
-        $usersNotDeleted = $this->sut->findBy(['id' => $usersNotDeleted]);
+        $usersNotDeleted = self::$sut->findBy(['id' => $usersNotDeleted]);
         $this->assertCount(2, $usersNotDeleted);
     }
 }
