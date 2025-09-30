ac0f70bbe658127445c5d3d4961cc99734d71024
jamesrwarren <james.warren@digital.justice.gov.uk>
2025-09-30 13:51:54 +0100

    further sql change

--- terraform/environment/region/cloudwatch_scheduled_events.tf
+++ terraform/environment/region/cloudwatch_scheduled_events.tf
@@ -67,7 +67,43 @@ resource "aws_cloudwatch_event_target" "csv_automation_lay_processing" {
       "containerOverrides" : [
         {
           "name" : "api_app",
-          "command" : ["sh", "scripts/task_run_console_command.sh", "digideps:api:api-comparison", "client/get-all-clients-by-deputy-uid/{deputy_uid}", "v2/deputy/{deputy_id}/courtorders", "28479"]
+          "command" : ["sh", "scripts/task_run_console_command.sh", "digideps:api:api-comparison", "client/get-all-clients-by-deputy-uid/{deputy_uid}", "v2/deputy/{deputy_id}/courtorders", "all"]
+        }
+      ]
+    }
+  )
+}
+
+resource "aws_cloudwatch_event_rule" "court_order_api_comparison" {
+  name                = "court-order-api-compare-${local.environment}"
+  description         = "Test out our API comparer ${terraform.workspace}"
+  schedule_expression = "cron(0 5 * * ? *)"
+  tags                = var.default_tags
+}
+
+resource "aws_cloudwatch_event_target" "court_order_api_comparison" {
+  rule     = aws_cloudwatch_event_rule.court_order_api_comparison.name
+  arn      = aws_ecs_cluster.main.arn
+  role_arn = aws_iam_role.events_task_runner.arn
+
+  ecs_target {
+    task_count          = 1
+    task_definition_arn = aws_ecs_task_definition.api_high_memory.arn
+    launch_type         = "FARGATE"
+    platform_version    = "1.4.0"
+
+    network_configuration {
+      security_groups  = [module.api_service_security_group.id]
+      subnets          = data.aws_subnet.private[*].id
+      assign_public_ip = false
+    }
+  }
+  input = jsonencode(
+    {
+      "containerOverrides" : [
+        {
+          "name" : "api_app",
+          "command" : ["sh", "scripts/task_run_console_command.sh", "digideps:api:api-comparison", "client/get-all-clients-by-deputy-uid/{deputy_uid}", "v2/deputy/{deputy_id}/courtorders", "all"]
         }
       ]
     }
