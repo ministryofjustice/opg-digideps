2101fd7035cdcca315d6469f6804af2c93a9d461
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-10-03 17:45:42 +0100

    Move secondary user redirection to event subscriber

---
+++ client/app/src/EventSubscriber/LoginSubscriber.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EventSubscriber;
+
+use App\Entity\User;
+use App\Service\Client\Internal\UserApi;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class LoginSubscriber implements EventSubscriberInterface
+{
+    public function __construct(
+        private readonly UserApi $userApi,
+        private readonly UrlGeneratorInterface $urlGenerator,
+        private readonly TokenStorageInterface $tokenStorage,
+    ) {
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [KernelEvents::REQUEST => 'onKernelRequest'];
+    }
+
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        /** @var User $user */
+        $user = $this->tokenStorage->getToken()?->getUser();
+
+        if (!is_null($user) && !$user->getIsPrimary()) {
+            $primaryEmail = $this->userApi->returnPrimaryEmail($user->getDeputyUid());
+
+            $flashBag = $event->getRequest()->getSession()->getFlashBag();
+
+            if (is_null($primaryEmail)) {
+                $flashBag->add(
+                    'nonPrimaryRedirectUnknownEmail',
+                    [
+                        'sentenceOne' => 'This account has been closed.',
+                        'sentenceTwo' => 'You can now access all of your reports in the same place from your primary account.',
+                        'sentenceThree' => 'If you need assistance, contact your case manager on 0300 456 0300.',
+                    ]
+                );
+            } else {
+                $flashBag->add(
+                    'nonPrimaryRedirect',
+                    [
+                        'sentenceOne' => 'This account has been closed.',
+                        'sentenceTwo' => 'You can now access all of your reports in the same place from your account under',
+                        'primaryEmail' => $primaryEmail,
+                    ]
+                );
+            }
+
+            $url = $this->urlGenerator->generate('app_logout', ['notPrimaryAccount' => true]);
+            $response = new RedirectResponse($url);
+            $event->setResponse($response);
+        }
+    }
+}
