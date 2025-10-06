063ac444aa9dd1d3a6814c1a8c30ca7f1a1d214e
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-10-06 15:00:52 +0100

    Fix NDR login e2e test

--- api/app/tests/Behat/bootstrap/v2/ACL/ACLFeatureContext.php
+++ api/app/tests/Behat/bootstrap/v2/ACL/ACLFeatureContext.php
@@ -6,11 +6,13 @@ namespace App\Tests\Behat\v2\ACL;
 
 use App\Tests\Behat\Common\FormTrait;
 use App\Tests\Behat\v2\Common\BaseFeatureContext;
+use App\Tests\Behat\v2\CourtOrder\CourtOrderTrait;
 use App\Tests\Behat\v2\DeputyManagement\DeputyManagementTrait;
 
 class ACLFeatureContext extends BaseFeatureContext
 {
     use ACLTrait;
+    use CourtOrderTrait;
     use DeputyManagementTrait;
     use FormTrait;
 }
