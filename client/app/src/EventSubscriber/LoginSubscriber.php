bcfd44bb0f79b92387354a3d82ae899588dd033f
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-10-07 16:08:32 +0100

    Only protect /courtorder/* pages against secondary users

--- client/app/src/EventSubscriber/LoginSubscriber.php
+++ client/app/src/EventSubscriber/LoginSubscriber.php
@@ -27,40 +27,49 @@ class LoginSubscriber implements EventSubscriberInterface
         return [KernelEvents::REQUEST => 'onKernelRequest'];
     }
 
-    public function onKernelRequest(RequestEvent $event): void
+    public function onKernelRequest(RequestEvent $event): bool
     {
+        $uri = $event->getRequest()->getRequestUri();
+        if (!stristr($uri, '/courtorder')) {
+            return false;
+        }
+
         /** @var User $user */
         $user = $this->tokenStorage->getToken()?->getUser();
 
         // only redirect non-primary users after they have registered
-        if (!is_null($user) && ($user->getRoleName() === User::ROLE_LAY_DEPUTY) && !$user->getIsPrimary()) {
-            $primaryEmail = $this->userApi->returnPrimaryEmail($user->getDeputyUid());
-
-            $flashBag = $event->getRequest()->getSession()->getFlashBag();
-
-            if (is_null($primaryEmail)) {
-                $flashBag->add(
-                    'nonPrimaryRedirectUnknownEmail',
-                    [
-                        'sentenceOne' => 'This account has been closed.',
-                        'sentenceTwo' => 'You can now access all of your reports in the same place from your primary account.',
-                        'sentenceThree' => 'If you need assistance, contact your case manager on 0300 456 0300.',
-                    ]
-                );
-            } else {
-                $flashBag->add(
-                    'nonPrimaryRedirect',
-                    [
-                        'sentenceOne' => 'This account has been closed.',
-                        'sentenceTwo' => 'You can now access all of your reports in the same place from your account under',
-                        'primaryEmail' => $primaryEmail,
-                    ]
-                );
-            }
-
-            $url = $this->urlGenerator->generate('app_logout', ['notPrimaryAccount' => true]);
-            $response = new RedirectResponse($url);
-            $event->setResponse($response);
+        if (is_null($user) || $user->getIsPrimary()) {
+            return false;
         }
+
+        $primaryEmail = $this->userApi->returnPrimaryEmail($user->getDeputyUid());
+
+        $flashBag = $event->getRequest()->getSession()->getFlashBag();
+
+        if (is_null($primaryEmail)) {
+            $flashBag->add(
+                'nonPrimaryRedirectUnknownEmail',
+                [
+                    'sentenceOne' => 'This account has been closed.',
+                    'sentenceTwo' => 'You can now access all of your reports in the same place from your primary account.',
+                    'sentenceThree' => 'If you need assistance, contact your case manager on 0300 456 0300.',
+                ]
+            );
+        } else {
+            $flashBag->add(
+                'nonPrimaryRedirect',
+                [
+                    'sentenceOne' => 'This account has been closed.',
+                    'sentenceTwo' => 'You can now access all of your reports in the same place from your account under',
+                    'primaryEmail' => $primaryEmail,
+                ]
+            );
+        }
+
+        $url = $this->urlGenerator->generate('app_logout', ['notPrimaryAccount' => true]);
+        $response = new RedirectResponse($url);
+        $event->setResponse($response);
+
+        return true;
     }
 }
