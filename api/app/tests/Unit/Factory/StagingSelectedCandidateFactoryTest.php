a95b8524e89fbc3e7a9296b26f9ad964149b271e
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-10-21 17:25:50 +0100

    Add unit test for creating an update deputy status on order candidate

--- api/app/tests/Unit/Factory/StagingSelectedCandidateFactoryTest.php
+++ api/app/tests/Unit/Factory/StagingSelectedCandidateFactoryTest.php
@@ -24,22 +24,27 @@ final class StagingSelectedCandidateFactoryTest extends TestCase
         $this->csvDeputyShipRow->orderStatus = 'ACTIVE';
         $this->csvDeputyShipRow->orderMadeDate = '2018-01-21';
         $this->csvDeputyShipRow->deputyUid = '700761111002';
+        $this->csvDeputyShipRow->deputyStatusOnOrder = 'ACTIVE';
     }
 
     public function testCreateInsertOrderCandidate(): void
     {
-        $mockClient = $this->createMock(Client::class);
-        $mockClient
-            ->expects($this->any())
-            ->method('getId')
-            ->willReturn(1);
-
-        $candidateRecord = $this->factory->createInsertOrderCandidate($this->csvDeputyShipRow, $mockClient->getId());
+        $candidateRecord = $this->factory->createInsertOrderCandidate($this->csvDeputyShipRow, 1);
 
         self::assertEquals($this->csvDeputyShipRow->orderUid, $candidateRecord->orderUid);
         self::assertEquals($this->csvDeputyShipRow->orderType, $candidateRecord->orderType);
         self::assertEquals($this->csvDeputyShipRow->orderStatus, $candidateRecord->status);
-        self::assertEquals($mockClient->getId(), $candidateRecord->clientId);
+        self::assertEquals(1, $candidateRecord->clientId);
         self::assertEquals($this->csvDeputyShipRow->orderMadeDate, $candidateRecord->orderMadeDate);
+    }
+
+    public function testCreateUpdateDeputyStatusCandidate(): void
+    {
+        $candidateRecord = $this->factory->createUpdateDeputyStatusCandidate($this->csvDeputyShipRow, deputyId: 1, courtOrderId: 2);
+
+        self::assertEquals($this->csvDeputyShipRow->deputyUid, $candidateRecord->deputyUid);
+        self::assertEquals(true, $candidateRecord->deputyStatusOnOrder);
+        self::assertEquals(1, $candidateRecord->deputyId);
+        self::assertEquals(2, $candidateRecord->orderId);
     }
 }
