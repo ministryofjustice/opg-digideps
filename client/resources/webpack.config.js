fe8ea2d16f2965a1bfb85a2216e0476ae578c669
jamesrwarren <james.warren@digital.justice.gov.uk>
2025-10-23 10:04:08 +0100

    fix intermittent icon load issue

--- client/resources/webpack.config.js
+++ client/resources/webpack.config.js
@@ -77,6 +77,8 @@ module.exports = {
     },
     new CopyPlugin({
       patterns: [
+        { from: 'assets/images/generic-images', to: path.resolve(__dirname, 'public') },
+        { from: 'assets/images/generic-images', to: path.resolve(__dirname, 'public/images') }
         { from: 'node_modules/jquery/dist/jquery.min.js', to: 'javascripts' },
         { from: 'node_modules/govuk-frontend/dist/govuk/assets/fonts', to: 'stylesheets/fonts' },
         { from: 'node_modules/govuk-frontend/dist/govuk/assets/images', to: path.resolve(__dirname, 'public/images') },
@@ -85,9 +87,7 @@ module.exports = {
           to: path.resolve(__dirname, 'public/images')
         },
         { from: 'node_modules/govuk_frontend_toolkit/images', to: path.resolve(__dirname, 'public/images') },
-        { from: 'assets/images', to: path.resolve(__dirname, 'public/images') },
-        { from: 'assets/images/generic-images', to: path.resolve(__dirname, 'public') },
-        { from: 'assets/images/generic-images', to: path.resolve(__dirname, 'public/images') }
+        { from: 'assets/images', to: path.resolve(__dirname, 'public/images') }
       ]
     }),
     new MiniCssExtractPlugin({
