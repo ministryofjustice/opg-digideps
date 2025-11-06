878f7bba6a4656a3cd281c1e20ca8ed1a0bde9cb
Iqpal Mannan <iqpal.mannan@digital.justice.gov.uk>
2025-11-06 16:48:47 +0000

    Delete report reassignment endpoint

--- client/app/src/Controller/Admin/ToolsController.php
+++ client/app/src/Controller/Admin/ToolsController.php
@@ -6,7 +6,6 @@ namespace App\Controller\Admin;
 
 use App\Controller\AbstractController;
 use App\Exception\RestClientException;
-use App\Form\Admin\Tool\ReportReassignmentType;
 use App\Service\Client\RestClient;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -31,32 +30,5 @@ class ToolsController extends AbstractController
     public function tools()
     {
         return [];
-    }
-
-    /**
-     * @Route("/report-reassignment", name="admin_tools_report_reassignment")
-     * @Security("is_granted('ROLE_SUPER_ADMIN')")
-     * @Template("@App/Admin/Tools/report-reasssignment.html.twig")
-     */
-    public function reportAssignmentAction(Request $request)
-    {
-        $form = $this->createForm(ReportReassignmentType::class, null, ['method' => 'POST']);
-
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $submitted = $form->getData();
-
-            try {
-                $this->restClient->post('v2/tools/reassign-reports', json_encode([
-                    'firstClientId' => $submitted['firstClientId'],
-                    'secondClientId' => $submitted['secondClientId'],
-                ]), [], 'response');
-                $this->addFlash('fixture', 'Reports reassigned successfully!');
-            } catch (RestClientException $e) {
-                $this->addFlash('error', $e->getData()['message']);
-            }
-        }
-
-        return ['form' => $form->createView()];
     }
 }
