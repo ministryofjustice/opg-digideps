210a214ec3fdac6b349a63adbb9f55771241cfdd
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-11-11 17:35:03 +0000

    Don't mix permanent and transient files in build dir

--- client/resources/deps-builder.js
+++ client/resources/deps-builder.js
@@ -6,10 +6,14 @@
 import * as esbuild from "esbuild"
 import path from "path"
 import { fileURLToPath } from "url"
+import fs from "fs"
 
 const tag = (new Date()).getTime()
 const filename = fileURLToPath(import.meta.url)
-const outputDirWithTimestamp = path.resolve(path.dirname(filename), "public/assets/" + tag)
+const dirname = path.dirname(filename)
+const outputDirWithTimestamp = path.resolve(dirname, "public/assets/" + tag)
+
+fs.mkdirSync(outputDirWithTimestamp, { recursive: true })
 
 // TODO don't make sourcemaps for prod build
 const generateSourceMaps = true
@@ -29,10 +33,12 @@ const bundleJs = async function (entryPoints, outFile) {
   })
 }
 
+// JS COMPILATION
 Promise
   .all([
     bundleJs(["./assets/javascripts/common.js"], "javascripts/common.js"),
-    bundleJs(["./assets/javascripts/pages/clientBenefitsCheckForm.js"], "javascripts/clientBenefitsCheckForm.js")
+    bundleJs(["./assets/javascripts/pages/clientBenefitsCheckForm.js"], "javascripts/clientBenefitsCheckForm.js"),
+    bundleJs(["./node_modules/jquery/dist/jquery.min.js"], "javascripts/jquery.min.js")
   ])
   .then(r => {
     r.forEach(item => {
@@ -42,3 +48,27 @@ Promise
     })
   })
   .finally(() => console.log("Finished compiling JS"))
+
+// COPY IMAGES
+const imagesToCopy = [
+  { from: "assets/images/generic-images/", to: "public/" },
+  { from: "assets/images/generic-images/", to: "public/images/" },
+  { from: "node_modules/govuk-frontend/dist/govuk/assets/fonts/", to: "public/stylesheets/fonts/" },
+  { from: "node_modules/govuk-frontend/dist/govuk/assets/images/", to: "public/images/" },
+  {
+    from: "node_modules/@ministryofjustice/frontend/moj/assets/images/",
+    to: "public/images/"
+  },
+  { from: "node_modules/govuk_frontend_toolkit/images/", to: "public/images/" },
+  { from: "assets/images/", to: "public/images/" }
+]
+
+imagesToCopy.forEach(copySpec => {
+  const destPath = path.resolve(dirname, copySpec.to)
+  fs.mkdirSync(path.dirname(destPath), { recursive: true })
+  fs.cpSync(copySpec.from, destPath, { recursive: true })
+})
+
+console.log("Finished copying image files")
+
+// COMPILE CSS
