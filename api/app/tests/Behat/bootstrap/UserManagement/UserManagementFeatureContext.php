41499fb47c33861f8d2b1d26725a4fd33584eac1
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-11-14 15:41:21 +0000

    Fix config and traits for UserManagementFeatureContext

--- api/app/tests/Behat/bootstrap/UserManagement/UserManagementFeatureContext.php
+++ api/app/tests/Behat/bootstrap/UserManagement/UserManagementFeatureContext.php
@@ -8,9 +8,13 @@ use App\Tests\Behat\Common\BaseFeatureContext;
 use App\Tests\Behat\Common\LinksTrait;
 use App\Tests\Behat\Common\RegionTrait;
 use App\Tests\Behat\UserTrait;
+use App\Tests\Behat\v2\AdminManagement\AdminManagementTrait;
+use App\Tests\Behat\v2\Common\AuthTrait;
 
 class UserManagementFeatureContext extends BaseFeatureContext
 {
+    use AdminManagementTrait;
+    use AuthTrait;
     use LinksTrait;
     use RegionTrait;
     use UserManagementTrait;
