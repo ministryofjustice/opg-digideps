53dd5c3bd0e7029d495a3800bfbb2e7288a512e9
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-11-14 11:44:30 +0000

    Remove unused behat methods

--- api/app/tests/Behat/bootstrap/v2/Common/INavigateToFrontendTrait.php
+++ api/app/tests/Behat/bootstrap/v2/Common/INavigateToFrontendTrait.php
@@ -7,15 +7,6 @@ namespace App\Tests\Behat\v2\Common;
 trait INavigateToFrontendTrait
 {
     /**
-     * @When /^I navigate to the upload further documents page$/
-     */
-    public function iNavigateToTheUploadFurtherDocumentsPage()
-    {
-        $this->clickLink('Attach documents');
-        $this->iAmOnFurtherUploadDocumentsPage();
-    }
-
-    /**
      * @When /^I navigate to my user settings page$/
      */
     public function iNavigateToMyUserSettingsPage()
@@ -66,14 +57,5 @@ trait INavigateToFrontendTrait
     {
         $this->clickLink('Client details');
         $this->iAmOnClientDetailsPage();
-    }
-
-    /**
-     * @When /^I navigate to the Choose a client homepage$/
-     */
-    public function iNavigateToTheChooseAClientHomepage()
-    {
-        $this->clickLink('Choose a client');
-        $this->iAmOnChooseAClientMainPage();
     }
 }
