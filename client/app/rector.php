31a2d3ba7fe4b907639a8af5ecd024def8f551ee
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-11-14 09:34:04 +0000

    Update rector config for client controller refactorings

--- client/app/rector.php
+++ client/app/rector.php
@@ -2,29 +2,81 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
-use Rector\Core\ValueObject\PhpVersion;
-use Rector\Php74\Rector\Property\TypedPropertyRector;
-use Rector\Set\ValueObject\SetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Rector\CodingStyle\Rector\Stmt\RemoveUselessAliasInUseStatementRector;
+use Rector\Config\RectorConfig;
+use Rector\Php80\Rector\Class_\AnnotationToAttributeRector;
+use Rector\Php80\ValueObject\AnnotationToAttribute;
+use Rector\Renaming\Rector\String_\RenameStringRector;
+use Rector\Symfony\Symfony62\Rector\Class_\SecurityAttributeToIsGrantedAttributeRector;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(SetList::PHP_80);
+// Rector doesn't guarantee the order in which refactorings are applied: if one refactor depends on the output
+// from a previous one, it probably won't work. For that reason, this script has to run in steps, so that later
+// refactorings can be applied on top of previous ones (for example, convert annotations to attributes, then
+// convert the attribute; this is needed for Sensio Security annotations, which first have to be translated to
+// attributes, then converted from Sensio attributes to Symfony IsGranted aattributes).
+// Run this with:
+//   STEP=1 ./vendor/bin/rector process
+// then run again with the next step, e.g. STEP=2, and so on.
+$configBuilder = RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src/Controller/UserController.php',
+    ])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+    )
+    ->withPhpSets();
 
-    // get parameters
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PATHS, [
-        __DIR__.'/src',
-    ]);
+$step = intval(getenv('STEP'));
+if ($step === 0) {
+    $step = 1;
+}
 
-    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_80);
+switch ($step) {
+    case 1:
+        $configBuilder
+            ->withConfiguredRule(AnnotationToAttributeRector::class, [
+                new AnnotationToAttribute('Symfony\Component\Routing\Annotation\Route'),
+                new AnnotationToAttribute('Sensio\Bundle\FrameworkExtraBundle\Configuration\Security'),
+                new AnnotationToAttribute('Sensio\Bundle\FrameworkExtraBundle\Configuration\Template'),
+            ]);
+        break;
 
-    // Define what rule sets will be applied
-    $containerConfigurator->import(SetList::DEAD_CODE);
+    case 2:
+        $configBuilder
+            ->withRules([
+                SecurityAttributeToIsGrantedAttributeRector::class,
+            ]);
+        break;
 
-    // get services (needed for register a single rule)
-    // $services = $containerConfigurator->services();
+    case 3:
+        $configBuilder
+            ->withConfiguredRule(RenameStringRector::class, [
+                'Sensio' => 'Symfony',
+            ]);
+        break;
 
-    // register a single rule
-    // $services->set(TypedPropertyRector::class);
-};
+    case 4:
+        $configBuilder
+            ->withImportNames(removeUnusedImports: true)
+            ->withPreparedSets(
+                codingStyle: true,
+            );
+        break;
+
+    case 5:
+        $configBuilder->withRules([
+            RemoveUselessAliasInUseStatementRector::class,
+        ]);
+        break;
+}
+
+return $configBuilder;
+
+// after this, manual steps (could probably automate with Rector if I wanted):
+// - fix imports of built-ins (e.g. Throwable, DateTime to \Throwable, \DateTime)
+// - remove screwy aliases on imports (e.g. FormDir)
+// - optimise imports
+// - sort parameters on attributes
+// - replace Sensio\Bundle\FrameworkExtraBundle\Configuration\Template with Symfony\Bridge\Twig\Attribute\Template
