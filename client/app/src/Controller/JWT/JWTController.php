f01dcaac3e9bee0f639a7aa9a0bd2bb95caad058
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-11-19 10:20:39 +0000

    Refactor JWTController (Sensio -> Symfony)

--- client/app/src/Controller/JWT/JWTController.php
+++ client/app/src/Controller/JWT/JWTController.php
@@ -9,14 +9,12 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class JWTController extends AbstractController
 {
-    public function __construct(private HttpClientInterface $phpApiClient)
+    public function __construct(private readonly HttpClientInterface $phpApiClient)
     {
     }
 
-    /**
-     * @Route("/v2/.well-known/jwks.json", name="jwks")
-     */
-    public function jwks()
+    #[Route(path: '/v2/.well-known/jwks.json', name: 'jwks')]
+    public function jwks(): JsonResponse
     {
         $jwkResponse = $this->phpApiClient->request('GET', 'jwk-public-key');
 
