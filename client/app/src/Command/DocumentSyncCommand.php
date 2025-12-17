a9cb7e74d0d42578de62260b6c2cf4a3e79d625f
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-12-17 15:26:38 +0000

    Factor out business logic for document sync command
    This enables us to more easily reuse that business logic
    in a test context.

--- client/app/src/Command/DocumentSyncCommand.php
+++ client/app/src/Command/DocumentSyncCommand.php
@@ -4,26 +4,18 @@ declare(strict_types=1);
 
 namespace App\Command;
 
-use App\Model\Sirius\QueuedDocumentData;
-use App\Service\Client\RestClient;
-use App\Service\DocumentSyncService;
+use App\Service\DocumentSyncRunner;
 use App\Service\ParameterStoreService;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Serializer\SerializerInterface;
 
 class DocumentSyncCommand extends DaemonableCommand
 {
-    public const string FALLBACK_ROW_LIMITS = '100';
-    public const string COMPLETED_MESSAGE = 'Sync command completed';
-
     public static $defaultName = 'digideps:document-sync';
 
     public function __construct(
-        private readonly DocumentSyncService $documentSyncService,
-        private readonly RestClient $restClient,
-        private readonly SerializerInterface $serializer,
-        private readonly ParameterStoreService $parameterStore
+        private readonly ParameterStoreService $parameterStore,
+        private readonly DocumentSyncRunner $documentSyncRunner,
     ) {
         parent::__construct();
     }
@@ -39,56 +31,14 @@ class DocumentSyncCommand extends DaemonableCommand
     {
         ini_set('memory_limit', '512M');
 
-        $isFeatureEnabled = '1' === $this->parameterStore->getFeatureFlag(ParameterStoreService::FLAG_DOCUMENT_SYNC);
+        $isFeatureEnabled = ('1' === $this->parameterStore->getFeatureFlag(ParameterStoreService::FLAG_DOCUMENT_SYNC));
 
         if (!$isFeatureEnabled) {
             $output->writeln('Feature disabled, sleeping');
             return 0;
         }
 
-        $syncRowLimit = $this->parameterStore->getParameter(ParameterStoreService::PARAMETER_DOCUMENT_SYNC_ROW_LIMIT);
-
-        $queuedDocumentData = $this->restClient->apiCall(
-            'get',
-            'document/queued',
-            ['row_limit' => $syncRowLimit ?? self::FALLBACK_ROW_LIMITS],
-            'array',
-            [],
-            false
-        );
-
-        $documents = $this->serializer->deserialize($queuedDocumentData, 'App\Model\Sirius\QueuedDocumentData[]', 'json');
-
-        $output->writeln(sprintf('%d documents to upload', count($documents)));
-
-        /** @var QueuedDocumentData $document */
-        foreach ($documents as $document) {
-            $this->documentSyncService->syncDocument($document);
-        }
-
-        if (count($this->documentSyncService->getSyncErrorSubmissionIds()) > 0) {
-            $output->writeln(
-                sprintf(
-                    'sync_documents_to_sirius - failure - %d documents failed to sync',
-                    count($this->documentSyncService->getSyncErrorSubmissionIds())
-                )
-            );
-            $this->documentSyncService->setSubmissionsDocumentsToPermanentError();
-            $this->documentSyncService->setSyncErrorSubmissionIds([]);
-        }
-
-        if ($this->documentSyncService->getDocsNotSyncedCount() > 0) {
-            $output->writeln(
-                sprintf(
-                    'sync_documents_to_sirius - success - %d documents remaining to sync',
-                    $this->documentSyncService->getDocsNotSyncedCount()
-                )
-            );
-
-            $this->documentSyncService->setDocsNotSyncedCount(0);
-        } else {
-            $output->writeln(sprintf('sync_documents_to_sirius - success - %s', self::COMPLETED_MESSAGE));
-        }
+        $this->documentSyncRunner->run($output);
 
         return 0;
     }
---
+++ client/app/src/Service/DocumentSyncRunner.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Model\Sirius\QueuedDocumentData;
+use App\Service\Client\RestClient;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+
+/**
+ * Run the document sync process.
+ */
+class DocumentSyncRunner
+{
+    public const string FALLBACK_ROW_LIMITS = '100';
+    public const string COMPLETED_MESSAGE = 'Sync command completed';
+
+    public function __construct(
+        private readonly DocumentSyncService $documentSyncService,
+        private readonly RestClient $restClient,
+        private readonly SerializerInterface $serializer,
+        private readonly ParameterStoreService $parameterStore
+    ) {
+    }
+
+    public function run(OutputInterface $output): void
+    {
+        $syncRowLimit = $this->parameterStore->getParameter(ParameterStoreService::PARAMETER_DOCUMENT_SYNC_ROW_LIMIT);
+
+        $queuedDocumentData = $this->restClient->apiCall(
+            'get',
+            'document/queued',
+            ['row_limit' => $syncRowLimit ?? self::FALLBACK_ROW_LIMITS],
+            'array',
+            [],
+            false
+        );
+
+        $documents = $this->serializer->deserialize($queuedDocumentData, 'App\Model\Sirius\QueuedDocumentData[]', 'json');
+
+        $output->writeln(sprintf('%d documents to upload', count($documents)));
+
+        /** @var QueuedDocumentData $document */
+        foreach ($documents as $document) {
+            $this->documentSyncService->syncDocument($document);
+        }
+
+        if (count($this->documentSyncService->getSyncErrorSubmissionIds()) > 0) {
+            $output->writeln(
+                sprintf(
+                    'sync_documents_to_sirius - failure - %d documents failed to sync',
+                    count($this->documentSyncService->getSyncErrorSubmissionIds())
+                )
+            );
+            $this->documentSyncService->setSubmissionsDocumentsToPermanentError();
+            $this->documentSyncService->setSyncErrorSubmissionIds([]);
+        }
+
+        if ($this->documentSyncService->getDocsNotSyncedCount() > 0) {
+            $output->writeln(
+                sprintf(
+                    'sync_documents_to_sirius - success - %d documents remaining to sync',
+                    $this->documentSyncService->getDocsNotSyncedCount()
+                )
+            );
+
+            $this->documentSyncService->setDocsNotSyncedCount(0);
+        } else {
+            $output->writeln(sprintf('sync_documents_to_sirius - success - %s', self::COMPLETED_MESSAGE));
+        }
+    }
+}
