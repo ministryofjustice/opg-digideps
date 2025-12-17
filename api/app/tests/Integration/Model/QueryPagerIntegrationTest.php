03921ac39aa21fb2537fa041153dc9e3a4486bc2
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-12-17 10:28:12 +0000

    Add more tests for QueryPager
    As we saw issues with duplicate IDs when paging a query, add
    a test which checks for this specifically.
    
    Also add tests which check that fetching rows across boundaries
    (page size, limits etc.) return the correct number of rows.

--- api/app/tests/Integration/Model/QueryPagerIntegrationTest.php
+++ api/app/tests/Integration/Model/QueryPagerIntegrationTest.php
@@ -9,14 +9,20 @@ use App\Model\QueryPager;
 use App\Tests\Integration\ApiIntegrationTestCase;
 use App\v2\Registration\Enum\DeputyshipCandidateAction;
 
-class QueryPagerIntegrationIntegrationTest extends ApiIntegrationTestCase
+class QueryPagerIntegrationTest extends ApiIntegrationTestCase
 {
     public function testGetRows(): void
     {
         $courtOrderUid = '99775533';
-        $numRowsExpected = 20;
 
-        for ($i = 0; $i < $numRowsExpected; ++$i) {
+        // these variables are intended to test behaviour at boundaries:
+        // number of rows in database > limit, and not a multiple of page size;
+        // limit is not divisible by page size
+        $limit = 175;
+        $numRowsInDatabase = 190;
+        $pageSize = 11;
+
+        for ($i = 0; $i < $numRowsInDatabase; ++$i) {
             $candidate = new StagingSelectedCandidate(DeputyshipCandidateAction::InsertOrder, $courtOrderUid);
             self::$entityManager->persist($candidate);
         }
@@ -31,30 +37,40 @@ class QueryPagerIntegrationIntegrationTest extends ApiIntegrationTestCase
         $sut = new QueryPager($pageQueryBuilder);
 
         // test getting rows as objects
-        $generator = $sut->getRows(pageSize: 3, asArray: false);
+        $generator = $sut->getRows(pageSize: $pageSize, asArray: false, limit: $limit);
 
         $numRowsActual = 0;
+        $candidateIds = [];
 
         /** @var StagingSelectedCandidate $candidate */
         foreach ($generator as $candidate) {
             ++$numRowsActual;
+            $candidateIds[] = $candidate->id;
             self::assertEquals(DeputyshipCandidateAction::InsertOrder, $candidate->action);
             self::assertEquals($courtOrderUid, $candidate->orderUid);
         }
 
-        self::assertEquals($numRowsExpected, $numRowsActual);
+        self::assertEquals($limit, $numRowsActual);
+        self::assertEquals($limit, count(array_unique($candidateIds)));
 
         // test getting rows as arrays
-        $generator = $sut->getRows(pageSize: 3);
+        $generator = $sut->getRows(pageSize: $pageSize, limit: $limit);
 
         $numRowsActual = 0;
+        $candidateIds = [];
 
         foreach ($generator as $candidate) {
             ++$numRowsActual;
+            $candidateIds[] = $candidate['id'];
             self::assertEquals(DeputyshipCandidateAction::InsertOrder, $candidate['action']);
             self::assertEquals($courtOrderUid, $candidate['orderUid']);
         }
 
-        self::assertEquals($numRowsExpected, $numRowsActual);
+        self::assertEquals($limit, $numRowsActual);
+        self::assertEquals($limit, count(array_unique($candidateIds)));
+
+        // test getting rows without a limit (should get all rows)
+        $rows = iterator_to_array($sut->getRows(pageSize: $pageSize));
+        self::assertCount($numRowsInDatabase, $rows);
     }
 }
