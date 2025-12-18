9f20cbfa43009d4a5e407f874e40276b4050b35c
Chris Rafferty <chris.rafferty@digital.justice.gov.uk>
2025-12-18 15:19:20 +0000

    Fixes bug in errorTrait, creates new step for error checking

--- api/app/tests/Behat/bootstrap/v2/Common/ErrorsTrait.php
+++ api/app/tests/Behat/bootstrap/v2/Common/ErrorsTrait.php
@@ -8,29 +8,49 @@ use App\Tests\Behat\BehatException;
 
 trait ErrorsTrait
 {
-    public function assertOnErrorMessage(string $errorMessage)
+    private function errorLocations()
     {
         $errorDiv = $this->getSession()->getPage()->find('css', 'div#error-summary');
         $flashDiv = $this->getSession()->getPage()->find('css', 'div.opg-alert--error');
 
-        if (is_null($errorDiv) && is_null($flashDiv)) {
-            $missingDivMessage = <<<MESSAGE
-A div with the id error-summary or class opg-alert--error was not found.
-This suggests one of the following:
+        return ['errorDiv' => $errorDiv, 'flashDiv' => $flashDiv];
+    }
 
-- an error was not triggered
-- the form error appears in a non-GDS error element
-- the flash error element is not using macro.notification.
-MESSAGE;
+    public function assertOnErrorMessage(string $errorMessage)
+    {
+        $errors = $this->errorLocations();
+
+        if (is_null($errors['errorDiv']) || is_null($errors['flashDiv'])) {
+            $missingDivMessage = <<<MESSAGE
+            A div with the id error-summary or class opg-alert--error was not found.
+            This suggests one of the following:
+
+            - an error was not triggered
+            - the form error appears in a non-GDS error element
+            - the flash error element is not using macro.notification.
+            MESSAGE;
 
             throw new BehatException($missingDivMessage);
         }
 
-        $errorHtml = $errorDiv ? $errorDiv->getHtml() : $flashDiv->getHtml();
+        $errorHtml = $errors['errorDiv'] ? $errors['errorDiv']->getHtml() : $errors['flashDiv']->getHtml();
         $errorMessageFound = str_contains($errorHtml, $errorMessage);
 
         if (!$errorMessageFound) {
             throw new BehatException(sprintf('The error summary did not contain the expected error message. Expected: "%s", got (full HTML): %s', $errorMessage, $errorHtml));
+        }
+    }
+
+    /**
+     * @Then I should see no errors
+     */
+    public function assertNoErrorMessages()
+    {
+        $errors = $this->errorLocations();
+
+        if (!is_null($errors['errorDiv']) || !is_null($errors['flashDiv'])) {
+            $errorHtml = $errors['errorDiv'] ? $errors['errorDiv']->getHtml() : $errors['flashDiv']->getHtml();
+            throw new BehatException(sprintf('Error message displayed: %s', $errorHtml));
         }
     }
 }
