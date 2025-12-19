2398edf95bec00e49715d427be1deba5556b1d6a
Elliot Smith <elliot.smith2@justice.gov.uk>
2025-12-19 14:43:16 +0000

    Cleanup SiriusHelpers

--- client/app/tests/phpunit/Helpers/SiriusHelpers.php
+++ client/app/tests/phpunit/Helpers/SiriusHelpers.php
@@ -9,21 +9,20 @@ use App\Model\Sirius\SiriusDocumentFile;
 use App\Model\Sirius\SiriusDocumentUpload;
 use App\Model\Sirius\SiriusReportPdfDocumentMetadata;
 use App\Model\Sirius\SiriusSupportingDocumentMetadata;
-use DateTime;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class SiriusHelpers extends KernelTestCase
 {
     public static function generateSiriusReportPdfDocumentUpload(
-        DateTime $startDate,
-        DateTime $endDate,
-        DateTime $submittedDate,
+        \DateTime $startDate,
+        \DateTime $endDate,
+        \DateTime $submittedDate,
         string $orderType,
         int $submissionId,
         string $fileName,
         ?string $fileContents,
         ?string $s3Reference
-    ) {
+    ): SiriusDocumentUpload {
         $siriusReportPdfDocumentMetadata = (new SiriusReportPdfDocumentMetadata())
             ->setReportingPeriodFrom($startDate)
             ->setReportingPeriodTo($endDate)
@@ -50,8 +49,12 @@ class SiriusHelpers extends KernelTestCase
             ->setFile($file);
     }
 
-    public static function generateSiriusSupportingDocumentUpload(int $submissionId, string $fileName, ?string $fileContents, ?string $s3Reference)
-    {
+    public static function generateSiriusSupportingDocumentUpload(
+        int $submissionId,
+        string $fileName,
+        ?string $fileContents,
+        ?string $s3Reference
+    ): SiriusDocumentUpload {
         $siriusSupportingDocumentMetadata = (new SiriusSupportingDocumentMetadata())
             ->setSubmissionId($submissionId);
 
@@ -78,8 +81,8 @@ class SiriusHelpers extends KernelTestCase
         string $fileContents,
         int $submissionId,
         string $submitterEmail,
-        DateTime $reportingPeriodFrom,
-        DateTime $reportingPeriodTo,
+        \DateTime $reportingPeriodFrom,
+        \DateTime $reportingPeriodTo,
         int $year,
         string $type
     ) {
