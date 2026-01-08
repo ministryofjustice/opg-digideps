b8f760bbbf42701fcdb4e917a05f8ccdf608a876
jamesrwarren <james.warren@digital.justice.gov.uk>
2026-01-08 12:28:29 +0000

    DDLS-1157c more account level changes for new network module

--- terraform/account/region/vpc_endpoints.tf
+++ terraform/account/region/vpc_endpoints.tf
@@ -55,3 +55,62 @@ resource "aws_vpc_endpoint" "s3" {
   route_table_ids   = aws_route_table.private[*].id
   tags              = merge(var.default_tags, { Name = "s3" })
 }
+
+# New VPC Endpoints
+module "secrets_endpoint_vpc" {
+  source              = "./modules/vpc_endpoint"
+  subnet_ids          = module.network[0].application_subnets[*].id
+  vpc                 = module.network[0].vpc
+  region              = data.aws_region.current.name
+  service             = "secretsmanager"
+  service_short_title = "secrets"
+  tags                = var.default_tags
+}
+
+module "ecr_endpoint_vpc" {
+  source              = "./modules/vpc_endpoint"
+  subnet_ids          = module.network[0].application_subnets[*].id
+  vpc                 = module.network[0].vpc
+  region              = data.aws_region.current.name
+  service             = "ecr.dkr"
+  service_short_title = "ecr"
+  tags                = var.default_tags
+}
+
+module "ecr_api_endpoint_vpc" {
+  source              = "./modules/vpc_endpoint"
+  subnet_ids          = module.network[0].application_subnets[*].id
+  vpc                 = module.network[0].vpc
+  region              = data.aws_region.current.name
+  service             = "ecr.api"
+  service_short_title = "ecr_api"
+  tags                = var.default_tags
+}
+
+module "logs_endpoint_vpc" {
+  source              = "./modules/vpc_endpoint"
+  subnet_ids          = module.network[0].application_subnets[*].id
+  vpc                 = module.network[0].vpc
+  region              = data.aws_region.current.name
+  service             = "logs"
+  service_short_title = "logs"
+  tags                = var.default_tags
+}
+
+module "ssm_endpoint_vpc" {
+  source              = "./modules/vpc_endpoint"
+  subnet_ids          = module.network[0].application_subnets[*].id
+  vpc                 = module.network[0].vpc
+  region              = data.aws_region.current.name
+  service             = "ssm"
+  service_short_title = "ssm"
+  tags                = var.default_tags
+}
+
+resource "aws_vpc_endpoint" "s3_endpoint_vpc" {
+  service_name      = "com.amazonaws.eu-west-1.s3"
+  vpc_id            = module.network[0].vpc.id
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = module.network[0].application_subnet_route_tables[*].id
+  tags              = merge(var.default_tags, { Name = "s3" })
+}
