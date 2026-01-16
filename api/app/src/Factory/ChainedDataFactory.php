4dd5ae2040b3613bed650e38ec3894880f9d8a21
Elliot Smith <elliot.smith2@justice.gov.uk>
2026-01-16 17:01:06 +0000

    Wire up dummy DataFactory and aggregate ChainedDataFactory outputs

--- api/app/src/Factory/ChainedDataFactory.php
+++ api/app/src/Factory/ChainedDataFactory.php
@@ -9,12 +9,39 @@ namespace App\Factory;
  */
 class ChainedDataFactory implements DataFactoryInterface
 {
-    public function __construct()
-    {
+    public function __construct(
+        /** @var array<DataFactoryInterface> $dataFactories */
+        private readonly array $dataFactories = [],
+    ) {
     }
 
+    /**
+     * @return DataFactoryResult Aggregated results from all data factories; the messages and error messages are
+     * keyed by the name of the data factory that produced them.
+     */
     public function run(): DataFactoryResult
     {
-        return new DataFactoryResult();
+        $result = new DataFactoryResult();
+
+        foreach ($this->dataFactories as $dataFactory) {
+            $factoryResult = $dataFactory->run();
+
+            $factoryName = $dataFactory->getName();
+
+            foreach ($factoryResult->getMessages() as $key => $messages) {
+                $result->addMessages(source: $factoryName . ':' . $key, messages: array_values($messages));
+            }
+
+            foreach ($factoryResult->getErrorMessages() as $key => $errorMessages) {
+                $result->addErrorMessages(source: $factoryName . ':' . $key, errorMessages: array_values($errorMessages));
+            }
+        }
+
+        return $result;
+    }
+
+    public function getName(): string
+    {
+        return 'ChainedDataFactory';
     }
 }
