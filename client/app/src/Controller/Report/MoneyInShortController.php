1ef9360c9c53a2222c1566174dce4da6f4672a27
Iqpal Mannan <iqpal.mannan@digital.justice.gov.uk>
2026-01-20 16:38:25 +0000

    Add flash message if no money categories have been selected

--- client/app/src/Controller/Report/MoneyInShortController.php
+++ client/app/src/Controller/Report/MoneyInShortController.php
@@ -152,19 +152,30 @@ class MoneyInShortController extends AbstractController
 
         if ($form->get('save')->isClicked() && $form->isSubmitted() && $form->isValid()) {
             $data = $form->getData();
+            $categories = $data->getMoneyShortCategoriesIn();
 
-            $this->restClient->put('report/' . $reportId, $data, ['moneyShortCategoriesIn']);
-
-            if ($fromSummaryPage) {
-                $request->getSession()->getFlashBag()->add(
-                    'notice',
-                    'Answer edited'
-                );
-
-                return $this->redirectToRoute('money_in_short_summary', ['reportId' => $reportId]);
+            // Count the number of categories where 'present' is true
+            $presentCount = 0;
+            foreach ($categories as $category) {
+                if ($category->isPresent()) {
+                    $presentCount++;
+                }
             }
 
-            return $this->redirectToRoute('money_in_short_one_off_payments_exist', ['reportId' => $reportId]);
+            if ($presentCount === 0) {
+                $this->addFlash('error', 'Select at least one category of money');
+            } else {
+                $this->restClient->put('report/' . $reportId, $data, ['moneyShortCategoriesIn']);
+                if ($fromSummaryPage) {
+                    $request->getSession()->getFlashBag()->add(
+                        'notice',
+                        'Answer edited'
+                    );
+
+                    return $this->redirectToRoute('money_in_short_summary', ['reportId' => $reportId]);
+                }
+                return $this->redirectToRoute('money_in_short_one_off_payments_exist', ['reportId' => $reportId]);
+            }
         }
 
         return [
