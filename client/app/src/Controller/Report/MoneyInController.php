6bdab703574a687c46827dbc1f9aafaf539a4d9f
Elliot Smith <elliot.smith2@justice.gov.uk>
2026-02-05 11:58:41 +0000

    Add the 'add another' element into the add money in form

--- client/app/src/Controller/Report/MoneyInController.php
+++ client/app/src/Controller/Report/MoneyInController.php
@@ -9,6 +9,7 @@ use App\Entity\Report\BankAccount;
 use App\Entity\Report\MoneyTransaction;
 use App\Entity\Report\Status;
 use App\Form\AddAnotherRecordType;
+use App\Form\AddAnotherThingType;
 use App\Form\ConfirmDeleteType;
 use App\Form\Report\DoesMoneyInExistType;
 use App\Form\Report\MoneyTransactionType;
@@ -223,10 +224,17 @@ class MoneyInController extends AbstractController
                 'report' => $report,
             ]
         );
+
+        // if we are on step 2, we need an "add another" option
+        if (2 === $step) {
+            $form->add('addAnother', AddAnotherThingType::class);
+        }
+
         $form->handleRequest($request);
 
         /** @var SubmitButton $saveButton */
         $saveButton = $form->get('save');
+
         if ($saveButton->isClicked() && $form->isSubmitted() && $form->isValid()) {
             // decide what data in the partial form needs to be passed to next step
             if (1 == $step) {
@@ -235,18 +243,27 @@ class MoneyInController extends AbstractController
 
                 $stepUrlData['category'] = $transaction->getCategory();
             } elseif ($step == $totalSteps) {
-                if ($transactionId) { // edit
+                if ($transactionId) {
+                    // edit
                     $this->addFlash(
                         'notice',
                         'Entry edited'
                     );
+
                     $this->restClient->put("/report/$reportId/money-transaction/$transactionId", $transaction, ['transaction', 'account']);
 
                     return $this->redirectToRoute('money_in_summary', ['reportId' => $reportId]);
-                } else { // add
+                } else {
+                    // add
                     $this->restClient->post("/report/$reportId/money-transaction", $transaction, ['transaction', 'account']);
 
-                    return $this->redirectToRoute('money_in_add_another', ['reportId' => $reportId]);
+                    // check whether we are adding another after this one and redirect appropriately
+                    switch ($form['addAnother']->getData()) {
+                        case 'yes':
+                            return $this->redirectToRoute('money_in_step', ['step' => 1, 'reportId' => $reportId]);
+                        case 'no':
+                            return $this->redirectToRoute('money_in_summary', ['reportId' => $reportId]);
+                    }
                 }
             }
 
@@ -266,30 +283,6 @@ class MoneyInController extends AbstractController
             'backLink' => $stepRedirector->getBackLink(),
             'skipLink' => null,
             'categoriesGrouped' => MoneyTransaction::getCategoriesGrouped('in'),
-        ];
-    }
-
-    #[Route(path: '/report/{reportId}/money-in/add_another', name: 'money_in_add_another')]
-    #[Template('@App/Report/MoneyIn/addAnother.html.twig')]
-    public function addAnotherAction(Request $request, int $reportId): RedirectResponse|array
-    {
-        $report = $this->reportApi->getReportIfNotSubmitted($reportId);
-
-        $form = $this->createForm(AddAnotherRecordType::class, $report, ['translation_domain' => 'report-money-transaction']);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            switch ($form['addAnother']->getData()) {
-                case 'yes':
-                    return $this->redirectToRoute('money_in_step', ['reportId' => $reportId, 'step' => 1, 'from' => 'money_in_add_another']);
-                case 'no':
-                    return $this->redirectToRoute('money_in_summary', ['reportId' => $reportId]);
-            }
-        }
-
-        return [
-            'form' => $form->createView(),
-            'report' => $report,
         ];
     }
 
