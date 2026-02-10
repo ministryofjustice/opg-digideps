3f578fd9dee5b9b4cc7518df33877f40aca983a9
Elliot Smith <elliot.smith2@justice.gov.uk>
2026-02-10 13:53:17 +0000

    DDLS-809 Client view cleanup (revised) (#2327)
    * DDLS-809 Remove unused client views and related code (revised)
    
    * Remove unused ClientController methods
    
    * Remove unused ReportController methods and templates
    
    * Remove unused CoDeputyController method and template
    
    * Remove unused method
    
    * Update phpstan baseline for client
    
    * Remove unused NDR code - to enable removal of client/Client::getActiveReport()
    
    * Remove method only used by routes we've removed
    
    * Remove unnecessary test (now tested through choose-a-court-order feature)
    
    * Remove controller method only called by removed code
    
    * Remove unused API route (was called by invite co-deputy)
    
    * Remove e2e test which checks choose-a-client page (removed)
    
    * Remove behat functions concerning choose-a-client page
    
    * Modify log query to examine choose-a-court-order
    
    * Remove broken NDR tests
    
    These tests are covered by near-identical test for standard reports.
    
    They're broken because I removed an NDR-related event trigger, so that
    I could in turn remove some code otherwise only used in client controllers.
    
    * Fix phpstan client issues
    
    * Fix phpstan api issues
    
    * Remove irrelevant NDR test
    
    The submit action no longer triggers any changes to the db.
    
    * Fix phpstan error
    
    * Fix config for editor (it was doing nothing)
    
    * Fix feature file formatting
    
    * Skeleton for test of report-overview reports display
    
    * Add assertions about submitted report to e2e test

--- api/app/src/Controller/PreRegistrationController.php
+++ api/app/src/Controller/PreRegistrationController.php
@@ -8,7 +8,6 @@ use App\Entity\User;
 use App\Repository\PreRegistrationRepository;
 use App\Service\Formatter\RestFormatter;
 use App\Service\PreRegistrationVerificationService;
-use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use RuntimeException;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,7 +18,6 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 class PreRegistrationController extends RestController
 {
     public function __construct(
-        private readonly PreRegistrationVerificationService $preRegistrationVerificationService,
         private readonly RestFormatter $formatter,
         private readonly EntityManagerInterface $em,
     ) {
@@ -84,7 +82,7 @@ class PreRegistrationController extends RestController
         }
 
         $user->setDeputyUid(intval($preregMatches[0]->getDeputyUid()));
-        $user->setPreRegisterValidatedDate(new DateTime());
+        $user->setPreRegisterValidatedDate(new \DateTime());
         $user->setIsPrimary(true);
         $this->em->persist($user);
         $this->em->flush();
@@ -104,11 +102,5 @@ class PreRegistrationController extends RestController
         $result = $qb->getQuery()->getSingleScalarResult();
 
         return $result;
-    }
-
-    #[Route(path: '/clientHasCoDeputies/{caseNumber}', methods: ['GET'])]
-    public function clientHasCoDeputies(string $caseNumber): bool
-    {
-        return $this->preRegistrationVerificationService->isMultiDeputyCase($caseNumber);
     }
 }
