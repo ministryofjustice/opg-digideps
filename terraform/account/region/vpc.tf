482196cffee2b77ccb504554df9cb511d7cf820b
jamesrwarren <james.warren@digital.justice.gov.uk>
2026-02-19 10:47:42 +0000

    DDLS-1141e removal of old subnets

--- terraform/account/region/vpc.tf
+++ terraform/account/region/vpc.tf
@@ -1,5 +1,3 @@
-data "aws_availability_zones" "all" {}
-
 data "aws_vpc" "default" {
   default = true
 }
@@ -11,38 +9,5 @@ resource "aws_vpc" "main" {
   tags = merge(
     var.default_tags,
     { Name = "private" },
-  )
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-  tags = merge(
-    var.default_tags,
-    {
-      "Name" = "internet-gateway-${var.account.name}"
-    },
-  )
-}
-
-resource "aws_eip" "nat" {
-  domain = "vpc"
-  count  = 3
-  tags = merge(
-    var.default_tags,
-    {
-      "Name" = "nat-gw-eip-${var.account.name}-${count.index}"
-    },
-  )
-}
-
-resource "aws_nat_gateway" "nat" {
-  count         = 3
-  allocation_id = aws_eip.nat[count.index].id
-  subnet_id     = aws_subnet.public[count.index].id
-  tags = merge(
-    var.default_tags,
-    {
-      "Name" = "nat-gateway-${var.account.name}-${count.index}"
-    },
   )
 }
