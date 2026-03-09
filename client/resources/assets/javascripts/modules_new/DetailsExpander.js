6efaa973eef6985653556bd6e55e39f4c71f5689
Elliot Smith <elliot.smith2@justice.gov.uk>
2026-03-09 09:18:25 +0000

    Improve accessibility features for details expander

--- client/resources/assets/javascripts/modules_new/DetailsExpander.js
+++ client/resources/assets/javascripts/modules_new/DetailsExpander.js
@@ -20,13 +20,29 @@ const DetailsExpander = {
   makeHandler: function (triggerElement, expandableElement) {
     return function () {
       const numericValue = parseFloat(triggerElement.value.replace(/,/g, ''))
+
+      // if this attribute is present, the expandable element is shown only
+      // when the value of the expander text input is zero; otherwise, the
+      // expandable element is shown when the text input is *not* zero (default)
       const expandIfZero = triggerElement.hasAttribute('data-expand-if-zero')
 
       const shouldExpand = !isNaN(numericValue) && (
         (expandIfZero && numericValue === 0.0) || (!expandIfZero && numericValue > 0.0)
       )
 
+      // if the expandable element contains an aria-live element, update
+      // its content so that users of assistive technologies are aware that
+      // additional information is being requested
+      const ariaLiveElement = expandableElement.querySelector('[aria-live]')
+      if (ariaLiveElement) {
+        ariaLiveElement.textContent = shouldExpand ?
+          'Additional information is required'
+          : 'Additional information is not required'
+      }
+
       expandableElement.classList.toggle('js-hidden', !shouldExpand)
+      expandableElement.setAttribute('aria-hidden', !shouldExpand)
+      expandableElement.setAttribute('aria-expanded', shouldExpand)
     }
   }
 }
