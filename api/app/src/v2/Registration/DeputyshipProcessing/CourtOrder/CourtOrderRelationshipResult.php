474f6aeba0c0af354df32154ee562f05defa836e
olivier-nicodemi <265032004+olivier-nicodemi@users.noreply.github.com>
2026-03-17 15:52:03 +0000

    DDLS-1353

---
+++ api/app/src/v2/Registration/DeputyshipProcessing/CourtOrder/CourtOrderRelationshipResult.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\v2\Registration\DeputyshipProcessing\CourtOrder;
+
+final readonly class CourtOrderRelationshipResult
+{
+    public function __construct(private CourtOrderRelationshipChange $change)
+    {
+    }
+
+    public function getMessage(): string
+    {
+        return "Changes in CourtOrder {$this->change->courtOrder->getId()}:"
+            . ($this->change->hasSiblingIdChange() ? " SiblingId changed from '{$this->change->oldSiblingId}' -> '{$this->change->courtOrder->getSibling()?->getId()}'." : '')
+            . ($this->change->hasKindChange() ? "Kind changed from '{$this->change->oldKind}' -> '{$this->change->courtOrder->getKind()}'" : '');
+    }
+
+    public function isError(): bool
+    {
+        return false;
+    }
+
+    public function getErrorMessage(): string
+    {
+        return "";
+    }
+}
