b1c187dee4a1a07e105cf50b6259b113eb01388e
Elliot Smith <elliot.smith2@justice.gov.uk>
2026-03-27 16:34:17 +0000

    Remove references to NDRs from api/ controllers

--- api/app/src/Controller/Report/MoneyReceivedOnClientsBehalfController.php
+++ api/app/src/Controller/Report/MoneyReceivedOnClientsBehalfController.php
@@ -6,7 +6,6 @@ namespace App\Controller\Report;
 
 use App\Controller\RestController;
 use App\Repository\MoneyReceivedOnClientsBehalfRepository;
-use App\Repository\NdrMoneyReceivedOnClientsBehalfRepository;
 use App\Service\Formatter\RestFormatter;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,20 +16,19 @@ class MoneyReceivedOnClientsBehalfController extends RestController
 {
     public function __construct(
         private readonly MoneyReceivedOnClientsBehalfRepository $reportMoneyRepository,
-        private readonly NdrMoneyReceivedOnClientsBehalfRepository $ndrMoneyRepository,
         private readonly RestFormatter $formatter,
         EntityManagerInterface $em
     ) {
         parent::__construct($em);
     }
 
-    #[Route(path: '{reportOrNdr}/money-type/delete/{moneyTypeId}', methods: ['DELETE'], name: 'delete_money_type')]
+    #[Route(path: '/report/money-type/delete/{moneyTypeId}', name: 'delete_money_type', methods: ['DELETE'])]
     #[IsGranted(attribute: 'ROLE_DEPUTY')]
-    public function delete(Request $request, string $reportOrNdr, string $moneyTypeId): array
+    public function delete(Request $request, string $moneyTypeId): array
     {
-        $groups = $request->request->has('groups') ? $request->request->all('groups') : ['client-benefits-check', 'report', 'ndr'];
+        $groups = $request->request->has('groups') ? $request->request->all('groups') : ['client-benefits-check', 'report'];
         $this->formatter->setJmsSerialiserGroups($groups);
-        'ndr' === $reportOrNdr ? $this->ndrMoneyRepository->delete($moneyTypeId) : $this->reportMoneyRepository->delete($moneyTypeId);
+        $this->reportMoneyRepository->delete($moneyTypeId);
 
         return [];
     }
