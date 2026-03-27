c62b656b74b0faa74127d72da0f2a4a7f571f0ca
Elliot Smith <elliot.smith2@justice.gov.uk>
2026-03-27 16:06:20 +0000

    Remove NDR-only controllers and associated tests

--- api/app/scripts/api_integration_test.sh
+++ api/app/scripts/api_integration_test.sh
@@ -47,8 +47,6 @@ case "$INTEGRATION_SELECTION" in
     # IMPORTANT: these tests are order dependent, so don't rearrange them or try to run them as an aggregate
     printf '\n Running DBAL Suite \n\n'
     php vendor/bin/phpunit --debug -c tests/Integration tests/Integration/DBAL/ --coverage-php tests/coverage/DBAL.cov
-    printf '\n Running Controller-Ndr Suite \n\n'
-    php vendor/bin/phpunit -c tests/Integration tests/Integration/Controller-Ndr/ --coverage-php tests/coverage/Controller-Ndr.cov
     printf '\n Running Entity Suite \n\n'
     php vendor/bin/phpunit -c tests/Integration tests/Integration/Entity/ --coverage-php tests/coverage/Entity.cov
     printf '\n Running Command Suite \n\n'
