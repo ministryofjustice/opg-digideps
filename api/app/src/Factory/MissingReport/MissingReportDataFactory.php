ec73204f32e03b3b113a9e4f4518a1c8f037382d
olivier-nicodemi <265032004+olivier-nicodemi@users.noreply.github.com>
2026-04-09 15:39:30 +0100

    Integration test

--- api/app/src/Factory/MissingReport/MissingReportDataFactory.php
+++ api/app/src/Factory/MissingReport/MissingReportDataFactory.php
@@ -2,11 +2,13 @@
 
 declare(strict_types=1);
 
-namespace App\Factory;
+namespace App\Factory\MissingReport;
 
 use App\Domain\CourtOrder\CourtOrderKind;
 use App\Entity\CourtOrder;
 use App\Entity\Report\Report;
+use App\Factory\DataFactoryInterface;
+use App\Factory\DataFactoryResult;
 use App\Service\ReportService;
 use Doctrine\ORM\EntityManagerInterface;
 
@@ -14,6 +16,7 @@ final readonly class MissingReportDataFactory implements DataFactoryInterface
 {
     public function __construct(
         private EntityManagerInterface $em,
+        private MissingReportFinder $finder,
         private ReportService $reportService,
     ) {
     }
@@ -23,38 +26,11 @@ final readonly class MissingReportDataFactory implements DataFactoryInterface
         return 'MissingReport';
     }
 
-    /**
-     * @return \Generator<CourtOrder>
-     */
-    private function findCourtOrdersWithMissingReports(): \Generator
-    {
-        $repository = $this->em->getRepository(CourtOrder::class);
-        foreach (
-            $this->em->getConnection()->executeQuery("
-            SELECT DISTINCT co.id
-            FROM court_order co
-            LEFT JOIN court_order_report cor
-                ON co.id = cor.court_order_id
-            LEFT JOIN report r
-                ON r.id = cor.report_id
-            WHERE
-                NOT r.submitted
-                AND co.status <> 'ACTIVE'
-                AND r.id IS NULL
-            ")->iterateColumn() as $courtOrderId
-        ) {
-            $order = $repository->find($courtOrderId);
-            if ($order !== null) {
-                yield $order;
-            }
-        }
-    }
-
     public function run(): DataFactoryResult
     {
         $errors = [];
         $count = 0;
-        foreach ($this->findCourtOrdersWithMissingReports() as $courtOrder) {
+        foreach ($this->finder->findCourtOrdersWithMissingReports() as $courtOrder) {
             $result = $this->createReport($courtOrder);
             if ($result === null) {
                 $count++;
@@ -77,7 +53,7 @@ final readonly class MissingReportDataFactory implements DataFactoryInterface
                     $this->em->flush();
                     return null;
                 }
-                $latest ??= $courtOrder->getSibling()?->getLatestReport();
+                $latest ??= $siblingReport;
             }
 
             $newReport = $latest !== null ? $this->reportService->createNextYearReport($latest) : new Report(
@@ -87,14 +63,11 @@ final readonly class MissingReportDataFactory implements DataFactoryInterface
                 (clone $courtOrder->getOrderMadeDate())->modify('+12 months -1 day'),
                 false,
             );
-            if ((!$newReport instanceof Report)) {
-                return "Could not generate missing report for court order: {$courtOrder->getCourtOrderUid()}";
-            }
             $courtOrder->addReport($newReport);
             $this->em->flush();
         } catch (\Throwable $throwable) {
             $class = $throwable::class;
-            return "{$class} {$throwable->getMessage()} in {$throwable->getFile()}({$throwable->getLine()})";
+            return "Could not generate missing report for court order: {$courtOrder->getCourtOrderUid()}: {$class} {$throwable->getMessage()} in {$throwable->getFile()}({$throwable->getLine()})";
         }
         return null;
     }
