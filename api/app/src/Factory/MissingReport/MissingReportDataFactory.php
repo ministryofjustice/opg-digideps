c9a320561d4ee26dac28d02f7ef922efd0575f49
olivier-nicodemi <265032004+olivier-nicodemi@users.noreply.github.com>
2026-04-10 15:06:45 +0100

    Amended

--- api/app/src/Factory/MissingReport/MissingReportDataFactory.php
+++ api/app/src/Factory/MissingReport/MissingReportDataFactory.php
@@ -29,46 +29,78 @@ final readonly class MissingReportDataFactory implements DataFactoryInterface
     public function run(): DataFactoryResult
     {
         $errors = [];
-        $count = 0;
+        $created = 0;
+        $linked = 0;
+
         foreach ($this->finder->findCourtOrdersWithMissingReports() as $courtOrder) {
-            $result = $this->createReport($courtOrder);
-            if ($result === null) {
-                $count++;
-            } else {
-                $errors[] = $result;
+            try {
+                if ($this->linkHybridReportIfItExists($courtOrder)) {
+                    $linked++;
+                } else {
+                    $this->createMissingReport($courtOrder);
+                    $created++;
+                }
+            } catch (\Throwable $throwable) {
+                $class = $throwable::class;
+                $errors[] = "Could not generate missing report for court order: {$courtOrder->getCourtOrderUid()}: {$class} {$throwable->getMessage()} in {$throwable->getFile()}({$throwable->getLine()})";
             }
         }
 
-        return new DataFactoryResult(['Success' => ["Created {$count} missing reports"]], ['Errors' => $errors]);
+        return new DataFactoryResult(['success' => ["Created {$created} missing reports", "Linked {$linked} unlinked reports"]], ['errors' => $errors]);
     }
 
-    private function createReport(CourtOrder $courtOrder): ?string
+    private function createMissingReport(CourtOrder $courtOrder): void
     {
-        try {
-            $latest = $courtOrder->getLatestReport();
-            if ($courtOrder->getOrderKind() === CourtOrderKind::Hybrid) {
-                $siblingReport = $courtOrder->getSibling()?->getLatestReport();
-                if ($siblingReport !== null && empty($siblingReport->getSubmitted())) {
-                    $courtOrder->addReport($siblingReport);
-                    $this->em->flush();
-                    return null;
-                }
-                $latest ??= $siblingReport;
-            }
+        $latest = $this->getLatestReport($courtOrder);
+        $newReport = $latest !== null ? $this->createReportFromReport($latest) : $this->createReportFromOrder($courtOrder);
+        $courtOrder->addReport($newReport);
+        $this->em->persist($courtOrder);
+        $this->em->flush();
+    }
 
-            $newReport = $latest !== null ? $this->reportService->createNextYearReport($latest) : new Report(
-                $courtOrder->getClient(),
-                $courtOrder->getDesiredReportType(),
-                $courtOrder->getOrderMadeDate(),
-                (clone $courtOrder->getOrderMadeDate())->modify('+12 months -1 day'),
-                false,
-            );
-            $courtOrder->addReport($newReport);
-            $this->em->flush();
-        } catch (\Throwable $throwable) {
-            $class = $throwable::class;
-            return "Could not generate missing report for court order: {$courtOrder->getCourtOrderUid()}: {$class} {$throwable->getMessage()} in {$throwable->getFile()}({$throwable->getLine()})";
+    private function linkHybridReportIfItExists(CourtOrder $courtOrder): bool
+    {
+        if ($courtOrder->getOrderKind() === CourtOrderKind::Hybrid) {
+            $siblingReport = $courtOrder->getSibling()?->getLatestReport();
+            if ($siblingReport !== null && empty($siblingReport->getSubmitted())) {
+                $courtOrder->addReport($siblingReport);
+                $this->em->persist($courtOrder);
+                $this->em->flush();
+                return true;
+            }
         }
-        return null;
+        return false;
+    }
+
+    private function getLatestReport(CourtOrder $courtOrder): ?Report
+    {
+        $latest = $courtOrder->getLatestReport();
+        if ($latest === null && $courtOrder->getOrderKind() === CourtOrderKind::Hybrid) {
+            $latest = $courtOrder->getSibling()?->getLatestReport();
+
+            if ($latest !== null && empty($latest->getSubmitted())) {
+                throw new \LogicException('We already checked for a missing link in linkHybridReportIfItExists() so this should never happen.');
+            }
+        }
+        return $latest;
+    }
+
+    private function createReportFromOrder(CourtOrder $courtOrder): Report
+    {
+        $newReport = new Report(
+            $courtOrder->getClient(),
+            "{$courtOrder->getDesiredReportType()}",
+            $courtOrder->getOrderMadeDate(),
+            (clone $courtOrder->getOrderMadeDate())->modify('+12 months -1 day'),
+            false,
+        );
+        $newReport->updateSectionsStatusCache($newReport->getAvailableSections());
+        $this->em->persist($newReport);
+        return $newReport;
+    }
+
+    private function createReportFromReport(Report $latest): Report
+    {
+        return $this->reportService->createNextYearReport($latest);
     }
 }
