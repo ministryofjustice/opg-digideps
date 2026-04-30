f065cdb90b8ea70a151c8186cfc246f3e8414890
Elliot Smith <elliot.smith2@justice.gov.uk>
2026-04-30 12:21:57 +0100

    Use partial import for MapCell

--- api/app/src/Entity/StagingDeputyship.php
+++ api/app/src/Entity/StagingDeputyship.php
@@ -6,7 +6,7 @@ namespace OPG\Digideps\Backend\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use JMS\Serializer\Annotation as JMS;
-use League\Csv\Serializer\MapCell;
+use League\Csv\Serializer;
 use OPG\Digideps\Backend\Repository\StagingDeputyshipRepository;
 
 /**
@@ -28,70 +28,70 @@ class StagingDeputyship
     #[ORM\SequenceGenerator(sequenceName: 'deputyship_id_seq', allocationSize: 1, initialValue: 1)]
     public ?int $id = null;
 
-    #[MapCell(column: 'OrderUid')]
+    #[Serializer\MapCell(column: 'OrderUid')]
     #[ORM\Column(name: 'order_uid', type: 'string', length: 30)]
     public string $orderUid;
 
-    #[MapCell(column: 'DeputyUid')]
+    #[Serializer\MapCell(column: 'DeputyUid')]
     #[ORM\Column(name: 'deputy_uid', type: 'string', length: 30)]
     public string $deputyUid;
 
-    #[MapCell(column: 'OrderType')]
+    #[Serializer\MapCell(column: 'OrderType')]
     #[ORM\Column(name: 'order_type', type: 'string', length: 30, nullable: true)]
     public ?string $orderType;
 
-    #[MapCell(column: 'OrderSubType')]
+    #[Serializer\MapCell(column: 'OrderSubType')]
     #[ORM\Column(name: 'order_sub_type', type: 'string', length: 30, nullable: true)]
     public ?string $orderSubType;
 
-    #[MapCell(column: 'OrderMadeDate')]
+    #[Serializer\MapCell(column: 'OrderMadeDate')]
     #[ORM\Column(name: 'order_made_date', type: 'string', length: 30, nullable: true)]
     public ?string $orderMadeDate;
 
-    #[MapCell(column: 'OrderStatus')]
+    #[Serializer\MapCell(column: 'OrderStatus')]
     #[ORM\Column(name: 'order_status', type: 'string', length: 30, nullable: true)]
     public ?string $orderStatus;
 
-    #[MapCell(column: 'OrderUpdatedDate')]
+    #[Serializer\MapCell(column: 'OrderUpdatedDate')]
     #[ORM\Column(name: 'order_updated_date', type: 'string', length: 30, nullable: true)]
     public ?string $orderUpdatedDate;
 
-    #[MapCell(column: 'CaseNumber')]
+    #[Serializer\MapCell(column: 'CaseNumber')]
     #[ORM\Column(name: 'case_number', type: 'string', length: 30, nullable: true)]
     public ?string $caseNumber;
 
-    #[MapCell(column: 'ClientUid')]
+    #[Serializer\MapCell(column: 'ClientUid')]
     #[ORM\Column(name: 'client_uid', type: 'string', length: 30, nullable: true)]
     public ?string $clientUid;
 
-    #[MapCell(column: 'ClientStatus')]
+    #[Serializer\MapCell(column: 'ClientStatus')]
     #[ORM\Column(name: 'client_status', type: 'string', length: 30, nullable: true)]
     public ?string $clientStatus;
 
-    #[MapCell(column: 'ClientStatusDate')]
+    #[Serializer\MapCell(column: 'ClientStatusDate')]
     #[ORM\Column(name: 'client_status_date', type: 'string', length: 30, nullable: true)]
     public ?string $clientStatusDate;
 
-    #[MapCell(column: 'DeputyType')]
+    #[Serializer\MapCell(column: 'DeputyType')]
     #[ORM\Column(name: 'deputy_type', type: 'string', length: 30, nullable: true)]
     public ?string $deputyType;
 
     /**
      * This is true if the deputy is ACTIVE on the order, false otherwise.
      */
-    #[MapCell(column: 'DeputyStatusOnOrder')]
+    #[Serializer\MapCell(column: 'DeputyStatusOnOrder')]
     #[ORM\Column(name: 'deputy_status_on_order', type: 'string', length: 30, nullable: true)]
     public ?string $deputyStatusOnOrder;
 
-    #[MapCell(column: 'DeputyStatusChangeDate')]
+    #[Serializer\MapCell(column: 'DeputyStatusChangeDate')]
     #[ORM\Column(name: 'deputy_status_change_date', type: 'string', length: 30, nullable: true)]
     public ?string $deputyStatusChangeDateString;
 
-    #[MapCell(column: 'ReportType')]
+    #[Serializer\MapCell(column: 'ReportType')]
     #[ORM\Column(name: 'report_type', type: 'string', length: 30, nullable: true)]
     public ?string $reportType;
 
-    #[MapCell(column: 'IsHybrid')]
+    #[Serializer\MapCell(column: 'IsHybrid')]
     #[ORM\Column(name: 'is_hybrid', type: 'string', length: 30, nullable: true)]
     public ?string $isHybrid;
 
