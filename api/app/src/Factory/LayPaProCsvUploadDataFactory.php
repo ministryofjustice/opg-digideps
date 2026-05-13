ad6032ca3df6e0dacd38221b4d84199ba760978f
olivier-nicodemi <265032004+olivier-nicodemi@users.noreply.github.com>
2026-05-13 09:28:43 +0100

    Fix DDLS-1455 table ids

--- api/app/src/Factory/LayPaProCsvUploadDataFactory.php
+++ api/app/src/Factory/LayPaProCsvUploadDataFactory.php
@@ -89,7 +89,7 @@ class LayPaProCsvUploadDataFactory implements DataFactoryInterface
         private readonly ParameterBagInterface $parameters,
         private readonly S3Client $s3,
     ) {
-        $this->production = $this->parameters->get('workspace') !== 'production';
+        $this->production = $this->parameters->get('workspace') === 'production';
         $this->errors = [];
     }
 
@@ -100,15 +100,19 @@ class LayPaProCsvUploadDataFactory implements DataFactoryInterface
 
     public function run(bool $dryRun): DataFactoryResult
     {
-        $layCount = $paCount = $proCount = 0;
+        try {
+            $layCount = $paCount = $proCount = 0;
 
-        $localPaths = $this->fetchFromAWS();
-        if ($localPaths !== null) {
-            ['layPath' => $layPath, 'proPaPath' => $proPaPath] = $localPaths;
-            $layCount = $this->runLay($layPath, $dryRun);
-            $this->deleteLocalFile($layPath);
-            ['paCount' => $paCount, 'proCount' => $proCount] = $this->runPaPro($proPaPath, $dryRun);
-            $this->deleteLocalFile($proPaPath);
+            $localPaths = $this->fetchFromAWS();
+            if ($localPaths !== null) {
+                ['layPath' => $layPath, 'proPaPath' => $proPaPath] = $localPaths;
+                $layCount = $this->runLay($layPath, $dryRun);
+                $this->deleteLocalFile($layPath);
+                ['paCount' => $paCount, 'proCount' => $proCount] = $this->runPaPro($proPaPath, $dryRun);
+                $this->deleteLocalFile($proPaPath);
+            }
+        } catch (\Throwable $e) {
+            $this->errors[] = "Unexpected exception during LayPaProCsvUpload: {$e->getMessage()}";
         }
 
         return new DataFactoryResult([
@@ -157,6 +161,9 @@ class LayPaProCsvUploadDataFactory implements DataFactoryInterface
             return null;
         }
         $localPath = "/tmp/{$awsPath}";
+        if (file_exists($localPath)) {
+            unlink($localPath);
+        }
 
         try {
             $this->s3->getObject([
@@ -185,36 +192,38 @@ class LayPaProCsvUploadDataFactory implements DataFactoryInterface
             $reader = new CsvToArray(self::LAY_EXPECTED_COLUMNS, self::OPTIONAL_COLUMNS);
             foreach ($reader->createAsIterator($path) as $row => $item) {
                 $item = new ValidatingArray($item);
+                $case = $item->getStringOrThrow('Case');
+                $deputyUid = $item->getStringOrThrow('DeputyUid');
                 try {
                     yield new StagingLayIngest(
-                        $item->getStringOrThrow('Case'),
-                        $this->process($item->getStringOrThrow('ClientFirstname')),
-                        $this->process($item->getStringOrThrow('ClientSurname')),
-                        $this->process($item->getStringOrDefault('ClientAddress1', '')),
-                        $this->process($item->getStringOrDefault('ClientAddress2', '')),
-                        $this->process($item->getStringOrDefault('ClientAddress3', '')),
-                        $this->process($item->getStringOrDefault('ClientAddress4', '')),
-                        $this->process($item->getStringOrDefault('ClientAddress5', '')),
-                        $this->process($item->getStringOrDefault('ClientPostcode', ''), 'W1'),
-                        $item->getStringOrThrow('DeputyUid'),
-                        $this->process($item->getStringOrThrow('DeputyFirstname')),
-                        $this->process($item->getStringOrThrow('DeputySurname')),
-                        $this->process($item->getStringOrDefault('DeputyAddress1', '')),
-                        $this->process($item->getStringOrDefault('DeputyAddress2', '')),
-                        $this->process($item->getStringOrDefault('DeputyAddress3', '')),
-                        $this->process($item->getStringOrDefault('DeputyAddress4', '')),
-                        $this->process($item->getStringOrDefault('DeputyAddress5', '')),
-                        $this->process($item->getStringOrDefault('DeputyPostcode', ''), 'W2'),
+                        $this->validate($case, 20),
+                        $this->process($this->validate($item->getStringOrThrow('ClientFirstname'), 50)),
+                        $this->process($this->validate($item->getStringOrThrow('ClientSurname'), 50)),
+                        $this->process($this->validate($item->getStringOrDefault('ClientAddress1', ''), 200)),
+                        $this->process($this->validate($item->getStringOrDefault('ClientAddress2', ''), 200)),
+                        $this->process($this->validate($item->getStringOrDefault('ClientAddress3', ''), 200)),
+                        $this->process($this->validate($item->getStringOrDefault('ClientAddress4', ''), 200)),
+                        $this->process($this->validate($item->getStringOrDefault('ClientAddress5', ''), 200)),
+                        $this->process($this->validate($item->getStringOrDefault('ClientPostcode', ''), 10), 'W1'),
+                        $this->validate($deputyUid, 20),
+                        $this->process($this->validate($item->getStringOrThrow('DeputyFirstname'), 100)),
+                        $this->process($this->validate($item->getStringOrThrow('DeputySurname'), 100)),
+                        $this->process($this->validate($item->getStringOrDefault('DeputyAddress1', ''), 200)),
+                        $this->process($this->validate($item->getStringOrDefault('DeputyAddress2', ''), 200)),
+                        $this->process($this->validate($item->getStringOrDefault('DeputyAddress3', ''), 200)),
+                        $this->process($this->validate($item->getStringOrDefault('DeputyAddress4', ''), 200)),
+                        $this->process($this->validate($item->getStringOrDefault('DeputyAddress5', ''), 200)),
+                        $this->process($this->validate($item->getStringOrDefault('DeputyPostcode', ''), 10), 'W2'),
                         CourtOrderReportType::from(strtoupper($item->getStringOrThrow('ReportType'))),
                         new \DateTimeImmutable($item->getStringOrThrow('MadeDate')),
                         CourtOrderType::from(strtolower($item->getStringOrThrow('OrderType'))),
                     );
                 } catch (\Throwable $e) {
-                    $this->errors[] = "Error processing CSV row {$row}: {$e->getMessage()}";
+                    $this->errors[] = "Error processing lay CSV row {$row} - {$case} - {$deputyUid}: {$e->getMessage()}";
                 }
             }
         } catch (\Throwable $e) {
-            $this->errors[] = "Error processing CSV: {$e->getMessage()}";
+            $this->errors[] = "Error processing lay CSV: {$e->getMessage()}";
         }
     }
 
@@ -228,51 +237,65 @@ class LayPaProCsvUploadDataFactory implements DataFactoryInterface
             $reader = new CsvToArray(self::PRO_PA_EXPECTED_COLUMNS, self::OPTIONAL_COLUMNS);
             foreach ($reader->createAsIterator($path) as $row => $item) {
                 $item = new ValidatingArray($item);
+                $case = $item->getStringOrThrow('Case');
+                $deputyUid = $item->getStringOrThrow('DeputyUid');
                 try {
                     yield new StagingPaProIngest(
-                        $item->getStringOrThrow('Case'),
-                        $this->process($item->getStringOrThrow('ClientFirstname')),
-                        $this->process($item->getStringOrThrow('ClientSurname')),
+                        $this->validate($case, 20),
+                        $this->process($this->validate($item->getStringOrThrow('ClientForename'), 50)),
+                        $this->process($this->validate($item->getStringOrThrow('ClientSurname'), 50)),
                         new \DateTimeImmutable($this->process($item->getStringOrDefault('ClientDateOfBirth', ''), '1953-06-02')),
-                        $this->process($item->getStringOrDefault('ClientAddress1', '')),
-                        $this->process($item->getStringOrDefault('ClientAddress2', '')),
-                        $this->process($item->getStringOrDefault('ClientAddress3', '')),
-                        $this->process($item->getStringOrDefault('ClientAddress4', '')),
-                        $this->process($item->getStringOrDefault('ClientAddress5', '')),
-                        $this->process($item->getStringOrDefault('ClientPostcode', ''), 'W1'),
+                        $this->process($this->validate($item->getStringOrDefault('ClientAddress1', ''), 200)),
+                        $this->process($this->validate($item->getStringOrDefault('ClientAddress2', ''), 200)),
+                        $this->process($this->validate($item->getStringOrDefault('ClientAddress3', ''), 200)),
+                        $this->process($this->validate($item->getStringOrDefault('ClientAddress4', ''), 200)),
+                        $this->process($this->validate($item->getStringOrDefault('ClientAddress5', ''), 200)),
+                        $this->process($this->validate($item->getStringOrDefault('ClientPostcode', ''), 10), 'W1'),
                         DeputyType::from(strtoupper($item->getStringOrThrow('DeputyType'))),
-                        $item->getStringOrThrow('DeputyUid'),
-                        $this->process($item->getStringOrThrow('DeputyEmail'), email: true),
-                        $item->getStringOrDefault('DeputyOrganisation', ''),
-                        $this->process($item->getStringOrThrow('DeputyFirstname')),
-                        $this->process($item->getStringOrThrow('DeputySurname')),
-                        $this->process($item->getStringOrDefault('DeputyAddress1', '')),
-                        $this->process($item->getStringOrDefault('DeputyAddress2', '')),
-                        $this->process($item->getStringOrDefault('DeputyAddress3', '')),
-                        $this->process($item->getStringOrDefault('DeputyAddress4', '')),
-                        $this->process($item->getStringOrDefault('DeputyAddress5', '')),
-                        $this->process($item->getStringOrDefault('DeputyPostcode', ''), 'W2'),
+                        $this->validate($deputyUid, 20),
+                        $this->process($this->validate($item->getStringOrThrow('DeputyEmail'), 60), email: true),
+                        $this->validate($item->getStringOrDefault('DeputyOrganisation', ''), 100),
+                        $this->process($this->validate($item->getStringOrThrow('DeputyForename'), 100)),
+                        $this->process($this->validate($item->getStringOrThrow('DeputySurname'), 100)),
+                        $this->process($this->validate($item->getStringOrDefault('DeputyAddress1', ''), 200)),
+                        $this->process($this->validate($item->getStringOrDefault('DeputyAddress2', ''), 200)),
+                        $this->process($this->validate($item->getStringOrDefault('DeputyAddress3', ''), 200)),
+                        $this->process($this->validate($item->getStringOrDefault('DeputyAddress4', ''), 200)),
+                        $this->process($this->validate($item->getStringOrDefault('DeputyAddress5', ''), 200)),
+                        $this->process($this->validate($item->getStringOrDefault('DeputyPostcode', ''), 10), 'W2'),
                         new \DateTimeImmutable($item->getStringOrThrow('MadeDate')),
                         CourtOrderReportType::from(strtoupper($item->getStringOrThrow('ReportType'))),
                         CourtOrderType::from(strtolower($item->getStringOrThrow('OrderType'))),
                     );
                 } catch (\Throwable $e) {
-                    $this->errors[] = "Error processing CSV row {$row}: {$e->getMessage()}";
+                    $this->errors[] = "Error processing pa-pro CSV row {$row} - {$case} - {$deputyUid}: {$e->getMessage()}";
                 }
             }
         } catch (\Throwable $e) {
-            $this->errors[] = "Error processing CSV: {$e->getMessage()}";
+            $this->errors[] = "Error processing pa-pro CSV: {$e->getMessage()}";
         }
+    }
+
+    private function validate(string $value, int $length): string
+    {
+        if (strlen($value) > $length) {
+            throw new \DomainException("Value was expected to be at most {$length} characters long.");
+        }
+        return $value;
     }
 
     private function process(string $value, ?string $static = null, bool $email = false): string
     {
         if (!$this->production && $value !== '') {
             if ($static !== null) {
+                if (strlen($value) > 10) {
+                    throw new \DomainException("Value was expected to be at most 10 characters long. Got: {$value}");
+                }
                 $value = $static;
             } elseif ($email && str_contains($value, '@')) {
                 [$user, $domain] = explode('@', $value, 2);
-                $value = "{$this->anonymise($user)}@{$domain}";
+                $user = substr($this->anonymise($user), 0, 59 - strlen($domain));
+                $value = "{$user}@{$domain}";
             } else {
                 $value = $this->anonymise($value);
                 $value[0] = strtoupper($value[0]);
