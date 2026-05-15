6f8eec0d97c0f3c95e103f7485ede8e8676e0775
Elliot Smith <elliot.smith2@justice.gov.uk>
2026-05-15 18:04:29 +0100

    Changes on some form elements should be ignored

--- client/resources/assets/javascripts/modules_new/Autosave.js
+++ client/resources/assets/javascripts/modules_new/Autosave.js
@@ -11,28 +11,56 @@ const Autosave = {
 
     autosaveForms.forEach(autosaveForm => {
       const saveButton = autosaveForm.querySelector('button.js-autosave-save-progress-button')
-      saveButton.addEventListener('click', this.makeHandler(autosaveForm, saveButton))
+
+      // Form elements which, when they change, don't trigger an autosave.
+      //
+      // This is necessary for forms which have a separate submit button for
+      // some fields, e.g. the "Save further information" button on the checklist page.
+      //
+      // Note that these elements are still included if the "save progress"
+      // button is explicitly pressed (preserving current behaviour on
+      // the checklist page, for example).
+      const ignoredElements = autosaveForm.querySelectorAll('.js-autosave-ignore')
+
+      const handler = this.makeHandler(autosaveForm, saveButton, ignoredElements)
+
+      saveButton.addEventListener('click', handler)
+      autosaveForm.addEventListener('change', handler)
     })
   },
 
-  makeHandler: function (autosaveForm, saveProgressButton) {
+  makeHandler: function (autosaveForm, saveProgressButton, ignoredElements) {
     return async (e) => {
-      e.preventDefault()
-      saveProgressButton.disabled = true
+      let ignoredElement = false
 
-      const formData = new FormData(autosaveForm)
-
-      // the name of the button has to be part of the payload
-      // so that the controller can route it correctly
-      formData.set(saveProgressButton.name, saveProgressButton.value)
-
-      try {
-        await fetch(autosaveForm.action, {
-          method: 'POST',
-          body: formData
+      // if the event is a change event and came from an ignored element,
+      // don't do anything
+      if (e.type === 'change') {
+        ignoredElements.forEach((el) => {
+          if (el === e.target) {
+            ignoredElement = true
+          }
         })
-      } finally {
-        saveProgressButton.disabled = false
+      }
+
+      if (!ignoredElement) {
+        e.preventDefault()
+        saveProgressButton.disabled = true
+
+        const formData = new FormData(autosaveForm)
+
+        // the name of the "save progress" button has to be part of the payload
+        // so that the controller can route the request correctly
+        formData.set(saveProgressButton.name, saveProgressButton.value)
+
+        try {
+          await fetch(autosaveForm.action, {
+            method: 'POST',
+            body: formData
+          })
+        } finally {
+          saveProgressButton.disabled = false
+        }
       }
     }
   }
