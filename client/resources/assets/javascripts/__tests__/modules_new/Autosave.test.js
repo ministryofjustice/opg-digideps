065450e50a3cc47f07142ab173a77cb5661f82eb
Elliot Smith <elliot.smith2@justice.gov.uk>
2026-05-19 10:16:54 +0100

    Use fetchMock from jest instead of homebrew

--- client/resources/assets/javascripts/__tests__/modules_new/Autosave.test.js
+++ client/resources/assets/javascripts/__tests__/modules_new/Autosave.test.js
@@ -1,33 +1,42 @@
-import { describe, expect, it } from '@jest/globals'
+import { beforeEach, describe, expect, it } from '@jest/globals'
+import fetchMock from 'jest-fetch-mock'
 import Autosave from '../../modules_new/Autosave'
 
+fetchMock.enableMocks()
+
 describe('Form autosave', () => {
-  document.body.innerHTML = `
-    <form class="js-autosave" action="/save/form">
-      <input type="text" name="field1">
-      <input type="text" name="field2" class="js-autosave-ignore">
-      <button class="js-autosave-save-progress-button">Save progress</button>
-    </form>
-  `
+  beforeEach(() => {
+    fetchMock.resetMocks()
 
-  it('should save even if change events are not triggered', () => {
-    let mockFetchCalled = false
+    document.body.innerHTML = `
+      <form class="js-autosave" action="/save/form">
+        <input type="text" name="field1">
+        <input type="text" name="field2" class="js-autosave-ignore">
+        <button class="js-autosave-save-progress-button">Save progress</button>
+      </form>
+    `
+  })
 
-    const mockFetch = () => {
-      mockFetchCalled = true
+  it('should save even if change events are not triggered', async () => {
+    fetchMock.mockResponse(JSON.stringify({ status: 200 }))
 
-      return Promise.resolve({
-        ok: true
-      })
-    }
-
-    const autosaver = Autosave.init(document, mockFetch, 0.1)
+    const autosaver = Autosave.init(document, 0.1, fetchMock)
 
     // add a timer to stop the autosave on the form from running for too long
     const form = autosaver.autosaveForms[0]
-    setTimeout(() => {
+
+    await new Promise((resolve) => setTimeout(() => {
       clearInterval(parseInt(form.getAttribute('autosave-interval-id')))
-      expect(mockFetchCalled).toBe(true)
-    }, 0.3)
+      resolve()
+    }, 200))
+
+    expect(fetchMock).toHaveBeenCalled()
+
+    // check args passed to fetch
+    const [url, options] = fetchMock.mock.calls[0]
+
+    expect(url).toContain('/save/form')
+    expect(options.method).toBe('POST')
+    expect(options.body).toBeInstanceOf(FormData)
   })
 })
