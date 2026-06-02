3c49057bba6b9f5052dea643a54068104d838117
Elliot Smith <elliot.smith2@justice.gov.uk>
2026-06-02 17:10:51 +0100

    Reapply "DDLS-1214 Autosave the checklist form (#2600)" (#2644)
    This reverts commit 298efc18de15d2a606c44121dab90919be8ad5b3.

--- api/app/src/Controller/Report/ReportController.php
+++ api/app/src/Controller/Report/ReportController.php
@@ -917,7 +917,6 @@ class ReportController extends RestController
             throw new UnauthorisedException('client secret not accepted.');
         }
 
-        /** @var array $data */
         $data = $this->formatter->deserializeBodyContent($request);
 
         /** @var ReportRepository $reportRepo */
