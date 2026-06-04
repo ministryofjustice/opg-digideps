9a0cb2de42153a2933dbf6d513d79d96edee9a4d
olivier-nicodemi <265032004+olivier-nicodemi@users.noreply.github.com>
2026-06-04 12:54:42 +0100

    DDLS-1472 api - deputy

--- api/app/src/TestHelpers/DeputyTestHelper.php
+++ api/app/src/TestHelpers/DeputyTestHelper.php
@@ -21,18 +21,21 @@ class DeputyTestHelper
         if ($deputyUid !== null && $em !== null) {
             $deputy = $em->getRepository(Deputy::class)->findOneBy(['deputyUid' => (string)$deputyUid]);
         }
+        $deputyUid = (string)($deputyUid ?? $faker->randomNumber(8));
+        $firstname = is_null($user) ? $faker->firstName() : $user->getFirstName();
+        $lastname = is_null($user) ? $faker->lastName() : $user->getLastName();
 
-        $deputy = ($deputy ?? new Deputy())
-            ->setDeputyUid((string)($deputyUid ?? $faker->randomNumber(8)))
-            ->setFirstname(is_null($user) ? $faker->firstName() : $user->getFirstName())
-            ->setLastname(is_null($user) ? $faker->lastName() : $user->getLastName())
+
+        $deputy = ($deputy ?? new Deputy($deputyUid, DeputyType::LAY, $firstname, $lastname))
+            ->setDeputyUid($deputyUid)
+            ->setFirstname($firstname)
+            ->setLastname($lastname)
             ->setEmail1($email ?: $faker->safeEmail() . rand(1, 100000))
             ->setAddress1(is_null($user) ? $faker->streetAddress() : $user->getAddress1())
             ->setAddress2(is_null($user) ? $faker->city() : $user->getAddress2())
             ->setAddress3(is_null($user) ? $faker->county : $user->getAddress3())
             ->setAddressPostcode(is_null($user) ? $faker->postcode() : $user->getAddressPostcode())
             ->setPhoneMain(is_null($user) ? $faker->phoneNumber() : $user->getPhoneMain())
-            ->setDeputyType(DeputyType::LAY)
             ->setOrganisation(null);
 
         if (!is_null($user)) {
