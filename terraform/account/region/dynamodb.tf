33a3c17626ddc061b982b703e730597aa4d16143
Matty Cooke <matthew.cooke@justice.gov.uk>
2026-06-08 10:17:33 +0100

    fixed alerts

--- terraform/account/region/dynamodb.tf
+++ terraform/account/region/dynamodb.tf
@@ -11,10 +11,6 @@ resource "aws_dynamodb_table" "workspace_cleanup_table" {
     type = "S"
   }
 
-  point_in_time_recovery {
-    enabled = true
-  }
-
   ttl {
     attribute_name = "ExpiresTTL"
     enabled        = true
