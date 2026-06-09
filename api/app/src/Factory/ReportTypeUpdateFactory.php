92a9ca887b0d29366f2cbd1fbe844f6ea5cf3959
Elliot Smith <elliot.smith2@justice.gov.uk>
2026-06-09 09:44:05 +0100

    DDLS-1450 Update all report types, not just those on changed court orders (#2651)
    * DDLS-1450 Set report types on all reports, not just possibly changed ones
    
    * Exclude reports which only have inactive court orders

--- api/app/src/Factory/ReportTypeUpdateFactory.php
+++ api/app/src/Factory/ReportTypeUpdateFactory.php
@@ -10,7 +10,6 @@ use OPG\Digideps\Backend\Entity\CourtOrder;
 use OPG\Digideps\Backend\Entity\Report\Report;
 use OPG\Digideps\Backend\Repository\ReportRepository;
 use OPG\Digideps\Backend\Service\ReportTypeService;
-use OPG\Digideps\Backend\v2\Registration\Enum\DeputyshipCandidateAction;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Query\ResultSetMappingBuilder;
 use Psr\Log\LoggerInterface;
@@ -32,37 +31,20 @@ readonly class ReportTypeUpdateFactory implements DataFactoryInterface
     /**
      * @return \Generator<Report>
      */
-    private function getChangedReports(): \Generator
+    private function getAllReportsOnActiveCourtOrders(): \Generator
     {
-        $actionsNeeded = [
-            DeputyshipCandidateAction::InsertOrderDeputy->value,
-            DeputyshipCandidateAction::InsertOrderReport->value,
-            DeputyshipCandidateAction::UpdateDeputyStatus->value
-        ];
-
-        $sql = <<<SQL
-        SELECT DISTINCT r.id
-        FROM court_order co
-        INNER JOIN staging.selectedcandidates ssc ON ssc.order_uid = co.court_order_uid
-        INNER JOIN court_order_report cor ON cor.court_order_id = co.id
-        INNER JOIN report r ON r.id = cor.report_id
-        LEFT JOIN report_submission rs ON rs.report_id = r.id
-        WHERE
-            ssc.action IN (:actions)
-        AND
-            rs.id IS NULL
-        SQL;
-
         $rsm = new ResultSetMappingBuilder($this->entityManager);
-        $rsm->addScalarResult('id', 'id');
-        $reportIds = $this->entityManager
-            ->createNativeQuery($sql, $rsm)
-            ->setParameters(['actions' => $actionsNeeded]);
+        $rsm->addRootEntityFromClassMetadata(Report::class, 'r');
 
-        foreach ($reportIds->toIterable() as $reportId) {
-            /** @var Report $report */
-            $report = $this->reportRepository->find($reportId);
+        /** @var Report[] $result */
+        $result = $this->entityManager->createNativeQuery(<<<SQL
+            SELECT DISTINCT r.* FROM report r
+            INNER JOIN court_order_report cor ON cor.report_id = r.id
+            INNER JOIN court_order co ON co.id = cor.court_order_id
+            WHERE co.status = 'ACTIVE'
+        SQL, $rsm)->getResult();
 
+        foreach ($result as $report) {
             yield $report;
         }
     }
@@ -73,7 +55,7 @@ readonly class ReportTypeUpdateFactory implements DataFactoryInterface
         $indeterminate = [];
         $dangerous = [];
 
-        foreach ($this->getChangedReports() as $report) {
+        foreach ($this->getAllReportsOnActiveCourtOrders() as $report) {
             $reportId = $report->getId();
 
             /** @var CourtOrder[] $courtOrders */
@@ -86,11 +68,13 @@ readonly class ReportTypeUpdateFactory implements DataFactoryInterface
                 continue;
             }
 
+            // ignore if we couldn't figure out a valid report type
             if ($possibleReportType === null) {
                 $indeterminate[] = $reportId;
                 continue;
             }
 
+            // ignore hybrid <-> separate reports(s) transitions
             if (
                 $currentReportType !== null &&
                 (
@@ -121,14 +105,12 @@ readonly class ReportTypeUpdateFactory implements DataFactoryInterface
 
         $messages = ['success' => ["Updated $count report types"]];
 
-        // don't treat indeterminate or dangerous report type transitions as errors which will stop the ingest;
-        // just warn about them
+        // don't treat indeterminate or dangerous report type transitions as errors which will stop the ingest
         $numIndeterminate = count($indeterminate);
         if ($numIndeterminate > 0) {
             $messages['indeterminate'] = ["Unable to determine report type for $numIndeterminate report IDs: " . implode(', ', $indeterminate)];
         }
 
-        // while we log this as a warning, we apply the change to/from hybrid anyway
         $numDangerous = count($dangerous);
         if ($numDangerous > 0) {
             $messages['dangerous'] = ["Possible dangerous change of report type to/from hybrid for $numDangerous report IDs: " . implode(', ', $dangerous)];
