db883a7c84159f4a7337273df1053f3750dc1f41
Elliot Smith <elliot.smith2@justice.gov.uk>
2026-06-12 16:17:40 +0100

    Upgrading Notify client requires a change to our mock

--- client/app/src/Service/JWT/JWTService.php
+++ client/app/src/Service/JWT/JWTService.php
@@ -7,7 +7,6 @@ namespace OPG\Digideps\Frontend\Service\JWT;
 use Jose\Component\Core\AlgorithmManager;
 use Jose\Component\Core\JWKSet;
 use Jose\Component\Signature\Algorithm\RS256;
-use Jose\Component\Signature\JWS;
 use Jose\Component\Signature\JWSVerifier;
 use Jose\Component\Signature\Serializer\CompactSerializer;
 use Jose\Component\Signature\Serializer\JWSSerializerManager;
@@ -15,36 +14,34 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class JWTService
 {
+    private JWSVerifier $jwsVerifier;
+    private JWSSerializerManager $serializerManager;
+
     public function __construct(
         private readonly HttpClientInterface $openInternetClient,
     ) {
-    }
-
-    private function decodeAndVerifyWithJWK(JWS $jws, array $jwks): void
-    {
-        $kid = $jws->getSignature(0)->getProtectedHeader()['kid'];
-        $jwk = JWKSet::createFromKeyData($jwks)->get($kid);
-
-        $jwsVerifier = new JWSVerifier(new AlgorithmManager([new RS256()]));
-
-        $isVerified = $jwsVerifier->verifyWithKey($jws, $jwk, 0);
-
-        if (!$isVerified) {
-            throw new \DomainException('Invalid JWS');
-        }
+        $this->jwsVerifier = new JWSVerifier(new AlgorithmManager([new RS256()]));
+        $this->serializerManager = new JWSSerializerManager([new CompactSerializer()]);
     }
 
     public function getUrn(string $jwt): ?string
     {
-        $serializerManager = new JWSSerializerManager([new CompactSerializer()]);
-        $jws = $serializerManager->unserialize($jwt);
-        $jku = $jws->getSignature(0)->getProtectedHeader()['jku'];
+        $jws = $this->serializerManager->unserialize($jwt);
+        $signature = $jws->getSignature(0);
+        $jku = $signature->getProtectedHeader()['jku'];
 
         // Get public key from API
         $jwkResponse = $this->openInternetClient->request('GET', $jku);
         $jwks = json_decode($jwkResponse->getContent(), true);
 
-        $this->decodeAndVerifyWithJWK($jws, $jwks);
+        $kid = $signature->getProtectedHeader()['kid'];
+        $jwk = JWKSet::createFromKeyData($jwks)->get($kid);
+
+        $isVerified = $this->jwsVerifier->verifyWithKey($jws, $jwk, 0);
+
+        if (!$isVerified) {
+            throw new \DomainException('Invalid JWS');
+        }
 
         return json_decode($jws->getPayload(), true)['sub'] ?? null;
     }
