411f425b72f25168e98586671f0b77a64dbff42d
Elliot Smith <elliot.smith2@justice.gov.uk>
2026-06-16 17:42:26 +0100

    Ensure exception thrown during auth is a 498 code

--- api/app/src/Security/LoginRequestAuthenticator.php
+++ api/app/src/Security/LoginRequestAuthenticator.php
@@ -16,6 +16,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationCredentialsNotFoundException;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\TooManyLoginAttemptsAuthenticationException;
 use Symfony\Component\Security\Core\Exception\UserNotFoundException;
@@ -133,7 +134,7 @@ class LoginRequestAuthenticator extends AbstractAuthenticator
             throw new TooManyLoginAttemptsAuthenticationException($interval);
         }
 
-        throw $exception;
+        throw new AuthenticationCredentialsNotFoundException($exception->getMessage(), 498);
     }
 
     private function hasRequiredLoginDetails(Request $request): bool
