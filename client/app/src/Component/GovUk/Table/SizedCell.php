c7607c3f93eef55d4b624e7ca10b1118a734153b
Olivier Nicodemi <265032004+olivier-nicodemi@users.noreply.github.com>
2026-06-17 14:28:35 +0100

    DDLS-1280 (#2704)
    * DDLS-1280
    
    * DDLS-1280 post prod review

---
+++ client/app/src/Component/GovUk/Table/SizedCell.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OPG\Digideps\Frontend\Component\GovUk\Table;
+
+final readonly class SizedCell
+{
+    public string $classNames;
+
+    public function __construct(
+        public string $content,
+        public bool $isHeader = false,
+        ?string $format = null,
+        ?int $count = null,
+    ) {
+        $classNames = ['govuk-table__cell'];
+        if ($this->isHeader) {
+            $classNames[] = 'govuk-table__header';
+        }
+        if ($format === 'numeric') {
+            $classNames[] = 'govuk-table__cell--numeric';
+        }
+        if ($count !== null) {
+            $classNames[] = match ($count) {
+                1 => 'govuk-!-width-full',
+                2 => 'govuk-!-width-one-half',
+                3 => 'govuk-!-width-one-third',
+                4 => 'govuk-!-width-one-fourth',
+                default => null,
+            };
+        }
+
+        $this->classNames = implode(' ', $classNames);
+    }
+}
