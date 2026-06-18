c1359113ad1a26f79d9606029555c97946e9fe90
olivier-nicodemi <265032004+olivier-nicodemi@users.noreply.github.com>
2026-06-18 13:59:11 +0100

    DDLS-1291

--- client/app/src/Component/GovUk/Table/Row.php
+++ client/app/src/Component/GovUk/Table/Row.php
@@ -8,13 +8,21 @@ final readonly class Row
 {
     public array $cells;
 
-    public function __construct(bool $sized, bool $firstColumnIsHeader, Cell ...$cells)
-    {
+    public function __construct(
+        bool $sized,
+        bool $firstColumnIsHeader,
+        bool $isTableHeader,
+        Cell ...$cells
+    ) {
         $sizedCells = [];
         foreach ($cells as $index => $cell) {
+            $isFirstColumn = $index === 0;
+            $isHeader = $cell->isHeader ?? ($isFirstColumn && $firstColumnIsHeader);
+            $hasScope = $isTableHeader || ($isFirstColumn && $isHeader);
             $sizedCells[] = new SizedCell(
                 $cell->content,
-                $cell->isHeader ?? ($index === 0 && $firstColumnIsHeader),
+                $isHeader,
+                $hasScope,
                 $cell->format,
                 $sized ? count($cells) : null
             );
