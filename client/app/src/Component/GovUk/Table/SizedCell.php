7cfc728bcc9c10a1a3eb06f3306931315d3aa7cc
olivier-nicodemi <265032004+olivier-nicodemi@users.noreply.github.com>
2026-06-18 18:06:30 +0100

    DDLS-1290 + DDLS-1289

--- client/app/src/Component/GovUk/Table/SizedCell.php
+++ client/app/src/Component/GovUk/Table/SizedCell.php
@@ -14,7 +14,13 @@ final readonly class SizedCell
         public bool $hasScope = false,
         ?string $format = null,
         ?int $count = null,
+        int $size = 1,
     ) {
+        $this->classNames = $this->calculateClassNames($format, $count, $size);
+    }
+
+    private function calculateClassNames(?string $format, ?int $count, int $size): string
+    {
         $classNames = ['govuk-table__cell'];
         if ($this->isHeader) {
             $classNames[] = 'govuk-table__header';
@@ -22,16 +28,27 @@ final readonly class SizedCell
         if ($format === 'numeric') {
             $classNames[] = 'govuk-table__cell--numeric';
         }
-        if ($count !== null) {
-            $classNames[] = match ($count) {
-                1 => 'govuk-!-width-full',
-                2 => 'govuk-!-width-one-half',
-                3 => 'govuk-!-width-one-third',
-                4 => 'govuk-!-width-one-fourth',
-                default => null,
-            };
+        if ($count !== null && $size <= $count) {
+            $classNames[] = $this->calculateSizeClassName($size, $count);
         }
+        return implode(' ', $classNames);
+    }
 
-        $this->classNames = implode(' ', $classNames);
+    private function calculateSizeClassName(int $size, int $count): string
+    {
+        $dividend = match ($size) {
+            2 => 'two',
+            3 => 'three',
+            4 => 'fourth',
+            default => 'one',
+        };
+        $divisor = match ($count) {
+            2 => 'half',
+            3 => 'third',
+            4 => 'fourth',
+            default => 'one',
+        };
+
+        return $dividend === $divisor ? 'govuk-!-width-full' : "govuk-!-width-{$dividend}-{$divisor}";
     }
 }
