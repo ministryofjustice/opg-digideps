d0e8df66b64ff7bb8d33c2bb2855580d2475f8ea
Elliot Smith <elliot.smith2@justice.gov.uk>
2026-07-03 09:59:46 +0100

    Basic playwright setup to create fixtures via API

--- playwright/tests/fixtures/fixtures.ts
+++ playwright/tests/fixtures/fixtures.ts
@@ -11,6 +11,14 @@ interface TestUser {
   password: string;
 }
 
+interface Scenario {
+  clientId: number
+}
+
+interface ScenarioFunction {
+  (authToken: string): Promise<Scenario | null>;
+}
+
 const testPassword = "DigidepsPass1234";
 
 const fixtureUsers: Record<UserType, FixtureUser> = {
@@ -34,13 +42,53 @@ export function getUserFixture(type: UserType): TestUser {
   };
 }
 
-export function loginToApi(user: TestUser): Promise<Response> {
-  return fetch(new Request(apiUrl + "/auth/login", {
+// login to the API and return the auth token from the response headers
+async function getAuthToken(user: TestUser): Promise<string | null> {
+  const res = await fetch(new Request(apiUrl + "/auth/login", {
     method: "POST",
-    body: JSON.stringify({"email": user.email, "password": user.password}),
+    body: JSON.stringify({ "email": user.email, "password": user.password }),
     headers: {
-      "ClientSecret": "api-frontend-key",
+      // TODO get from env
+      "ClientSecret": "api-admin-key",
       "Content-Type": "application/json"
     }
   }))
+
+  return res.headers.get("authtoken")
+}
+
+export async function createSimpleLay(authToken: string): Promise<Scenario | null> {
+  return await fetch(new Request(apiUrl + "/fixtures/scenarios/simplelay", {
+    method: "POST",
+    headers: {
+      "AuthToken": authToken,
+    }
+  }))
+    .then(res => res.text())
+    .then(text => {
+      let scenario: Scenario = JSON.parse(text)["data"]
+      return scenario
+    })
+    .catch(err => {
+      console.error(err)
+      return null
+    })
+}
+
+export async function setupScenario(scenarioFn: ScenarioFunction): Promise<Scenario | null> {
+  const user = getUserFixture("admin_user")
+
+  // set up scenario
+  return await getAuthToken(user)
+    .then(authToken => {
+      if (authToken === null) {
+        throw new Error("No auth token")
+      }
+
+      return scenarioFn(authToken)
+    })
+    .catch(err => {
+      console.error(err)
+      return null
+    })
 }
