e1818ae48f344882a1ebbe174bbf2f4539973f35
Elliot Smith <elliot.smith2@justice.gov.uk>
2026-07-08 14:49:59 +0100

    Replicate old attaching-further-documents feature with Playwright

---
+++ playwright/tests/pages/AdminLoginPage.ts
@@ -0,0 +1,23 @@
+import process = require("process")
+
+import { Page } from "@playwright/test";
+
+export default class AdminLoginPage {
+  constructor(private page: Page, private adminURL: string|undefined = process.env.ADMIN_URL) {
+    if (this.adminURL === undefined) {
+      throw new Error("ADMIN_URL is not set");
+    }
+  }
+
+  async loginAdmin(user: { email: string; password: string }) {
+    await this.page.goto(this.adminURL + "/login");
+
+    await this.page.fill("#login_email", user.email);
+    await this.page.fill("#login_password", user.password);
+
+    await Promise.all([
+      this.page.waitForLoadState("networkidle"),
+      this.page.click("#login_login"),
+    ]);
+  }
+}
