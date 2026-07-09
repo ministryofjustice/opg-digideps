3e7365de5873086a4a2a58ca41de2e7bd9e09443
Elliot Smith <elliot.smith2@justice.gov.uk>
2026-07-09 12:44:32 +0100

    Complete first documents page test

--- playwright/tests/pages/DocumentsSection.ts
+++ playwright/tests/pages/DocumentsSection.ts
@@ -9,4 +9,9 @@ export default class DocumentsSection {
     await this.page.goto("/report/" + String(reportId) + "/documents")
     await this.page.locator(".behat-link-start").click()
   }
+
+  async checkDocumentsToAdd(option: "yes" | "no") {
+    await this.page.locator('[name="document[wishToProvideDocumentation]"][value="' + option + '"]').setChecked(true)
+    await this.page.getByText("Save and continue").click()
+  }
 }
--- playwright/tests/test-documents.spec.ts
+++ playwright/tests/test-documents.spec.ts
@@ -1,32 +1,52 @@
-import { test } from "@playwright/test"
+import { Page, test } from "@playwright/test";
 import { createSimpleLay, Scenario, setupScenario, testPassword } from "./fixtures/fixtures";
 import DocumentsSection from "./pages/DocumentsSection"
 import LoginPage from "./pages/LoginPage";
+import ReportOverviewPage from "./pages/ReportOverviewPage";
 
 const deputyReference = "documents-user"
 
-test("a user has no supporting documents to add", async ({ page }) => {
-  const runTest = async function (scenario: Scenario) {
-    const reportId = scenario.orders[0].reports[1].id
-    const email = scenario.users[deputyReference].email
+const startDocumentsSection = async (
+  page: Page, email: string, reportId: number, documentsToAdd: "yes" | "no"
+): Promise<DocumentsSection> => {
+  // login as deputy
+  const loginPage = new LoginPage(page)
+  await loginPage.goto()
+  await loginPage.login({ email: email, password: testPassword })
 
-    // login as deputy
-    const loginPage = new LoginPage(page)
-    await loginPage.goto()
-    await loginPage.login({ email: email, password: testPassword })
+  // start documents section
+  const documentsSection = new DocumentsSection(page)
+  await documentsSection.start(reportId)
+  await documentsSection.checkDocumentsToAdd(documentsToAdd)
 
-    // start documents section
-    const documentsSection = new DocumentsSection(page)
-    await documentsSection.start(reportId)
-  }
+  return documentsSection
+}
 
-  await setupScenario(createSimpleLay(deputyReference))
+const setupAndRunTest = (runTest: (scenario: Scenario) => Promise<void>) => {
+  return setupScenario(createSimpleLay(deputyReference))
     .then(scenario => {
       if (scenario === null) {
-        throw new Error("Unable to create scenario for attaching further documents")
+        throw new Error("Unable to create scenario for attaching further documents");
       }
 
       return scenario
     })
     .then(runTest)
+}
+
+test("a user has no supporting documents to add", async ({ page }) => {
+  const runTest = async (scenario: Scenario): Promise<void> => {
+    // use reports[1], as we don't want the submitted report, we want the current one
+    const reportId = scenario.orders[0].reports[1].id
+    const email = scenario.users[deputyReference].email
+
+    await startDocumentsSection(page, email, reportId, "no")
+
+    // check "No documents" is shown for documents section in report overview
+    const reportOverviewPage = new ReportOverviewPage(page)
+    await reportOverviewPage.goto(reportId)
+    await reportOverviewPage.expectSectionStatus("Supporting documents", "No documents")
+  }
+
+  await setupAndRunTest(runTest)
 })
