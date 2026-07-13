d2e139878d7da786cf3636cac1ae0e4a8caf88aa
Elliot Smith <elliot.smith2@justice.gov.uk>
2026-07-13 15:05:15 +0100

    Extend test to cover post-submission document attachment

--- api/app/tests/Behat/features-v2/report-submission/document-synchronisation.feature
+++ api/app/tests/Behat/features-v2/report-submission/document-synchronisation.feature
@@ -2,17 +2,6 @@
 Feature: Synchronising Documents with Sirius
 
     @super-admin @prof-admin-health-welfare-submitted
-    Scenario: Submitting supporting documents after a report submission sets the synchronisation status to queued
-        Given a Professional Deputy has submitted a Health and Welfare report
-        And I attach a supporting document "testimage2.png" to the submitted report
-        And I send the documents to complete the upload process on the submitted report
-        And a super admin user accesses the admin app
-        And I visit the admin submissions page
-        And I search for submissions using the case number of the deputy I am interacting with and check the 'Pending' column
-        Then I should see the case number of the user I'm interacting with
-        And the document "testimage2.png" should be queued
-
-    @super-admin @prof-admin-health-welfare-submitted
     Scenario: Running the document-sync command syncs queued documents with Sirius
         Given a Professional Deputy has submitted a Health and Welfare report
         And I attach a supporting document "testimage3.png" to the submitted report
--- playwright/tests/test-document-synchronisation.spec.ts
+++ playwright/tests/test-document-synchronisation.spec.ts
@@ -12,7 +12,7 @@ import ReportReviewPage from "./pages/ReportReviewPage"
 
 const deputyReference = "document-synchronisation-user"
 
-test("submitting a report sets the report PDF document's synchronisation status to queued in the admin dashboard", async ({ page }) => {
+test("submitting a report sets documents' synchronisation status to queued in the admin dashboard", async ({ page }) => {
   const runTest = async (scenario: Scenario) => {
     const reportId = scenario.orders[0].reports[0].id
     const email = scenario.users[deputyReference].email
@@ -48,17 +48,32 @@ test("submitting a report sets the report PDF document's synchronisation status 
     await reportDeclarationPage.isExpected()
     await reportDeclarationPage.submitReport()
 
-    // check the report document's status in admin dashboard
-    const adminLoginPage = new AdminLoginPage(page)
-    await adminLoginPage.loginAdmin(getUserFixture("admin_user"))
-
     // check documents in admin UI
+    const adminUser = getUserFixture("admin_user")
+    const adminLoginPage = new AdminLoginPage(page)
+    await adminLoginPage.loginAdmin(adminUser)
+
     const adminClientSearchPage = new AdminDocumentsListPage(page)
     await adminClientSearchPage.goto()
     await adminClientSearchPage.openPendingTab()
     await adminClientSearchPage.search(caseNumber)
     await adminClientSearchPage.expectDocumentsWithStatuses(
       { name: /testimage1\.png/, status: "Queued" },
+      { name: new RegExp(`DigiRep-.+_${caseNumber}\.pdf`), status: "Queued" }
+    )
+
+    // submit another document on the already-submitted report
+    await documentsUploadPage.goto()
+    await documentsUploadPage.attachFiles(path.join(__dirname, `/testFiles/testimage2.png`))
+    await documentsUploadPage.sendDocuments()
+
+    // check newly-uploaded document is also queued
+    await adminClientSearchPage.goto()
+    await adminClientSearchPage.openPendingTab()
+    await adminClientSearchPage.search(caseNumber)
+    await adminClientSearchPage.expectDocumentsWithStatuses(
+      { name: /testimage1\.png/, status: "Queued" },
+      { name: /testimage2\.png/, status: "Queued" },
       { name: new RegExp(`DigiRep-.+_${caseNumber}\.pdf`), status: "Queued" }
     )
   }
@@ -73,5 +88,5 @@ test("submitting a report sets the report PDF document's synchronisation status 
 test("submitting a supporting document after a report submission sets that document's synchronisation status to queued in the admin dashboard", async ({ page }) => {
 })
 
-test("running the document sync command synchronises queued documents with Sirius", async ({ page }) => {
+test("running the document sync command in the admin UI synchronises queued documents with Sirius", async ({ page }) => {
 })
