dafae11933fe5a6778be8a9aaff2be72e29d9f5b
Elliot Smith <elliot.smith2@justice.gov.uk>
2026-07-16 15:48:31 +0100

    Remove mockery and prophecy from test

---
+++ api/app/tests/Unit/S3ClientMock.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\OPG\Digideps\Backend\Unit;
+
+use Aws\Result;
+use Aws\S3\S3Client;
+
+// because S3Client uses magic methods which phpunit can't mock
+class S3ClientMock extends S3Client
+{
+    public function getObject(...$names): Result
+    {
+        return new Result();
+    }
+}
