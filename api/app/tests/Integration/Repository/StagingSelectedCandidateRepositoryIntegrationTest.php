e421880be6db407fcc2b4f911033b4c737ec3b46
Elliot Smith <elliot.smith2@justice.gov.uk>
2026-07-28 15:58:30 +0100

    Add integration test to ensure candidates are de-duplicated
    ...and ordered by court order UID.

--- api/app/tests/Integration/Repository/StagingSelectedCandidateRepositoryIntegrationTest.php
+++ api/app/tests/Integration/Repository/StagingSelectedCandidateRepositoryIntegrationTest.php
@@ -6,15 +6,20 @@ namespace Tests\OPG\Digideps\Backend\Integration\Repository;
 
 use OPG\Digideps\Backend\Entity\Staging\StagingSelectedCandidate;
 use OPG\Digideps\Backend\Repository\StagingSelectedCandidateRepository;
+use OPG\Digideps\Backend\v2\Registration\Enum\DeputyshipCandidateAction;
 use Tests\OPG\Digideps\Backend\Integration\ApiIntegrationTestCase;
+use Tests\OPG\Digideps\Backend\Integration\Fixtures;
 
 class StagingSelectedCandidateRepositoryIntegrationTest extends ApiIntegrationTestCase
 {
+    private static Fixtures $fixtures;
     private static StagingSelectedCandidateRepository $sut;
 
     public static function setUpBeforeClass(): void
     {
         parent::setUpBeforeClass();
+
+        self::$fixtures = new Fixtures(self::$entityManager);
 
         /** @var StagingSelectedCandidateRepository $sut */
         $sut = self::$entityManager->getRepository(StagingSelectedCandidate::class);
@@ -23,7 +28,19 @@ class StagingSelectedCandidateRepositoryIntegrationTest extends ApiIntegrationTe
 
     public function testGetDistinctOrderedCandidates(): void
     {
-        $result = self::$sut->getDistinctOrderedCandidates();
-        self::assertNotEmpty(iterator_to_array($result));
+        // add two insert order candidates for the same court order UID
+        self::$fixtures->createSelectedCandidate(DeputyshipCandidateAction::InsertOrder, '12121212');
+        self::$fixtures->createSelectedCandidate(DeputyshipCandidateAction::InsertOrder, '12121212');
+
+        // add another insert order candidate for a different UID
+        self::$fixtures->createSelectedCandidate(DeputyshipCandidateAction::InsertOrder, '99999999');
+
+        $result = iterator_to_array(self::$sut->getDistinctOrderedCandidates());
+
+        // ensure that only one of the two duplicate candidates is returned
+        self::assertCount(2, $result);
+
+        // the results should be ordered by court order UID
+        self::assertTrue($result[1]['orderUid'] > $result[0]['orderUid']);
     }
 }
