70c4c20e608a61c43e863fd52bb3c0b2826d397d
Iqpal Mannan <iqpal.mannan@digital.justice.gov.uk>
2026-07-31 13:21:19 +0100

    Retry for connectivity issues

--- api/app/src/Service/Client/TokenStorage/RedisStorage.php
+++ api/app/src/Service/Client/TokenStorage/RedisStorage.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace OPG\Digideps\Backend\Service\Client\TokenStorage;
 
 use Predis\ClientInterface as PredisClientInterface;
+use Predis\Connection\ConnectionException;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 
 /**
@@ -23,17 +24,38 @@ class RedisStorage extends TokenStorage
 
     public function get($id): ?string
     {
-        return $this->redis->get($this->sessionPrefix . $id);
+        return $this->executeWithRetry(fn () => $this->redis->get($this->sessionPrefix . $id));
     }
 
     public function set($id, $value): void
     {
-        $this->redis->set($this->sessionPrefix . $id, $value);
+        $this->executeWithRetry(fn () => $this->redis->set($this->sessionPrefix . $id, $value));
     }
 
     public function remove($id): void
     {
-        $this->redis->set($this->sessionPrefix . $id, null);
-        $this->redis->expire($this->sessionPrefix . $id, 0);
+        $this->executeWithRetry(function () use ($id) {
+            $this->redis->set($this->sessionPrefix . $id, null);
+            $this->redis->expire($this->sessionPrefix . $id, 0);
+        });
+    }
+
+    private function executeWithRetry(callable $operation, int $maxRetries = 3, int $retryDelay = 1000)
+    {
+        $attempt = 0;
+
+        while ($attempt < $maxRetries) {
+            try {
+                return $operation();
+            } catch (ConnectionException $e) {
+                $attempt++;
+
+                if ($attempt >= $maxRetries) {
+                    throw new \RuntimeException("Operation failed after {$maxRetries} retries.", 0, $e);
+                }
+
+                usleep($retryDelay * 1000); // Delay in milliseconds
+            }
+        }
     }
 }
