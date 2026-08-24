3d4ebb8ab424a9ccbf4b6a69c5ac3df9900b270c
olivier-nicodemi <265032004+olivier-nicodemi@users.noreply.github.com>
2026-08-24 10:01:44 +0100

    DDLS-1660

---
+++ api/app/src/Entity/Cleanup/ReportCleanupProblem.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OPG\Digideps\Backend\Entity\Cleanup;
+
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\Table;
+use OPG\Digideps\Backend\Cleanup\Model\Problem;
+
+#[Table(name: 'report_cleanup_problem')]
+#[Entity]
+class ReportCleanupProblem
+{
+    public function __construct(
+        #[Column(name: 'client_id', type: 'integer', nullable: true)]
+        public readonly ?int $clientId,
+        #[Column(name: 'report_id', type: 'integer', nullable: true)]
+        public readonly ?int $reportId,
+        #[Column(name: 'order_id', type: 'integer', nullable: true)]
+        public readonly ?int $orderId,
+        #[Column(name: 'problem', type: 'integer', nullable: false, enumType: Problem::class)]
+        public readonly Problem $problem,
+        #[Id, GeneratedValue(strategy: 'IDENTITY'), Column(name: 'id', type: 'integer', nullable: true)]
+        public ?int $id = null
+    ) {
+    }
+}
---
+++ api/app/src/Migrations/Version320.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version320 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE report_cleanup_action (report_id INT NOT NULL, order_id INT NOT NULL, keep BOOLEAN NOT NULL, PRIMARY KEY(report_id, order_id))');
+        $this->addSql('CREATE TABLE report_cleanup_problem (id INT NOT NULL, client_id INT DEFAULT NULL, report_id INT DEFAULT NULL, order_id INT DEFAULT NULL, problem INT NOT NULL, PRIMARY KEY(id))');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE report_cleanup_action');
+        $this->addSql('DROP TABLE report_cleanup_problem');
+    }
+}
