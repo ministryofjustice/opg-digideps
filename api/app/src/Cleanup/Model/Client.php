9813e4ca5e8bb39ddb1c957e4237364f4de2ad44
olivier-nicodemi <265032004+olivier-nicodemi@users.noreply.github.com>
2026-08-24 10:01:44 +0100

    DDLS-1660

---
+++ api/app/src/Cleanup/Model/Client.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OPG\Digideps\Backend\Cleanup\Model;
+
+final readonly class Client
+{
+    /**
+     * @var array<Report>
+     */
+    public array $reports;
+    /**
+     * @var array<Order>
+     */
+    public array $orders;
+
+    public ?Order $activeOrder;
+
+    /**
+     * @param array<array<string, mixed>> $rows
+     */
+    public function __construct(
+        public int $clientId,
+        public string $caseNumber,
+        array $rows,
+    ) {
+        $orders = [];
+        $reports = [];
+
+        foreach ($rows as $row) {
+            $orderId = (int)$row['order_id'];
+            if (!array_key_exists($orderId, $orders)) {
+                $orders[$orderId] = new Order($this, $orderId, $row['order_uid'], new \DateTimeImmutable($row['order_made_date'])->setTime(0, 0), (bool)$row['open']);
+            }
+        }
+        $this->orders = $orders;
+        $activeOrders = array_filter($this->orders, fn (Order $order): bool => $order->open);
+        $activeOrder = null;
+        foreach ($activeOrders as $candidate) {
+            $activeOrder ??= $candidate;
+            if ($activeOrder->madeDate < $candidate->madeDate) {
+                $activeOrder = $candidate;
+            }
+        }
+        $this->activeOrder = $activeOrder;
+
+        foreach ($rows as $row) {
+            $orderId = (int)$row['order_id'];
+            $reportId = (int)$row['report_id'];
+            $reports[$reportId] ??= [
+                'orders' => [],
+                'report_id' => $reportId,
+                'start_date' => new \DateTimeImmutable($row['report_start_date'])->setTime(0, 0),
+                'end_date' => new \DateTimeImmutable($row['report_end_date'])->setTime(0, 0),
+                'submitted' => $row['submitted']
+            ];
+            $reports[$reportId]['orders'][$orderId] = $this->orders[$orderId];
+        }
+        $this->reports = array_map(fn (array $data): Report => new Report(
+            $this,
+            $data['report_id'],
+            $data['start_date'],
+            $data['end_date'],
+            $data['submitted'],
+            ...$data['orders']
+        ), $reports);
+    }
+}
