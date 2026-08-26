309b4e7d1c5914f3b00f2622aa7b82fc515595ef
Matty Cooke <matthew.cooke@justice.gov.uk>
2026-08-26 15:21:44 +0100

    added ok actions

--- terraform/account/region/pagerduty.tf
+++ terraform/account/region/pagerduty.tf
@@ -8,32 +8,33 @@ resource "aws_secretsmanager_secret" "pagerduty_integration_key" {
 }
 
 data "aws_secretsmanager_secret_version" "pagerduty_integration_key" {
-  count = var.account.pagerduty_enabled ? 1 : 0
+  count = local.pagerduty_is_enabled ? 1 : 0
 
   secret_id = aws_secretsmanager_secret.pagerduty_integration_key[0].id
 }
 
 locals {
-  pagerduty_endpoint = var.account.pagerduty_enabled ? "https://events.pagerduty.com/integration/${trimspace(data.aws_secretsmanager_secret_version.pagerduty_integration_key[0].secret_string)}/enqueue" : null
+  pagerduty_is_enabled = var.account.name == "production" && var.account.pagerduty_enabled
+  pagerduty_endpoint   = local.pagerduty_is_enabled ? "https://events.pagerduty.com/integration/${trimspace(data.aws_secretsmanager_secret_version.pagerduty_integration_key[0].secret_string)}/enqueue" : null
 }
 
 resource "aws_sns_topic_subscription" "pagerduty_alerts" {
-  count = var.account.pagerduty_enabled ? 1 : 0
+  count = local.pagerduty_is_enabled ? 1 : 0
 
   topic_arn              = aws_sns_topic.alerts.arn
   protocol               = "https"
   endpoint_auto_confirms = true
-  raw_message_delivery   = false
   endpoint               = local.pagerduty_endpoint
+  raw_message_delivery   = false
 }
 
 resource "aws_sns_topic_subscription" "pagerduty_availability_alerts" {
   provider = aws.global
-  count    = var.account.pagerduty_enabled ? 1 : 0
+  count    = local.pagerduty_is_enabled ? 1 : 0
 
   topic_arn              = aws_sns_topic.availability-alert.arn
   protocol               = "https"
   endpoint_auto_confirms = true
-  raw_message_delivery   = false
   endpoint               = local.pagerduty_endpoint
+  raw_message_delivery   = false
 }
