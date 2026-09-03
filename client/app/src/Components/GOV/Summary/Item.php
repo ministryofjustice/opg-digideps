36b946149a4b8a4ceb27e0a9f3587c04229b9e8b
olivier-nicodemi <265032004+olivier-nicodemi@users.noreply.github.com>
2026-09-03 10:59:45 +0100

    TaggedValue for Balance

--- client/app/src/Components/GOV/Summary/Item.php
+++ client/app/src/Components/GOV/Summary/Item.php
@@ -5,13 +5,12 @@ declare(strict_types=1);
 namespace OPG\Digideps\Frontend\Components\GOV\Summary;
 
 use OPG\Digideps\Frontend\Components\RenderableInterface;
-use Twig\Markup;
 
 final readonly class Item
 {
     public function __construct(
         public RenderableInterface|string $key,
-        public Markup|RenderableInterface|string $value,
+        public RenderableInterface|string $value,
         public null $action = null
     ) {
     }
