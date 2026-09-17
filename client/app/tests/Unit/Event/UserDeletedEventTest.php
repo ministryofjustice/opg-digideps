f61f1179f6f9df6cc6fdf1bfdd88034fee88c9b5
Elliot Smith <elliot.smith2@justice.gov.uk>
2026-09-17 13:42:59 +0100

    General cleanup (no prophecy)

--- client/app/tests/Unit/Event/UserDeletedEventTest.php
+++ client/app/tests/Unit/Event/UserDeletedEventTest.php
@@ -10,8 +10,7 @@ use PHPUnit\Framework\TestCase;
 
 class UserDeletedEventTest extends TestCase
 {
-    /** @test */
-    public function eventIsInitialisedCorrectly()
+    public function testEventIsInitialisedCorrectly(): void
     {
         $deletedUser = UserHelpers::createUser();
         $deletedBy = UserHelpers::createUser();
