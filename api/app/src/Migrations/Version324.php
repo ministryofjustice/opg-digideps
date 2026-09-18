158d08d3b7a97a30986a6301f855f7f99e9af858
olivier-nicodemi <265032004+olivier-nicodemi@users.noreply.github.com>
2026-09-18 09:21:18 +0100

    DDLS-1591 Disable Migrations TO REVERT

--- api/app/src/Migrations/Version324.php
+++ api/app/src/Migrations/Version324.php
@@ -20,15 +20,15 @@ final class Version324 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DELETE FROM report_submission WHERE report_id IS NULL');
-        $this->addSql('ALTER TABLE report_submission ALTER report_id SET NOT NULL');
-        $this->addSql('CREATE TABLE staging.sirius_organisation (domain VARCHAR(60) NOT NULL, name VARCHAR(256) DEFAULT NULL, local_id INT DEFAULT NULL, PRIMARY KEY(domain))');
+        //$this->addSql('DELETE FROM report_submission WHERE report_id IS NULL');
+        //$this->addSql('ALTER TABLE report_submission ALTER report_id SET NOT NULL');
+        //$this->addSql('CREATE TABLE staging.sirius_organisation (domain VARCHAR(60) NOT NULL, name VARCHAR(256) DEFAULT NULL, local_id INT DEFAULT NULL, PRIMARY KEY(domain))');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE staging.sirius_organisation');
-        $this->addSql('ALTER TABLE report_submission ALTER report_id DROP NOT NULL');
+        //$this->addSql('DROP TABLE staging.sirius_organisation');
+        //$this->addSql('ALTER TABLE report_submission ALTER report_id DROP NOT NULL');
     }
 }
